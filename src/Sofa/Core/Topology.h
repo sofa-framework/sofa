93339f684dc6366a45eab231ecf8c0a0bc0bba4c
Jeremie Allard <jeremie.allard@insimo.fr>
2006-03-10 21:57:17 +0000

    r182/sofa-dev : Missing file Topology.h

---
+++ src/Sofa/Core/Topology.h
@@ -0,0 +1,35 @@
+#ifndef SOFA_CORE_TOPOLOGY_H
+#define SOFA_CORE_TOPOLOGY_H
+
+#include <stdlib.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include "Sofa/Abstract/Base.h"
+
+namespace Sofa
+{
+
+namespace Core
+{
+
+class Topology : public virtual Abstract::Base
+{
+public:
+    virtual ~Topology() { }
+
+    // Access to embedded position information (in case the topology is a regular grid for instance)
+    // This is not very clean and is quit slow but it should only be used during initialization
+
+    virtual bool hasPos() { return false; }
+    virtual int getNbPoints() { return 0; }
+    virtual double getPX(int /*i*/) { return 0.0; }
+    virtual double getPY(int /*i*/) { return 0.0; }
+    virtual double getPZ(int /*i*/) { return 0.0; }
+};
+
+} // namespace Core
+
+} // namespace Sofa
+
+#endif
