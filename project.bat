db606f99b4125d35d495ffe89d5972de08ebc3d1
Jeremie Allard <jeremie.allard@insimo.fr>
2006-05-04 16:05:08 +0000

    r235/sofa-dev : Better support for Visual C++ 2005 (less warnings)
     //partial sync (2/7 changes)//

--- project.bat
+++ project.bat
@@ -3,10 +3,11 @@ REM This batch permit to create all dsp project or vcproj project for
 REM Visual C++ 6 or Visual dot Net 2003
 REM use : project [VC6 / VC7 / clean]
 REM default visual project depends on the environment variable QMAKESPEC
-set QTDIR=D:\Sofa\tools
+set QTDIR=%CD%\tools
 set PATH=%QTDIR%;%PATH%
 if "%1" == "VC6" goto vc6
 if "%1" == "VC7" goto vc7
+if "%1" == "VC8" goto vc8
 if "%1" == "clean" goto clean
 :vc6
 set QMAKESPEC=win32-msvc
@@ -18,12 +19,16 @@ goto common
 set QMAKESPEC=win32-msvc.net
 type sofaVC7.cfg > sofa.cfg
 type sofaCommon.cfg >> sofa.cfg
-rem copy /A sofaVC7.cfg+sofaCommon.cfg sofa.cfg
+goto common
+:vc8
+set QMAKESPEC=win32-msvc2005
+type sofaVC8.cfg > sofa.cfg
+type sofaCommon.cfg >> sofa.cfg
 :common
 @echo on
 @if "%QMAKESPEC%"=="win32-msvc" @echo Making Visual project 6
 @if "%QMAKESPEC%"=="win32-msvc.net" @echo Making Visual project 7
-@echo off
+@if "%QMAKESPEC%"=="win32-msvc2005" @echo Making Visual project 8
 for /R %%i in (*.pro) do qmake "%%i"
 goto end
 :clean
