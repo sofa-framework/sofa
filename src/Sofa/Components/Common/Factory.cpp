480bdead6d9ce278d42233caaac819c51434856b
Jeremie Allard <jeremie.allard@insimo.fr>
2006-05-26 01:28:30 +0000

    r271/sofa-dev : More readable class names and graph displays.
     //partial sync (1/4 changes)//

--- src/Sofa/Components/Common/Factory.cpp
+++ src/Sofa/Components/Common/Factory.cpp
@@ -26,17 +26,25 @@ std::string gettypename(const std::type_info& t)
         name = realname;
         free(realname);
         // Shorten name
-        for(;;)
-        {
-            std::string::size_type pos = name.find("::");
-            if (pos == std::string::npos) break;
-            std::string::size_type first = name.find_last_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_",pos-1);
-            if (first == std::string::npos) first = 0;
-            else first++;
-            name.erase(first,pos-first+2);
-        }
     }
 #endif
+    // Remove namespaces
+    for(;;)
+    {
+        std::string::size_type pos = name.find("::");
+        if (pos == std::string::npos) break;
+        std::string::size_type first = name.find_last_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_",pos-1);
+        if (first == std::string::npos) first = 0;
+        else first++;
+        name.erase(first,pos-first+2);
+    }
+    //Remove "class "
+    for(;;)
+    {
+        std::string::size_type pos = name.find("class ");
+        if (pos == std::string::npos) break;
+        name.erase(pos,6);
+    }
     return name;
 }
 
