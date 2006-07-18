a6d97b76a98d4816cdcab697d50c52a9b7d53a60
Herve Delingette <herve.delingette@inria.fr>
2006-07-18 14:44:49 +0000

    r365/sofa-dev : added plane constraint

---
+++ src/Sofa/Components/FixedPlaneConstraint.cpp
@@ -0,0 +1,71 @@
+#include "FixedPlaneConstraint.inl"
+#include "Sofa/Components/Common/ObjectFactory.h"
+#include "Sofa/Components/Common/Vec3Types.h"
+#include "Sofa/Components/Common/RigidTypes.h"
+#include "GL/Repere.h"
+
+namespace Sofa
+{
+
+namespace Components
+{
+
+using namespace Common;
+
+
+
+SOFA_DECL_CLASS(FixedPlaneConstraint)
+
+template class FixedPlaneConstraint<Vec3dTypes>;
+template class FixedPlaneConstraint<Vec3fTypes>;
+
+namespace Common   // \todo Why this must be inside Common namespace
+{
+
+
+template<class DataTypes>
+void create(FixedPlaneConstraint<DataTypes>*& obj, ObjectDescription* arg)
+{
+    typedef typename DataTypes::Coord::value_type   Real;
+    typedef typename DataTypes::Coord   Coord;
+
+    XML::createWithParent< FixedPlaneConstraint<DataTypes>, Core::MechanicalModel<DataTypes> >(obj, arg);
+    if (obj!=NULL)
+    {
+        if (arg->getAttribute("indices"))
+        {
+            const char* str = arg->getAttribute("indices");
+            const char* str2 = NULL;
+            for(;;)
+            {
+                int v = (int)strtod(str,(char**)&str2);
+                if (str2==str) break;
+                str = str2;
+                obj->addConstraint(v);
+            }
+        }
+        if (arg->getAttribute("direction"))
+        {
+            const char* str = arg->getAttribute("direction");
+            const char* str2 = NULL;
+            Real val[3];
+            unsigned int i;
+            for(i=0; i<3; i++)
+            {
+                val[i] = (Real)strtod(str,(char**)&str2);
+                if (str2==str) break;
+                str = str2;
+            }
+            Coord dir(val);
+            obj->setDirection(dir);
+        }
+    }
+}
+}
+
+Creator< ObjectFactory, FixedPlaneConstraint<Vec3dTypes> > FixedPlaneConstraint3dClass("FixedPlaneConstraint",true);
+Creator< ObjectFactory, FixedPlaneConstraint<Vec3fTypes> > FixedPlaneConstraint3fClass("FixedPlaneConstraint",true);
+
+} // namespace Components
+
+} // namespace Sofa
