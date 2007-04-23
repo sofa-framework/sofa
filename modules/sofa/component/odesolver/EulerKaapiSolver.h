ea431920e0f49965e2494ec08aa9cf5462f087c2
ehermann <ehermann@users.gforge.inria.fr>
2007-04-23 15:48:30 +0000

    r1031/sofa-dev : Begin of a monitored version of the EulerSolver

---
+++ modules/sofa/component/odesolver/EulerKaapiSolver.h
@@ -0,0 +1,46 @@
+#ifndef SOFA_COMPONENT_ODESOLVER_EULERSOLVER_H
+#define SOFA_COMPONENT_ODESOLVER_EULERSOLVER_H
+
+#include <sofa/core/componentmodel/behavior/OdeSolver.h>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace odesolver
+{
+
+/** The simplest time integration.
+Two variants are available, depending on the value of field "symplectic".
+If true (the default), the symplectic variant of Euler's method is applied:
+If false, the basic Euler's method is applied (less robust)
+*/
+class EulerKaapiSolver : public core::componentmodel::behavior::OdeSolver
+{
+public:
+    EulerKaapiSolver();
+    void solve (double dt);
+    void computeAcc (double t, VecId a, VecId x, VecId v);
+    DataField<bool> symplectic;
+    void v_free(VecId v);
+    void v_clear(VecId v); ///< v=0
+    VecId v_alloc(VecId::Type t);
+
+    void propagatePositionAndVelocity(double t, VecId x, VecId v);
+    void computeForce(VecId result);
+    void accFromF(VecId a, VecId f);
+    void projectResponse(VecId dx, double **W=NULL);
+    void v_peq(VecId v, VecId a, double f); ///< v+=f*a
+
+
+};
+
+} // namespace odesolver
+
+} // namespace component
+
+} // namespace sofa
+
+#endif
