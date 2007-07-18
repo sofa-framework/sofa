d2b5e04dcf67a74a4a8e6cc1833557d88b492cbd
ehermann <ehermann@users.gforge.inria.fr>
2007-07-18 12:59:46 +0000

    r1328/sofa-dev : Added a Ray Triangle intersection method

---
+++ modules/sofa/component/collision/RayTriangleIntersection.cpp
@@ -0,0 +1,87 @@
+/*******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 1       *
+*                (c) 2006-2007 MGH, INRIA, USTL, UJF, CNRS                     *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Contact information: contact@sofa-framework.org                              *
+*                                                                              *
+* Authors: J. Allard, P-J. Bensoussan, S. Cotin, C. Duriez, H. Delingette,     *
+* F. Faure, S. Fonteneau, L. Heigeas, C. Mendoza, M. Nesme, P. Neumann,        *
+* and F. Poyer                                                                 *
+*******************************************************************************/
+#include "RayTriangleIntersection.h"
+#include <sofa/helper/LCPSolver.inl>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace collision
+{
+
+RayTriangleIntersection::RayTriangleIntersection()
+{
+}
+RayTriangleIntersection::~RayTriangleIntersection()
+{
+}
+bool
+RayTriangleIntersection::NewComputation(Triangle *triP, const Vector3 &origin, const Vector3 &direction,   double &t,  double &u, double &v)
+
+{
+    t = 0; u = 0; v = 0;
+
+    Vector3 edge1 = triP->p2() - triP->p1();
+    Vector3 edge2 = triP->p3() - triP->p1();
+
+    Vector3 tvec, pvec, qvec;
+    double det, inv_det;
+
+    pvec = direction.cross(edge2);
+
+    det = dot(edge1, pvec);
+
+    if (fabs(triP->n()*direction) < 0.00001f)
+        return false;
+
+    inv_det = 1.0f / det;
+
+    tvec = origin - triP->p1();
+
+    u = dot(tvec, pvec) * inv_det;
+    if (u < 0.00001f||u >1)
+        return false;
+
+    qvec = tvec.cross(edge1);
+
+    v = dot(direction, qvec) * inv_det;
+    if (v < -0.001f || (u +v) > 1.001f)
+        return false;
+
+    t = dot(edge2, qvec) * inv_det;
+
+    if (t < 0)
+        return false;
+
+    return true;
+}
+
+}
+}
+}
+
---
+++ modules/sofa/component/collision/RayTriangleIntersection.h
@@ -0,0 +1,71 @@
+/*******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 1       *
+*                (c) 2006-2007 MGH, INRIA, USTL, UJF, CNRS                     *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Contact information: contact@sofa-framework.org                              *
+*                                                                              *
+* Authors: J. Allard, P-J. Bensoussan, S. Cotin, C. Duriez, H. Delingette,     *
+* F. Faure, S. Fonteneau, L. Heigeas, C. Mendoza, M. Nesme, P. Neumann,        *
+* and F. Poyer                                                                 *
+*******************************************************************************/
+#ifndef SOFA_COMPONENT_RAYTRIANGLEINTERSECTION_H
+#define SOFA_COMPONENT_RAYTRIANGLEINTERSECTION_H
+
+#include <sofa/component/collision/Triangle.h>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace collision
+{
+
+
+//-----------------------------------------------------------------------------
+//--IntersectionRayTriangle--
+//------------------
+// this class computes if a Triangle P intersects a line segment
+//-----------------------------------------------------------------------------
+
+class RayTriangleIntersection
+{
+public:
+    RayTriangleIntersection(); // start a Proximity solver
+    ~RayTriangleIntersection();
+
+    // init the solver with the new coordinates of the triangle & the segment
+    // solve the lcp
+    bool NewComputation( Triangle *triP, const sofa::defaulttype::Vector3 &origin, const sofa::defaulttype::Vector3 &direction,  double &t,  double &u, double &v);
+
+    //double getAlphaP(){return _result[6];}
+    //double getBetaP(){return _result[7];}
+    //double getAlphaQ(){return _result[8];}
+    //double getBetaQ(){return _result[9];}
+
+
+private:
+    //double **_A;
+    //double *_b;
+    //double *_result;
+};
+
+}
+}
+}
+#endif
