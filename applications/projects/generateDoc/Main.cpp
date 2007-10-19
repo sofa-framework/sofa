4d66e7ee11bcd9edfee261c2e2f2488be9fcd68c
Jeremie Allard <jeremie.allard@insimo.fr>
2007-10-19 13:13:43 +0000

    r1501/sofa-dev : FIX compilation (remove unnecessary viewer/Main.h includes in generateDoc and GenerateRigid).

--- applications/projects/generateDoc/Main.cpp
+++ applications/projects/generateDoc/Main.cpp
@@ -2,15 +2,6 @@
 #include <sofa/simulation/tree/init.h>
 #include <iostream>
 #include <fstream>
-#ifdef SOFA_GUI_FLTK
-#include <sofa/gui/fltk/Main.h>
-#elif  SOFA_GUI_QTVIEWER
-#include <sofa/gui/viewer/Main.h>
-#elif  SOFA_GUI_QGLVIEWER
-#include <sofa/gui/viewer/Main.h>
-#elif  SOFA_GUI_QTOGREVIEWER
-#include <sofa/gui/viewer/Main.h>
-#endif
 
 int main(int /*argc*/, char** /*argv*/)
 {
