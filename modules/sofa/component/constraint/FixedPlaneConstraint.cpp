251f4905a8c742894fc005375449fae9e3adb141
Herve Delingette <herve.delingette@inria.fr>
2007-11-09 07:51:49 +0000

    r1572/sofa-dev : Modified constraint to use PointSubset in order to handle topological changes

--- modules/sofa/component/constraint/FixedPlaneConstraint.cpp
+++ modules/sofa/component/constraint/FixedPlaneConstraint.cpp
@@ -44,7 +44,7 @@ template class FixedPlaneConstraint<Vec3dTypes>;
 template class FixedPlaneConstraint<Vec3fTypes>;
 
 
-int FixedPlaneConstraintClass = core::RegisterObject("TODO-FixedPlaneConstraintClass")
+int FixedPlaneConstraintClass = core::RegisterObject("FixedPlaneConstraint")
         .add< FixedPlaneConstraint<Vec3dTypes> >()
         .add< FixedPlaneConstraint<Vec3fTypes> >()
         ;
