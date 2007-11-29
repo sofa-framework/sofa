0c3fa6c47498229d5a0323edbaf11ef5bb134e20
Herve Delingette <herve.delingette@inria.fr>
2007-11-29 08:24:41 +0000

    r1675/sofa-dev : ADD : new force field that support topological changes on tetrahedral meshes. TetrahedralCorotationalFEMForceField is a translation of TetrahedronFEMForceField using TetrahedronData container. Needs further testing
     //partial sync (3/6 changes)//

---
+++ modules/sofa/component/forcefield/TetrahedralCorotationalFEMForceField.cpp
@@ -0,0 +1,60 @@
+/*******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 1       *
+*                (c) 2006-2007 MGH, INRIA, USTL, UJF, CNRS                     *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Contact information: contact@sofa-framework.org                              *
+*                                                                              *
+* Authors: J. Allard, P-J. Bensoussan, S. Cotin, C. Duriez, H. Delingette,     *
+* F. Faure, S. Fonteneau, L. Heigeas, C. Mendoza, M. Nesme, P. Neumann,        *
+* and F. Poyer                                                                 *
+*******************************************************************************/
+#include <sofa/component/forcefield/TetrahedralCorotationalFEMForceField.inl>
+#include <sofa/defaulttype/Vec3Types.h>
+#include <sofa/component/MechanicalObject.h>
+#include <sofa/core/ObjectFactory.h>
+//#include <typeinfo>
+
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace forcefield
+{
+
+using namespace sofa::defaulttype;
+
+template class TetrahedralCorotationalFEMForceField<Vec3dTypes>;
+template class TetrahedralCorotationalFEMForceField<Vec3fTypes>;
+
+
+SOFA_DECL_CLASS(TetrahedralCorotationalFEMForceField)
+
+// Register in the Factory
+int TetrahedralCorotationalFEMForceFieldClass = core::RegisterObject("Corotational FEM Tetrahedral finite elements")
+        .add< TetrahedralCorotationalFEMForceField<Vec3dTypes> >()
+        .add< TetrahedralCorotationalFEMForceField<Vec3fTypes> >()
+        ;
+
+} // namespace forcefield
+
+} // namespace component
+
+} // namespace sofa
+
