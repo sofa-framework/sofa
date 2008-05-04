ff93c96db8a5a63d05e2edf0c43d2f596d774902
Francois Faure <francois.faure@inria.fr>
2008-05-04 11:08:42 +0000

    r2265/sofa-dev : FIX visit pruning in Boostkernel
     //partial sync (3/7 changes)//

--- applications/projects/BoostKernel/lib/dfs_adapter.h
+++ applications/projects/BoostKernel/lib/dfs_adapter.h
@@ -25,7 +25,13 @@ namespace bgl
 {
 
 /**
-Adapt a sofa visitor to depth-first search in a bgl mapping graph.
+Adapt a sofa visitor to a depth-first visit in a bgl graph encoding the mechanical mapping hierarchy.
+This visitor is aimed to be used by a depth first visit with a pruning criterion.
+The criterion is embedded within the dfs_adapter.
+The usual method discover_vertex is replaced by operator(), used by the bgl to evaluate if the visit must be pruned.
+Operator () calls visitor->processNodeTopDown and returns true iff this method has returned RESULT_PRUNE.
+
+The BglScene::H_vertex_node_map is used to get the sofa::simulation::Node associated with a bgl vertex.
 
 	@author The SOFA team </www.sofa-framework.org>
 */
@@ -35,14 +41,19 @@ public:
     sofa::simulation::tree::Visitor* visitor;
 
     typedef BglScene::Hgraph Graph; ///< BGL graph to traverse
+    typedef Graph::vertex_descriptor Vertex;
+
     BglScene::H_vertex_node_map& systemMap;      ///< access the System*
 
     dfs_adapter( sofa::simulation::tree::Visitor* v, BglScene::H_vertex_node_map& s );
 
     ~dfs_adapter();
 
-    void discover_vertex( Graph::vertex_descriptor u, const Graph &) const;
-    void finish_vertex(Graph::vertex_descriptor u, const Graph &) const;
+    /// Applies visitor->processNodeTopDown
+    bool operator() (Vertex u, const Graph &);
+
+    /// Applies visitor->processNodeBottomUp
+    void finish_vertex(Vertex u, const Graph &) const;
 
 };
 }
