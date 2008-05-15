4792082a9688b09b6807de93ef87b5c8e7b9fd0a
Frederick Roy <frederick.roy@gmail.com>
2008-05-15 13:21:51 +0000

    r2335/sofa-dev : Update : -setting uniform vectors (int and float)          -OglShader: can set now output/input types and max vertices for geometry shader

--- modules/sofa/component/visualmodel/OglShader.h
+++ modules/sofa/component/visualmodel/OglShader.h
@@ -34,6 +34,12 @@ protected:
     Data<std::string> vertFilename;
     Data<std::string> fragFilename;
     Data<std::string> geoFilename;
+
+    //TODO replace it?
+    Data<int> geometryInputTypes;
+    Data<int> geometryOutputTypes;
+    Data<int> geometryVerticesOut;
+
     sofa::helper::gl::CShader m_shader;
 
     bool hasGeometryShader;
@@ -44,6 +50,7 @@ public:
 
     void initVisual();
     void init();
+    void reinit();
     void drawVisual();
     void updateVisual();
 
@@ -51,11 +58,26 @@ public:
     void stop();
 
     void setTexture(const char* name, unsigned short unit);
-    void setInt(const char* name, unsigned int i);
+
+    void setInt(const char* name, int i);
+    void setInt2(const char* name, int i1, int i2);
+    void setInt3(const char* name, int i1, int i2, int i3);
+    void setInt4(const char* name, int i1, int i2, int i3, int i4);
+
     void setFloat(const char* name, float f1);
     void setFloat2(const char* name, float f1, float f2);
     void setFloat3(const char* name, float f1, float f2, float f3);
     void setFloat4(const char* name, float f1, float f2, float f3, float f4);
+
+    void setIntVector(const char* name, int count, const int* i);
+    void setIntVector2(const char* name, int count, const int* i);
+    void setIntVector3(const char* name, int count, const int* i);
+    void setIntVector4(const char* name, int count, const int* i);
+
+    void setFloatVector(const char* name, int count, const float* f);
+    void setFloatVector2(const char* name, int count, const float* f);
+    void setFloatVector3(const char* name, int count, const float* f);
+    void setFloatVector4(const char* name, int count, const float* f);
 
     GLint getGeometryInputType() ;
     void  setGeometryInputType(GLint v) ;
--- modules/sofa/component/visualmodel/OglVariable.h
+++ modules/sofa/component/visualmodel/OglVariable.h
@@ -25,9 +25,61 @@ public:
     OglVariable() { };
     virtual ~OglVariable() { };
 
+    virtual void init() { OglShaderElement::init(); };
     virtual void initVisual() { }
-    virtual void reinit() { initVisual(); }
+    virtual void reinit() { init(); initVisual(); }
 };
+
+/** SINGLE INT VARIABLE **/
+class OglIntVariable : public OglVariable
+{
+private:
+    Data<int> value;
+
+public:
+    OglIntVariable();
+    virtual ~OglIntVariable() { };
+
+    void initVisual();
+};
+
+class OglInt2Variable : public OglVariable
+{
+private:
+    Data<defaulttype::Vec<2, int> > value;
+
+public:
+    OglInt2Variable();
+    virtual ~OglInt2Variable() { };
+
+    void initVisual();
+};
+
+class OglInt3Variable : public OglVariable
+{
+private:
+    Data<defaulttype::Vec<3, int> > value;
+
+public:
+    OglInt3Variable();
+    virtual ~OglInt3Variable() { };
+
+    void initVisual();
+};
+
+class OglInt4Variable : public OglVariable
+{
+private:
+    Data<defaulttype::Vec<4, int> > value;
+
+public:
+    OglInt4Variable();
+    virtual ~OglInt4Variable() { };
+
+    void initVisual();
+};
+
+/** SINGLE FLOAT VARIABLE **/
 
 class OglFloatVariable : public OglVariable
 {
@@ -63,6 +115,107 @@ public:
     virtual ~OglFloat3Variable() { };
 
     void initVisual();
+};
+
+class OglFloat4Variable : public OglVariable
+{
+private:
+    Data<defaulttype::Vec4f> value;
+
+public:
+    OglFloat4Variable();
+    virtual ~OglFloat4Variable() { };
+
+    void initVisual();
+};
+
+/** INT VECTOR VARIABLE **/
+class OglIntVectorVariable : public OglVariable
+{
+protected:
+    Data<helper::vector<int> > iv;
+
+public:
+    OglIntVectorVariable();
+    virtual ~OglIntVectorVariable() { };
+
+    virtual void init();
+    virtual void initVisual();
+};
+
+class OglIntVector2Variable : public OglIntVectorVariable
+{
+
+public:
+    OglIntVector2Variable();
+    virtual ~OglIntVector2Variable() { };
+
+    virtual void init();
+    virtual void initVisual();
+};
+
+class OglIntVector3Variable : public OglIntVectorVariable
+{
+public:
+    OglIntVector3Variable();
+    virtual ~OglIntVector3Variable() { };
+
+    virtual void init();
+    virtual void initVisual();
+};
+
+class OglIntVector4Variable : public OglIntVectorVariable
+{
+public:
+    OglIntVector4Variable();
+    virtual ~OglIntVector4Variable() { };
+
+    virtual void init();
+    virtual void initVisual();
+};
+
+/** FLOAT VECTOR VARIABLE **/
+class OglFloatVectorVariable : public OglVariable
+{
+protected:
+    Data<helper::vector<float> > fv;
+
+public:
+    OglFloatVectorVariable();
+    virtual ~OglFloatVectorVariable() { };
+
+    virtual void init();
+    virtual void initVisual();
+};
+
+class OglFloatVector2Variable : public OglFloatVectorVariable
+{
+public:
+    OglFloatVector2Variable();
+    virtual ~OglFloatVector2Variable() { };
+
+    virtual void init();
+    virtual void initVisual();
+};
+
+class OglFloatVector3Variable : public OglFloatVectorVariable
+{
+public:
+    OglFloatVector3Variable();
+    virtual ~OglFloatVector3Variable() { };
+
+    virtual void init();
+    virtual void initVisual();
+};
+
+class OglFloatVector4Variable : public OglFloatVectorVariable
+{
+public:
+    OglFloatVector4Variable();
+    virtual ~OglFloatVector4Variable() { };
+
+    virtual void init();
+    virtual void initVisual();
 };
 
 }
