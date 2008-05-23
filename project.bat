834db59d8ca9747dd2f197c0cb7f0b82bdfa4aa4
madam <madam@users.gforge.inria.fr>
2008-05-23 13:43:22 +0000

    r2412/sofa-dev : ADD : Visual 2008 (VC9) support. warning, current version of qt distributed with Sofa doesn't support VC9 project generation. You need to generate a VC8 project and then to convert it into VC9. Also, debug mode doesn't work on VC9 with this version of qt (and QGLviewer too). Solution is to use the latest version of qt (4.4.x) compiled on VC9. Maybe we should provide specific dependencies packages for VC8 and VC9 for the next release. By the way, just ask me if you need the qt and qglviewer binaries to use with VC9.
     //partial sync (3/11 changes)//

--- project.bat
+++ project.bat
@@ -1,39 +1,56 @@
 @echo off
 REM This batch permit to create all dsp project or vcproj project for
 REM Visual C++ 2003 or 2005
-REM use : project [VC7 / VC8 / clean]
-REM default visual project depends on the environment variable QMAKESPEC
+REM use : project [VC7 / VC8 / vc9 / clean]
+REM default visual project depends on the environment variable QMAKESPEC 
+
+@if not "%QMAKEPATH%" == ""  goto params
 set QMAKEPATH=%CD%\tools\qt4win
 set QTDIR=%CD%\tools\qt4win
 set PATH=%QMAKEPATH%\bin;%PATH%
+
+
+:params
 if "%1" == "VC7" goto vc7
 if "%1" == "VC8" goto vc8
+if "%1" == "VC9" goto vc9
 if "%1" == "clean" goto clean
+
 :console
 @echo Making Makefiles
 qmake -recursive
 @goto end
+
 :vc7
 set QMAKESPEC=win32-msvc.net
 goto common
+
 :vc8
 set QMAKESPEC=win32-msvc2005
+goto common
+
+:vc9
+set QMAKESPEC=win32-msvc2008
+
 :common
 @echo on
 @if "%QMAKESPEC%"=="win32-msvc.net" @echo Making Visual project 7
 @if "%QMAKESPEC%"=="win32-msvc2005" @echo Making Visual project 8
+@if "%QMAKESPEC%"=="win32-msvc2008" @echo Making Visual project 9
 @REM cd src
 @REM for /R %%i in (*.pro) do qmake "%%i"
 @REM cd ..
-qmake -tp vc -recursive -o Sofa QT_INSTALL_PREFIX="%QTDIR%"
+qmake -tp vc -recursive -o Sofa.sln Sofa.pro QT_INSTALL_PREFIX="%QTDIR%"
 @if "%ERRORLEVEL%"=="0" goto end
 echo ERROR %ERRORLEVEL%
 pause
 @goto end
+
 :clean
 @echo cleaning all VC Projects
 for /R %%i in (*.ncb, *.suo, Makefile, *.idb, *.pdb, *.plg, *.opt) do del "%%i"
 cd src
 for /R %%i in (*.dsp, *.vcproj, *.vcproj.*) do del "%%i"
 cd ..
+
 :end
