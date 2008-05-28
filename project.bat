171cc708f2f0df6b5e427335f74237cfaf82ba35
lenka <lenka@users.gforge.inria.fr>
2008-05-28 09:20:59 +0000

    r2431/sofa-dev : FIX: use different qmake command lines for VC8 and VC9 due to problems with the extensions of generated files (e.g., Sofa.sln.sln or Sofa was generated instead of Sofa.sln)

--- project.bat
+++ project.bat
@@ -9,7 +9,6 @@ set QMAKEPATH=%CD%\tools\qt4win
 set QTDIR=%CD%\tools\qt4win
 set PATH=%QMAKEPATH%\bin;%PATH%
 
-
 :params
 if "%1" == "VC7" goto vc7
 if "%1" == "VC8" goto vc8
@@ -17,30 +16,33 @@ if "%1" == "VC9" goto vc9
 if "%1" == "clean" goto clean
 
 :console
+@echo on
 @echo Making Makefiles
 qmake -recursive
 @goto end
 
 :vc7
 set QMAKESPEC=win32-msvc.net
+@echo on
+@echo Making Visual project 7
+qmake -tp vc -recursive -o Sofa QT_INSTALL_PREFIX="%QTDIR%"
 goto common
 
 :vc8
 set QMAKESPEC=win32-msvc2005
+@echo on
+@echo Making Visual project 8
+qmake -tp vc -recursive -o Sofa QT_INSTALL_PREFIX="%QTDIR%"
 goto common
 
 :vc9
 set QMAKESPEC=win32-msvc2008
+@echo on
+@echo Making Visual project 9
+qmake -tp vc -recursive -o Sofa.sln Sofa.pro QT_INSTALL_PREFIX="%QTDIR%"
+goto common
 
 :common
-@echo on
-@if "%QMAKESPEC%"=="win32-msvc.net" @echo Making Visual project 7
-@if "%QMAKESPEC%"=="win32-msvc2005" @echo Making Visual project 8
-@if "%QMAKESPEC%"=="win32-msvc2008" @echo Making Visual project 9
-@REM cd src
-@REM for /R %%i in (*.pro) do qmake "%%i"
-@REM cd ..
-qmake -tp vc -recursive -o Sofa.sln Sofa.pro QT_INSTALL_PREFIX="%QTDIR%"
 @if "%ERRORLEVEL%"=="0" goto end
 echo ERROR %ERRORLEVEL%
 pause
