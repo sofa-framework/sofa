83491370d1252323409dbf803965aa0c0437782f
gurvanlm <gurvanlm@users.gforge.inria.fr>
2008-06-13 12:52:47 +0000

    r2523/sofa-dev : FIX : Some link issues for Chai3D Implementation under linux and windows REMOVE: chai3D dependencies files have been removed from repository and put in a zip file you can download on the "fichiers" tab on the gforge page. Read the Sofa/applications/projects/Haptic/doc/HOWTO.html for more details init.cpp : remove the dependencies with HapticMapping sofa.cfg : remove Chai3D link directives : now they are inside the Haptic project private.txt : chai3d dependencies are no more an issue
     //partial sync (2/11 changes)//

--- modules/sofa/simulation/common/init.cpp
+++ modules/sofa/simulation/common/init.cpp
@@ -219,9 +219,7 @@ SOFA_LINK_CLASS(OglFloatVector4Variable)
 SOFA_LINK_CLASS(OglTetrahedralModel)
 SOFA_LINK_CLASS(VoidMapping)
 
-#ifdef SOFA_HAVE_CHAI3D
-SOFA_LINK_CLASS(HapticMapping)
-#endif // SOFA_HAVE_CHAI3D
+
 
 #ifdef SOFA_DEV
 
