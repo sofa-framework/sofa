560afea903b1138bbec20bc52eae7a387ba7ef3a
Juan Pablo De La Alcade <juanpablo.delaplata@insimo.fr>
2008-07-02 17:41:05 +0000

    r2645/sofa-dev : ADDED: a new controller to use .bvh files with the Articulated Hierachy Container. The old method (using the compliance solver) is removed. The examples which used it, are updated.
     //partial sync (4/8 changes)//

---
+++ modules/sofa/component/controller/ArticulatedHierarchyBVHController.h
@@ -0,0 +1,103 @@
+/*******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 1       *
+*                (c) 2006-2007 MGH, INRIA, USTL, UJF, CNRS                     *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Contact information: contact@sofa-framework.org                              *
+*                                                                              *
+* Authors: J. Allard, P-J. Bensoussan, S. Cotin, C. Duriez, H. Delingette,     *
+* F. Faure, S. Fonteneau, L. Heigeas, C. Mendoza, M. Nesme, P. Neumann,        *
+* and F. Poyer                                                                 *
+*******************************************************************************/
+//
+// C++ Implementation: ArticulatedHierarchyBVHController
+//
+// Description:
+//
+//
+// Author: The SOFA team </www.sofa-framework.org>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef SOFA_COMPONENT_CONTROLLER_ARTICULATEDHIERARCHYBVHCONTROLLER_H
+#define SOFA_COMPONENT_CONTROLLER_ARTICULATEDHIERARCHYBVHCONTROLLER_H
+
+#include <sofa/component/controller/ArticulatedHierarchyController.h>
+#include <sofa/helper/io/bvh/BVHLoader.h>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace controller
+{
+
+/**
+ * @brief ArticulatedHierarchyController Class.
+ *
+ * Implements a handler that controls the values of the
+ * articulations of an articulated hierarchy container.
+ * .bvh files are controlling the value.
+ */
+class ArticulatedHierarchyBVHController : public ArticulatedHierarchyController
+{
+public:
+
+    /**
+     * @brief Default Constructor.
+     */
+    ArticulatedHierarchyBVHController()
+    {
+        this->f_listening.setValue(true);
+    };
+
+    /**
+     * @brief Default Destructor.
+     */
+    virtual ~ArticulatedHierarchyBVHController() {};
+
+    /**
+     * @brief Init method called during the scene graph initialization.
+     */
+    virtual void init();
+
+    /**
+     * @brief Reset to initial state
+     */
+    virtual void reset();
+
+    /**
+     * @brief Apply the controller current modifications to its controled component.
+     */
+    virtual void applyController(void);
+
+protected:
+    ArtCenterVec m_artCenterVec; ///< List of ArticulationCenters controlled by the controller.
+    ArticulatedHierarchyContainer* ahc;
+    int frame;
+    int n;
+};
+
+} // namespace controller
+
+} // namespace component
+
+} // namespace sofa
+
+#endif //SOFA_COMPONENT_CONTROLLER_ARTICULATEDHIERARCHYBVHCONTROLLER_H
