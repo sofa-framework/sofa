75ed931f6cce39047e8fdb1fb3ae90f023564c21
Jeremie Allard <jeremie.allard@insimo.fr>
2008-09-17 16:29:26 +0000

    r3073/sofa-dev : BUGFIX: edges creation from facets in MeshTopologyLoader was not removing duplicates (introduced during refactoring in commit r2932), breaking neighbor queries

--- framework/sofa/helper/io/MeshTopologyLoader.cpp
+++ framework/sofa/helper/io/MeshTopologyLoader.cpp
@@ -78,6 +78,7 @@ bool MeshTopologyLoader::loadObj(const char *filename)
     }
 
     const vector< vector < vector <int> > > & facets = mesh->getFacets();
+    std::set< std::pair<int,int> > edges;
     for (unsigned int i=0; i<facets.size(); i++)
     {
         const vector<int>& facet = facets[i][0];
@@ -110,7 +111,6 @@ bool MeshTopologyLoader::loadObj(const char *filename)
                 addTriangle(facet[0],facet[j-1],facet[j]);
         }
 
-        std::set< std::pair<int,int> > edges;
         // Add edges
         if (facet.size()>2)
         {
