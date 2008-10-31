6c496d7f7be1675954cbc90b79fdde3b16627571
lenka <lenka@users.gforge.inria.fr>
2008-10-31 14:30:38 +0000

    r3257/sofa-dev : FIX: win32 compilation (dll issue) ADD: Mesh2Point mappings missing in the init.cpp

--- modules/sofa/component/mapping/SkinningMapping.cpp
+++ modules/sofa/component/mapping/SkinningMapping.cpp
@@ -28,10 +28,6 @@
 #include <sofa/core/componentmodel/behavior/MechanicalState.h>
 #include <sofa/core/componentmodel/behavior/MechanicalMapping.inl>
 
-#ifdef SOFA_DEV
-#include <sofa/helper/DualQuat.inl>
-#endif
-
 namespace sofa
 {
 
