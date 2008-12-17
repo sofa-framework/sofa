6cdf1a014a2f95176d9daf14440b1674945dab37
falipou <falipou@users.gforge.inria.fr>
2008-12-17 17:16:38 +0000

    r3549/sofa-dev : FIX: bad usage of Data in SphereModel. Filename was not used as a "Data"

--- modules/sofa/component/collision/SphereModel.cpp
+++ modules/sofa/component/collision/SphereModel.cpp
@@ -67,7 +67,9 @@ SphereModel::SphereModel()
     : mstate(NULL)
     , radius(initData(&radius, "listRadius","Radius of each sphere"))
     , defaultRadius(initData(&defaultRadius,(SReal)(1.0), "radius","Default Radius"))
+    , filename(initData(&filename, std::string("fileSphere"), "fileSphere", "File .sph describing the spheres"))
 {
+    addAlias(&filename,"filename");
 }
 
 void SphereModel::resize(int size)
