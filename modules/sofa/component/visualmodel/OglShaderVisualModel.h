7336111d2ec1f1e31176fa55eb5b555fa288b459
Frederick Roy <frederick.roy@gmail.com>
2009-02-09 09:40:15 +0000

    r3918/sofa-dev : ADD: OglShaderVisualModel which manages an oglshader, in order to not taint OglModel
     //partial sync (3/5 changes)//

---
+++ modules/sofa/component/visualmodel/OglShaderVisualModel.h
@@ -0,0 +1,48 @@
+/*
+ * GlslModel.h
+ *
+ *  Created on: 9 févr. 2009
+ *      Author: froy
+ */
+
+#ifndef OGLSHADERVISUALMODEL_H_
+#define OGLSHADERVISUALMODEL_H_
+
+#include <sofa/component/visualmodel/OglModel.h>
+#include <sofa/component/visualmodel/OglShader.h>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace visualmodel
+{
+
+class SOFA_COMPONENT_VISUALMODEL_API OglShaderVisualModel : public OglModel
+{
+protected:
+
+    typedef ExtVec3fTypes::Coord Coord;
+
+    GLuint abo;
+    OglShader* shader;
+
+    ResizableExtVector<Coord> vrestpositions;
+
+public:
+    OglShaderVisualModel();
+    virtual ~OglShaderVisualModel();
+
+    void init();
+    void initVisual();
+};
+
+} //namespace visualmodel
+
+} //namespace component
+
+} //namespace sofa
+
+#endif /* OGLSHADERVISUALMODEL_H_ */
