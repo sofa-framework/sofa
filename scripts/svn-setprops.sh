ad201befad8ddfe15c531c72e1da1bfbade88b1b
Jeremie Allard <jeremie.allard@insimo.fr>
2009-02-24 20:40:54 +0000

    r4100/sofa-dev : SCRIPT: apply svn-setprops.sh ( set 'svn:eol-style' on all text files )
     //partial sync (375/1391 changes)//

--- scripts/svn-setprops.sh
+++ scripts/svn-setprops.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # this script set the end of line style to native for all text files so that dump editors don't change all line endings and create big diffs hiding the real changes...
 # Note that it is better to enable it as an auto-prop in subversion config: http://www.zope.org/DevHome/Subversion/SubversionConfigurationForLineEndings
-find $* '(' -name '*.c' -o -name '*.cpp' -o -name '*.cu' -o -name '*.cxx'  -o -name '*.h' -o -name '*.hpp' -o -name '*.inl' -o -name '*.cuh' -o -name '*.txt' -o -name '*.pro' -o -name '*.pri' -o -name '*.cfg' ')' -exec svn propset svn:eol-style native '{}' ';'
+find $* '(' -name '*.c' -o -name '*.cpp' -o -name '*.cu' -o -name '*.cxx'  -o -name '*.h' -o -name '*.hpp' -o -name '*.inl' -o -name '*.cuh' -o -name '*.txt' -o -name '*.pro' -o -name '*.pri' -o -name '*.cfg' -o -name '*.scn' -o -name '*.xml' -o -name '*.pscn' ')' -exec svn propset svn:eol-style native '{}' ';'
