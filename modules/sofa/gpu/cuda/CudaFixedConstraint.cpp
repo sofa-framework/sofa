a9ab81ab75bcaaef629cdc59f69529446b03c0a6
Jeremie Allard <jeremie.allard@insimo.fr>
2009-02-24 15:53:27 +0000

    r4095/sofa-dev : FIX compilation on linux and with cuda
     //partial sync (1/5 changes)//

--- modules/sofa/gpu/cuda/CudaFixedConstraint.cpp
+++ modules/sofa/gpu/cuda/CudaFixedConstraint.cpp
@@ -24,7 +24,6 @@
 ******************************************************************************/
 #include "CudaTypes.h"
 #include "CudaFixedConstraint.inl"
-#include <sofa/component/constraint/BoxConstraint.inl>
 #include <sofa/core/ObjectFactory.h>
 #include <sofa/defaulttype/Vec3Types.h>
 #include <sofa/defaulttype/RigidTypes.h>
@@ -47,17 +46,6 @@ int FixedConstraintCudaClass = core::RegisterObject("Supports GPU-side computati
 #ifdef SOFA_GPU_CUDA_DOUBLE
         .add< component::constraint::FixedConstraint<CudaVec3dTypes> >()
         .add< component::constraint::FixedConstraint<CudaVec3d1Types> >()
-#endif // SOFA_GPU_CUDA_DOUBLE
-#endif // SOFA_DEV
-        ;
-
-int BoxConstraintCudaClass = core::RegisterObject("Supports GPU-side computations using CUDA")
-        .add< component::constraint::BoxConstraint<CudaVec3fTypes> >()
-        .add< component::constraint::BoxConstraint<CudaVec3f1Types> >()
-#ifdef SOFA_DEV
-#ifdef SOFA_GPU_CUDA_DOUBLE
-        .add< component::constraint::BoxConstraint<CudaVec3dTypes> >()
-        .add< component::constraint::BoxConstraint<CudaVec3d1Types> >()
 #endif // SOFA_GPU_CUDA_DOUBLE
 #endif // SOFA_DEV
         ;
