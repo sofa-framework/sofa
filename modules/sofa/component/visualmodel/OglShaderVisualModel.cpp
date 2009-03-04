5e1ea9f7a0864740c97a5f574ea0d5319289b07a
bousket <bousket@users.gforge.inria.fr>
2009-03-04 16:51:23 +0000

    r4213/sofa-dev : ADD: OglAttribute class. CHG: OglShaderVisualModel now use OglAttributes
     //partial sync (6/8 changes)//

--- modules/sofa/component/visualmodel/OglShaderVisualModel.cpp
+++ modules/sofa/component/visualmodel/OglShaderVisualModel.cpp
@@ -8,6 +8,14 @@
 #include <sofa/component/visualmodel/OglShaderVisualModel.h>
 #include <sofa/core/ObjectFactory.h>
 
+#include <sofa/component/topology/PointSetTopologyChange.h>
+#include <sofa/component/topology/TriangleSetTopologyChange.h>
+#include <sofa/component/topology/QuadSetTopologyChange.h>
+#include <sofa/component/topology/TetrahedronSetTopologyChange.h>
+#include <sofa/component/topology/HexahedronSetTopologyChange.h>
+#include <sofa/core/componentmodel/behavior/MechanicalState.h>
+#include <sofa/simulation/common/Node.h>
+
 namespace sofa
 {
 
@@ -16,6 +24,9 @@ namespace component
 
 namespace visualmodel
 {
+
+using namespace sofa::core::componentmodel::topology;
+using namespace sofa::core::componentmodel::behavior;
 
 SOFA_DECL_CLASS(OglShaderVisualModel)
 
@@ -41,19 +52,30 @@ void OglShaderVisualModel::init()
 
     shader = context->core::objectmodel::BaseContext::get<OglShader>();
 
-    vrestpositions.resize(vertices.size());
-    vrestnormals.resize(vnormals.size());
-
-    for (unsigned int i = 0; i < vertices.size(); i++)
+    vrestpositions.setContext( this->getContext());
+    vrestpositions.setID( std::string("restPosition"));
+    vrestpositions.setIndexShader( 0);
+    vrestpositions.init();
+    ResizableExtVector<Coord>& vrestpos = * ( vrestpositions.beginEdit() );
+    vrestpos.resize ( vertices.size() );
+    for ( unsigned int i = 0; i < vertices.size(); i++ )
     {
-        vrestpositions[i] = vertices[i];
+        vrestpos[i] = vertices[i];
     }
+    vrestpositions.endEdit();
 
-    for (unsigned int i = 0; i < vnormals.size(); i++)
+
+    vrestnormals.setContext( this->getContext());
+    vrestnormals.setID( "restNormal");
+    vrestnormals.setIndexShader( 0);
+    vrestnormals.init();
+    ResizableExtVector<Coord>& vrestnorm = * ( vrestnormals.beginEdit() );
+    vrestnorm.resize ( vnormals.size() );
+    for ( unsigned int i = 0; i < vnormals.size(); i++ )
     {
-        vrestnormals[i] = vnormals[i];
+        vrestnorm[i] = vnormals[i];
     }
-
+    vrestnormals.endEdit();
 }
 
 
@@ -64,43 +86,11 @@ void OglShaderVisualModel::initVisual()
     //Store other attributes
     if(shader)
     {
-        unsigned int restPositionsSize = vrestpositions.size() * sizeof(vrestpositions[0]);
-        unsigned int restNormalsSize = vrestnormals.size() * sizeof(vrestnormals[0]);
-        unsigned int totalSize = restPositionsSize + restNormalsSize;
-
-        glGenBuffers(1, &abo);
-
-        glBindBuffer(GL_ARRAY_BUFFER, abo);
-
-        glBufferData(GL_ARRAY_BUFFER,
-                totalSize,
-                NULL,
-                GL_DYNAMIC_DRAW);
-
-        //Rest Positions
-        glBufferSubData(GL_ARRAY_BUFFER,
-                0,
-                restPositionsSize,
-                vrestpositions.getData());
-
-        glEnableVertexAttribArray(shader->getAttribute(0, "restPosition"));
-        glVertexAttribPointer(shader->getAttribute(0, "restPosition"), 3, GL_FLOAT, GL_FALSE, 0, (char*)NULL + 0);
-
-        //Rest Normals
-        glBufferSubData(GL_ARRAY_BUFFER,
-                restPositionsSize,
-                restNormalsSize,
-                vrestnormals.getData());
-
-        glEnableVertexAttribArray(shader->getAttribute(0, "restNormal"));
-        glVertexAttribPointer(shader->getAttribute(0, "restNormal"), 3, GL_FLOAT, GL_FALSE, 0, (char*)NULL + 0 + restPositionsSize);
-
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-
+        vrestpositions.initVisual();
+        vrestnormals.initVisual();
 
     }
 }
-
 
 } //namespace visualmodel
 
