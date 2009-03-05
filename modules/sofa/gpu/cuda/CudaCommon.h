ae25248e7b07787e9d73a46e91004a00f14b45e3
comas <comas@users.gforge.inria.fr>
2009-03-05 15:39:08 +0000

    r4223/sofa-dev : CHANGE: substantial cleaning of the code and many comments have been added to describe the TLED implementation +  enum SHAREDMEMORYSIZE added into CudaCommon.h to avoid that the maximum size of shared memory was coded in hard + maximal number of elements per block is now computed from the size of shared memory of the GPU + flag added to choose whether we would like to print the details during the partitioning carried out at initialisation + showCollisionModels in Accuracy/cylinder_TLEDTetraSolution.scn disabled + isotropic and elastic formulation chosen by default in cube10x10x10-testTLED.scn
     //partial sync (2/6 changes)//

--- modules/sofa/gpu/cuda/CudaCommon.h
+++ modules/sofa/gpu/cuda/CudaCommon.h
@@ -28,9 +28,11 @@
 // Default size of thread blocks
 // Between 16 and 512
 enum { BSIZE=64 };
-
 enum { MBSIZE=96 };
 // Max size of thread blocks
 enum { MAXTHREADS=512 };
+
+// Size of shared memory
+enum { SHAREDMEMORYSIZE=16384 };
 
 #endif
