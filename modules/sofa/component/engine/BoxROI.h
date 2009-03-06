2a151a87ee3c2469e3950187ce65f23a4f56ce51
Jeremie Allard <jeremie.allard@insimo.fr>
2009-03-06 18:54:14 +0000

    r4252/sofa-dev : FIX: Compilation on Windows
     //partial sync (3/4 changes)//

--- modules/sofa/component/engine/BoxROI.h
+++ modules/sofa/component/engine/BoxROI.h
@@ -118,10 +118,10 @@ public:
 #if defined(WIN32) && !defined(SOFA_COMPONENT_ENGINE_BOXROI_CPP)
 #pragma warning(disable : 4231)
 #ifndef SOFA_FLOAT
-template class SOFA_COMPONENT_ENGINE_API BoxROI<Vec3dTypes>;
+template class SOFA_COMPONENT_ENGINE_API BoxROI<defaulttype::Vec3dTypes>;
 #endif //SOFA_FLOAT
 #ifndef SOFA_DOUBLE
-template class SOFA_COMPONENT_ENGINE_API BoxROI<Vec3fTypes>;
+template class SOFA_COMPONENT_ENGINE_API BoxROI<defaulttype::Vec3fTypes>;
 #endif //SOFA_DOUBLE
 #endif
 
