dfe55320bddc5f6d75a7cd7e3af5e3bdad6992c2
Erik Pernod <erik.pernod@gmail.com>
2009-03-09 09:51:15 +0000

    r4259/sofa-dev : CHANGE: name functions to be more clear. Also add header doxygen doc. And start cleaning the code.

--- modules/sofa/component/topology/ManifoldTriangleSetTopologyModifier.h
+++ modules/sofa/component/topology/ManifoldTriangleSetTopologyModifier.h
@@ -61,24 +61,14 @@ public:
 
     virtual void reinit();
 
-    /**\brief PreCondition to fulfill before removing triangles. In this class topology should stay manifold.
-    * This function call private functions to test the topology: createFutureModifications(), testRemoveModifications().
+    /**\brief Function swaping edge between two adjacents triangles. Create two new triangles and remove the two old one.
+    * This function call private functions of the container reordering the different shells.
+    * Different from the others used in adding and removing triangles which are faster but need informations of
+    * the state's topology before modifications.
+    * @see ManifoldTriangleSetTopologyContainer::reorderingTopologyOnROI()
+    * @param index of first triangle.
+    * @param index of second triangle adjacent to the first one.
     */
-    virtual bool removePrecondition(sofa::helper::vector< unsigned int >& items);
-
-    /**\brief Postprocessing to apply to the triangle topology. In this class topology should stay manifold.
-    * This function reorder the triangles around each vertex where triangles have been deleted.
-    * In order that only one connexe composante stay.
-    */
-    virtual void removePostProcessing(const sofa::helper::vector< unsigned int >& edgeToBeRemoved, const sofa::helper::vector< unsigned int >& vertexToBeRemoved );
-
-
-
-    virtual bool addPrecondition (const sofa::helper::vector <Triangle>& triangles);
-
-
-    virtual void addPostProcessing(const sofa::helper::vector <Triangle>& triangles);
-
     bool edgeSwap (const TriangleID& indexTri1, const TriangleID& indexTri2);
 
     virtual void Debug(); // TO BE REMOVED WHEN CLASS IS SURE.
@@ -87,23 +77,51 @@ protected:
 
     Data< sofa::helper::vector< unsigned int> > m_triSwap;
 
+    /**\brief Preconditions to fulfill before removing triangles. In this class topology should stay manifold.
+    * This function call private functions to test the topology:
+    * @see createRemovingTrianglesFutureModifications()
+    * @see createRemovingEdgesFutureModifications()
+    * @see testRemovingModifications().
+    */
+    virtual bool removeTrianglesPreconditions(sofa::helper::vector< unsigned int >& items);
+
+    /**\brief Postprocessing to apply to the triangle topology. In this class topology should stay manifold.
+    * These functions reorder the triangles around each vertex where triangles have been deleted.
+    * In order that only one connexe composante stay. It call the internal functions:
+    * @see internalRemovingPostProcessingEdges()
+    * @see internalRemovingPostProcessingTriangles()
+    * @see updateRemovingModifications()
+    * @see reorderEdgeForRemoving()
+    */
+    virtual void removeTrianglesPostProcessing(const sofa::helper::vector< unsigned int >& edgeToBeRemoved, const sofa::helper::vector< unsigned int >& vertexToBeRemoved );
+
+
+    /**\brief Preconditions to fulfill before adding triangles. In this class topology should stay manifold.
+     * Test if triangles could be added and stock the informations of where triangles are added in the map:
+     * @see m_modificationsEdge
+     */
+    virtual bool addTrianglesPreconditions (const sofa::helper::vector <Triangle>& triangles);
+
+
+    /**\brief Postprocessing to apply to the triangle topology. In this class topology should stay manifold.
+     * Using the map @see m_modificationsEdge, reorder the different shells.
+     */
+    virtual void addTrianglesPostProcessing(const sofa::helper::vector <Triangle>& triangles);
+
+
 private:
 
     ManifoldTriangleSetTopologyContainer* m_container;
 
+    /** \brief iterator for the map.
+     */
+    std::map< unsigned int, sofa::helper::vector<unsigned int> >::iterator it_modif;
+    std::map< unsigned int, sofa::helper::vector<int> >::iterator it_add;
+
+
     /** \brief This map store all the modifications (for the triangles) to apply to the topology.
      */
     std::map< unsigned int, sofa::helper::vector <unsigned int> > m_modifications;
-
-    /** \brief This map store all the modifications (for the triangles) to apply to the topology.
-     */
-    std::map< unsigned int, sofa::helper::vector <int> > m_Addmodifications;
-
-    /** \brief iterator for the m_modification map.
-     */
-    std::map< unsigned int, sofa::helper::vector<unsigned int> >::iterator it_modif;
-
-    std::map< unsigned int, sofa::helper::vector<int> >::iterator it_add;
 
 
     /** \brief This vector store all the modifications (for the edges) to apply to the topology.
@@ -111,34 +129,49 @@ private:
     sofa::helper::vector< unsigned int> m_modificationsEdge;
 
 
+    /** \brief This map store all the modifications (for the triangles) to apply to the topology.
+     */
+    std::map< unsigned int, sofa::helper::vector <int> > m_Addmodifications;
+
+
+    /** Fill the vector m_modificationEdge with the 3 edges of each triangle to be removed (without duplications).
+     * This is needed,if orientations of edges have to be changed (always oriented in the right direction regarding the
+     * first or the only one triangle of m_TriangleEdgeShellArray[ the_edge ]);
+     */
+    void createRemovingEdgesFutureModifications (const sofa::helper::vector <unsigned int> items);
+
+
     /** Create the vector m_modifications which store the modifications to apply to the topology.
      * Thus, tests can be done before any triangle(s) removal, in order to keep the topology Manifold.
      * m_modifications[0] = vertex index number.
      * m_modifications[i>0] = 0 (no change) or 1 (remove m_triangleVertexShell[ m_modifications[0] ][i+1])
      */
-    void createFutureModifications(sofa::helper::vector< unsigned int >& items);
+    void createRemovingTrianglesFutureModifications(sofa::helper::vector< unsigned int >& items);
 
 
     /** Test the modifications to apply around one vertex. After removing triangles, only one connexe composante
      * should stay.
+     */
+    bool testRemovingModifications();
+
+
+    /** According to m_modification map, reorder the m_triangleVertexShellArray.
      *
      */
-    bool testRemoveModifications();
+    void internalRemovingPostProcessingTriangles();
+
+
+    /** According to m_modificationEdge vector, reorder the m_EdgeVertexShellArray.
+     *
+     */
+    void internalRemovingPostProcessingEdges();
 
 
     /** If isolate edges or vertices have to be removed during the operation. This function update the information in the container:
      * m_modification and m_modificationEdge
      *
      */
-    void updateModifications (const sofa::helper::vector< unsigned int >& edgeToBeRemoved, const sofa::helper::vector< unsigned int >& vertexToBeRemoved);
-
-
-    /** Fill the vector m_modificationEdge with the 3 edges of each triangle to be removed (without duplications).
-     * This is needed,if orientations of edges have to be changed (always oriented in the right direction regarding the
-     * first or the only one triangle of m_TriangleEdgeShellArray[ the_edge ]);
-
-     */
-    void createFutureModificationsEdge (const sofa::helper::vector <unsigned int> items);
+    void updateRemovingModifications (const sofa::helper::vector< unsigned int >& edgeToBeRemoved, const sofa::helper::vector< unsigned int >& vertexToBeRemoved);
 
 
     /** For each edge of m_modificationEdge, this function call ManifoldTriangleSetTopologyContainer::reorderingEdge() to
@@ -147,17 +180,6 @@ private:
      */
     void reorderEdgeForRemoving();
 
-
-    /** According to m_modification map, reorder the m_triangleVertexShellArray.
-     *
-     */
-    void removePostProcessingTriangles();
-
-
-    /** According to m_modificationEdge vector, reorder the m_EdgeVertexShellArray.
-     *
-     */
-    void removePostProcessingEdges();
 
 };
 
--- modules/sofa/component/topology/TriangleSetTopologyContainer.h
+++ modules/sofa/component/topology/TriangleSetTopologyContainer.h
@@ -1,27 +1,27 @@
 /******************************************************************************
-*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 3      *
-*                (c) 2006-2008 MGH, INRIA, USTL, UJF, CNRS                    *
-*                                                                             *
-* This library is free software; you can redistribute it and/or modify it     *
-* under the terms of the GNU Lesser General Public License as published by    *
-* the Free Software Foundation; either version 2.1 of the License, or (at     *
-* your option) any later version.                                             *
-*                                                                             *
-* This library is distributed in the hope that it will be useful, but WITHOUT *
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
-* for more details.                                                           *
-*                                                                             *
-* You should have received a copy of the GNU Lesser General Public License    *
-* along with this library; if not, write to the Free Software Foundation,     *
-* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
-*******************************************************************************
-*                               SOFA :: Modules                               *
-*                                                                             *
-* Authors: The SOFA Team and external contributors (see Authors.txt)          *
-*                                                                             *
-* Contact information: contact@sofa-framework.org                             *
-******************************************************************************/
+ *       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 3      *
+ *                (c) 2006-2008 MGH, INRIA, USTL, UJF, CNRS                    *
+ *                                                                             *
+ * This library is free software; you can redistribute it and/or modify it     *
+ * under the terms of the GNU Lesser General Public License as published by    *
+ * the Free Software Foundation; either version 2.1 of the License, or (at     *
+ * your option) any later version.                                             *
+ *                                                                             *
+ * This library is distributed in the hope that it will be useful, but WITHOUT *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+ * for more details.                                                           *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public License    *
+ * along with this library; if not, write to the Free Software Foundation,     *
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+ *******************************************************************************
+ *                               SOFA :: Modules                               *
+ *                                                                             *
+ * Authors: The SOFA Team and external contributors (see Authors.txt)          *
+ *                                                                             *
+ * Contact information: contact@sofa-framework.org                             *
+ ******************************************************************************/
 #ifndef SOFA_COMPONENT_TOPOLOGY_TRIANGLESETTOPOLOGYCONTAINER_H
 #define SOFA_COMPONENT_TOPOLOGY_TRIANGLESETTOPOLOGYCONTAINER_H
 
@@ -49,7 +49,8 @@ typedef BaseMeshTopology::TriangleEdges		TriangleEdges;
 typedef BaseMeshTopology::VertexTriangles	VertexTriangles;
 typedef BaseMeshTopology::EdgeTriangles		EdgeTriangles;
 
-/** Object that stores a set of triangles and provides access
+/*! \class TriangleSetTopologyContainer
+\brief: Object that stores a set of triangles and provides access
 to each triangle and its edges and vertices */
 class SOFA_COMPONENT_CONTAINER_API TriangleSetTopologyContainer : public EdgeSetTopologyContainer
 {
@@ -72,15 +73,17 @@ public:
 
     virtual void init();
 
+    virtual void draw();
+
+
     /// BaseMeshTopology API
     /// @{
-
     const SeqTriangles& getTriangles()
     {
         return getTriangleArray();
     }
 
-    /** Returns the indices of a triangle given three vertex indices : returns -1 if none */
+    /* Returns the indices of a triangle given three vertex indices : returns -1 if none */
     virtual int getTriangleIndex(PointID v1, PointID v2, PointID v3);
 
     /// Returns the set of edges adjacent to a given triangle.
@@ -90,14 +93,14 @@ public:
     }
 
     /** \brief Returns the set of triangles adjacent to a given vertex.
-    *
-    */
+     *
+     */
     virtual const VertexTriangles& getTriangleVertexShell(PointID i);
 
 
     /** \brief Returns the set of triangles adjacent to a given edge.
-    *
-    */
+     *
+     */
     virtual const EdgeTriangles& getTriangleEdgeShell(EdgeID i) ;
 
     /** returns the index (either 0, 1 ,2) of the vertex whose global index is vertexIndex. Returns -1 if none */
@@ -109,74 +112,74 @@ public:
     /// @}
 
     /** \brief Checks if the topology is coherent
-    *
-    * Check if the shell arrays are coherent
-    */
+     *
+     * Check if the shell arrays are coherent
+     */
     virtual bool checkTopology() const;
 
     /** \brief Returns the Triangle array.
-    *
-    */
+     *
+     */
     const sofa::helper::vector<Triangle> &getTriangleArray();
 
     /** \brief Returns the Triangle Vertex Shells array.
-    *
-    */
+     *
+     */
     const sofa::helper::vector< sofa::helper::vector<unsigned int> > &getTriangleVertexShellArray();
 
     /** \brief Returns the TriangleEdges array (ie provide the 3 edge indices for each triangle)
-    *
-    */
+     *
+     */
     const sofa::helper::vector< TriangleEdges > &getTriangleEdgeArray() ;
 
     /** \brief Returns the Triangle Edge Shells array (ie provides the triangles adjacent to each edge)
-    *
-    */
+     *
+     */
     const sofa::helper::vector< sofa::helper::vector<unsigned int> > &getTriangleEdgeShellArray() ;
 
     /** \brief Returns the number of triangles in this topology.
-    *	The difference to getNbTriangles() is that this method does not generate the triangle array if it does not exist.
-    */
+     *	The difference to getNbTriangles() is that this method does not generate the triangle array if it does not exist.
+     */
     unsigned int getNumberOfTriangles() const;
 
 
     /** \brief Returns the 3 edges adjacent to a given triangle.
-    *
-    */
+     *
+     */
     const TriangleEdges &getTriangleEdge(const unsigned int i) ;
 
 protected:
     /** \brief Creates the TriangleSet array.
-    *
-    * This function must be implemented by derived classes to create a list of triangles from a set of tetrahedra for instance
-    */
+     *
+     * This function must be implemented by derived classes to create a list of triangles from a set of tetrahedra for instance
+     */
     virtual void createTriangleSetArray();
 
     /** \brief Creates the EdgeSet array.
-    *
-    * Create the set of edges when needed.
-    */
+     *
+     * Create the set of edges when needed.
+     */
     virtual void createEdgeSetArray();
 
     /** \brief Creates the array of edge indices for each triangle
-    *
-    * This function is only called if the TriangleEdge array is required.
-    * m_triangleEdge[i] contains the 3 indices of the 3 edges opposite to the ith vertex
-    */
+     *
+     * This function is only called if the TriangleEdge array is required.
+     * m_triangleEdge[i] contains the 3 indices of the 3 edges opposite to the ith vertex
+     */
     void createTriangleEdgeArray();
 
     /** \brief Creates the Triangle Vertex Shell Array
-    *
-    * This function is only called if the TriangleVertexShell array is required.
-    * m_triangleVertexShell[i] contains the indices of all triangles adjacent to the ith vertex
-    */
+     *
+     * This function is only called if the TriangleVertexShell array is required.
+     * m_triangleVertexShell[i] contains the indices of all triangles adjacent to the ith vertex
+     */
     virtual void createTriangleVertexShellArray();
 
     /** \brief Creates the Triangle Edge Shell Array
-    *
-    * This function is only called if the TriangleVertexShell array is required.
-    * m_triangleEdgeShell[i] contains the indices of all triangles adjacent to the ith edge
-    */
+     *
+     * This function is only called if the TriangleVertexShell array is required.
+     * m_triangleEdgeShell[i] contains the indices of all triangles adjacent to the ith edge
+     */
     virtual void createTriangleEdgeShellArray();
 
 
@@ -196,15 +199,14 @@ protected:
 
     void clearTriangleEdgeShell();
 
-
 private:
     /** \brief Returns a non-const triangle vertex shell given a vertex index for subsequent modification
-    *
-    */
+     *
+     */
     sofa::helper::vector< unsigned int > &getTriangleVertexShellForModification(const unsigned int vertexIndex);
     /** \brief Returns a non-const triangle edge shell given the index of an edge for subsequent modification
-    *
-    */
+     *
+     */
     sofa::helper::vector< unsigned int > &getTriangleEdgeShellForModification(const unsigned int edgeIndex);
 
 protected:
