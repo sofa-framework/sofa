6b2b89a75bf0f5e92d3f26b503dcebccb8386170
madurand <madurand@users.gforge.inria.fr>
2009-03-17 14:35:21 +0000

    r4420/sofa-dev : ADD: FixedPlaneConstraint Rigid3dTypes and Rigid3fTypes specialization.

--- modules/sofa/component/constraint/FixedPlaneConstraint.cpp
+++ modules/sofa/component/constraint/FixedPlaneConstraint.cpp
@@ -44,11 +44,67 @@ SOFA_DECL_CLASS(FixedPlaneConstraint)
 int FixedPlaneConstraintClass = core::RegisterObject("Project particles on a given plane")
 #ifndef SOFA_FLOAT
         .add< FixedPlaneConstraint<Vec3dTypes> >()
+        .add< FixedPlaneConstraint<Rigid3dTypes> >()
 #endif
 #ifndef DOUBLE
         .add< FixedPlaneConstraint<Vec3fTypes> >()
+        .add< FixedPlaneConstraint<Rigid3fTypes> >()
 #endif
         ;
+
+#ifndef SOFA_FLOAT
+template <>
+void FixedPlaneConstraint<Rigid3dTypes>::projectResponse(VecDeriv& res)
+{
+    const int N=Coord::static_size;
+    Vec<N,Real> dir=direction.getValue().getCenter();
+
+    for (helper::vector< unsigned int > ::const_iterator it = this->indices.getValue().begin(); it != this->indices.getValue().end(); ++it)
+    {
+        res[*it].getVCenter() -= dir*(dir*(res[*it].getVCenter()));
+    }
+}
+
+template <>
+bool FixedPlaneConstraint<Rigid3dTypes>::isPointInPlane(Coord p)
+{
+    const int N=Coord::static_size;
+    Vec<N,Real> pos = p.getCenter();
+    Real d=pos*direction.getValue().getCenter();
+    if ((d>dmin.getValue())&& (d<dmax.getValue()))
+        return true;
+    else
+        return false;
+}
+#endif
+
+#ifndef SOFA_DOUBLE
+template <>
+void FixedPlaneConstraint<Rigid3fTypes>::projectResponse(VecDeriv& res)
+{
+    const int N=Coord::static_size;
+    Vec<N,Real> dir=direction.getValue().getCenter();
+
+    for (helper::vector< unsigned int > ::const_iterator it = this->indices.getValue().begin(); it != this->indices.getValue().end(); ++it)
+    {
+        res[*it].getVCenter() -= dir*(dir*(res[*it].getVCenter()));
+    }
+}
+
+template <>
+bool FixedPlaneConstraint<Rigid3fTypes>::isPointInPlane(Coord p)
+{
+    const int N=Coord::static_size;
+    Vec<N,Real> pos = p.getCenter();
+    Real d=pos*direction.getValue().getCenter();
+    if ((d>dmin.getValue())&& (d<dmax.getValue()))
+        return true;
+    else
+        return false;
+}
+#endif
+
+
 #ifndef SOFA_FLOAT
 template class SOFA_COMPONENT_CONSTRAINT_API FixedPlaneConstraint<Vec3dTypes>;
 #endif
