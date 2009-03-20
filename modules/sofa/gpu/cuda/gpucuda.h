c5659a40c28fa81712ad07ed2e7a1bc956cbe229
Jeremie Allard <jeremie.allard@insimo.fr>
2009-03-20 19:22:01 +0000

    r4477/sofa-dev : CUDA: put CudaBoxROI out of SOFA_DEV + use a dll on windows (untested)

--- modules/sofa/gpu/cuda/gpucuda.h
+++ modules/sofa/gpu/cuda/gpucuda.h
@@ -22,82 +22,15 @@
 *                                                                             *
 * Contact information: contact@sofa-framework.org                             *
 ******************************************************************************/
+#ifndef SOFA_GPU_CUDA_GPUCUDA_H
+#define SOFA_GPU_CUDA_GPUCUDA_H
+
 #include <sofa/helper/system/config.h>
-#include <iostream>
-#include <fstream>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-#include "mycuda.h"
+#ifdef SOFA_BUILD_GPU_CUDA
+#  define SOFA_GPU_CUDA_API SOFA_EXPORT_DYNAMIC_LIBRARY
+#else
+#  define SOFA_GPU_CUDA_API SOFA_IMPORT_DYNAMIC_LIBRARY
+#endif
 
-namespace sofa
-{
-namespace gpu
-{
-namespace cuda
-{
-
-SOFA_LINK_CLASS(CudaBoxROI)
-SOFA_LINK_CLASS(CudaFixedConstraint)
-SOFA_LINK_CLASS(CudaMechanicalObject)
-SOFA_LINK_CLASS(CudaSpringForceField)
-SOFA_LINK_CLASS(CudaUniformMass)
-SOFA_LINK_CLASS(CudaPlaneForceField)
-SOFA_LINK_CLASS(CudaSphereForceField)
-SOFA_LINK_CLASS(CudaEllipsoidForceField)
-SOFA_LINK_CLASS(CudaIdentityMapping)
-SOFA_LINK_CLASS(CudaBarycentricMapping)
-SOFA_LINK_CLASS(CudaRigidMapping)
-SOFA_LINK_CLASS(CudaSubsetMapping)
-SOFA_LINK_CLASS(CudaDistanceGridCollisionModel)
-SOFA_LINK_CLASS(CudaTetrahedronFEMForceField)
-SOFA_LINK_CLASS(CudaCollision)
-SOFA_LINK_CLASS(CudaCollisionDetection)
-SOFA_LINK_CLASS(CudaPointModel)
-SOFA_LINK_CLASS(CudaSphereModel)
-SOFA_LINK_CLASS(CudaTestForceField)
-SOFA_LINK_CLASS(CudaSetTopology)
-SOFA_LINK_CLASS(CudaVisualModel)
-SOFA_LINK_CLASS(CudaOglTetrahedralModel)
-
-#ifdef SOFA_DEV
-
-SOFA_LINK_CLASS(CudaTetrahedronTLEDForceField)
-SOFA_LINK_CLASS(CudaHexahedronTLEDForceField)
-SOFA_LINK_CLASS(CudaTetrahedronSuperTLEDForceField)
-SOFA_LINK_CLASS(CudaMasterContactSolver)
-SOFA_LINK_CLASS(CudaSpatialGridContainer)
-
-#endif // SOFA_DEV
-
-extern "C"
-{
-//MycudaVerboseLevel mycudaVerboseLevel = LOG_ERR;
-    MycudaVerboseLevel mycudaVerboseLevel = LOG_INFO;
-//MycudaVerboseLevel mycudaVerboseLevel = LOG_TRACE;
-}
-
-void mycudaLogError(const char* err, const char* src)
-{
-    std::cerr << "CUDA error: "<< err <<" returned from "<< src <<".\n";
-    exit(1);
-}
-
-int myprintf(const char* fmt, ...)
-{
-    va_list args;
-    va_start( args, fmt );
-    int r = vfprintf( stderr, fmt, args );
-    va_end( args );
-    return r;
-}
-
-const char* mygetenv(const char* name)
-{
-    return getenv(name);
-}
-
-} // namespace cuda
-} // namespace gpu
-} // namespace sofa
+#endif
