032261ea764f8d2bec260f45374ea1810e8d0170
comas <comas@users.gforge.inria.fr>
2009-04-01 15:51:07 +0000

    r4603/sofa-dev :

--- applications/sofa/gui/Main.cpp
+++ applications/sofa/gui/Main.cpp
@@ -50,6 +50,41 @@ SOFA_LINK_CLASS(QTGUI)
 SOFA_LINK_CLASS(OgreGUI)
 #endif
 
+
+// ligne 1 et 3
+#ifdef __GNUC__ 4
+#ifdef __GNUC__MINOR__ 2
+int fish
+#endif
+#endif
+
+#ifndef SOFA_DEV
+int fish
+#endif
+
+#ifdef __APPLE__
+int fish
+#endif
+
+// ligne 2
+//#ifdef __GNUC__ 4
+//    #ifdef __GNUC__MINOR__ 3
+//        int fish
+//    #endif
+//#endif
+//
+//#ifdef __GNUC__ 4
+//    #ifdef __GNUC__MINOR__ 1
+//        int fish
+//    #endif
+//#endif
+//
+//#ifdef SOFA_GPU_CUDA
+//        int fish
+//#endif
+
+
+
 int SofaGUI::Init()
 {
     sofa::component::init();
