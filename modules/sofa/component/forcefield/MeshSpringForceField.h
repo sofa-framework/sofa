9ca14b61761da1f8f12713626b5db619d1452a96
falipou <falipou@users.gforge.inria.fr>
2009-05-11 14:46:35 +0000

    r4824/sofa-dev : FIX: revert previous correction on default values. Correct the scene files instead

--- modules/sofa/component/forcefield/MeshSpringForceField.h
+++ modules/sofa/component/forcefield/MeshSpringForceField.h
@@ -66,18 +66,20 @@ protected:
 
 public:
     MeshSpringForceField()
-        : linesStiffness(initData(&linesStiffness,Real(100),"linesStiffness","Stiffness for the Lines",false))
-        , linesDamping(initData(&linesDamping,Real(5),"linesDamping","Damping for the Lines",false))
-        , trianglesStiffness(initData(&trianglesStiffness,Real(100),"trianglesStiffness","Stiffness for the Triangles",false))
-        , trianglesDamping(initData(&trianglesDamping,Real(5),"trianglesDamping","Damping for the Triangles",false))
-        , quadsStiffness(initData(&quadsStiffness,Real(100),"quadsStiffness","Stiffness for the Quads",false))
-        , quadsDamping(initData(&quadsDamping,Real(5),"quadsDamping","Damping for the Quads",false))
-        , tetrasStiffness(initData(&tetrasStiffness,Real(100),"tetrasStiffness","Stiffness for the Tetras",false))
-        , tetrasDamping(initData(&tetrasDamping,Real(5),"tetrasDamping","Damping for the Tetras",false))
-        , cubesStiffness(initData(&cubesStiffness,Real(100),"cubesStiffness","Stiffness for the Cubes",false))
-        , cubesDamping(initData(&cubesDamping,Real(5),"cubesDamping","Damping for the Cubes",false))
+        : linesStiffness(initData(&linesStiffness,Real(0),"linesStiffness","Stiffness for the Lines",true))
+        , linesDamping(initData(&linesDamping,Real(0),"linesDamping","Damping for the Lines",true))
+        , trianglesStiffness(initData(&trianglesStiffness,Real(0),"trianglesStiffness","Stiffness for the Triangles",true))
+        , trianglesDamping(initData(&trianglesDamping,Real(0),"trianglesDamping","Damping for the Triangles",true))
+        , quadsStiffness(initData(&quadsStiffness,Real(0),"quadsStiffness","Stiffness for the Quads",true))
+        , quadsDamping(initData(&quadsDamping,Real(0),"quadsDamping","Damping for the Quads",true))
+        , tetrasStiffness(initData(&tetrasStiffness,Real(0),"tetrasStiffness","Stiffness for the Tetras",true))
+        , tetrasDamping(initData(&tetrasDamping,Real(0),"tetrasDamping","Damping for the Tetras",true))
+        , cubesStiffness(initData(&cubesStiffness,Real(0),"cubesStiffness","Stiffness for the Cubes",true))
+        , cubesDamping(initData(&cubesDamping,Real(0),"cubesDamping","Damping for the Cubes",true))
         , localRange( initData(&localRange, defaulttype::Vec<2,int>(-1,-1), "localRange", "optional range of local DOF indices. Any computation involving only indices outside of this range are discarded (useful for parallelization using mesh partitionning)" ) )
     {
+        this->ks.setDisplayed(false);
+        this->kd.setDisplayed(false);
         addAlias(&linesStiffness,    "stiffness"); addAlias(&linesDamping,    "damping");
         addAlias(&trianglesStiffness,"stiffness"); addAlias(&trianglesDamping,"damping");
         addAlias(&quadsStiffness,    "stiffness"); addAlias(&quadsDamping,    "damping");
