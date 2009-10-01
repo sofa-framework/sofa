150c87b14c00b94ac2be972abc3cb3483b0d130e
Pierre-Jean Bensoussan <pierre-jean.bensoussan@insimo.fr>
2009-10-01 10:09:29 +0000

    r5492/sofa-dev : FIX : Compilation with only SOFA_DOUBLE.

--- applications/sofa/gui/PickHandler.cpp
+++ applications/sofa/gui/PickHandler.cpp
@@ -44,8 +44,12 @@
 namespace sofa
 {
 using namespace component::collision;
+#ifndef SOFA_FLOAT
 helper::Creator<ComponentMouseInteraction::ComponentMouseInteractionFactory,TComponentMouseInteraction<defaulttype::Vec3dTypes> > ComponentMouseInteractionVec3dClass ("MouseSpringVec3d",true);
+#endif
+#ifndef SOFA_DOUBLE
 helper::Creator<ComponentMouseInteraction::ComponentMouseInteractionFactory, TComponentMouseInteraction<defaulttype::Vec3fTypes> > ComponentMouseInteractionVec3fClass ("MouseSpringVec3f",true);
+#endif
 
 namespace gui
 {
