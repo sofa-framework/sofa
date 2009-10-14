c1809f1eba1f1632a4482383beabd15b5cf72616
Hadrien Courtecuisse <hadrien.courtecuisse@gmail.com>
2009-10-14 22:40:09 +0000

    r5581/sofa-dev : CudaRasterizer : Use CudaVisualModel to compute normals
     //partial sync (1/5 changes)//

--- examples/Benchmark/Performance/run-Bar16.sh
+++ examples/Benchmark/Performance/run-Bar16.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-for d in Vec3f CudaVec3f;
+for d in CudaVec3f;
 do
-let i=1; while [ $i -le 52 ];
+let i=1; while [ $i -le 24 ];
 do
 export s=$i
 echo $d - $i - fem
-sofaCUDA Bar16-fem-implicit-$d.pscn 1000
-mv -f Bar16-fem-implicit-$d.-log.txt Bar16-$i-fem-implicit-$d-log.txt
+php examples/Benchmark/Performance/Bar16-fem-implicit-$d.pscn > examples/Benchmark/Performance/Bar16-fem-implicit-$d.scn
+sofaCUDA examples/Benchmark/Performance/Bar16-fem-implicit-$d.scn 100
+mv -f examples/Benchmark/Performance/Bar16-fem-implicit-$d-log.txt examples/Benchmark/Performance/Bar16-$i-fem-implicit-$d-log.txt
 echo $d - $i - spring
-sofaCUDA Bar16-spring-rk4-$d.pscn 1000
-mv -f Bar16-spring-rk4-$d.-log.txt Bar16-$i-spring-rk4-$d-log.txt
+php examples/Benchmark/Performance/Bar16-spring-rk4-$d.pscn > examples/Benchmark/Performance/Bar16-spring-rk4-$d.scn
+sofaCUDA examples/Benchmark/Performance/Bar16-spring-rk4-$d.scn 100
+mv -f examples/Benchmark/Performance/Bar16-spring-rk4-$d-log.txt examples/Benchmark/Performance/Bar16-$i-spring-rk4-$d-log.txt
 if [ $i -lt 8 ]; then
 let i+=1
 else
