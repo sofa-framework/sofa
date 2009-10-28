9588f314f01171361d492542d8b6668c851efc31
falipou <falipou@users.gforge.inria.fr>
2009-10-28 16:51:30 +0000

    r5646/sofa-dev : FIX: calls of GetGUIName() always returned "". We specify the name of the viewer in RealGUI.

--- applications/sofa/gui/qt/RealGUI.cpp
+++ applications/sofa/gui/qt/RealGUI.cpp
@@ -718,6 +718,9 @@ void RealGUI::addViewer()
                 application->exit();
             }
 
+    //Specify the name of the viewer here
+    this->GetGUIName()=name;
+
 #ifdef SOFA_QT4
     left_stack->addWidget ( viewer->getQWidget() );
     left_stack->setCurrentWidget ( viewer->getQWidget() );
