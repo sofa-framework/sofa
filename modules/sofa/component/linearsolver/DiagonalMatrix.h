7b0e06c1197843c3408dbd17dd8b8dddb7463522
Hadrien Courtecuisse <hadrien.courtecuisse@gmail.com>
2009-11-06 15:58:54 +0000

    r5751/sofa-dev : Add Diagonal Matrix for BlockJacobiPreconditioneur
     //partial sync (3/4 changes)//

--- modules/sofa/component/linearsolver/DiagonalMatrix.h
+++ modules/sofa/component/linearsolver/DiagonalMatrix.h
@@ -25,8 +25,7 @@
 #ifndef SOFA_COMPONENT_LINEARSOLVER_DIAGONALMATRIX_H
 #define SOFA_COMPONENT_LINEARSOLVER_DIAGONALMATRIX_H
 
-#include <sofa/defaulttype/BaseMatrix.h>
-#include "FullVector.h"
+#include "NewMatMatrix.h"
 
 namespace sofa
 {
@@ -154,6 +153,204 @@ public:
     }
 
     static const char* Name() { return "DiagonalMatrix"; }
+};
+
+
+/// Simple full matrix container
+template<int LC>
+class BlockDiagonalMatrix : public defaulttype::BaseMatrix
+{
+public:
+    typedef double Real;
+    typedef int Index;
+    typedef NEWMAT::Matrix SubMatrixType;
+    typedef NEWMAT::InvertedMatrix InvertedMatrix;
+
+protected:
+    std::vector< SubMatrixType > data;
+    unsigned cSize;
+
+public:
+    int bandWidth;
+
+    BlockDiagonalMatrix()
+    {
+        bandWidth = LC-1;
+    }
+
+    ~BlockDiagonalMatrix() {}
+
+    void resize(int nbRow, int )
+    {
+        cSize = nbRow;
+        data.resize((cSize+LC-1) / LC);
+        for (unsigned i=0; i<data.size(); i++) data[i].ReSize(LC,LC);
+    }
+
+    unsigned rowSize(void) const
+    {
+        return cSize;
+    }
+
+    unsigned colSize(void) const
+    {
+        return cSize;
+    }
+
+    Real element(int i, int j) const
+    {
+        if (i/LC!=j/LC) return (Real)0;
+        return data[i/LC].element(i%LC,j%LC);
+    }
+
+    void set(int i, int j, double v)
+    {
+        if (i/LC==j/LC) data[i/LC].element(i%LC,j%LC) = (Real)v;
+    }
+
+    void add(int i, int j, double v)
+    {
+        if (i/LC==j/LC) data[i/LC].element(i%LC,j%LC) += (Real)v;
+    }
+
+    void clear()
+    {
+        for (unsigned b=0; b<data.size(); b++)
+            for (int j=0; j<LC; j++)
+                for (int i=0; i<LC; i++)
+                    data[b].element(i,j) = 0.0;;
+    }
+
+    void clear(int i, int j)
+    {
+        if (i/LC==j/LC) data[i/LC].element(i%LC,j%LC) = 0.0;
+    }
+
+    void clearRow(int i)
+    {
+        for (int j=0; j<LC; j++)
+            data[i/LC].element(i,j) = 0.0;;
+    }
+
+    void clearCol(int j)
+    {
+        for (int i=0; i<LC; i++)
+            data[j/LC].element(i,j) = 0.0;;
+
+    }
+
+    void clearRowCol(int i)
+    {
+        clearRow(i);
+        clearCol(i);
+    }
+
+    SubMatrixType sub(int l,int ,int ,int )
+    {
+        SubMatrixType m=data[l/LC];
+        return m;
+    }
+
+    void setSubMatrix(unsigned l,unsigned ,unsigned ,unsigned ,InvertedMatrix m)
+    {
+        data[l/LC] = m;
+    }
+
+    void i()
+    {
+        for (unsigned i=0; i<data.size(); i++) data[i].i();
+    }
+
+
+    template<class Real2>
+    FullVector<Real2> operator*(const FullVector<Real2>& v) const
+    {
+        FullVector<Real2> res;
+        res.resize(cSize);
+        for (unsigned b=0; b<data.size()-1; b++)
+        {
+            for (int j=0; j<LC; j++)
+            {
+                res[b*LC+j] = 0;
+                for (int i=0; i<LC; i++)
+                {
+                    res[b*LC+j] += data[b].element(i,j) * v[b*LC+i];
+                }
+            }
+        }
+
+        int last_block = (data.size()-1)*LC;
+        for (int j=0; last_block+j<(int) cSize; j++)
+        {
+            res[last_block+j] = 0;
+            for (int i=0; i<LC; i++)
+            {
+                res[j] += data[data.size()-1].element(i,j) * v[last_block+i];
+            }
+        }
+
+        return res;
+    }
+
+    template<class Real2>
+    void mult(FullVector<Real2>& res, const FullVector<Real2>& v) const
+    {
+        for (unsigned b=0; b<data.size()-1; b++)
+        {
+            for (int j=0; j<LC; j++)
+            {
+                res[b*LC+j] = 0;
+                for (int i=0; i<LC; i++)
+                {
+                    res[b*LC+j] += data[b].element(i,j) * v[b*LC+i];
+                }
+            }
+        }
+
+        int last_block = (data.size()-1)*LC;
+        for (int j=0; last_block+j<(int) cSize; j++)
+        {
+            res[last_block+j] = 0;
+            for (int i=0; i<LC; i++)
+            {
+                res[j] += data[data.size()-1].element(i,j) * v[last_block+i];
+            }
+        }
+    }
+
+    friend std::ostream& operator << (std::ostream& out, const BlockDiagonalMatrix<LC>& v )
+    {
+        out << "[";
+        for (unsigned i=0; i<v.data.size(); i++) out << " " << v.data[i];
+        out << " ]";
+        return out;
+    }
+
+    static const char* Name() { return "BlockDiagonalMatrix"; }
+};
+
+class BlockDiagonalMatrix3 : public BlockDiagonalMatrix<3>
+{
+public :
+    static const char* Name() { return "BlockDiagonalMatrix3"; }
+};
+
+class BlockDiagonalMatrix6 : public BlockDiagonalMatrix<6>
+{
+public :
+    static const char* Name() { return "BlockDiagonalMatrix6"; }
+};
+
+class BlockDiagonalMatrix9 : public BlockDiagonalMatrix<9>
+{
+public :
+    static const char* Name() { return "BlockDiagonalMatrix9"; }
+};
+
+class BlockDiagonalMatrix12 : public BlockDiagonalMatrix<12>
+{
+public :
+    static const char* Name() { return "BlockDiagonalMatrix12"; }
 };
 
 } // namespace linearsolver
