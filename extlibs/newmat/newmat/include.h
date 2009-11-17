8c63dcdb978acc5dd115db63aa8201fd56881dc6
theimann <theimann@users.gforge.inria.fr>
2009-11-17 13:31:20 +0000

    r5801/sofa-dev : FIX: adjust floating point type according to SOFA_FLOAT/SOFA_DOUBLE settings

--- extlibs/newmat/newmat/include.h
+++ extlibs/newmat/newmat/include.h
@@ -28,8 +28,11 @@
 
 //#define DO_FREE_CHECK                   // check news and deletes balance
 
+#ifndef SOFA_FLOAT
 #define USING_DOUBLE                    // elements of type double
-//#define USING_FLOAT                   // elements of type float
+#else
+#define USING_FLOAT                   // elements of type float
+#endif
 
 #define bool_LIB 0                      // for compatibility with my older libraries
 
