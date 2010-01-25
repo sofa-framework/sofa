5d92f74cc8923f58d0054908c216784f5b4ebbc9
falipou <falipou@users.gforge.inria.fr>
2010-01-25 16:11:45 +0000

    r6396/sofa-dev : ADD: a control panel for the Trace of the visitor in order to configure the output of the state vectors, and a text to modify the focus of the graph of execution

---
+++ applications/sofa/gui/qt/QVisitorControlPanel.h
@@ -0,0 +1,45 @@
+#ifndef SOFA_GUI_QT_SOFA_GUI_QT_QVISITORCONTROLPANEL_H
+#define SOFA_GUI_QT_SOFA_GUI_QT_QVISITORCONTROLPANEL_H
+
+#include <sofa/simulation/common/Node.h>
+
+#ifdef SOFA_QT4
+#include <QWidget>
+#include <QLineEdit>
+#else
+#include <qwidget.h>
+#include <qlineedit.h>
+#endif
+
+
+namespace sofa
+{
+namespace gui
+{
+namespace qt
+{
+
+class QVisitorControlPanel : public QWidget
+{
+    Q_OBJECT
+public:
+    QVisitorControlPanel(QWidget* parent);
+
+public slots:
+    void activateTraceStateVectors(bool);
+    void changeFirstIndex(int);
+    void changeRange(int);
+    void filterResults();
+signals:
+    void focusOn(QString);
+protected:
+    QLineEdit *textFilter;
+};
+
+
+} // qt
+} // gui
+} //sofa
+
+#endif // SOFA_GUI_QT_QVISITORCONTROLPANEL_H
+
