a6663ec6ed3a185e2f03d696ca1a5bfe3011e969
Jeremie Allard <jeremie.allard@insimo.fr>
2010-01-25 18:22:53 +0000

    r6408/sofa-dev : WarpPreconditioner: cpu version is now working
     //partial sync (2/4 changes)//

--- modules/sofa/component/linearsolver/WarpPreconditioner.cpp
+++ modules/sofa/component/linearsolver/WarpPreconditioner.cpp
@@ -26,27 +26,17 @@
 //
 // Copyright: See COPYING file that comes with this distribution
 #include <sofa/component/linearsolver/WarpPreconditioner.h>
-#include <sofa/component/linearsolver/NewMatMatrix.h>
 #include <sofa/component/linearsolver/FullMatrix.h>
 #include <sofa/component/linearsolver/SparseMatrix.h>
-#include <sofa/core/ObjectFactory.h>
-#include <iostream>
-#include "sofa/helper/system/thread/CTime.h"
 #include <sofa/core/objectmodel/BaseContext.h>
 #include <sofa/core/componentmodel/behavior/LinearSolver.h>
-#include <math.h>
+#include <sofa/helper/accessor.h>
 #include <sofa/helper/system/thread/CTime.h>
-#include <sofa/component/forcefield/TetrahedronFEMForceField.h>
+#include <sofa/core/ObjectFactory.h>
 #include <sofa/defaulttype/Vec3Types.h>
 
-#ifdef SOFA_HAVE_CSPARSE
-#include <sofa/component/linearsolver/SparseCholeskySolver.h>
-#include <sofa/component/linearsolver/SparseLDLSolver.h>
-#include <sofa/component/linearsolver/CompressedRowSparseMatrix.h>
-
-#else
-#include <sofa/component/linearsolver/CholeskySolver.h>
-#endif
+#include <iostream>
+#include <math.h>
 
 namespace sofa
 {
@@ -60,246 +50,129 @@ namespace linearsolver
 template<class DataTypes>
 WarpPreconditioner<DataTypes>::WarpPreconditioner()
     : f_verbose( initData(&f_verbose,false,"verbose","Dump system state at each iteration") )
-    , realSolver(NULL)
+    , solverName(initData(&solverName, std::string(""), "solverName", "Name of the solver/preconditioner to warp"))
+    , realSolver(NULL), mstate(NULL), forceField(NULL)
 {
     first = true;
 }
 
 
 template<class DataTypes>
-void WarpPreconditioner<DataTypes>::init()
+void WarpPreconditioner<DataTypes>::bwdInit()
 {
-    this->getContext()->get(realSolver);
+    mstate = dynamic_cast<core::componentmodel::behavior::MechanicalState<DataTypes>*>(this->getContext()->getMechanicalState());
+    this->getContext()->get(realSolver, solverName.getValue());
+    this->getContext()->get(forceField);
 }
 
-#if 0
-template<class TMatrix, class TVector,class TDataTypes>
-void WarpPreconditioner<TMatrix,TVector,TDataTypes>::setSystemMBKMatrix(double mFact, double bFact, double kFact)
+template<class DataTypes>
+void WarpPreconditioner<DataTypes>::setSystemMBKMatrix(double mFact, double bFact, double kFact)
 {
-    // Update the matrix only the first time
-    if (first) Inherit::setSystemMBKMatrix(mFact,bFact,kFact);
-    else Inherit::needInvert = true;
+    if (first && realSolver) realSolver->setSystemMBKMatrix(mFact, bFact, kFact);
 }
 
-//Solve x = R * M^-1 * R^t * b
-template<class TMatrix, class TVector,class TDataTypes>
-void WarpPreconditioner<TMatrix,TVector,TDataTypes>::solve (Matrix& M, Vector& z, Vector& r)
+template<class DataTypes>
+void WarpPreconditioner<DataTypes>::resetSystem()
 {
-    //Solve z = R^t * b
-    for (unsigned i=0; i<R.size(); i++)
-    {
-        z[i * 3 + 0] = R[i][0][0] * r[i * 3 + 0] + R[i][1][0] * r[i * 3 + 1] + R[i][2][0] * r[i * 3 + 2];
-        z[i * 3 + 1] = R[i][0][1] * r[i * 3 + 0] + R[i][1][1] * r[i * 3 + 1] + R[i][2][1] * r[i * 3 + 2];
-        z[i * 3 + 2] = R[i][0][2] * r[i * 3 + 0] + R[i][1][2] * r[i * 3 + 1] + R[i][2][2] * r[i * 3 + 2];
-    }
-
-    //Solve tmp = M^-1 * z
-    for (unsigned j=0; j<M.rowSize(); j++)
-    {
-        tmp[j] = 0.0;
-        for (unsigned i=0; i<M.colSize(); i++)
-        {
-            //initial matrix + current rotations
-            tmp[j] += M.element(i,j) * z[i];
-        }
-    }
-
-    //Solve z = R * tmp
-    for (unsigned i=0; i<R.size(); i++)
-    {
-        z[i * 3 + 0] = R[i][0][0] * tmp[i * 3] + R[i][0][1] * tmp[i * 3 + 1] + R[i][0][2] * tmp[i * 3 + 2];
-        z[i * 3 + 1] = R[i][1][0] * tmp[i * 3] + R[i][1][1] * tmp[i * 3 + 1] + R[i][1][2] * tmp[i * 3 + 2];
-        z[i * 3 + 2] = R[i][2][0] * tmp[i * 3] + R[i][2][1] * tmp[i * 3 + 1] + R[i][2][2] * tmp[i * 3 + 2];
-    }
+    if (first && realSolver) realSolver->resetSystem();
 }
 
-#ifdef SOFA_HAVE_CSPARSE
-
-template<class TMatrix, class TVector,class TDataTypes>
-void WarpPreconditioner<TMatrix,TVector,TDataTypes>::precompute(Matrix& M)
+template<class DataTypes>
+void WarpPreconditioner<DataTypes>::invertSystem()
 {
-    std::stringstream ss;
-    ss << this->getContext()->getName() << ".comp";
-    std::ifstream compFileIn(ss.str().c_str(), std::ifstream::binary);
-    unsigned int n=0, nbCol=0;
-    this->getMatrixDimension(&n, &nbCol);
+    if (first && realSolver) realSolver->invertSystem();
+}
 
-    if(compFileIn.good())
+/// Set the linear system right-hand term vector, from the values contained in the (Mechanical/Physical)State objects
+template<class DataTypes>
+void WarpPreconditioner<DataTypes>::setSystemRHVector(VecId v)
+{
+    systemRHVId = v;
+
+    //if (realSolver) realSolver->setSystemRHVector(v);
+}
+
+/// Set the initial estimate of the linear system left-hand term vector, from the values contained in the (Mechanical/Physical)State objects
+/// This vector will be replaced by the solution of the system once solveSystem is called
+template<class DataTypes>
+void WarpPreconditioner<DataTypes>::setSystemLHVector(VecId v)
+{
+    systemLHVId = v;
+    //if (realSolver) realSolver->setSystemLHVector(v);
+}
+
+/// Solve the system as constructed using the previous methods
+template<class DataTypes>
+void WarpPreconditioner<DataTypes>::solveSystem()
+{
+    //std::cout << "SOLVE" << std::endl;
+    if (!realSolver || !mstate) return;
+    //std::cout << ">SOLVE" << std::endl;
+
+    if (rotatedLHVId.isNull())
     {
-        cout << "file open : " << ss.str() << " compliance being loaded" << endl;
-        compFileIn.read((char*)M[0], n * n * sizeof(double));
-        compFileIn.close();
+        rotatedLHVId = VecId(VecId::V_DERIV, VecId::V_FIRST_DYNAMIC_INDEX);
+        mstate->vAvail(rotatedLHVId);
+        mstate->vAlloc(rotatedLHVId);
+        sout << "Allocated LH vector " << rotatedLHVId << sendl;
+    }
+    if (rotatedRHVId.isNull())
+    {
+        rotatedRHVId = VecId(VecId::V_DERIV, VecId::V_FIRST_DYNAMIC_INDEX);
+        mstate->vAvail(rotatedRHVId);
+        mstate->vAlloc(rotatedRHVId);
+        sout << "Allocated RH vector " << rotatedRHVId << sendl;
+    }
+
+    if (forceField)
+        getRotations();
+
+    if (forceField)
+    {
+        helper::ReadAccessor <VecDeriv> v = *mstate->getVecDeriv(systemRHVId.index);
+        helper::WriteAccessor<VecDeriv> vR = *mstate->getVecDeriv(rotatedRHVId.index);
+        unsigned int size = v.size();
+        for (unsigned int i=0; i<size; ++i)
+            vR[i] = data.R[i].multTranspose(v[i]);              // rotatedRH = R^t * systemRH
     }
     else
+        mstate->vOp(rotatedRHVId, systemRHVId);   // rotatedRH = systemRH
+
+    realSolver->setSystemRHVector(rotatedRHVId);
+    realSolver->setSystemLHVector(rotatedLHVId);
+    realSolver->solveSystem();                     // rotatedLH = M^-1 * rotatedRH
+
+    if (forceField)
     {
-        cout << "Compute the initial invert matrix" << endl;
-
-        sofa::component::linearsolver::SparseCholeskySolver< CompressedRowSparseMatrix<double>, FullVector<double> > sparsecholesky;
-
-        FullVector<double> b;
-        FullVector<double> x;
-        CompressedRowSparseMatrix<double> Mc;
-
-        b.resize(n);
-        x.resize(n);
-        Mc.resize(n,n);
-
-        for (unsigned j=0; j<n; j++)
-        {
-            for (unsigned i=0; i<n; i++)
-            {
-                if (M.element(i,j)) Mc.set(i,j,M.element(i,j));
-            }
-        }
-
-        /*
-        std::ifstream comp("toto", std::ifstream::binary);
-        if(comp.good()) {
-        	if (!sparsecholesky.readFile(comp)) {
-        		std::ofstream comp2("toto", std::fstream::binary);
-        		sparsecholesky.invert(Mc);
-        		sparsecholesky.writeFile(comp2);
-        	} else {
-        		std::cout << "file loaded" << std::endl;
-        	}
-        } else {
-        	std::ofstream comp2("toto", std::fstream::binary);
-        	sparsecholesky.invert(Mc); //creat LU decomposition in cholesky
-        	sparsecholesky.writeFile(comp2);
-        }
-        */
-
-        sparsecholesky.invert(Mc); //creat LU decomposition in cholesky
-
-        //Compute M^-1 in the systemMatrix of WarpPreconditioner (M)
-        for (unsigned i=0; i<n; i++)
-        {
-            b.clear();
-            b.set(i,1.0);
-
-            sparsecholesky.solve(Mc,x,b); //M n'est pas utilisé ni modifié, dans l'algo de cholesky
-
-            for (unsigned j=0; j<n; j++) M.set(i,j,x.element(j));
-        }
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////
-        std::ofstream compFileOut(ss.str().c_str(), std::fstream::out | std::fstream::binary);
-        compFileOut.write((char*)M[0], n * n*sizeof(double));
-        compFileOut.close();
-
-        Mc.clear();
+        helper::WriteAccessor<VecDeriv> v = *mstate->getVecDeriv(systemLHVId.index);
+        helper::ReadAccessor <VecDeriv> vR = *mstate->getVecDeriv(rotatedLHVId.index);
+        unsigned int size = v.size();
+        for (unsigned int i=0; i<size; ++i)
+            v[i] = data.R[i] * (vR[i]); // systemLH = R * rotatedLH
     }
+    else
+        mstate->vOp(systemLHVId, rotatedLHVId);   // systemLH = rotatedLH
 
+    //std::cout << "<SOLVE" << std::endl;
     first = false;
-
 }
 
-#else
-
-template<class TMatrix, class TVector,class TDataTypes>
-void WarpPreconditioner<TMatrix,TVector,TDataTypes>::precompute(Matrix& M)
+template<class TDataTypes>
+void WarpPreconditioner<TDataTypes>::getRotations()
 {
-    std::stringstream ss;
-    ss << this->getContext()->getName() << ".comp";
-    std::ifstream compFileIn(ss.str().c_str(), std::ifstream::binary);
-    unsigned int n=0, nbCol=0;
-    this->getMatrixDimension(&n, &nbCol);
-
-    if(compFileIn.good())
-    {
-        cout << "file open : " << ss.str() << " compliance being loaded" << endl;
-        compFileIn.read((char*)M[0], n * n * sizeof(double));
-        compFileIn.close();
-    }
-    else
-    {
-        cout << "Compute the initial invert matrix" << endl;
-
-        sofa::component::linearsolver::CholeskySolver< TMatrix, TVector > cholesky;
-
-        FullVector<double> b;
-        FullVector<double> x;
-
-        b.resize(n);
-        x.resize(n);
-
-        cholesky.invert(M); //creat LU decomposition in cholesky
-
-        //Compute M^-1 in the systemMatrix of WarpPreconditioner (M)
-        for (unsigned i=0; i<n; i++)
-        {
-            b.clear();
-            b.set(i,1.0);
-
-            cholesky.solve(M,x,b); //M n'est pas utilisé ni modifié, dans l'algo de cholesky
-
-            for (unsigned j=0; j<n; j++) M.set(i,j,x.element(j));
-        }
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////
-        std::ofstream compFileOut(ss.str().c_str(), std::fstream::out | std::fstream::binary);
-        compFileOut.write((char*)M[0], n * n*sizeof(double));
-        compFileOut.close();
-    }
-
-    first = false;
-
+    if (!forceField || !mstate) return;
+    unsigned int size = mstate->getSize();
+    data.R.resize(size);
+    for (unsigned int i=0; i<size; ++i)
+        forceField->getRotation(data.R[i], i);
 }
-
-#endif
-
-template<class TMatrix, class TVector,class TDataTypes>
-void WarpPreconditioner<TMatrix,TVector,TDataTypes>::invert(Matrix& M)
-{
-    if (first)
-    {
-        mstate = dynamic_cast< behavior::MechanicalState<TDataTypes>* >(this->getContext()->getMechanicalState());
-        assert(mstate);
-
-        this->precompute(M);
-        unsigned int n=0, nbCol=0;
-        this->getMatrixDimension(&n, &nbCol);
-
-        R.resize(n/3);
-        tmp.resize(n);
-        //R.clear();
-
-        for (unsigned i=0; i<R.size(); i++)
-        {
-            R[i][0][0] = R[i][1][1] = R[i][2][2] = 1.0;
-            R[i][0][1] = R[i][0][2] = R[i][1][0] = R[i][1][2] = R[i][2][0] = R[i][2][1] = 0.0;
-        }
-    }
-
-    this->rotateConstraints();
-}
-
-template<class TMatrix, class TVector,class TDataTypes>
-void WarpPreconditioner<TMatrix,TVector,TDataTypes>::rotateConstraints()
-{
-    simulation::Node *node = dynamic_cast<simulation::Node *>(this->getContext());
-
-    sofa::component::forcefield::TetrahedronFEMForceField<TDataTypes>* forceField = NULL;
-    if (node != NULL)
-    {
-        forceField = node->get<component::forcefield::TetrahedronFEMForceField<TDataTypes> > ();
-    }
-    else
-    {
-        sout << "No rotation defined  !";
-        return;
-    }
-
-    VecDeriv& dx = *mstate->getDx();
-    for(unsigned int j = 0; j < dx.size(); j++)	forceField->getRotation(R[j], j);
-}
-#endif
 
 SOFA_DECL_CLASS(WarpPreconditioner)
 
 int WarpPreconditionerClass = core::RegisterObject("")
-        .add< WarpPreconditioner< defaulttype::Vec3dTypes >(true)
-        ;
+        .add< WarpPreconditioner< defaulttype::Vec3dTypes > >(true)
+        .addAlias("WarpLinearSolver");
+;
 
 } // namespace linearsolver
 
