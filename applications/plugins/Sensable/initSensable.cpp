7b06c95d88753f410a1f8e334ad157ed3e3c25c5
Jeremie Allard <jeremie.allard@insimo.fr>
2010-02-05 20:25:41 +0000

    r6524/sofa-dev : FIX: win32 compilation of metis, vrpn, taucs, and try to fix issue with spaces in path of OpenHaptics SDK Also remove files from Xitact SDK, as we cannot redistribute them...
     //partial sync (4/36 changes)//

--- applications/plugins/Sensable/initSensable.cpp
+++ applications/plugins/Sensable/initSensable.cpp
@@ -39,6 +39,14 @@
 #endif
 #endif
 
+#ifdef WIN32
+// BUGFIX(Jeremie A. 02-05-2009): put OpenHaptics libs here instead of the project file to work around a bug in qmake when there is a space in an environment variable used to look-up a library
+#pragma comment(lib,"hl.lib")
+#pragma comment(lib,"hd.lib")
+#pragma comment(lib,"hdu.lib")
+#endif
+
+
 namespace sofa
 {
 
