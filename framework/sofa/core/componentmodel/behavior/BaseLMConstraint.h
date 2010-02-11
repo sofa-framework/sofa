ac326776fe339e6ee18021e59ba561a55b884ab0
Maxime Tournier <mtournie@users.gforge.inria.fr>
2010-02-11 09:53:31 +0000

    r6573/sofa-dev : WARNING FIX ADD fixed map bugs, added convenience functions to get indices in a group, added checks for single object constraints

--- framework/sofa/core/componentmodel/behavior/BaseLMConstraint.h
+++ framework/sofa/core/componentmodel/behavior/BaseLMConstraint.h
@@ -1,29 +1,29 @@
 /******************************************************************************
-*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 4      *
-*                (c) 2006-2009 MGH, INRIA, USTL, UJF, CNRS                    *
-*                                                                             *
-* This library is free software; you can redistribute it and/or modify it     *
-* under the terms of the GNU Lesser General Public License as published by    *
-* the Free Software Foundation; either version 2.1 of the License, or (at     *
-* your option) any later version.                                             *
-*                                                                             *
-* This library is distributed in the hope that it will be useful, but WITHOUT *
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
-* for more details.                                                           *
-*                                                                             *
-* You should have received a copy of the GNU Lesser General Public License    *
-* along with this library; if not, write to the Free Software Foundation,     *
-* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
-*******************************************************************************
-*                              SOFA :: Framework                              *
-*                                                                             *
-* Authors: M. Adam, J. Allard, B. Andre, P-J. Bensoussan, S. Cotin, C. Duriez,*
-* H. Delingette, F. Falipou, F. Faure, S. Fonteneau, L. Heigeas, C. Mendoza,  *
-* M. Nesme, P. Neumann, J-P. de la Plata Alcade, F. Poyer and F. Roy          *
-*                                                                             *
-* Contact information: contact@sofa-framework.org                             *
-******************************************************************************/
+ *       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 4      *
+ *                (c) 2006-2009 MGH, INRIA, USTL, UJF, CNRS                    *
+ *                                                                             *
+ * This library is free software; you can redistribute it and/or modify it     *
+ * under the terms of the GNU Lesser General Public License as published by    *
+ * the Free Software Foundation; either version 2.1 of the License, or (at     *
+ * your option) any later version.                                             *
+ *                                                                             *
+ * This library is distributed in the hope that it will be useful, but WITHOUT *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+ * for more details.                                                           *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public License    *
+ * along with this library; if not, write to the Free Software Foundation,     *
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+ *******************************************************************************
+ *                              SOFA :: Framework                              *
+ *                                                                             *
+ * Authors: M. Adam, J. Allard, B. Andre, P-J. Bensoussan, S. Cotin, C. Duriez,*
+ * H. Delingette, F. Falipou, F. Faure, S. Fonteneau, L. Heigeas, C. Mendoza,  *
+ * M. Nesme, P. Neumann, J-P. de la Plata Alcade, F. Poyer and F. Roy          *
+ *                                                                             *
+ * Contact information: contact@sofa-framework.org                             *
+ ******************************************************************************/
 #ifndef SOFA_COMPONENT_CONSTRAINT_BASELMCONSTRAINT_H
 #define SOFA_COMPONENT_CONSTRAINT_BASELMCONSTRAINT_H
 
@@ -59,10 +59,10 @@ public:
 
     /**
      * \brief Expression of a line of the system created to solve the constraint
-         *
+     *
      * @param idxInConstrainedDOF1 index of the line of the Jacobian in the Constrained DOF1 (can be a mapped dof)
      * @param idxInConstrainedDOF2 index of the line of the Jacobian in the Constrained DOF2 (can be a mapped dof)
-         * @param correction           right hand term of the equation: corresponds to a correction we have to apply to the system
+     * @param correction           right hand term of the equation: corresponds to a correction we have to apply to the system
      **/
     struct ConstraintEquation
     {
@@ -92,7 +92,7 @@ public:
          * @param i0 index of the entry in the VecConst for the first object
          * @param i1 index of the entry in the VecConst for the second object
          * @param c  correction we need to apply in order to solve the constraint
-             **/
+         **/
         void addConstraint(  unsigned int i0, unsigned int i1, SReal c)
         {
             equations.resize(equations.size()+1);
@@ -106,7 +106,7 @@ public:
          *
          * @param i  index of the entry in the VecConst for the first object
          * @param c  correction we need to apply in order to solve the constraint
-             **/
+         **/
         void addConstraint(  unsigned int i0,  SReal c)
         {
             equations.resize(equations.size()+1);
@@ -178,42 +178,90 @@ public:
     /// @see ConstraintGroup
     virtual ConstraintGroup* addGroupConstraint( ConstOrder Order);
 
-    /// Get Left Hand Term
+
+    /// Get Left Hand Term for a given constraint group
     template <typename DataStorage>
-    void getIndicesUsed1(ConstOrder Order, DataStorage &used0)
+    void getIndicesUsed1(const BaseLMConstraint::ConstraintGroup* group, DataStorage &used0) const
     {
-        const helper::vector< BaseLMConstraint::ConstraintGroup* > &constraints=constraintOrder[Order];
+        if( ! getConstrainedMechModel1() )  return;
+
+        typedef ConstraintGroup::EquationConstIterator iterator_t;
+        std::pair< iterator_t, iterator_t > range=group->data();
+
+        for (iterator_t equation=range.first; equation!=range.second; ++equation)
+        {
+            if (equation->idxInConstrainedDOF1 >= 0)
+            {
+                line_map::const_iterator line = linesInSimulatedObject1.find(equation->idxInConstrainedDOF1);
+                assert( line != linesInSimulatedObject1.end() );
+
+                used0.push_back(line->second);
+
+            }
+        }
+
+    }
+
+    /// Get Left Hand Term for each ConstraintGroup of a given order
+    template <typename DataStorage>
+    void getIndicesUsed1(ConstOrder Order, DataStorage &used0) const
+    {
+        constraintOrder_t::const_iterator g = constraintOrder.find(Order);
+        assert( g != constraintOrder.end() );
+
+        const helper::vector< BaseLMConstraint::ConstraintGroup* > &constraints =
+            g->second;
 
         for (unsigned int idxGroupConstraint=0; idxGroupConstraint<constraints.size(); ++idxGroupConstraint)
         {
             ConstraintGroup *group=constraints[idxGroupConstraint];
-            std::pair< ConstraintGroup::EquationIterator, ConstraintGroup::EquationIterator > range=group->data();
+            getIndicesUsed1(group, used0);
+        }
+    }
 
-            for (ConstraintGroup::EquationIterator equation=range.first; equation!=range.second; ++equation)
+
+
+    template <typename DataStorage>
+    void getIndicesUsed2(const BaseLMConstraint::ConstraintGroup* group, DataStorage &used1) const
+    {
+        if( ! getConstrainedMechModel2() )  return;
+
+        typedef ConstraintGroup::EquationConstIterator iterator_t;
+        std::pair< iterator_t, iterator_t > range=group->data();
+
+        for (iterator_t equation=range.first; equation!=range.second; ++equation)
+        {
+            if (equation->idxInConstrainedDOF2 >= 0)
             {
-                if (equation->idxInConstrainedDOF1 >= 0)
-                    used0.push_back(linesInSimulatedObject1[equation->idxInConstrainedDOF1]);
+                typename line_map::const_iterator line =
+                    linesInSimulatedObject2.find(equation->idxInConstrainedDOF2);
+
+                assert( line != linesInSimulatedObject2.end() );
+
+                used1.push_back(line->second);
             }
         }
     }
 
+
     template <typename DataStorage>
-    void getIndicesUsed2(ConstOrder Order, DataStorage &used1)
+    void getIndicesUsed2(ConstOrder Order, DataStorage &used1) const
     {
-        const helper::vector< BaseLMConstraint::ConstraintGroup* > &constraints=constraintOrder[Order];
+        constraintOrder_t::const_iterator g = constraintOrder.find(Order);
+        assert( g != constraintOrder.end() );
+
+        const helper::vector< BaseLMConstraint::ConstraintGroup* > &constraints =
+            g->second;
 
         for (unsigned int idxGroupConstraint=0; idxGroupConstraint<constraints.size(); ++idxGroupConstraint)
         {
             ConstraintGroup *group=constraints[idxGroupConstraint];
-            std::pair< ConstraintGroup::EquationIterator, ConstraintGroup::EquationIterator > range=group->data();
-
-            for (ConstraintGroup::EquationIterator equation=range.first; equation!=range.second; ++equation)
-            {
-                if (equation->idxInConstrainedDOF2 >= 0)
-                    used1.push_back(linesInSimulatedObject2[equation->idxInConstrainedDOF2]);
-            }
+            getIndicesUsed2( group, used1 );
         }
     }
+
+
+
 
     /// Get Right Hand Term
     virtual void getCorrections(ConstOrder Order, helper::vector<SReal>& c);
@@ -222,7 +270,12 @@ public:
     /// Get the internal structure: return all the constraint stored by their nature in a map
     virtual void getConstraints( std::map< ConstOrder, helper::vector< ConstraintGroup* > >  &i) { i=constraintOrder;}
     /// Get all the constraints stored of a given nature
-    virtual const helper::vector< ConstraintGroup* > &getConstraintsOrder(ConstOrder Order) { return constraintOrder[Order];}
+    virtual const helper::vector< ConstraintGroup* > &getConstraintsOrder(ConstOrder Order) const
+    {
+        constraintOrder_t::const_iterator c = constraintOrder.find( Order );
+        assert( c != constraintOrder.end());
+        return c->second;
+    }
 
 
 
@@ -232,14 +285,14 @@ public:
 
 
     /// get Mechanical State 1 where the constraint will be expressed (can be a Mapped mechanical state)
-    virtual BaseMechanicalState* getConstrainedMechModel1()=0;
+    virtual BaseMechanicalState* getConstrainedMechModel1() const=0;
     /// get Mechanical State 2 where the constraint will be expressed (can be a Mapped mechanical state)
-    virtual BaseMechanicalState* getConstrainedMechModel2()=0;
+    virtual BaseMechanicalState* getConstrainedMechModel2() const=0;
 
     /// get Mechanical State 1 where the constraint will be solved
-    virtual BaseMechanicalState* getSimulatedMechModel1()=0;
+    virtual BaseMechanicalState* getSimulatedMechModel1()const =0;
     /// get Mechanical State 2 where the constraint will be solved
-    virtual BaseMechanicalState* getSimulatedMechModel2()=0;
+    virtual BaseMechanicalState* getSimulatedMechModel2()const =0;
 
     /// If the constraint is applied only on a subset of particles.
     /// That way, we can optimize the time spent traversing the mappings
@@ -260,7 +313,8 @@ protected:
 
     /// Constraints stored depending on their nature
     /// @see ConstraintGroup
-    std::map< ConstOrder, helper::vector< ConstraintGroup* > > constraintOrder;
+    typedef std::map< ConstOrder, helper::vector< ConstraintGroup* > > constraintOrder_t;
+    constraintOrder_t constraintOrder;
 
 
     Data<std::string> pathObject1;
@@ -268,8 +322,9 @@ protected:
 
 
     /// stores the indices of the lines in the vector C of each MechanicalState
-    std::map< unsigned int,unsigned int > linesInSimulatedObject1;
-    std::map< unsigned int,unsigned int > linesInSimulatedObject2;
+    typedef std::map< unsigned int,unsigned int > line_map;
+    line_map linesInSimulatedObject1;
+    line_map linesInSimulatedObject2;
 };
 }
 }
