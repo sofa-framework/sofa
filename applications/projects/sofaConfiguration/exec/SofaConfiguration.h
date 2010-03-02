9e8777ecfce2559dc81f86ac6b6fa0b4fabe10ba
Francois Jourdes <francois.jourdes@insimo.fr>
2010-03-02 09:32:15 +0000

    r6769/sofa-dev : CHANGE win32 and macOS : the projects generation script is launched through a q3process so that the gui is not idle during its execution.

--- applications/projects/sofaConfiguration/exec/SofaConfiguration.h
+++ applications/projects/sofaConfiguration/exec/SofaConfiguration.h
@@ -37,6 +37,8 @@
 #include <QLineEdit>
 #include <QDir>
 #include <QLayout>
+#include <QPushButton>
+#include <Q3Process>
 #else
 #include <qmainwindow.h>
 #include <qcheckbox.h>
@@ -44,6 +46,9 @@
 #include <qlineedit.h>
 #include <qdir.h>
 #include <qlayout.h>
+#include <qpushbutton.h>
+#include <qprocess.h>
+typedef QProcess Q3Process;
 #endif
 
 #include <iostream>
@@ -171,8 +176,10 @@ class SofaConfiguration : public QMainWindow
     Q_OBJECT
 public :
 
-    SofaConfiguration(std::string path, std::vector< DEFINES >& config);
-    ~SofaConfiguration() {};
+    SofaConfiguration(std::string path_, std::vector< DEFINES >& config);
+    ~SofaConfiguration()
+    {
+    };
 
     bool getValue(CONDITION &option);
 
@@ -184,6 +191,11 @@ public slots:
     void updateOptions();
     void updateConditions();
     void saveConfiguration();
+
+protected slots:
+    void redirectStdErr();
+    void redirectStdOut();
+    void saveConfigurationDone();
 protected:
     std::string path;
     std::vector< DEFINES >& data;
@@ -191,7 +203,11 @@ protected:
     std::set< QWidget *> optionsModified;
 
     QLineEdit *projectVC;
+    QPushButton* saveButton;
+    Q3Process* p;
 };
+
+
 }
 }
 }
