04f0f2a0b37953bdebbc53363948bf848913ddb2
Frederick Roy <frederick.roy@inria.fr>
2010-03-22 15:48:45 +0000

    r6962/sofa-dev : UPDATE: InputEventReader : save (linux) events into a file, which can be read afterwards

--- modules/sofa/component/misc/InputEventReader.cpp
+++ modules/sofa/component/misc/InputEventReader.cpp
@@ -28,11 +28,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#ifdef __linux__
-#include <linux/input.h>
-#include <poll.h>
-#endif
-
 #include <sofa/core/objectmodel/KeypressedEvent.h>
 #include <sofa/core/objectmodel/KeyreleasedEvent.h>
 
@@ -58,6 +53,9 @@ InputEventReader::InputEventReader()
 //, timeout( initData(&timeout, 0, "timeout", "time out to get an event from file" ))
     , p_key1(initData(&p_key1, '0', "key1","Key event generated when the left pedal is pressed"))
     , p_key2(initData(&p_key2, '1', "key2","Key event generated when the right pedal is pressed"))
+    , p_writeEvents(initData(&p_writeEvents, false , "writeEvents","If true, write incoming events ; if false, read events from that file (if an output filename is provided)"))
+    , p_outputFilename(initData(&p_outputFilename, "outputFilename","Other filename where events will be stored (or read)"))
+    , inFile(NULL), outFile(NULL)
     , fd(-1)
     , deplX(0), deplY(0)
     , pedalValue(-1)
@@ -71,6 +69,31 @@ void InputEventReader::init()
     if((fd = open(filename.getFullPath().c_str(), O_RDONLY)) < 0)
         sout << "ERROR: impossible to open the file: " << filename.getValue() << sendl;
 #endif
+
+    if(p_outputFilename.isSet())
+    {
+        if (p_writeEvents.getValue())
+        {
+            outFile = new std::ofstream();
+            outFile->open(p_outputFilename.getFullPath().c_str());
+            if( !outFile->is_open() )
+            {
+                serr << "File " <<p_outputFilename.getFullPath() << " not writable" << std::endl;
+                delete outFile;
+                outFile = NULL;
+            }
+        }
+        else
+        {
+            inFile = new std::ifstream(p_outputFilename.getFullPath().c_str(), std::ifstream::in | std::ifstream::binary);
+            if( !inFile->is_open() )
+            {
+                serr << "File " <<p_outputFilename.getFullPath() << " not readable" << std::endl;
+                delete inFile;
+                inFile = NULL;
+            }
+        }
+    }
 }
 
 InputEventReader::~InputEventReader()
@@ -81,78 +104,125 @@ InputEventReader::~InputEventReader()
 #endif
 }
 
-void InputEventReader::getInputEvents()
+void InputEventReader::manageEvent(const input_event &ev)
 {
 #ifdef __linux__
-    if (fd < 0) return;
-    pollfd pfd;
-    pfd.fd = fd;
-    pfd.events = POLLIN;
-    pfd.revents = 0;
+    if (p_printEvent.getValue())
+        serr << "event type 0x" << std::hex << ev.type << std::dec << " code 0x" << std::hex << ev.code << std::dec << " value " << ev.value << sendl;
 
-    while (poll(&pfd, 1, 0 /*timeout.getValue()*/)>0 && (pfd.revents & POLLIN))
+    if (ev.type == EV_REL)
     {
-        struct input_event ev;
-        if (read(fd, &ev, sizeof(struct input_event)) == -1)
-            serr << "Error: read function return an error." << sendl;
-
-        if (p_printEvent.getValue())
-            serr << "event type 0x" << std::hex << ev.type << std::dec << " code 0x" << std::hex << ev.code << std::dec << " value " << ev.value << sendl;
-
-        if (ev.type == EV_REL)
+        switch (ev.code)
         {
-            switch (ev.code)
+        case REL_X:
+            if (inverseSense.getValue())
             {
-            case REL_X:
-                if (inverseSense.getValue())
-                {
-                    deplX -= ev.value; break;
-                }
-                else
-                {
-                    deplX += ev.value; break;
-                }
-            case REL_Y:
-                if (inverseSense.getValue())
-                {
-                    deplY -= ev.value; break;
-                }
-                else
-                {
-                    deplY += ev.value; break;
-                }
+                deplX -= ev.value; break;
             }
-        }
-
-        pedalValue = -1;
-
-        if (ev.type == EV_ABS)
-        {
-            switch (ev.code)
+            else
             {
-            case ABS_Y:
-                pedalValue = ev.value;
-                oldPedalState = currentPedalState;
-                //TODO: less specific
-                if (pedalValue > 156)
-                {
-                    //Left
-                    currentPedalState = LEFT_PEDAL;
-                }
-                else if (pedalValue < 100)
-                {
-                    //Right
-                    currentPedalState = RIGHT_PEDAL;
-                }
-                else
-                {
-                    //Nothing
-                    currentPedalState = NO_PEDAL;
-                }
-                break;
+                deplX += ev.value; break;
+            }
+        case REL_Y:
+            if (inverseSense.getValue())
+            {
+                deplY -= ev.value; break;
+            }
+            else
+            {
+                deplY += ev.value; break;
             }
         }
     }
+
+    pedalValue = -1;
+
+    if (ev.type == EV_ABS)
+    {
+        switch (ev.code)
+        {
+        case ABS_Y:
+            pedalValue = ev.value;
+            oldPedalState = currentPedalState;
+            //TODO: less specific
+            if (pedalValue > 156)
+            {
+                //Left
+                currentPedalState = LEFT_PEDAL;
+            }
+            else if (pedalValue < 100)
+            {
+                //Right
+                currentPedalState = RIGHT_PEDAL;
+            }
+            else
+            {
+                //Nothing
+                currentPedalState = NO_PEDAL;
+            }
+            break;
+        }
+    }
+#endif
+}
+
+void InputEventReader::getInputEvents()
+{
+#ifdef __linux__
+    //read device
+    if (!inFile)
+    {
+        if (fd < 0) return;
+        pollfd pfd;
+        pfd.fd = fd;
+        pfd.events = POLLIN;
+        pfd.revents = 0;
+        struct input_event ev, temp;
+
+        while (poll(&pfd, 1, 0 /*timeout.getValue()*/)>0 && (pfd.revents & POLLIN))
+        {
+            if (read(fd, &temp, sizeof(struct input_event)) == -1)
+                serr << "Error: read function return an error." << sendl;
+
+            memcpy(&ev, &temp, sizeof(struct input_event));
+
+            manageEvent(ev);
+
+            if (p_writeEvents.getValue())
+            {
+                if(outFile->good())
+                {
+                    *outFile << ev.type << " " << ev.code << " " << ev.value << " ";
+                }
+            }
+        }
+        if (p_writeEvents.getValue())
+        {
+            if(outFile->good())
+            {
+                *outFile << std::endl;
+            }
+        }
+    }
+    else //read file
+    {
+        if (!inFile->eof())
+        {
+            std::string line;
+            std::getline(*inFile, line);
+            std::istringstream ln(line);
+
+            //if(ln.str().empty()) std::cout << "NO EVENT" << std::endl;
+
+            while(!ln.eof())
+            {
+                struct input_event ev;
+                ln >> ev.type >> ev.code >> ev.value;
+                manageEvent(ev);
+            }
+        }
+    }
+
 #endif
 }
 
@@ -212,7 +282,6 @@ void InputEventReader::handleEvent(core::objectmodel::Event *event)
                 }
             }
         }
-
     }
 }
 
