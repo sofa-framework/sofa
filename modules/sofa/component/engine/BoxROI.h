4459813cbe0f261e67809d7c414b89fb75438772
Erik Pernod <erik.pernod@gmail.com>
2010-04-12 15:39:02 +0000

    r7092/sofa-dev : FIX: restore canCreate function in BoxROI. Still a problem of behavior in some case, see Ticket #136.

--- modules/sofa/component/engine/BoxROI.h
+++ modules/sofa/component/engine/BoxROI.h
@@ -96,14 +96,14 @@ public:
 
     /// Pre-construction check method called by ObjectFactory.
     /// Check that DataTypes matches the MechanicalState.
-    /*   template<class T>
-       static bool canCreate(T*& obj, core::objectmodel::BaseContext* context, core::objectmodel::BaseObjectDescription* arg)
-       {
-           if (dynamic_cast<MechanicalState<DataTypes>*>(context->getMechanicalState()) == NULL)
-               return false;
-           return BaseObject::canCreate(obj, context, arg);
-       }
-    */
+    template<class T>
+    static bool canCreate(T*& obj, core::objectmodel::BaseContext* context, core::objectmodel::BaseObjectDescription* arg)
+    {
+        if (dynamic_cast<MechanicalState<DataTypes>*>(context->getMechanicalState()) == NULL)
+            return false;
+        return BaseObject::canCreate(obj, context, arg);
+    }
+
     /// Construction method called by ObjectFactory.
     template<class T>
     static void create(T*& obj, core::objectmodel::BaseContext* context, core::objectmodel::BaseObjectDescription* arg)
