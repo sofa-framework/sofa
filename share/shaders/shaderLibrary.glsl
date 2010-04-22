ad84df7d9834921824166bed5248c2c5468228dc
mareko <mareko@users.gforge.inria.fr>
2010-04-22 19:44:55 +0000

    r7206/sofa-dev : UPDATE: shaders

--- share/shaders/shaderLibrary.glsl
+++ share/shaders/shaderLibrary.glsl
@@ -14,6 +14,15 @@ vec3 VectorOrthogonalize(vec3 V1, vec3 V2)
     return V2 - VectorProject(V1, V2);
 }
 
+// Pow(x,y) = x^y, which works even with negative base (x) numbers
+
+#define POW return pow(abs(x), y) * sign(x);
+
+float Pow(float x, float y) { POW }
+vec2 Pow(vec2 x, vec2 y) { POW }
+vec3 Pow(vec3 x, vec3 y) { POW }
+vec4 Pow(vec4 x, vec4 y) { POW }
+
 ///////////////////////////////////////////////////////////////////////////////
 // The brushed-metal normal computation.
 ///////////////////////////////////////////////////////////////////////////////
@@ -48,15 +57,15 @@ vec3 BrushedMetalNormal(vec3 Normal, vec3 Position, vec3 Direction, float Roughn
 // Good for: Skin rendering
 ///////////////////////////////////////////////////////////////////////////////
 
-float SchlickFresnel(float ViewDotHalf, float Reflectance)
+/*float SchlickFresnel(float ViewDotHalf, float Reflectance)
 {
     float Exponential = pow(1.0 - ViewDotHalf, 5.0);
     return Exponential + (1.0 - Exponential) * Reflectance;
-}
+}*/
 
 ///////////////////////////////////////////////////////////////////////////////
 // The Cook-Torrance specular lighting model with Lambert's diffuse lighting.
-// Good for: Metalic rendering
+// Good for: Pretty much anything :D
 ///////////////////////////////////////////////////////////////////////////////
 
 vec3 CookTorrance(vec3 Normal, vec3 LightDir, vec3 ViewDir,
@@ -109,12 +118,15 @@ mat3 ComputeFlatTangentSpaceBasis(vec3 Position, vec2 Texcoord)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// Vertex shader for:
+// Vertex shader:
 ///////////////////////////////////////////////////////////////////////////////
 #if defined(VertexShader)
 
 // Light parameters
 uniform vec3 LightPosition;
+
+// Planar mapping
+uniform vec4 PlaneS, PlaneT;
 
 varying vec3 Position;
 varying vec3 Normal;
@@ -132,17 +144,27 @@ void main()
 #if defined(Mirrored)
     Normal = -Normal;
 #endif
+
+#if defined(PlanarMapping)
+    Texcoord.x = dot(vec4(gl_Vertex.xyz, 1.0), PlaneS);
+    Texcoord.y = dot(vec4(gl_Vertex.xyz, 1.0), PlaneT);
+#else
     Texcoord  = gl_MultiTexCoord0.xy;
+#endif
     Tangent   = gl_MultiTexCoord1.xyz;
     Bitangent = gl_MultiTexCoord2.xyz;
 
     ViewDirection  = gl_ModelViewMatrixInverse[3].xyz - Position;
+#if 0
     LightDirection = (gl_ModelViewMatrixInverse * vec4(LightPosition, 1)).xyz - Position;
+#else
+    LightDirection = LightPosition - Position;
+#endif
 }
 
 #endif
 ///////////////////////////////////////////////////////////////////////////////
-// Fragment shader for:
+// Fragment shader:
 ///////////////////////////////////////////////////////////////////////////////
 #if defined(FragmentShader)
 
@@ -157,6 +179,16 @@ uniform vec3 BrushDirection;
 uniform float BrushDistance;
 uniform float BrushRoughness;
 uniform float BrushStrength;
+
+// Custom per-pixel distance-based model clipping
+uniform vec3 ClipOrigin;
+uniform float ClipDistance;
+
+// Exponential scale of texture coordinates
+uniform vec2 ExpScale;
+
+// DiffuseMap contrast adjustment
+uniform vec2 DiffuseContrastScaleBias;
 
 // Light parameters
 uniform vec3 AmbientColor;
@@ -178,6 +210,19 @@ varying vec3 LightDirection;
 
 vec3 mainFS()
 {
+#if defined(DistanceBasedCutting)
+    if (distance(Position, ClipOrigin) > ClipDistance)
+        discard;
+#endif
+
+    vec2 Texcoord = Texcoord;
+#if defined(ExponentialMapping)
+    Texcoord = fract(Texcoord);
+    Texcoord -= 0.5;
+    Texcoord = Pow(Texcoord, ExpScale);
+    Texcoord += 0.5;
+#endif
+
     // Copy and correct the input
     vec3 Ambient = AmbientColor;
     vec3 Diffuse  = DiffuseColor;
@@ -193,8 +238,14 @@ vec3 mainFS()
     //Specular = vec3(0.0);
 
 #if defined(DiffuseMap_Present)
+    vec3 DiffuseTexColor = texture2D(DiffuseMap, Texcoord).xyz;
+
+#if defined(DiffuseMap_AdjustContrast)
+    DiffuseTexColor = (DiffuseTexColor - DiffuseContrastScaleBias.y) * DiffuseContrastScaleBias.x + DiffuseContrastScaleBias.y;
+#endif    
+
     // Apply the diffuse map
-    Diffuse *= texture2D(DiffuseMap, Texcoord).xyz;
+    Diffuse *= DiffuseTexColor;
 #endif
 
 #if defined(NormalMap_Present)
@@ -221,7 +272,7 @@ vec3 mainFS()
 
 void main()
 {
-    gl_FragColor = vec4(mainFS(), 1.0);
+    gl_FragColor = vec4(mainFS(), 0);
 }
 
 #endif
