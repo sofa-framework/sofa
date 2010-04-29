4e7cfce547bd1b8b2f759ec2f619374009fd18cc
Pierre-Jean Bensoussan <pierre-jean.bensoussan@insimo.fr>
2010-04-29 15:51:46 +0000

    r7276/sofa-dev : ADD : core::componentmodel namespace removal scripts.

---
+++ scripts/rem-componentmodel.sh
@@ -0,0 +1,19 @@
+size=0
+
+for g in $(find $1 -name *.h -o -name *.cpp -o -name *.inl) 
+do
+	size=$(($size+1))
+done
+
+#echo "$size"
+
+current=0
+for g in $(find $1 -name *.h -o -name *.cpp -o -name *.inl) 
+do
+	./rem-componentmodel.awk $g > tmp
+	cat tmp > $g
+	current=$(($current+1))
+	#rate=$(echo "$current / $size" | bc -l)
+	rate=$(($current*100 / $size))
+	echo -ne "Progression : $rate%\r"
+done
