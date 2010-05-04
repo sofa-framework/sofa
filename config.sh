0c441a5cd8044d2dbc10ac98c18b2c9b4b83cc43
Frederic Chateau <fchateau@users.gforge.inria.fr>
2010-05-04 07:51:04 +0000

    r7329/sofa-dev : FIX: syntax error in config.sh which was generating warnings. equality comparison operator is '=' in shell scripts, not '=='.

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PLATFORM=$(uname)
-if [ "$PLATFORM" == "Darwin" ]; then
+if [ "$PLATFORM" = "Darwin" ]; then
 PLATFORM=macx
 else
 PLATFORM=linux
@@ -8,6 +8,6 @@ fi
 export SOFA_DIR=$PWD
 export PATH=$SOFA_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$SOFA_DIR/lib/$PLATFORM:$SOFA_DIR/lib/sofa-plugins/:$LD_LIBRARY_PATH
-if [ "$PLATFORM" == "macx" ]; then
+if [ "$PLATFORM" = "macx" ]; then
 export DYLD_LIBRARY_PATH=$SOFA_DIR/lib/$PLATFORM:$SOFA_DIR/lib/sofa-plugins/:$DYLD_LIBRARY_PATH
 fi
