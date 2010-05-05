19cd9a5e4888949b0d7902d7b09d8bfe995a5077
Francois Jourdes <francois.jourdes@insimo.fr>
2010-05-05 07:53:57 +0000

    r7353/sofa-dev : FIX: the script I commited to make the solution/vcproj files target the x64 compilator just do not work. This one should.

--- x64convert.bat
+++ x64convert.bat
@@ -1,7 +1,7 @@
 @echo off
 REM change taget platform from Win32 to x64 in Sofa.sln and related vcproj files.
-@sed
-@if errorlevel 0 goto main
+sed
+if errorlevel 0 goto main
 @echo on
 echo "sed is required to run that script"
 echo "http://gnuwin32.sourceforge.net/packages/sed.htm"
@@ -9,28 +9,25 @@ goto end
 
 :main
 @echo off
-@set BASEDIR=%CD%
-@set SOFASLN=%BASEDIR%\Sofa.sln
-@call :findandreplaceSLN
-@for /F "tokens=3 delims==," %%X in (%SOFASLN%)  do set FILE=%%X& call :findandreplaceVCPROJ 
-@goto end
+set BASEDIR=%CD%
+set SOFASLN=%BASEDIR%\Sofa.sln
+call :findandreplaceSLN
+for /F "tokens=3 delims==," %%X in (%SOFASLN%)  do set FILE=%%X& call :findandreplaceVCPROJ 
+goto end
 
 :findandreplaceSLN
-REM echo %SOFASLN%
-@set TMP=%SOFASLN%.tmp 
-@Copy /y %SOFASLN% %TMP%
-@sed  -e 's/Win32/x64/g' <%TMP%>%SOFASLN%
-@del %TMP%
+@echo %SOFASLN%
+set TMPSLN=%SOFASLN%.tmp 
+Copy /y %SOFASLN% %TMPSLN%
+sed  -e 's/Win32/x64/g' <%TMPSLN%>%SOFASLN%
+del %TMPSLN%
 
 :findandreplaceVCPROJ
-REM set FILE=%FILE: =%
-REM set FILE=%FILE:~1,-1%
-REM set FILE=%BASEDIR%\%FILE%
-REM echo %FILE%
-@set TMP=%FILE%.tmp 
-@Copy /y %FILE% %TMP%
-@sed  -e 's/Name="Win32"/Name="x64"/g' -e 's/"Debug|Win32"/"Debug|x64"/g' -e 's/"Release|Win32"/"Release|x64"/g' <%TMP%>%FILE%
-@del %TMP%
+@echo %FILE%
+set TMP=%FILE%.tmp 
+Copy /y %FILE% %TMP%
+sed  -e 's/Name="Win32"/Name="x64"/g' -e 's/"Debug|Win32"/"Debug|x64"/g' -e 's/"Release|Win32"/"Release|x64"/g' <%TMP%>%FILE%
+del %TMP%
 
 :end
-@cd %CD%
+cd %CD%
