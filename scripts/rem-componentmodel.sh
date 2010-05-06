d74ad5d49e3b7c4723b5b694137f01886ea26bfc
Frederick Roy <frederick.roy@gmail.com>
2010-05-06 13:59:04 +0000

    r7378/sofa-dev : UPDATE: give rights on script + improve error detection

--- scripts/rem-componentmodel.sh
+++ scripts/rem-componentmodel.sh
@@ -5,17 +5,24 @@ do
 	size=$(($size+1))
 done
 
-#echo "$size"
+echo "$size"
 
 current=0
 for g in $(find $1 -name *.h -o -name *.cpp -o -name *.inl) 
 do
-	./rem-componentmodel.awk $g > buff
-	cat buff > $g
-	current=$(($current+1))
-	#rate=$(echo "$current / $size" | bc -l)
-	rate=$(($current*100 / $size))
-	echo -ne "Progression : $rate%\r"
+	#echo "$g"
+	awk -f rem-componentmodel.awk "$g" > buff
+ 
+	if [ -s buff ]
+	then
+		cat buff > "$g"
+		current=$(($current+1))
+		#rate=$(echo "$current / $size" | bc -l)
+		rate=$(($current*100 / $size))
+		echo -ne "Progression : $rate%\r"
+	else
+		echo "Erreur in $g"
+	fi
 done
 
-rm -f buff
+#rm -f buff
