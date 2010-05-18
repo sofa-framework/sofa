45b9fbbd00ef25abd539c593fd708d4e2efad0d2
madurand <madurand@users.gforge.inria.fr>
2010-05-18 15:50:04 +0000

    r7530/sofa-dev : FIX: PartialLinearMovementConstraint compilation.

--- modules/sofa/component/constraint/PartialLinearMovementConstraint.h
+++ modules/sofa/component/constraint/PartialLinearMovementConstraint.h
@@ -64,6 +64,7 @@ public:
     typedef typename DataTypes::VecDeriv VecDeriv;
     typedef typename DataTypes::Coord Coord;
     typedef typename DataTypes::Deriv Deriv;
+    typedef typename DataTypes::SparseVecDeriv SparseVecDeriv;
     typedef typename DataTypes::Real Real;
     typedef topology::PointSubset SetIndex;
     typedef helper::vector<unsigned int> SetIndexArray;
@@ -107,7 +108,11 @@ public :
     /// -- Constraint interface
     void init();
     void reset();
+    template <class DataDeriv>
+    void projectResponseT(DataDeriv& dx);
     void projectResponse(VecDeriv& dx);
+    void projectResponse(SparseVecDeriv& dx);
+
     virtual void projectVelocity(VecDeriv& dx); ///< project dx to constrained space (dx models a velocity)
     virtual void projectPosition(VecCoord& x); ///< project x to constrained space (x models a position)
 
