8c0826c28413ecec199d21861f243f0004e48c88
Erik Pernod <erik.pernod@gmail.com>
2010-05-21 14:56:39 +0000

    r7578/sofa-dev : ADD: option useDataInputs option to TopologicalChangeProcessor. Now, given a time (+ a loop time) this component can remove a set of topological element as input. cf Trash box example: examples/Components/misc/TopologicalChangeProcessor_useDataInputs_option.scn

--- modules/sofa/component/misc/TopologicalChangeProcessor.h
+++ modules/sofa/component/misc/TopologicalChangeProcessor.h
@@ -64,9 +64,20 @@ public:
 
     sofa::core::objectmodel::DataFileName m_filename;
     Data < helper::vector< helper::vector <unsigned int> > > m_listChanges;
+
+    // Parameters for time
     Data < double > m_interval;
     Data < double > m_shift;
     Data < bool > m_loop;
+
+    // Inputs for operations on Data
+    Data <bool> m_useDataInputs;
+    Data <double> m_timeToRemove;
+    Data <sofa::helper::vector <unsigned int> > m_edgesToRemove;
+    Data <sofa::helper::vector <unsigned int> > m_trianglesToRemove;
+    Data <sofa::helper::vector <unsigned int> > m_quadsToRemove;
+    Data <sofa::helper::vector <unsigned int> > m_tetrahedraToRemove;
+    Data <sofa::helper::vector <unsigned int> > m_hexahedraToRemove;
 
 
 protected:
@@ -112,6 +123,7 @@ public:
 
         return BaseObject::canCreate(obj, context, arg);
     }
+
 
 };
 
