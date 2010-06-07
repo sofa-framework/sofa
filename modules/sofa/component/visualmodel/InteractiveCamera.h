d085eb7e02b00df7d4a16832be5d23c93f8300c8
Frederick Roy <frederick.roy@gmail.com>
2010-06-07 11:28:59 +0000

    r7703/sofa-dev : FIX : windows compilation
     //partial sync (1/2 changes)//

--- modules/sofa/component/visualmodel/InteractiveCamera.h
+++ modules/sofa/component/visualmodel/InteractiveCamera.h
@@ -12,7 +12,7 @@ namespace component
 namespace visualmodel
 {
 
-class InteractiveCamera : public BaseCamera
+class SOFA_COMPONENT_VISUALMODEL_API InteractiveCamera : public BaseCamera
 {
 public:
     SOFA_CLASS(InteractiveCamera, BaseCamera);
