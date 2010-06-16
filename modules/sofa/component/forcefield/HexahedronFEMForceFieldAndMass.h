feedd3f3db7378b1d3b0884317fa4ff8fae8eef0
Pierre-Jean Bensoussan <pierre-jean.bensoussan@insimo.fr>
2010-06-16 09:14:39 +0000

    r7777/sofa-dev : FIX : Try to remove WIN32 warnings...
     //partial sync (2/3 changes)//

--- modules/sofa/component/forcefield/HexahedronFEMForceFieldAndMass.h
+++ modules/sofa/component/forcefield/HexahedronFEMForceFieldAndMass.h
@@ -136,6 +136,16 @@ protected :
 
 };
 
+#if defined(WIN32) && !defined(SOFA_COMPONENT_FORCEFIELD_HEXAHEDRONFEMFORCEFIELDANDMASS_CPP)
+#pragma warning(disable : 4231)
+#ifndef SOFA_FLOAT
+extern template class SOFA_COMPONENT_FORCEFIELD_API HexahedronFEMForceFieldAndMass< defaulttype::Vec3dTypes >;
+#endif
+#ifndef SOFA_DOUBLE
+extern template class SOFA_COMPONENT_FORCEFIELD_API HexahedronFEMForceFieldAndMass< defaulttype::Vec3fTypes >;
+#endif
+#endif
+
 } // namespace forcefield
 
 } // namespace component
