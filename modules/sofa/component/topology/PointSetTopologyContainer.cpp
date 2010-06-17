10f0816723135a47b3b0d636a7486cd41ba50928
Erik Pernod <erik.pernod@gmail.com>
2010-06-17 18:32:49 +0000

    r7810/sofa-dev : FIX: clean warnings.

--- modules/sofa/component/topology/PointSetTopologyContainer.cpp
+++ modules/sofa/component/topology/PointSetTopologyContainer.cpp
@@ -46,8 +46,8 @@ int PointSetTopologyContainerClass = core::RegisterObject("Point set topology co
 
 PointSetTopologyContainer::PointSetTopologyContainer(int npoints)
     : nbPoints (initData(&nbPoints, (unsigned int )npoints, "nbPoints", "Number of points"))
-    , d_initPoints (initData(&d_initPoints, "position", "Initial position of points"))
     , m_topologyEngine(NULL)
+    , d_initPoints (initData(&d_initPoints, "position", "Initial position of points"))
 {
     addAlias(&d_initPoints,"points");
 }
