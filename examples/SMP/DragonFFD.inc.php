859c6719833aab0bbe0f5986bda276d3256df7e1
madurand <madurand@users.gforge.inria.fr>
2010-06-25 16:36:33 +0000

    r7893/sofa-dev : CHANGE: use of EulerImplicitSolver and ParallelCGLinearSolver on SMP scenes. Corrected xml syntax.

--- examples/SMP/DragonFFD.inc.php
+++ examples/SMP/DragonFFD.inc.php
@@ -8,27 +8,28 @@ public function DragonFFD(){
 public function printObj(){
 ?>
 	<Node name="Dragon" processor="<?=$this->processor?>">
-                       <Object type="ParallelCGImplicitSolver" iterations="25" threshold="0.000000000001" tolerance="0.000001" rayleighStiffness="0.1" rayleighMass="0" />
+    <EulerImplicitSolver rayleighStiffness="0.1" rayleighMass="0" />
+    <ParallelCGLinearSolver iterations="25" threshold="0.000000000001" tolerance="0.000001" />
 
-		<Object type="MechanicalObject" dx="<?=$this->dx?>" dy="<?=$this->dy?>" dz="<?=$this->dz?>" rx="<?=$this->rx?>" />
-		<Object type="UniformMass" totalmass="10"/>
-		<Object type="RegularGrid"
+		<MechanicalObject dx="<?=$this->dx?>" dy="<?=$this->dy?>" dz="<?=$this->dz?>" rx="<?=$this->rx?>" />
+		<UniformMass totalmass="10"/>
+		<RegularGrid
 			nx="6" ny="5" nz="3"
 			xmin="-11" xmax="11"
 			ymin="-7" ymax="7"
 			zmin="-4" zmax="4"
 			/>
-		<Object type="RegularGridSpringForceField" name="Springs" stiffness="350" damping="1" />
+		<RegularGridSpringForceField name="Springs" stiffness="350" damping="1" />
 		<Node name="Visu">
-			<Object type="OglModel" name="Visual" fileMesh="mesh/dragon.obj"  dx="<?=$this->dx?>" dy="<?=$this->dy?>" dz="<?=$this->dz?>" rx="<?=$this->rx?>"  color="red" />
-			<Object type="BarycentricMapping" object1="../.." object2="Visual" />
+			<OglModel name="Visual" fileMesh="mesh/dragon.obj"  dx="<?=$this->dx?>" dy="<?=$this->dy?>" dz="<?=$this->dz?>" rx="<?=$this->rx?>"  color="red" />
+			<BarycentricMapping object1="../.." object2="Visual" />
 		</Node>
 		<Node name="Surf">
-        		<Object type="MeshLoader" name="meshLoader" filename="mesh/dragon.obj"/>
-			<Object type="Mesh" />
-			<Object type="MechanicalObject"  dx="<?=$this->dx?>" dy="<?=$this->dy?>" dz="<?=$this->dz?>" rx="<?=$this->rx?>" />
-			<Object type="Triangle" />
-			<Object type="BarycentricMapping" />
+   		<MeshLoader name="meshLoader" filename="mesh/dragon.obj"/>
+			<Mesh />
+			<MechanicalObject  dx="<?=$this->dx?>" dy="<?=$this->dy?>" dz="<?=$this->dz?>" rx="<?=$this->rx?>" />
+			<Triangle />
+			<BarycentricMapping />
 		</Node>
 	</Node>
 
--- examples/SMP/TorusFFD.inc.php
+++ examples/SMP/TorusFFD.inc.php
@@ -8,26 +8,27 @@ public function TorusFFD(){
 public function printObj(){
 ?>
 	<Node name="Torus" processor="0">	
-                       <Object type="ParallelCGImplicitSolver" iterations="25" threshold="0.000000000001" tolerance="0.000001" rayleighStiffness="0.1" rayleighMass="0" />
-		<Object type="MechanicalObject" dx="<?=$this->dx?>" dy="<?=$this->dy?>" dz="<?=$this->dz?>" rx="<?=$this->rx?>" rz="<?=$this->rz?>" ry="<?=$this->ry?>" />
-		<Object type="UniformMass" totalmass="10"/>
-		<Object type="RegularGrid"
+    <EulerImplicitSolver rayleighStiffness="0.1" rayleighMass="0" />
+    <ParallelCGLinearSolver iterations="25" threshold="0.000000000001" tolerance="0.000001" />
+		<MechanicalObject dx="<?=$this->dx?>" dy="<?=$this->dy?>" dz="<?=$this->dz?>" rx="<?=$this->rx?>" rz="<?=$this->rz?>" ry="<?=$this->ry?>" />
+		<UniformMass totalmass="10"/>
+		<RegularGrid
 			nx="6" ny="5" nz="2"
 			xmin="-7.5" xmax="7.5"
 			ymin="-6" ymax="6"
 			zmin="-1.75" zmax="1.75"
 			/>
-		<Object type="RegularGridSpringForceField" name="Springs" stiffness="350" damping="1" />
+		<RegularGridSpringForceField name="Springs" stiffness="350" damping="1" />
 		<Node name="Visu">
-			<Object type="OglModel" name="Visual" fileMesh="mesh/torus2_scale3.obj"   dx="<?=$this->dx?>" dy="<?=$this->dy?>" dz="<?=$this->dz?>" rx="<?=$this->rx?>" rz="<?=$this->rz?>" ry="<?=$this->ry?>" color="blue" />
-			<Object type="BarycentricMapping" object1="../.." object2="Visual" />
+			<OglModel name="Visual" fileMesh="mesh/torus2_scale3.obj"   dx="<?=$this->dx?>" dy="<?=$this->dy?>" dz="<?=$this->dz?>" rx="<?=$this->rx?>" rz="<?=$this->rz?>" ry="<?=$this->ry?>" color="blue" />
+			<BarycentricMapping object1="../.." object2="Visual" />
 		</Node>
 		<Node name="Surf">
-        		<Object type="MeshLoader" name="meshLoader" filename="mesh/torus2_scale3.obj"/>
-			<Object type="Mesh" />
-			<Object type="MechanicalObject"  dx="<?=$this->dx?>" dy="<?=$this->dy?>" dz="<?=$this->dz?>" rx="<?=$this->rx?>" rz="<?=$this->rz?>" ry="<?=$this->ry?>" />
-			<Object type="Triangle" />
-			<Object type="BarycentricMapping" />
+     	<MeshLoader name="meshLoader" filename="mesh/torus2_scale3.obj"/>
+			<Mesh />
+			<MechanicalObject  dx="<?=$this->dx?>" dy="<?=$this->dy?>" dz="<?=$this->dz?>" rx="<?=$this->rx?>" rz="<?=$this->rz?>" ry="<?=$this->ry?>" />
+			<Triangle />
+			<BarycentricMapping />
 		</Node>
 	</Node>
 
