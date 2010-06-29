4f8f034c5e65e24aeaa1c35a0b040ece00d0a880
falipou <falipou@users.gforge.inria.fr>
2010-06-29 11:53:42 +0000

    r7915/sofa-dev : FIX: bad init of the tutorial + houseOfCards export the scene to a file, and then load it. Resolve the problem of bad locales... strange

--- applications/tutorials/oneParticule/Main.cpp
+++ applications/tutorials/oneParticule/Main.cpp
@@ -81,7 +81,7 @@ int main(int argc, char** argv)
     particule_node->addObject(mass);
     mass->setMass( 1 );
 
-    sofa::simulation::getSimulation()->init(groot);
+    sofa::simulation::tree::getSimulation()->init(groot);
     groot->setAnimate(false);
 
     //=======================================
