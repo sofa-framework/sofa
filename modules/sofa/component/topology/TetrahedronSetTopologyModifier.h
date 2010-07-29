a8509c18a23519bd7170ca7eb5a39414441a9b41
Erik Pernod <erik.pernod@gmail.com>
2010-07-29 01:01:11 +0000

    r8109/sofa-dev : ADD: PointSetTopologyEngine functions to link this engine to topology Data array. Links are done when creating callback. UPDATE: test component: TopologyChangeTest, to have a PointData<int> which display for each vertex the number of connected triangles. ADD: small example on a triangulation... and it seems to work.
     //partial sync (2/7 changes)//

--- modules/sofa/component/topology/TetrahedronSetTopologyModifier.h
+++ modules/sofa/component/topology/TetrahedronSetTopologyModifier.h
@@ -72,6 +72,9 @@ public:
 
     virtual void reinit();
 
+    /// TODO: temporary duplication of topological events (commented by default)
+    virtual void propagateTopologicalEngineChanges();
+
     /** \brief Sends a message to warn that some tetrahedra were added in this topology.
     *
     * \sa addTetrahedraProcess
