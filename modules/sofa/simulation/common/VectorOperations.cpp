088ca1f27ffdb67b38949a060cf52ab0cda4cb2a
Francois Jourdes <francois.jourdes@insimo.fr>
2010-10-01 08:56:12 +0000

    r8556/sofa-dev : UPDATE: VectorOperations use ExecParams instead of MechanicalParams. Updated MechanicalVAvailVisitor MechanicalVAllocVisitor and MechanicalVFreeVisitor.
     //partial sync (3/4 changes)//

--- modules/sofa/simulation/common/VectorOperations.cpp
+++ modules/sofa/simulation/common/VectorOperations.cpp
@@ -15,9 +15,8 @@ namespace simulation
 namespace common
 {
 
-VectorOperations::VectorOperations(const sofa::core::MechanicalParams *mparams, sofa::core::objectmodel::Context *ctx):
-    sofa::core::behavior::BaseVectorOperations(mparams,ctx),
-    mparams(mparams)
+VectorOperations::VectorOperations(const sofa::core::ExecParams* params, sofa::core::objectmodel::Context *ctx):
+    sofa::core::behavior::BaseVectorOperations(params,ctx)
 {
 }
 
@@ -41,37 +40,42 @@ void VectorOperations::v_alloc(sofa::core::MultiVecCoordId& v)
     /* template < VecType vtype > MechanicalVAvailVisitor;
     /* this can be probably merge in a single operation with the MultiVecId design */
     VecCoordId id(VecCoordId::V_FIRST_DYNAMIC_INDEX);
-    executeVisitor( MechanicalVAvailVisitor(mparams, id) );
+    executeVisitor( MechanicalVAvailVisitor<V_COORD>(params, id) );
     v.assign(id);
-    executeVisitor( MechanicalVAllocVisitor(mparams, v) );
+    executeVisitor( MechanicalVAllocVisitor<V_COORD>(params, v) );
 }
 
 void VectorOperations::v_alloc(sofa::core::MultiVecDerivId& v)
 {
     VecDerivId id(VecDerivId::V_FIRST_DYNAMIC_INDEX);
-    executeVisitor( MechanicalVAvailVisitor(mparams, id) );
+    executeVisitor( MechanicalVAvailVisitor<V_DERIV>(params, id) );
     v.assign(id);
-    executeVisitor(  MechanicalVAllocVisitor(mparams, v) );
+    executeVisitor(  MechanicalVAllocVisitor<V_DERIV>(params, v) );
 }
 
-void VectorOperations::v_free(sofa::core::MultiVecId v)
+void VectorOperations::v_free(sofa::core::MultiVecCoordId& id)
 {
-    executeVisitor( MechanicalVFreeVisitor(mparams, v) );
+    executeVisitor( MechanicalVFreeVisitor<V_COORD>(params, id) );
+}
+
+void VectorOperations::v_free(sofa::core::MultiVecDerivId& id)
+{
+    executeVisitor( MechanicalVFreeVisitor<V_DERIV>(params, id) );
 }
 
 void VectorOperations::v_clear(sofa::core::MultiVecId v) //v=0
 {
-    executeVisitor( MechanicalVOpVisitor(mparams, v) );
+    executeVisitor( MechanicalVOpVisitor(params, v) );
 }
 
 void VectorOperations::v_eq(sofa::core::MultiVecId v, sofa::core::MultiVecId a) // v=a
 {
-    executeVisitor( MechanicalVOpVisitor(mparams, v,a) );
+    executeVisitor( MechanicalVOpVisitor(params,v,a) );
 }
 #ifndef SOFA_SMP
 void VectorOperations::v_peq(sofa::core::MultiVecId v, sofa::core::MultiVecId a, double f)
 {
-    executeVisitor( MechanicalVOpVisitor(mparams ,v,v,a,f), true ); // enable prefetching
+    executeVisitor( MechanicalVOpVisitor(params,v,v,a,f), true ); // enable prefetching
 }
 #else
 void VectorOperations::v_peq(VecId v, VecId a, Shared<double> &fSh,double f)
@@ -95,12 +99,12 @@ void VectorOperations::v_teq(sofa::core::MultiVecId v, double f)
 {
     using namespace sofa::core;
     MultiVecId null( VecId::null() );
-    executeVisitor( MechanicalVOpVisitor(mparams, v, null,v,f) );
+    executeVisitor( MechanicalVOpVisitor(params, v, null,v,f) );
 }
 
 void VectorOperations::v_op(core::MultiVecId v, sofa::core::MultiVecId a, sofa::core::MultiVecId b, double f )
 {
-    executeVisitor( MechanicalVOpVisitor(mparams,v,a,b,f), true ); // enable prefetching
+    executeVisitor( MechanicalVOpVisitor(params,v,a,b,f), true ); // enable prefetching
 }
 
 #ifdef SOFA_SMP
@@ -113,7 +117,7 @@ void VectorOperations::v_op(sofa::core::MultiVecId v, sofa::core::MultiVecId a, 
 void VectorOperations::v_dot( sofa::core::MultiVecId a, sofa::core::MultiVecId b)
 {
     result = 0;
-    MechanicalVDotVisitor(mparams, a,b,&result).setTags(ctx->getTags()).execute( ctx, true ); // enable prefetching
+    MechanicalVDotVisitor(params,a,b,&result).setTags(ctx->getTags()).execute( ctx, true ); // enable prefetching
 
 }
 
