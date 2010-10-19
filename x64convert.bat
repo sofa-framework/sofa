940e54513995699d5440b4703506975960d9ee68
Francois Jourdes <francois.jourdes@insimo.fr>
2010-10-19 15:52:43 +0000

    r8833/sofa-dev : FIX: batch script to make visual studio solution and subprojects target the x64 compiler.

--- x64convert.bat
+++ x64convert.bat
@@ -1,19 +1,17 @@
-@echo off
-REM change taget platform from Win32 to x64 in Sofa.sln and related vcproj files.
-sed
-if errorlevel 0 goto main
 @echo on
-echo "sed is required to run that script"
-echo "http://gnuwin32.sourceforge.net/packages/sed.htm"
-goto end
-
+REM change taget platform from Win32 to x64 in Sofa.sln and related vcproj files.
+REM sed
+REM if errorlevel 0 goto main
+REM @echo on
+REM echo "sed is required to run that script"
+REM echo "http://gnuwin32.sourceforge.net/packages/sed.htm"
+REM goto end
+goto main
 :main
 @echo off
 set BASEDIR=%CD%
 set SOFASLN=%BASEDIR%\Sofa.sln
-call :findandreplaceSLN
-for /F "tokens=3 delims==," %%X in (%SOFASLN%)  do set FILE=%%X& call :findandreplaceVCPROJ 
-goto end
+goto findandreplaceSLN
 
 :findandreplaceSLN
 @echo %SOFASLN%
@@ -21,13 +19,20 @@ set TMPSLN=%SOFASLN%.tmp
 Copy /y %SOFASLN% %TMPSLN%
 sed  -e 's/Win32/x64/g' <%TMPSLN%>%SOFASLN%
 del %TMPSLN%
+goto findandreplaceVCPROJ
 
 :findandreplaceVCPROJ
+for /F "skip=1 tokens=3 delims==, eol=#" %%X in (%SOFASLN%)  do set FILE=%%X& call :replaceVCPROJ 
+goto end
+
+:replaceVCPROJ
 @echo %FILE%
-set TMP=%FILE%.tmp 
+set TMP=%FILE%.tmp
 Copy /y %FILE% %TMP%
 sed  -e 's/Name="Win32"/Name="x64"/g' -e 's/"Debug|Win32"/"Debug|x64"/g' -e 's/"Release|Win32"/"Release|x64"/g' <%TMP%>%FILE%
-del %TMP%
+del %TMP% 
+
 
 :end
 cd %CD%
+
