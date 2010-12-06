20a74baf86cbd4717bb4552ff28c3acf1b1e2e7c
Francois Jourdes <francois.jourdes@insimo.fr>
2010-12-06 15:07:46 +0000

    r9195/sofa-dev : FIX: ObjectCreator static lib for cpp examples. So ChainHybrid cpp tutorial should work as well.

--- applications/tutorials/objectCreator/ObjectCreator.cpp
+++ applications/tutorials/objectCreator/ObjectCreator.cpp
@@ -210,9 +210,9 @@ simulation::Node *ObjectCreator::CreateCollisionNodeVec3(MechanicalObject3* dof,
 
     AddCollisionModels(CollisionNode, elements);
 
-    BarycentricMechanicalMapping3_to_3* mechaMapping = new BarycentricMechanicalMapping3_to_3(dof, dof_surf);
-    mechaMapping->setPathObject1("../..");
-    mechaMapping->setPathObject2("..");
+    BarycentricMapping3_to_3* mechaMapping = new BarycentricMapping3_to_3(dof, dof_surf);
+    mechaMapping->setPathInputObject("@..");
+    mechaMapping->setPathOutPutObject("@.");
     CollisionNode->addObject(mechaMapping);
 
     return CollisionNode;
@@ -224,6 +224,8 @@ simulation::Node *ObjectCreator::CreateVisualNodeVec3(MechanicalObject3* dof,  c
     simulation::Node* VisualNode = simulation::getSimulation()->newNode("Visu");
 
     const std::string nameVisual="Visual";
+    const std::string refVisual = "@" + nameVisual;
+    const std::string refDof = "@.." + dof->getName();
     component::visualmodel::OglModel* visual = new component::visualmodel::OglModel;
     visual->setName(nameVisual);
     visual->setFilename(sofa::helper::system::DataRepository.getFile(filename));
@@ -234,8 +236,8 @@ simulation::Node *ObjectCreator::CreateVisualNodeVec3(MechanicalObject3* dof,  c
 
     BarycentricMapping3_to_Ext3* mapping = new BarycentricMapping3_to_Ext3(dof, visual);
     mapping->setName("Mapping Visual");
-    mapping->setPathObject1("../..");
-    mapping->setPathObject2(nameVisual);
+    mapping->setPathInputObject(refDof);
+    mapping->setPathOutPutObject(refVisual);
     VisualNode->addObject(mapping);
 
     return VisualNode;
@@ -246,6 +248,9 @@ simulation::Node *ObjectCreator::CreateVisualNodeVec3(MechanicalObject3* dof,  c
 simulation::Node *ObjectCreator::CreateCollisionNodeRigid(MechanicalObjectRigid3* dofRigid,  const std::string &filename, const std::vector<std::string> &elements,
         const Deriv3& translation, const Deriv3 &rotation)
 {
+    const std::string refdofRigid = "@../" + dofRigid->getName();
+    const std::string dofSurfName = "CollisionObject";
+    const std::string refdofSurf = "@"+dofSurfName;
     //Node COLLISION
     simulation::Node* CollisionNode = simulation::getSimulation()->newNode("Collision");
 
@@ -257,16 +262,16 @@ simulation::Node *ObjectCreator::CreateCollisionNodeRigid(MechanicalObjectRigid3
     component::topology::MeshTopology* meshTorus_surf= new component::topology::MeshTopology;
     CollisionNode->addObject(meshTorus_surf);
 
-    MechanicalObject3* dof_surf = new MechanicalObject3; dof_surf->setName("Collision Object ");
+    MechanicalObject3* dof_surf = new MechanicalObject3; dof_surf->setName(dofSurfName);
     dof_surf->setTranslation(translation[0],translation[1],translation[2]);
     dof_surf->setRotation(rotation[0],rotation[1],rotation[2]);
     CollisionNode->addObject(dof_surf);
 
     AddCollisionModels(CollisionNode, elements);
 
-    RigidMechanicalMappingRigid3_to_3* mechaMapping = new RigidMechanicalMappingRigid3_to_3(dofRigid, dof_surf);
-    mechaMapping->setPathObject1("../..");
-    mechaMapping->setPathObject2("..");
+    RigidMappingRigid3_to_3* mechaMapping = new RigidMappingRigid3_to_3(dofRigid, dof_surf);
+    mechaMapping->setPathInputObject(refdofRigid);
+    mechaMapping->setPathOutPutObject(refdofSurf);
     CollisionNode->addObject(mechaMapping);
 
     return CollisionNode;
@@ -278,6 +283,8 @@ simulation::Node *ObjectCreator::CreateVisualNodeRigid(MechanicalObjectRigid3* d
     simulation::Node* RigidVisualNode = simulation::getSimulation()->newNode("Visu");
 
     const std::string nameVisual="Visual";
+    const std::string refVisual="@"+nameVisual;
+    const std::string refdofRigid="@../"+dofRigid->getName();
     component::visualmodel::OglModel* visualRigid = new component::visualmodel::OglModel;
     visualRigid->setName(nameVisual);
     visualRigid->setFilename(sofa::helper::system::DataRepository.getFile(filename));
@@ -288,8 +295,8 @@ simulation::Node *ObjectCreator::CreateVisualNodeRigid(MechanicalObjectRigid3* d
 
     RigidMappingRigid3_to_Ext3* mappingRigid = new RigidMappingRigid3_to_Ext3(dofRigid, visualRigid);
     mappingRigid->setName("Mapping Visual");
-    mappingRigid->setPathObject1("../..");
-    mappingRigid->setPathObject2(nameVisual);
+    mappingRigid->setPathInputObject(refdofRigid);
+    mappingRigid->setPathOutPutObject(refVisual);
     RigidVisualNode->addObject(mappingRigid);
     return RigidVisualNode;
 }
