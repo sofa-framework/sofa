fe2ba90bf185dcce8292c3ca3847ac002ba8062c
bousket <bousket@users.gforge.inria.fr>
2010-12-13 16:41:38 +0000

    r9284/sofa-dev : FIX: compil

--- applications/plugins/frame/FrameBlendingMapping.cpp
+++ applications/plugins/frame/FrameBlendingMapping.cpp
@@ -38,6 +38,57 @@ namespace sofa
 namespace defaulttype
 {
 
+/*
+template<class _Real, int Dim>
+inline const Vec<3,_Real>& center(const Vec<Dim,_Real>& c)
+{
+    return DeformationGradientTypes<3, 3, 2, _Real>::center(c);
+}
+
+template<class _Real, int Dim>
+inline Vec<3,_Real>& center(Vec<Dim,_Real>& c)
+{
+    return DeformationGradientTypes<3, 3, 2, _Real>::center(c);
+}
+
+template<class _Real>
+inline const Vec<3,_Real>& center(const Vec<3,_Real>& c)
+{
+    return c;
+}
+
+template<class _Real>
+inline Vec<3,_Real>& center(Vec<3,_Real>& c)
+{
+    return c;
+}
+*/
+
+template<class _Real, int Dim>
+inline Mat<Dim, Dim, _Real> covNN(const Vec<Dim,_Real>& v1, const Vec<Dim,_Real>& v2)
+{
+    Mat<Dim, Dim, _Real> res;
+    for( unsigned int i = 0; i < Dim; ++i)
+        for( unsigned int j = i; j < Dim; ++j)
+        {
+            res[i][j] = v1[i] * v2[j];
+            res[j][i] = res[i][j];
+        }
+    return res;
+}
+
+template<class _Real, int Dim1, int Dim2>
+inline Mat<Dim1, Dim2, _Real> covMN(const Vec<Dim1,_Real>& v1, const Vec<Dim2,_Real>& v2)
+{
+    Mat<Dim1, Dim2, _Real> res;
+    for( unsigned int i = 0; i < Dim1; ++i)
+        for( unsigned int j = i; j < Dim2; ++j)
+        {
+            res[i][j] = v1[i] * v2[j];
+            res[j][i] = res[i][j];
+        }
+    return res;
+}
 template<int N, class R > Mat<N,N,R> identity()
 {
     Mat<N,N,R> m;
@@ -61,10 +112,10 @@ using defaulttype::Vec;
 ////  Specialization on Affine
 //////////////////////////////////////////////////////////////////////////////////
 
-template<class Out, class _Material, int nbRef, int order>
+template<class Out, class _Material, int nbRef>
 struct LinearBlendTypes<
         StdAffineTypes<3,typename _Material::Real>,
-        Out, _Material, nbRef,order
+        Out, _Material, nbRef,0
         >
 {
     typedef _Material Material;
