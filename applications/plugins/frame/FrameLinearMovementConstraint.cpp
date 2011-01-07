149fe4322f6f329fea6288ed7f09100db02f22b8
bousket <bousket@users.gforge.inria.fr>
2011-01-07 19:26:30 +0000

    r9468/sofa-dev : ADD: LinearMovementConstraint for Frames

---
+++ applications/plugins/frame/FrameLinearMovementConstraint.cpp
@@ -0,0 +1,75 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 4      *
+*                (c) 2006-2009 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                              SOFA :: Framework                              *
+*                                                                             *
+* Authors: M. Adam, J. Allard, B. Andre, P-J. Bensoussan, S. Cotin, C. Duriez,*
+* H. Delingette, F. Falipou, F. Faure, S. Fonteneau, L. Heigeas, C. Mendoza,  *
+* M. Nesme, P. Neumann, J-P. de la Plata Alcade, F. Poyer and F. Roy          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#define FRAME_FRAMELINEARMOVEMENTCONSTRAINT_CPP
+
+#include "FrameLinearMovementConstraint.h"
+#include <sofa/component/projectiveconstraintset/LinearMovementConstraint.inl>
+#include <sofa/core/ObjectFactory.h>
+#include <sofa/helper/gl/template.h>
+#include <iostream>
+using std::cerr;
+using std::endl;
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace projectiveconstraintset
+{
+
+SOFA_DECL_CLASS ( FrameLinearMovementConstraint )
+
+using namespace sofa::defaulttype;
+
+int LinearMovementConstraintClass = core::RegisterObject ( "mechanical state vectors" )
+#ifndef SOFA_FLOAT
+        .add< LinearMovementConstraint<Affine3dTypes> >()
+        .add< LinearMovementConstraint<Quadratic3dTypes> >()
+#endif
+#ifndef SOFA_DOUBLE
+        .add< LinearMovementConstraint<Affine3fTypes> >()
+        .add< LinearMovementConstraint<Quadratic3fTypes> >()
+#endif
+        ;
+
+
+
+#ifndef SOFA_FLOAT
+template class SOFA_FRAME_API LinearMovementConstraint<Affine3dTypes>;
+template class SOFA_FRAME_API LinearMovementConstraint<Quadratic3dTypes>;
+#endif
+#ifndef SOFA_DOUBLE
+template class SOFA_FRAME_API LinearMovementConstraint<Affine3fTypes>;
+template class SOFA_FRAME_API LinearMovementConstraint<Quadratic3fTypes>;
+#endif
+} // namespace behavior
+
+} // namespace core
+
+} // namespace sofa
---
+++ applications/plugins/frame/FrameLinearMovementConstraint.h
@@ -0,0 +1,61 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 4      *
+*                (c) 2006-2009 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                              SOFA :: Framework                              *
+*                                                                             *
+* Authors: M. Adam, J. Allard, B. Andre, P-J. Bensoussan, S. Cotin, C. Duriez,*
+* H. Delingette, F. Falipou, F. Faure, S. Fonteneau, L. Heigeas, C. Mendoza,  *
+* M. Nesme, P. Neumann, J-P. de la Plata Alcade, F. Poyer and F. Roy          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef FRAME_FRAMELINEARMOVEMENTCONSTRAINT_H
+#define FRAME_FRAMELINEARMOVEMENTCONSTRAINT_H
+
+#include <sofa/component/projectiveconstraintset/LinearMovementConstraint.h>
+#include "QuadraticTypes.h"
+#include "AffineTypes.h"
+#include "initFrame.h"
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace projectiveconstraintset
+{
+
+#if defined(WIN32) && !defined(FRAME_FRAMELINEARMOVEMENTCONSTRAINT_CPP)
+#ifndef SOFA_FLOAT
+extern template class SOFA_FRAME_API LinearMovementConstraint<defaulttype::Affine3dTypes>;
+extern template class SOFA_FRAME_API LinearMovementConstraint<defaulttype::Quadratic3dTypes>;
+#endif
+#ifndef SOFA_DOUBLE
+extern template class SOFA_FRAME_API LinearMovementConstraint<defaulttype::Affine3fTypes>;
+extern template class SOFA_FRAME_API LinearMovementConstraint<defaulttype::Quadratic3fTypes>;
+#endif
+#endif
+
+} // namespace projectiveconstraintset
+
+} // namespace component
+
+} // namespace sofa
+
+#endif
