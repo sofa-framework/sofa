a277b5b7c3af2864fbbf8dcce31256d028b69cb8
Frederic Chateau <fchateau@users.gforge.inria.fr>
2011-03-08 09:29:07 +0000

    r9920/sofa-dev : BRANCH: creating a new branch to implement and test the new buildsystem for SOFA.

--- scripts/set-headers.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find framework \( -iname '*.h' -o -iname '*.cpp' -o -iname '*.inl' -o -iname '*.c' -o -iname '*.cu' \) -exec scripts/set-header.sh \{\} framework/copyright.txt \;
-find modules \( -iname '*.h' -o -iname '*.cpp' -o -iname '*.inl' -o -iname '*.c' -o -iname '*.cu' \) -exec scripts/set-header.sh \{\} modules/copyright.txt \;
-find applications \( -iname '*.h' -o -iname '*.cpp' -o -iname '*.inl' -o -iname '*.c' -o -iname '*.cu' \) -exec scripts/set-header.sh \{\} applications/copyright.txt \;
