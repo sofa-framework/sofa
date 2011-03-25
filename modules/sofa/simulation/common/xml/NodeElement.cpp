53b85a556b8353b75b42cf1973c5b76f4b260e1d
Francois Jourdes <francois.jourdes@insimo.fr>
2011-03-25 15:32:33 +0000

    r10055/sofa-dev : FIX: warnings.

--- modules/sofa/simulation/common/xml/NodeElement.cpp
+++ modules/sofa/simulation/common/xml/NodeElement.cpp
@@ -37,10 +37,7 @@ namespace xml
 {
 
 using namespace sofa::defaulttype;
-namespace
-{
-int counterDefault = 0;
-}
+
 
 NodeElement::NodeElement(const std::string& name, const std::string& type, BaseElement* parent)
     : Element<core::objectmodel::BaseNode>(name, type, parent)
