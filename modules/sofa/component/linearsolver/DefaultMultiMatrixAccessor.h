2964f5b9caf995798ef971894028633d109c5382
Chi-Thanh Nguyen <ctnguyen@users.gforge.inria.fr>
2011-06-24 11:59:11 +0000

    r10417/sofa-dev : UPDATE : fix scenes and add comments
     //partial sync (3/4 changes)//

--- modules/sofa/component/linearsolver/DefaultMultiMatrixAccessor.h
+++ modules/sofa/component/linearsolver/DefaultMultiMatrixAccessor.h
@@ -47,8 +47,16 @@ using namespace sofa::core;
 using namespace sofa::core::behavior;
 
 
-/* see all scenes in example/SanBox/MatrixContribution*  for diversity of test
+/* DefaultMultiMatrixAccessor is a simplest class managing the global matrix setup.
+ * This class allow only cases where there are several Mechanical State on simulation scene
+ * and interactions between them.
  *
+ * MappedMultiMatrixAccessor is a more powerfull class managing the global system matrix.
+ * This class allow the scene where there are mappings so mapped Mechanical State. It compute
+ * The contribution of stiffness on mapped Mechanical State to the root State related by mapping.
+ *
+ * see Sofa/doc/modules/linearsolver/LinearSolver.tex
+ * see all scenes in example/Component/linearsolver/MatrixContribution*  for tests
  *
  * */
 
@@ -67,23 +75,34 @@ public:
     virtual void setGlobalMatrix(defaulttype::BaseMatrix* matrix);
 
     // When a real MS is visited by the visitor, it must be registed in a local data here (realStateOffsets)
-    // the global size of the system must be ajusted (adding the size of this MS)
+    // the global size of the system must be ajusted.
     virtual void addMechanicalState(const sofa::core::behavior::BaseMechanicalState* mstate);
 
     // When a mapping is visited by the visitor, satisfying that is a mechanical mapping
     // and having implemented getJ, this mapping must be registed in a local data here (mappingList)
     virtual void addMechanicalMapping(sofa::core::BaseMapping* mapping);
 
+    //do nothing for instance
     virtual void addMappedMechanicalState(const sofa::core::behavior::BaseMechanicalState* mstate);
 
+    //Read all Real Mechanical State
     virtual void setupMatrices();
 
+    //give the sum of size of all Real Mechanical State in ordre to set the global matrix dimension
     virtual int getGlobalDimension() const;
+
+    //give position in global matrix of the blog related to a given Mechanical State
     virtual int getGlobalOffset(const sofa::core::behavior::BaseMechanicalState* mstate) const;
 
+    //give the Matrix Reference (Matrix and Offset) related to a given Mechanical State
     virtual MatrixRef getMatrix(const sofa::core::behavior::BaseMechanicalState* mstate) const;
+
+    //give the Matrix Reference (Matrix and Offset) related to a interactionForceField (between 2 Mechanical State)
     virtual InteractionMatrixRef getMatrix(const BaseMechanicalState* mstate1, const BaseMechanicalState* mstate2) const;
 
+    //Compute the global system matrix
+    //If there are no mapping, do nothing
+    //If there are mappings, compute the contribution
     virtual void computeGlobalMatrix();
 
 protected:
@@ -122,9 +141,8 @@ protected:
     std::vector<sofa::core::BaseMapping*> mappingList;
 
 
-
-    //Matrix creating is only call when there are a mapped state,
-    //the stiffness and interaction stiffness of this state could'nt described on the principal matrix
+    //Matrix creating is only call when there are mapped state,
+    //the stiffness and interaction stiffness of this state couldn't directly described on the principal matrix
     //then it demande to create a new matrix
     virtual defaulttype::BaseMatrix* createMatrix(const BaseMechanicalState* mstate) const;
     virtual defaulttype::BaseMatrix* createInteractionMatrix(const BaseMechanicalState* mstate1, const BaseMechanicalState* mstate2) const;
@@ -143,10 +161,14 @@ public:
     MappedMultiMatrixAccessor() : DefaultMultiMatrixAccessor() {}
     ~MappedMultiMatrixAccessor() {	this->clear();}
 
+    //Creating the stiffness matrix for a mapped Mechanical State
     virtual defaulttype::BaseMatrix* createMatrix(const sofa::core::behavior::BaseMechanicalState* mstate) const;
-    virtual defaulttype::BaseMatrix* createInteractionMatrix(const sofa::core::behavior::BaseMechanicalState* mstate1, const sofa::core::behavior::BaseMechanicalState* mstate2) const;
-    virtual void computeGlobalMatrix();
 
+    //Creating the stiffness interaction matrix for pair of Mechanical State when they are not all real state
+    virtual defaulttype::BaseMatrix* createInteractionMatrix(const sofa::core::behavior::BaseMechanicalState* mstate1, const sofa::core::behavior::BaseMechanicalState* mstate2) const;
+
+    //Compute the contribution of all new created matrix to the the global system matrix
+    virtual void computeGlobalMatrix();
 };
 
 } // namespace linearsolver
