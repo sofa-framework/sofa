4f8e8cdf8b8fc9b75887d62433dd8ee40048bd8b
Francois Jourdes <francois.jourdes@insimo.fr>
2011-07-25 12:39:01 +0000

    r10634/sofa-dev : FIX: forgot file (again...)

---
+++ modules/sofa/component/visualmodel/VisualStyle.cpp
@@ -0,0 +1,57 @@
+#include <sofa/component/visualmodel/VisualStyle.h>
+#include <sofa/core/objectmodel/Context.h>
+#include <sofa/core/ObjectFactory.h>
+#include <sofa/simulation/common/UpdateContextVisitor.h>
+
+namespace sofa
+{
+namespace component
+{
+namespace visual
+{
+
+using namespace sofa::core::visual;
+using namespace sofa::core::objectmodel;
+using namespace sofa::simulation;
+
+int VisualStyleClass = core::RegisterObject("Edit the visual style.").add<VisualStyle>();
+
+VisualStyle::VisualStyle()
+    :displayFlags(initData(&displayFlags,"displayFlags","Display Flags"))
+{
+    displayFlags.setWidget("widget_displayFlags");
+    displayFlags.setGroup("Display Flags");
+}
+
+void VisualStyle::fwdDraw(VisualParams* vparams)
+{
+    Node* node = dynamic_cast<Node*>(this->getContext() );
+    backupFlags = node->displayFlags();
+    vparams->displayflags() = displayFlags.getValue(vparams);
+    node->displayFlags() = displayFlags.getValue(vparams);
+    //launch update visual flags visitor to propagate the changes to
+    //the subgraph of this node;
+    UpdateVisualContextVisitor act(vparams);
+    node->executeVisitor(&act);
+
+
+
+}
+
+void VisualStyle::bwdDraw(VisualParams* vparams)
+{
+    Node* node = dynamic_cast<Node*>(this->getContext() );
+    node->displayFlags()    = backupFlags;
+    vparams->displayflags() = backupFlags;
+    //launch update visual flags visitor to propagate the changes to
+    //the subgraph of this node;
+    UpdateVisualContextVisitor act(vparams);
+    node->executeVisitor(&act);
+
+}
+
+
+}
+}
+}
+
