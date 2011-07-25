b1c32a912d6d8a787ac17a0ed911dc6a90b3030f
Francois Jourdes <francois.jourdes@insimo.fr>
2011-07-25 12:45:04 +0000

    r10635/sofa-dev : svn add :\

---
+++ modules/sofa/component/visualmodel/VisualStyle.h
@@ -0,0 +1,39 @@
+#ifndef SOFA_COMPONENT_VISUALMODEL_VISUALSTYLE_H
+#define SOFA_COMPONENT_VISUALMODEL_VISUALSTYLE_H
+
+#include <sofa/core/visual/VisualModel.h>
+#include <sofa/core/visual/DisplayFlags.h>
+
+namespace sofa
+{
+namespace component
+{
+namespace visual
+{
+
+class VisualStyle : public sofa::core::visual::VisualModel
+{
+public:
+    SOFA_CLASS(VisualStyle,sofa::core::visual::VisualModel);
+
+    typedef sofa::core::visual::VisualParams VisualParams;
+    typedef sofa::core::visual::DisplayFlags DisplayFlags;
+    VisualStyle();
+
+    void fwdDraw(VisualParams* );
+    void bwdDraw(VisualParams* );
+
+    Data<DisplayFlags> displayFlags;
+
+protected:
+    DisplayFlags backupFlags;
+};
+
+
+} // visual
+
+} // component
+
+} // sofa
+
+#endif // SOFA_COMPONENT_VISUALMODEL_VISUALSTYLE_H
