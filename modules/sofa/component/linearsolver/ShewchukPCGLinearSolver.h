c2078ce119b55018e1e8581820dd80c5961b70db
Hadrien Courtecuisse <hadrien.courtecuisse@gmail.com>
2011-08-06 18:17:26 +0000

    r10826/sofa-dev : Add option use_first_precond in ShewchukPCGLinearSolver

--- modules/sofa/component/linearsolver/ShewchukPCGLinearSolver.h
+++ modules/sofa/component/linearsolver/ShewchukPCGLinearSolver.h
@@ -59,6 +59,7 @@ public:
     Data<unsigned> f_update_step;
     Data<bool> f_use_precond;
     Data<bool> f_build_precond;
+    Data<bool> f_use_first_precond;
     Data< helper::vector< std::string > > f_preconditioners;
     Data<std::map < std::string, sofa::helper::vector<double> > > f_graph;
     std::vector<sofa::core::behavior::LinearSolver*> preconditioners;
