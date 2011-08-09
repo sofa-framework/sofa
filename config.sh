52dda8a0a89467263eb70d141e5564226b90eb9a
Frederic Chateau <fchateau@users.gforge.inria.fr>
2011-08-09 09:43:04 +0000

    r10836/sofa-dev : FIX: config.sh is now supporting out of source builds: it can be sourced from the build directory and will set the environment correctly to make everything work.

--- config.sh
+++ config.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
+
+####
+# Determine the location of the script.
+# This algorithm also works when sourcing the script
+# or when some directories are symbolic links.
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+pushd . > /dev/null
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+####
+
+
+export SRC_DIR=$SCRIPT_PATH
+export BUILD_DIR=$PWD
+
 PLATFORM=$(uname)
 if [ "$PLATFORM" = "Darwin" ]; then
 PLATFORM=macx
 else
 PLATFORM=linux
 fi
-export SOFA_DIR=$PWD
-export PATH=$SOFA_DIR/bin:$PATH
-export LD_LIBRARY_PATH=$SOFA_DIR/lib:$SOFA_DIR/lib/$PLATFORM:$LD_LIBRARY_PATH
+
+export PATH=$BUILD_DIR/bin:$PATH
+export LD_LIBRARY_PATH=$BUILD_DIR/lib:$BUILD_DIR/lib/$PLATFORM:$LD_LIBRARY_PATH
 if [ "$PLATFORM" = "macx" ]; then
-export DYLD_LIBRARY_PATH=$SOFA_DIR/lib:$SOFA_DIR/lib/$PLATFORM:$DYLD_LIBRARY_PATH
+  export DYLD_LIBRARY_PATH=$BUILD_DIR/lib:$BUILD_DIR/lib/$PLATFORM:$DYLD_LIBRARY_PATH
+fi
+
+if [ "$BUILD_DIR" != "$SRC_DIR" ]; then
+  export SOFA_DATA_PATH=$SRC_DIR/examples:$SRC_DIR/share
 fi
