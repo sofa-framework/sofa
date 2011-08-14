63e226be1764c5a9d1801bdf5cacc8098f75c6bf
Jeremie Allard <jeremie.allard@insimo.fr>
2011-08-14 17:11:08 +0000

    r10871/sofa-dev : ADD: DilateEngine that can translate mesh vertices along their normal with a fixed distance or dynamically computed to thicken part of the mesh lower that a minimum thickness
     //partial sync (5/10 changes)//

---
+++ modules/sofa/component/engine/DilateEngine.cpp
@@ -0,0 +1,65 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 4      *
+*                (c) 2006-2009 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#define SOFA_COMPONENT_ENGINE_DILATEENGINE_CPP
+#include <sofa/component/engine/DilateEngine.inl>
+#include <sofa/core/ObjectFactory.h>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace engine
+{
+
+SOFA_DECL_CLASS(DilateEngine)
+
+int DilateEngineClass = core::RegisterObject("Move mesh vertices along their normal")
+//#ifdef SOFA_FLOAT
+//  .add< DilateEngine<defaulttype::Vec3fTypes> >(true) // default template
+//#else
+        .add< DilateEngine<defaulttype::Vec3dTypes> >(true) // default template
+//#ifndef SOFA_DOUBLE
+//  .add< DilateEngine<defaulttype::Vec3fTypes> >()
+//#endif
+//#endif
+//  .add< DilateEngine<defaulttype::ExtVec3fTypes> >()
+        ;
+
+//#ifndef SOFA_FLOAT
+template class SOFA_COMPONENT_ENGINE_API DilateEngine<defaulttype::Vec3dTypes>;
+//#endif //SOFA_FLOAT
+//#ifndef SOFA_DOUBLE
+//template class SOFA_COMPONENT_ENGINE_API DilateEngine<defaulttype::Vec3fTypes>;
+//#endif //SOFA_DOUBLE
+//template class SOFA_COMPONENT_ENGINE_API DilateEngine<defaulttype::ExtVec3fTypes>;
+
+} // namespace constraint
+
+} // namespace component
+
+} // namespace sofa
+
