9423272eb8015d612a23a943129a421ae43bad1a
Juan Pablo De La Alcade <juanpablo.delaplata@insimo.fr>
2011-08-17 17:06:16 +0000

    r10890/sofa-dev : ADD: Component to load a plugin from the scene file
     //partial sync (2/4 changes)//

---
+++ modules/sofa/component/misc/RequiredPlugin.h
@@ -0,0 +1,87 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 4      *
+*                (c) 2006-2009 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+
+#ifndef REQUIREDPLUGIN_H_
+#define REQUIREDPLUGIN_H_
+
+#include <sofa/core/objectmodel/BaseObject.h>
+#include <sofa/component/component.h>
+#include <sofa/core/objectmodel/DataFileName.h>
+#include <sofa/core/core.h>
+#include <sofa/core/behavior/BaseForceField.h>
+#include <sofa/core/behavior/MechanicalState.h>
+#include <sofa/core/objectmodel/Data.h>
+#include <sofa/core/MechanicalParams.h>
+#include <sofa/defaulttype/BaseVector.h>
+#include <sofa/defaulttype/Vec.h>
+#include <sofa/defaulttype/VecTypes.h>
+#include <sofa/defaulttype/RigidTypes.h>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace misc
+{
+
+class SOFA_COMPONENT_MISC_API RequiredPlugin : public core::objectmodel::BaseObject
+{
+public:
+    SOFA_CLASS(RequiredPlugin,core::objectmodel::BaseObject);
+    sofa::core::objectmodel::DataFileName pluginName;
+    RequiredPlugin();
+    virtual ~RequiredPlugin() {}
+    void loadPlugin();
+
+    /// Pre-construction check method called by ObjectFactory.
+    /// Check that DataTypes matches the MechanicalState.
+    template<class T>
+    static bool canCreate(T*& obj, core::objectmodel::BaseContext* context, core::objectmodel::BaseObjectDescription* arg)
+    {
+        create (obj, context, arg);
+        dynamic_cast<RequiredPlugin*>(obj)->loadPlugin();
+        return BaseObject::canCreate(obj, context, arg);
+    }
+
+    /// Construction method called by ObjectFactory.
+    template<class T>
+    static void create(T*& obj, core::objectmodel::BaseContext* context, core::objectmodel::BaseObjectDescription* arg)
+    {
+        obj = new T;
+        if (context) context->addObject(obj);
+        if (arg) obj->parse(arg);
+    }
+
+};
+
+}
+
+}
+
+}
+
+#endif /* REQUIREDPLUGIN_H_ */
