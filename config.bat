ec95aa3be3d966dd821e4b7d948ae85bd6aa02f5
Francois Jourdes <francois.jourdes@insimo.fr>
2011-09-19 09:16:26 +0000

    r11005/sofa-dev : ADD: windows .bat script to configure %PATH% environment variable and %QMAKESPEC% for msvc2005 and msvc2008

--- config.bat
+++ config.bat
@@ -1,2 +1,32 @@
 @echo off
 set PATH=%CD%/lib;%PATH%
+
+@if not "%QMAKEPATH%" == ""  goto params
+set QMAKEPATH=%CD%\tools\qt4win
+set QTDIR=%CD%\tools\qt4win
+set PATH=%QMAKEPATH%\bin;%PATH%
+
+:params
+if "%1" == "VC7" goto vc7
+if "%1" == "VC8" goto vc8
+if "%1" == "VC9" goto vc9
+
+:vc7
+set QMAKESPEC=win32-msvc.net
+goto common
+
+:vc8
+set QMAKESPEC=win32-msvc2005
+goto common
+
+:vc9
+set QMAKESPEC=win32-msvc2008
+goto common
+
+:common
+@if "%ERRORLEVEL%"=="0" goto end
+echo ERROR %ERRORLEVEL%
+pause
+@goto end
+
+:end
---
+++ config_VC8.bat
@@ -0,0 +1,1 @@
+config VC8 
