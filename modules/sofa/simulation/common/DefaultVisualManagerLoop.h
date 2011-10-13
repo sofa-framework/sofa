2c2c97ae65f37b0ed60eed76f26af4f1cc7e5fcd
Pierre-Jean Bensoussan <pierre-jean.bensoussan@insimo.fr>
2011-10-13 15:15:28 +0000

    r11228/sofa-dev : FIX : SPtr creation.
    Former-commit-id: 338e0c24e3f4a4826b5809425fc93eb5b67860d0

--- modules/sofa/simulation/common/DefaultVisualManagerLoop.h
+++ modules/sofa/simulation/common/DefaultVisualManagerLoop.h
@@ -79,7 +79,7 @@ public:
     static typename T::SPtr create(T*, BaseContext* context, BaseObjectDescription* arg)
     {
         simulation::Node* gnode = dynamic_cast<simulation::Node*>(context);
-        typename T::SPtr obj = new T(gnode);
+        typename T::SPtr obj = sofa::core::objectmodel::New<T>(gnode);
         if (context) context->addObject(obj);
         if (arg) obj->parse(arg);
         return obj;
