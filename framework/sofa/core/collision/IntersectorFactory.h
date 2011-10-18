6f66ff18a15eedf4b3078872ac25f465d7588033
Juan Pablo De La Alcade <juanpablo.delaplata@insimo.fr>
2011-10-18 07:51:44 +0000

    r11283/sofa-dev : ADD: missing factory to add intersectors
    Former-commit-id: 282aadae2646cd90229ae6a45406e82e5f40d334

---
+++ framework/sofa/core/collision/IntersectorFactory.h
@@ -0,0 +1,115 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 4      *
+*                (c) 2006-2009 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                              SOFA :: Framework                              *
+*                                                                             *
+* Authors: M. Adam, J. Allard, B. Andre, P-J. Bensoussan, S. Cotin, C. Duriez,*
+* H. Delingette, F. Falipou, F. Faure, S. Fonteneau, L. Heigeas, C. Mendoza,  *
+* M. Nesme, P. Neumann, J-P. de la Plata Alcade, F. Poyer and F. Roy          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef SOFA_CORE_COLLISION_INTERSECTORFACTORY_H
+#define SOFA_CORE_COLLISION_INTERSECTORFACTORY_H
+
+#include <sofa/core/CollisionModel.h>
+#include <sofa/core/collision/DetectionOutput.h>
+#include <sofa/helper/FnDispatcher.h>
+
+namespace sofa
+{
+
+namespace core
+{
+
+namespace collision
+{
+
+template<class TIntersectionClass>
+class BaseIntersectorCreator
+{
+public:
+    virtual ~BaseIntersectorCreator() {}
+
+    virtual void addIntersectors(TIntersectionClass* object) = 0;
+
+    virtual std::string name() const = 0;
+};
+
+template<class TIntersectionClass>
+class IntersectorFactory
+{
+protected:
+    typedef BaseIntersectorCreator<TIntersectionClass> Creator;
+    typedef std::vector<Creator*> CreatorVector;
+    CreatorVector creatorVector;
+
+public:
+
+    bool registerCreator(Creator* creator)
+    {
+        this->creatorVector.push_back(creator);
+        return true;
+    }
+
+    virtual void addIntersectors(TIntersectionClass* object)
+    {
+        typename CreatorVector::iterator it = creatorVector.begin();
+        typename CreatorVector::iterator end = creatorVector.end();
+        while (it != end)
+        {
+            BaseIntersectorCreator<TIntersectionClass>* creator = (*it);
+            creator->addIntersectors(object);
+            ++it;
+        }
+    }
+
+    static IntersectorFactory<TIntersectionClass>* getInstance()
+    {
+        static IntersectorFactory<TIntersectionClass> instance;
+        return &instance;
+    }
+};
+
+template<class TIntersectionClass, class TIntersectorClass>
+class IntersectorCreator : public BaseIntersectorCreator<TIntersectionClass>
+{
+public:
+    IntersectorCreator(std::string name) : m_name(name)
+    {
+        IntersectorFactory<TIntersectionClass>::getInstance()->registerCreator(this);
+    }
+    virtual ~IntersectorCreator() {}
+
+    virtual void addIntersectors(TIntersectionClass* object)
+    {
+        new TIntersectorClass(object);
+    }
+
+    virtual std::string name() const { return m_name; }
+protected:
+    std::string m_name;
+};
+
+} // namespace collision
+
+} // namespace core
+
+} // namespace sofa
+
+#endif
