89cec7b2300b26670d578b5128ab926822c55a03
Erik Pernod <erik.pernod@gmail.com>
2011-10-20 09:15:34 +0000

    r11344/sofa-dev : UPDATE: topo work, saving laptop to desk.
     //partial sync (4/5 changes)//
    
    
    Former-commit-id: 036993e70bb3b7f1621b73a79ad61b985e9e740c

---
+++ modules/sofa/component/topology/TopologyEngine.h
@@ -0,0 +1,235 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 4      *
+*                (c) 2006-2009 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+
+#ifndef SOFA_COMPONENT_TOPOLOGY_TOPOLOGYENGINE_H
+#define SOFA_COMPONENT_TOPOLOGY_TOPOLOGYENGINE_H
+
+#include <sofa/core/topology/BaseTopology.h>
+#include <sofa/defaulttype/VecTypes.h>
+#include <sofa/helper/list.h>
+#include <sofa/component/component.h>
+
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace topology
+{
+
+// Define topology elements
+using core::topology::BaseMeshTopology;
+typedef BaseMeshTopology::Point Point;
+typedef BaseMeshTopology::Edge Edge;
+typedef BaseMeshTopology::Triangle Triangle;
+typedef BaseMeshTopology::Quad Quad;
+typedef BaseMeshTopology::Tetrahedron Tetrahedron;
+typedef BaseMeshTopology::Hexahedron Hexahedron;
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////   Generic Topology Data Implementation   /////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template< class TopologyElementType, class VecT>
+class TopologyEngineImpl : public sofa::core::topology::TopologyEngine
+{
+public:
+    typedef VecT container_type;
+    typedef typename container_type::value_type value_type;
+    typedef sofa::core::topology::BaseTopologyData<VecT> t_topologicalData;
+
+    //TopologyEngineImpl();
+
+    //TopologyEngineImpl(t_topologicalData* _topologicalData);
+
+    TopologyEngineImpl(t_topologicalData* _topologicalData,
+            sofa::core::topology::BaseMeshTopology* _topology,
+            sofa::core::topology::TopologyHandler* _topoHandler);
+
+    void init();
+
+    void reinit();
+
+    void update();
+
+    void ApplyTopologyChanges();
+
+    void registerTopology(sofa::core::topology::BaseMeshTopology* _topology);
+
+    void registerTopology();
+
+    void registerTopologicalData(t_topologicalData *topologicalData) {m_topologicalData = topologicalData;}
+
+
+    /// Function to link DataEngine with Data array from topology
+    void linkToPointDataArray();
+    void linkToEdgeDataArray();
+    void linkToTriangleDataArray();
+    void linkToQuadDataArray();
+    void linkToTetrahedronDataArray();
+    void linkToHexahedronDataArray();
+
+protected:
+    t_topologicalData* m_topologicalData;
+    sofa::core::topology::TopologyContainer* m_topology;
+    sofa::core::topology::TopologyHandler* m_topoHandler;
+
+public:
+    bool m_pointsLinked;
+    bool m_edgesLinked;
+    bool m_trianglesLinked;
+    bool m_quadsLinked;
+    bool m_tetrahedraLinked;
+    bool m_hexahedraLinked;
+
+};
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////   Point Topology Data Implementation   /////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template< class VecT >
+class PointEngineImpl : public TopologyEngineImpl<Point, VecT>
+{
+public:
+    typedef typename TopologyEngineImpl<Point, VecT>::container_type container_type;
+    typedef typename TopologyEngineImpl<Point, VecT>::value_type value_type;
+
+
+    //protected:
+    //    PointSetTopologyEngine<VecT>* m_topologicalEngine;
+
+};
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////   Edge Topology Data Implementation   /////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template< class VecT >
+class EdgeEngineImpl : public TopologyEngineImpl<Edge, VecT>
+{
+public:
+    typedef typename TopologyEngineImpl<Edge, VecT>::container_type container_type;
+    typedef typename TopologyEngineImpl<Edge, VecT>::value_type value_type;
+
+    //protected:
+    //    EdgeSetTopologyEngine<VecT>* m_topologicalEngine;
+
+};
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////   Triangle Topology Data Implementation   ///////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template< class VecT >
+class TriangleEngineImpl : public TopologyEngineImpl<Triangle, VecT>
+{
+public:
+    typedef typename TopologyEngineImpl<Triangle, VecT>::container_type container_type;
+    typedef typename TopologyEngineImpl<Triangle, VecT>::value_type value_type;
+
+    //protected:
+    //    TriangleSetTopologyEngine<VecT>* m_topologicalEngine;
+
+};
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////   Quad Topology Data Implementation   /////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template< class VecT >
+class QuadEngineImpl : public TopologyEngineImpl<Quad, VecT>
+{
+public:
+    typedef typename TopologyEngineImpl<Quad, VecT>::container_type container_type;
+    typedef typename TopologyEngineImpl<Quad, VecT>::value_type value_type;
+
+    //protected:
+    //    QuadSetTopologyEngine<VecT>* m_topologicalEngine;
+
+};
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////   Tetrahedron Topology Data Implementation   /////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template< class VecT >
+class TetrahedronEngineImpl : public TopologyEngineImpl<Tetrahedron, VecT>
+{
+public:
+    typedef typename TopologyEngineImpl<Tetrahedron, VecT>::container_type container_type;
+    typedef typename TopologyEngineImpl<Tetrahedron, VecT>::value_type value_type;
+
+    //protected:
+    //    TetrahedronSetTopologyEngine<VecT>* m_topologicalEngine;
+
+};
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////   Hexahedron Topology Data Implementation   /////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template< class VecT >
+class HexahedronEngineImpl : public TopologyEngineImpl<Hexahedron, VecT>
+{
+public:
+    typedef typename TopologyEngineImpl<Hexahedron, VecT>::container_type container_type;
+    typedef typename TopologyEngineImpl<Hexahedron, VecT>::value_type value_type;
+
+    //protected:
+    //    HexahedronSetTopologyEngine<VecT>* m_topologicalEngine;
+
+};
+
+
+
+
+
+} // namespace topology
+
+} // namespace component
+
+} // namespace sofa
+
+
+#endif // SOFA_COMPONENT_TOPOLOGY_TOPOLOGYENGINE_H
