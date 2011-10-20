726c96224c4b7c3e04cef24c908bbf08ee8abac3
Erik Pernod <erik.pernod@gmail.com>
2011-10-20 09:15:34 +0000

    r11344/sofa-dev : UPDATE: topo work, saving laptop to desk.
     //partial sync (4/5 changes)//
    
    
    Former-commit-id: 036993e70bb3b7f1621b73a79ad61b985e9e740c

---
+++ modules/sofa/component/topology/TopologySubsetDataHandler.h
@@ -0,0 +1,138 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 4      *
+*                (c) 2006-2009 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef SOFA_COMPONENT_TOPOLOGY_TOPOLOGYSPARSEDATAHANDLER_H
+#define SOFA_COMPONENT_TOPOLOGY_TOPOLOGYSPARSEDATAHANDLER_H
+
+#include <sofa/core/topology/TopologyElementHandler.h>
+#include <sofa/core/topology/BaseTopologyData.h>
+
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace topology
+{
+
+// Define topology elements
+using namespace sofa::core::topology;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////   Generic Topology Data Implementation   /////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/** \brief A class for storing Edge related data. Automatically manages topology changes.
+*
+* This class is a wrapper of class helper::vector that is made to take care transparently of all topology changes that might
+* happen (non exhaustive list: Edges added, removed, fused, renumbered).
+*/
+
+template< class TopologyElementType, class VecT>
+class TopologyDataHandler : public sofa::core::topology::TopologyElementHandler< TopologyElementType >
+{
+public:
+    typedef VecT container_type;
+    typedef typename container_type::value_type value_type;
+
+    /// size_type
+    typedef typename container_type::size_type size_type;
+    /// reference to a value (read-write)
+    typedef typename container_type::reference reference;
+    /// const reference to a value (read only)
+    typedef typename container_type::const_reference const_reference;
+    /// const iterator
+    typedef typename container_type::const_iterator const_iterator;
+
+
+
+public:
+    // constructor
+    TopologyDataHandler(): sofa::core::topology::TopologyElementHandler < TopologyElementType >()
+        , m_topologyData(NULL) {}
+
+    bool isTopologyDataRegistered() {return m_topologyData;}
+
+    /** Public fonction to apply creation and destruction functions */
+    /// Apply removing current elementType elements
+    virtual void applyDestroyFunction(unsigned int, value_type& t) {t = VecT();}
+    /// Apply adding current elementType elements
+    virtual void applyCreateFunction(unsigned int, value_type&,
+            const sofa::helper::vector< unsigned int > &,
+            const sofa::helper::vector< double > &) {}
+
+    /// WARNING NEEED TO UNIFY THIS
+    /// Apply adding current elementType elements
+    virtual void applyCreateFunction(unsigned int, value_type&, const TopologyElementType& ,
+            const sofa::helper::vector< unsigned int > &,
+            const sofa::helper::vector< double > &) {}
+
+
+protected:
+    /// Swaps values at indices i1 and i2.
+    virtual void swap( unsigned int i1, unsigned int i2 );
+
+    /// Add some values. Values are added at the end of the vector.
+    virtual void add( unsigned int nbElements,
+            const sofa::helper::vector< TopologyElementType >& elems,
+            const sofa::helper::vector< sofa::helper::vector< unsigned int > > &ancestors,
+            const sofa::helper::vector< sofa::helper::vector< double > >& coefs);
+
+    virtual void add( unsigned int nbElements,
+            const sofa::helper::vector< sofa::helper::vector< unsigned int > > &ancestors,
+            const sofa::helper::vector< sofa::helper::vector< double > >& coefs);
+
+    /// Remove the values corresponding to the Edges removed.
+    virtual void remove( const sofa::helper::vector<unsigned int> &index );
+
+    /// Reorder the values.
+    virtual void renumber( const sofa::helper::vector<unsigned int> &index );
+
+    /// Move a list of points
+    virtual void move( const sofa::helper::vector<unsigned int> &indexList,
+            const sofa::helper::vector< sofa::helper::vector< unsigned int > >& ancestors,
+            const sofa::helper::vector< sofa::helper::vector< double > >& coefs);
+
+    /// Add Element after a displacement of vertices, ie. add element based on previous position topology revision.
+    virtual void addOnMovedPosition(const sofa::helper::vector<unsigned int> &indexList,
+            const sofa::helper::vector< TopologyElementType > & elems);
+
+    /// Remove Element after a displacement of vertices, ie. add element based on previous position topology revision.
+    virtual void removeOnMovedPosition(const sofa::helper::vector<unsigned int> &indices);
+
+protected:
+    BaseTopologyData <VecT>* m_topologyData;
+};
+
+
+} // namespace topology
+
+} // namespace component
+
+} // namespace sofa
+
+
+#endif SOFA_COMPONENT_TOPOLOGY_TOPOLOGYSPARSEDATAHANDLER_H
