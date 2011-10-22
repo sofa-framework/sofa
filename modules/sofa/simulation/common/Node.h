9b4eb69da016a2476e02251ae642d1b1dcb5c339
Jeremie Allard <jeremie.allard@insimo.fr>
2011-10-22 09:25:05 +0000

    r11388/sofa-dev : Missing include in Node.h
    Former-commit-id: 37a091872cbfe620fb519f689b4f4cfd37222bbf

--- modules/sofa/simulation/common/Node.h
+++ modules/sofa/simulation/common/Node.h
@@ -61,6 +61,7 @@
 #include <sofa/core/behavior/OdeSolver.h>
 #include <sofa/core/behavior/ConstraintSolver.h>
 #include <sofa/core/behavior/BaseAnimationLoop.h>
+#include <sofa/core/visual/VisualLoop.h>
 #include <sofa/core/collision/Pipeline.h>
 #include <sofa/core/loader/BaseLoader.h>
 #include <sofa/core/objectmodel/Event.h>
