5a92da9bb3d5ed5b030e7f7edda74b29044b7cf2
Jeremie Allard <jeremie.allard@insimo.fr>
2011-11-08 16:16:05 +0000

    r11575/sofa-dev : FIX: a few issues in svn-split scripts. Now I am stuck because the sofa-dev SVN repository is corrupted at r3525...
     //partial sync (1/5 changes)//
    
    
    Former-commit-id: a2b88084ec914c7f74c1be00cfeeefdf83e01d05

--- scripts/svn-setprops.sh
+++ scripts/svn-setprops.sh
@@ -4,7 +4,7 @@
 
 echo dos2unix
 
-find $* '(' -iname '*.c' -o -iname '*.cpp' -o -iname '*.cu' -o -iname '*.cxx'  -o -iname '*.h' -o -iname '*.hpp' -o -iname '*.inl' -o -iname '*.cuh' -o -iname '*.txt' -o -iname '*.pro' -o -iname '*.pri' -o -iname '*.prf' -o -iname '*.cfg' -o -iname '*.scn' -o -iname '*.xml' -o -iname '*.pscn' -o -iname '*.html' -o -iname '*.htm' -o -iname '*.php' ')' -exec /bin/bash -c 'file -m /dev/null "{}" | grep -q "CRLF, LF line terminators$" && echo dos2unix "{}" && dos2unix "{}"' ';'
+find $* '(' -iname '*.c' -o -iname '*.cpp' -o -iname '*.cu' -o -iname '*.cxx'  -o -iname '*.h' -o -iname '*.hpp' -o -iname '*.inl' -o -iname '*.cuh' -o -iname '*.txt' -o -iname '*.pro' -o -iname '*.pri' -o -iname '*.prf' -o -iname '*.cfg' -o -iname '*.scn' -o -iname '*.xml' -o -iname '*.pscn' -o -iname '*.html' -o -iname '*.htm' -o -iname '*.php' ')' -exec /bin/bash -c 'file -m /dev/null "{}" | grep -q "CRLF, \(LF|CR\) line terminators$" && echo dos2unix "{}" && dos2unix "{}"' ';'
 
 echo set svn:eol-style
 
