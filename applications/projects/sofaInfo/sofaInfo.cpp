837cd7cb9fd35ca3a7b8912f21ba0d8d24b08acd
Juan Pablo De La Alcade <juanpablo.delaplata@insimo.fr>
2012-01-06 14:08:03 +0000

    r11768/sofa-dev : ADD: project sofaInfo to list the components and libaries used by a scene
     //partial sync (2/3 changes)//
    
    
    Former-commit-id: 0e301ca26aac016116361d2865e540d262e3f932

---
+++ applications/projects/sofaInfo/sofaInfo.cpp
@@ -0,0 +1,103 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU General Public License as published by the Free  *
+* Software Foundation; either version 2 of the License, or (at your option)   *
+* any later version.                                                          *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    *
+* more details.                                                               *
+*                                                                             *
+* You should have received a copy of the GNU General Public License along     *
+* with this program; if not, write to the Free Software Foundation, Inc., 51  *
+* Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.                   *
+*******************************************************************************
+*                            SOFA :: Applications                             *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#include <sofa/simulation/common/xml/initXml.h>
+#include <sofa/simulation/tree/TreeSimulation.h>
+#include <sofa/component/init.h>
+#ifdef SOFA_DEV
+#include <sofa/component/initDev.h>
+#endif
+#include <sofa/core/ObjectFactory.h>
+
+// ---------------------------------------------------------------------
+// ---
+// ---------------------------------------------------------------------
+int main(int /*argc*/, char** argv)
+{
+    if (argv[1] == NULL)
+    {
+        std::cout << "Usage: sofaInfo FILE" << std::endl;
+        return -1;
+    }
+
+    sofa::simulation::setSimulation(new sofa::simulation::tree::TreeSimulation());
+
+    sofa::component::init();
+#ifdef SOFA_DEV
+    sofa::component::initDev();
+#endif
+    sofa::simulation::xml::initXml();
+
+    sofa::simulation::Node::SPtr groot = sofa::core::objectmodel::SPtr_dynamic_cast<sofa::simulation::Node>( sofa::simulation::getSimulation()->load(argv[1]));
+
+    if (groot==NULL)
+    {
+        groot = sofa::simulation::getSimulation()->createNewGraph("");
+    }
+
+    sofa::helper::vector<sofa::core::objectmodel::Base*> objects;
+    std::set<std::string> classNames;
+    std::set<std::string> targets;
+
+    groot->getTreeObjects<sofa::core::objectmodel::Base>(&objects);
+
+    // get the classes and targets of the scene
+    for (unsigned int i=0; i<objects.size(); i++)
+    {
+        sofa::core::ObjectFactory::ClassEntry* entry = sofa::core::ObjectFactory::getInstance()->getEntry(objects[i]->getClassName());
+        if (entry != NULL && ! entry->creatorList.empty())
+        {
+            classNames.insert(entry->className);
+
+            sofa::core::ObjectFactory::CreatorMap::iterator it = entry->creatorMap.find(objects[i]->getTemplateName());
+            if (it != entry->creatorMap.end() && *it->second->getTarget())
+            {
+                targets.insert(it->second->getTarget());
+            }
+        }
+    }
+
+    std::set<std::string>::const_iterator it = classNames.begin();
+    std::set<std::string>::const_iterator end = classNames.end();
+    std::cout << "=== CLASSES ===" << std::endl;
+    while (it != end)
+    {
+        std::cout << (*it) << std::endl;
+        it++;
+    }
+
+    it = targets.begin();
+    end = targets.end();
+    std::cout << std::endl << "=== TARGETS ===" << std::endl;
+    while (it != end)
+    {
+        std::cout << (*it) << std::endl;
+        it++;
+    }
+
+    if (groot!=NULL)
+        sofa::simulation::getSimulation()->unload(groot);
+
+    return 0;
+}
