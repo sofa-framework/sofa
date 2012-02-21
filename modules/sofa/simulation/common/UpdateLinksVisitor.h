f46660a3fd6cb501280e5c126be02fcb5d9afc92
Christophe Guebert <christophe.guebert@digital-trainers.com>
2012-02-21 08:21:08 +0000

    r11969/sofa-dev : WARNING : UpdateLinks is called after all objects have been created and before the Init.
    Former-commit-id: dccc45c92a25c5be0aad39ced9cd5b90a1d8add3

---
+++ modules/sofa/simulation/common/UpdateLinksVisitor.h
@@ -0,0 +1,58 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef SOFA_SIMULATION_UPDATELINKSACTION_H
+#define SOFA_SIMULATION_UPDATELINKSACTION_H
+
+#include <sofa/simulation/common/Visitor.h>
+#include <sofa/core/objectmodel/BaseObject.h>
+#include <string>
+#include <sofa/core/ExecParams.h>
+
+namespace sofa
+{
+
+namespace simulation
+{
+
+class SOFA_SIMULATION_COMMON_API UpdateLinksVisitor : public Visitor
+{
+public:
+    UpdateLinksVisitor(const core::ExecParams* params) : Visitor(params) {}
+
+    void processObject(core::objectmodel::BaseObject* obj);
+
+    virtual Result processNodeTopDown(simulation::Node* node);
+    virtual void processNodeBottomUp(simulation::Node* node);
+
+    /// Specify whether this action can be parallelized.
+    virtual bool isThreadSafe() const { return true; }
+    virtual const char* getClassName() const { return "UpdateLinksVisitor"; }
+};
+
+} // namespace simulation
+
+} // namespace sofa
+
+#endif
