37b2830753bb7cd3e415da2bc520e92d3256bac5
Francois Faure <francois.faure@inria.fr>
2012-03-13 01:02:28 +0000

    r12040/sofa-dev : ongoing work
    Former-commit-id: 6b4ca81bd763bea009ef092efd1a20b2492b21c1

--- applications/plugins/Compliant/ComplianceSolver.cpp
+++ applications/plugins/Compliant/ComplianceSolver.cpp
@@ -18,11 +18,9 @@ using namespace sofa::defaulttype;
 using namespace sofa::helper;
 using namespace core::behavior;
 
-int ComplianceSolverClass = core::RegisterObject("A simple explicit time integrator")
-        .add< ComplianceSolver >()
-        ;
-
 SOFA_DECL_CLASS(ComplianceSolver);
+int ComplianceSolverClass = core::RegisterObject("A simple explicit time integrator").add< ComplianceSolver >();
+
 
 
 ComplianceSolver::ComplianceSolver()
@@ -31,37 +29,66 @@ ComplianceSolver::ComplianceSolver()
 
 void ComplianceSolver::solve(const core::ExecParams* params, double dt, sofa::core::MultiVecCoordId xResult, sofa::core::MultiVecDerivId vResult)
 {
-//    cerr<<"ComplianceSolver::solve" << endl;
-    ComputeMatrixSizesVisitor computeSizes(params);
-    this->getContext()->executeVisitor(&computeSizes);
-    cerr<<"ComplianceSolver::solve, sizeM = " << computeSizes.sizeM <<", sizeJ = "<< computeSizes.sizeJ << endl;
+    //    cerr<<"ComplianceSolver::solve" << endl;
+
+    MatrixAssemblyVisitor assembly(params,this);
+    this->getContext()->executeVisitor(&assembly);
+    cerr<<"ComplianceSolver::solve, sizeM = " << assembly.sizeM <<", sizeC = "<< assembly.sizeC << endl;
+
+    matM.resize(assembly.sizeM,assembly.sizeM);
+    matC.resize(assembly.sizeC,assembly.sizeC);
+    matJ.resize(assembly.sizeC,assembly.sizeM);
+
+    assembly.pass++; // second pass
+    this->getContext()->executeVisitor(&assembly);
+
+
+
 }
 
-void ComplianceSolver::ComputeMatrixSizesVisitor::processNodeBottomUp(simulation::Node* node)
+simulation::Visitor::Result ComplianceSolver::MatrixAssemblyVisitor::processNodeTopDown(simulation::Node* node)
 {
-    // ==== process compliances
-    vector<BaseCompliance::SPtr> compliances;
-    node->getNodeObjects<BaseCompliance>(&compliances);
-//    cerr<<"node " << node->getName() << ", compliances: " << endl;
-//    for(unsigned i=0; i<compliances.size(); i++ ){
-//        cerr<< compliances[i]->getName() <<", ";
-//    }
-//    cerr<<endl;
-    if( compliances.size()>0 )
+    if( pass==1 )
     {
-        assert(node->mechanicalState);
-        sizeJ += node->mechanicalState->getMatrixSize();
-    }
-    assert(compliances.size()<2);
+        // ==== independent DOFs
+        if (node->mechanicalState != NULL  && node->mechanicalMapping == NULL )
+        {
+            //        cerr<<"node " << node->getName() << ", independent mechanical state: " << node->mechanicalState->getName() << endl;
+            m_offset[node->mechanicalState] = sizeM;
+            sizeM += node->mechanicalState->getMatrixSize();
+        }
 
-    // ==== process independent DOFs
-    if (node->mechanicalState != NULL  && node->mechanicalMapping == NULL )
-    {
-//        cerr<<"node " << node->getName() << ", independent mechanical state: " << node->mechanicalState->getName() << endl;
-        sizeM += node->mechanicalState->getMatrixSize();
+
+        // ==== process compliances
+        vector<BaseCompliance*> compliances;
+        node->getNodeObjects<BaseCompliance>(&compliances);
+        //    cerr<<"node " << node->getName() << ", compliances: " << endl;
+        //    for(unsigned i=0; i<compliances.size(); i++ ){
+        //        cerr<< compliances[i]->getName() <<", ";
+        //    }
+        //    cerr<<endl;
+        if( compliances.size()>0 )
+        {
+            assert(node->mechanicalState);
+            c_offset[compliances[0]] = sizeC;
+            sizeC += node->mechanicalState->getMatrixSize();
+        }
+        assert(compliances.size()<2);
+
     }
+    else if (pass==2)
+    {
+
+    }
+    else
+    {
+        cerr<<"ComplianceSolver::ComputeMatrixSizesVisitor::processNodeTopDown, unknown pass " << pass << endl;
+    }
+
+    return RESULT_CONTINUE;
 
 }
+
 
 
 }
--- applications/plugins/Compliant/ComplianceSolver.h
+++ applications/plugins/Compliant/ComplianceSolver.h
@@ -1,6 +1,7 @@
 #ifndef SOFA_COMPONENT_ODESOLVER_ComplianceSolver_H
 #define SOFA_COMPONENT_ODESOLVER_ComplianceSolver_H
 #include "initCompliant.h"
+#include "BaseCompliance.h"
 #include <sofa/core/behavior/OdeSolver.h>
 #include <sofa/simulation/common/Visitor.h>
 #include "../ModelHierarchies/EigenSparseSquareMatrix.h"
@@ -8,6 +9,7 @@
 
 namespace sofa
 {
+using helper::vector;
 
 namespace component
 {
@@ -34,20 +36,39 @@ protected:
     ComplianceSolver();
 
     typedef Eigen::DynamicSparseMatrix<SReal> Matrix;
+    typedef core::behavior::BaseMechanicalState MechanicalState;
+    typedef core::behavior::BaseCompliance Compliance;
+    typedef core::BaseMapping Mapping;
 
     Matrix matM;      ///< the mass matrix
     Matrix matJ;      ///< the concatenation of the constraint Jacobians
     Matrix matC;      ///< the compliance matrix used to regularize the system
 
-    struct ComputeMatrixSizesVisitor: public simulation::Visitor
+//    vector<MechanicalState::SPtr> states;  ///< The independent states
+//    vector<Compliance::SPtr> compliances;  ///< All the compliances
+
+    /** Visitor used to perform the assembly of M, C, and J.
+      Proceeds in several passes:<ol>
+      <li> the first pass counts the size of the matrices </li>
+      <li> the matrices are then created </li>
+      <li> the second pass performs the assembly by summing matrix products </li>
+      </ol>
+      */
+    struct MatrixAssemblyVisitor: public simulation::Visitor
     {
-        ComputeMatrixSizesVisitor(const core::ExecParams* params): simulation::Visitor(params), sizeM(0), sizeJ(0) {}
-
+        ComplianceSolver* solver;
         unsigned sizeM; ///< size of the mass matrix
-        unsigned sizeJ; ///< number of rows of J (=number of scalar constraints). The number of columns is the same as M.
+        unsigned sizeC; ///< size of the compliance matrix, number of scalar constraints
 
-        virtual void processNodeBottomUp(simulation::Node* node);
+        MatrixAssemblyVisitor(const core::ExecParams* params, ComplianceSolver* s): simulation::Visitor(params), solver(s), sizeM(0), sizeC(0), pass(1) {}
+        virtual Visitor::Result processNodeTopDown(simulation::Node* node);
+//        virtual void processNodeBottomUp(simulation::Node* node);
+
+        unsigned pass;  ///< Counter to represent the current pass
+        std::map<MechanicalState*, unsigned> m_offset;  ///< Start index of independent DOFs in the mass matrix
+        std::map<Compliance*, unsigned>      c_offset;  ///< Start index of compliances in the compliance matrix
     };
+
 
 };
 
