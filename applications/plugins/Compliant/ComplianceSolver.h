4ce50fac0126a7dd65f5b6ad3e83ba1ba9d6c278
Francois Faure <francois.faure@inria.fr>
2012-03-19 19:20:10 +0000

    r12083/sofa-dev : plugin documentation
     //partial sync (5/6 changes)//
    
    
    Former-commit-id: 0f3b8745f5afd677d818981a69bdbd990f03f491

--- applications/plugins/Compliant/ComplianceSolver.h
+++ applications/plugins/Compliant/ComplianceSolver.h
@@ -24,21 +24,34 @@ namespace odesolver
 
   Inspired from Servin,Lacoursière,Melin, Interactive Simulation of Elastic Deformable Materials,  http://www.ep.liu.se/ecp/019/005/ecp01905.pdf
 
-  Generalized to a tunable implicit integration scheme. The equation is:
-  \f$ \left( \begin{array}{cc} M & -J^T \\ LJ & \frac{1}{l} C \end{arrax}\right)
-      \left( \begin{array}{c} \delta v & \bar \lambda \end{array}\right)
-    = \left( \begin{array}{c} f & - \frac{1}{l} (\phi +(d+\alpha h) \dot \phi)  \end{array}\right) \f$
-    where \f$ M \f$ the mass matrix, \f$ \phi \f$ is the constraint violation, \f$ J \f$ the constraint Jacobian matrix,
-    \f$ C \f$ is the compliance matrix (i.e. inverse of constraint stiffness), \f$ l=\alpha(h \beta + d) \f$ is a term related to implicit integration and constraint damping,
-     \f$ \alpha \f$  and \f$ \beta \f$  define the integration scheme for a time step \f$ h \f$:
+  We generalize it to a tunable implicit integration scheme:
+      \f[ \begin{array}{ccc}
+    \Delta v &=& h.M^{-1}.(\alpha f_{n+1} + (1-\alpha) f_n)  \\
+    \Delta x &=& h.(\beta v_{n+1} + (1-\beta) v_n)
+    \end{array} \f]
+    where \f$ h \f$ is the time step, \f$ \alpha \f$ is the implicit velocity factor, and \f$ \beta \f$ is the implicit position factor.
 
-      \f$ \delta v = h.M^{-1}.(\alpha f_{n+1} + (1-\alpha) f_n)  \f$,   where \f$ \alpha \f$ is the implicit velocity factor.
+    The corresponding dynamic equation is:
+  \f[ \left( \begin{array}{cc} M & -J^T \\
+                               J & \frac{1}{l} C \end{array}\right)
+      \left( \begin{array}{c} \delta v \\ \bar\lambda \end{array}\right)
+    = \left( \begin{array}{c} f \\ - \frac{1}{l} (\phi +(d+\alpha h) \dot \phi)  \end{array}\right) \f]
+    where \f$ M \f$ is the mass matrix, \f$ \phi \f$ is the constraint violation, \f$ J \f$ the constraint Jacobian matrix,
+    \f$ C \f$ is the compliance matrix (i.e. inverse of constraint stiffness), \f$ l=\alpha(h \beta + d) \f$ is a term related to implicit integration and constraint damping, and
+      \f$ \bar\lambda \f$ is the average constraint forces, consistently with the implicit velocity integration.
 
-      \f$ \delta x = h.(\beta v_{n+1} + (1-\beta) v_n)  \f$,   where \f$ \beta \f$ is the implicit position factor.
+  A Shur complement is used to compute the constraint forces, then these are added to the external forces to obtain the final velocity increment,
+  and the positions are updated according to the implicit scheme:
 
-      \f$ \lambda \f$ is the average constraint force, consistently with to the implicit velocity integration.
+  \f[ \begin{array}{ccc}
+   ( JPM^{-1}PJ^T + \frac{1}{l}C ) \bar\lambda &=& \frac{-1}{l} (\phi + (d+h\alpha)\dot\phi ) - J M^{-1} f \\
+                                 \Delta v  &=&  P M^{-1}( f + J^T \bar\lambda ) \\
+                                 \Delta x  &=&  h( v + \beta \Delta v )
+  \end{array} \f]
+where \f$ P \f$ is the projection matrix corresponding to the projective constraints applied to the independent DOFs.
 
-  The equation is solved using a Shur complement: \f$ ( JM^{-1}J^T + C ) \f$
+\sa \ref sofa::core::behavior::BaseCompliance
+
 
 
 */
@@ -69,9 +82,11 @@ protected:
     VectorSofa vecF;      ///< top of the right-hand term: forces
     VectorSofa vecPhi;    ///< bottom of the right-hand term: constraint corrections
 
+public:
     Data<SReal>  implicitVelocity; ///< the \f$ \alpha \f$ parameter of the integration scheme
     Data<SReal>  implicitPosition; ///< the \f$ \beta  \f$ parameter of the integration scheme
     Data<bool>   verbose;        ///< print a lot of debug info
+protected:
 
 
 
