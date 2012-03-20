00cb38ee4c515383d2d79493ff96448d86f707e4
Jeremie Allard <jeremie.allard@insimo.fr>
2012-03-20 14:38:29 +0000

    r12099/sofa-dev : FIX: missing dependency to CGAL in CGALPlugin-dependencies.prf...
    Former-commit-id: 28b8907cd63a5a62a15c0dd4325bc4510d4cd4fe

--- Project MacOS.sh
+++ Project MacOS.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # generate a XCODE Project
-qmake -recursive -spec macx-xcode Sofa.pro
+#qmake -recursive -spec macx-xcode Sofa.pro
 
 # Generate a Makefile
 #qmake -recursive -spec macx-g++ Makefile Sofa.pro
