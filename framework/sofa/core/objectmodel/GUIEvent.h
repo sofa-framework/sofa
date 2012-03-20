5df722d2a9833b289980a1e97201eeceb2116693
Bruno Carrez <bruno.carrez@inria.fr>
2012-03-20 23:04:00 +0000

    r12116/sofa-dev : ADD sofa::core::objectmodel::GUIEvent
    Former-commit-id: 7d51ccf935fafe1de21802deecd20968d9fffee6

---
+++ framework/sofa/core/objectmodel/GUIEvent.h
@@ -0,0 +1,90 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                              SOFA :: Framework                              *
+*                                                                             *
+* Authors: The SOFA Team (see Authors.txt)                                    *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef SOFA_CORE_OBJECTMODEL_GUIEVENT_H
+#define SOFA_CORE_OBJECTMODEL_GUIEVENT_H
+
+#include <sofa/core/objectmodel/Event.h>
+#include <string>
+
+
+namespace sofa
+{
+
+namespace core
+{
+
+namespace objectmodel
+{
+
+/**
+ * @brief This event notifies about GUI interaction.
+ */
+class SOFA_CORE_API GUIEvent : public sofa::core::objectmodel::Event
+{
+public:
+
+
+    /**
+     * @brief Constructor.
+     */
+    GUIEvent(const std::string controlID, const std::string valueName, const std::string value);
+
+    /**
+     * @brief Destructor.
+     */
+    virtual ~GUIEvent();
+
+    /**
+     * @brief Get the emitter control ID
+     */
+    const std::string getControlID(void) const {return m_controlID;};
+
+    /**
+     * @brief Get the value name
+     */
+    const std::string getValueName(void) const {return m_valueName;};
+
+    /**
+     * @brief Get the value
+     */
+    const std::string getValue(void) const {return m_value;};
+
+
+    virtual const char* getClassName() const { return "GUIEvent"; }
+private:
+
+    std::string     m_controlID;
+    std::string     m_valueName;
+    std::string     m_value;
+
+};
+
+} // namespace objectmodel
+
+} // namespace core
+
+} // namespace sofa
+
+#endif // SOFA_CORE_OBJECTMODEL_GUIEVENT_H
