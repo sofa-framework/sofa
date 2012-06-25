130d4dda9ef72d015feba4e0346e317cf36bb936
Jeremie Allard <jeremie.allard@insimo.fr>
2012-06-25 22:48:54 +0000

    r12505/sofa-dev : Windows: use an installed version of Qt if Sofa/tools/qt4win does not exist. We should now provide small dependency packages without Qt and rely on up-to-date installable pre-compiled versions of Qt (at least for 32-bit mode).
    Former-commit-id: 22bc8ded9092f5a213cb090a2d5516b8d167f1db

--- project.bat
+++ project.bat
@@ -1,36 +1,43 @@
 @echo off
 REM This batch permit to create all dsp project or vcproj project for
-REM Visual C++ 2003 or 2005
-REM use : project [VC7 / VC8 / vc9 / clean]
+REM Visual C++ 2005, 2008, 2010
+REM use : project [VC8 / VC9 / VC10 / clean]
 REM default visual project depends on the environment variable QMAKESPEC 
 
 set TARGET_MACHINE=x86
 @if not "%QMAKEPATH%" == ""  goto params
+if exist %CD%\tools\qt4win\bin\qmake.exe goto qtlocal
+if "%QTDIR%" == "" goto noqt
+
+set PATH=%QTDIR%\bin;%PATH%
+qmake --version
+goto params
+
+:noqt
+echo No Qt found. Either install a pre-compiled Qt version and set QTDIR variable, or uncompress a Sofa dependency package providing Qt
+exit 1
+
+:qtlocal
 set QMAKEPATH=%CD%\tools\qt4win
 set QTDIR=%CD%\tools\qt4win
 set PATH=%QMAKEPATH%\bin;%PATH%
 
 :params
-if "%1" == "VC7" goto vc7
 if "%1" == "VC8" goto vc8
 if "%1" == "VC9" goto vc9
 if "%1" == "VC10" goto vc10
 if "%1" == "VC9_X64" goto vc9_x64
 if "%1" == "VC10_X64" goto vc10_x64
 if "%1" == "clean" goto clean
+if "%QMAKESPEC%" == "win32-msvc2005" goto vc8
+if "%QMAKESPEC%" == "win32-msvc2008" goto vc9
+if "%QMAKESPEC%" == "win32-msvc2010" goto vc10
 
 :console
 @echo on
 @echo Making Makefiles
 qmake -recursive
 @goto end
-
-:vc7
-set QMAKESPEC=win32-msvc.net
-@echo on
-@echo Making Visual project 7
-qmake -tp vc -recursive -o Sofa QT_INSTALL_PREFIX="%QTDIR%"
-goto common
 
 :vc8
 set QMAKESPEC=win32-msvc2005
@@ -43,6 +50,7 @@ goto common
 set QMAKESPEC=win32-msvc2008
 @echo on
 @echo Making Visual project 9
+call "%VS90COMNTOOLS%..\..\VC\vcvarsall.bat" x86
 qmake -tp vc -recursive -o Sofa.sln Sofa.pro QT_INSTALL_PREFIX="%QTDIR%"
 goto common
 
@@ -50,6 +58,7 @@ goto common
 set QMAKESPEC=win32-msvc2010
 @echo on
 @echo Making Visual project 10
+call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
 qmake -tp vc -recursive -o Sofa.sln Sofa.pro QT_INSTALL_PREFIX="%QTDIR%"
 goto common
 
