6f41b80277b6f6a90dda8f221bb2238ba6757b72
Bruno Carrez <bruno.carrez@inria.fr>
2012-06-27 14:05:28 +0000

    r12521/sofa-dev : SofaPython plugin: ADD Python visitors
    Former-commit-id: 4bfedacfb434c86f409ff36974bc54822785c95c

---
+++ applications/plugins/SofaPython/examples/Visitor.py
@@ -0,0 +1,139 @@
+import Sofa
+
+############################################################################################
+# this is a PythonScriptController example script
+############################################################################################
+
+
+
+############################################################################################
+# following defs are used later in the script
+############################################################################################
+
+
+def testNodes(node):
+ node.findData('name').value = 'god'
+
+ # Node creation
+ adam = node.createChild('Adam')
+ eve = node.createChild('Eve')
+ abel = eve.createChild('Abel')
+
+ # Node manipulation
+ eve.removeChild(abel)
+ adam.addChild(abel)
+ cleese.moveChild(gilliam)
+ 
+ #you can animate simulation directly by uncommenting the following line:
+ #node.animate=truee
+    
+ return 0
+
+############################################################################################
+# this is my parent visitor class
+############################################################################################
+
+class SofaVisitor(object):
+ def __init__(self,name):
+  print 'SofaVisitor constructor name='+name
+  self.name = name
+  
+ def processNodeTopDown(self,node):
+  print 'SofaVisitor "'+self.name+'" processNodeTopDown node='+node.findData('name').value
+  return True
+ 
+ def processNodeBottomUp(self,node):
+  print 'SofaVisitor "'+self.name+'" processNodeBottomUp node='+node.findData('name').value
+ 
+  
+
+
+############################################################################################
+# following defs are optionnal entry points, called by the PythonScriptController component;
+############################################################################################
+
+
+
+# called once the script is loaded
+def onLoaded(node):
+ print 'Controller script loaded from node %s'+node.findData('name').value
+ return 0
+
+# optionnally, script can create a graph...
+def createGraph(node):
+ print 'createGraph called (python side)'
+ 
+ #uncomment to create nodes
+ testNodes(node)
+
+    
+ return 0
+
+
+
+# called once graph is created, to init some stuff...
+def initGraph(node):
+ print 'initGraph called (python side)'
+ 
+ v = SofaVisitor('testVisitor')
+ node.executeVisitor(v)
+ 
+ return 0
+
+
+
+# called on each animation step
+def onBeginAnimationStep(dt):
+ return 0
+ 
+def onEndAnimationStep(dt):
+ return 0
+ 
+# called when necessary by Sofa framework... 
+def storeResetState():
+ print 'storeResetState called (python side)'
+ return 0
+
+def reset():
+ print 'reset called (python side)'
+ return 0
+
+def cleanup():
+ print 'cleanup called (python side)'
+ return 0
+ 
+ 
+# called when a GUIEvent is received
+def onGUIEvent(controlID,valueName,value):
+ print 'GUIEvent received: controldID='+controlID+' valueName='+valueName+' value='+value
+ return 0 
+ 
+ 
+ 
+# key and mouse events; use this to add some user interaction to your scripts 
+def onKeyPressed(k):
+ print 'onKeyPressed '+k
+ return 0 
+ 
+def onKeyReleased(k):
+ print 'onKeyReleased '+k
+ return 0 
+
+def onMouseButtonLeft(x,y,pressed):
+ print 'onMouseButtonLeft x='+str(x)+' y='+str(y)+' pressed='+str(pressed)
+ return 0
+ 
+def onMouseButtonRight(x,y,pressed):
+ print 'onMouseButtonRight x='+str(x)+' y='+str(y)+' pressed='+str(pressed)
+ return 0
+ 
+def onMouseButtonMiddle(x,y,pressed):
+ print 'onMouseButtonMiddle x='+str(x)+' y='+str(y)+' pressed='+str(pressed)
+ return 0
+ 
+def onMouseWheel(x,y,delta):
+ print 'onMouseButtonWheel x='+str(x)+' y='+str(y)+' delta='+str(delta)
+ return 0
+ 
+ 
+ 
