56f997c5050462ad21edc42df6eef0330faa5afc
Jerome Esnault <Jerome.Esnault@inria.fr>
2012-08-28 08:07:35 +0000

    r12702/sofa-dev : cancel commit : modification needed for OSC Sender in VCoRE
    Former-commit-id: fea0ed14230ca3e43c088f5771ae051b4da66d9e

--- framework/sofa/core/visual/VisualParams.h
+++ framework/sofa/core/visual/VisualParams.h
@@ -49,8 +49,7 @@ enum
     API_OpenGL = 0,
     API_OGRE = 1,
     API_OpenSceneGraph = 2,
-    API_OpenSG = 3,
-    API_OSC = 4
+    API_OpenSG = 3
 };
 
 /// Class gathering parameters used by visual components and by the draw method of each component. Transmitted by visitors
