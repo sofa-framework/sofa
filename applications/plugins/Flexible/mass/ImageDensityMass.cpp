f49c641cde3ca54e1724de6238961a4c87a49e31
Matthieu Nesme <matthieu.nesme@inria.fr>
2012-10-24 14:53:55 +0000

    r8798/sofa : Flexible: ADD ImageDensityMass = on-going work about precomputing mass at free dofs based on an integration over a density map. Todo: implementation for Affine dof (for now it is only implemented for particles, so FEM) Warning: in order to precompute the mass at the free dof, the mapping between the free dofs and the voxels is hard-coded as a linear interpolation. It should be great to find a way to precompute the mass from a SOFA graph that would be used only during init and forgotten during anim. This mechanism would allow to precompute the mass at free dofs for any mass defined at mapped dofs.
    Former-commit-id: 28e764882f44851dc59a6d73c4258dca3611c616

---
+++ applications/plugins/Flexible/mass/ImageDensityMass.cpp
@@ -0,0 +1,49 @@
+#define SOFA_FLEXIBLE_IMAGEDENSITYMASS_CPP
+
+#include "ImageDensityMass.inl"
+#include <sofa/core/ObjectFactory.h>
+#include <sofa/defaulttype/Mat.h>
+#include <sofa/defaulttype/Vec3Types.h>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace mass
+{
+
+using namespace sofa::defaulttype;
+
+
+
+SOFA_DECL_CLASS(ImageDensityMass)
+
+
+// Register in the Factory
+int ImageDensityMassClass = core::RegisterObject("Define a specific mass for each dof")
+#ifndef SOFA_FLOAT
+        .add< ImageDensityMass<Vec3dTypes,core::behavior::ShapeFunction3d,Mat3x3d> >( true )
+        .add< ImageDensityMass<Vec3dTypes,core::behavior::ShapeFunction2d,Mat3x3d> >()
+#endif
+#ifndef SOFA_DOUBLE
+        .add< ImageDensityMass<Vec3fTypes,core::behavior::ShapeFunction3f,Mat3x3f> >()
+        .add< ImageDensityMass<Vec3fTypes,core::behavior::ShapeFunction2f,Mat3x3f> >()
+#endif
+        ;
+#ifndef SOFA_FLOAT
+template class ImageDensityMass<Vec3dTypes,core::behavior::ShapeFunction3d,Mat3x3d>;
+template class ImageDensityMass<Vec3dTypes,core::behavior::ShapeFunction2d,Mat3x3d>;
+#endif
+#ifndef SOFA_DOUBLE
+template class ImageDensityMass<Vec3fTypes,core::behavior::ShapeFunction3f,Mat3x3f>;
+template class ImageDensityMass<Vec3fTypes,core::behavior::ShapeFunction2f,Mat3x3f>;
+#endif
+
+} // namespace mass
+
+} // namespace component
+
+} // namespace sofa
+
