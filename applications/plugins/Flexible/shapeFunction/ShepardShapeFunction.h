76fe9325cdaf9aead66ce0dceab75e4fd9944685
Julien Bosman <julien.bosman@inria.fr>
2012-11-08 15:36:12 +0000

    r8822/sofa : ShapeFunction modified for Cuda GPU implementation
    Former-commit-id: 622124275c249905402344a47bde74705b3358e1

--- applications/plugins/Flexible/shapeFunction/ShepardShapeFunction.h
+++ applications/plugins/Flexible/shapeFunction/ShepardShapeFunction.h
@@ -85,7 +85,7 @@ public:
 		Inherit::computeShapeFunction(childPosition, M, ref, w, dw, ddw);
 	}
 
-    void computeShapeFunction(const Coord& childPosition, MaterialToSpatial& M, VRef& ref, VReal& w, VGradient* dw=NULL,VHessian* ddw=NULL)
+	virtual void computeShapeFunction(const Coord& childPosition, MaterialToSpatial& M, VRef& ref, VReal& w, VGradient* dw=NULL,VHessian* ddw=NULL)
     {
 		helper::ReadAccessor<Data<VCoord > > parent(this->f_position);
         unsigned int nbp=parent.size(),nbRef=this->f_nbRef.getValue();
