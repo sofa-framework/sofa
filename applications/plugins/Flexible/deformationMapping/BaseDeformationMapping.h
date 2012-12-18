0d4fed001fb9be747e679a12c3d40e0606ecc352
Olivier Carre <olivier.carre@inria.fr>
2012-12-18 14:27:33 +0000

    r8936/sofa : FIX: FLEXIBLE - frames are now correctly displayed on Windows, also fixed some warnings
    Former-commit-id: d31510a37b9ce84b9fa3f474c35afb30a03969e9

--- applications/plugins/Flexible/deformationMapping/BaseDeformationMapping.h
+++ applications/plugins/Flexible/deformationMapping/BaseDeformationMapping.h
@@ -198,11 +198,11 @@ public:
     virtual void init();
     virtual void reinit();
 
-    virtual void apply(const core::MechanicalParams */*mparams*/ , Data<OutVecCoord>& dOut, const Data<InVecCoord>& dIn);
-    virtual void applyJ(const core::MechanicalParams */*mparams*/ , Data<OutVecDeriv>& dOut, const Data<InVecDeriv>& dIn);
-    virtual void applyJT(const core::MechanicalParams */*mparams*/ , Data<InVecDeriv>& dIn, const Data<OutVecDeriv>& dOut);
+    virtual void apply(const core::MechanicalParams * /*mparams*/ , Data<OutVecCoord>& dOut, const Data<InVecCoord>& dIn);
+    virtual void applyJ(const core::MechanicalParams * /*mparams*/ , Data<OutVecDeriv>& dOut, const Data<InVecDeriv>& dIn);
+    virtual void applyJT(const core::MechanicalParams * /*mparams*/ , Data<InVecDeriv>& dIn, const Data<OutVecDeriv>& dOut);
     virtual void applyDJT(const core::MechanicalParams* mparams, core::MultiVecDerivId parentDfId, core::ConstMultiVecDerivId );
-    virtual void applyJT(const core::ConstraintParams */*cparams*/ , Data<InMatrixDeriv>& /*out*/, const Data<OutMatrixDeriv>& /*in*/);
+    virtual void applyJT(const core::ConstraintParams * /*cparams*/ , Data<InMatrixDeriv>& /*out*/, const Data<OutMatrixDeriv>& /*in*/);
 
     const defaulttype::BaseMatrix* getJ(const core::MechanicalParams */*mparams*/)
     {
