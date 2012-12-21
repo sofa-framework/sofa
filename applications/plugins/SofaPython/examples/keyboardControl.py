c7a2c7d01eacf5adffea35ab81b4e70cbda497e4
Bruno Carrez <bruno.carrez@inria.fr>
2012-12-21 15:03:32 +0000

    r8953/sofa : SofaPython: fixed the keyboardControl.scn example
    Former-commit-id: 9172dff4c4b09eb391006432f99d2ac6273c9622

--- applications/plugins/SofaPython/examples/keyboardControl.py
+++ applications/plugins/SofaPython/examples/keyboardControl.py
@@ -12,31 +12,31 @@ class KeyboardControl(Sofa.PythonScriptController):
 	def onKeyPressed(self,k):
 		
 		# free_position is a scalar array : [tx,ty,tz,rx,ry,rz,rw]
-		free_position=self.MechanicalState.findData('free_position').value
-		
+		free_position=self.MechanicalState.free_position
+
 		# translation speed
 		speed = 1 
 
 		# UP key : front
 		if ord(k)==19:
-			free_position[2]-=speed
+			free_position[0][2]-=speed
 		# DOWN key : rear
 		if ord(k)==21:
-			free_position[2]+=speed
+			free_position[0][2]+=speed
 		# LEFT key : left
 		if ord(k)==18:
-			free_position[1]-=speed
+			free_position[0][1]-=speed
 		# RIGHT key : right
 		if ord(k)==20:
-			free_position[1]+=speed
+			free_position[0][1]+=speed
 		# PAGEUP key : up
 		if ord(k)==22:
-			free_position[0]-=speed
+			free_position[0][0]-=speed
 		# PAGEDN key : down
 		if ord(k)==23:
-			free_position[0]+=speed
+			free_position[0][0]+=speed
 			
-		self.MechanicalState.findData('free_position').value=free_position
+		self.MechanicalState.free_position=free_position
 		return 0 
 	 
 	 
