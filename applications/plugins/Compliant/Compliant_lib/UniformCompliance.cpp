7e9f85b483a8309b4d519c04c3ce10e57cb315c5
Maxime Tournier <mtournie@users.gforge.inria.fr>
2013-01-14 15:29:28 +0000

    r8968/sofa : Compliant: instanciated UniformCompliance on vec3 types
    Former-commit-id: d74a06f4e00ebde1bf9a4d2dc881fb760dfce86f

--- applications/plugins/Compliant/Compliant_lib/UniformCompliance.cpp
+++ applications/plugins/Compliant/Compliant_lib/UniformCompliance.cpp
@@ -15,10 +15,12 @@ using namespace sofa::defaulttype;
 int UniformComplianceClass = core::RegisterObject("Uniform compliance")
 #ifndef SOFA_FLOAT
         .add< UniformCompliance< Vec1dTypes > >(true)
+	.add< UniformCompliance< Vec3dTypes > >(true)
         .add< UniformCompliance< Vec6dTypes > >(true)
 #endif
 #ifndef SOFA_DOUBLE
         .add< UniformCompliance< Vec1fTypes > >(true)
+	.add< UniformCompliance< Vec3fTypes > >(true)
         .add< UniformCompliance< Vec6fTypes > >(true)
 #endif
         ;
@@ -27,9 +29,11 @@ SOFA_DECL_CLASS(UniformCompliance)
 
 
 template class SOFA_Compliant_API UniformCompliance<Vec1dTypes>;
+template class SOFA_Compliant_API UniformCompliance<Vec3dTypes>;
 template class SOFA_Compliant_API UniformCompliance<Vec6dTypes>;
 
 template class SOFA_Compliant_API UniformCompliance<Vec1fTypes>;
+template class SOFA_Compliant_API UniformCompliance<Vec3fTypes>;
 template class SOFA_Compliant_API UniformCompliance<Vec6fTypes>;
 
 
