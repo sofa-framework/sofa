7e58ebbd50ac8318c1ff5a0a95c9fc4ed0fc0121
Benjamin GILLES <benjamin.gilles@inria.fr>
2013-03-18 17:42:26 +0000

    r9128/sofa : update Flexible: examples of anisotropic material  with von mises stress visualization
    Former-commit-id: ec54d123634d1ef297e59945f70970e004931c06

---
+++ applications/plugins/Flexible/examples/VonMisesStressTri.py
@@ -0,0 +1,19 @@
+import Sofa
+
+class Visualizer(Sofa.PythonScriptController):
+
+	def createGraph(self,node):
+		self.strain=node.getParents()[0].getObject('E')			
+		self.gpSampler=node.getParents()[0].getParents()[0].getObject('sampler')			
+		self.disp = node.createObject('DataDisplay')
+		return 0
+
+	def onEndAnimationStep(self,dt):
+		S = self.strain.findData('force').value
+		vol= self.gpSampler.findData('volume').value
+		L = len(S)*[None]
+		for index, item in enumerate(S):
+        		L[index]= (max(0,item[0]*item[0] + item[1]*item[1] - item[0]*item[1] + 3.0*item[2]*item[2]))**(0.5) / vol[index][0]
+		self.disp.findData('cellData').value= str(L).replace('[', '').replace("]", '').replace(",", ' ')
+		return 0
+
