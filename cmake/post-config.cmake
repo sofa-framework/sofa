f319f007aa99ec57a3ba3b3a6d606ea9b1b1081c
Olivier Carre <olivier.carre@inria.fr>
2013-03-22 17:05:11 +0000

    r9150/sofa : UPDATE: cmake config files
    Former-commit-id: 465182139e8af5b19cf3232cd17724870a9ec9ac

--- cmake/post-config.cmake
+++ cmake/post-config.cmake
@@ -2,5 +2,5 @@ cmake_minimum_required(VERSION 2.8)
 
 # compile flags
 if(WIN32)
-	set_target_properties(${name} PROPERTIES COMPILE_FLAGS "/bigobj")
+	set_target_properties(${PROJECT_NAME} PROPERTIES COMPILE_FLAGS "-wd4250 -wd4251 -wd4275 -wd4675 -wd4996 /bigobj")
 endif()
