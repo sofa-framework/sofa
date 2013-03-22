4cbd72f9faa894b82572e0b449120edc0e186c32
Olivier Carre <olivier.carre@inria.fr>
2013-03-22 17:05:11 +0000

    r9150/sofa : UPDATE: cmake config files
    Former-commit-id: 465182139e8af5b19cf3232cd17724870a9ec9ac

--- cmake/post.cmake
+++ cmake/post.cmake
@@ -1,5 +1,7 @@
 cmake_minimum_required(VERSION 2.8)
 
-include(${CMAKE_CURRENT_LIST_DIR}/post-config.cmake)
+if(NOT PROJECT_NAME STREQUAL "${SOLUTION_NAME}")
+	include(${CMAKE_CURRENT_LIST_DIR}/post-config.cmake)
+endif()
 
 message("> ${PROJECT_NAME} : Done\n")
