56bf5abb47cf3dfb156c97ef9eb923fdeabf966e
Bruno Carrez <bruno.carrez@inria.fr>
2013-03-24 23:15:44 +0000

    r9159/sofa : cmake: extlib/tinyxml
    Former-commit-id: 82f62a5b2b67eb9e6b3a2979882f0e4c166cd095

--- sofa-dependencies.cmake
+++ sofa-dependencies.cmake
@@ -2,6 +2,7 @@ cmake_minimum_required(VERSION 2.8)
 
 # extlibs
 add_subdirectory("${SOFA_EXTLIBS_DIR}/newmat")
+add_subdirectory("${SOFA_EXTLIBS_DIR}/tinyxml")
 
 # sofa projects
 add_subdirectory("${SOFA_FRAMEWORK_DIR}/sofa/helper")
