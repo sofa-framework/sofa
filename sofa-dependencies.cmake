2a02aded49249449654fb52049ceefda8221a553
Bruno Carrez <bruno.carrez@inria.fr>
2013-03-24 23:07:55 +0000

    r9158/sofa : cmake: added extlib/newmat and modules/sofa_base_animation_loop
    Former-commit-id: 48dbf6af8dbdc7e0415643ddb6ce1b75795eadd1

--- sofa-dependencies.cmake
+++ sofa-dependencies.cmake
@@ -1,9 +1,15 @@
 cmake_minimum_required(VERSION 2.8)
 
+# extlibs
+add_subdirectory("${SOFA_EXTLIBS_DIR}/newmat")
+
 # sofa projects
 add_subdirectory("${SOFA_FRAMEWORK_DIR}/sofa/helper")
 add_subdirectory("${SOFA_FRAMEWORK_DIR}/sofa/defaulttype")
 add_subdirectory("${SOFA_FRAMEWORK_DIR}/sofa/core")
+
+# modules
+add_subdirectory(${SOFA_MODULES_DIR}/sofa/component)
 
 # plugins
 foreach(plugin ${SOFA_PLUGINS})
