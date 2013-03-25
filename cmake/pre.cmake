ca93ba951ecf0c622962f1df543da8a4f5953aeb
Olivier Carre <olivier.carre@inria.fr>
2013-03-25 14:41:39 +0000

    r9172/sofa : CHANGE: SofaSimulation is now handled like a "Solution" CHANGE: cmake message function now use status / error handling
    Former-commit-id: eac6f830ba2403ba9245c3da4714350eb48c1b68

--- cmake/pre.cmake
+++ cmake/pre.cmake
@@ -1,6 +1,10 @@
 cmake_minimum_required(VERSION 2.8)
 
-message("> ${PROJECT_NAME} : Generating")
+if(NOT PROJECT_NAME STREQUAL "${SOLUTION_NAME}")
+	message(STATUS "  > ${PROJECT_NAME} : Generating Project")
+else()
+	message(STATUS "> ${PROJECT_NAME} : Generating Solution\n")
+endif()
 
 include(${CMAKE_CURRENT_LIST_DIR}/environment.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/functions.cmake)
