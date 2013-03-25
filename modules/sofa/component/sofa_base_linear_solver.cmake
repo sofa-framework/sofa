e64cfcd02dd0e09c9d384a4a8124ace97568995a
Brina Goyette <bgoyette@users.gforge.inria.fr>
2013-03-25 12:49:35 +0000

    r9165/sofa : cmake: Add sofa_base_linear_solver.cmake
    Former-commit-id: 4e3ada7cedff60712179cb7e93b3150c77073771

---
+++ modules/sofa/component/sofa_base_linear_solver.cmake
@@ -0,0 +1,51 @@
+cmake_minimum_required(VERSION 2.8)
+
+project("SofaBaseLinearSolver")
+
+include(${SOFA_CMAKE_DIR}/pre.cmake)
+
+set(HEADER_FILES
+
+    initBaseLinearSolver.h 
+    linearsolver/CGLinearSolver.h 
+    linearsolver/CGLinearSolver.inl 
+    linearsolver/CholeskySolver.h 
+    linearsolver/CholeskySolver.inl 
+    linearsolver/BTDLinearSolver.h 
+    linearsolver/BTDLinearSolver.inl 
+    linearsolver/FullVector.h 
+    linearsolver/FullMatrix.h 
+    linearsolver/DiagonalMatrix.h 
+    linearsolver/SingleMatrixAccessor.h 
+    linearsolver/SparseMatrix.h 
+    linearsolver/CompressedRowSparseMatrix.h 
+    linearsolver/CompressedRowSparseMatrix.inl 
+    linearsolver/GraphScatteredTypes.h 
+    linearsolver/DefaultMultiMatrixAccessor.h 
+    linearsolver/MatrixLinearSolver.h 
+    linearsolver/MatrixExpr.h 
+    linearsolver/matrix_bloc_traits.h 
+    linearsolver/BlocMatrixWriter.h
+    
+    )
+    
+set(SOURCE_FILES
+
+    initBaseLinearSolver.cpp 
+    linearsolver/CGLinearSolver.cpp 
+    linearsolver/CholeskySolver.cpp 
+    linearsolver/BTDLinearSolver.cpp 
+    linearsolver/FullVector.cpp 
+    linearsolver/GraphScatteredTypes.cpp 
+    linearsolver/DefaultMultiMatrixAccessor.cpp 
+    linearsolver/MatrixLinearSolver.cpp 
+    linearsolver/SingleMatrixAccessor.cpp
+
+    )
+    
+add_library(${PROJECT_NAME} SHARED ${HEADER_FILES} ${SOURCE_FILES})
+target_link_libraries(${PROJECT_NAME} SofaSimulationTree )
+    
+set_target_properties(${PROJECT_NAME} PROPERTIES COMPILE_DEFINITIONS "${GLOBAL_DEFINES};SOFA_BUILD_BASE_LINEAR_SOLVER")
+    
+include(${SOFA_CMAKE_DIR}/post.cmake)
