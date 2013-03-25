dae9aa89c0bc62c4604be2c02e12d868f790549a
Brina Goyette <bgoyette@users.gforge.inria.fr>
2013-03-25 14:13:26 +0000

    r9169/sofa : cmake: Add: sofa_deformable.cmake, sofa_explicit_ode_solver.cmake, sofa_implicit_ode_solver.cmake, sofa_graph_component.cmake
    Former-commit-id: 9ac420192e7049e1135ace81fe438401e2aa0b63

---
+++ modules/sofa/component/sofa_deformable.cmake
@@ -0,0 +1,69 @@
+cmake_minimum_required(VERSION 2.8)
+
+project("SofaDeformable")
+
+include(${SOFA_CMAKE_DIR}/pre.cmake)
+
+set(HEADER_FILES
+
+    initDeformable.h 
+    forcefield/FastTriangularBendingSprings.h 
+    forcefield/FastTriangularBendingSprings.inl 
+    forcefield/QuadularBendingSprings.h 
+    forcefield/QuadularBendingSprings.inl 
+    forcefield/RestShapeSpringsForceField.h 
+    forcefield/RestShapeSpringsForceField.inl 
+    forcefield/TriangularBendingSprings.h 
+    forcefield/TriangularBendingSprings.inl 
+    forcefield/TriangularBiquadraticSpringsForceField.h 
+    forcefield/TriangularBiquadraticSpringsForceField.inl 
+    forcefield/TriangularQuadraticSpringsForceField.h 
+    forcefield/TriangularQuadraticSpringsForceField.inl 
+    forcefield/TriangularTensorMassForceField.h 
+    forcefield/TriangularTensorMassForceField.inl 
+    interactionforcefield/FrameSpringForceField.h 
+    interactionforcefield/FrameSpringForceField.inl 
+    interactionforcefield/MeshSpringForceField.h 
+    interactionforcefield/MeshSpringForceField.inl 
+    interactionforcefield/QuadBendingSprings.h 
+    interactionforcefield/QuadBendingSprings.inl 
+    interactionforcefield/RegularGridSpringForceField.h 
+    interactionforcefield/RegularGridSpringForceField.inl 
+    interactionforcefield/SpringForceField.h 
+    interactionforcefield/SpringForceField.inl 
+    interactionforcefield/StiffSpringForceField.h 
+    interactionforcefield/StiffSpringForceField.inl 
+    interactionforcefield/TriangleBendingSprings.h 
+    interactionforcefield/TriangleBendingSprings.inl 
+    interactionforcefield/VectorSpringForceField.h 
+    interactionforcefield/VectorSpringForceField.inl
+
+    )
+    
+set(SOURCE_FILES
+
+    initDeformable.cpp 
+    forcefield/FastTriangularBendingSprings.cpp 
+    forcefield/QuadularBendingSprings.cpp 
+    forcefield/RestShapeSpringsForceField.cpp 
+    forcefield/TriangularBendingSprings.cpp 
+    forcefield/TriangularBiquadraticSpringsForceField.cpp 
+    forcefield/TriangularQuadraticSpringsForceField.cpp 
+    forcefield/TriangularTensorMassForceField.cpp 
+    interactionforcefield/FrameSpringForceField.cpp 
+    interactionforcefield/MeshSpringForceField.cpp 
+    interactionforcefield/QuadBendingSprings.cpp 
+    interactionforcefield/RegularGridSpringForceField.cpp 
+    interactionforcefield/SpringForceField.cpp 
+    interactionforcefield/StiffSpringForceField.cpp 
+    interactionforcefield/TriangleBendingSprings.cpp 
+    interactionforcefield/VectorSpringForceField.cpp
+
+    )
+    
+add_library(${PROJECT_NAME} SHARED ${HEADER_FILES} ${SOURCE_FILES})
+target_link_libraries(${PROJECT_NAME} SofaBaseTopology )
+    
+set_target_properties(${PROJECT_NAME} PROPERTIES COMPILE_DEFINITIONS "${GLOBAL_DEFINES};SOFA_BUILD_DEFORMABLE")
+    
+include(${SOFA_CMAKE_DIR}/post.cmake)
