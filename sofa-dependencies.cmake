0ee1b754dc0e49c60774190b9c95904769a9106a
Bruno Carrez <bruno.carrez@inria.fr>
2013-03-25 14:58:29 +0000

    r9173/sofa : cmake: extlib/miniFlowVR + options EXTERNAL_HAVE_CSPARSE, EXTERNAL_HAVE_EIGEN2, EXTERNAL_HAVE_FLOWVR
    Former-commit-id: 4a4830924f4c4e7bc696290c2159c55a22bcb4f2

--- sofa-dependencies.cmake
+++ sofa-dependencies.cmake
@@ -3,8 +3,15 @@ cmake_minimum_required(VERSION 2.8)
 # extlibs
 add_subdirectory("${SOFA_EXTLIBS_DIR}/newmat")
 add_subdirectory("${SOFA_EXTLIBS_DIR}/tinyxml")
-add_subdirectory("${SOFA_EXTLIBS_DIR}/csparse")
-add_subdirectory("${SOFA_EXTLIBS_DIR}/eigen-3.1.1")
+if(EXTERNAL_HAVE_CSPARSE)
+	add_subdirectory("${SOFA_EXTLIBS_DIR}/csparse")
+endif()
+if(EXTERNAL_HAVE_EIGEN2)
+	add_subdirectory("${SOFA_EXTLIBS_DIR}/eigen-3.1.1")
+endif()
+if(EXTERNAL_HAVE_FLOWVR)
+	add_subdirectory("${SOFA_EXTLIBS_DIR}/miniFlowVR")
+endif()
 
 # sofa projects
 add_subdirectory("${SOFA_FRAMEWORK_DIR}/sofa/helper")
