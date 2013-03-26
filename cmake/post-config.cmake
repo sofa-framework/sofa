4532ccdb8125b37ff099e07b326c19c84c43292b
Olivier Carre <olivier.carre@inria.fr>
2013-03-26 11:47:56 +0000

    r9193/sofa : CHANGE: projects now use the new naming convention CHANGE: GLOBAL_DEFINES has been renamed GLOBAL_COMPILER_DEFINES ADD: COMPILER_FLAGS and LINKER_FLAGS are now applied to targets
    Former-commit-id: ee88675c258e382158b88d960517f117933fb29b

--- cmake/post-config.cmake
+++ cmake/post-config.cmake
@@ -14,14 +14,23 @@ endforeach()
 if(TARGET ${PROJECT_NAME})
 
 	# compile definitions
-	set_target_properties(${PROJECT_NAME} PROPERTIES COMPILE_DEFINITIONS "${GLOBAL_DEFINES};${COMPILER_DEFINES}")
+	set_target_properties(${PROJECT_NAME} PROPERTIES COMPILE_DEFINITIONS "${GLOBAL_COMPILER_DEFINES};${COMPILER_DEFINES}")
 
 	# compile flags
+	set(COMPILER_FLAGS_OS_SPECIFIC "")
 	if(WIN32)
-		set_target_properties(${PROJECT_NAME} PROPERTIES COMPILE_FLAGS "-wd4250 -wd4251 -wd4275 -wd4675 -wd4996 /bigobj")
+		set(COMPILER_FLAGS_OS_SPECIFIC "-wd4250 -wd4251 -wd4275 -wd4675 -wd4996 /bigobj")
 	endif()
+	set_target_properties(${PROJECT_NAME} PROPERTIES COMPILE_FLAGS "${COMPILER_FLAGS_OS_SPECIFIC} ${COMPILER_FLAGS}")
 
 	# link dependencies
 	target_link_libraries(${PROJECT_NAME} ${LINKER_DEPENDENCIES})
+
+	#link flags
+	set(LINKER_FLAGS_OS_SPECIFIC "")
+	if(WIN32)
+		set(LINKER_FLAGS_OS_SPECIFIC "")
+	endif()
+	set_target_properties(${PROJECT_NAME} PROPERTIES LINK_FLAGS "${LINKER_FLAGS_OS_SPECIFIC} ${LINKER_FLAGS}")
 
 endif()
