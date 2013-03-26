13de38f683e4e430b775cf79daa0cdd725398756
Brina Goyette <bgoyette@users.gforge.inria.fr>
2013-03-26 12:27:02 +0000

    r9194/sofa : cmake: Add: SofaExporter SofaLoader SofaMiscMapping SofaTaucsSolver
    Former-commit-id: a9b5cf92ed3012ce281bc68a659881f587541ec4

---
+++ modules/sofa/component/SofaExporter.cmake
@@ -0,0 +1,37 @@
+cmake_minimum_required(VERSION 2.8)
+
+project("SofaExporter")
+
+include(${SOFA_CMAKE_DIR}/pre.cmake)
+
+set(HEADER_FILES
+
+    initExporter.h 
+    misc/WriteState.h 
+    misc/WriteState.inl 
+    misc/WriteTopology.h 
+    misc/WriteTopology.inl 
+    misc/VTKExporter.h 
+    misc/OBJExporter.h 
+    misc/MeshExporter.h
+
+    )
+    
+set(SOURCE_FILES
+
+    initExporter.cpp 
+    misc/WriteState.cpp 
+    misc/WriteTopology.cpp 
+    misc/VTKExporter.cpp 
+    misc/OBJExporter.cpp 
+    misc/MeshExporter.cpp
+ 
+    )
+    
+add_library(${PROJECT_NAME} SHARED ${HEADER_FILES} ${SOURCE_FILES})
+
+set(COMPILER_DEFINES "SOFA_BUILD_EXPORTER" )
+set(LINKER_DEPENDENCIES ${ZLIB_LIBRARIES} SofaSimulationTree )
+
+    
+include(${SOFA_CMAKE_DIR}/post.cmake)
---
+++ modules/sofa/component/SofaTaucsSolver.cmake
@@ -0,0 +1,34 @@
+cmake_minimum_required(VERSION 2.8)
+
+project("SofaTaucsSolver")
+
+include(${SOFA_CMAKE_DIR}/pre.cmake)
+
+set(HEADER_FILES
+
+    initTaucsSolver.h 
+    linearsolver/SparseTAUCSSolver.h 
+    linearsolver/IncompleteTAUCSSolver.h 
+           
+    )
+    
+set(SOURCE_FILES
+
+    linearsolver/SparseTAUCSSolver.cpp 
+    linearsolver/IncompleteTAUCSSolver.cpp 
+ 
+    )
+    
+if(SOFA_EXTLIBS_TAUCS_MT)
+	list(APPEND HEADER_FILES "linearsolver/SparseTAUCSLLtSolver.h")
+
+	list(APPEND SOURCE_FILES "linearsolver/SparseTAUCSLLtSolver.cpp")
+endif()
+    
+add_library(${PROJECT_NAME} SHARED ${HEADER_FILES} ${SOURCE_FILES})
+
+set(COMPILER_DEFINES "SOFA_BUILD_TAUCS_SOLVER" )
+set(LINKER_DEPENDENCIES  )
+
+    
+include(${SOFA_CMAKE_DIR}/post.cmake)
