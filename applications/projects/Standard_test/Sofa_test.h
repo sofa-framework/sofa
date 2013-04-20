14ebd76498cf768b32d5c51ca859e6ca8924ed9b
Francois Faure <francois.faure@inria.fr>
2013-04-20 08:26:12 +0000

    r9379/sofa : Working on solver tests
    Former-commit-id: 868a621ec9d56699e9b19a322c2ed99ebc09b803

--- applications/projects/Standard_test/Sofa_test.h
+++ applications/projects/Standard_test/Sofa_test.h
@@ -195,11 +195,11 @@ struct Sofa_test : public ::testing::Test
 
 /// helper for more compact component creation
 template<class Component>
-typename Component::SPtr addNew( sofa::simulation::Node::SPtr parentNode, std::string name="component" )
+typename Component::SPtr addNew( sofa::simulation::Node::SPtr parentNode, const char* name="component" )
 {
     typename Component::SPtr component = sofa::core::objectmodel::New<Component>();
     parentNode->addObject(component);
-    component->setName(parentNode->getName()+"_"+name);
+    component->setName(parentNode->getName()+"_"+std::string(name));
     return component;
 }
 
