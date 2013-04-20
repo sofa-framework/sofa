8643fc0e6067cd6ac1d53ba60f75f7dfd5d3e001
Francois Faure <francois.faure@inria.fr>
2013-04-20 17:32:43 +0000

    r9382/sofa : Created the first solver test. Gets global vectors, tests convergence to a static solution, and compares to an expected solution.
    Former-commit-id: 733d23dc82aef993e2519d5c021992111c7803d8

--- applications/projects/Standard_test/Solver_test.h
+++ applications/projects/Standard_test/Solver_test.h
@@ -25,7 +25,7 @@
 
 
 #include <gtest/gtest.h>
-#include "../Standard_test/Sofa_test.h"
+#include "Sofa_test.h"
 #include <sofa/component/init.h>
 #include "../../tutorials/objectCreator/ObjectCreator.h"
 #include <sofa/simulation/graph/DAGSimulation.h>
@@ -124,102 +124,8 @@ private:
 };
 
 
-struct EulerImplicit_test_2particles : public Simulation_test
-{
-    EulerImplicit_test_2particles()
-    {
-        //*******
-        initSofa();
-        //*******
-        // begin create scene under the root node
-
-        modeling::addNew<odesolver::EulerImplicitSolver>(getRoot(),"odesolver" );
-        modeling::addNew<CGLinearSolver>(getRoot(),"linearsolver");
-
-        Node::SPtr string = massSpringString(
-                    getRoot(),
-                    0,0,0, // first endpoint
-                    1,0,0, // second endpoint
-                    2,     // number of particles
-                    2.0,    // total mass
-                    1000.0,   // stiffness
-                    0.1    // damping ratio
-                    );
-        FixedConstraint3::SPtr fixed = modeling::addNew<FixedConstraint3>(string,"fixedConstraint");
-        fixed->addConstraint(0);
-
-        // end create scene
-        //*********
-        initScene();
-        //*********
-
-        FullVector x0, x1, v0, v1;
-        getAssembledPositionVector(&x0);
-//        cerr<<"My_test, initial positions : " << x0 << endl;
-        getAssembledVelocityVector(&v0);
-//        cerr<<"My_test, initial velocities: " << v0 << endl;
-
-        Real dx, dv;
-        unsigned n=0;
-        const unsigned nMax=100;
-        const double  precision = 1.e-4;
-        do {
-            sofa::simulation::getSimulation()->animate(getRoot().get(),1.0);
-
-            getAssembledPositionVector(&x1);
-//            cerr<<"My_test, new positions : " << x1 << endl;
-            getAssembledVelocityVector(&v1);
-//            cerr<<"My_test, new velocities: " << v1 << endl;
-
-            dx = this->vectorCompare(x0,x1);
-            dv = this->vectorCompare(v0,v1);
-            x0 = x1;
-            v0 = v1;
-            n++;
-
-        } while( (dx>1.e-4 || dv>1.e-4) && n<nMax );
-
-        // test convergence
-        if( n==nMax )
-            ADD_FAILURE() << "Solver test has not converged in " << nMax << " iterations, precision = " << precision << endl
-                          <<" previous x = " << x0 << endl
-                          <<" current x  = " << x1 << endl
-                          <<" previous v = " << v0 << endl
-                          <<" current v  = " << v1 << endl;
-
-        // test position of the second particle
-        Vec3d expected(0,-1.00981,0), actual( x0[3],x0[4],x0[5]);
-        if( vectorCompare(expected,actual)>precision )
-            ADD_FAILURE() << "Solver test has not converged to the expected position" <<
-                             " expected: " << expected << endl <<
-                             " actual " << actual << endl;
-
-    }
-
-};
-
-TEST_F( EulerImplicit_test_2particles, myEulerImplicit_test_2particles )
-{
-}
 
 }// namespace sofa
-
-
-
-
-///// This is a starting point for a test fixture. Copy-paste it and uncomment it to create yours.
-//namespace sofa
-//{
-
-//struct My_test : public Sofa_test<>
-//{
-//};
-
-//TEST_F( My_test, my_test )
-//{
-//}
-
-//}// namespace sofa
 
 
 
