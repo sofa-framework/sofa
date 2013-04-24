b2a09491132322d654b4e5eaaab085f2374e332b
Olivier Carre <olivier.carre@inria.fr>
2013-04-24 15:48:40 +0000

    r9395/sofa : CHANGE: projects now get their project name from a variable defined when we register the project. This way we avoid redundancy and bugs that could happen when we gave the same name multiple times (during registration and in its cmakelists).
    Former-commit-id: f3fefe9789195a0b484369d1b905018500bbdde4

--- cmake/preBuildConfig.cmake
+++ cmake/preBuildConfig.cmake
@@ -1,5 +1,24 @@
 cmake_minimum_required(VERSION 2.8)
 
+# retrieve and set project name
+set(projectName)
+get_directory_property(properties VARIABLES)
+foreach(property ${properties})
+	if(NOT property STREQUAL "")
+		string(REGEX MATCH "^GLOBAL_PROJECT_PATH_.+$" pathMatch ${property})
+		if(NOT pathMatch STREQUAL "")
+			if(${pathMatch} STREQUAL "${CMAKE_CURRENT_SOURCE_DIR}")
+				string(SUBSTRING "${pathMatch}" 20 -1 projectName)
+				break()
+			endif()
+		endif()
+	endif()
+endforeach()
+if(projectName STREQUAL "")
+	message(FATAL_ERROR "Error while trying to add a non-registered project : ${CMAKE_CURRENT_SOURCE_DIR}")
+endif()
+project(${projectName})
+
 # useful variables
 set(COMPILER_DEFINES "")
 set(COMPILER_FLAGS "")
