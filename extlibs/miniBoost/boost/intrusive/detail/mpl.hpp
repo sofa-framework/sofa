405f9d848057bfcd3bc8b0e9405f19f5f3947b19
cguerin <cguerin@users.gforge.inria.fr>
2013-04-30 12:05:44 +0000

    r9431/sofa : FIX: miniBoost compilation on 360.
    Former-commit-id: 62e1a703b2bad2ce777140263a6c9b52a54588a6

--- extlibs/miniBoost/boost/intrusive/detail/mpl.hpp
+++ extlibs/miniBoost/boost/intrusive/detail/mpl.hpp
@@ -130,7 +130,7 @@ struct identity
 #define BOOST_INTRUSIVE_TT_DECL 
 #endif
 
-#if defined(_MSC_EXTENSIONS) && !defined(__BORLAND__) && !defined(_WIN64)
+#if defined(_MSC_EXTENSIONS) && !defined(__BORLAND__) && !defined(_WIN64) && !defined(_XBOX)
 #define BOOST_INTRUSIVE_TT_TEST_MSC_FUNC_SIGS
 #endif
 
