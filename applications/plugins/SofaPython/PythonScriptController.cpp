7a124b5f88d16574bdcfdf03708a6eebf797bc28
Bruno Carrez <bruno.carrez@inria.fr>
2013-05-13 14:34:59 +0000

    r9472/sofa : fixed some warnings (again)
    Former-commit-id: 87189e37bf2da33d5672a551716b0c878602514f

--- applications/plugins/SofaPython/PythonScriptController.cpp
+++ applications/plugins/SofaPython/PythonScriptController.cpp
@@ -281,7 +281,8 @@ void PythonScriptController::script_onScriptEvent(core::objectmodel::ScriptEvent
     else
     {
 //        SP_CALL_MODULEFUNC(m_Func_onScriptEvent,"(OsO)",SP_BUILD_PYSPTR(pyEvent->getSender().get()),pyEvent->getEventName().c_str(),pyEvent->getUserData())
-        SP_CALL_OBJECTFUNC("onScriptEvent",const_cast<char*>("(OsO)"),SP_BUILD_PYSPTR(pyEvent->getSender().get()),pyEvent->getEventName().c_str(),pyEvent->getUserData())
+
+        SP_CALL_OBJECTFUNC(const_cast<char*>("onScriptEvent"),const_cast<char*>("(OsO)"),SP_BUILD_PYSPTR(pyEvent->getSender().get()),const_cast<char*>(pyEvent->getEventName().c_str()),pyEvent->getUserData())
     }
 
     //TODO
