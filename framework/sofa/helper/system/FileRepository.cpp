19fbfb4dc89adbc29442c1e84edacd6d15664834
Matthieu Nesme <matthieu.nesme@inria.fr>
2013-05-29 12:02:15 +0000

    r9526/sofa : fixed gcc compilation & qmake building cleaned FileRepository predefined paths must be tested on visual studio (both by using qmake & cmake) and for cmake/gcc
    Former-commit-id: 20253cf4c4111d083890ca0969f47b86461bc40d

--- framework/sofa/helper/system/FileRepository.cpp
+++ framework/sofa/helper/system/FileRepository.cpp
@@ -51,64 +51,64 @@ namespace system
 {
 
 #ifndef SOFA_SRC_BIN_DIR
-#define SOFA_SRC_BIN_DIR ""
+#define SOFA_SRC_BIN_DIR ./
 #endif
 
 #ifndef SOFA_BUILD_DIR
-#define SOFA_BUILD_DIR "../"
+#define SOFA_BUILD_DIR ../
 #endif
 
+
+#define ADD_SOFA_BUILD_DIR( x ) sofa_tostring(SOFA_BUILD_DIR)sofa_tostring(x)
+#define ADD_SOFA_SRC_BIN_DIR( x ) sofa_tostring(SOFA_SRC_BIN_DIR)sofa_tostring(x)
+
+
+
 #if defined (WIN32) || defined (_XBOX)
-FileRepository PluginRepository("SOFA_PLUGIN_PATH", sofa_concat(SOFA_BUILD_DIR, "../bin"));
+#define SOFA_PLUGIN_SUBDIR ../bin
 #else
-FileRepository PluginRepository("SOFA_PLUGIN_PATH", sofa_concat(SOFA_BUILD_DIR, "../lib"));
+#define SOFA_PLUGIN_SUBDIR ../lib
 #endif
+
+FileRepository PluginRepository("SOFA_PLUGIN_PATH", ADD_SOFA_BUILD_DIR(SOFA_PLUGIN_SUBDIR));
+
+
+#undef SOFA_PLUGIN_SUBDIR
+
+
+
+static FileRepository createSofaDataPath()
+{
+    FileRepository repository("SOFA_DATA_PATH");
 #if defined (WIN32) || defined (_XBOX) || defined(PS3)
-FileRepository DataRepository("SOFA_DATA_PATH", sofa_concat(
-													sofa_concat(SOFA_BUILD_DIR, ";"),
-													sofa_concat(
-														sofa_concat(SOFA_SRC_BIN_DIR, "../share;"),
-														sofa_concat(SOFA_SRC_BIN_DIR, "../examples")
-																)
-															)
-							 );
+    repository.addLastPath( sofa_tostring(SOFA_BUILD_DIR) );
+    repository.addLastPath( ADD_SOFA_SRC_BIN_DIR( ../share ) );
+    repository.addLastPath( ADD_SOFA_SRC_BIN_DIR( ../examples ) );
 #elif defined (__APPLE__)
-FileRepository DataRepository("SOFA_DATA_PATH", sofa_concat(
-													sofa_concat(SOFA_BUILD_DIR, ":"),
-													sofa_concat(
-														sofa_concat(SOFA_SRC_BIN_DIR, "../share:",),
-														sofa_concat(
-															sofa_concat(SOFA_SRC_BIN_DIR, "../examples:"),
-															sofa_concat(
-																sofa_concat(SOFA_SRC_BIN_DIR, "../Resources/examples:"),
-																sofa_concat(
-																	sofa_concat(SOFA_SRC_BIN_DIR, "../Resources:"),
-																	sofa_concat(
-																		sofa_concat(SOFA_SRC_BIN_DIR, "../../../../examples:"),
-																		sofa_concat(SOFA_SRC_BIN_DIR, "../../../../share")
-																				)
-																			)
-																		)
-																	)
-																)
-															)
-								);
+    repository.addLastPath( sofa_tostring(SOFA_BUILD_DIR) );
+    repository.addLastPath( ADD_SOFA_SRC_BIN_DIR( ../share ) );
+    repository.addLastPath( ADD_SOFA_SRC_BIN_DIR( ../examples ) );
+    repository.addLastPath( ADD_SOFA_SRC_BIN_DIR( ../Resources/examples ) );
+    repository.addLastPath( ADD_SOFA_SRC_BIN_DIR( ../Resources ) );
+    repository.addLastPath( ADD_SOFA_SRC_BIN_DIR( ../../../../examples ) );
+    repository.addLastPath( ADD_SOFA_SRC_BIN_DIR( ../../../../share ) );
 #else // LINUX
-FileRepository DataRepository("SOFA_DATA_PATH", sofa_concat(
-													sofa_concat(SOFA_BUILD_DIR, ":"),
-													sofa_concat(
-														sofa_concat(SOFA_SRC_BIN_DIR, "../share:"),
-														sofa_concat(
-															sofa_concat(SOFA_SRC_BIN_DIR, "../examples:"),
-															sofa_concat(
-																sofa_concat(SOFA_SRC_BIN_DIR, "../../Verification/data:"),
-																sofa_concat(SOFA_SRC_BIN_DIR, "../../Verification/simulation")
-																		)
-																	)
-																)
-															)
-								);
+    repository.addLastPath( sofa_tostring(SOFA_BUILD_DIR) );
+    repository.addLastPath( ADD_SOFA_SRC_BIN_DIR( ../share ) );
+    repository.addLastPath( ADD_SOFA_SRC_BIN_DIR( ../examples ) );
+    repository.addLastPath( ADD_SOFA_SRC_BIN_DIR( ../../Verification/data ) );
+    repository.addLastPath( ADD_SOFA_SRC_BIN_DIR( ../../Verification/simulation ) );
 #endif
+    return repository;
+}
+
+FileRepository DataRepository = createSofaDataPath();
+
+
+#undef ADD_SOFA_BUILD_DIR
+#undef ADD_SOFA_SRC_BIN_DIR
+
+
 
 #if defined (_XBOX) || defined(PS3)
 char* getenv(const char* varname) { return NULL; } // NOT IMPLEMENTED
