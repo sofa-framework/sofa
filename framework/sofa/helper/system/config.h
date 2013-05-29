3e4d392395d60d8b092f29ca0e5b50dc26fb8208
Matthieu Nesme <matthieu.nesme@inria.fr>
2013-05-29 12:02:15 +0000

    r9526/sofa : fixed gcc compilation & qmake building cleaned FileRepository predefined paths must be tested on visual studio (both by using qmake & cmake) and for cmake/gcc
    Former-commit-id: 20253cf4c4111d083890ca0969f47b86461bc40d

--- framework/sofa/helper/system/config.h
+++ framework/sofa/helper/system/config.h
@@ -76,9 +76,9 @@ typedef float SReal;
 typedef double SReal;
 #endif
 
-#define sofa_concat(a,b) sofa_do_concat(a,b)
+#define sofa_do_concat2(a,b) a ## b
 #define sofa_do_concat(a,b) sofa_do_concat2(a,b)
-#define sofa_do_concat2(a,b) a##b
+#define sofa_concat(a,b) sofa_do_concat(a,b)
 
 #define sofa_tostring(a) sofa_do_tostring(a)
 #define sofa_do_tostring(a) #a
