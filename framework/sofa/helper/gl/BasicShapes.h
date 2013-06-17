2ce58989c4d199e6385d003cb0b164599ca9aed8
remibessard <remibessard@users.gforge.inria.fr>
2013-06-17 08:21:41 +0000

    r9614/sofa : Fix BasicShapes.h (sofa::defaulttype namespace)
    Former-commit-id: acf3be067106885b2cd6089e5dc4f448fb1ed75f

--- framework/sofa/helper/gl/BasicShapes.h
+++ framework/sofa/helper/gl/BasicShapes.h
@@ -40,6 +40,8 @@ namespace helper
 namespace gl
 {
 
+using namespace sofa::defaulttype;
+
 template <typename V>
 void drawCone(const V& p1, const V& p2, const float& radius1, const float& radius2, const int subd=8)
 {
