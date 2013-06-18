51dc165a1f3f33aad42336d5acd65121b499a5ea
Matthieu Nesme <matthieu.nesme@inria.fr>
2013-06-18 16:19:49 +0000

    r9629/sofa : collision: cleaning
    Former-commit-id: ce3c63a53c005f976898b812a831be9510a5da24

--- modules/sofa/component/collision/BaseIntTool.h
+++ modules/sofa/component/collision/BaseIntTool.h
@@ -28,21 +28,14 @@ public:
     template <class DataTypes1,class DataTypes2>
     static bool testIntersection(TSphere<DataTypes1>& sph1, TSphere<DataTypes2>& sph2,double alarmDist)
     {
-        typename TSphere<DataTypes1>::Real tworadius = sph1.r() + sph2.r();
-        return ( sph1.center() - sph2.center() ).norm2() < tworadius*tworadius + alarmDist;
+        typename TSphere<DataTypes1>::Real r = sph1.r() + sph2.r() + alarmDist;
+        return ( sph1.center() - sph2.center() ).norm2() <= r*r;
     }
 
 
     template <class DataTypes1,class DataTypes2>
     static int computeIntersection(TSphere<DataTypes1>& sph1, TSphere<DataTypes2>& sph2,double alarmDist,double contactDist,OutputVector* contacts);
 
-    template <class TReal>
-    static int computeIntersection(TSphere<StdVectorTypes<Vec<3,TReal>,Vec<3,TReal>,TReal> >& sph1, TSphere<StdRigidTypes<3,TReal> >& sph2,
-                                         double alarmDist, double contactDist, OutputVector * contacts);
-
-    template <class TReal>
-    static int computeIntersection(TSphere<StdVectorTypes<Vec<3,TReal>,Vec<3,TReal>,TReal> >& sph1, TSphere<StdVectorTypes<Vec<3,TReal>,Vec<3,TReal>,TReal> >& sph2,
-                                         double alarmDist, double contactDist, OutputVector * contacts);
 
     inline static int computeIntersection(Capsule &c1, Capsule &c2, double alarmDist, double contactDist, OutputVector *contacts){
         return CapsuleIntTool::computeIntersection(c1,c2,alarmDist,contactDist,contacts);
@@ -73,73 +66,18 @@ int BaseIntTool::computeIntersection(TSphere<DataTypes1>& sph1, TSphere<DataType
     double r = sph1.r() + sph2.r();
     double myAlarmDist = alarmDist + r;
     Vector3 dist = sph2.center() - sph1.center();
+    SReal norm2 = dist.norm2();
 
-    if (dist.norm2() >= myAlarmDist*myAlarmDist)
+    if (norm2 >= myAlarmDist*myAlarmDist)
         return 0;
 
     contacts->resize(contacts->size()+1);
     DetectionOutput *detection = &*(contacts->end()-1);
     detection->normal = dist;
-    double distSph1Sph2 = detection->normal.norm();
+    double distSph1Sph2 = helper::rsqrt(norm2);
     detection->normal /= distSph1Sph2;
     detection->point[0] = sph1.center() + detection->normal * sph1.r();
     detection->point[1] = sph2.center() - detection->normal * sph2.r();
-
-    detection->value = distSph1Sph2 - r - contactDist;
-    detection->elem.first = sph1;
-    detection->elem.second = sph2;
-    detection->id = (sph1.getCollisionModel()->getSize() > sph2.getCollisionModel()->getSize()) ? sph1.getIndex() : sph2.getIndex();
-
-    return 1;
-}
-
-
-template <class TReal>
-int BaseIntTool::computeIntersection(TSphere<StdVectorTypes<Vec<3,TReal>,Vec<3,TReal>,TReal> >& sph1, TSphere<StdRigidTypes<3,TReal> >& sph2,
-                                     double alarmDist, double contactDist, OutputVector * contacts)
-{
-    double r = sph1.r() + sph2.r();
-    double myAlarmDist = alarmDist + r;
-    Vector3 dist = sph2.center() - sph1.center();
-
-    if (dist.norm2() >= myAlarmDist*myAlarmDist)
-        return 0;
-
-    contacts->resize(contacts->size()+1);
-    DetectionOutput *detection = &*(contacts->end()-1);
-    detection->normal = dist;
-    double distSph1Sph2 = detection->normal.norm();
-    detection->normal /= distSph1Sph2;
-    detection->point[0] = sph1.center();
-    detection->point[1] = sph2.center() - detection->normal * sph2.r();
-
-    detection->value = distSph1Sph2 - r - contactDist;
-    detection->elem.first = sph1;
-    detection->elem.second = sph2;
-    detection->id = (sph1.getCollisionModel()->getSize() > sph2.getCollisionModel()->getSize()) ? sph1.getIndex() : sph2.getIndex();
-
-    return 1;
-}
-
-
-template <class TReal>
-int BaseIntTool::computeIntersection(TSphere<StdVectorTypes<Vec<3,TReal>,Vec<3,TReal>,TReal> >& sph1, TSphere<StdVectorTypes<Vec<3,TReal>,Vec<3,TReal>,TReal> >& sph2,
-                                     double alarmDist, double contactDist, OutputVector * contacts)
-{
-    double r = sph1.r() + sph2.r();
-    double myAlarmDist = alarmDist + r;
-    Vector3 dist = sph2.center() - sph1.center();
-
-    if (dist.norm2() >= myAlarmDist*myAlarmDist)
-        return 0;
-
-    contacts->resize(contacts->size()+1);
-    DetectionOutput *detection = &*(contacts->end()-1);
-    detection->normal = dist;
-    double distSph1Sph2 = detection->normal.norm();
-    detection->normal /= distSph1Sph2;
-    detection->point[0] = sph1.center();
-    detection->point[1] = sph2.center();
 
     detection->value = distSph1Sph2 - r - contactDist;
     detection->elem.first = sph1;
