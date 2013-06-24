194e0152006a5466a2580ba4aed99f22d54f431a
Moreno Trlin <moreno.trlin@inria.fr>
2013-06-24 15:19:03 +0000

    r9652/sofa : Fix the OBB/OBB normal response
    Former-commit-id: 49ec53aae06073fb56b497e9f6bfe5b24a696343

--- modules/sofa/component/collision/OBBIntTool.cpp
+++ modules/sofa/component/collision/OBBIntTool.cpp
@@ -16,14 +16,23 @@ int OBBIntTool::computeIntersection(OBB & box0, OBB & box1,double alarmDist,doub
     IntrOBBOBB intr(box0,box1);
     //double max_time = helper::rsqrt((alarmDist * alarmDist)/((box1.lvelocity() - box0.lvelocity()).norm2()));
     if(/*intr.Find(max_time,box0.lvelocity(),box1.lvelocity())*/intr.Find(alarmDist)){
-        OBB::Real dist2 = (intr.pointOnFirst() - intr.pointOnSecond()).norm2();
+        Vector3 P0P1(intr.pointOnSecond() - intr.pointOnFirst());
+        OBB::Real dist2 = P0P1.norm2();
         if((!intr.colliding()) && dist2 > alarmDist * alarmDist)
-            return 0;
+            return 0;        
+
+        assert((!intr.colliding()) || (P0P1.cross(intr.separatingAxis()).norm2() < IntrUtil<SReal>::ZERO_TOLERANCE()));
 
         contacts->resize(contacts->size()+1);
         DetectionOutput *detection = &*(contacts->end()-1);
 
-        detection->normal = intr.separatingAxis();
+        if((P0P1.cross(intr.separatingAxis()).norm2() < IntrUtil<SReal>::ZERO_TOLERANCE())){
+            detection->normal = intr.separatingAxis();//separatingAxis is normalized while P0P1 is not
+        }
+        else{
+            detection->normal = P0P1;
+            detection->normal.normalize();
+        }
 
         detection->point[0] = intr.pointOnFirst();
         detection->point[1] = intr.pointOnSecond();
