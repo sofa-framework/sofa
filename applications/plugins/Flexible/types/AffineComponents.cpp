f459b609c07863aa694a24918f1a79ff3028c594
Matthieu Nesme <matthieu.nesme@inria.fr>
2013-06-25 10:02:32 +0000

    r9654/sofa : Flexible: another try to compile FlexibleDev on both linux & windows
    Former-commit-id: 403b138d4b0b85811113d89d66a5049e77c65b9b

--- applications/plugins/Flexible/types/AffineComponents.cpp
+++ applications/plugins/Flexible/types/AffineComponents.cpp
@@ -1,29 +1,6 @@
-#define FLEXIBLE_AffineComponents_CPP // warning this define must follow the shape FLEXIBLE_TYPEABSTRACTNAMEComponents_CPP
+#define FLEXIBLE_AffineComponents_CPP
 
 #include "AffineComponents.h"
 
 #define TYPEABSTRACTNAME Affine
 #include "ComponentSpecializations.cpp.inl"
-
-namespace sofa
-{
-
-namespace core
-{
-
-namespace behavior
-{
-
-#ifndef SOFA_FLOAT
-template class SOFA_Flexible_API ForceField<defaulttype::Affine3dTypes>;
-#endif
-
-#ifndef SOFA_DOUBLE
-template class SOFA_Flexible_API ForceField<defaulttype::Affine3fTypes>;
-#endif
-
-} // namespace behavior
-
-} // namespace core
-
-} // namespace sofa
