fdce0f78be941b09d9d6d4885b79a21e677fd4b0
Matthieu Nesme <matthieu.nesme@inria.fr>
2013-07-10 15:43:32 +0000

    r9726/sofa : Compliant: AssembleSystem: added a function to test if the system is diagonal dominant
    Former-commit-id: 7a5fb3f33c14ff813d89afbb11d96cf168d7b456

--- applications/plugins/Compliant/Compliant_lib/AssembledSystem.h
+++ applications/plugins/Compliant/Compliant_lib/AssembledSystem.h
@@ -56,6 +56,9 @@ public:
 	std::vector< dofs_type* > master, compliant;
 	
 	void debug(SReal threshold = 0) const;
+
+    // return true iff the magnitude of every diagonal entries are larger or equal than the sum of the magnitudes of all the non-diagonal entries in the same row
+    bool isDiagonalDominant() const;
 	
 };
 
