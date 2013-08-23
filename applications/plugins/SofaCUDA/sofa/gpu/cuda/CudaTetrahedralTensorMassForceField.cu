7bbc777271a94378bbbeda3c03aac326ad6a9d64
Brina Goyette <bgoyette@users.gforge.inria.fr>
2013-08-23 15:13:05 +0000

    r9881/sofa : SofaCUDA: Moved CudaTetrahedralTensorMassForceField from SofaDev to SofaPublic
    Former-commit-id: 6f329dfd819a4c2bcca8f6c04ba5ff51c8aba5b9

---
+++ applications/plugins/SofaCUDA/sofa/gpu/cuda/CudaTetrahedralTensorMassForceField.cu
@@ -0,0 +1,329 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#include "cuda.h"
+#include <cstdio>
+#include <sofa/gpu/cuda/CudaCommon.h>
+#include <sofa/gpu/cuda/CudaMath.h>
+
+#define EDGE_SIZE 11
+#define V0 9
+#define V1 10
+#define DFDX 0
+
+#define TIMING true
+
+#if defined(__cplusplus) && CUDA_VERSION < 2000
+namespace sofa
+{
+namespace gpu
+{
+namespace cuda
+{
+#endif
+
+extern "C"
+{
+void TetrahedralTensorMassForceFieldCuda3f_addForce(int nbPoints, int nbMaxEdgesPerNode, const void* neighbourhoodPoints, void* contribEdge, int nbEdges, void* f, const void* x, const void* initialPoints, const void* edgeInfo );
+void TetrahedralTensorMassForceFieldCuda3f_addDForce(int nbPoints, int nbMaxEdgesPerNode, const void* neighbourhoodPoints, void* contribEdge, int nbEdges, void* df, const void* dx, const void* edgeInfo, float kFactor );
+#ifdef SOFA_GPU_CUDA_DOUBLE
+void TetrahedralTensorMassForceFieldCuda3d_addForce(int nbPoints, int nbMaxEdgesPerNode, const void* neighbourhoodPoints, void* contribEdge, int nbEdges, void* f, const void* x, const void* initialPoints, const void* edgeInfo );
+void TetrahedralTensorMassForceFieldCuda3d_addDForce(int nbPoints, int nbMaxEdgesPerNode, const void* neighbourhoodPoints, void* contribEdge, int nbEdges, void* df, const void* dx, const void* edgeInfo, double kFactor );
+#endif
+}
+
+//////////////////////
+// GPU-side methods //
+//////////////////////
+
+template <class real>
+__global__ void TetrahedralTensorMassForceFieldCuda_addForce_EdgeContribution_kernel(int size, real* contribEdge, const real* x, const real* initialPoints, const real* edgeInfo)
+{
+    
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if(i < size)
+    {
+        //v0=_topology->getEdge(i)[0];
+        int v0 = (int) edgeInfo[i*EDGE_SIZE+V0];
+        //v1=_topology->getEdge(i)[1];
+        int v1 = (int) edgeInfo[i*EDGE_SIZE+V1];
+
+        real dp0[3];
+        real dp1[3];
+        real dp[3];
+
+        //dp0=x[v0]-_initialPoints[v0];
+        dp0[0] = x[v0*3] - initialPoints[v0*3];
+        dp0[1] = x[v0*3+1] - initialPoints[v0*3+1];
+        dp0[2] = x[v0*3+2] - initialPoints[v0*3+2];
+
+        //dp1=x[v1]-_initialPoints[v1];
+        dp1[0] = x[v1*3] - initialPoints[v1*3];
+        dp1[1] = x[v1*3+1] - initialPoints[v1*3+1];
+        dp1[2] = x[v1*3+2] - initialPoints[v1*3+2];
+
+        //dp = dp1-dp0;
+        dp[0] = dp1[0] - dp0[0];
+        dp[1] = dp1[1] - dp0[1];
+        dp[2] = dp1[2] - dp0[2];
+
+        const real* matrix = &edgeInfo[i*EDGE_SIZE+DFDX];
+
+        //contribEdge[i][0]= -(einfo->DfDx.transposeMultiply(dp));
+        contribEdge[i*6] = - (matrix[0]*dp[0] + matrix[3]*dp[1] + matrix[6]*dp[2]) ;
+        contribEdge[i*6+1] = - (matrix[1]*dp[0] + matrix[4]*dp[1] + matrix[7]*dp[2]) ;
+        contribEdge[i*6+2] = - (matrix[2]*dp[0] + matrix[5]*dp[1] + matrix[8]*dp[2]) ;
+
+        //contribEdge[i][1]=einfo->DfDx*dp;
+        contribEdge[i*6+3] = (matrix[0]*dp[0] + matrix[1]*dp[1] + matrix[2]*dp[2]);
+        contribEdge[i*6+4] = (matrix[3]*dp[0] + matrix[4]*dp[1] + matrix[5]*dp[2]);
+        contribEdge[i*6+5] = (matrix[6]*dp[0] + matrix[7]*dp[1] + matrix[8]*dp[2]);
+		
+   }
+
+}
+
+template <class real>
+__global__ void TetrahedralTensorMassForceFieldCuda_addDForce_EdgeContribution_kernel(int size, real* contribEdge, const real* dx, const real* edgeInfo, real kFactor)
+{
+    /*	unsigned int v0,v1;
+	EdgeRestInformation *einfo;
+	Deriv force;
+	Coord dp0,dp1,dp;
+
+	for(int i=0; i<nbEdges; i++ )
+	{
+		einfo=&edgeInf[i];
+		v0=_topology->getEdge(i)[0];
+		v1=_topology->getEdge(i)[1];
+		dp0=dx[v0];
+		dp1=dx[v1];
+		dp = dp1-dp0;
+       
+		df[v1]+= (einfo->DfDx*dp) * kFactor;
+		df[v0]-= (einfo->DfDx.transposeMultiply(dp)) * kFactor;
+	}
+*/
+
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if(i < size)
+    {
+		//v0=_topology->getEdge(i)[0];
+        int v0 = (int) edgeInfo[i*EDGE_SIZE+V0];
+        //v1=_topology->getEdge(i)[1];
+        int v1 = (int) edgeInfo[i*EDGE_SIZE+V1];
+
+        real dp0[3];
+        real dp1[3];
+        real dp[3];
+
+        //dp0=dx[v0];
+        dp0[0] = dx[v0*3];
+        dp0[1] = dx[v0*3+1];
+        dp0[2] = dx[v0*3+2];
+
+        //dp1=dx[v1];
+        dp1[0] = dx[v1*3];
+        dp1[1] = dx[v1*3+1];
+        dp1[2] = dx[v1*3+2];
+
+        //dp = dp1-dp0;
+        dp[0] = dp1[0] - dp0[0];
+        dp[1] = dp1[1] - dp0[1];
+        dp[2] = dp1[2] - dp0[2];
+
+        const real* matrix = &edgeInfo[i*EDGE_SIZE+DFDX];
+
+		//contribEdge[i] = -(einfo->DfDx.transposeMultiply(dp)) * kFactor;
+        real transposeMultiplied[3];
+        transposeMultiplied[0] = matrix[0]*dp[0] + matrix[3]*dp[1] + matrix[6]*dp[2];
+        transposeMultiplied[1] = matrix[1]*dp[0] + matrix[4]*dp[1] + matrix[7]*dp[2];
+        transposeMultiplied[2] = matrix[2]*dp[0] + matrix[5]*dp[1] + matrix[8]*dp[2];
+
+        contribEdge[i*6] = - (transposeMultiplied[0]*kFactor);
+        contribEdge[i*6+1] = - (transposeMultiplied[1]*kFactor) ;
+        contribEdge[i*6+2] =  - (transposeMultiplied[2]*kFactor);
+
+		//contribEdge[i] = (einfo->DfDx*dp) * kFactor;
+        real multiplied[3];
+
+        multiplied[0] = matrix[0]*dp[0] + matrix[1]*dp[1] + matrix[2]*dp[2];
+        multiplied[1] = matrix[3]*dp[0] + matrix[4]*dp[1] + matrix[5]*dp[2];
+        multiplied[2] = matrix[6]*dp[0] + matrix[7]*dp[1] + matrix[8]*dp[2];
+
+        contribEdge[i*6+3] = (multiplied[0]*kFactor);
+        contribEdge[i*6+4] = (multiplied[1]*kFactor);
+        contribEdge[i*6+5] = (multiplied[2]*kFactor);
+   }
+
+}
+
+template <class real>
+__global__ void TetrahedralTensorMassForceFieldCuda_MemoryAlignment_PointsAccumulation_kernel(int nbPoints, int nbMaxEdgesPerNode, const real* contribEdge, const int* neighbourhoodPoints, real* f)
+{
+    int threadIndex = threadIdx.x + blockIdx.x * blockDim.x;
+    int forceIndex = threadIndex / 3;
+    int offset = threadIndex % 3;
+
+    if(forceIndex < nbPoints)
+    {
+        int indexNeighbourhood = forceIndex * nbMaxEdgesPerNode;
+        real acc = 0;
+
+        for(int j=0; j<nbMaxEdgesPerNode; j++)
+        {
+            int indexEdge = neighbourhoodPoints[indexNeighbourhood+j];
+            if(indexEdge == -1)
+                break;
+            else
+            {
+                acc += contribEdge[indexEdge*3+offset];
+            }
+        }
+
+        f[threadIndex] += acc;
+    }
+}
+
+template <class real>
+__global__ void TetrahedralTensorMassForceFieldCuda_PointsAccumulation_kernel(int nbPoints, int nbMaxEdgesPerNode, const real* contribEdge, const int* neighbourhoodPoints, real* f)
+{
+
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    if(i < nbPoints)
+    {
+        int indexNeighbourhood = i*nbMaxEdgesPerNode;
+        real acc[3];
+        acc[0] = 0; acc[1] = 0; acc[2] = 0;
+
+        for(int j=0; j<nbMaxEdgesPerNode; j++)
+        {
+            int indexEdge = neighbourhoodPoints[indexNeighbourhood+j];
+            if(indexEdge == -1)
+                break;
+            else
+            {
+                acc[0] += contribEdge[indexEdge*3];
+                acc[1] += contribEdge[indexEdge*3+1];
+                acc[2] += contribEdge[indexEdge*3+2];
+            }
+        }
+
+        f[i*3] += acc[0];
+        f[i*3+1] += acc[1];
+        f[i*3+2] += acc[2];
+    }
+    
+}
+
+//////////////////////
+// CPU-side methods //
+//////////////////////
+
+void TetrahedralTensorMassForceFieldCuda3f_addForce(int nbPoints, int nbMaxEdgesPerNode, const void* neighbourhoodPoints, void* contribEdge, int nbEdges, void* f, const void* x, const void* initialPoints, const void* edgeInfo )
+{
+    dim3 threads(BSIZE,1);
+    dim3 grid((nbEdges+BSIZE-1)/BSIZE,1);
+
+    TetrahedralTensorMassForceFieldCuda_addForce_EdgeContribution_kernel<float><<<grid, threads>>>(nbEdges, (float*)contribEdge, (const float*) x, (const float*) initialPoints, (const float*) edgeInfo);
+    mycudaDebugError("TetrahedralTensorMassForceField_addForce_GPUGEMS_EdgeConstribution_kernel<float>");
+
+    dim3 threads2(BSIZE);
+    dim3 grid2((nbPoints*3+BSIZE-1)/BSIZE,1);
+
+    TetrahedralTensorMassForceFieldCuda_MemoryAlignment_PointsAccumulation_kernel<float><<<grid2, threads2>>>(nbPoints, nbMaxEdgesPerNode, (const float*) contribEdge, (const int*) neighbourhoodPoints, (float*) f);
+    mycudaDebugError("TetrahedralTensorMassForceFieldCuda_addForce_GPUGEMS_PointsAccumulation_kernel<float>");
+
+    if(TIMING)
+        cudaDeviceSynchronize();
+}
+
+void TetrahedralTensorMassForceFieldCuda3f_addDForce(int nbPoints, int nbMaxEdgesPerNode, const void* neighbourhoodPoints, void* contribEdge, int nbEdges, void* df, const void* dx,  const void* edgeInfo, float kFactor)
+{
+    dim3 threads(BSIZE,1);
+    dim3 grid((nbEdges+BSIZE-1)/BSIZE,1);
+    
+    TetrahedralTensorMassForceFieldCuda_addDForce_EdgeContribution_kernel<float><<<grid, threads>>>(nbEdges, (float*)contribEdge, (const float*) dx, (const float*) edgeInfo, kFactor);
+    mycudaDebugError("TetrahedralTensorMassForceField_addForce_GPUGEMS_EdgeConstribution_kernel<float>");
+
+    dim3 threads2(BSIZE);
+    dim3 grid2((nbPoints*3+BSIZE-1)/BSIZE,1);
+
+    TetrahedralTensorMassForceFieldCuda_MemoryAlignment_PointsAccumulation_kernel<float><<<grid2, threads2>>>(nbPoints, nbMaxEdgesPerNode, (const float*) contribEdge, (const int*) neighbourhoodPoints, (float*) df);
+    mycudaDebugError("TetrahedralTensorMassForceFieldCuda_addForce_GPUGEMS_PointsAccumulation_kernel<float,1>");
+    
+    if(TIMING)
+        cudaDeviceSynchronize();
+}
+
+
+#ifdef SOFA_GPU_CUDA_DOUBLE
+
+void TetrahedralTensorMassForceFieldCuda3d_addForce(int nbPoints, int nbMaxEdgesPerNode, const void* neighbourhoodPoints, void* contribEdge, int nbEdges, void* f, const void* x, const void* initialPoints, const void* edgeInfo )
+{
+    dim3 threads(BSIZE,1);
+    dim3 grid((nbEdges+BSIZE-1)/BSIZE,1);
+
+	TetrahedralTensorMassForceFieldCuda_addForce_EdgeContribution_kernel<double><<<grid, threads>>>(nbEdges, (double*)contribEdge, (const double*) x, (const double*) initialPoints, (double*) edgeInfo);
+    mycudaDebugError("TetrahedralTensorMassForceField_addForce_GPUGEMS_EdgeConstribution_kernel<double>");
+
+    dim3 threads2(BSIZE);
+    dim3 grid2((nbPoints*3+BSIZE-1)/BSIZE,1);
+
+    TetrahedralTensorMassForceFieldCuda_MemoryAlignment_PointsAccumulation_kernel<double><<<grid2, threads2>>>(nbPoints, nbMaxEdgesPerNode, (const double*) contribEdge, (const int*) neighbourhoodPoints, (double*) f);
+    mycudaDebugError("TetrahedralTensorMassForceFieldCuda_addForce_GPUGEMS_PointsAccumulation_kernel<double>");
+
+    if(TIMING)
+        cudaDeviceSynchronize();
+}
+
+void TetrahedralTensorMassForceFieldCuda3d_addDForce(int nbPoints, int nbMaxEdgesPerNode, const void* neighbourhoodPoints, void* contribEdge, int nbEdges, void* df, const void* dx,  const void* edgeInfo, double kFactor)
+{
+    dim3 threads(BSIZE,1);
+    dim3 grid((nbEdges+BSIZE-1)/BSIZE,1);
+    
+    TetrahedralTensorMassForceFieldCuda_addDForce_EdgeContribution_kernel<double><<<grid, threads>>>(nbEdges, (double*)contribEdge, (const double*) dx, (const double*) edgeInfo, kFactor);
+    mycudaDebugError("TetrahedralTensorMassForceField_addForce_GPUGEMS_EdgeConstribution_kernel<double>");
+
+    dim3 threads2(BSIZE);
+    dim3 grid2((nbPoints*3+BSIZE-1)/BSIZE,1);
+
+    TetrahedralTensorMassForceFieldCuda_MemoryAlignment_PointsAccumulation_kernel<double><<<grid2, threads2>>>(nbPoints, nbMaxEdgesPerNode, (const double*) contribEdge, (const int*) neighbourhoodPoints, (double*) df);
+    mycudaDebugError("TetrahedralTensorMassForceFieldCuda_addForce_GPUGEMS_PointsAccumulation_kernel<double,1>");
+    
+    if(TIMING)
+        cudaDeviceSynchronize();
+}
+#endif
+
+
+#if defined(__cplusplus) && CUDA_VERSION < 2000
+} // namespace cuda
+} // namespace gpu
+} // namespace sofa
+#endif
+
+
---
+++ applications/plugins/SofaCUDA/sofa/gpu/cuda/CudaTetrahedralTensorMassForceField.inl
@@ -0,0 +1,227 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef SOFA_GPU_CUDA_CUDATETRAHEDRALTENSORMASSFORCEFIELD_INL
+#define SOFA_GPU_CUDA_CUDATETRAHEDRALTENSORMASSFORCEFIELD_INL
+
+#include <sofa/gpu/cuda/CudaTetrahedralTensorMassForceField.h>
+#include <sofa/component/forcefield/TetrahedralTensorMassForceField.inl>
+
+namespace sofa
+{
+namespace gpu
+{
+namespace cuda
+{
+    extern "C"
+    {
+        void TetrahedralTensorMassForceFieldCuda3f_addForce(int nbPoints, int nbMaxEdgesPerNode, const void* neighbourhoodPoints, void* contribEdge, int nbEdges, void* f, const void* x, const void* initialPoints, const void* edgeInfo );
+        void TetrahedralTensorMassForceFieldCuda3f_addDForce(int nbPoints, int nbMaxEdgesPerNode, const void* neighbourhoodPoints, void* contribEdge, int nbEdges, void* df, const void* dx, const void* edgeInfo, float kFactor );
+#ifdef SOFA_GPU_CUDA_DOUBLE
+        void TetrahedralTensorMassForceFieldCuda3d_addForce(int nbPoints, int nbMaxEdgesPerNode, const void* neighbourhoodPoints, void* contribEdge, int nbEdges, void* f, const void* x, const void* initialPoints, const void* edgeInfo );
+        void TetrahedralTensorMassForceFieldCuda3d_addDForce(int nbPoints, int nbMaxEdgesPerNode, const void* neighbourhoodPoints, void* contribEdge, int nbEdges, void* df, const void* dx, const void* edgeInfo, double kFactor );
+#endif
+    }
+} // namespace cuda
+} // namespace gpu
+
+namespace component
+{
+
+namespace forcefield
+{
+
+using namespace gpu::cuda;
+
+    template <>
+    void TetrahedralTensorMassForceField<gpu::cuda::CudaVec3fTypes>::addForce(const core::MechanicalParams* /*mparams*/ /* PARAMS FIRST */, DataVecDeriv& d_f, const DataVecCoord& d_x, const DataVecDeriv& /*d_v*/)
+    {
+		sofa::helper::AdvancedTimer::stepBegin("addForceTetraTensorMass");
+
+        VecDeriv& f = *d_f.beginEdit();
+        const VecCoord& x = d_x.getValue();
+
+        int nbEdges=_topology->getNbEdges();
+        int nbPoints=_topology->getNbPoints();
+
+        edgeRestInfoVector& edgeInf = *(edgeInfo.beginEdit());
+
+        TetrahedralTensorMassForceField_contribEdge.resize(6*nbEdges);
+        TetrahedralTensorMassForceFieldCuda3f_addForce(nbPoints, TetrahedralTensorMassForceField_nbMaxEdgesPerNode, TetrahedralTensorMassForceField_neighbourhoodPoints.deviceRead(), TetrahedralTensorMassForceField_contribEdge.deviceWrite(), nbEdges,  f.deviceWrite(), x.deviceRead(), _initialPoints.deviceRead(), edgeInf.deviceRead());
+
+        edgeInfo.endEdit();
+        d_f.endEdit();
+		sofa::helper::AdvancedTimer::stepEnd("addForceTetraTensorMass");
+    }
+
+    template <>
+    void TetrahedralTensorMassForceField<gpu::cuda::CudaVec3fTypes>::addDForce(const core::MechanicalParams* mparams /* PARAMS FIRST */, DataVecDeriv& d_df, const DataVecDeriv& d_dx)
+    {
+		sofa::helper::AdvancedTimer::stepBegin("addDForceTetraTensorMass");
+
+        VecDeriv& df = *d_df.beginEdit();
+        const VecDeriv& dx = d_dx.getValue();
+        double kFactor = mparams->kFactor();
+
+        int nbEdges=_topology->getNbEdges();
+        int nbPoints=_topology->getNbPoints();
+        edgeRestInfoVector& edgeInf = *(edgeInfo.beginEdit());
+
+        TetrahedralTensorMassForceField_contribEdge.resize(6*nbEdges);
+        TetrahedralTensorMassForceFieldCuda3f_addDForce(nbPoints, TetrahedralTensorMassForceField_nbMaxEdgesPerNode, TetrahedralTensorMassForceField_neighbourhoodPoints.deviceRead(), TetrahedralTensorMassForceField_contribEdge.deviceWrite(), nbEdges,  df.deviceWrite(), dx.deviceRead(), edgeInf.deviceRead(), (float)kFactor);
+
+        edgeInfo.endEdit();
+        d_df.endEdit();
+
+        sofa::helper::AdvancedTimer::stepEnd("addDForceTetraTensorMass");
+    }
+
+
+    template<>
+    void TetrahedralTensorMassForceField<CudaVec3fTypes>::initNeighbourhoodPoints()
+    {
+        std::cout<<"(TetrahedralTensorMassForceField) GPU-GEMS activated"<<std::endl;
+
+        /// Initialize the number max of edges per node
+        TetrahedralTensorMassForceField_nbMaxEdgesPerNode = 0;
+
+        /// Compute it
+        for(int i=0;i<_topology->getNbPoints();++i)
+        {
+            if((int)_topology->getEdgesAroundVertex(i).size()>TetrahedralTensorMassForceField_nbMaxEdgesPerNode)
+                TetrahedralTensorMassForceField_nbMaxEdgesPerNode = _topology->getEdgesAroundVertex(i).size();
+        }
+
+        /// Initialize the vector neighbourhoodPoints
+        TetrahedralTensorMassForceField_neighbourhoodPoints.resize((_topology->getNbPoints())*TetrahedralTensorMassForceField_nbMaxEdgesPerNode);
+
+        unsigned int edgeID;
+
+        for (int i=0;i<_topology->getNbPoints();++i)
+        {
+            for(int j=0;j<TetrahedralTensorMassForceField_nbMaxEdgesPerNode;++j)
+            {
+                if(j>(int)_topology->getEdgesAroundVertex(i).size()-1)
+                    TetrahedralTensorMassForceField_neighbourhoodPoints[i*TetrahedralTensorMassForceField_nbMaxEdgesPerNode+j] = -1;
+                else
+                {
+                    edgeID = _topology->getEdgesAroundVertex(i)[j];
+                    if(i == (int)_topology->getEdge(edgeID)[0])
+                        TetrahedralTensorMassForceField_neighbourhoodPoints[i*TetrahedralTensorMassForceField_nbMaxEdgesPerNode+j] = 2*edgeID;   //v0
+                    else
+                        TetrahedralTensorMassForceField_neighbourhoodPoints[i*TetrahedralTensorMassForceField_nbMaxEdgesPerNode+j] = 2*edgeID+1; //v1
+                }
+            }
+        }
+
+
+    }
+
+
+#ifdef SOFA_GPU_CUDA_DOUBLE
+    template <>
+    void TetrahedralTensorMassForceField<gpu::cuda::CudaVec3dTypes>::addForce(const core::MechanicalParams* /*mparams*/ /* PARAMS FIRST */, DataVecDeriv& d_f, const DataVecCoord& d_x, const DataVecDeriv& /*d_v*/)
+    {
+        VecDeriv& f = *d_f.beginEdit();
+        const VecCoord& x = d_x.getValue();
+
+        int nbEdges=_topology->getNbEdges();
+        int nbPoints=_topology->getNbPoints();
+
+        edgeRestInfoVector& edgeInf = *(edgeInfo.beginEdit());
+
+        TetrahedralTensorMassForceField_contribEdge.resize(6*nbEdges);
+        TetrahedralTensorMassForceFieldCuda3d_addForce(nbPoints, TetrahedralTensorMassForceField_nbMaxEdgesPerNode, TetrahedralTensorMassForceField_neighbourhoodPoints.deviceRead(), TetrahedralTensorMassForceField_contribEdge.deviceWrite(), nbEdges,  f.deviceWrite(), x.deviceRead(), _initialPoints.deviceRead(), edgeInf.deviceRead());
+
+        edgeInfo.endEdit();
+        d_f.endEdit();
+
+
+    }
+
+    template <>
+    void TetrahedralTensorMassForceField<gpu::cuda::CudaVec3dTypes>::addDForce(const core::MechanicalParams* mparams /* PARAMS FIRST */, DataVecDeriv& d_df, const DataVecDeriv& d_dx)
+    {
+        VecDeriv& df = *d_df.beginEdit();
+        const VecDeriv& dx = d_dx.getValue();
+        double kFactor = mparams->kFactor();
+
+        int nbEdges=_topology->getNbEdges();
+        int nbPoints=_topology->getNbPoints();
+        edgeRestInfoVector& edgeInf = *(edgeInfo.beginEdit());
+
+        TetrahedralTensorMassForceField_contribEdge.resize(6*nbEdges);
+        TetrahedralTensorMassForceFieldCuda3d_addDForce(nbPoints, TetrahedralTensorMassForceField_nbMaxEdgesPerNode, TetrahedralTensorMassForceField_neighbourhoodPoints.deviceRead(), TetrahedralTensorMassForceField_contribEdge.deviceWrite(), nbEdges,  df.deviceWrite(), dx.deviceRead(), edgeInf.deviceRead(), kFactor);
+
+        edgeInfo.endEdit();
+        d_df.endEdit();
+    }
+
+	template<>
+	void TetrahedralTensorMassForceField<CudaVec3dTypes>::initNeighbourhoodPoints()
+	{
+		std::cout<<"(TetrahedralTensorMassForceField) GPU-GEMS activated"<<std::endl;
+
+		/// Initialize the number max of edges per node
+		TetrahedralTensorMassForceField_nbMaxEdgesPerNode = 0;
+
+		/// Compute it
+		for(int i=0;i<_topology->getNbPoints();++i)
+		{
+			if((int)_topology->getEdgesAroundVertex(i).size()>TetrahedralTensorMassForceField_nbMaxEdgesPerNode)
+				TetrahedralTensorMassForceField_nbMaxEdgesPerNode = _topology->getEdgesAroundVertex(i).size();
+		}
+
+		/// Initialize the vector neighbourhoodPoints
+		TetrahedralTensorMassForceField_neighbourhoodPoints.resize((_topology->getNbPoints())*TetrahedralTensorMassForceField_nbMaxEdgesPerNode);
+
+		unsigned int edgeID;
+
+		for (int i=0;i<_topology->getNbPoints();++i)
+		{
+			for(int j=0;j<TetrahedralTensorMassForceField_nbMaxEdgesPerNode;++j)
+			{
+				if(j>(int)_topology->getEdgesAroundVertex(i).size()-1)
+					TetrahedralTensorMassForceField_neighbourhoodPoints[i*TetrahedralTensorMassForceField_nbMaxEdgesPerNode+j] = -1;
+				else
+				{
+					edgeID = _topology->getEdgesAroundVertex(i)[j];
+					if(i == _topology->getEdge(edgeID)[0])
+						TetrahedralTensorMassForceField_neighbourhoodPoints[i*TetrahedralTensorMassForceField_nbMaxEdgesPerNode+j] = 2*edgeID;   //v0
+					else
+						TetrahedralTensorMassForceField_neighbourhoodPoints[i*TetrahedralTensorMassForceField_nbMaxEdgesPerNode+j] = 2*edgeID+1; //v1
+				}
+			}
+		}
+
+
+	}
+
+#endif
+
+
+} // namespace forcefield
+} // namespace component
+} // namespace sofa
+#endif //SOFA_GPU_CUDA_CUDATETRAHEDRALTENSORMASSFORCEFIELD_INL
