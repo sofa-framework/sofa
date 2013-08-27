2b7485842e070a7492e6df31b3731305be646d61
cguerin <cguerin@users.gforge.inria.fr>
2013-08-27 14:56:35 +0000

    r9894/sofa : FIX: SOFA_FLOAT build.
    Former-commit-id: a481d1cfcc92bc38443e21880ae63de167f6f6ba

--- modules/sofa/component/forcefield/StandardTetrahedralFEMForceField.h
+++ modules/sofa/component/forcefield/StandardTetrahedralFEMForceField.h
@@ -238,8 +238,12 @@ public:
     
 };
 
+#ifndef SOFA_FLOAT
 using sofa::defaulttype::Vec3dTypes;
+#endif
+#ifndef SOFA_DOUBLE
 using sofa::defaulttype::Vec3fTypes;
+#endif
 
 #if defined(SOFA_EXTERN_TEMPLATE) && !defined(SOFA_COMPONENT_FORCEFIELD_STANDARDTETRAHEDRALFEMFORCEFIELD_CPP)
 
