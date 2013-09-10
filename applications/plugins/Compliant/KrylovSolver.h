a532a56d31c9e8e9b81bbefaefd48ac64c0aae57
Maxime Tournier <mtournie@users.gforge.inria.fr>
2013-09-10 12:25:32 +0000

    r9955/sofa : Compliant: moved KKT functor outside MinresSolver for reuse in CompliantDev, added parallel Minres solves
    Former-commit-id: d712bf37d384e93430487265b5eb15861738204a

--- applications/plugins/Compliant/KrylovSolver.h
+++ applications/plugins/Compliant/KrylovSolver.h
@@ -6,28 +6,18 @@
 #include "AssembledSystem.h"
 #include <sofa/core/objectmodel/BaseObject.h>
 
-
-#include "utils/minres.h"
-#include "utils/cg.h"
-
-
 namespace sofa {
 namespace component {
 namespace linearsolver {
 			
-// Solver for an AssembledSystem (could be non-linear in case of
-// inequalities). This will eventually serve as a base class for
-// all kinds of derived solver (sparse cholesky, minres, qp)
-			
-// TODO: base + derived classes (minres/cholesky/unilateral)
-template<class NumericalSolver>
-class SOFA_Compliant_API KrylovSolver : public KKTSolver {
-  public:
-    SOFA_ABSTRACT_CLASS(SOFA_TEMPLATE(KrylovSolver,NumericalSolver), KKTSolver);
-	
-    KrylovSolver();
 
-    typedef AssembledSystem::vec vec;
+class SOFA_Compliant_API MinresSolver : public KKTSolver {
+  public:
+	SOFA_CLASS(MinresSolver, KKTSolver);
+	
+	MinresSolver();				
+	
+	typedef AssembledSystem::vec vec;
 
 	// solve the KKT system: \mat{ M - h^2 K & J^T \\ J, -C } x = rhs
 	// (watch out for the compliance scaling)
@@ -37,24 +27,23 @@ class SOFA_Compliant_API KrylovSolver : public KKTSolver {
 
 	virtual void factor(const AssembledSystem& system);
 				
+  private:
+	virtual void solve_schur(vec& x,
+	                         const AssembledSystem& system,
+	                         const vec& rhs) const;
+	virtual void solve_kkt(vec& x,
+	                       const AssembledSystem& system,
+	                       const vec& rhs) const;
+	
+	
   public:
 	Data<SReal> precision;
 	Data<unsigned> iterations;
 	Data<bool> relative;
 
+	Data<bool> use_schur, fast_schur, parallel;
+	
 	Data<bool> verbose;
-};
-
-class SOFA_Compliant_API MinresSolver : public KrylovSolver< ::minres<SReal> >
-{
-public:
-  SOFA_CLASS(MinresSolver,SOFA_TEMPLATE(KrylovSolver,::minres<SReal>));
-};
-
-class SOFA_Compliant_API CgSolver : public KrylovSolver< ::cg<SReal> >
-{
-public:
-  SOFA_CLASS(CgSolver,SOFA_TEMPLATE(KrylovSolver,::cg<SReal>));
 };
 
 
