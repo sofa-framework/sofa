cbf053d06c7cd2a6a0d774fa709bf6ad2f32e1b1
Maxime Tournier <mtournie@users.gforge.inria.fr>
2013-09-10 12:25:34 +0000

    r9956/sofa : Compliant: implementing Schur solves in MinresSolver
    Former-commit-id: c61e28a5d080df6122175c29f9b5fb20ccc9635d

--- applications/plugins/Compliant/KrylovSolver.h
+++ applications/plugins/Compliant/KrylovSolver.h
@@ -1,10 +1,12 @@
-#ifndef MINRESSOLVER_H
-#define MINRESSOLVER_H
+#ifndef COMPLIANT_MINRESSOLVER_H
+#define COMPLIANT_MINRESSOLVER_H
 
 #include "KKTSolver.h"
 
 #include "AssembledSystem.h"
 #include <sofa/core/objectmodel/BaseObject.h>
+
+#include <Eigen/SparseCholesky>
 
 namespace sofa {
 namespace component {
@@ -17,22 +19,23 @@ class SOFA_Compliant_API MinresSolver : public KKTSolver {
 	
 	MinresSolver();				
 	
-	typedef AssembledSystem::vec vec;
+	typedef AssembledSystem system_type;
+	typedef system_type::vec vec;
 
 	// solve the KKT system: \mat{ M - h^2 K & J^T \\ J, -C } x = rhs
 	// (watch out for the compliance scaling)
 	virtual void solve(vec& x,
-	                   const AssembledSystem& system,
+	                   const system_type& system,
 	                   const vec& rhs) const;
 
 	virtual void factor(const AssembledSystem& system);
 				
   private:
 	virtual void solve_schur(vec& x,
-	                         const AssembledSystem& system,
+	                         const system_type& system,
 	                         const vec& rhs) const;
 	virtual void solve_kkt(vec& x,
-	                       const AssembledSystem& system,
+	                       const system_type& system,
 	                       const vec& rhs) const;
 	
 	
@@ -44,6 +47,15 @@ class SOFA_Compliant_API MinresSolver : public KKTSolver {
 	Data<bool> use_schur, fast_schur, parallel;
 	
 	Data<bool> verbose;
+
+  protected:
+	
+	typedef system_type::real real;
+	typedef Eigen::SparseMatrix<real, Eigen::ColMajor> cmat;
+	typedef Eigen::SimplicialLDLT< cmat > response_type;
+	
+	// response matrix for schur solves
+	response_type response;
 };
 
 
