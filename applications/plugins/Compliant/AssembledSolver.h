eb4eaa1912dfffc1278a5d9754dc0c1b3519d60b
Matthieu Nesme <matthieu.nesme@inria.fr>
2013-10-09 12:21:36 +0000

    r10009/sofa : Compliant: AssembledSolver: added implicit factors (alpha & beta) when the system is solved in acceleration (not yet relevant in velocity)
    Former-commit-id: 9badf251658f063df21e940b41b960c4bec36541

--- applications/plugins/Compliant/AssembledSolver.h
+++ applications/plugins/Compliant/AssembledSolver.h
@@ -25,12 +25,52 @@ struct AssembledSystem;
 }
 
 
-
 namespace odesolver {
 			
-// simple example compliance solver using system assembly,
-// integration using velocity implicit euler. needs a KKTSolver
-// at the same level in the scene graph.
+
+
+/** Solver using a regularized KKT matrix.
+  The compliance values are used to regularize the system.
+
+ simple example compliance solver using system assembly,
+ integration using velocity implicit euler. needs a KKTSolver
+ at the same level in the scene graph.
+
+
+  Inspired from Servin,Lacoursiere,Melin, Interactive Simulation of Elastic Deformable Materials,  http://www.ep.liu.se/ecp/019/005/ecp01905.pdf
+
+  We generalize it to a tunable implicit integration scheme:
+      \f[ \begin{array}{ccc}
+    \Delta v &=& h.M^{-1}.(\alpha f_{n+1} + (1-\alpha) f_n)  \\
+    \Delta x &=& h.(\beta v_{n+1} + (1-\beta) v_n)
+    \end{array} \f]
+    where \f$ h \f$ is the time step, \f$ \alpha \f$ is the implicit velocity factor, and \f$ \beta \f$ is the implicit position factor.
+
+    The corresponding dynamic equation is:
+  \f[ \left( \begin{array}{cc} \frac{1}{h} PM & -PJ^T \\
+                               J & \frac{1}{l} C \end{array}\right)
+      \left( \begin{array}{c} \Delta v \\ \bar\lambda \end{array}\right)
+    = \left( \begin{array}{c} Pf \\ - \frac{1}{l} (\phi +(d+\alpha h) \dot \phi)  \end{array}\right) \f]
+    where \f$ M \f$ is the mass matrix, \f$ P \f$ is a projection matrix to impose boundary conditions on displacements (typically maintain fixed points), \f$ \phi \f$ is the constraint violation, \f$ J \f$ the constraint Jacobian matrix,
+    \f$ C \f$ is the compliance matrix (i.e. inverse of constraint stiffness) used to soften the constraints, \f$ l=\alpha(h \beta + d) \f$ is a term related to implicit integration and constraint damping, and
+      \f$ \bar\lambda \f$ is the average constraint forces, consistently with the implicit velocity integration.
+
+      The system is singular due to the projection matrix \f$ P \f$ (corresponding to the projective constraints applied to the independent DOFs), however we can use \f$ P M^{-1}P \f$ as inverse mass matrix to compute Schur complements.
+
+ In the default implementation, a Schur complement is used to compute the constraint forces, then these are added to the external forces to obtain the final velocity increment,
+  and the positions are updated according to the implicit scheme:
+
+  \f[ \begin{array}{ccc}
+   ( hJPM^{-1}PJ^T + \frac{1}{l}C ) \bar\lambda &=& -\frac{1}{l} (\phi + (d+h\alpha)\dot\phi ) - h J M^{-1} f \\
+                                 \Delta v  &=&  h P M^{-1}( f + J^T \bar\lambda ) \\
+                                 \Delta x  &=&  h( v + \beta \Delta v )
+  \end{array} \f]
+
+\sa \ref sofa::core::behavior::BaseCompliance
+
+*/
+
+
 class SOFA_Compliant_API AssembledSolver : public sofa::core::behavior::OdeSolver {
   public:
 				
@@ -78,6 +118,7 @@ class SOFA_Compliant_API AssembledSolver : public sofa::core::behavior::OdeSolve
     // solve velocity dynamics ?
 	Data<bool> use_velocity, warm_start, propagate_lambdas, stabilization, debug;
     Data<SReal> f_rayleighStiffness, f_rayleighMass;  ///< uniform Rayleigh damping ratio applied to the stiffness and mass matrices
+    Data<SReal> implicitVelocity, implicitPosition;     ///< the \f$ \alpha \f$ and \f$ \beta  \f$ parameters of the integration scheme
 
     simulation::AssemblyVisitor* _assemblyVisitor;
 
@@ -90,6 +131,8 @@ class SOFA_Compliant_API AssembledSolver : public sofa::core::behavior::OdeSolve
 				
 	// integrate positions
     void integrate( const core::MechanicalParams* params, core::MultiVecCoordId posId, core::MultiVecDerivId velId );
+    // integrate positions and velocities
+    void integrate( const core::MechanicalParams* params, core::MultiVecCoordId posId, core::MultiVecDerivId velId, core::MultiVecDerivId dvId );
 
 
 	// propagate velocities
