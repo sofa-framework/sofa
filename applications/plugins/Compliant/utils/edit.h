85e251a9a81003803caa48c8000b2f64374a8b1f
Maxime Tournier <mtournie@users.gforge.inria.fr>
2013-10-11 16:00:45 +0000

    r10024/sofa : Compliant: added addMDx to RigidMass, imported AssembledMultiMapping from CompliantDev. Started RigidJointMultiMapping.
    Former-commit-id: 49c95165f5fa5fddbc1a8664938fc5e0ceaf7fc1

---
+++ applications/plugins/Compliant/utils/edit.h
@@ -0,0 +1,31 @@
+#ifndef EDIT_H
+#define EDIT_H
+
+namespace {
+
+template<class T>
+class editor {
+	typedef	sofa::core::objectmodel::Data<T> data_type;
+	data_type& data;
+	T* ptr;												// TODO better
+public:
+	editor( data_type& data ) 
+		: data(data),
+		  ptr(data.beginEdit()) {
+	}
+
+	~editor() {
+		data.endEdit();
+	}
+		
+	T& operator*() const { return *get(); }
+	T* get() const { return ptr; }
+	T* operator->() const { return get(); }
+};
+
+template<class T>
+editor<T> edit( sofa::core::objectmodel::Data<T>& data) { return editor<T>(data); }
+
+}
+
+#endif
