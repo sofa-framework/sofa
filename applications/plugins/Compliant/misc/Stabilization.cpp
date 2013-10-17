c5cc291e9212c5f434cb40fd0edd1e844d64a7aa
Maxime Tournier <mtournie@users.gforge.inria.fr>
2013-10-17 09:12:26 +0000

    r10056/sofa : Revert "Revert "Compilant: stabilization""
    This reverts commit 34efc10147b905d5a4e62c2d42e6778e325a2946.
    
    
    Former-commit-id: ff4a3236fcbcbe5f54314df30ade9375ccb12a18

--- applications/plugins/Compliant/misc/Stabilization.cpp
+++ applications/plugins/Compliant/misc/Stabilization.cpp
@@ -10,6 +10,35 @@ namespace odesolver {
 SOFA_DECL_CLASS(Stabilization);
 int StabilizationClass = core::RegisterObject("Kinematic constraint stabilization").add< Stabilization >();
 
+
+void ConstraintValue::correction(SReal* dst, unsigned n) const {
+	assert( mstate );
+	mstate->copyToBuffer(dst, core::VecCoordId::position(), n);
+
+	// non-zero for stabilized
+	unsigned i = 0;
+	for(SReal* last = dst + n; dst < last; ++dst, ++i) {
+		if( !mask.empty() && !mask[i] ) *dst = 0;
+	}
+	
+}
+
+
+void ConstraintValue::dynamics(SReal* dst, unsigned n) const {
+	assert( mstate );
+	mstate->copyToBuffer(dst, core::VecCoordId::position(), n);
+
+	// zero for stabilized
+	unsigned i = 0;
+	for(SReal* last = dst + n; dst < last; ++dst, ++i) {
+		if( mask.empty() || mask[i] ) *dst = 0;
+	}
+
+}
+
+
+
+
 }
 }
 }
