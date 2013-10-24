71952b7c62ebea651295872fc106fbd9a818cc7d
Maxime Tournier <mtournie@users.gforge.inria.fr>
2013-10-24 16:50:32 +0000

    r10110/sofa : Compliant: examples update
    Former-commit-id: 3e309f44633ea5d6892f8ba8bdb8db5513d3238b

--- applications/plugins/Compliant/examples/chain.py
+++ applications/plugins/Compliant/examples/chain.py
@@ -1,62 +1,72 @@
 import Sofa
 
-# TODO handle this more cleanly, i.e. standardize plugins python
-# directory, then use something like Sofa.add_plugin_path('Compliant')
+# TODO standardize plugins python directory, then use something like
+# Sofa.plugin_path('Compliant')
 
 import sys
 sys.path.append( Sofa.src_dir() + '/applications/plugins/Compliant/python' )
 
 from Compliant import Rigid
 
-
 def createScene(root):
+    
+    # root node setup
     root.createObject('RequiredPlugin', name = 'Compliant')
     root.createObject('VisualStyle', displayFlags="showBehavior" )
     
-    root.dt = 0.001
+    # simuation parameters
+    root.dt = 1e-2
     root.gravity = [0, -9.8, 0]
     
+    # ode solver
     ode = root.createObject('AssembledSolver')
     ode.stabilization = True
     
+    # numerical solver
     num = root.createObject('MinresSolver')
     num.iterations = 500
     
+    # scene node
     scene = root.createChild('scene')
     
+    # script variables
     n = 10
     length = 2
-
+    
     # objects/nodes creation
     obj = []
     node = []
-
+    
     for i in xrange(n):
+	# rigid bodies
         o = Rigid.Body()
         o.name = 'link-' + str(i)
         o.dofs.translation = [0, length * i, 0]
         obj.append( o )
         # insert the object into the scene node, saves the created
-        # node
+        # node in body_node
         body_node = o.insert(scene)
         node.append( body_node )
-    
+
     # joints creation
     for i in xrange(n-1):
+	# the joint
         j = Rigid.SphericalJoint()
-
-        # joint offsets
+        
+        # joint offset definitions
         up = Rigid.Frame()
         up.translation = [0, length /2 , 0]
-
+        
         down = Rigid.Frame()
         down.translation = [0, -length /2 , 0]
-
-        # node/offset
+        
+        # append node/offset to the joint
         j.append( node[i], up ) # parent
         j.append( node[i+1], down) # child
-    
+        
         j.insert(scene)
     
-    # fix first node
+    # attach first node
     node[0].createObject('FixedConstraint', indices='0')
+    
+    # and wheee !
