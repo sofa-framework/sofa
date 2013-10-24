e6808437a6639c19fab311f93de5c6bb6e3b0ef4
Bruno Carrez <bruno.carrez@inria.fr>
2013-10-24 08:59:47 +0000

    r10100/sofa : FIX SofaSparseSolver build with clang
    Former-commit-id: acde63648f88c7ed5002ffb4ca9d2d55f5aec152

--- cmake/FindMETIS.cmake
+++ cmake/FindMETIS.cmake
@@ -1,7 +1,6 @@
 # Try to find the METIS libraries
 # METIS_FOUND - system has METIS lib
 # METIS_INCLUDE_DIR - the METIS include directory
-# METIS_LIBRARIES_DIR - Directory where the METIS libraries are located
 # METIS_LIBRARIES - the METIS libraries
 
 
@@ -14,18 +13,22 @@ if(WIN32)
 endif(WIN32)
 
 if(UNIX)
-        find_path(METIS_INCLUDE_DIR NAMES metis/metis.h)
+        find_path(METIS_INCLUDE_DIR NAMES metis.h)
+        if(NOT EXISTS "${METIS_INCLUDE_DIR}/metis.h")
+            # on some systems metis.h is located in /usr/include/metis...
+            find_path(METIS_INCLUDE_DIR NAMES metis.h PATHS /usr/include/metis /opt/local/include/metis)
+        endif()
         find_library(METIS_LIBRARIES NAMES metis)
 
         #message(STATUS "METIS Lib found: METIS_LIBRARIES=${METIS_LIBRARIES} METIS_INCLUDE_DIR=${METIS_INCLUDE_DIR}")
 
-        if(EXISTS "${METIS_INCLUDE_DIR}/metis/metis.h" AND EXISTS ${METIS_LIBRARIES})
+        if(EXISTS "${METIS_INCLUDE_DIR}/metis.h" AND EXISTS ${METIS_LIBRARIES})
                 #if (NOT DEFINED METIS_FOUND)
                         message(STATUS "METIS found: ${METIS_LIBRARIES} ${METIS_INCLUDE_DIR}")
                 #endif(NOT DEFINED METIS_FOUND)
                 set(METIS_FOUND CACHE INTERNAL TRUE)
         else()
-                message(ERROR "METIS NOT FOUND ${METIS_LIBRARIES} ${METIS_INCLUDE_DIR}")
+                message(ERROR " - METIS NOT FOUND! ${METIS_LIBRARIES} ${METIS_INCLUDE_DIR}")
                 set(METIS_FOUND FALSE)
         endif()
 
