d1083c3850f3f70505ce3e6428a4733cc58698d7
Matthieu Nesme <matthieu.nesme@inria.fr>
2013-10-25 09:36:43 +0000

    r10117/sofa : Compliant: fixing clang compil
    Former-commit-id: 544035db5e61fa57fc3e190ee490fbff4e10188c

--- applications/plugins/Compliant/utils/pair.h
+++ applications/plugins/Compliant/utils/pair.h
@@ -4,25 +4,62 @@
 #include <ostream>
 #include <istream>
 
-namespace sofa {
 
-namespace helper
+namespace sofa
 {
 
-template<class First, class Second>
-std::ostream& operator<<(std::ostream& out,
-                         const std::pair<First, Second>& p)
+namespace defaulttype
 {
-	return out << p.first << " " << p.second;
-}
+
+    template<class First, class Second>
+    struct SerializablePair
+    {
+        typedef SerializablePair<First,Second> SerializablePairType;
+        typedef std::pair<First,Second> PairType;
+
+        SerializablePair() {}
+        SerializablePair( const SerializablePairType& p ) : pair(p.pair) {}
+        SerializablePair( const PairType& p ) : pair(p) {}
 
 
-template<class First, class Second>
-std::istream& operator>>(std::istream& in,
-                         std::pair<First, Second>& p)
-{
-	return in >> p.first >> p.second;
-}
+        PairType pair;
+
+
+        inline First& first() { return pair.first; }
+        inline const First& first() const { return pair.first; }
+        inline Second& second() { return pair.second; }
+        inline const Second& second() const { return pair.second; }
+
+        inline friend std::ostream& operator<<(std::ostream& out, const SerializablePairType& p)
+        {
+            return out << p.pair.first << " " << p.pair.second;
+        }
+
+        inline friend std::ostream& operator<<(std::ostream& out, const PairType& p)
+        {
+            return out << p.first << " " << p.second;
+        }
+
+
+
+        inline friend std::istream& operator>>(std::istream& in, SerializablePairType& p)
+        {
+            return in >> p.pair.first >> p.pair.second;
+        }
+
+        inline friend std::istream& operator>>(std::istream& in, PairType& p)
+        {
+            return in >> p.first >> p.second;
+        }
+
+
+
+    };
+
+
+
+
+
 
 }
 }
