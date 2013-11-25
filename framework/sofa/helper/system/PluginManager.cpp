sofa-framework/sofa

commit add1aa0c21361d3e3eedea2c75bedbdfea068254

MERGE r9896/ISSofa by jallard : FIX linux build of SixenseHydra plugin + ADD more error messages in loadPlugin()
git-svn-id: svn://scm.gforge.inria.fr/svn/sofa/trunk/Sofa@10218 b7f1a160-662a-0410-a6d8-bea165342f16

framework/sofa/helper/system/PluginManager.cpp
@@ -139,10 +139,12 @@ bool PluginManager::loadPlugin(std::string& pluginPath, std::ostream* errlog)
 #else
         pluginPath = std::string("lib") + pluginPath + std::string(".so");
 #endif
+        //std::cout << "System-specific plugin filename: " << pluginPath << std::endl;
     }
 
     if( !PluginRepository.findFile(pluginPath,"",errlog) )
     {
+        (*errlog) << "Plugin " << pluginPath << " NOT FOUND in: " << PluginRepository << std::endl;
         return false;
     }
     if(m_pluginMap.find(pluginPath) != m_pluginMap.end() )
@@ -154,11 +156,16 @@ bool PluginManager::loadPlugin(std::string& pluginPath, std::ostream* errlog)
     Plugin p;
     if( d == NULL )
     {
+        (*errlog) << "Plugin " << pluginPath << " loading FAILED" << std::endl;
         return false;
     }
     else
     {
-        if(! getPluginEntry(p.initExternalModule,d,errlog) ) return false;
+        if(! getPluginEntry(p.initExternalModule,d,errlog) )
+        {
+            (*errlog) << "Plugin " << pluginPath << " method initExternalModule() NOT FOUND" << std::endl;
+            return false;
+        }
         getPluginEntry(p.getModuleName,d,errlog);
         getPluginEntry(p.getModuleDescription,d,errlog);
         getPluginEntry(p.getModuleLicense,d,errlog);

applications/plugins/SixenseHydra/CMakeLists.txt
@@ -25,16 +25,6 @@ set(LINKER_DEPENDENCIES SofaBaseLinearSolver SofaConstraint SofaUserInteraction 
 # add Sixense SDK
 find_package(Sixense REQUIRED)
 include_directories(${SIXENSE_INCLUDE_DIR})
-if(SIXENSE_LIBRARY_DYNAMIC)
-  set(LIBRARIES
-    ${LIBRARIES}
-    ${SIXENSE_LIBRARY_DYNAMIC} ${SIXENSE_UTILS_LIBRARY}
-    )
-else(SIXENSE_LIBRARY_DYNAMIC)
-  set(LIBRARIES
-    ${LIBRARIES}
-    ${SIXENSE_LIBRARY_STATIC} ${SIXENSE_UTILS_LIBRARY}
-    )
-endif(SIXENSE_LIBRARY_DYNAMIC)
+TARGET_LINK_LIBRARIES(SixenseHydra ${SIXENSE_LIBRARY} ${SIXENSE_UTILS_LIBRARY})
 
 include(${SOFA_CMAKE_DIR}/postProject.cmake)

applications/plugins/SixenseHydra/cmake/FindSixense.cmake
@@ -1,0 +1,77 @@
+# This script locates the Sixense SDK
+# ------------------------------------
+#
+# usage:
+# find_package(Sixense ...)
+#
+# searches in SIXENSE_ROOT and usual locations
+#
+# Sets SIXENSE_INCLUDE_DIR, SIXENSE_LIBRARY_STATIC and SIXENSE_LIBRARY_DYNAMIC
+
+set(SIXENSE_POSSIBLE_PATHS
+	${SIXENSE_ROOT}
+	$ENV{SIXENSE_ROOT}
+	"C:/Program Files/Steam/steamapps/common/sixense sdk/SixenseSDK"
+	"C:/Program Files (x86)/Steam/steamapps/common/sixense sdk/SixenseSDK"
+	~/Library/Frameworks
+	/Library/Frameworks
+	/usr/local/
+	/usr/
+	/sw # Fink
+	/opt/local/ # DarwinPorts
+	/opt/csw/ # Blastwave
+	/opt/
+    ${CMAKE_CURRENT_SOURCE_DIR}
+    $ENV{HOME} # user installation
+	)
+
+
+find_path(SIXENSE_INCLUDE_DIR sixense.h
+	PATH_SUFFIXES
+		"include"
+	PATHS
+		${SIXENSE_POSSIBLE_PATHS}
+	)
+
+find_library(SIXENSE_LIBRARY_STATIC sixense_s
+	PATH_SUFFIXES
+		"lib"
+		"lib/win32/release_static"
+	PATHS
+		${SIXENSE_POSSIBLE_PATHS}
+	)
+
+find_library(SIXENSE_LIBRARY_DYNAMIC NAMES sixense_x64 sixense
+	PATH_SUFFIXES
+		"lib"
+		"lib/win32/release_dll"
+		"lib/linux/release"
+	PATHS
+		${SIXENSE_POSSIBLE_PATHS}
+	)
+
+find_library(SIXENSE_UTILS_LIBRARY NAMES sixense_utils_x64 sixense_utils
+	PATH_SUFFIXES
+		"lib"
+		"lib/win32/release_dll"
+		"lib/linux/release"
+	PATHS
+		${SIXENSE_POSSIBLE_PATHS}
+	)
+if(NOT SIXENSE_LIBRARY_DYNAMIC)
+  set(SIXENSE_LIBRARY ${SIXENSE_LIBRARY_STATIC})
+else(NOT SIXENSE_LIBRARY_DYNAMIC)
+  set(SIXENSE_LIBRARY ${SIXENSE_LIBRARY_DYNAMIC})
+endif(NOT SIXENSE_LIBRARY_DYNAMIC)
+
+if ((NOT SIXENSE_INCLUDE_DIR) OR ((NOT SIXENSE_LIBRARY_STATIC) AND (NOT SIXENSE_LIBRARY_DYNAMIC)))
+    if(Sixense_FIND_REQUIRED) #prefix is filename, case matters
+        message(FATAL_ERROR "Could not find Sixense SDK!")
+    elseif(NOT Sixense_FIND_QUIETLY)
+        message("Could not find Sixense SDK!")
+    endif(Sixense_FIND_REQUIRED)
+endif ((NOT SIXENSE_INCLUDE_DIR) OR ((NOT SIXENSE_LIBRARY_STATIC) AND (NOT SIXENSE_LIBRARY_DYNAMIC)))
+
+if (NOT Sixense_FIND_QUIETLY)
+	message("Found Sixense SDK: Headers ${SIXENSE_INCLUDE_DIR} Dynamic lib ${SIXENSE_LIBRARY_DYNAMIC} Static lib ${SIXENSE_LIBRARY_STATIC} Utils lib ${SIXENSE_UTILS_LIBRARY}")
+endif (NOT Sixense_FIND_QUIETLY)

applications/plugins/SixenseHydra/initSixenseHydra.cpp
@@ -23,6 +23,7 @@
 * Contact information: contact@sofa-framework.org                             *
 ******************************************************************************/
 #include "initSixenseHydra.h"
+#include <sofa/core/ObjectFactory.h>
 
 namespace sofa
 {
@@ -57,7 +58,7 @@ const char* getModuleName()
 
 const char* getModuleVersion()
 {
-    return "0.2";
+    return "1.0";
 }
 
 const char* getModuleLicense()
@@ -73,7 +74,9 @@ const char* getModuleDescription()
 
 const char* getModuleComponentList()
 {
-    return "SixenseHydra";
+    /// string containing the names of the classes provided by the plugin
+    static std::string classes = sofa::core::ObjectFactory::getInstance()->listClassesFromTarget(sofa_tostring(SOFA_TARGET));
+    return classes.c_str();
 }
 
 

applications/plugins/SixenseHydra/initSixenseHydra.h
@@ -30,7 +30,7 @@
 #ifdef SOFA_BUILD_SIXENSE_HYDRA
 #define SOFA_SIXENSE_HYDRA_API SOFA_EXPORT_DYNAMIC_LIBRARY
 #else
-#define SOFA_SIXENSE_HYDRA_API  SOFA_IMPORT_DYNAMIC_LIBRARY
+#define SOFA_SIXENSE_HYDRA_API SOFA_IMPORT_DYNAMIC_LIBRARY
 #endif
 
 /** \mainpage

commit cfc072d21fea077332ed83d0bf54e6a24038f71e

r10264/sofa : FIX RequiredPlugin crash introduced in r10218 (null pointer not tested...)
Former-commit-id: 43835b817fed7b494350f4ba388075d6e3402005

framework/sofa/helper/system/PluginManager.cpp
@@ -144,7 +144,7 @@ bool PluginManager::loadPlugin(std::string& pluginPath, std::ostream* errlog)
 
     if( !PluginRepository.findFile(pluginPath,"",errlog) )
     {
-        (*errlog) << "Plugin " << pluginPath << " NOT FOUND in: " << PluginRepository << std::endl;
+        if (errlog) (*errlog) << "Plugin " << pluginPath << " NOT FOUND in: " << PluginRepository << std::endl;
         return false;
     }
     if(m_pluginMap.find(pluginPath) != m_pluginMap.end() )
@@ -156,14 +156,14 @@ bool PluginManager::loadPlugin(std::string& pluginPath, std::ostream* errlog)
     Plugin p;
     if( d == NULL )
     {
-        (*errlog) << "Plugin " << pluginPath << " loading FAILED" << std::endl;
+        if (errlog) (*errlog) << "Plugin " << pluginPath << " loading FAILED" << std::endl;
         return false;
     }
     else
     {
         if(! getPluginEntry(p.initExternalModule,d,errlog) )
         {
-            (*errlog) << "Plugin " << pluginPath << " method initExternalModule() NOT FOUND" << std::endl;
+            if (errlog) (*errlog) << "Plugin " << pluginPath << " method initExternalModule() NOT FOUND" << std::endl;
             return false;
         }
         getPluginEntry(p.getModuleName,d,errlog);