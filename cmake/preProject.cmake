0a5bb098c3332732a1c546cb7f45eba9e0eaf61d
Marc Legendre <marc.legendre@inria.fr>
2013-12-17 15:04:17 +0000

    r10386/sofa : Make CMake output simpler and cleaner
    This commit makes CMake output more bearable by removing spurious lines
    and bad formatting.
    It also makes the output for dependency resolution less verbose, unless
    the variable SOFA-MISC_CMAKE_VERBOSE is ON.
    
    
    Former-commit-id: 7ac0613dacdaff9979e46dffc8410a5425fe642f

--- cmake/preProject.cmake
+++ cmake/preProject.cmake
@@ -1,3 +1,5 @@
 include(${CMAKE_CURRENT_LIST_DIR}/preBuildConfig.cmake)
 
-message(STATUS "  > ${PROJECT_NAME} : Configuring Project")
+if(SOFA-MISC_CMAKE_VERBOSE)
+    message(STATUS "> ${PROJECT_NAME}: configuring project")
+endif()
