d681b49262e929f7294c6c367a62b2b6a3ccf153
Maxime Tournier <mtournie@users.gforge.inria.fr>
2013-12-17 04:31:42 +0000

    r10367/sofa : Compliant: added ProjectionMapping for general-purpose mapping from python side
    Former-commit-id: 1b07d0310dda5381ab0d4301a12b5703ccbe085a

---
+++ applications/plugins/Compliant/mapping/ProjectionMapping.cpp
@@ -0,0 +1,46 @@
+#include "ProjectionMapping.h"
+
+#include <sofa/core/ObjectFactory.h>
+#include <sofa/core/Mapping.inl>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace mapping
+{
+
+SOFA_DECL_CLASS(ProjectionMapping)
+
+using namespace defaulttype;
+
+// Register in the Factory
+int ProjectionMappingClass = core::RegisterObject("Projects dofs on a given vector set.")
+
+#ifndef SOFA_FLOAT
+	.add< ProjectionMapping< Vec6dTypes, Vec1dTypes > >()
+#endif
+#ifndef SOFA_DOUBLE
+.add< ProjectionMapping< Vec6fTypes, Vec1fTypes > >()
+#endif
+;
+
+#ifndef SOFA_FLOAT
+template class SOFA_Compliant_API ProjectionMapping<  Vec6dTypes, Vec1dTypes >;
+#endif
+
+#ifndef SOFA_DOUBLE
+template class SOFA_Compliant_API ProjectionMapping< Vec6fTypes, Vec1fTypes >;
+
+#endif
+
+
+
+} // namespace mapping
+
+} // namespace component
+
+} // namespace sofa
+
