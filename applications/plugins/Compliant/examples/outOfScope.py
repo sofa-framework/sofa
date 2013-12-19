bec437fbd8fb1b5f972ad05d0c7279ce721bd571
Matthieu Nesme <matthieu.nesme@inria.fr>
2013-12-19 15:46:11 +0000

    r10398/sofa : Compliant: adding an example with an out-of-scope dof
    Former-commit-id: f0845fda5536fc705853aebb48b7ec983f019043

---
+++ applications/plugins/Compliant/examples/outOfScope.py
@@ -0,0 +1,41 @@
+import Sofa
+
+import sys
+
+
+def createScene(root):
+    
+    # root node setup
+    root.createObject('RequiredPlugin', pluginName = 'Compliant')
+    root.createObject('VisualStyle', displayFlags="showBehavior" )
+    
+    # simuation parameters
+    root.dt = 1e-2
+    root.gravity = [0, -10, 0]
+    
+    
+    # scene nodes
+    inside = root.createChild('insideScope')
+    outside = root.createChild('outsideScope')
+    common = inside.createChild('common')
+    extension = common.createChild('extension')
+    
+    ode = inside.createObject('AssembledSolver')
+    ode.stabilization = True
+    num = inside.createObject('LDLTSolver')
+    insidedof = inside.createObject('MechanicalObject',name='insidedof', position="1 0 0")
+    inside.createObject('UniformMass',name='mass',totalmass='1')
+    
+    
+    outsidedof = outside.createObject('MechanicalObject',name='outsidedof', position="0 0 0")
+    outside.createObject('UniformMass',name='mass',totalmass='1')
+    
+    commondof = common.createObject('MechanicalObject',name='commondof')    
+    common.createObject('SubsetMultiMapping', template='Vec3d,Vec3d', input='@../insidedof @../../outsideScope/outsidedof', output='@commondof' , indexPairs="1 0  0 0" )	
+    outside.addChild( common )
+    
+    
+    extensiondof = extension.createObject('MechanicalObject',template="Vec1d",name='extensiondof')
+    extension.createObject('EdgeSetTopologyContainer',edges="0 1")
+    extension.createObject('UniformCompliance',template="Vec1d",compliance="0")
+    extension.createObject('ExtensionMapping',template="Vec3d,Vec1d")
