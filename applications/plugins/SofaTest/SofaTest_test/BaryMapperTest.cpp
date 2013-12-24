76671091e478d7fac37014e91ccc21ec2abbebb7
Moreno Trlin <moreno.trlin@inria.fr>
2013-12-24 11:18:15 +0000

    r10427/sofa : Compilation fix number 3
    Former-commit-id: 8eb75f80d07a01c8d75923dac11278eef08829d7

--- applications/plugins/SofaTest/SofaTest_test/BaryMapperTest.cpp
+++ applications/plugins/SofaTest/SofaTest_test/BaryMapperTest.cpp
@@ -16,7 +16,7 @@ struct BaryMapperTest  : public ::testing::Test{
 
     MeshTopology* initMesh(NodePtr & father);
 
-    bool test_inside(double alpha,double beta);
+    bool test_inside(SReal alpha,SReal beta);
     bool test_outside(int index);
     void initTriPts();
 
@@ -31,8 +31,8 @@ void BaryMapperTest::initTriPts(){
     triPts[2] = Vector3(0,1,0);
 }
 
-double tol = 1e-6;
-double tol2 = tol*tol;
+SReal tol = 1e-6;
+SReal tol2 = tol*tol;
 //BaryMapperTest::initTriPts();
 
 //Vector3 BaryMapperTest::triPts[] = {Vector3(-1,-1,0),Vector3(1,-1,0),Vector3(0,1,0)};
@@ -65,15 +65,15 @@ MeshTopology* BaryMapperTest::initMesh(NodePtr &father){
 //{
 //}
 
-bool BaryMapperTest::test_inside(double alpha,double beta){
+bool BaryMapperTest::test_inside(SReal alpha,SReal beta){
     initTriPts();
     sofa::simulation::Node::SPtr father = New<sofa::simulation::tree::GNode>();
     MeshTopology * topo = initMesh(father);
     //makeTri()
     component::mapping::BarycentricMapperMeshTopology<DataTypes, DataTypes>::SPtr mapper = sofa::core::objectmodel::New<component::mapping::BarycentricMapperMeshTopology<DataTypes, DataTypes> >(topo,(component::topology::PointSetTopologyContainer*)0x0,(helper::ParticleMask *)0x0,(helper::ParticleMask *)0x0/*model->getMeshTopology(), (topology::PointSetTopologyContainer*)NULL, &model->getMechanicalState()->forceMask, &mstate->forceMask*/);
 
-    Vector3 the_point = (1 - alpha - beta) * triPts[0] + alpha * triPts[1] + beta * triPts[2];
-    Vector3 the_point_trans = the_point + 10.0 * norm;
+    Vector3 the_point = ((SReal)(1.0) - alpha - beta) * triPts[0] + alpha * triPts[1] + beta * triPts[2];
+    Vector3 the_point_trans = the_point + (SReal)(10.0) * norm;
     mapper->createPointInTriangle( the_point_trans, 0, &triPts );
 
     VecCoord res;
@@ -91,7 +91,7 @@ bool BaryMapperTest::test_outside(int index){
     //makeTri()
     component::mapping::BarycentricMapperMeshTopology<DataTypes, DataTypes>::SPtr mapper = sofa::core::objectmodel::New<component::mapping::BarycentricMapperMeshTopology<DataTypes, DataTypes> >(topo,(component::topology::PointSetTopologyContainer*)0x0,(helper::ParticleMask *)0x0,(helper::ParticleMask *)0x0/*model->getMeshTopology(), (topology::PointSetTopologyContainer*)NULL, &model->getMechanicalState()->forceMask, &mstate->forceMask*/);
 
-    Vector3 the_point = 2.0 * triPts[index] + 10.0 * norm;
+    Vector3 the_point = (SReal)(2.0) * triPts[index] + (SReal)(10.0) * norm;
 
     mapper->createPointInTriangle( the_point, 0, &triPts );
 
@@ -102,9 +102,9 @@ bool BaryMapperTest::test_outside(int index){
     return equal(triPts[index],res[0]);
 }
 
-TEST_F(BaryMapperTest, alpha0dot3_beta0dot2 ) { ASSERT_TRUE( test_inside(0.3,0.2)); }
-TEST_F(BaryMapperTest, alpha0dot4_beta0dot6 ) { ASSERT_TRUE( test_inside(0.4,0.6)); }
-TEST_F(BaryMapperTest, alpha0_beta0 ) { ASSERT_TRUE( test_inside(0,0)); }
+TEST_F(BaryMapperTest, alpha0dot3_beta0dot2 ) { ASSERT_TRUE( test_inside((SReal)(0.3),(SReal)(0.2))); }
+TEST_F(BaryMapperTest, alpha0dot4_beta0dot6 ) { ASSERT_TRUE( test_inside((SReal)(0.4),(SReal)(0.6))); }
+TEST_F(BaryMapperTest, alpha0_beta0 ) { ASSERT_TRUE( test_inside((SReal)(0),(SReal)(0))); }
 TEST_F(BaryMapperTest, out_0 ) { ASSERT_TRUE( test_outside(0)); }
 TEST_F(BaryMapperTest, out_1 ) { ASSERT_TRUE( test_outside(1)); }
 TEST_F(BaryMapperTest, out_2 ) { ASSERT_TRUE( test_outside(2)); }
