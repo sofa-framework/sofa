1a364511be2bc5fd5e75b93eb3a54148ed0e6518
Maxime Tournier <mtournie@users.gforge.inria.fr>
2013-12-25 05:55:59 +0000

    r10431/sofa : Compliant: added proxy objects for vectors in python api
    Former-commit-id: fa2b65dc75f394b7c5f877627e99e5c194900cc9

--- applications/plugins/Compliant/python/Compliant/Vec.py
+++ applications/plugins/Compliant/python/Compliant/Vec.py
@@ -37,4 +37,30 @@ def cross(x, y):
              x[2] * y[0] - x[0] * y[2],
              x[0] * y[1] - x[1] * y[0] ]
 
+# eases long operations
+class Proxy:
+    def __init__(self, data):
+        self.data = data
+
+    def __add__(self, other):
+        return Proxy( sum(self.data, other.data) )
+
+    def __sub__(self, other):
+        return Proxy( diff(self.data, other.data) )
+
+    def __rmul__(self, scalar):
+        return Proxy( scal(scalar, self.data) )
+
+    def norm2(self): 
+        return norm2(self.data)
+
+    def norm(self): 
+        return norm(self.data)
+
+    def dot(self, other): 
+        return dot(self.data, other.data)
+
+
+    
+
 
