c8f58ea5fdd348d50ce7c458a1cb7397be390d3d
Benjamin GILLES <benjamin.gilles@inria.fr>
2014-01-02 23:09:04 +0000

    r10450/sofa : Flexible: fixed shapefunctiondiscretizer and moved customjacobianblock from modalSubspace plugin
    Former-commit-id: f0bd93a747a94e4686523212590dd8d11a3eb6f6

---
+++ applications/plugins/Flexible/deformationMapping/CustomJacobianBlock.h
@@ -0,0 +1,216 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Plugins                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef CustomJacobianBlock_H
+#define CustomJacobianBlock_H
+
+#include "../BaseJacobian.h"
+
+#define V1(type) StdVectorTypes<Vec<1,type>,Vec<1,type>,type>
+
+
+namespace sofa
+{
+
+namespace defaulttype
+{
+
+/** Template class used to implement one custom jacobian block with constant J matrix */
+template<class In, class Out>
+class CustomJacobianBlock : public BaseJacobianBlock<In,Out>
+{
+public:
+    typedef BaseJacobianBlock<In,Out> Inherit;
+    typedef typename Inherit::InCoord InCoord;
+    typedef typename Inherit::InDeriv InDeriv;
+    typedef typename Inherit::OutCoord OutCoord;
+    typedef typename Inherit::OutDeriv OutDeriv;
+    typedef typename Inherit::MatBlock MatBlock;
+    typedef typename Inherit::KBlock KBlock;
+    typedef typename Inherit::Real Real;
+
+    enum { dim = Out::spatial_dimensions };
+    typedef Vec<dim, Real> SpatialCoord;
+    typedef Mat<dim,dim,Real> MaterialToSpatial;
+
+    MatBlock J;
+    OutCoord X0;
+    static const bool constant=true;
+
+    void init(const MatBlock& _J, const SpatialCoord& SPos, const MaterialToSpatial& /*M*/)    { J=_J; X0=SPos; }
+    void addapply( OutCoord& result, const InCoord& data )    { result +=  J*data + X0; }
+    void addmult( OutDeriv& result,const InDeriv& data )    { result += J*data; }
+    void addMultTranspose( InDeriv& result, const OutDeriv& data )    { result += J.multTranspose(data); }
+    MatBlock getJ()    { return J; }
+    KBlock getK(const OutDeriv& /*childForce*/) {return KBlock();}
+    void addDForce( InDeriv& /*df*/, const InDeriv& /*dx*/,  const OutDeriv& /*childForce*/, const double& /*kfactor */) {}
+};
+
+
+template<class InReal,class OutReal>
+class CustomJacobianBlock< V1(InReal) , F331(OutReal) > :
+    public  BaseJacobianBlock< V1(InReal) , F331(OutReal) >
+{
+public:
+    typedef V1(InReal) In;
+    typedef F331(OutReal) Out;
+
+    typedef BaseJacobianBlock<In,Out> Inherit;
+    typedef typename Inherit::InCoord InCoord;
+    typedef typename Inherit::InDeriv InDeriv;
+    typedef typename Inherit::OutCoord OutCoord;
+    typedef typename Inherit::OutDeriv OutDeriv;
+    typedef typename Inherit::MatBlock MatBlock;
+    typedef typename Inherit::KBlock KBlock;
+    typedef typename Inherit::Real Real;
+
+    enum { dim = Out::spatial_dimensions };
+    enum { mdim = Out::material_dimensions };
+    typedef Vec<dim, Real> SpatialCoord;
+    typedef Mat<dim,mdim,Real> MaterialToSpatial;
+
+    MatBlock J;
+    OutCoord X0;
+    static const bool constant=true;
+
+    void init(const MatBlock& _J, const SpatialCoord& /*SPos*/, const MaterialToSpatial& M)    { J=_J; X0.getF()=M; }
+    void addapply( OutCoord& result, const InCoord& data )    { result.getVec() +=  J*data + X0.getVec(); }
+    void addmult( OutDeriv& result,const InDeriv& data )    { result.getVec() += J*data; }
+    void addMultTranspose( InDeriv& result, const OutDeriv& data )    { result += J.multTranspose(data.getVec()); }
+    MatBlock getJ()    { return J; }
+    KBlock getK(const OutDeriv& /*childForce*/) {return KBlock();}
+    void addDForce( InDeriv& /*df*/, const InDeriv& /*dx*/,  const OutDeriv& /*childForce*/, const double& /*kfactor */) {}
+};
+
+template<class InReal,class OutReal>
+class CustomJacobianBlock< V1(InReal) , F321(OutReal) > :
+    public  BaseJacobianBlock< V1(InReal) , F321(OutReal) >
+{
+public:
+    typedef V1(InReal) In;
+    typedef F321(OutReal) Out;
+
+    typedef BaseJacobianBlock<In,Out> Inherit;
+    typedef typename Inherit::InCoord InCoord;
+    typedef typename Inherit::InDeriv InDeriv;
+    typedef typename Inherit::OutCoord OutCoord;
+    typedef typename Inherit::OutDeriv OutDeriv;
+    typedef typename Inherit::MatBlock MatBlock;
+    typedef typename Inherit::KBlock KBlock;
+    typedef typename Inherit::Real Real;
+
+    enum { dim = Out::spatial_dimensions };
+    enum { mdim = Out::material_dimensions };
+    typedef Vec<dim, Real> SpatialCoord;
+    typedef Mat<dim,mdim,Real> MaterialToSpatial;
+
+    MatBlock J;
+    OutCoord X0;
+    static const bool constant=true;
+
+    void init(const MatBlock& _J, const SpatialCoord& /*SPos*/, const MaterialToSpatial& M)    { J=_J; X0.getF()=M; }
+    void addapply( OutCoord& result, const InCoord& data )    { result.getVec() +=  J*data + X0.getVec(); }
+    void addmult( OutDeriv& result,const InDeriv& data )    { result.getVec() += J*data; }
+    void addMultTranspose( InDeriv& result, const OutDeriv& data )    { result += J.multTranspose(data.getVec()); }
+    MatBlock getJ()    { return J; }
+    KBlock getK(const OutDeriv& /*childForce*/) {return KBlock();}
+    void addDForce( InDeriv& /*df*/, const InDeriv& /*dx*/,  const OutDeriv& /*childForce*/, const double& /*kfactor */) {}
+};
+
+template<class InReal,class OutReal>
+class CustomJacobianBlock< V1(InReal) , F311(OutReal) > :
+    public  BaseJacobianBlock< V1(InReal) , F311(OutReal) >
+{
+public:
+    typedef V1(InReal) In;
+    typedef F311(OutReal) Out;
+
+    typedef BaseJacobianBlock<In,Out> Inherit;
+    typedef typename Inherit::InCoord InCoord;
+    typedef typename Inherit::InDeriv InDeriv;
+    typedef typename Inherit::OutCoord OutCoord;
+    typedef typename Inherit::OutDeriv OutDeriv;
+    typedef typename Inherit::MatBlock MatBlock;
+    typedef typename Inherit::KBlock KBlock;
+    typedef typename Inherit::Real Real;
+
+    enum { dim = Out::spatial_dimensions };
+    enum { mdim = Out::material_dimensions };
+    typedef Vec<dim, Real> SpatialCoord;
+    typedef Mat<dim,mdim,Real> MaterialToSpatial;
+
+    MatBlock J;
+    OutCoord X0;
+    static const bool constant=true;
+
+    void init(const MatBlock& _J, const SpatialCoord& /*SPos*/, const MaterialToSpatial& M)    { J=_J; X0.getF()=M; }
+    void addapply( OutCoord& result, const InCoord& data )    { result.getVec() +=  J*data + X0.getVec(); }
+    void addmult( OutDeriv& result,const InDeriv& data )    { result.getVec() += J*data; }
+    void addMultTranspose( InDeriv& result, const OutDeriv& data )    { result += J.multTranspose(data.getVec()); }
+    MatBlock getJ()    { return J; }
+    KBlock getK(const OutDeriv& /*childForce*/) {return KBlock();}
+    void addDForce( InDeriv& /*df*/, const InDeriv& /*dx*/,  const OutDeriv& /*childForce*/, const double& /*kfactor */) {}
+};
+
+template<class InReal,class OutReal>
+class CustomJacobianBlock< V1(InReal) , F332(OutReal) > :
+    public  BaseJacobianBlock< V1(InReal) , F332(OutReal) >
+{
+public:
+    typedef V1(InReal) In;
+    typedef F332(OutReal) Out;
+
+    typedef BaseJacobianBlock<In,Out> Inherit;
+    typedef typename Inherit::InCoord InCoord;
+    typedef typename Inherit::InDeriv InDeriv;
+    typedef typename Inherit::OutCoord OutCoord;
+    typedef typename Inherit::OutDeriv OutDeriv;
+    typedef typename Inherit::MatBlock MatBlock;
+    typedef typename Inherit::KBlock KBlock;
+    typedef typename Inherit::Real Real;
+
+    enum { dim = Out::spatial_dimensions };
+    enum { mdim = Out::material_dimensions };
+    typedef Vec<dim, Real> SpatialCoord;
+    typedef Mat<dim,mdim,Real> MaterialToSpatial;
+
+    MatBlock J;
+    OutCoord X0;
+    static const bool constant=true;
+
+    void init(const MatBlock& _J, const SpatialCoord& /*SPos*/, const MaterialToSpatial& M)    { J=_J; X0.getF()=M; }
+    void addapply( OutCoord& result, const InCoord& data )    { result.getVec() +=  J*data + X0.getVec(); }
+    void addmult( OutDeriv& result,const InDeriv& data )    { result.getVec() += J*data; }
+    void addMultTranspose( InDeriv& result, const OutDeriv& data )    { result += J.multTranspose(data.getVec()); }
+    MatBlock getJ()    { return J; }
+    KBlock getK(const OutDeriv& /*childForce*/) {return KBlock();}
+    void addDForce( InDeriv& /*df*/, const InDeriv& /*dx*/,  const OutDeriv& /*childForce*/, const double& /*kfactor */) {}
+};
+
+} // namespace defaulttype
+} // namespace sofa
+
+
+
+#endif
