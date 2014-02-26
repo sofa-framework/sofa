e7cc953605bcd10810b56f4c2afaefe9a4fd5036
Matthieu Nesme <matthieu.nesme@inria.fr>
2014-02-26 15:53:12 +0000

    r10733/sofa : Compliant: forgotten file
    Former-commit-id: 891d3b87cbef4d417256ea11492041456cc8662b

---
+++ applications/plugins/Compliant/numericalsolver/KKTSolver.cpp
@@ -0,0 +1,33 @@
+#include "KKTSolver.h"
+
+#include "../utils/bench.h"
+
+namespace sofa {
+namespace component {
+namespace linearsolver {
+			
+
+
+    KKTSolver::KKTSolver()
+       : debug(initData(&debug,false,"debug","print debug info"))
+       , _preconditioner( NULL )
+       , benchmarkPath(initData(&benchmarkPath, "benchmarkPath", "path to save convergence benchmark output"))
+    {}
+
+
+    void KKTSolver::init()
+    {
+        // look for an optional preconditioner
+        _preconditioner = this->getContext()->get<preconditioner_type>(core::objectmodel::BaseContext::Local);
+
+        if( !benchmarkPath.getValue().empty() )
+            _benchmark = new Benchmark( benchmarkPath.getValue() );
+        else
+            _benchmark = new BaseBenchmark(); // does nothing and should be optimized by the compiler not to add any overcost
+
+    }
+	
+
+}
+}
+}
