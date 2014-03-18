3082dc13124bd5c06bbf77696397cbe91453134b
Herve Delingette <herve.delingette@inria.fr>
2014-03-18 12:35:15 +0000

    r10851/sofa : ADD : createCylinderTractionScene and CylinderTractionStruct to create a scene with a cylinder under traction using TrianglePressureForceField and FixedPlaneConstraint. Used for test of various elastic force fields
    Former-commit-id: e272addf2b56e20db0d64326091b95d0b5dc2ab0

--- applications/plugins/SceneCreator/SceneCreator.h
+++ applications/plugins/SceneCreator/SceneCreator.h
@@ -31,18 +31,27 @@
 #include <sofa/simulation/common/Node.h>
 #include <sofa/component/loader/MeshObjLoader.h>
 #include <sofa/component/mass/UniformMass.h>
+#include <sofa/component/mass/MeshMatrixMass.h>
 #include <sofa/core/objectmodel/BaseData.h>
 
 // Solvers
 #include <sofa/component/odesolver/EulerImplicitSolver.h>
+#include <sofa/component/odesolver/StaticSolver.h>
 #include <sofa/component/linearsolver/CGLinearSolver.h>
 
 // Box roi
 #include <sofa/component/engine/PairBoxRoi.h>
 #include <sofa/component/engine/BoxROI.h>
+#include <sofa/component/engine/GenerateCylinder.h>
 
 // Constraint
+#include <sofa/component/projectiveconstraintset/ProjectToLineConstraint.h>
+#include <sofa/component/projectiveconstraintset/FixedConstraint.h>
 #include <sofa/component/projectiveconstraintset/BilinearMovementConstraint.h>
+#include <sofa/component/projectiveconstraintset/FixedPlaneConstraint.h>
+
+// ForceField
+#include <sofa/component/forcefield/TrianglePressureForceField.h>
 
 //Using double by default, if you have SOFA_FLOAT in use in you sofa-default.cfg, then it will be FLOAT.
 #include <sofa/component/typedef/Sofa_typedef.h>
@@ -224,7 +233,106 @@ template<class T> PatchTestStruct<T> createRegularGridScene(simulation::Node::SP
     patchStruct.SquareNode = SquareNode;
     return patchStruct;
 }
- 
+
+/// Structure which contains the nodes and the pointers useful for the patch test
+template<class T>
+struct CylinderTractionStruct
+{
+   simulation::Node::SPtr root;
+   typename component::container::MechanicalObject<T>::SPtr dofs;
+   typename component::forcefield::TrianglePressureForceField<T>::SPtr forceField; 
+};
+
+template<class DataTypes> CylinderTractionStruct<DataTypes>  createCylinderTractionScene(size_t resolutionCircumferential=10,size_t  resolutionRadial=4,  size_t  resolutionHeight=10,size_t maxIter=1500)
+{
+    // Definitions
+	 typedef typename DataTypes::Coord Coord;
+    typedef typename component::container::MechanicalObject<DataTypes> MechanicalObject;
+    typedef typename sofa::component::mass::UniformMass <DataTypes, SReal> UniformMass;
+    typedef typename component::engine::BoxROI<DataTypes> BoxRoi;
+    typedef typename sofa::component::engine::PairBoxROI<DataTypes> PairBoxRoi;
+    typedef component::linearsolver::CGLinearSolver<component::linearsolver::GraphScatteredMatrix, component::linearsolver::GraphScatteredVector> CGLinearSolver;
+	typename simulation::Node::SPtr root;
+	CylinderTractionStruct<DataTypes> tractionStruct;
+
+    // Root node
+	root = simulation::getSimulation()->createNewGraph("root");
+	tractionStruct.root=root;
+
+    root->setGravity( Coord3(0,0,0) );
+    root->setAnimate(false);
+    root->setDt(0.05);
+
+
+	// GenerateCylinder object
+	sofa::component::engine::GenerateCylinder<DataTypes>::SPtr eng= sofa::modeling::addNew<sofa::component::engine::GenerateCylinder<DataTypes> >(root,"cylinder");
+	eng->f_radius=0.2;
+	eng->f_height=1.0;
+	eng->f_resolutionCircumferential=resolutionCircumferential;
+	eng->f_resolutionRadial=resolutionRadial;
+	eng->f_resolutionHeight=resolutionHeight;
+	// TetrahedronSetTopologyContainer object
+	sofa::component::topology::TetrahedronSetTopologyContainer::SPtr container1= sofa::modeling::addNew<sofa::component::topology::TetrahedronSetTopologyContainer>(root,"Container1");
+	sofa::modeling::setDataLink(&eng->f_tetrahedron,&container1->d_tetrahedron);
+	sofa::modeling::setDataLink(&eng->f_outputX,&container1->d_initPoints);
+	container1->d_createTriangleArray=true;
+	// TetrahedronSetGeometryAlgorithms object
+	sofa::component::topology::TetrahedronSetGeometryAlgorithms<DataTypes>::SPtr geo1= sofa::modeling::addNew<sofa::component::topology::TetrahedronSetGeometryAlgorithms<DataTypes> >(root);
+
+	// CGLinearSolver
+	CGLinearSolver::SPtr cgLinearSolver = addNew< CGLinearSolver >(root,"linearSolver");
+	cgLinearSolver->f_maxIter=maxIter;
+	cgLinearSolver->f_tolerance =1e-9;
+	cgLinearSolver->f_smallDenominatorThreshold=1e-9;
+	// StaticSolver
+	typename component::odesolver::StaticSolver::SPtr solver = addNew<component::odesolver::StaticSolver>(root,"StaticSolver");
+	// mechanicalObject object
+	typename MechanicalObject::SPtr meca1= sofa::modeling::addNew<MechanicalObject>(root);
+	sofa::modeling::setDataLink(&eng->f_outputX,&meca1->x);
+	tractionStruct.dofs=meca1;
+	// MeshMatrixMass
+	typename sofa::component::mass::MeshMatrixMass<DataTypes,SReal>::SPtr mass= sofa::modeling::addNew<sofa::component::mass::MeshMatrixMass<DataTypes,SReal> >(root,"BezierMass");
+	mass->m_massDensity=1.0;
+	mass->lumping=false;
+	/// box fixed
+	helper::vector < defaulttype::Vec<6,SReal> > vecBox;
+	defaulttype::Vec<6,SReal> box;
+	box[0]= -0.01;box[1]= -0.01;box[2]= -0.01;box[3]= 0.01;box[4]= 0.01;box[5]= 0.01;
+    vecBox.push_back(box);
+    typename BoxRoi::SPtr boxRoi1 = addNew<BoxRoi>(root,"boxRoiFix");
+    boxRoi1->boxes.setValue(vecBox);
+	// FixedConstraint
+	typename component::projectiveconstraintset::FixedConstraint<DataTypes>::SPtr fc=
+		addNew<typename component::projectiveconstraintset::FixedConstraint<DataTypes> >(root);
+	sofa::modeling::setDataLink(&boxRoi1->f_indices,&fc->f_indices);
+	// FixedPlaneConstraint
+	typename component::projectiveconstraintset::FixedPlaneConstraint<DataTypes>::SPtr fpc=
+		addNew<typename component::projectiveconstraintset::FixedPlaneConstraint<DataTypes> >(root);
+	fpc->dmin= -0.01;
+	fpc->dmax= 0.01;
+	fpc->direction=Coord(0,0,1);
+	/// box pressure
+	box[0]= -0.2;box[1]= -0.2;box[2]= 0.99;box[3]= 0.2;box[4]= 0.2;box[5]= 1.01;
+    vecBox[0]=box;
+    typename BoxRoi::SPtr boxRoi2 = addNew<BoxRoi>(root,"boxRoiPressure");
+    boxRoi2->boxes.setValue(vecBox);
+	boxRoi2->f_computeTriangles=true;
+	/// TrianglePressureForceField
+	typename component::forcefield::TrianglePressureForceField<DataTypes>::SPtr tpff=
+		addNew<typename component::forcefield::TrianglePressureForceField<DataTypes> >(root);
+	tractionStruct.forceField=tpff;
+	sofa::modeling::setDataLink(&boxRoi2->f_triangleIndices,&tpff->triangleList);
+	// ProjectToLineConstraint
+	typename component::projectiveconstraintset::ProjectToLineConstraint<DataTypes>::SPtr ptlc=
+		addNew<typename component::projectiveconstraintset::ProjectToLineConstraint<DataTypes> >(root);
+	ptlc->f_direction=Coord(1,0,0);
+	ptlc->f_origin=Coord(0,0,0);
+	sofa::helper::vector<unsigned int> vArray;
+	vArray.push_back(resolutionCircumferential*(resolutionRadial-1)+1);
+	ptlc->f_indices.setValue(vArray);
+    
+    return tractionStruct;
+}
 
 }// modeling
 
