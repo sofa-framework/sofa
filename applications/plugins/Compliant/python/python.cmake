a27e881813970d2a7942ea3721052667c2e56821
Matthieu Nesme <matthieu.nesme@inria.fr>
2014-04-08 13:41:48 +0000

    r10971/sofa : Compliant: - adding python files in CMakeLists so they appear in the IDE project. - compiling DampingCompliance for Vec1 dofs.
    Former-commit-id: ab43852c4bc6f4685e3f6fb14442003038afd58b

---
+++ applications/plugins/Compliant/python/python.cmake
@@ -0,0 +1,10 @@
+set(PYTHON_FILES
+
+        python/Compliant/Control.py
+        python/Compliant/Quaternion.py
+        python/Compliant/Rigid.py
+        python/Compliant/Script.py
+        python/Compliant/Tools.py
+        python/Compliant/Vec.py
+
+        )
