05ebcf83fa959d9c39a060180485f457a4e397cb
Matthieu Nesme <matthieu.nesme@inria.fr>
2014-04-08 13:41:48 +0000

    r10971/sofa : Compliant: - adding python files in CMakeLists so they appear in the IDE project. - compiling DampingCompliance for Vec1 dofs.
    Former-commit-id: ab43852c4bc6f4685e3f6fb14442003038afd58b

--- applications/plugins/Compliant/compliance/DampingCompliance.cpp
+++ applications/plugins/Compliant/compliance/DampingCompliance.cpp
@@ -16,9 +16,11 @@ using namespace defaulttype;
 int DampingComplianceClass = core::RegisterObject("Damping Compliance")
 #ifndef SOFA_FLOAT
 	.add< DampingCompliance< Vec6dTypes > >(true)
+    .add< DampingCompliance< Vec1dTypes > >()
 #endif
 #ifndef SOFA_DOUBLE
 	.add< DampingCompliance< Vec6fTypes > >(true)
+    .add< DampingCompliance< Vec1fTypes > >()
 #endif
 	;
 
@@ -26,9 +28,11 @@ SOFA_DECL_CLASS(DampingCompilance)
 
 #ifndef SOFA_FLOAT
 template class SOFA_Compliant_API DampingCompliance<Vec6dTypes>;
+template class SOFA_Compliant_API DampingCompliance<Vec1dTypes>;
 #endif
 #ifndef SOFA_DOUBLE
 template class SOFA_Compliant_API DampingCompliance<Vec6fTypes>;
+template class SOFA_Compliant_API DampingCompliance<Vec1fTypes>;
 #endif
 }
 }
