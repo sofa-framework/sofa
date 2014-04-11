746f9c6a6644593df2dd69bc8065d197df1515d4
Marc Legendre <marc.legendre@inria.fr>
2014-04-11 07:04:57 +0000

    r10979/sofa : push fc51474..b48229c
    * commit b48229c7d25a06c24281720e683cdd667b6c0101
    helper: add the SOFA_UNUSED macro
    
    Its purpose is to silence compiler warnings about 'unused variables',
    typically when we need to document a parameter that is not used inside
    a function.
    
       1 files changed, 5 insertions(+), 0 deletions(-)
    
    
    
    
    Former-commit-id: 6f4c2fbf5c43614ad88c2ba97276a6164e6621d1

--- framework/sofa/helper/system/config.h
+++ framework/sofa/helper/system/config.h
@@ -86,6 +86,11 @@ typedef double SReal;
 #define SOFA_DECL_CLASS(name) extern "C" { int sofa_concat(class_,name) = 0; }
 #define SOFA_LINK_CLASS(name) extern "C" { extern int sofa_concat(class_,name); int sofa_concat(link_,name) = sofa_concat(class_,name); }
 
+// Prevent compiler warnings about 'unused variables'.
+// This should be used when a parameter name is needed (e.g. for
+// documentation purposes) even if it is not used in the code.
+#define SOFA_UNUSED(x) (void)(x)
+
 // Use of "extern template" is activated by the following macro
 // It must be used for DLLs on windows, and is now also activated
 // on other platforms (unless SOFA_NO_EXTERN_TEMPLATE is set), as
