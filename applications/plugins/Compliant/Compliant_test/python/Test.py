291b0f16497367e51111063a5be71fd134d174b9
Maxime Tournier <mtournie@users.gforge.inria.fr>
2014-04-15 04:40:20 +0000

    r11006/sofa : Compliant: better python tests
    Former-commit-id: cdea4489bc689d65fba5e128a6a2c2ebe5a54c78

--- applications/plugins/Compliant/Compliant_test/python/Test.py
+++ applications/plugins/Compliant/Compliant_test/python/Test.py
@@ -1,5 +1,9 @@
 import Sofa
 
+import inspect
+import os
+
+
 # some helpers
 class Controller(Sofa.PythonScriptController):
 
@@ -7,8 +11,23 @@ class Controller(Sofa.PythonScriptController):
         self.node = node
         self.root = node.getRoot()
         
-    def should(self, value):
+    def should(self, value, msg = 'unknown'):
         if value:
             self.node.sendScriptEvent('success', 0)
         else:
+            callerframerecord = inspect.stack()[1]
+
+            frame = callerframerecord[0]
+            info = inspect.getframeinfo(frame)
+
+            f = open(info.filename)
+            lines = f.readlines()
+
+            # print
+            print '{0}:{1}: {2}'.format(os.path.abspath(info.filename),
+                                            info.lineno, 'Failure')
+            print 'Reason:', msg
+            # print #lines[ info.lineno - 1 ]
+
             self.node.sendScriptEvent('failure', 0)
+            
