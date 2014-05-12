6e87d5242a860cbfb60cfec5241bae4e23b5767b
Matthieu Nesme <matthieu.nesme@inria.fr>
2014-05-12 08:21:36 +0000

    r11178/sofa : SofaTest: fixing python scene test for now
    Former-commit-id: c87f853c69ce0df9007336e98e9f7412416c6d49

--- applications/plugins/SofaTest/python/SofaTest/__init__.py
+++ applications/plugins/SofaTest/python/SofaTest/__init__.py
@@ -9,16 +9,20 @@ import os
 ## A controller to return a test result directly from a python script.
 class Controller(Sofa.PythonScriptController):
 
+    # to ensure member variables are declared and accessible
+    node = 0
+    root = 0
+
+    ## @internal storing node pointers
+    # @warning must be called manually if your own Controller::onLoaded is surcharged
     def onLoaded(self, node):
         self.node = node
-        self.rootNode = node.getRoot()
+        self.root = node.getRoot()
     
     ## Send a success event.
-    #
-    #  @param msg A message to print out.
-    def sendSuccess(self, msg = 'unknown'):
+    def sendSuccess(self):
         self.node.sendScriptEvent('success', 0)
-        self.rootNode.findData('animate').value = 0
+        self.root.findData('animate').value = 0
 
     ## Send a failure event.
     #
@@ -38,30 +42,15 @@ class Controller(Sofa.PythonScriptController):
         print 'Reason:', msg
         # print #lines[ info.lineno - 1 ]
         self.node.sendScriptEvent('failure', 0)
-        self.rootNode.findData('animate').value = 0
+        self.root.findData('animate').value = 0
         
     ## Send a success event if value is true. Otherwise a failure event is sent.
     #  @param value A value used to set which failure/success event is sent.
     #  @param msg A meesage to print out.
     def should(self, value, msg = 'unknown'):
         if value:
-            self.node.sendScriptEvent('success', 0)
+            self.sendSuccess()
         else:
-            callerframerecord = inspect.stack()[1]
-
-            frame = callerframerecord[0]
-            info = inspect.getframeinfo(frame)
-
-            f = open(info.filename)
-            lines = f.readlines()
-
-            # print
-            print '{0}:{1}: {2}'.format(os.path.abspath(info.filename),
-                                            info.lineno, 'Failure')
-            print 'Reason:', msg
-            # print #lines[ info.lineno - 1 ]
-
-            self.node.sendScriptEvent('failure', 0)
-        self.rootNode.findData('animate').value = 0
+            self.sendFailure( msg )
             
 
