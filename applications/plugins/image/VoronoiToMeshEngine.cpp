17974c0e1e38e1656d88be089230b06ba4c098b7
Benjamin GILLES <benjamin.gilles@inria.fr>
2014-05-21 13:05:44 +0000

    r11252/sofa : image: add VoronoiToMesh engine to make a mesh out of a voronoi image
    Former-commit-id: 19870a6bb8e699eb4ff3af112e703178546e44cb

--- applications/plugins/image/VoronoiToMeshEngine.cpp
+++ applications/plugins/image/VoronoiToMeshEngine.cpp
@@ -22,8 +22,10 @@
 *                                                                             *
 * Contact information: contact@sofa-framework.org                             *
 ******************************************************************************/
-#include <sofa/helper/system/config.h>
-#include "initImage.h"
+#define SOFA_IMAGE_VoronoiToMeshENGINE_CPP
+
+#include "VoronoiToMeshEngine.h"
+#include <sofa/core/ObjectFactory.h>
 
 namespace sofa
 {
@@ -31,76 +33,31 @@ namespace sofa
 namespace component
 {
 
-//Here are just several convenient functions to help user to know what contains the plugin
-
-extern "C" {
-    SOFA_IMAGE_API void initExternalModule();
-    SOFA_IMAGE_API const char* getModuleName();
-    SOFA_IMAGE_API const char* getModuleVersion();
-    SOFA_IMAGE_API const char* getModuleLicense();
-    SOFA_IMAGE_API const char* getModuleDescription();
-    SOFA_IMAGE_API const char* getModuleComponentList();
-}
-
-void initExternalModule()
+namespace engine
 {
-    static bool first = true;
-    if (first)
-    {
-        first = false;
-    }
-}
 
-const char* getModuleName()
-{
-    return "Image Plugin";
-}
+using namespace defaulttype;
 
-const char* getModuleVersion()
-{
-    return "0.1";
-}
+SOFA_DECL_CLASS(VoronoiToMeshEngine)
 
-const char* getModuleLicense()
-{
-    return "LGPL";
-}
+int VoronoiToMeshEngineClass = core::RegisterObject("Generate flat faces between adjacent regions of an image")
+        .add<VoronoiToMeshEngine<ImageUI> >(true)
+        .add<VoronoiToMeshEngine<ImageUC> >()
+#ifdef BUILD_ALL_IMAGE_TYPES
+        .add<VoronoiToMeshEngine<ImageUS> >()
+        .add<VoronoiToMeshEngine<ImageUL> >()
+#endif
+        ;
 
+template class SOFA_IMAGE_API VoronoiToMeshEngine<ImageUI>;
+template class SOFA_IMAGE_API VoronoiToMeshEngine<ImageUC>;
+#ifdef BUILD_ALL_IMAGE_TYPES
+template class SOFA_IMAGE_API VoronoiToMeshEngine<ImageUS>;
+template class SOFA_IMAGE_API VoronoiToMeshEngine<ImageUL>;
+#endif
 
-const char* getModuleDescription()
-{
-    return "Image support in SOFA";
-}
-
-const char* getModuleComponentList()
-{
-    return "ImageContainer,ImageExporter,ImageViewer,ImageFilter,ImageToMeshEngine";
-}
-
-} // namespace image
+} //
+} // namespace component
 
 } // namespace sofa
-
-////////// BEGIN CLASS LIST //////////
-SOFA_LINK_CLASS(DepthMapToMeshEngine)
-SOFA_LINK_CLASS(ImageAccumulator)
-SOFA_LINK_CLASS(ImageContainer)
-SOFA_LINK_CLASS(ImageDataDisplay)
-SOFA_LINK_CLASS(ImageExporter)
-SOFA_LINK_CLASS(ImageFilter)
-SOFA_LINK_CLASS(ImageOperation)
-SOFA_LINK_CLASS(ImageSampler)
-SOFA_LINK_CLASS(ImageTransform)
-SOFA_LINK_CLASS(ImageValuesFromPositions)
-#ifndef SOFA_NO_OPENGL
-SOFA_LINK_CLASS(ImageViewer)
-#endif /* SOFA_NO_OPENGL */
-SOFA_LINK_CLASS(MarchingCubesEngine)
-SOFA_LINK_CLASS(VoronoiToMeshEngine)
-SOFA_LINK_CLASS(MergeImages)
-SOFA_LINK_CLASS(MeshToImageEngine)
-SOFA_LINK_CLASS(TransferFunction)
-#ifdef SOFA_HAVE_LIBFREENECT
-SOFA_LINK_CLASS(Kinect)
-#endif
 
