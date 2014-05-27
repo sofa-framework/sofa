e868714f034391ce4aa4d039fcd9536f0403e4b8
Thomas Lemaire <thomas.lemaire@inria.fr>
2014-05-27 15:43:07 +0000

    r11290/sofa : some more macro with their test
    Former-commit-id: 78601226d2388214510e0a09f3104818b788ebe1

--- applications/plugins/SofaTest/SofaTest_test/python_tests/python_test_test.py
+++ applications/plugins/SofaTest/SofaTest_test/python_tests/python_test_test.py
@@ -1,16 +1,32 @@
 import sys
 
+import numpy
+import numpy.linalg
+
 from SofaTest.Macro import *
 
 # arguments are an option in python tests
 if len(sys.argv) != 3 :
-  print "ERROR: wrong number of arguments"
+    print "ERROR: wrong number of arguments"
   
 # a pure python function to test
 def isNull( x ):
-  return x==0
-  
+    return x==0
+
 # the python test MUST have a "run" function with no arguments that returns the test result
 def run():
-  # here it tests if the first argument is null and the second is not
-  return EXPECT_TRUE(isNull( int(sys.argv[1]) ), "isNull") and EXPECT_FALSE(isNull( int(sys.argv[2]) ), "isNull" )
+    # here it tests if the first argument is null and the second is not
+    ok = True
+    ok &= EXPECT_TRUE(isNull( int(sys.argv[1]) ), "isNull")
+    ok &= EXPECT_FALSE(isNull( int(sys.argv[2]) ), "isNull")
+    ok &= EXPECT_EQ("toto","toto", "EXPECT_EQ")
+    # a very small value
+    e = 1e-8
+    ok &= EXPECT_FLOAT_EQ(1.0, 1.0+e, "EXPECT_FLOAT_EQ")
+    ok &= EXPECT_VECT_EQ([1., 2., 3.], [1., 2., 3.], "EXPECT_VEC_EQ")
+    # FIXME: I don't know why I need this - thomas.lemaire 27/05/2014
+    if ok:
+        return True
+    else:
+        return False
+#    return ok
