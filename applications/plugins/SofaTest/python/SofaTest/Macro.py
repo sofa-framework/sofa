7c2c239cf695ea0251e7ae783323e7f8f4edc043
Thomas Lemaire <thomas.lemaire@inria.fr>
2014-05-27 15:43:07 +0000

    r11290/sofa : some more macro with their test
    Former-commit-id: 78601226d2388214510e0a09f3104818b788ebe1

--- applications/plugins/SofaTest/python/SofaTest/Macro.py
+++ applications/plugins/SofaTest/python/SofaTest/Macro.py
@@ -1,14 +1,37 @@
- #
- # Macro to mimic google test behavior
- # For now only EXPECT_* can be implemented (not EXPECT_* style)
- #
+import math
+import numpy
+import numpy.linalg
+
+#
+# Macro to mimic google test behavior
+# For now only EXPECT_* can be implemented (not EXPECT_* style)
+#
+
+EPSILON = 1e-6
 
 def EXPECT_TRUE(value, message=""):
     if not value:
-        print "EXPECT_TRUE gets False - ", message
+        print "Value:", value, "Expected: True", message
     return value
 
 def EXPECT_FALSE(value, message=""):
     if value:
-        print "EXPECT_FALSE gets True - ", message
+        print "Value:", value, "Expected: False", message
     return not value
+
+def EXPECT_EQ(expected, actual, message=""):
+    if not expected==actual:
+        print "Value:", actual, "Expected:", expected, "-", message
+    return expected==actual
+
+def EXPECT_FLOAT_EQ(expected, actual, message=""):
+    test = math.fabs(expected-actual)<EPSILON
+    if not test:
+        print "Value:", actual, "Expected:", expected, "-", message
+    return test
+
+def EXPECT_VECT_EQ(expected, actual, message=""):
+    test = numpy.linalg.norm(numpy.array(expected)-numpy.array(actual))<EPSILON
+    if not test:
+        print "Value:", actual, "Expected:", expected, "-", message
+    return test
