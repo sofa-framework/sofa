845a24a01e392fe5cdd34118d1a865aa34e1d6b9
Thomas Jund <thomas.jund@insimo.fr>
2014-06-06 15:37:15 +0000

    FIX: memory leak
    Former-commit-id: 4729b88078e92b81ce80a155a1746ec6b915345e

--- framework/sofa/helper/LCPcalc.cpp
+++ framework/sofa/helper/LCPcalc.cpp
@@ -374,6 +374,12 @@ int resoudreLCP(int dim, double * q, double ** M, double * res)
             printf("*** Pas de solution *** \n");
             boucles=MAX_BOU;
             result=0;
+            for(compteur=0; compteur<dim; compteur++)
+            {
+                free(mat[compteur]);
+            }
+            free(mat);
+            free(base);
             return result;
         }
         else
@@ -1994,6 +2000,10 @@ int nlcp_multiGrid(int dim, double *dfree, double**W, double *f, double mu, doub
             else
             {
                 std::cerr<<"ERROR in nlcp_multiGrid: no projection found for group" << g << std::endl;
+                free(d_free_coarse);
+                free(F_coarse_1)
+                free(F_coarse);
+                free(d_coarse);
                 return 0;
             }
 
@@ -2167,6 +2177,11 @@ int nlcp_multiGrid(int dim, double *dfree, double**W, double *f, double mu, doub
         std::cout<<"after 10 iteration at the finer LEVEL: "<<std::endl;
         afficheResult( f,dim);
     }
+
+    free(d_free_coarse);
+    free(F_coarse_1)
+    free(F_coarse);
+    free(d_coarse);
 
     return result;
 
