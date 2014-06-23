ffba8a46190a0438d42484022efc4f76479d6dc0
jourdes <francois.jourdes@insimo.fr>
2014-06-23 17:49:07 +0200

    UPDATE: TinyXml package usage. Transitively propagate all targets include directories within the build tree with target_include_directories
    Former-commit-id: 79f917e4d1b3af5ae4fa5599ed4f21e517ad296c

---
+++ framework/sofa/SofaFramework.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                              SOFA :: Framework                              *
+*                                                                             *
+* Authors: The SOFA Team (see Authors.txt)                                    *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+
+#ifndef _SOFA_FRAMEWORK_H_
+#define _SOFA_FRAMEWORK_H_
+
+#define SOFA_HAVE_ZLIB
+
+#define SOFA_HAVE_PNG
+
+#define SOFA_HAVE_GLEW
+
+#define SOFA_HAVE_GLUT
+
+/* #undef SOFA_HAVE_BOOST_THREAD */
+
+/* #undef SOFA_HAVE_BOOST_SYSTEM */
+
+#define SOFA_HAVE_TINYXML
+
+
+#define SOFAFRAMEWORK_MAJOR_VERSION 0
+#define SOFAFRAMEWORK_MINOR_VERSION 0
+#define SOFAFRAMEWORK_BUILD_VERSION 1
+
+#endif // _SOFA_FRAMEWORK_H_
