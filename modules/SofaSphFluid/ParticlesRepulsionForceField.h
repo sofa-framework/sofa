ce12378556d4c9661421baf92d3a912b53c9a164
Geoffrey Lienhart <geoffrey.lienhart@insimo.fr>
2014-06-27 14:02:56 +0200

    FIX: compilation with MSVC 2010
    Former-commit-id: 92f887ac7c691c69fadd34baaaf2e8f188867e68

--- modules/SofaSphFluid/ParticlesRepulsionForceField.h
+++ modules/SofaSphFluid/ParticlesRepulsionForceField.h
@@ -102,6 +102,8 @@ public:
     virtual double getPotentialEnergy(const sofa::core::MechanicalParams* /*mparams*/ /* PARAMS FIRST */, const DataVecCoord& /* x */) const
     {
         serr << "getPotentialEnergy not implemented" << sendl;
+
+        return 0.0;
     }
 
     void draw(const core::visual::VisualParams* vparams);
