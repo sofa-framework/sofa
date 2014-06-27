ecd2a995235ff43f01dc808adb8ce3e043974be7
Geoffrey Lienhart <geoffrey.lienhart@insimo.fr>
2014-06-27 14:38:05 +0200

    CLEAN: variable is reassigned a value before the old one has been used
    Former-commit-id: b8504206f6d1ccc4d1458963b2fd771ef5189086

--- modules/SofaMiscTopology/TopologicalChangeProcessor.cpp
+++ modules/SofaMiscTopology/TopologicalChangeProcessor.cpp
@@ -1139,11 +1139,9 @@ void  TopologicalChangeProcessor::findElementIndex(Vector3 coord, int& triangleI
 
     for (unsigned int i = 0 ; i < triIndices.size() ; i++)
     {
-        bool isPointInTriangle = false;
-
-        bool is_tested = false;
-        unsigned int indTest;
-        isPointInTriangle = triangleGeo->isPointInsideTriangle(triIndices[i], is_tested, coord, indTest);
+        const bool is_tested = false;
+        unsigned int indTest = 0;
+        const bool isPointInTriangle = triangleGeo->isPointInsideTriangle(triIndices[i], is_tested, coord, indTest);
 
         if (isPointInTriangle)
         {
@@ -1193,12 +1191,10 @@ void  TopologicalChangeProcessor::findElementIndex(Vector3 coord, int& triangleI
         projectedPoint[1] = (- a * b * x + (a * a + c * c) * y - b * c * z - d * b) /*/normalNorm*/;
         projectedPoint[2] = (- a * c * x - b * c * y + (a * a + b * b) * z - d * c) /*/normalNorm*/;
 
-        bool isPointInTriangle = false;
-
-        bool is_tested = false;
-        unsigned int indTest;
+        const bool is_tested = false;
+        unsigned int indTest = 0;
         //test if the projected point is inside the current triangle
-        isPointInTriangle = triangleGeo->isPointInsideTriangle(i, is_tested, projectedPoint, indTest);
+        const bool isPointInTriangle = triangleGeo->isPointInsideTriangle(i, is_tested, projectedPoint, indTest);
 
         if (isPointInTriangle)
         {
