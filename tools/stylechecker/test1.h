0d58e17c32832c6694f705cae96cad023efc8978
Damien Marchal <damien.marchal@lifl.fr>
2014-06-30 14:58:53 +0200

    New version of stylecheck. This version can test: 	- member's name conformance to guideline 	- namespace's name conformance to guideline 	- Data and Link's name conformance to guideline 	- variable declaration conformance to guideline 	- give coding advice when the amount of includes is too above a threshold.
    Former-commit-id: 47b03007e06bd26ef9167c853677049b58aeff26

---
+++ tools/stylechecker/test1.h
@@ -0,0 +1,87 @@
+#ifndef MYCLASS_H__
+#define MYCLASS_H__
+
+template<class T>
+class Data
+{
+private:
+    T* t;
+
+} ;
+
+template<class T>
+class SingleLink
+{
+private:
+    T* m_t;
+
+} ;
+
+template<class T>
+class DualLink
+{
+private:
+    T* m_t;
+
+} ;
+
+template<class T>
+class OtherType
+{
+private:
+    T* m_t;
+
+} ;
+
+
+
+namespace validnamespace {
+	namespace InvalidNamespace{
+	
+	}
+}
+
+using namespace std ; // Interdit
+using namespace validnamespace ; 
+
+class MyClass
+{
+	public:
+		MyClass(){}
+		int use(){return 0; }
+        int publicposition ;
+
+        Data<int> 			d_DatafieldVALID ;
+        SingleLink<int> 		d_SinglelinkVALID ;
+
+        DualLink<int> 			d_DuallinkVALID ;
+        OtherType<int> 			m_OthertypeVALID ;
+
+	void* 				m_voidpointerVALID ;
+	int* 				m_intpointerVALID ;
+
+        int 				privatepositionINVALID ;
+        int 				m_privatepositionVALID ;
+        Data<int> 			private_datafieldINVALID ;
+        SingleLink<int> 		private_singlelinkINVALID ;
+        DualLink<int> 			private_duallinkINVALID ;
+        OtherType<int> 			private_othertypeINVALID ;
+
+	void* 				private_voidpointerINVALID;
+	int* 				private_intpointerINVALID;
+
+	bool 				invalidBooleanINVALID;
+	bool 				m_isAValidBooleanVALID;
+};
+
+class myInvalidClass
+{
+};
+
+class MyInvalid_Class
+{
+};
+
+
+
+#endif // MCLASS_H__
