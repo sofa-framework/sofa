20622dc3ab9e7c455091df8e3affbdbcf8324d52
nesme <matthieu.nesme@inria.fr>
2014-07-07 11:14:33 +0200

    Compliant: StructuralAPI: while building a rigid body from a mesh, automatically rotate the rigid dof to be aligned with inertia principal axis (to get a diagonal inertia matrix)
    Former-commit-id: a0b5d9053a0bd6ed3cf48bd96b0da69037d2c217

--- applications/plugins/Compliant/python/Compliant/StructuralAPI.py
+++ applications/plugins/Compliant/python/Compliant/StructuralAPI.py
@@ -17,6 +17,8 @@ import Tools
 from Tools import cat as concat
 import Vec as vec
 import numpy as np
+from SofaPython import Quaternion
+import math
 
 class RigidBody:
         ## Generic Rigid Body
@@ -28,18 +30,15 @@ class RigidBody:
                 self.frame = Rigid.Frame()
                 self.framecom = Rigid.Frame()
 
-        def setFromMesh(self, filepath, density = 1000.0, offset = [0,0,0,0,0,0,1], inertia_forces = False ):
+        def setFromMesh(self, filepath, density = 1000.0, offset = [0,0,0,0,0,0,1], scale3d=[1,1,1], inertia_forces = False ):
                 ## create the rigid body from a mesh (inertia and com are automatically computed)
-                info = Rigid.generate_rigid(filepath, density)
+                info = Rigid.generate_rigid(filepath, density, scale3d)
 
                 self.framecom = Rigid.Frame()
+                self.framecom.rotation = info.inertia_rotation
                 self.framecom.translation = info.com
 
                 self.frame = Rigid.Frame(offset) * self.framecom
-
-                # !!!! TODO !!! use info.inertia_rotation to rotate the rigid dof so the inertia axis are aligned to the rigid axis
-                if( info.inertia_rotation != [0,0,0,1] ):
-                    print "WARNING: inertia matrix of "+filepath+" is not diagonal, its inertia is then WRONG (the rigid dof should be rotated to be aligned with inertia axis)"
 
                 self.dofs = self.frame.insert( self.node, name = 'dofs' )
                 self.mass = self.node.createObject('RigidMass',
@@ -86,9 +85,10 @@ class RigidBody:
 
         class CollisionMesh:
 
-            def __init__(self, node, filepath, scale3d, translation):
+            def __init__(self, node, filepath, scale3d, offset):
                 self.node = node.createChild( "collision" )  # node
-                self.loader = self.node.createObject("MeshObjLoader", name='loader', filename=filepath, scale3d=concat(scale3d), translation=concat(translation), triangulate=1 )
+                r = Quaternion.to_euler(offset[3:])  * 180.0 / math.pi
+                self.loader = self.node.createObject("MeshObjLoader", name='loader', filename=filepath, scale3d=concat(scale3d), translation=concat(offset[:3]), rotation=concat(r), triangulate=1 )
                 self.topology = self.node.createObject('MeshTopology', name='topology', src="@loader" )
                 self.dofs = self.node.createObject('MechanicalObject', name='dofs')
                 self.triangles = self.node.createObject('TriangleModel', template='Vec3d', name='model')
@@ -106,9 +106,10 @@ class RigidBody:
 
 
         class VisualModel:
-            def __init__(self, node, filepath, scale3d, translation):
+            def __init__(self, node, filepath, scale3d, offset):
                 self.node = node.createChild( "visual" )  # node
-                self.model = self.node.createObject('VisualModel', template='ExtVec3f', name='model', fileMesh=filepath, scale3d=concat(scale3d), translation=concat(translation))
+                r = Quaternion.to_euler(offset[3:])  * 180.0 / math.pi
+                self.model = self.node.createObject('VisualModel', template='ExtVec3f', name='model', fileMesh=filepath, scale3d=concat(scale3d), translation=concat(offset[:3]) , rotation=concat(r) )
                 self.mapping = self.node.createObject('RigidMapping', name="mapping")
 
         class Offset:
