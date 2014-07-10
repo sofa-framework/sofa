2e3ae7aea7002fea644a6654c89f2bdd0921b4dd
Thomas Lemaire <thomas.lemaire@inria.fr>
2014-07-10 14:33:08 +0200

    fix SofaPython.Tools test
    Former-commit-id: 8315cecdd3fd2e00d604b14c00db170c2e879368

--- applications/plugins/SofaPython/SofaPython_test/python/test_Tools.py
+++ applications/plugins/SofaPython/SofaPython_test/python/test_Tools.py
@@ -4,6 +4,6 @@ from SofaTest.Macro import *
 
 def run():
     ok=True
-    ok&=EXPECT_EQ("1 2 3 4", SofaPython.Tools.cat([1,2,3,4]) )
+    ok&=EXPECT_EQ("1 2 3 4", SofaPython.Tools.listToStr([1,2,3,4]) )
     return ok
 
