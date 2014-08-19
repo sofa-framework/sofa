e6d04fafec8ed846001410f63718218c9b164f00
Geoffrey Lienhart <geoffrey.lienhart@insimo.fr>
2014-08-19 18:08:47 +0000

    FIX: crash when multithreaded collision detection is activated
    git-svn-id: https://code.insimo.fr/svn/IS/ISSofa/branches/ISSofa@10483 f8a8aa39-ebdb-4476-bae3-08f188f1b544

--- SofaKernel/modules/SofaBaseTopology/MeshTopology.cpp
+++ SofaKernel/modules/SofaBaseTopology/MeshTopology.cpp
@@ -638,6 +638,53 @@ void MeshTopology::init()
         quadUpdate->setName("quadUpdate");
         this->addSlave(quadUpdate);
     }
+
+
+    for(PointID pid=0;pid<(std::size_t)this->getNbPoints();++pid)
+    {
+        getEdgesAroundVertex(pid);
+        getOrientedEdgesAroundVertex(pid);
+        getTrianglesAroundVertex(pid);
+        //getOrientedTrianglesAroundVertex(pid);
+        getQuadsAroundVertex(pid);
+        //getOrientedQuadsAroundVertex(pid);
+        getTetrahedraAroundVertex(pid);
+        getHexahedraAroundVertex(pid);
+    }
+
+    for(EdgeID eid=0;eid<(std::size_t)this->getNbEdges();++eid)
+    {
+        getTrianglesAroundEdge(eid);
+        getQuadsAroundEdge(eid);
+        getTetrahedraAroundEdge(eid);
+        getHexahedraAroundEdge(eid);
+    }
+
+    for(TriangleID tid=0;tid<(std::size_t)this->getNbTriangles();++tid)
+    {
+        getEdgesInTriangle(tid);
+        getTetrahedraAroundTriangle(tid);
+    }
+
+    for(QuadID qid=0;qid<(std::size_t)this->getNbQuads();++qid)
+    {
+        getEdgesInQuad(qid);
+        getHexahedraAroundQuad(qid);
+    }
+
+    for(TetraID tetid=0;tetid<(std::size_t)this->getNbTetrahedra();++tetid)
+    {
+        getEdgesInTetrahedron(tetid);
+        getTrianglesInTetrahedron(tetid);
+    }
+
+    for(HexaID hexid=0;hexid<(std::size_t)this->getNbHexas();++hexid)
+    {
+        getEdgesInHexahedron(hexid);
+        getQuadsInHexahedron(hexid);
+    }
+
+
 }
 
 void MeshTopology::clear()
