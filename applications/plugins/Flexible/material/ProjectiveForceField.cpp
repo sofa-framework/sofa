3e65507bb61fe50d97f7db01c93adbc0f6f3f30d
bgilles <benjamin.gilles@inria.fr>
2014-08-20 15:38:07 +0100

    Flexible: add projective dynamics force field Former-commit-id: dbd108b9d0ca92f700350aea808fb9e8c85f2a82

---
+++ applications/plugins/Flexible/material/ProjectiveForceField.cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 4      *
+*                (c) 2006-2009 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#define SOFA_ProjectiveFORCEFIELD_CPP
+
+#include "../initFlexible.h"
+#include "../material/ProjectiveForceField.h"
+#include "../types/DeformationGradientTypes.h"
+#include <sofa/core/ObjectFactory.h>
+
+namespace sofa
+{
+namespace component
+{
+namespace forcefield
+{
+
+SOFA_DECL_CLASS(ProjectiveForceField);
+
+using namespace defaulttype;
+
+// Register in the Factory
+int ProjectiveForceFieldClass = core::RegisterObject("Quadratic distance to a projected dofs")
+        .add< ProjectiveForceField< F331Types > >(true)
+        ;
+
+template class SOFA_Flexible_API ProjectiveForceField< F331Types >;
+
+
+}
+}
+}
+
