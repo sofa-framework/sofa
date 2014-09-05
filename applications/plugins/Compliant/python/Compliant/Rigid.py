74fcbb76213b0e3e3c0be942cd1739f10cee19e8
Thomas Lemaire <thomas.lemaire@inria.fr>
2014-09-05 11:50:05 +0200

    Make it possible to save/load rigid information from a file
    Former-commit-id: 056a55f448ff5c863b76be65affbc5dcb1748ac5

--- applications/plugins/Compliant/python/Compliant/Rigid.py
+++ applications/plugins/Compliant/python/Compliant/Rigid.py
@@ -103,14 +103,16 @@ class MassInfo:
         pass
 
 # front-end to sofa GenerateRigid tool. density unit is kg/m^3
-def generate_rigid(filename, density = 1000.0, scale=[1,1,1], rotation=[0,0,0]):
+def generate_rigid(filename, density = 1000.0, scale=[1,1,1], rotation=[0,0,0], rigidFilename=None):
 
         # TODO bind GenerateRigid
         # - faster than writing in a file
         # - more robust (if several processes try to work in the same file)
 
-
-        tmpfilename = Tools.path( __file__ ) +"/tmp.rigid"
+        if rigidFilename is None:
+            tmpfilename = Tools.path( __file__ ) +"/tmp.rigid"
+        else:
+            tmpfilename = rigidFilename
 
         cmd = [ Sofa.build_dir() + '/bin/GenerateRigid', filename, tmpfilename, str(density), str(scale[0]), str(scale[1]), str(scale[2]), str(rotation[0]), str(rotation[1]), str(rotation[2]) ]
 
@@ -131,51 +133,50 @@ def generate_rigid(filename, density = 1000.0, scale=[1,1,1], rotation=[0,0,0]):
                     raise
 
         output.communicate() # wait until Popen command is finished!!!
+        return read_rigid(tmpfilename)
 
         # GenerateRigid output is stored in the file tmpfilename
-        rigidFile = open( tmpfilename, "r" )
-        line = list( rigidFile )
-        rigidFile.close()
-        
+
+# parse a .rigid file
+def read_rigid(rigidFileName):
+    rigidFile = open( rigidFileName, "r" )
+    line = list( rigidFile )
+    rigidFile.close()
 #        for i in xrange(len(line)):
 #            print str(i) + str(line[i])
                     
-        start = 1
-        
-        res = MassInfo()
-        
-        res.mass = float( line[start].split(' ')[1] )
-        #volm = float( line[start + 1].split(' ')[1] )
-        res.com = map(float, line[start + 3].split(' ')[1:] )
+    start = 1
 
+    res = MassInfo()
 
-        inertia = map(float, line[start + 2].split(' ')[1:] ) # pick inertia matrix from file
-        res.inertia = array( [res.mass * x for x in inertia] ).reshape( 3, 3 ) # convert it in numpy 3x3 matrix
+    res.mass = float( line[start].split(' ')[1] )
+    #volm = float( line[start + 1].split(' ')[1])
+    res.com = map(float, line[start + 3].split(' ')[1:] )
 
+    inertia = map(float, line[start + 2].split(' ')[1:] ) # pick inertia matrix from file
+    res.inertia = array( [res.mass * x for x in inertia] ).reshape( 3, 3 ) # convert it in numpy 3x3 matrix
 
-        # extracting principal axes basis and corresponding rotation and diagonal inertia
+    # extracting principal axes basis and corresponding rotation and diagonal inertia
 
-        if inertia[1]>1e-5 or inertia[2]>1e-5 or inertia[5]>1e-5 : # if !diagonal (1e-5 seems big but the precision from a mesh is poor)
-#            print res.inertia
-            U, res.diagonal_inertia, V = linalg.svd(res.inertia)
-            # det should be 1->rotation or -1->reflexion
-            if linalg.det(U) < 0 : # reflexion
-                # made it a rotation by negating a column
-#                print "REFLEXION"
-                U[:,0] = -U[:,0]
-            res.inertia_rotation = quat.from_matrix( U )
-#            print "generate_rigid not diagonal U" +str(U)
-#            print "generate_rigid not diagonal V" +str(V)
-#            print "generate_rigid not diagonal d" +str(res.diagonal_inertia)
-        else :
-            res.diagonal_inertia = res.inertia.diagonal()
-            res.inertia_rotation = [0,0,0,1]
-
-        
+    if inertia[1]>1e-5 or inertia[2]>1e-5 or inertia[5]>1e-5 : # if !diagonal (1e-5 seems big but the precision from a mesh is poor)
+#        print res.inertia
+        U, res.diagonal_inertia, V = linalg.svd(res.inertia)
+        # det should be 1->rotation or -1->reflexion
+        if linalg.det(U) < 0 : # reflexion
+            # made it a rotation by negating a column
+#            print "REFLEXION"
+            U[:,0] = -U[:,0]
+        res.inertia_rotation = quat.from_matrix( U )
+#       print "generate_rigid not diagonal U" +str(U)
+#       print "generate_rigid not diagonal V" +str(V)
+#       print "generate_rigid not diagonal d" +str(res.diagonal_inertia)
+    else :
+        res.diagonal_inertia = res.inertia.diagonal()
+        res.inertia_rotation = [0,0,0,1]
 
 #        print "generate_rigid " + str(res.mass) + " " + str( res.inertia ) + " " + str( res.diagonal_inertia )
 
-        return res
+    return res
 
 
 
