7576aecd697606234c066b32398c3623df8b2a13
nesme <matthieu.nesme@inria.fr>
2014-09-12 16:47:26 +0200

    Compliant: cleaning HolonomicConstraintValue
    Former-commit-id: b1f49d72b2c6e9b0b5436ed25156fbb5e2d644d4

--- applications/plugins/Compliant/constraint/HolonomicConstraintValue.cpp
+++ applications/plugins/Compliant/constraint/HolonomicConstraintValue.cpp
@@ -18,39 +18,13 @@ HolonomicConstraintValue::HolonomicConstraintValue( mstate_type* mstate )
     : Stabilization( mstate )
 {}
 
-void HolonomicConstraintValue::correction(SReal* dst, unsigned n, unsigned dim, const core::MultiVecCoordId& posId, const core::MultiVecDerivId&) const {
-	assert( mstate );
-
-    unsigned size = n*dim;
-
-    assert( mask.getValue().empty() || mask.getValue().size() == size );
-	
-    mstate->copyToBuffer(dst, posId.getId(mstate.get()), size);
-	
-	// TODO needed ?
-    map(dst, size) = -map(dst, size) / this->getContext()->getDt();
-
-	const mask_type& mask = this->mask.getValue();
-	
-	// non-zero for stabilized
-	unsigned i = 0;
-    for(SReal* last = dst + size; dst < last; ++dst, ++i) {
-		if( !mask.empty() && !mask[i] ) *dst = 0;
-    }
-}
-
 
 void HolonomicConstraintValue::dynamics(SReal* dst, unsigned n, unsigned dim, bool /*stabilization*/, const core::MultiVecCoordId&, const core::MultiVecDerivId&) const {
 	assert( mstate );
 
     const unsigned size = n*dim;
 
-//    memset( dst, 0, size );
-
-    unsigned i = 0;
-    for(SReal* last = dst + size; dst < last; ++dst, ++i) {
-        *dst = 0;
-    }
+    memset( dst, 0, size*sizeof(SReal) );
 }
 
 
