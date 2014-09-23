8f31ee69887e58ac387d04aab1ad94704fb2a1ae
nesme <matthieu.nesme@inria.fr>
2014-09-23 11:50:45 +0200

    Compliant: horizontal Coulomb cone projection by default
    Former-commit-id: cee2a671db9a4080d477431e6d6c176d24959e9f

--- applications/plugins/Compliant/contact/FrictionCompliantContact.h
+++ applications/plugins/Compliant/contact/FrictionCompliantContact.h
@@ -41,13 +41,13 @@ protected:
     FrictionCompliantContact()
         : Inherit()
         , mu( initData(&mu, SReal(0.0), "mu", "friction coefficient (0 for frictionless contacts)") )
-        , horizontalConeProjection( initData(&horizontalConeProjection, false, "horizontalConeProjection", "Should the Coulomb cone projection be horizontal? By default a regular orthogonal cone projection is performed.") )
+        , horizontalConeProjection( initData(&horizontalConeProjection, true, "horizontalConeProjection", "Should the Coulomb cone projection be horizontal? By default a regular orthogonal cone projection is performed.") )
     {}
 
     FrictionCompliantContact(CollisionModel1* model1, CollisionModel2* model2, Intersection* intersectionMethod)
         : Inherit(model1, model2, intersectionMethod)
         , mu( initData(&mu, SReal(0.0), "mu", "friction coefficient (0 for frictionless contacts)") )
-        , horizontalConeProjection( initData(&horizontalConeProjection, false, "horizontalConeProjection", "Should the Coulomb cone projection be horizontal? By default a regular orthogonal cone projection is performed.") )
+        , horizontalConeProjection( initData(&horizontalConeProjection, true, "horizontalConeProjection", "Should the Coulomb cone projection be horizontal? By default a regular orthogonal cone projection is performed.") )
     {}
 
 
