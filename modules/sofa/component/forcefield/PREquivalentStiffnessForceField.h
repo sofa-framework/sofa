6d019c99e9bb01ba6194d6052e4ab558a954b989
Julien Bosman <julien.bosman@inria.fr>
2014-09-23 18:07:42 +0200

    Ongoing work on PartialRigidification + Debug compilation fix
    Former-commit-id: 676787779bba34426935af93637449190d065fb2

--- modules/sofa/component/forcefield/PREquivalentStiffnessForceField.h
+++ modules/sofa/component/forcefield/PREquivalentStiffnessForceField.h
@@ -6,6 +6,7 @@
 #include <sofa/defaulttype/Mat.h>
 #include <sofa/defaulttype/RigidTypes.h>
 #include <sofa/defaulttype/BaseMatrix.h>
+#include <sofa/component/linearsolver/CompressedRowSparseMatrix.h>
 #include <sofa/core/MechanicalParams.h>
 #include <sofa/core/objectmodel/Data.h>
 #include <sofa/helper/vector.h>
@@ -51,22 +52,34 @@ public :
     typedef sofa::defaulttype::Mat<6, 6, Real> Mat66;
     typedef sofa::defaulttype::Mat<12, 12, Real> Mat12x12;
 
+    typedef sofa::component::linearsolver::CompressedRowSparseMatrix<Mat66> CSRMatB66;
+
+    typedef typename sofa::component::linearsolver::CompressedRowSparseMatrix<Mat66>::ColBlockConstIterator CSRMatB66ColBlockIter;
+    typedef typename sofa::component::linearsolver::CompressedRowSparseMatrix<Mat66>::RowBlockConstIterator CSRMatB66RowBlockIter;
+    typedef typename sofa::component::linearsolver::CompressedRowSparseMatrix<Mat66>::BlockConstAccessor CSRMatB66BlockConstAccessor;
+
+    typedef helper::vector<Mat66> VecMat66;
+    typedef helper::vector<Mat12x12> VecMat12;
+
 public :
 	PREquivalentStiffnessForceField();
 	virtual ~PREquivalentStiffnessForceField();
 
-	virtual void bwdInit();
+    virtual void init();
 
 	virtual void addForce(const MechanicalParams* /*mparams*/ /* PARAMS FIRST */, DataVecDeriv& f , const DataVecCoord& x , const DataVecDeriv& v);
 	virtual void addDForce(const MechanicalParams* mparams /* PARAMS FIRST */, DataVecDeriv&   df , const DataVecDeriv&   dx );
     virtual void addKToMatrix(sofa::defaulttype::BaseMatrix * matrix, double kFact, unsigned int &offset);
 
+    void displaceFrames(const VecCoord& frames, VecCoord& displaced, const VecDeriv& dq, const Real epsilon);
+    void computeForce(const VecCoord& pos, const VecCoord& restPos, VecDeriv& f);
+
 protected :
     Data<std::string> m_complianceFile;
-    Mat66 m_complianceMat;
-    Mat66 m_CInv;
-    Mat66 m_H;
-    Mat12x12 m_K;
+    VecMat66 m_complianceMat;
+    VecMat66 m_CInv;
+    VecMat66 m_H;
+    VecMat12 m_K;
     VecCoord m_pos;
     VecCoord m_restPos;
 
