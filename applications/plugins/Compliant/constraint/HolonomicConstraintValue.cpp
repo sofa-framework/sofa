17c490b65d1463d6838f9ad37fe98976e3bae260
Francois Faure <Francois.Faure@inria.fr>
2014-09-30 20:25:53 +0200

    Revert "Compliant: cleaning HolonomicConstraintValue"
    This reverts commit 7576aecd697606234c066b32398c3623df8b2a13 [formerly b1f49d72b2c6e9b0b5436ed25156fbb5e2d644d4].
    
    
    Former-commit-id: f6be318b3c8eb9673d3eb30c27a43b827581329a

--- applications/plugins/Compliant/constraint/HolonomicConstraintValue.cpp
+++ applications/plugins/Compliant/constraint/HolonomicConstraintValue.cpp
@@ -18,13 +18,39 @@ HolonomicConstraintValue::HolonomicConstraintValue( mstate_type* mstate )
     : Stabilization( mstate )
 {}
 
+void HolonomicConstraintValue::correction(SReal* dst, unsigned n, unsigned dim, const core::MultiVecCoordId& posId, const core::MultiVecDerivId&) const {
+	assert( mstate );
+
+    unsigned size = n*dim;
+
+    assert( mask.getValue().empty() || mask.getValue().size() == size );
+	
+    mstate->copyToBuffer(dst, posId.getId(mstate.get()), size);
+	
+	// TODO needed ?
+    map(dst, size) = -map(dst, size) / this->getContext()->getDt();
+
+	const mask_type& mask = this->mask.getValue();
+	
+	// non-zero for stabilized
+	unsigned i = 0;
+    for(SReal* last = dst + size; dst < last; ++dst, ++i) {
+		if( !mask.empty() && !mask[i] ) *dst = 0;
+    }
+}
+
 
 void HolonomicConstraintValue::dynamics(SReal* dst, unsigned n, unsigned dim, bool /*stabilization*/, const core::MultiVecCoordId&, const core::MultiVecDerivId&) const {
 	assert( mstate );
 
     const unsigned size = n*dim;
 
-    memset( dst, 0, size*sizeof(SReal) );
+//    memset( dst, 0, size );
+
+    unsigned i = 0;
+    for(SReal* last = dst + size; dst < last; ++dst, ++i) {
+        *dst = 0;
+    }
 }
 
 
