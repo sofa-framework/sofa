58fa938da97792a66e6319df92d973d017c29aa0
Francois Faure <Francois.Faure@inria.fr>
2014-09-30 20:25:53 +0200

    Revert "Compliant: cleaning HolonomicConstraintValue"
    This reverts commit 7576aecd697606234c066b32398c3623df8b2a13 [formerly b1f49d72b2c6e9b0b5436ed25156fbb5e2d644d4].
    
    
    Former-commit-id: f6be318b3c8eb9673d3eb30c27a43b827581329a

--- applications/plugins/Compliant/constraint/HolonomicConstraintValue.h
+++ applications/plugins/Compliant/constraint/HolonomicConstraintValue.h
@@ -15,7 +15,10 @@ class SOFA_Compliant_API HolonomicConstraintValue : public Stabilization {
     SOFA_CLASS(HolonomicConstraintValue, Stabilization);
 
     HolonomicConstraintValue( mstate_type* mstate = 0 );
-
+	
+	// value for stabilization
+    virtual void correction(SReal* dst, unsigned n, unsigned dim, const core::MultiVecCoordId& posId = core::VecCoordId::position(), const core::MultiVecDerivId& velId = core::VecDerivId::velocity()) const;
+	
 	// value for dynamics
     virtual void dynamics(SReal* dst, unsigned n, unsigned dim, bool stabilization, const core::MultiVecCoordId& posId = core::VecCoordId::position(), const core::MultiVecDerivId& velId = core::VecDerivId::velocity()) const;
 	
