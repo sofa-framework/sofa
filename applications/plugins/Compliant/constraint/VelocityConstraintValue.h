46fdf6c93dadfb6d2afdcc3f99a92b76a218d595
nesme <matthieu.nesme@inria.fr>
2014-10-01 11:35:56 +0200

    Compliant: adding a ConstraintValue to directly constrain the velocity value
    Former-commit-id: 8da0f3592a2569fe41f92dc986d7165ad916cb4e

---
+++ applications/plugins/Compliant/constraint/VelocityConstraintValue.h
@@ -0,0 +1,38 @@
+#ifndef VELOCITYCONSTRAINTVALUE_H
+#define VELOCITYCONSTRAINTVALUE_H
+
+#include "BaseConstraintValue.h"
+
+namespace sofa {
+namespace component {
+namespace odesolver {
+
+/**
+
+   Enforce the velocities to the given values
+
+*/
+
+class SOFA_Compliant_API VelocityConstraintValue : public BaseConstraintValue {
+  public:
+
+    SOFA_CLASS(VelocityConstraintValue, BaseConstraintValue);
+
+    VelocityConstraintValue( mstate_type* mstate = 0 );
+
+	// value for stabilization
+    virtual void correction(SReal* dst, unsigned n, unsigned dim, const core::MultiVecCoordId& posId = core::VecCoordId::position(), const core::MultiVecDerivId& velId = core::VecDerivId::velocity()) const;
+	
+	// value for dynamics
+    virtual void dynamics(SReal* dst, unsigned n, unsigned dim, bool, const core::MultiVecCoordId& posId = core::VecCoordId::position(), const core::MultiVecDerivId& velId = core::VecDerivId::velocity()) const;
+
+
+    Data< vector<SReal> > d_velocities;  ///< fixed velocities
+	
+};
+
+}
+}
+}
+
+#endif
