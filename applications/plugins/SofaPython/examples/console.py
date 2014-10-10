7ce5800fafc0d41bdd23f3ffb7fadb1eb2d2ad9a
nesme <matthieu.nesme@inria.fr>
2014-10-10 14:35:38 +0200

    Python console: minor example modif. Trop cool cette console, merci Max !!!
    Former-commit-id: c3f6c93d2addb17bab770b1c882c914b82decbd5

--- applications/plugins/SofaPython/examples/console.py
+++ applications/plugins/SofaPython/examples/console.py
@@ -1,12 +1,11 @@
-from SofaPython import console
-
-
 def createScene(node):
 
     def hello():
         print 'hello !'
 
     print "usage: as usual, call dir() to see what's available."
+    
+    from SofaPython import console # the console is started during the import
     c = console.Console( locals() )
     
 
