8851189459fa62290e98e1689c9ad3e988a31d1f
Marc Legendre <marc.legendre@inria.fr>
2014-10-22 16:00:19 +0200

    helper: fix wstring related functions in Utils
    The "conversion" functions between string and wstring were a kind of a
    stub. Now they actually do a conversion, provided LC_CTYPE is set
    accordingly. (Which means a program should call setlocale(LC_CTYPE, "")
    at the beginning.)
    
    
    Former-commit-id: 57fa45be64487fcd1ab7de24bd0f922a00d89537

--- framework/framework_test/helper/system/UtilsTest.cpp
+++ framework/framework_test/helper/system/UtilsTest.cpp
@@ -3,14 +3,20 @@
 
 using namespace sofa::helper::system;
 
-#ifdef WIN32
-
 TEST(UtilsTest, string_to_widestring_to_string)
 {
     std::string ascii_chars;
     for (char c = 32 ; c <= 126 ; c++)
         ascii_chars.push_back(c);
     EXPECT_EQ(ascii_chars, Utils::ws2s(Utils::s2ws(ascii_chars)));
+
+    const std::string s("chaîne de test avec des caractères accentués");
+    EXPECT_EQ(s, Utils::ws2s(Utils::s2ws(s)));
 }
 
-#endif
+TEST(UtilsTest, widestring_to_string_to_widestring)
+{
+    const std::string s("chaîne de test avec des caractères accentués");
+    const std::wstring ws = Utils::s2ws(s);
+    EXPECT_EQ(ws, Utils::s2ws(Utils::ws2s(ws)));
+}
