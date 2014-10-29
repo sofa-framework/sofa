29c6e30171ef055b9acf80b1c273a692c5f79594
Thomas Lemaire <thomas.lemaire@inria.fr>
2014-10-29 14:15:03 +0100

    ADD : a python function to select the correct MeshLoader based on file extension
    support obj and vtk for now
    
    
    Former-commit-id: 9edd1966c1a333b62116381586874497d015264c

--- applications/plugins/SofaPython/python/SofaPython/Tools.py
+++ applications/plugins/SofaPython/python/SofaPython/Tools.py
@@ -1,5 +1,7 @@
 import Sofa
 
+import os.path
+
 def listToStr(x):
     """ concatenate lists for use with data.
     """
@@ -18,3 +20,19 @@ def getNode(rootNode, path):
             print "SofaPython.Tools.findNode: can't find node at", path
             return None
     return currentNode
+
+def meshLoader(parentNode, filename, name=None, **args):
+    """ Insert the correct MeshLoader based on the filename extension
+    """
+    ext = os.path.splitext(filename)[1]
+    if name is None:
+        _name="loader_"+os.path.splitext(os.path.basename(filename))[0]
+    else:
+        _name=name
+    if ext == ".obj":
+        return parentNode.createObject('MeshObjLoader', filename=filename, triangulate="1", name=_name, **args)
+    elif ext == ".vtu" or ext == ".vtk":
+        return parentNode.createObject('MeshVTKLoader', filename=filename, triangulate="1", name=_name, **args)
+    else:
+        print "ERROR SofaPython.Tools.meshLoader: unknown mesh extension:", ext
+        return None
