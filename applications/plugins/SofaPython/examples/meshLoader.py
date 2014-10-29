89b3318407fdd549e43d26770a7a25e7fe686b4a
Thomas Lemaire <thomas.lemaire@inria.fr>
2014-10-29 14:26:58 +0100

    simplified example
    Former-commit-id: 8f5f285d62fddd2db249d82d3c5f111b76b28ea9

--- applications/plugins/SofaPython/examples/meshLoader.py
+++ applications/plugins/SofaPython/examples/meshLoader.py
@@ -6,10 +6,10 @@ def createScene(node):
     
     obj = node.createChild("obj")
     meshLoader = SofaPython.Tools.meshLoader(obj, "mesh/Armadillo_simplified.obj", scale="10 10 10")
-    obj.createObject('VisualModel', name="visual", position="@{0}.position".format(meshLoader.name), triangles="@{0}.triangles".format(meshLoader.name))
+    obj.createObject('VisualModel', name="visual", src="@"+meshLoader.name)
     
     vtk = node.createChild("vtk")
     meshLoader = SofaPython.Tools.meshLoader(vtk, "mesh/liver.vtk")
-    vtk.createObject('VisualModel', name="visual", position="@{0}.position".format(meshLoader.name), triangles="@{0}.triangles".format(meshLoader.name))
+    vtk.createObject('VisualModel', name="visual", src="@"+meshLoader.name)
     
     return node
