d046481de242558e80d4c1af311696f70c7c1b80
nesme <matthieu.nesme@inria.fr>
2014-11-03 16:32:46 +0100

    Revert "renaming a few more python scene files"
    This reverts commit 49bf59670096f2852f4d1c42794e298aca86033e [formerly 9cf7f497a69d81e9da58bbcbef6b076ca8e27023].
    
    
    Former-commit-id: 7f3c450cf7e6766a35704c7381de4c67a22b4187

--- applications/plugins/Flexible/Flexible_test/python_test_list.cpp
+++ applications/plugins/Flexible/Flexible_test/python_test_list.cpp
@@ -10,7 +10,7 @@ static struct Tests : public Python_test_list
     {
         static const std::string scenePath = std::string(FLEXIBLE_TEST_PYTHON_DIR);
 
-        addTest( "reInitMapping.pys", scenePath );
+        addTest( "reInitMapping.py", scenePath );
         // add python tests here
     }
 } tests;
--- applications/plugins/SofaTest/SofaTest_test/python_test_list.cpp
+++ applications/plugins/SofaTest/SofaTest_test/python_test_list.cpp
@@ -23,7 +23,7 @@ static struct SceneTests : public Python_test_list
         arguments[3] = "1e-3"; // error threshold
         arguments[4] = "1.0"; // mass
         arguments[5] = "1.0"; // radius
-        addTest( "damping.pys", scenePath, arguments );
+        addTest( "damping.py", scenePath, arguments );
 
         // add python scene tests here
     }
