cee28cedd5d5c63e04ba404985dc3c5f345dc44d
Marc Legendre <marc.legendre@inria.fr>
2014-12-03 18:32:58 +0100

    Add the scripts used for continuous integration in scripts/ci/
    Former-commit-id: fcc03a96fb641058092cd7806cf13e2a09946ca9

---
+++ scripts/ci/scene-tests.sh
@@ -0,0 +1,351 @@
+#! /bin/bash
+
+# This scripts tries to run runSofa in batch mode on each .scn file in the
+# repository, and saves the results in a bunch of files.
+#
+# More precisely, it deals with .scn files under the examples/ at the root of
+# the source tree, and the .scn files found in the examples/ directory of each
+# plugin that was compiled.
+#
+# The default behaviour it to run 100 iterations for each scene, with a timeout
+# of 30 seconds.  This can be influenced via a .scene-tests put directly in one
+# of the searched directories, and that contains directives like those:
+#
+# ignore "path/to/file.scn"
+# timeout "path/to/file.scn" "number-of-seconds"
+# iterations "path/to/file.scn" "number-of-iterations"
+
+set -o errexit
+
+usage() {
+    echo "Usage: scene-tests.sh [run|count-warnings|count-errors|print-summary] <build-dir> <src-dir>"
+}
+
+if [[ "$#" = 3 ]]; then
+    command="$1"
+    build_dir="$2"
+    src_dir="$3"
+    output_dir="$build_dir/scene-testing"
+else
+    usage; exit 1
+fi
+
+if [[ ! -d "$build_dir/lib/" ]]; then
+    echo "Error: '$build_dir' does not look like a Sofa build."
+    usage; exit 1
+elif [[ ! -d "$src_dir/applications/plugins" ]]; then
+    echo "Error: '$src_dir' does not look like a Sofa source tree."
+    usage; exit 1
+fi
+
+
+### Utils
+
+filter-out-comments() {
+    sed -e 's/#.*//'
+}
+remove-leading-blanks() {
+    sed -e 's/^[[:blank:]]*//'
+}
+remove-trailing-blanks() {
+    sed -e 's/[[:blank:]]*$//'
+}
+delete-blank-lines() {
+    sed -e '/^$/d'
+}
+clean-line() {
+    filter-out-comments | remove-leading-blanks | remove-trailing-blanks | delete-blank-lines
+}
+log() {
+    # # Send to stderr not to interfere
+    tee -a "$output_dir/log.txt" 1>&2
+    # cat >> "$output_dir/log.txt"
+}
+
+# Well-formed option: 'option "arg 1" "arg 2" "arg 3"'
+option-is-well-formed() {
+    local cmd='[^[:blank:]]*'
+    local arg='"[^"]*"'
+    echo "$1" | grep -xqE "^$cmd([[:blank:]]*$arg)+"
+}
+# $ option-split-args '"a b c" "d" "e f"'
+# a b c
+# d
+# e f
+option-split-args() {
+    local line="$1"
+    local rest="$line"
+    while [[ "$rest" != "" ]]; do
+        local format='^"\([^"]*\)"[[:blank:]]*\(.*\)'
+        local arg=$(echo "$rest" | sed "s/$format/\1/")
+        local rest=$(echo "$rest" | sed "s/$format/\2/")
+        if [[ "$arg" == "" ]]; then
+            # (This should never happen.)
+            echo "Warning: error parsing arguments: $line" 1>&2
+        fi
+        echo "$arg"
+    done
+}
+# get-args 'foo "a" "b" "c"'
+# "a" "b" "c"
+get-args() {
+    echo "$1" | sed -e 's/[^[:blank:]][^[:blank:]]*[[:blank:]][[:blank:]]*//'
+}
+# get-option 'foo "a" "b" "c"'
+# foo
+get-option() {
+    echo "$1" | sed -e 's/\([^[:blank:]][^[:blank:]]*\).*/\1/'
+}
+# $ get-arg '"a" "b" "c"' 2
+# b
+get-arg() {
+    echo "$1" | option-split-args "$1" | sed -n "$2p"
+}
+# $ count-args '"a" "b" "c"'
+# 3
+count-args() {
+    option-split-args "$1" | wc -l | tr -d ' 	'
+}
+
+list-scenes() {
+    local directory="$1"
+    /usr/bin/find "$directory" -name '*.scn' | sed -e "s:$directory/::"
+}
+
+
+get-lib() {
+    pushd "$build_dir/lib/" > /dev/null
+    ls {lib,}"$1"*.{dylib,so,lib}* 2> /dev/null | xargs echo
+    popd > /dev/null
+}
+
+list-plugins() {
+    pushd "$src_dir/applications/plugins" > /dev/null
+    for plugin in *; do
+        if [ -e "$plugin/CMakeLists.txt" ]; then
+            echo "$plugin"
+        fi
+    done
+    popd > /dev/null
+}
+
+list-scene-directories() {
+    # Main directory
+    mkdir -p "$output_dir/examples"
+    echo examples >> "$output_dir/directories.txt"
+    # List directories for compiled plugins only
+    list-plugins | while read plugin; do
+        local lib="$(get-lib "$plugin")"
+        if [ -n "$lib" ]; then
+            echo "Plugin $plugin: built (found $lib)" | log
+            if [ -d "$src_dir/applications/plugins/$plugin/examples" ]; then
+                mkdir -p "$output_dir/applications/plugins/$plugin/examples"
+                echo "applications/plugins/$plugin/examples"
+            else
+                echo "Plugin $plugin: no examples/ directory" | log
+            fi
+        else
+            echo "Plugin $plugin: not built" | log
+        fi
+    done >> "$output_dir/directories.txt"
+}
+
+create-directories() {
+    # List directories where scenes will be tested
+    list-scene-directories
+
+    # echo "Creating directory structure."
+    # List all scenes
+    while read path; do
+        rm -f "$output_dir/$path/ignore-patterns.txt"
+        touch "$output_dir/$path/ignore-patterns.txt"
+        list-scenes "$src_dir/$path" > "$output_dir/$path/scenes.txt"
+        while read scene; do
+            mkdir -p "$output_dir/$path/$scene"
+            echo 30 > "$output_dir/$path/$scene/timeout.txt" # Default timeout, in seconds
+            echo 100 > "$output_dir/$path/$scene/iterations.txt" # Default number of iterations
+            echo "$path/$scene" >> "$output_dir/all-scenes.txt"
+        done < "$output_dir/$path/scenes.txt"
+    done < "$output_dir/directories.txt"
+}
+
+parse-options-files() {
+    # echo "Parsing option files."
+    while read path; do
+        if [[ -e "$src_dir/$path/.scene-tests" ]]; then
+            clean-line < "$src_dir/$path/.scene-tests" | while read line; do
+                if option-is-well-formed "$line"; then
+                    local option=$(get-option "$line")
+                    local args=$(get-args "$line")
+                    case "$option" in
+                        ignore)
+                            if [[ "$(count-args "$args")" = 1 ]]; then
+                                get-arg "$args" 1 >> "$output_dir/$path/ignore-patterns.txt"
+                            else
+                                echo "$path/.scene-tests: warning: 'ignore' expects one argument: ignore <pattern>" | log
+                            fi
+                            ;;
+                        timeout)
+                            if [[ "$(count-args "$args")" = 2 ]]; then
+                                scene="$(get-arg "$args" 1)"
+                                if [[ -e "$src_dir/$path/$scene" ]]; then
+                                    get-arg "$args" 2 > "$output_dir/$path/$scene/timeout.txt"
+                                else
+                                    echo "$path/.scene-tests: warning: no such file: $scene" | log
+                                fi
+                            else
+                                echo "$path/.scene-tests: warning: 'timeout' expects two arguments: timeout <file> <timeout>" | log
+                            fi
+                            ;;
+                        iterations)
+                            if [[ "$(count-args "$args")" = 2 ]]; then
+                                scene="$(get-arg "$args" 1)"
+                                if [[ -e "$src_dir/$path/$scene" ]]; then
+                                    get-arg "$args" 2 > "$output_dir/$path/$scene/iterations.txt"
+                                else
+                                    echo "$path/.scene-tests: warning: no such file: $scene" | log
+                                fi
+                            else
+                                echo "$path/.scene-tests: warning: 'iterations' expects two arguments: iterations <file> <number>" | log
+                            fi
+                            ;;
+                        *)
+                            echo "$path/.scene-tests: warning: unknown option: $option" | log
+                            ;;
+                    esac
+                else
+                    echo "$path/.scene-tests: warning: ill-formed line: $line" | log
+                fi
+            done
+        fi
+    done < "$output_dir/directories.txt"
+
+    # echo "Listing ignored scenes."
+    while read path; do
+        grep -xf "$output_dir/$path/ignore-patterns.txt" \
+            "$output_dir/$path/scenes.txt" \
+            > "$output_dir/$path/ignored-scenes.txt" || true
+        if [ -s "$output_dir/$path/ignore-patterns.txt" ]; then
+            grep -xvf "$output_dir/$path/ignore-patterns.txt" \
+                "$output_dir/$path/scenes.txt" \
+                > "$output_dir/$path/tested-scenes.txt" || true
+        else
+            cp  "$output_dir/$path/scenes.txt" "$output_dir/$path/tested-scenes.txt"
+        fi
+
+        sed -e "s:^:$path/:" "$output_dir/$path/ignored-scenes.txt" >> "$output_dir/all-ignored-scenes.txt"
+        sed -e "s:^:$path/:" "$output_dir/$path/tested-scenes.txt" >> "$output_dir/all-tested-scenes.txt"
+    done < "$output_dir/directories.txt"
+}
+
+initialize-scene-testing() {
+    echo "Initializing scene testing."
+    rm -rf "$output_dir"
+    mkdir -p "$output_dir"
+
+    runSofa="$(ls "$build_dir/bin/runSofa"{,d} 2> /dev/null || true)"
+    if [[ -x "$runSofa" ]]; then
+        echo "Found runSofa: $runSofa" | log
+    else
+        echo "Error: could not find runSofa."
+        exit 1
+    fi
+
+    touch "$output_dir/warnings.txt"
+    touch "$output_dir/errors.txt"
+
+    create-directories
+    parse-options-files
+}
+
+test-all-scenes() {
+    echo "Scene testing in progress..."
+    while read scene; do
+        echo "- $scene"
+        local iterations=$(cat "$output_dir/$scene/iterations.txt")
+        local options="-g batch -s dag -n $iterations"
+        local runSofa_cmd="$runSofa $options $src_dir/$scene >> $output_dir/$scene/output.txt 2>&1"
+        local timeout=$(cat "$output_dir/$scene/timeout.txt")
+        echo "$runSofa_cmd" > "$output_dir/$scene/command.txt"
+        "$src_dir/scripts/ci/timeout.sh" runSofa "$runSofa_cmd" $timeout
+        local status=-1
+        if [[ -e runSofa.timeout ]]; then
+            echo 'Timeout!'
+            echo timeout > "$output_dir/$scene/status.txt"
+            rm -f runSofa.timeout
+        else
+            cat runSofa.exit_code > "$output_dir/$scene/status.txt"
+        fi
+        rm -f runSofa.exit_code
+    done < "$output_dir/all-tested-scenes.txt"
+    echo "Done."
+}
+
+extract-warnings() {
+    while read scene; do
+        if [[ -e "$output_dir/$scene/output.txt" ]]; then
+            sed -ne "/^WARNING\[[^]]*\]/s:WARNING\[\([^]]*\)\]:$scene\: warning\: \1:p" \
+                "$output_dir/$scene/output.txt"
+        fi
+    done < "$output_dir/all-tested-scenes.txt" > "$output_dir/warnings.txt"
+}
+
+extract-errors() {
+    while read scene; do
+        if [[ -e "$output_dir/$scene/status.txt" ]]; then
+            local status="$(cat "$output_dir/$scene/status.txt")"
+            if [[ "$status" != 0 ]]; then
+                echo "$scene: error: $status"
+            fi
+        fi
+    done < "$output_dir/all-tested-scenes.txt" > "$output_dir/errors.txt"
+}
+
+count-warnings() {
+    sort "$output_dir/warnings.txt" | uniq | wc -l | tr -d ' 	'
+}
+
+count-errors() {
+    wc -l < "$output_dir/errors.txt" | tr -d ' 	'
+}
+
+print-summary() {
+    echo "Scene testing summary:"
+    echo "- $(count-warnings) warning(s)"
+    echo "- $(count-errors) error(s):"
+    while read scene; do
+        if [[ -e "$output_dir/$scene/status.txt" ]]; then
+            local status="$(cat "$output_dir/$scene/status.txt")"
+            case "$status" in
+                "timeout")
+                    echo "  - Timeout: $scene"
+                    ;;
+                [0-9]*)
+                    if [[ "$status" -gt 128 && ( $(uname) = Darwin || $(uname) = Linux ) ]]; then
+                        echo "  - Exit with status $status ($(kill -l $status)): $scene"
+                    elif [[ "$status" != 0 ]]; then
+                        echo "  - Exit with status $status: $scene"
+                    fi
+                    ;;
+                *)
+                    echo "Error: unexpected value in $output_dir/$scene/status.txt: $status"
+                    ;;
+            esac
+        fi
+    done < "$output_dir/all-tested-scenes.txt"
+}
+
+if [[ "$command" = run ]]; then
+    initialize-scene-testing
+    test-all-scenes
+    extract-warnings
+    extract-errors
+elif [[ "$command" = print-summary ]]; then
+    print-summary
+elif [[ "$command" = count-warnings ]]; then
+    count-warnings
+elif [[ "$command" = count-errors ]]; then
+    count-errors
+else
+    echo "Unknown command: $command"
+fi
---
+++ scripts/ci/tests.sh
@@ -0,0 +1,186 @@
+#! /bin/bash
+
+# This script runs all the googletest-based automatic tests it can find, assumed
+# to be the executables in bin/ that match *_test, and saves the results in XML
+# files, that can be understood by Jenkins.
+
+set -o errexit
+
+usage() {
+    echo "Usage: tests.sh (run|print-summary) <build-dir> <src-dir>"
+}
+
+if [[ "$#" = 3 ]]; then
+    command="$1"
+    build_dir="$2"
+    src_dir="$3"
+    output_dir="$build_dir/tests"
+else
+    usage; exit 1
+fi
+
+if [[ ! -d "$build_dir/lib/" ]]; then
+    echo "Error: '$build_dir' does not look like a Sofa build."
+    usage; exit 1
+elif [[ ! -d "$src_dir/applications/plugins" ]]; then
+    echo "Error: '$src_dir' does not look like a Sofa source tree."
+    usage; exit 1
+fi
+
+
+export SOFA_DATA_PATH="$src_dir:$src_dir/examples:$src_dir/share"
+
+list-tests() {
+    pushd "$build_dir/bin" > /dev/null
+    for file in *; do
+        case "$file" in
+            *_test|*_testd|*_test.exe|*_testd.exe)
+                echo $file
+                ;;
+        esac
+    done
+    popd > /dev/null
+}
+
+initialize-testing() {
+    echo "Initializing testing."
+    rm -rf "$output_dir"
+    rm -rf "$output_dir/reports"
+    mkdir -p "$output_dir"
+    mkdir -p "$output_dir/reports"
+    list-tests | while read test; do
+        echo "$test"
+        mkdir -p "$output_dir/$test"
+    done > "$output_dir/tests.txt"
+}
+
+fix-test-report() {
+    # Little fix: Googletest marks skipped tests with a 'status="notrun"' attribute,
+    # but the JUnit XML understood by Jenkins requires a '<skipped/>' element instead.
+    # source: http://stackoverflow.com/a/14074664
+    sed -i'.bak' 's:\(<testcase [^>]*status="notrun".*\)/>:\1><skipped/></testcase>:' "$1"
+}
+
+run-single-test() {
+    local test=$1
+    local output_file="$output_dir/$test/report.xml"
+    local test_cmd="$build_dir/bin/$test --gtest_output=xml:report.xml 2>&1 | tee $output_dir/$test/output.txt"
+    local timeout=900
+
+    echo "$test_cmd" > "$output_dir/$test/command.txt"
+    echo "Running $test with a timeout of $timeout seconds"
+    rm -f report.xml
+    "$src_dir/scripts/ci/timeout.sh" test "$test_cmd" $timeout
+    mv report.xml "$output_file" || true
+
+    if [[ -e test.timeout ]]; then
+        echo 'Timeout!'
+        echo timeout > "$output_dir/$test/status.txt"
+        rm -f test.timeout
+    else
+        cat test.exit_code > "$output_dir/$test/status.txt"
+    fi
+    rm -f test.exit_code
+    if [ -e "$output_file" ]; then
+        fix-test-report "$output_file"
+        cp "$output_file" "$output_dir/reports/$test.xml"
+    else
+        echo "$0: error: $test ended with code $(cat $output_dir/$test/status.txt)" >&2
+    fi
+}
+
+run-all-tests() {
+    while read test; do
+        run-single-test "$test"
+    done < "$output_dir/tests.txt"
+}
+
+
+count-test-suites() {
+    list-tests | wc -w | tr -d ' '
+}
+count-test-reports() {
+    ls "$output_dir/reports/"*.xml 2> /dev/null | wc -l | tr -d ' '
+}
+count-crashes() {
+    echo "$(( $(count-test-suites) - $(count-test-reports) ))"
+}
+
+# Fetch the <testsuites> XML elements in reports/*.xml,
+# extract and sum the attribute given in argument
+# This function relies on the element being written on a single line:
+# E.g. <testsuites tests="212" failures="4" disabled="0" errors="0" ...
+tests-get ()
+{
+    # Check the existence of report files
+    if ! ls "$output_dir/reports/"*.xml &> /dev/null; then
+        echo 0
+        return
+    fi
+    attribute="$1"
+
+    # grep the lines containing '<testsuites'; for each one, match the
+    # 'attribute="..."' pattern, and collect the "..." part
+    counts=$(sed -ne "s/.*<testsuites[^>]* $attribute=\"//" \
+                 -e "/^[0-9]/s/\".*//p" "$output_dir/reports/"*.xml)
+    # sum the values
+    total=0
+    for value in $counts; do
+        total=$(( $total + $value ))
+    done
+    echo "$total"
+}
+
+print-summary() {
+    echo "Testing summary:"
+    echo "- $(count-test-suites) test suite(s)"
+    local crashes='$(count-crashes)'
+    echo "- $(count-crashes) crash(es)"
+    if [[ "$crashes" != 0 ]]; then
+        while read test; do
+            if [[ ! -e "$output_dir/$test/report.xml" ]]; then
+                local status="$(cat "$output_dir/$test/status.txt")"
+                case "$status" in
+                    "timeout")
+                        echo "  - Timeout: $test"
+                        ;;
+                    [0-9]*)
+                        if [[ "$status" -gt 128 && ( $(uname) = Darwin || $(uname) = Linux ) ]]; then
+                            echo "  - Exit with status $status ($(kill -l $status)): $test"
+                        elif [[ "$status" != 0 ]]; then
+                            echo "  - Exit with status $status: $test"
+                        fi
+                        ;;
+                    *)
+                        echo "Error: unexpected value in $output_dir/$test/status.txt: $status"
+                        ;;
+                esac
+            fi
+        done < "$output_dir/tests.txt"
+    fi
+    echo "- $(tests-get tests) test(s)"
+    echo "- $(tests-get failures) failure(s)"
+    echo "- $(tests-get disabled) disabled test(s)"
+    echo "- $(tests-get errors) error(s)"
+}
+
+if [[ "$command" = run ]]; then
+    initialize-testing
+    run-all-tests
+elif [[ "$command" = count-tests ]]; then
+    tests-get tests
+elif [[ "$command" = count-failures ]]; then
+    tests-get failures
+elif [[ "$command" = count-disabled ]]; then
+    tests-get disabled
+elif [[ "$command" = count-errors ]]; then
+    tests-get errors
+elif [[ "$command" = count-test-suites ]]; then
+    count-test-suites
+elif [[ "$command" = count-crashes ]]; then
+    count-crashes
+elif [[ "$command" = print-summary ]]; then
+    print-summary
+else
+    usage
+fi
