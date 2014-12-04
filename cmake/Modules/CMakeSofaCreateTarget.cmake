01213dbb0e4ff362938583123859bcede132b242
François Jourdes <francois.jourdes@insimo.fr>
2014-12-04 09:56:59 +0100

    BUILD UPDATE: SofaFramework uses the sofa_create_target macro to convert system libraries found with find_package directive into imported target. This is important for install portability, specially on windows. This macro is defined in the  CMakeSofaCreateTarget.cmake file and gets to be redistributed by default in share/cmake/Modules.
    BUILD UPDATE: some minor updates in the configuration files of the Sofa collections and extlibs, related to the usage of cmake_required_components.
    
    BUILD FIX: SofaModeler lib was exporting its target in SofaBase as opposed to SofaGui
    
    
    Former-commit-id: b7e07fae850da276dc93d7ebcc0c13420afb4321

---
+++ cmake/Modules/CMakeSofaCreateTarget.cmake
@@ -0,0 +1,58 @@
+# - Create an imported target from a library path and an include dir path.
+#   Handle the special case where LIBRARY_PATH is in fact an existing target. 
+#
+# sofa_create_imported_target(TARGETNAME LIBRARY_PATH INCLUDE_DIRS)
+#  TARGETNAME_Target  - (output) variable which contains the name of the created target. 
+#                       It is usually contains TARGETTNAME with one notable exception. 
+#                       If LIBRARY_PATH is an existing target, TARGETNAME_TARGETS
+#                       contains LIBRARY_PATH instead.
+#  TARGETNAME         - (input) the name of the target to create
+#  LIBRARY_PATH       - (input) the path to the library ( .so or .lib depending on the platform)
+#  INCLUDE_DIRS       - (input) include directories associated with the library, 
+#                       which are added as INTERFACE_INCLUDE_DIRECTORIES for the target
+#
+# The typical usage scenario is to convert the absolute paths to a system library that cmake return 
+# after a find_package call into an imported target. By using the cmake target mechanism, it is 
+# easier to redistribute a software that depends on system libraries, whose locations are not 
+# known before hand on the consumer system.
+#
+# For further reference about this subject :
+# http://public.kitware.com/pipermail/cmake-developers/2014-March/009983.html
+# Quoted from https://github.com/Kitware/CMake/blob/master/Help/manual/cmake-packages.7.rst
+# "Note that it is not advisable to populate any properties which may contain paths, 
+#  such as :prop_tgt:`INTERFACE_INCLUDE_DIRECTORIES` and :prop_tgt:`INTERFACE_LINK_LIBRARIES`, 
+#  with paths relevnt to dependencies. That would hard-code into installed packages the 
+#  include directory or library paths for dependencies as found on the machine the package 
+#  was made on."
+#
+# Example: 
+#
+# add_library( SHARED myLib )
+# find_package(PNG REQUIRED)
+# sofa_create_target( PNG MyNamespace ${PNG_LIBRARY} ${PNG_INCLUDE_DIRS} ) 
+# target_link_libraries( myLib PUBLIC ${PNG_TARGETS} )
+#
+
+macro(sofa_create_target TARGETNAME NAMESPACE LIBRARY_PATH INCLUDE_DIRS)
+#    message("TARGETNAME ${TARGETNAME}")
+    if(NOT TARGET ${LIBRARY_PATH} )
+#        message("${LIBRARY_PATH} is not a TARGET")
+        if(NOT TARGET ${TARGETNAME} )
+#            message("${TARGETNAME} is not a TARGET")
+            set(NAMESPACE_TARGETNAME "${NAMESPACE}::${TARGETNAME}")
+            if(NOT TARGET ${NAMESPACE_TARGETNAME} )
+#                message("${NAMESPACE_TARGETNAME} is not a TARGET")
+                add_library( ${NAMESPACE_TARGETNAME} UNKNOWN IMPORTED )
+                set_target_properties( ${NAMESPACE_TARGETNAME} PROPERTIES IMPORTED_LOCATION ${LIBRARY_PATH} )
+                set_target_properties( ${NAMESPACE_TARGETNAME} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${INCLUDE_DIRS}" )
+                set(${TARGETNAME}_Target ${NAMESPACE_TARGETNAME} )
+            endif()
+        else()
+            message( SEND_ERROR "sofa_create_target error. ${TARGETNAME} is an already an existing TARGET.\ 
+                                 Choose a different name.")
+        endif()
+    else()
+#        message("${LIBRARY_PATH} is a TARGET")
+        set(${TARGETNAME}_Target ${LIBRARY_PATH} )
+    endif()
+endmacro()
--- extlibs/tinyxml/tinyxml.h
+++ extlibs/tinyxml/tinyxml.h
@@ -53,6 +53,8 @@ distribution.
 #define DEBUG
 #endif
 
+#define TIXML_USE_STL
+
 #ifdef TIXML_USE_STL
 	#include <string>
  	#include <iostream>
