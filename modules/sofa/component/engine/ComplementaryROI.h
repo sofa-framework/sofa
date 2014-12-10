0472ca2d5877cfe4369efc157c55df79ff086b5d
Thomas Lemaire <thomas.lemaire@inria.fr>
2014-12-10 11:00:01 +0100

    FIX (an attempt to): windows compilation
    Former-commit-id: 2863a97b1a62169b5cfb7858b523bd7fc00de0c8

--- modules/sofa/component/engine/ComplementaryROI.h
+++ modules/sofa/component/engine/ComplementaryROI.h
@@ -99,6 +99,15 @@ protected:
 
 };
 
+#if defined(SOFA_EXTERN_TEMPLATE) && !defined(SOFA_COMPONENT_ENGINE_COMPLEMENTARYROI_CPP)
+#ifndef SOFA_FLOAT
+extern template class SOFA_ENGINE_API ComplementaryROI<defaulttype::Vec3dTypes>;
+#endif //SOFA_FLOAT
+#ifndef SOFA_DOUBLE
+extern template class SOFA_ENGINE_API ComplementaryROI<defaulttype::Vec3fTypes>;
+#endif //SOFA_DOUBLE
+#endif
+
 } // namespace engine
 
 } // namespace component
