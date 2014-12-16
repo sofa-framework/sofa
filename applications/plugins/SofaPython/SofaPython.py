6382c73a4784a466dd5891a3c973db06d389b7ae
Thomas Dupeux <thomas.dupeux@univ-lyon1.fr>
2014-12-16 08:50:15 +0100

    [CHANGE] passing a base sofa directory as argument to python script SofaPython.py for it to search for plugins directory
    Former-commit-id: 58f5eab94ac710ce417ed318a11b35d269e797d1

--- applications/plugins/SofaPython/SofaPython.py
+++ applications/plugins/SofaPython/SofaPython.py
@@ -13,6 +13,10 @@ plugins_paths = ['applications/plugins',
 
 for relative in plugins_paths:
     absolute = os.path.join(Sofa.src_dir(), relative)
+    if __name__ == "__main__":
+        if sys.argc>=2 and os.path.isdir(os.path.abspath(sys.argv[1])):
+            absolute=os.path.join(os.path.abspath(sys.argv[1]),relative)
+
     if os.path.exists(absolute): # applications-dev is not necessarily existing
         
         for plugin in os.listdir( absolute ):
@@ -21,6 +25,7 @@ for relative in plugins_paths:
                 python = os.path.join(path, 'python')
                 if os.path.exists( python ):
                     print "SofaPython: added plugin path for", plugin
+                    #print "SofaPython: located in ", python					
                     sys.path.append( python )
 
 # add more customization here if needed
