3b3be28f31f9a64a9a2ec790214023049012ace9
Thomas Lemaire <thomas.lemaire@inria.fr>
2015-01-15 14:38:56 +0100

    Revert "[SofaPython] ADD: Node::getObject(name) binding + example"
    getObject is already bound in BaseContext
    
    This reverts commit 2de7678d2908cfd18875176eca310ac89b648009 [formerly f381fd5d1e20a4a2e08f41db0ca5879fe238fd0b].
    
    
    Former-commit-id: ce37594e6a0e1dce78c036adec04dce24434d069

--- applications/plugins/SofaPython/examples/node.py
+++
@@ -1,6 +0,0 @@
-import Sofa 
-
-def createScene(rootNode):
-    A = rootNode.createChild('A')
-    A.createObject("MechanicalObject", name="dofs")
-    print A.getObject("dofs").name,":",A.getObject("dofs").position
