da15d3d77265f2d72ce768b6cef0d48f10a92bc4
Marc Legendre <marc.legendre@inria.fr>
2015-01-19 16:06:57 +0100

    image: write new CMakeLists.txt
    To do:
    - Remove files when SOFA_MISC_NO_OPENGL is defined
    - Add files if libfreenect is available
    - Add files if opencv is available
    
    
    Former-commit-id: f8c8996955c538f31853331001e0f91ef7169a89

--- applications/plugins/image/ImageExporter.h
+++ applications/plugins/image/ImageExporter.h
@@ -31,7 +31,6 @@
 
 #include <sofa/core/objectmodel/BaseObject.h>
 #include <sofa/defaulttype/VecTypes.h>
-#include <sofa/component/component.h>
 #include <sofa/core/objectmodel/DataFileName.h>
 #include <sofa/core/behavior/BaseMechanicalState.h>
 #include <sofa/core/objectmodel/Event.h>
--- applications/plugins/image/initImage.cpp
+++ applications/plugins/image/initImage.cpp
@@ -22,8 +22,8 @@
 *                                                                             *
 * Contact information: contact@sofa-framework.org                             *
 ******************************************************************************/
-#include <sofa/helper/system/config.h>
 #include "initImage.h"
+#include <sofa/helper/system/config.h>
 
 namespace sofa
 {
