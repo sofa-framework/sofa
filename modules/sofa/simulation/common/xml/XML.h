ed6bdf8230132a6567fb1f78820d158060690f31
François Jourdes <francois.jourdes@insimo.fr>
2015-01-20 10:25:31 +0100

    Revert "Make sure TIXML_USE_STL is defined before including tinyxml.h"
    This reverts commit da5e9018ae519c9af3fecbf827346ac8cb44455b [formerly b13dbab6b6b31d08fa2e860872f81dc34fe6bad7].
    
    
    Former-commit-id: 2f26fe8fc253466a40e69c73318405bd906344a9

--- modules/sofa/simulation/common/xml/XML.h
+++ modules/sofa/simulation/common/xml/XML.h
@@ -29,9 +29,6 @@
 #include <sofa/simulation/common/xml/Element.h>
 
 #ifdef SOFA_XML_PARSER_TINYXML
-#ifndef TIXML_USE_STL
-#define TIXML_USE_STL
-#endif
 #include <tinyxml.h>
 #endif
 #ifdef SOFA_XML_PARSER_LIBXML
