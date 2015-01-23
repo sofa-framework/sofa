3fc6886fc7c408e68ca89ddec7ee6914906c551a
degletagne <aurelie.degletagne@inria.fr>
2015-01-23 17:09:46 +0100

    ADD: Test the dynamic of euler implicit and newmark implicit solver by comparing simulated and analytical positions of a mass-spring system.
    Former-commit-id: 51bcb48a71f913b240df5dc7bcff12f57943a64e

---
+++ applications/plugins/SofaTest/SofaTest_test/SpringSolverDynamic_test.cpp
@@ -0,0 +1,157 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU General Public License as published by the Free  *
+* Software Foundation; either version 2 of the License, or (at your option)   *
+* any later version.                                                          *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    *
+* more details.                                                               *
+*                                                                             *
+* You should have received a copy of the GNU General Public License along     *
+* with this program; if not, write to the Free Software Foundation, Inc., 51  *
+* Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.                   *
+*******************************************************************************
+*                            SOFA :: Applications                             *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+
+#include "stdafx.h"
+#include "Sofa_test.h"
+
+#include <sofa/component/init.h>
+#include <sofa/core/ExecParams.h>
+
+//Including Simulation
+#include <sofa/simulation/common/Simulation.h>
+#include <sofa/simulation/graph/DAGSimulation.h>
+#include <sofa/simulation/common/Node.h>
+
+// Including mechanical object
+#include <sofa/component/container/MechanicalObject.h>
+#include <sofa/core/MechanicalParams.h>
+
+#include <sofa/defaulttype/VecTypes.h>
+
+namespace sofa {
+
+using namespace component;
+using namespace defaulttype;
+
+/**  Dynamic solver test.
+Test the dynamic behavior of solver: study a mass-spring system under gravity initialize with spring rest length it will oscillate around its equilibrium position if there is no damping.
+The movement follows the equation:
+x(t)= A cos(wt + phi) with w the pulsation w=sqrt(K/M), K the stiffness, M the mass and phi the phase.
+In this test: x(t=0)= 1 and v(t=0)=0 and K = spring stiffness and phi = 0 of material thus x(t)= cos(wt)
+This test compares siumlated mass position to analytic mass position during 2s every time step (here dt=0.001).
+*/
+
+template <typename _DataTypes>
+struct SpringSolverDynamic_test : public Sofa_test<_DataTypes>
+{
+    typedef _DataTypes DataTypes;
+    typedef typename DataTypes::CPos CPos;
+    typedef typename DataTypes::Coord Coord;
+    typedef typename DataTypes::VecCoord VecCoord;
+    typedef typename DataTypes::VecDeriv VecDeriv;
+    typedef typename DataTypes::Deriv Deriv;
+    typedef typename DataTypes::Real Real;
+
+    typedef container::MechanicalObject<DataTypes> MechanicalObject;
+
+    /// Root of the scene graph
+    simulation::Node::SPtr root;      
+    /// Tested simulation
+    simulation::Simulation* simulation;  
+
+    
+    /// Create the context for the scene
+    void SetUp()
+    { 
+        // Init simulation
+        sofa::component::init();
+        sofa::simulation::setSimulation(simulation = new sofa::simulation::graph::DAGSimulation());
+        root = simulation::getSimulation()->createNewGraph("root");
+    }
+
+    void loadScene(std::string sceneName)
+    {
+        // Load the scene from the xml file
+        std::string fileName = std::string(SOFATEST_SCENES_DIR) + "/" + sceneName;
+        root = sofa::core::objectmodel::SPtr_dynamic_cast<sofa::simulation::Node>( sofa::simulation::getSimulation()->load(fileName.c_str()));
+    }
+
+    /// After simulation compare the positions of points to the theoretical positions.
+    bool compareSimulatedToTheoreticalPositions(double tolerance)
+    {
+        // Init simulation
+        sofa::simulation::getSimulation()->init(root.get());
+        double time = root->getTime();
+        double stiffnessSpring = 100;
+        double mass = 10;
+        double w = sqrt(stiffnessSpring/mass);
+
+        // Get mechanical object
+        simulation::Node::SPtr massNode = root->getChild("MassNode");
+        typename MechanicalObject::SPtr dofs = massNode->get<MechanicalObject>(root->SearchDown);
+
+        // Animate
+        do
+        {              
+            // Record the mass position
+            Coord p0=(*(dofs.get()->getX()))[0];
+
+            // Absolute error
+            double absoluteError = fabs(p0[1]-(cos(w*time)));
+
+            // Compare mass position to the theoretical position
+            if( absoluteError > tolerance )
+            {
+                ADD_FAILURE() << "Position of mass at time " << time << " is wrong: "  << std::endl
+                    <<" expected Position is " << cos(sqrt(stiffnessSpring/mass)*time) << std::endl
+                    <<" actual Position is   " << p0[1] << std::endl
+                    << "absolute error     = " << absoluteError << std::endl;
+                return false;
+            }
+
+            //Animate
+            sofa::simulation::getSimulation()->animate(root.get(),0.001);
+            time = root->getTime();
+        }
+        while (time < 2);
+        return true;
+    }
+
+};
+
+// Define the list of DataTypes to instanciate
+using testing::Types;
+typedef Types<
+    Vec3Types
+> DataTypes; // the types to instanciate.
+
+// Test suite for all the instanciations
+TYPED_TEST_CASE(SpringSolverDynamic_test, DataTypes);
+
+// Test case EulerImplicit Solver
+TYPED_TEST( SpringSolverDynamic_test , EulerImplicitSolverDynamicTest )
+{
+   this->loadScene("EulerImplicitSpringDynamicTest.xml");
+   ASSERT_TRUE( this->compareSimulatedToTheoreticalPositions(0.01));
+}
+
+// Test case NewmarkImplicit Solver
+TYPED_TEST( SpringSolverDynamic_test , NewmarkImplicitSolverDynamicTest )
+{
+   this->loadScene("NewmarkSpringDynamicTest.xml");
+   ASSERT_TRUE( this->compareSimulatedToTheoreticalPositions(0.004));
+}
+
+} // namespace sofa
