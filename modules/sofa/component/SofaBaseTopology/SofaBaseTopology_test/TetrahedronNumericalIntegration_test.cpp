633b01158fe2597de9e767b0d56cd2c907e79fb7
degletagne <aurelie.degletagne@inria.fr>
2015-02-03 11:40:30 +0100

    CHANGE: Split SofaTest_test second step: Register new test projects in components and add CMakeLists.txt files in each test project.
    Former-commit-id: 5212a330031d3fefe4d88822f4accf1f9ded88a3

--- modules/sofa/component/SofaBaseTopology/SofaBaseTopology_test/TetrahedronNumericalIntegration_test.cpp
+++ modules/sofa/component/SofaBaseTopology/SofaBaseTopology_test/TetrahedronNumericalIntegration_test.cpp
@@ -22,9 +22,7 @@
 *                                                                             *
 * Contact information: contact@sofa-framework.org                             *
 ******************************************************************************/
-
-#include "stdafx.h"
-#include "Sofa_test.h"
+#include <plugins/SofaTest/Sofa_test.h>
 #include <sofa/component/init.h>
 //#include <plugins/SceneCreator/SceneCreator.h>
 //Including Simulation
