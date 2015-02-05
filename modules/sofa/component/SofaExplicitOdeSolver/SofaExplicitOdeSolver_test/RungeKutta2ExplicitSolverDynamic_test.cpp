046c39b3cda17aeaf7883571d2c3500f14867993
degletagne <aurelie.degletagne@inria.fr>
2015-02-05 09:51:59 +0100

    ADD: Dynamic test for Runge Kutta 2nd order explicit solver in SofaExplicitOdeSolver_test.
    Former-commit-id: b4ecd20ccf9ff2147feeb7c15331deb17cb7bead

--- modules/sofa/component/SofaExplicitOdeSolver/SofaExplicitOdeSolver_test/RungeKutta2ExplicitSolverDynamic_test.cpp
+++ modules/sofa/component/SofaExplicitOdeSolver/SofaExplicitOdeSolver_test/RungeKutta2ExplicitSolverDynamic_test.cpp
@@ -37,7 +37,8 @@
 #include <sofa/component/container/MechanicalObject.h>
 
 // Solvers
-#include <sofa/component/odesolver/EulerSolver.h>
+#include <sofa/component/odesolver/RungeKutta2Solver.h>
+#include <sofa/component/linearsolver/CGLinearSolver.h>
 
 #include <sofa/defaulttype/VecTypes.h>
 
@@ -53,31 +54,33 @@ Test the dynamic behavior of solver: study a mass-spring system under gravity in
 The movement follows the equation:
 x(t)= A cos(wt + phi) with w the pulsation w=sqrt(K/M), K the stiffness, M the mass and phi the phase.
 In this test: x(t=0)= 1 and v(t=0)=0 and K = spring stiffness and phi = 0 of material thus x(t)= cos(wt)
-This tests generates the discrete mass position obtained with euler explicit solver with different parameter values (K,M,h).
+This tests generates the discrete mass position obtained with rungeKutta2 solver with different parameter values (K,M,h).
 Then it compares the effective mass position to the computed mass position every time step.
 */
 
 template <typename _DataTypes>
-struct EulerExplicitDynamic_test : public Elasticity_test<_DataTypes>
+struct RungeKutta2ExplicitSolverDynamic_test : public Elasticity_test<_DataTypes>
 {
     typedef _DataTypes DataTypes;
     typedef typename DataTypes::Coord Coord;
 
     typedef container::MechanicalObject<DataTypes> MechanicalObject;
-    typedef component::odesolver::EulerSolver EulerSolver;
+    typedef component::odesolver::RungeKutta2Solver RungeKutta2Solver;
+    typedef component::linearsolver::CGLinearSolver<component::linearsolver::GraphScatteredMatrix, component::linearsolver::GraphScatteredVector> CGLinearSolver;
+
 
     /// Root of the scene graph
     simulation::Node::SPtr root;      
     /// Tested simulation
     simulation::Simulation* simulation;  
-    /// Position and velocity array
+    /// Position, velocity and acceleration array
     vector<double> positionsArray;
     vector<double> velocitiesArray;
-
+    vector<double> accelerationsArray;
     
     /// Create the context for the scene
     void createScene(double K, double m, double l0)
-    { 
+    {
         // Init simulation
         sofa::component::init();
         sofa::simulation::setSimulation(simulation = new sofa::simulation::graph::DAGSimulation());
@@ -87,7 +90,12 @@ struct EulerExplicitDynamic_test : public Elasticity_test<_DataTypes>
         root->setGravity(Coord3(0,-10,0));
 
         // Solver
-        EulerSolver::SPtr eulerSolver = addNew<EulerSolver> (getRoot());
+        RungeKutta2Solver::SPtr rungeKutta2Solver = addNew<RungeKutta2Solver> (getRoot());
+
+        CGLinearSolver::SPtr cgLinearSolver = addNew<CGLinearSolver> (getRoot());
+        cgLinearSolver->f_maxIter=3000;
+        cgLinearSolver->f_tolerance =1e-9;
+        cgLinearSolver->f_smallDenominatorThreshold=1e-9;
 
         // Set initial positions and velocities of fixed point and mass
         MechanicalObject3::VecCoord xFixed(1);
@@ -97,7 +105,7 @@ struct EulerExplicitDynamic_test : public Elasticity_test<_DataTypes>
         MechanicalObject3::VecCoord xMass(1);
         MechanicalObject3::DataTypes::set( xMass[0], 0., 1.,0.);
         MechanicalObject3::VecDeriv vMass(1);
-        MechanicalObject3::DataTypes::set( vFixed[0], 0.,0.,0.);
+        MechanicalObject3::DataTypes::set( vMass[0], 0., 0., 0.);
 
         // Mass spring system
         root = this-> createMassSpringSystem(
@@ -109,21 +117,25 @@ struct EulerExplicitDynamic_test : public Elasticity_test<_DataTypes>
                 vFixed, // Initial velocity of fixed point
                 xMass,  // Initial position of mass
                 vMass); // Initial velocity of mass
-
     }
 
-    /// Generate discrete mass position values with euler explicit solver
-    void generateDiscreteMassPositions (double h, double K, double m, double z0, double v0,double g, double finalTime, double cd)
+
+    /// Generate discrete mass position values with euler implicit solver
+    void generateDiscreteMassPositions (double h, double K, double m, double z0, double v0,double g, double finalTime)
     {
         int size = 0 ;
 
-        // During t=5s
+        // During t=finalTime
         if((finalTime/h) > 0)
         {
             size = int(finalTime/h);
             positionsArray.reserve(size);
             velocitiesArray.reserve(size);
+            accelerationsArray.reserve(size);
         }
+
+        // First acceleration
+        accelerationsArray.push_back((-K*(z0-z0)-m*g)/m);
 
         // First position is z0
         positionsArray.push_back(double(z0));
@@ -131,11 +143,18 @@ struct EulerExplicitDynamic_test : public Elasticity_test<_DataTypes>
         // First velocity is v0
         velocitiesArray.push_back(v0);
 
-        // Compute recursively velocity and position
         for(int i=1;i< size+1; i++)
         {
-            velocitiesArray.push_back(velocitiesArray[i-1]+h*(-K*(positionsArray[i-1]-z0)-m*g-velocitiesArray[i-1]*cd)/m);
-            positionsArray.push_back(positionsArray[i-1]+h*velocitiesArray[i]);
+            // At time t + h/2
+            double acc = (-K*(positionsArray[i-1]-z0)-m*g)/m;
+            double newPos = positionsArray[i-1]+0.5*h*velocitiesArray[i-1];
+            double newVel = velocitiesArray[i-1]+0.5*h*acc;
+
+            // At time t + h
+            accelerationsArray.push_back((-K*(newPos-z0)-m*g)/m);
+            positionsArray.push_back(positionsArray[i-1]+h*newVel);
+            velocitiesArray.push_back(velocitiesArray[i-1]+h*accelerationsArray[i]);
+
         }
 
     }
@@ -173,7 +192,6 @@ struct EulerExplicitDynamic_test : public Elasticity_test<_DataTypes>
             //Animate
             sofa::simulation::getSimulation()->animate(root.get(),h);
             time = root->getTime();
-
             // Iterate
             i++;
         }
@@ -190,30 +208,30 @@ typedef Types<
 > DataTypes; // the types to instanciate.
 
 // Test suite for all the instanciations
-TYPED_TEST_CASE(EulerExplicitDynamic_test, DataTypes);
+TYPED_TEST_CASE(RungeKutta2ExplicitSolverDynamic_test, DataTypes);
 
-// Test case: h=0.001 k=100 m=10
-TYPED_TEST( EulerExplicitDynamic_test , eulerExplicitSolverDynamicTest_small_dt)
+// Test case: h=0.1 k=100 m =10 rm=0.1 rk=0.1
+TYPED_TEST( RungeKutta2ExplicitSolverDynamic_test , rungeKutta2ExplicitSolverDynamicTest_medium_dt)
 {
    this->createScene(100,10,1); // k,m,l0
-   this->generateDiscreteMassPositions (0.001, 100, 10,1, 0,10, 2, 0);
-   this-> compareSimulatedToTheoreticalPositions(5e-16,0.001);
-}
-
-// Test case: h=0.01 k=1000 m=10
-TYPED_TEST( EulerExplicitDynamic_test , eulerExplicitSolverDynamicTest_medium_dt)
-{
-   this->createScene(1000,10,1); // k,m,l0
-   this->generateDiscreteMassPositions (0.01, 1000, 10,1, 0,10, 2, 0);
+   this->generateDiscreteMassPositions (0.01, 100, 10, 1, 0,10, 2);
    this-> compareSimulatedToTheoreticalPositions(5e-16,0.01);
 }
 
-// Test case: h=0.01 k=1000 m=100
-TYPED_TEST( EulerExplicitDynamic_test , eulerExplicitSolverDynamicTest_high_dt)
+// Test case: h=0.1 K=1000 m = 10
+TYPED_TEST( RungeKutta2ExplicitSolverDynamic_test , rungeKutta2ExplicitSolverDynamicTest_high_dt)
 {
-   this->createScene(1000,100,1); // k,m,l0
-   this->generateDiscreteMassPositions (0.1, 1000, 100,1, 0,10, 2, 0);
+   this->createScene(1000,10,1); // k,m,l0
+   this->generateDiscreteMassPositions (0.1, 1000, 10, 1, 0, 10, 2);
    this-> compareSimulatedToTheoreticalPositions(5e-16,0.1);
+}
+
+// Test case: h=0.001 k=100 m=100
+TYPED_TEST( RungeKutta2ExplicitSolverDynamic_test , rungeKutta2ExplicitSolverDynamicTest_small_dt)
+{
+   this->createScene(100,100,1); // k,m,l0
+   this->generateDiscreteMassPositions (0.001, 100, 100, 1, 0, 10, 2);
+   this-> compareSimulatedToTheoreticalPositions(9e-16,0.001);
 }
 
 } // namespace sofa
