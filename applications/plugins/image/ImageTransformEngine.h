9443080fe34aed3582cf02b771e376a9f24cc5cb
bgilles <benjamin.gilles@inria.fr>
2015-02-12 18:57:19 +0100

    image: add imageTransformEngine with an example Former-commit-id: b2e81fe1456b4679d7ffd918e6b696b78298bbe6

---
+++ applications/plugins/image/ImageTransformEngine.h
@@ -0,0 +1,151 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 4      *
+*                (c) 2006-2009 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef SOFA_IMAGE_ImageTransformEngine_H
+#define SOFA_IMAGE_ImageTransformEngine_H
+
+#include "initImage.h"
+#include "ImageTypes.h"
+#include <sofa/core/DataEngine.h>
+#include <sofa/core/objectmodel/BaseObject.h>
+#include <sofa/component/component.h>
+#include <sofa/helper/Quater.h>
+
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace engine
+{
+
+using helper::vector;
+using cimg_library::CImg;
+using cimg_library::CImgList;
+
+/**
+ * Apply a transform to the data 'transform'
+ * in future: could be templated on ImageTransform type
+ * @author Benjamin GILLES
+ */
+
+
+class SOFA_IMAGE_API ImageTransformEngine : public core::DataEngine
+{
+public:
+    typedef core::DataEngine Inherited;
+    SOFA_CLASS(ImageTransformEngine,Inherited);
+
+    typedef SReal Real;
+    typedef defaulttype::ImageLPTransform<Real> TransformType;
+    typedef TransformType::Coord Coord;
+    typedef helper::WriteAccessor<Data< TransformType > > waTransform;
+    typedef helper::ReadAccessor<Data< TransformType > > raTransform;
+
+    Data< TransformType > inputTransform;
+    Data< TransformType > outputTransform;
+
+    Data<defaulttype::Vector3> translation; // translation
+    Data<defaulttype::Vector3> rotation; // rotation
+    Data<Real> scale; // scale
+    Data<bool> inverse;
+
+    virtual std::string getTemplateName() const    { return templateName(this);    }
+    static std::string templateName(const ImageTransformEngine* = NULL) { return std::string();  }
+
+    ImageTransformEngine()    :   Inherited()
+      , inputTransform(initData(&inputTransform,TransformType(),"inputTransform",""))
+      , outputTransform(initData(&outputTransform,TransformType(),"outputTransform",""))
+      , translation(initData(&translation, defaulttype::Vector3(0,0,0),"translation", "translation vector ") )
+      , rotation(initData(&rotation, defaulttype::Vector3(0,0,0), "rotation", "rotation vector ") )
+      , scale(initData(&scale, (Real)1.0,"scale", "scale factor") )
+      , inverse(initData(&inverse, false, "inverse", "true to apply inverse transformation"))
+    {
+    }
+
+    virtual ~ImageTransformEngine() {}
+
+    virtual void init()
+    {
+        addInput(&translation);
+        addInput(&rotation);
+        addInput(&scale);
+        addInput(&inverse);
+        addInput(&inputTransform);
+        addOutput(&outputTransform);
+        setDirtyValue();
+    }
+
+    virtual void reinit() { update(); }
+
+protected:
+
+    virtual void update()
+    {
+        cleanDirty();
+        raTransform inT(this->inputTransform);
+        waTransform outT(this->outputTransform);
+
+        Real s;
+        helper::Quater<Real> r;
+        Coord t;
+
+        if(this->inverse.getValue())
+        {
+            s=(Real)1./this->scale.getValue();
+            r=helper::Quater< Real >::createQuaterFromEuler(this->rotation.getValue()* (Real)M_PI / (Real)180.0 ).inverse();
+            t=-r.rotate(this->translation.getValue())*s;
+        }
+        else
+        {
+            s=this->scale.getValue();
+            t=this->translation.getValue();
+            r= helper::Quater< Real >::createQuaterFromEuler(this->rotation.getValue()* (Real)M_PI / (Real)180.0 );
+        }
+
+        outT->getScale() = inT->getScale() * s;
+        outT->getTranslation() = r.rotate(inT->getTranslation())*s + t;
+
+        helper::Quater<Real> q = r*inT->qrotation;
+        // outT->getRotation()=q.toEulerVector() * (Real)180.0 / (Real)M_PI ;  //  this does not convert quaternion to euler angles for some reasons..
+        if(q[0]*q[0]+q[1]*q[1]==0.5 || q[1]*q[1]+q[2]*q[2]==0.5) {q[3]+=10-3; q.normalize();} // hack to avoid singularities
+        outT->getRotation()[0]=atan2(2*(q[3]*q[0]+q[1]*q[2]),1-2*(q[0]*q[0]+q[1]*q[1])) * (Real)180.0 / (Real)M_PI;
+        outT->getRotation()[1]=asin(2*(q[3]*q[1]-q[2]*q[0])) * (Real)180.0 / (Real)M_PI;
+        outT->getRotation()[2]=atan2(2*(q[3]*q[2]+q[0]*q[1]),1-2*(q[1]*q[1]+q[2]*q[2])) * (Real)180.0 / (Real)M_PI;
+
+        outT->update(); // update internal data
+    }
+
+};
+
+
+} // namespace engine
+
+} // namespace component
+
+} // namespace sofa
+
+#endif // SOFA_IMAGE_ImageTransformEngine_H
