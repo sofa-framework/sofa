870250e5665807332667f0ba402886b0ef676f68
Marc Legendre <marc.legendre@inria.fr>
2015-02-12 11:07:59 +0100

    framework: remove use of nested-name-specifier tinyxml::
    There is no 'tinyxml' namespace in the actual tinyxml library (i.e. not
    the one in extlibs/). Thus using tinyxml:: in MeshVTK would result in
    compilation errors.
    
    
    Former-commit-id: 01cd816c2bbdf1cf809e4f0026fbea9714de5abc

--- framework/sofa/helper/io/MeshVTK.cpp
+++ framework/sofa/helper/io/MeshVTK.cpp
@@ -61,7 +61,7 @@ void MeshVTK::init(std::string filename)
 void MeshVTK::readVTU(const std::string &filename)
 {
 
-    tinyxml::TiXmlDocument vtu;
+    TiXmlDocument vtu;
     vtu.LoadFile(filename);
     if (vtu.Error())
     {
@@ -69,7 +69,7 @@ void MeshVTK::readVTU(const std::string &filename)
         std::cerr << vtu.ErrorDesc() << std::endl;
         return;
     }
-    tinyxml::TiXmlElement* piece = vtu.FirstChildElement("VTKFile")->FirstChildElement("UnstructuredGrid")->FirstChildElement("Piece");
+    TiXmlElement* piece = vtu.FirstChildElement("VTKFile")->FirstChildElement("UnstructuredGrid")->FirstChildElement("Piece");
 
     unsigned int nbPoints, nbCells;
     piece->QueryUnsignedAttribute("NumberOfPoints", &nbPoints);
@@ -88,7 +88,7 @@ void MeshVTK::readVTU(const std::string &filename)
     // read triangles
     std::stringstream connectivityDataArray;
     std::stringstream typesDataArray;
-    tinyxml::TiXmlElement* cellDataArray = piece->FirstChildElement("Cells")->FirstChildElement("DataArray");
+    TiXmlElement* cellDataArray = piece->FirstChildElement("Cells")->FirstChildElement("DataArray");
     std::string name;
     while(cellDataArray)
     {
