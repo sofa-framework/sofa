b9239a719c9420bd168585745b40104258326924
Étienne Schmitt <etienne.schmitt@inria.fr>
2015-02-18 18:55:33 +0100

    FIX : fixed some issues with IHM2 class
    Signed-off-by: Étienne Schmitt <etienne.schmitt@inria.fr>
    
    Former-commit-id: 84ba92a57061e4f72beb0bab6fe7614f2da7704c

--- extlibs/CGoGN/include/Topology/ihmap/ihm2.h
+++ extlibs/CGoGN/include/Topology/ihmap/ihm2.h
@@ -30,166 +30,161 @@
 namespace CGoGN
 {
 
-template<typename T, unsigned int ORBIT> class AttributeHandler_IHM ;
-
-typedef EmbeddedMap2::TOPO_MAP TOPO_MAP;
-
 class ImplicitHierarchicalMap2 : public EmbeddedMap2
-{
-	template<typename T, unsigned int ORBIT> friend class AttributeHandler_IHM ;
-
+{ 
+    template <typename T, unsigned int ORBIT, class MAP>
+    friend class AttributeHandler;
 private:
-	unsigned int m_curLevel ;
-	unsigned int m_maxLevel ;
-	unsigned int m_idCount ;
+    unsigned int m_curLevel ;
+    unsigned int m_maxLevel ;
+    unsigned int m_idCount ;
 
-	DartAttribute<unsigned int, ImplicitHierarchicalMap2> m_dartLevel ;
-	DartAttribute<unsigned int, ImplicitHierarchicalMap2> m_edgeId ;
+    DartAttribute<unsigned int, EmbeddedMap2> m_dartLevel ;
+    DartAttribute<unsigned int, EmbeddedMap2> m_edgeId ;
 
-	AttributeMultiVector<unsigned int>* m_nextLevelCell[NB_ORBITS] ;
+    AttributeMultiVector<unsigned int>* m_nextLevelCell[NB_ORBITS] ;
 
 public:
-	ImplicitHierarchicalMap2() ;
+    ImplicitHierarchicalMap2() ;
+    ~ImplicitHierarchicalMap2() ;
 
-	~ImplicitHierarchicalMap2() ;
+    //!
+    /*!
+     *
+     */
+    void update_topo_shortcuts();
 
-	//!
-	/*!
-	 *
-	 */
-	void update_topo_shortcuts();
+    //!
+    /*!
+     *
+     */
+    void initImplicitProperties();
 
-	//!
-	/*!
-	 *
-	 */
-	void initImplicitProperties();
+    /**
+     * clear the map
+     * @param remove attrib remove attribute (not only clear the content)
+     */
+    void clear(bool removeAttrib);
 
-	/**
-	 * clear the map
-	 * @param remove attrib remove attribute (not only clear the content)
-	 */
-	void clear(bool removeAttrib);
+    /***************************************************
+     *             ATTRIBUTES MANAGEMENT               *
+     ***************************************************/
 
-	/***************************************************
-	 *             ATTRIBUTES MANAGEMENT               *
-	 ***************************************************/
+    template <typename T, unsigned int ORBIT, typename MAP>
+    AttributeHandler<T, ORBIT, MAP> addAttribute(const std::string& nameAttr) ;
 
-	template <typename T, unsigned int ORBIT>
-	AttributeHandler_IHM<T, ORBIT> addAttribute(const std::string& nameAttr) ;
+    template <typename T, unsigned int ORBIT, typename MAP>
+    inline AttributeHandler<T, ORBIT, MAP> getAttribute(const std::string& nameAttr) ;
 
-	template <typename T, unsigned int ORBIT>
-	AttributeHandler_IHM<T, ORBIT> getAttribute(const std::string& nameAttr) ;
+    /***************************************************
+     *                 MAP TRAVERSAL                   *
+     ***************************************************/
 
-	/***************************************************
-	 *                 MAP TRAVERSAL                   *
-	 ***************************************************/
+    inline Dart newDart() ;
 
-	inline Dart newDart() ;
+    inline Dart phi1(Dart d) const ;
 
-	inline Dart phi1(Dart d) const ;
+    inline Dart phi_1(Dart d) const ;
 
-	inline Dart phi_1(Dart d) const ;
+    inline Dart phi2(Dart d) const ;
 
-	inline Dart phi2(Dart d) const ;
+    inline Dart alpha0(Dart d) const ;
 
-	inline Dart alpha0(Dart d) const ;
+    inline Dart alpha1(Dart d) const ;
 
-	inline Dart alpha1(Dart d) const ;
+    inline Dart alpha_1(Dart d) const ;
 
-	inline Dart alpha_1(Dart d) const ;
+    inline Dart begin() const ;
 
-	inline Dart begin() const ;
+    inline Dart end() const ;
 
-	inline Dart end() const ;
+    inline void next(Dart& d) const ;
 
-	inline void next(Dart& d) const ;
+    //	template <unsigned int ORBIT, typename FUNC>
+    //	void foreach_dart_of_orbit(Cell<ORBIT> c, FUNC f) const ;
+    template <unsigned int ORBIT, typename FUNC>
+    void foreach_dart_of_orbit(Cell<ORBIT> c, const FUNC& f) const ;
 
-//	template <unsigned int ORBIT, typename FUNC>
-//	void foreach_dart_of_orbit(Cell<ORBIT> c, FUNC f) const ;
-	template <unsigned int ORBIT, typename FUNC>
-	void foreach_dart_of_orbit(Cell<ORBIT> c, const FUNC& f) const ;
-
-	template <typename FUNC>
+    template <typename FUNC>
     void foreach_dart_of_vertex(Dart d, const FUNC& f) const;
 
-	template <typename FUNC>
+    template <typename FUNC>
     void foreach_dart_of_edge(Dart d, const FUNC& f) const ;
 
-	template <typename FUNC>
+    template <typename FUNC>
     void foreach_dart_of_oriented_face(Dart d, const FUNC& f) const ;
 
-	template <typename FUNC>
+    template <typename FUNC>
     void foreach_dart_of_face(Dart d, const FUNC& f)  const;
 
-	template <typename FUNC>
+    template <typename FUNC>
     void foreach_dart_of_oriented_volume(Dart d, const FUNC& f) const ;
 
-	template <typename FUNC>
+    template <typename FUNC>
     void foreach_dart_of_volume(Dart d, const FUNC& f) const ;
 
-	template <typename FUNC>
+    template <typename FUNC>
     void foreach_dart_of_vertex1(Dart d, const FUNC& f) const;
 
-	template <typename FUNC>
+    template <typename FUNC>
     void foreach_dart_of_edge1(Dart d, const FUNC& f) const;
 
-	template <typename FUNC>
+    template <typename FUNC>
     void foreach_dart_of_cc(Dart d, const FUNC& f) const ;
 
-	/***************************************************
-	 *               MAP MANIPULATION                  *
-	 ***************************************************/
+    /***************************************************
+     *               MAP MANIPULATION                  *
+     ***************************************************/
 
-	void splitFace(Dart d, Dart e) ;
+    void splitFace(Dart d, Dart e) ;
 
     unsigned int vertexDegree(Dart d);
 
-	/***************************************************
-	 *              LEVELS MANAGEMENT                  *
-	 ***************************************************/
+    /***************************************************
+     *              LEVELS MANAGEMENT                  *
+     ***************************************************/
 
-	unsigned int getCurrentLevel() ;
+    unsigned int getCurrentLevel() ;
 
-	void setCurrentLevel(unsigned int l) ;
+    void setCurrentLevel(unsigned int l) ;
 
-	void incCurrentLevel();
+    void incCurrentLevel();
 
-	void decCurrentLevel();
+    void decCurrentLevel();
 
-	unsigned int getMaxLevel() ;
+    unsigned int getMaxLevel() ;
 
-	unsigned int getDartLevel(Dart d) ;
+    unsigned int getDartLevel(Dart d) ;
 
-	void setDartLevel(Dart d, unsigned int i) ;
+    void setDartLevel(Dart d, unsigned int i) ;
 
-	void setMaxLevel(unsigned int l);
+    void setMaxLevel(unsigned int l);
 
-	/***************************************************
-	 *             EDGE ID MANAGEMENT                  *
-	 ***************************************************/
+    /***************************************************
+     *             EDGE ID MANAGEMENT                  *
+     ***************************************************/
 
-	/**
-	 * Give a new unique id to all the edges of the map
-	 */
-	void initEdgeId() ;
+    /**
+     * Give a new unique id to all the edges of the map
+     */
+    void initEdgeId() ;
 
-	/**
-	 * Return the next available edge id
-	 */
-	unsigned int getNewEdgeId() ;
+    /**
+     * Return the next available edge id
+     */
+    unsigned int getNewEdgeId() ;
 
-	unsigned int getEdgeId(Dart d) ;
+    unsigned int getEdgeId(Dart d) ;
 
-	void setEdgeId(Dart d, unsigned int i) ;
+    void setEdgeId(Dart d, unsigned int i) ;
 
-	unsigned int getTriRefinementEdgeId(Dart d);
+    unsigned int getTriRefinementEdgeId(Dart d);
 
-	unsigned int getQuadRefinementEdgeId(Dart d);
+    unsigned int getQuadRefinementEdgeId(Dart d);
 
-	/***************************************************
-	 *               CELLS INFORMATION                 *
-	 ***************************************************/
+    /***************************************************
+     *               CELLS INFORMATION                 *
+     ***************************************************/
 
     /**
      * @brief faceDegree
@@ -198,106 +193,112 @@ public:
      */
     unsigned int faceDegree(Dart d);
 
-	/**
-	 * Return the level of insertion of the vertex of d
-	 */
-	unsigned int vertexInsertionLevel(Dart d) ;
+    /**
+     * Return the level of insertion of the vertex of d
+     */
+    unsigned int vertexInsertionLevel(Dart d) ;
 
-//	/**
-//	 * Return the level of the edge of d in the current level map
-//	 */
-//	unsigned int edgeLevel(Dart d) ;
+    //	/**
+    //	 * Return the level of the edge of d in the current level map
+    //	 */
+    //	unsigned int edgeLevel(Dart d) ;
 
-//	/**
-//	 * Return the level of the face of d in the current level map
-//	 */
-//	unsigned int faceLevel(Dart d) ;
+    //	/**
+    //	 * Return the level of the face of d in the current level map
+    //	 */
+    //	unsigned int faceLevel(Dart d) ;
 
-//	/**
-//	 * Given the face of d in the current level map,
-//	 * return a level 0 dart of its origin face
-//	 */
-//	Dart faceOrigin(Dart d) ;
+    //	/**
+    //	 * Given the face of d in the current level map,
+    //	 * return a level 0 dart of its origin face
+    //	 */
+    //	Dart faceOrigin(Dart d) ;
 
-//	/**
-//	 * Return the oldest dart of the face of d in the current level map
-//	 */
-//	Dart faceOldestDart(Dart d) ;
+    //	/**
+    //	 * Return the oldest dart of the face of d in the current level map
+    //	 */
+    //	Dart faceOldestDart(Dart d) ;
 
-//	/**
-//	 * Return true if the edge of d in the current level map
-//	 * has already been subdivided to the next level
-//	 */
-//	bool edgeIsSubdivided(Dart d) ;
+    //	/**
+    //	 * Return true if the edge of d in the current level map
+    //	 * has already been subdivided to the next level
+    //	 */
+    //	bool edgeIsSubdivided(Dart d) ;
 
-//	/**
-//	 * Return true if the edge of d in the current level map
-//	 * is subdivided to the next level,
-//	 * none of its resulting edges is in turn subdivided to the next level
-//	 * and the middle vertex is of degree 2
-//	 */
-//	bool edgeCanBeCoarsened(Dart d) ;
+    //	/**
+    //	 * Return true if the edge of d in the current level map
+    //	 * is subdivided to the next level,
+    //	 * none of its resulting edges is in turn subdivided to the next level
+    //	 * and the middle vertex is of degree 2
+    //	 */
+    //	bool edgeCanBeCoarsened(Dart d) ;
 
-//	/**
-//	 * Return true if the face of d in the current level map
-//	 * has already been subdivided to the next level
-//	 */
-//	bool faceIsSubdivided(Dart d) ;
+    //	/**
+    //	 * Return true if the face of d in the current level map
+    //	 * has already been subdivided to the next level
+    //	 */
+    //	bool faceIsSubdivided(Dart d) ;
 
-//	/**
-//	 * Return true if the face of d in the current level map
-//	 * is subdivided to the next level
-//	 * and none of its resulting faces is in turn subdivided to the next level
-//	 */
-//	bool faceIsSubdividedOnce(Dart d) ;
+    //	/**
+    //	 * Return true if the face of d in the current level map
+    //	 * is subdivided to the next level
+    //	 * and none of its resulting faces is in turn subdivided to the next level
+    //	 */
+    //	bool faceIsSubdividedOnce(Dart d) ;
 } ;
 
+
+/**
+ * partial specialization for IHM2
+ */
 template <typename T, unsigned int ORBIT>
-class AttributeHandler_IHM : public AttributeHandler<T, ORBIT, ImplicitHierarchicalMap2>
+class AttributeHandler<T,ORBIT,ImplicitHierarchicalMap2> : public AttributeHandlerGen
 {
+    friend class ImplicitHierarchicalMap2;
 public:
-	typedef T DATA_TYPE ;
+    typedef ImplicitHierarchicalMap2 MAP;
+protected:
+    MAP* m_map;
+    AttributeMultiVector<T>* m_attrib;
 
-	AttributeHandler_IHM() : AttributeHandler<T, ORBIT, ImplicitHierarchicalMap2>()
-	{}
+    void registerInMap() ;
+    void unregisterFromMap() ;
+private:
+    template <unsigned int ORBIT2>
+    AttributeHandler(const AttributeHandler<T, ORBIT2, MAP>& h) ;
+    template <unsigned int ORBIT2>
+    AttributeHandler<T, ORBIT, MAP>& operator=(const AttributeHandler<T, ORBIT2, MAP>& ta) ;
 
-	AttributeHandler_IHM(ImplicitHierarchicalMap2* m, AttributeMultiVector<T>* amv) : AttributeHandler<T, ORBIT, ImplicitHierarchicalMap2>(m, amv)
-	{}
-
-	AttributeMultiVector<T>* getDataVector() const
-	{
-		return AttributeHandler<T, ORBIT, ImplicitHierarchicalMap2>::getDataVector() ;
-	}
-
-	bool isValid() const
-	{
-		return AttributeHandler<T, ORBIT, ImplicitHierarchicalMap2>::isValid() ;
-	}
-
-	T& operator[](Dart d) ;
-
-	const T& operator[](Dart d) const ;
-
-	T& operator[](unsigned int a)
-	{
-		return AttributeHandler<T, ORBIT, ImplicitHierarchicalMap2>::operator[](a) ;
-	}
-
-	const T& operator[](unsigned int a) const
-	{
-		return AttributeHandler<T, ORBIT, ImplicitHierarchicalMap2>::operator[](a) ;
-	}
+public:
+    typedef T DATA_TYPE ;
+    AttributeHandler() ;
+    AttributeHandler(MAP* m, AttributeMultiVector<T>* amv) ;
+    AttributeHandler(const AttributeHandler<T, ORBIT, MAP>& ta) ;
+    AttributeHandler<T, ORBIT, MAP>& operator=(const AttributeHandler<T, ORBIT, MAP>& ta) ;
+    virtual ~AttributeHandler() ;
+    MAP* map() const
+    {
+        return m_map ;
+    }
+    AttributeMultiVector<T>* getDataVector() const ;
+    virtual AttributeMultiVectorGen* getDataVectorGen() const ;
+    virtual int getSizeOfType() const ;
+    virtual unsigned int getOrbit() const ;
+    unsigned int getIndex() const ;
+    virtual const std::string& name() const ;
+    virtual const std::string& typeName() const ;
+    unsigned int nbElements() const;
+    T& operator[](Cell<ORBIT> c) ;
+    const T& operator[](Cell<ORBIT> c) const ;
+    T& operator[](unsigned int a) ;
+    const T& operator[](unsigned int a) const ;
+    unsigned int insert(const T& elt) ;
+    unsigned int newElt() ;
+    void setAllValues(const T& v) ;
+    unsigned int begin() const;
+    unsigned int end() const;
+    void next(unsigned int& iter) const;
 } ;
-
-template <typename T>
-class VertexAttribute_IHM : public AttributeHandler_IHM<T, VERTEX>
-{
-public:
-	VertexAttribute_IHM() : AttributeHandler_IHM<T, VERTEX>() {}
-	VertexAttribute_IHM(const AttributeHandler_IHM<T, VERTEX>& ah) : AttributeHandler_IHM<T, VERTEX>(ah) {}
-	VertexAttribute_IHM<T>& operator=(const AttributeHandler_IHM<T, VERTEX>& ah) { this->AttributeHandler_IHM<T, VERTEX>::operator=(ah); return *this; }
-};
-
 
 } //namespace CGoGN
 
