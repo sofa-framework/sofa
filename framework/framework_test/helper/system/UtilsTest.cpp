2b4b10ff177691ecd337a451f1ce39bf7db4538e
Marc Legendre <marc.legendre@inria.fr>
2015-02-18 11:47:19 +0100

    helper: Utils: rename s2ws() and ws2s() to widenString() and narrowString()
    Curiously, I didn't think those were terrible names when I wrote them.
    
    
    Former-commit-id: 070854912f3235ca2e61572e2fbdc5fadc472261

--- framework/framework_test/helper/system/UtilsTest.cpp
+++ framework/framework_test/helper/system/UtilsTest.cpp
@@ -8,17 +8,17 @@ TEST(UtilsTest, string_to_widestring_to_string)
     std::string ascii_chars;
     for (char c = 32 ; c <= 126 ; c++)
         ascii_chars.push_back(c);
-    EXPECT_EQ(ascii_chars, Utils::ws2s(Utils::s2ws(ascii_chars)));
+    EXPECT_EQ(ascii_chars, Utils::narrowString(Utils::widenString(ascii_chars)));
 
     const std::string s("chaîne de test avec des caractères accentués");
-    EXPECT_EQ(s, Utils::ws2s(Utils::s2ws(s)));
+    EXPECT_EQ(s, Utils::narrowString(Utils::widenString(s)));
 }
 
 TEST(UtilsTest, widestring_to_string_to_widestring)
 {
     const std::string s("chaîne de test avec des caractères accentués");
-    const std::wstring ws = Utils::s2ws(s);
-    EXPECT_EQ(ws, Utils::s2ws(Utils::ws2s(ws)));
+    const std::wstring ws = Utils::widenString(s);
+    EXPECT_EQ(ws, Utils::widenString(Utils::narrowString(ws)));
 }
 
 TEST(UtilsTest, getExecutablePath)
