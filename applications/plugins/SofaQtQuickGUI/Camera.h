ba842d18d8e47db56db2b0968537ea0361143d29
Olivier Carré <ocarre@dhcp54.inrialpes.fr>
2015-02-18 17:16:51 +0100

    ADD: Camera now has a target to turn around ADD: a crosshair is now drawn during camera rotation and move CHANGE: smoothing during camera zoom has been temporarily removed ADD: camera move speed is now dependent of the "zoom factor" (i.e how close the eye is to the target)
    Former-commit-id: faea3fdfefabd7d8bea0dcd7466cf9e6bce5eddf

--- applications/plugins/SofaQtQuickGUI/Camera.h
+++ applications/plugins/SofaQtQuickGUI/Camera.h
@@ -22,36 +22,16 @@ public:
 	~Camera();
 
 public:
-	Q_PROPERTY(double zoomFactor READ zoomFactor WRITE setZoomFactor NOTIFY zoomFactorChanged)
-	Q_PROPERTY(double smoothedZoomFactor READ smoothedZoomFactor WRITE setSmoothedZoomFactor NOTIFY smoothedZoomFactorChanged)
-	Q_PROPERTY(bool smoothZoom READ smoothZoom WRITE setSmoothZoom NOTIFY smoothZoomChanged)
-	Q_PROPERTY(double zoomSpeed MEMBER myZoomSpeed NOTIFY zoomSpeedChanged)
-
-public:
-	double zoomFactor() const				{return myZoomFactor;}
-	void setZoomFactor(double newZoomFactor);
-
-	double smoothedZoomFactor() const		{return mySmoothedZoomFactor;}
-	void setSmoothedZoomFactor(double newSmoothedZoomFactor);
-
-	double smoothZoom() const				{return mySmoothZoom;}
-	void setSmoothZoom(bool newSmoothZoom);
-
-signals:
-	void zoomFactorChanged(double newZoomFactor);
-	void smoothedZoomFactorChanged(double newSmoothedZoomFactor);
-	void smoothZoomChanged(bool newEnableSmoothZoom);
-	void zoomSpeedChanged(double newZoomSpeed);
-
-public:
 	const QMatrix4x4& projection() const;
 	const QMatrix4x4& view() const;
 	const QMatrix4x4& model() const;
 
-	Q_INVOKABLE QVector3D eye() const				{return  model().column(3).toVector3D();}
-	Q_INVOKABLE QVector3D direction() const			{return -model().column(2).toVector3D();}
-	Q_INVOKABLE QVector3D up() const				{return  model().column(1).toVector3D();}
-	Q_INVOKABLE QVector3D right() const				{return  model().column(0).toVector3D();}
+    Q_INVOKABLE QVector3D eye() const				{return model().column(3).toVector3D();}
+    Q_INVOKABLE QVector3D target() const			{return myTarget;}
+
+    Q_INVOKABLE QVector3D direction() const			{return -model().column(2).toVector3D().normalized();}
+    Q_INVOKABLE QVector3D up() const				{return  model().column(1).toVector3D().normalized();}
+    Q_INVOKABLE QVector3D right() const				{return  model().column(0).toVector3D().normalized();}
 
 public:
 	void setFovY(double fovY);
@@ -64,30 +44,22 @@ public:
 public slots:
 	void move(double x, double y, double z);
 	void turn(double angleAroundX, double angleAroundY, double angleAroundZ);
-	void zoom(double factor, bool smooth = true);
+    void zoom(double factor);
 
 private:
-	void applyZoom();
-
-private:
-	double				myDistanceToCenter;
-	double				myZoomFactor;
-	double				mySmoothedZoomFactor;
-	bool				mySmoothZoom;
-	double				myZoomedDistanceToCenter;
-	double				myZoomSpeed;
-
 	double				myFovY;
 	double				myAspectRatio;
 	double				myZNear;
 	double				myZFar;
 
 	mutable QMatrix4x4	myProjection;
-	QMatrix4x4			myView;
-	mutable QMatrix4x4	myModel;
+    mutable QMatrix4x4	myView;
+    QMatrix4x4          myModel;
+
+    QVector3D           myTarget;
 
 	mutable bool		myProjectionDirty;
-	mutable bool		myModelDirty;
+    mutable bool		myViewDirty;
 };
 
 }
