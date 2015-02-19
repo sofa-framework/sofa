9d6de35feb6f05e2794aa6f69393de5fc1bd9fc7
Marc Legendre <marc.legendre@inria.fr>
2015-02-19 15:08:53 +0100

    image: fix compilation
    Former-commit-id: 742ef54eff5e94d3daeea3c161cd5ac2ea3c4e78

--- applications/plugins/image/ImageTransformEngine.h
+++ applications/plugins/image/ImageTransformEngine.h
@@ -29,7 +29,6 @@
 #include "ImageTypes.h"
 #include <sofa/core/DataEngine.h>
 #include <sofa/core/objectmodel/BaseObject.h>
-#include <sofa/component/component.h>
 #include <sofa/helper/Quater.h>
 
 
