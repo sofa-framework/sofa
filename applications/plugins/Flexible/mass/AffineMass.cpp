43aa4ea51c83a1c71824a55c32d15187167747fd
bgilles <benjamin.gilles@inria.fr>
2015-02-19 15:27:51 +0100

    Flexible: add affine mass components Former-commit-id: 0f981fbf5f4d8a2d9a080db118cc659bd67f304f

---
+++ applications/plugins/Flexible/mass/AffineMass.cpp
@@ -0,0 +1,64 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Plugins                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+
+#define SOFA_COMPONENT_MASS_AffineMass_CPP
+
+#include "../initFlexible.h"
+#include "AffineMass.h"
+#include <sofa/core/ObjectFactory.h>
+#include "../types/AffineTypes.h"
+
+
+namespace sofa {
+namespace component {
+namespace mass {
+
+using namespace sofa::defaulttype;
+
+SOFA_DECL_CLASS(AffineMass)
+
+// Register in the Factory
+int AffineMassClass = core::RegisterObject("Mass for affine frames")
+        #ifndef SOFA_FLOAT
+        .add< AffineMass< Affine3dTypes > >()
+        #endif
+        #ifndef SOFA_DOUBLE
+        .add< AffineMass< Affine3fTypes > >()
+        #endif
+        ;
+
+#ifndef SOFA_FLOAT
+template class SOFA_Flexible_API AffineMass<  Affine3dTypes >;
+#endif
+
+#ifndef SOFA_DOUBLE
+template class SOFA_Flexible_API AffineMass< Affine3fTypes >;
+#endif
+
+
+} // namespace mass
+} // namespace component
+} // namespace sofa
+
---
+++ applications/plugins/Flexible/mass/MassFromDensity.cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 beta 4      *
+*                (c) 2006-2009 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#define SOFA_Flexible_MassFromDensity_CPP
+
+#include "../initFlexible.h"
+#include "MassFromDensity.h"
+#include <sofa/core/ObjectFactory.h>
+#include "../types/AffineTypes.h"
+
+namespace sofa {
+namespace component {
+namespace engine {
+
+using namespace defaulttype;
+
+SOFA_DECL_CLASS(MassFromDensity)
+
+int MassFromDensityClass = core::RegisterObject("Compute a mass matrix from a density image")
+        .add<MassFromDensity<Affine3Types,ImageD > >(true)
+        .add<MassFromDensity<Affine3Types,ImageF > >()
+        .add<MassFromDensity<Affine3Types,ImageUI> >()
+        .add<MassFromDensity<Affine3Types,ImageUC > >()
+;
+
+template class SOFA_Flexible_API MassFromDensity<Affine3Types,ImageD  >;
+template class SOFA_Flexible_API MassFromDensity<Affine3Types,ImageF   >;
+template class SOFA_Flexible_API MassFromDensity<Affine3Types,ImageUI  >;
+template class SOFA_Flexible_API MassFromDensity<Affine3Types,ImageUC >;
+
+}
+}
+}
