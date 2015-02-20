9ac639026441190c342af72917844708ca845aea
Thomas Lemaire <thomas.lemaire@inria.fr>
2015-02-20 16:47:29 +0100

    [Image] python Image package already exists (PIL) rename to SofaImage
    Former-commit-id: 72e9ab9ccc9da62eb8d518de5901068b92223337

--- applications/plugins/image/examples/structuralAPI.py
+++ applications/plugins/image/examples/structuralAPI.py
@@ -1,15 +1,15 @@
 import SofaPython.Tools
-import Image.StructuralAPI
+import SofaImage.StructuralAPI
 
 def createScene(rootNode):
-    rootNode.createObject("RequiredPlugin", name="image")
-    im = Image.StructuralAPI.Image(rootNode,"armadillo")
+    rootNode.createObject("RequiredPlugin", name="Image")
+    im = SofaImage.StructuralAPI.Image(rootNode,"armadillo")
     im.addMeshLoader("mesh/Armadillo_simplified.obj")
     im.addImage(0.5, 1)
     im.addViewer()
     im.addExporter("armadillo.mhd")
     rootNode.createObject("OglModel", name="visu",  src="@"+SofaPython.Tools.getObjectPath(im.mesh), color="0.5 0.5 1 .5")
     
-    im2 = Image.StructuralAPI.Image(rootNode,"armadillo2")
+    im2 = SofaImage.StructuralAPI.Image(rootNode,"armadillo2")
     im2.addContainer("armadillo.mhd")
     im2.addViewer()
