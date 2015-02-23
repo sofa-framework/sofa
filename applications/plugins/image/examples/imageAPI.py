0937577a2c1f5b693317f2d589ee9d7b00b14b74
Thomas Lemaire <thomas.lemaire@inria.fr>
2015-02-23 11:00:52 +0100

    [Image] ADD: mesh visual
    Former-commit-id: 6669a74a25ce0221eee26acd1be1989b032827b7

--- applications/plugins/image/examples/imageAPI.py
+++ applications/plugins/image/examples/imageAPI.py
@@ -8,7 +8,8 @@ def createScene(rootNode):
     im.addMeshToImage(0.5)
     im.addViewer()
     im.addExporter("armadillo.mhd")
-    rootNode.createObject("OglModel", name="visu",  src="@"+SofaPython.Tools.getObjectPath(im.meshes["armadillo"].mesh), color="0.5 0.5 1 .5")
+    im.addMeshVisual("armadillo")
+    im.meshes["armadillo"].visual.setColor(.5, 0.5, 1, .5)
     
     im2 = ImageAPI.Image(rootNode,"armadillo2")
     im2.addContainer("armadillo.mhd")
