af77ff007a788f184101a7c0c3eb687a2f0f0675
Thomas Lemaire <thomas.lemaire@inria.fr>
2015-02-23 10:21:40 +0100

    [Image] ADD: API support multi meshes
    Former-commit-id: 70c6f72ce4860e9488fa9e9b21c7cda92aba0a4d

--- applications/plugins/image/python/SofaImage/ImageAPI.py
+++ applications/plugins/image/python/SofaImage/ImageAPI.py
@@ -1,29 +1,51 @@
+import os.path
+
 import SofaPython.Tools
 import SofaPython.units
 
 class Image:
+
+    class Mesh:
+        # TODO add support for ROI
+        def __init__(self):
+            self.mesh=None
+            self.visual=None
+            self.value=None
+            self.closingValue=None
+
     def __init__(self, parentNode, name, imageType="ImageUC"):
         self.imageType = imageType
         self.node = parentNode.createChild("image_"+name)
         self.name = name
-        self.mesh = None
+        self.meshes = dict()
         self.value = None
         self.closingValue = None
         self.image = None
         self.viewer = None
         self.exporter = None
 
-    def addMeshLoader(self, mesh):
-        self.mesh = SofaPython.Tools.meshLoader(self.node, mesh, name="loader")
+    def addMeshLoader(self, meshFile, value, closingValue=None, name=None):
+        mesh = Image.Mesh()
+        mesh.value = value
+        mesh.closingValue = value if closingValue is None else closingValue
+        _name = name if not name is None else os.path.splitext(os.path.basename(meshFile))[0]
+        mesh.mesh = SofaPython.Tools.meshLoader(self.node, meshFile, name="meshLoader_"+_name)
+        self.meshes[_name] = mesh
 
-    # TODO add support for ROI
-    def addImage(self, voxelSize, value, closingValue=None):
-        if self.mesh is None:
-            print "[ImageAPI.Image] ERROR: no mesh"
-        self.value = value
-        self.closingValue = value if closingValue is None else closingValue
-        meshPath = SofaPython.Tools.getObjectPath(self.mesh)
-        self.image = self.node.createObject('MeshToImageEngine', template=self.imageType, name="image", position="@"+meshPath+".position", triangles="@"+meshPath+".triangles", voxelSize=SofaPython.units.length_from_SI(voxelSize), value=self.value, closingValue=self.closingValue, fillInside="true", rotateImage="false",  connectivity="6") # TODO connectivity ?
+    def addMeshToImage(self, voxelSize):
+        args=dict()
+        i=1
+        for name,mesh in self.meshes.iteritems():
+            if mesh.mesh is None:
+                print "[ImageAPI.Image] ERROR: no mesh for", name
+            meshPath = SofaPython.Tools.getObjectPath(mesh.mesh)
+            args["position"+(str(i) if i>1 else "")]="@"+meshPath+".position"
+            args["triangles"+(str(i) if i>1 else "")]="@"+meshPath+".triangles"
+            args["value"+(str(i) if i>1 else "")]=mesh.value
+#            args["closingValue"+(str(i) if i>1 else "")]=mesh.closingValue not yet a closingValue per mesh !
+            args["closingValue"]=mesh.closingValue # last wins
+            i+=1
+        self.image = self.node.createObject('MeshToImageEngine', template=self.imageType, name="image", voxelSize=SofaPython.units.length_from_SI(voxelSize), subdiv=8, fillInside="true", rotateImage="false", nbMeshes=len(self.meshes), **args)
 
     def addViewer(self):
         if self.image is None:
