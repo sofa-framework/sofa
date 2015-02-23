0d7ceba9a9c108ef61f1e848ee9097b7479b5de7
Thomas Lemaire <thomas.lemaire@inria.fr>
2015-02-23 10:21:40 +0100

    [Image] ADD: API support multi meshes
    Former-commit-id: 70c6f72ce4860e9488fa9e9b21c7cda92aba0a4d

--- applications/plugins/image/examples/imageAPI.py
+++ applications/plugins/image/examples/imageAPI.py
@@ -4,11 +4,11 @@ from SofaImage import ImageAPI
 def createScene(rootNode):
     rootNode.createObject("RequiredPlugin", name="Image")
     im = ImageAPI.Image(rootNode,"armadillo")
-    im.addMeshLoader("mesh/Armadillo_simplified.obj")
-    im.addImage(0.5, 1)
+    im.addMeshLoader("mesh/Armadillo_simplified.obj",1,name="armadillo")
+    im.addMeshToImage(0.5)
     im.addViewer()
     im.addExporter("armadillo.mhd")
-    rootNode.createObject("OglModel", name="visu",  src="@"+SofaPython.Tools.getObjectPath(im.mesh), color="0.5 0.5 1 .5")
+    rootNode.createObject("OglModel", name="visu",  src="@"+SofaPython.Tools.getObjectPath(im.meshes["armadillo"].mesh), color="0.5 0.5 1 .5")
     
     im2 = ImageAPI.Image(rootNode,"armadillo2")
     im2.addContainer("armadillo.mhd")
