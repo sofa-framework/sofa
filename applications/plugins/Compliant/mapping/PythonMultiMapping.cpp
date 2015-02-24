4114854af7fe5b35d6d299ba4e233bb42fa1af64
Maxime Tournier <maxime.tournier@brain.riken.jp>
2015-02-24 21:32:05 +0100

    Compliant: added general python mapping
    Former-commit-id: 6b7c0c44a68283e1ce8ce12553355b6aeae26186

---
+++ applications/plugins/Compliant/mapping/PythonMultiMapping.cpp
@@ -0,0 +1,46 @@
+#include "PythonMultiMapping.h"
+
+#include <sofa/core/MultiMapping.inl>
+#include <sofa/core/ObjectFactory.h>
+
+
+namespace sofa {
+namespace component {
+namespace mapping {
+
+SOFA_DECL_CLASS(PythonMultiMapping)
+
+using namespace defaulttype;
+
+// Register in the Factory
+static int PythonMultiMappingClass = core::RegisterObject("Arbitrary Python mapping")
+
+#ifndef SOFA_FLOAT
+.add< PythonMultiMapping< Vec6dTypes, Vec1dTypes > >()
+.add< PythonMultiMapping< Vec3dTypes, Vec1dTypes > >()
+.add< PythonMultiMapping< Vec1dTypes, Vec1dTypes > >()
+#endif
+#ifndef SOFA_DOUBLE
+.add< PythonMultiMapping< Vec6fTypes, Vec1fTypes > >()
+.add< PythonMultiMapping< Vec3fTypes, Vec1fTypes > >()
+.add< PythonMultiMapping< Vec1fTypes, Vec1fTypes > >()
+#endif
+;
+
+#ifndef SOFA_FLOAT
+template class SOFA_Compliant_API PythonMultiMapping<  Vec6dTypes, Vec1dTypes >;
+template class SOFA_Compliant_API PythonMultiMapping<  Vec3dTypes, Vec1dTypes >;
+template class SOFA_Compliant_API PythonMultiMapping<  Vec1dTypes, Vec1dTypes >;
+#endif
+
+#ifndef SOFA_DOUBLE
+template class SOFA_Compliant_API PythonMultiMapping< Vec6fTypes, Vec1fTypes >;
+template class SOFA_Compliant_API PythonMultiMapping< Vec3fTypes, Vec1fTypes >;
+template class SOFA_Compliant_API PythonMultiMapping< Vec1fTypes, Vec1fTypes >;
+#endif
+
+
+
+}
+}
+}
