314117a9b821e564e7b3fb0914f49661abf13a43
Thomas Lemaire <thomas.lemaire@inria.fr>
2015-02-24 14:56:50 +0100

    [Flexible] ADD: high level API for ShapeFunction
    Former-commit-id: 1429cca415b9b9f9ba4ef1844adbd024cbe4beef

--- applications/plugins/Flexible/python/Flexible/FlexibleAPI.py
+++ applications/plugins/Flexible/python/Flexible/FlexibleAPI.py
@@ -1,4 +1,5 @@
 import math
+import os.path
 
 import SofaPython.Tools
 from SofaPython.Tools import listToStr as concat
@@ -27,3 +28,37 @@ class Deformable:
 
     def addSkinning(self, bonesPath, indices, weights):
         self.skinning = self.node.createObject("LinearMapping", template="Rigid3d,Vec3d", name="skinning", input="@"+bonesPath, indices=concat(indices), weights=concat(weights))
+
+class ShapeFunction:
+    def __init__(self, node, name):
+        self.node = node.createChild(name)
+        self.name = name
+        self.shapeFunction=None
+   
+    def addVoronoi(self, position, image):
+        """ Add a Voronoi shape function using position from position component and BranchingImage image
+        """
+        imagePath = SofaPython.Tools.getObjectPath(image.branchingImage)
+        self.shapeFunction = self.node.createObject('VoronoiShapeFunction', name="shapeFunction", template="ShapeFunctiond,BranchingImageD", position="@"+SofaPython.Tools.getObjectPath(position)+".position", src="@"+imagePath, method=0, nbRef=8, bias=True)
+   
+    def getFilenameIndices(self, filenamePrefix=None, directory=""):
+        _filename=filenamePrefix if not filenamePrefix is None else self.name
+        _filename+="_indices.mhd"
+        _filename=os.path.join(directory, _filename)
+        return _filename
+
+    def getFilenameWeights(self, filenamePrefix=None, directory=""):
+        _filename=filenamePrefix if not filenamePrefix is None else self.name
+        _filename+="_weights.mhd"
+        _filename=os.path.join(directory, _filename)
+        return _filename
+
+    def addExporter(self, filenamePrefix=None, directory=""):
+        if self.shapeFunction is None:
+            print "[FelexibleAPI.ShapeFunction] ERROR: no shapeFunction"
+        sfPath = SofaPython.Tools.getObjectPath(self.shapeFunction)
+        self.node.createObject('ImageExporter', template="BranchingImageUI", name="exporterIndices", image="@"+sfPath+".indices", transform="@"+sfPath+".transform", filename=self.getFilenameIndices(filenamePrefix, directory), exportAtEnd=True, printLog=True)
+        self.node.createObject('ImageExporter', template="BranchingImageD", name="exporterWeights", image="@"+sfPath+".weights", transform="@"+sfPath+".transform", filename=self.getFilenameWeights(filenamePrefix, directory), exportAtEnd=True, printLog=True)
+       
+       
+       
