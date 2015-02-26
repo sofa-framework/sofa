c7e918a51a787a3e1442dd2c963bf729bc85cdc2
Maxime Tournier <maxime.tournier@brain.riken.jp>
2015-02-26 10:38:03 +0100

    Compliant: added RigidJointMultiMapping for rigid out types (discard log coordinates)
    Former-commit-id: 2d21f8b62b94b60fe039dac17ae28bfed4d19e84

--- applications/plugins/Compliant/mapping/RigidJointMultiMapping.cpp
+++ applications/plugins/Compliant/mapping/RigidJointMultiMapping.cpp
@@ -19,23 +19,26 @@ SOFA_DECL_CLASS(RigidJointMultiMapping)
 using namespace defaulttype;
 
 // Register in the Factory
-int RigidJointMultiMappingClass = core::RegisterObject("Computes relative rigid configurations")
+static int RigidJointMultiMappingClass = core::RegisterObject("Computes relative rigid configurations")
 
 #ifndef SOFA_FLOAT
-.add< RigidJointMultiMapping< Rigid3dTypes, Vec6dTypes > >()
+    .add< RigidJointMultiMapping< Rigid3dTypes, Vec6dTypes > >()
+    .add< RigidJointMultiMapping< Rigid3dTypes, Rigid3dTypes > >()
 #endif
 #ifndef SOFA_DOUBLE
-.add< RigidJointMultiMapping< Rigid3fTypes, Vec6fTypes > >()
+    .add< RigidJointMultiMapping< Rigid3fTypes, Vec6fTypes > >()
+    .add< RigidJointMultiMapping< Rigid3fTypes, Rigid3fTypes > >()
 #endif
 ;
 
 #ifndef SOFA_FLOAT
 template class SOFA_Compliant_API RigidJointMultiMapping<  Rigid3dTypes, Vec6dTypes >;
+template class SOFA_Compliant_API RigidJointMultiMapping<  Rigid3dTypes, Rigid3dTypes >;
 #endif
 
 #ifndef SOFA_DOUBLE
 template class SOFA_Compliant_API RigidJointMultiMapping< Rigid3fTypes, Vec6fTypes >;
-
+template class SOFA_Compliant_API RigidJointMultiMapping< Rigid3fTypes, Rigid3fTypes >;
 #endif
 
 
