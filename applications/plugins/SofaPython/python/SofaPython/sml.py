13b6df04da1b094f82f213b6232610778c5ce271
Thomas Lemaire <thomas.lemaire@inria.fr>
2015-03-03 16:27:48 +0100

    [SofaPython.sml] rigid and deformable are fused an object tag
    Former-commit-id: 894ec1163b2c1347d957dd2582ed764e2e30311d

--- applications/plugins/SofaPython/python/SofaPython/sml.py
+++ applications/plugins/SofaPython/python/SofaPython/sml.py
@@ -55,7 +55,7 @@ class Model:
                 for d in g.iter("data"):
                     self.group[g.attrib["id"]].data[d.attrib["name"]]=parseData(d)                    
     
-    class Rigid:
+    class Object:
         def __init__(self, objXml):
             parseIdName(self,objXml)
             parseTag(self,objXml)
@@ -67,6 +67,7 @@ class Model:
                 self.density=float(objXml.find("density").text)
             if not objXml.find("mass") is None:
                 self.mass = float(objXml.find("mass").text)
+            self.skinnings=list()
 
     class Offset:
         def __init__(self, offsetXml):
@@ -105,24 +106,14 @@ class Model:
             for dof in jointXml.iter("dof"):
                 self.dofs.append(Model.Dof(dof))
         
-    class Deformable:
         
-        class Skinning:
-            """ Skinning definition, vertices index influenced by rigid with weight
-            """
-            def __init__(self):
-                self.rigid=None
-                self.index=list()
-                self.weight=list()
-        
-        def __init__(self,objXml):
-            parseIdName(self,objXml)
-            parseTag(self,objXml)
-            self.position = Tools.strToListFloat(objXml.find("position").text)
-            self.mesh = None
-            self.indices=dict()
-            self.weights=dict()
-            self.skinnings=list()
+    class Skinning:
+        """ Skinning definition, vertices index influenced by bone with weight
+        """
+        def __init__(self):
+            self.object=None
+            self.index=list()
+            self.weight=list()
 
     class Surface:
         def __init__(self):
@@ -145,12 +136,10 @@ class Model:
         self.modelDir = os.path.dirname(filename)
         self.units=dict()
         self.meshes=dict()
-        self.rigids=dict()
-        #self.rigidsbyType=dict()
+        self.objects=dict()
+        #self.objectsbyType=dict()
         self.genericJoints=dict()
-        self.deformables=dict()
         self.slidingContacts=dict()
-        #self.deformablesByType=dict()
         
         with open(filename,'r') as f:
             # TODO automatic DTD validation could go here, not available in python builtin ElementTree module
@@ -176,41 +165,33 @@ class Model:
                         print "WARNING: sml.Model: mesh not found:", mesh.source
                     self.meshes[m.attrib["id"]] = mesh
                     
-            # rigids
-            for r in modelXml.iter("rigid"):
-                if r.attrib["id"] in self.rigids: #TODO check deformables too
-                    print "ERROR: sml.Model: rigid defined twice, id:", r.attrib["id"]
+            # objects
+            for objXml in modelXml.iter("object"):
+                if objXml.attrib["id"] in self.objects:
+                    print "ERROR: sml.Model: object defined twice, id:", r.attrib["id"]
                     continue
-                rigid=Model.Rigid(r)
-                self.parseMesh(rigid, r)
-                self.rigids[rigid.id]=rigid
-            
-            # joints
-            self.parseJointGenerics(modelXml)
-            
-            #deformable
-            for d in modelXml.iter("deformable"): #TODO check rigids too
-                if d.attrib["id"] in self.deformables:
-                    print "ERROR: sml.Model: deformable defined twice, id:", d.attrib["id"]
-                    continue
-                deformable=Model.Deformable(d)
-                self.parseMesh(deformable, d)
-                mesh=deformable.mesh # shortcut
-                
-                for s in d.iter("skinning"):
-                    if not s.attrib["rigid"] in self.rigids:
-                        print "ERROR: sml.Model: skinning for deformable {0}: rigid {1} is not defined".format(name, s.attrib["rigid"])
+                object=Model.Object(objXml)
+                self.parseMesh(object, objXml)
+
+                mesh=object.mesh # shortcut
+
+                for s in objXml.iter("skinning"):
+                    if not s.attrib["object"] in self.objects:
+                        print "ERROR: sml.Model: skinning for object {0}: object {1} is not defined".format(name, s.attrib["object"])
                         continue
-                    skinning = Model.Deformable.Skinning()
-                    skinning.rigid = self.rigids[s.attrib["rigid"]]
+                    skinning = Model.Skinning()
+                    skinning.object = self.objects[s.attrib["object"]]
                     if not (s.attrib["group"] in mesh.group and s.attrib["weight"] in mesh.group[s.attrib["group"]].data):
-                        print "ERROR: sml.Model: skinning for deformable {0}: group {1} - weight {2} is not defined".format(name, s.attrib["group"], s.attrib["weight"])
+                        print "ERROR: sml.Model: skinning for object {0}: group {1} - weight {2} is not defined".format(name, s.attrib["group"], s.attrib["weight"])
                         continue
                     skinning.index = mesh.group[s.attrib["group"]].index
                     skinning.weight = mesh.group[s.attrib["group"]].data[s.attrib["weight"]]
-                    deformable.skinnings.append(skinning)
-                
-                self.deformables[deformable.id]=deformable
+                    object.skinnings.append(skinning)
+
+                self.objects[object.id]=object
+            
+            # joints
+            self.parseJointGenerics(modelXml)
                 
             # contacts
             for c in modelXml.iter("contactSliding"):
@@ -221,10 +202,8 @@ class Model:
                 surfaces=c.findall("surface")
                 for i,s in enumerate(surfaces):
                     contact.surfaces[i] = Model.Surface()
-                    if s.attrib["object"] in self.rigids:
-                        contact.surfaces[i].object = self.rigids[s.attrib["object"]]
-                    elif s.attrib["object"] in self.deformables:
-                        contact.surfaces[i].object = self.deformables[s.attrib["object"]]
+                    if s.attrib["object"] in self.objects:
+                        contact.surfaces[i].object = self.objects[s.attrib["object"]]
                     else:
                         print "ERROR: sml.Model: in contact {0}, unknown object {1} referenced".format(contact.name, s.attrib["object"])
                     contact.surfaces[i].mesh = contact.surfaces[i].object.mesh # for now a single mesh is supported
@@ -292,35 +271,27 @@ class BaseScene:
         self.node=parentNode.createChild(self.model.name)
 
 class SceneDisplay(BaseScene):
-    """ Creates a scene to display rigids and deformables meshes
+    """ Creates a scene to display object meshes
     """
     def __init__(self,parentNode,model):
         BaseScene.__init__(self,parentNode,model)
-        self.param.rigidColor="1. 0. 0."
-        self.param.deformableColor="0. 1. 0."
+        self.param.colorDefault="1. 1. 1."
         self.param.colorByTag=dict()
    
     def getTagColor(self, tags):
+        """ get the color from the given tags
+        if several tags are defined, which corresponds to several colors, one of these color is returned
+        """
         tag = tags & set(self.param.colorByTag.keys())
         if len(tag)==0:
-            return None
+            return self.param.colorDefault
         else:
             return self.param.colorByTag[tag.pop()]
 
 
     def createScene(self):
         model=self.model # shortcut
-        for rigid in model.rigids.values():
-            print "Display rigid:",rigid.name
-            color = self.getTagColor(rigid.tags)
-            if color is None:
-                color = self.param.rigidColor
-            insertVisual(self.node, rigid, color)
-        
-        for deformable in model.deformables.values():
-            print "Display deformable:",deformable.name
-            color = self.getTagColor(deformable.tags)
-            if color is None:
-                color = self.param.deformableColor
-            insertVisual(self.node, deformable, color)
-            
+        for object in model.objects.values():
+            print "Display object:", object.name
+            color = self.getTagColor(object.tags)
+            insertVisual(self.node, object, color)
