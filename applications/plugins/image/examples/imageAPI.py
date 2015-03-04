a0095a4c86745f2bb90aa0340e0e68ff6f6dd45e
Thomas Lemaire <thomas.lemaire@inria.fr>
2015-03-04 14:57:59 +0100

    [Image] SofaImage API refactoring
    Former-commit-id: 430db8ed2012602fb0c19e2f70f7c65174be253b

--- applications/plugins/image/examples/imageAPI.py
+++ applications/plugins/image/examples/imageAPI.py
@@ -1,9 +1,9 @@
 import SofaPython.Tools
-from SofaImage import ImageAPI
+import SofaImage.API
 
 def createScene(rootNode):
     rootNode.createObject("RequiredPlugin", name="Image")
-    im = ImageAPI.Image(rootNode,"armadillo")
+    im = SofaImage.API.Image(rootNode,"armadillo")
     im.addMeshLoader("mesh/Armadillo_simplified.obj",1,name="armadillo")
     im.addMeshToImage(0.5)
     im.addViewer()
@@ -11,6 +11,6 @@ def createScene(rootNode):
     im.addMeshVisual("armadillo")
     im.meshes["armadillo"].visual.setColor(.5, 0.5, 1, .5)
     
-    im2 = ImageAPI.Image(rootNode,"armadillo2")
+    im2 = SofaImage.API.Image(rootNode,"armadillo2")
     im2.addContainer(im.getFilename())
     im2.addViewer()
--- applications/plugins/image/python/python.cmake
+++ applications/plugins/image/python/python.cmake
@@ -1,4 +1,4 @@
 set(PYTHON_FILES
     python/SofaImage/__init__.py
-    python/SofaImage/ImageAPI.py
+    python/SofaImage/API.py
 )
