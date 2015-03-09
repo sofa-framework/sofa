0a4a02ac31c931d75ba136a3044253a589c5d13d
nesme <matthieu.nesme@inria.fr>
2015-03-09 15:18:46 +0100

    RigidMapping: adding assembled geometric stiffness matrix
    Former-commit-id: 08cb01adf6d89efbd7c6ef7cc064f1e184308447

--- modules/SofaRigid/RigidMapping.cpp
+++ modules/SofaRigid/RigidMapping.cpp
@@ -84,6 +84,28 @@ template class SOFA_RIGID_API RigidMapping< Rigid2fTypes, Vec2dTypes >;
 #endif
 
 
+
+
+
+#ifndef SOFA_FLOAT
+template<>
+const defaulttype::BaseMatrix* RigidMapping< sofa::defaulttype::Rigid2dTypes, sofa::defaulttype::Vec2dTypes >::getK()
+{
+    serr<<"TODO: assembled geometric stiffness not implemented"<<sendl;
+    return NULL;
+}
+#endif
+#ifndef SOFA_DOUBLE
+template<>
+const defaulttype::BaseMatrix* RigidMapping< sofa::defaulttype::Rigid2fTypes, sofa::defaulttype::Vec2fTypes >::getK()
+{
+    serr<<"TODO: assembled geometric stiffness not implemented"<<sendl;
+    return NULL;
+}
+#endif
+
+
+
 } // namespace mapping
 
 } // namespace component
