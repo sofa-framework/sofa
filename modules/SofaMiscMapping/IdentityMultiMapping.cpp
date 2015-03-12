3ad8da07c4b123ef94dcd7511d646e526bc80e94
nesme <matthieu.nesme@inria.fr>
2015-03-12 11:18:35 +0100

    adding IdentityMultiMapping to concatenate several states (with its example)
    Former-commit-id: 76049d72d67c6bd00c476139965db485e6891d56

---
+++ modules/SofaMiscMapping/IdentityMultiMapping.cpp
@@ -0,0 +1,70 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#define SOFA_COMPONENT_MAPPING_IDENTITYMULTIMAPPING_CPP
+
+#include <SofaMiscMapping/IdentityMultiMapping.inl>
+#include <sofa/defaulttype/Vec3Types.h>
+#include <sofa/defaulttype/RigidTypes.h>
+#include <sofa/core/ObjectFactory.h>
+
+using namespace sofa::defaulttype;
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace mapping
+{
+
+SOFA_DECL_CLASS(IdentityMultiMapping)
+
+// Register in the Factory
+int IdentityMultiMappingClass = core::RegisterObject("Concatenate several States together")
+#ifndef SOFA_FLOAT
+    .add< IdentityMultiMapping< Vec3dTypes, Vec3dTypes > >()
+    .add< IdentityMultiMapping< Rigid3dTypes, Rigid3dTypes > >()
+#endif
+#ifndef SOFA_DOUBLE
+    .add< IdentityMultiMapping< Vec3fTypes, Vec3fTypes > >()
+    .add< IdentityMultiMapping< Rigid3fTypes, Rigid3fTypes > >()
+#endif
+        ;
+
+#ifndef SOFA_FLOAT
+template class SOFA_MISC_MAPPING_API IdentityMultiMapping< Vec3dTypes, Vec3dTypes >;
+template class SOFA_MISC_MAPPING_API IdentityMultiMapping< Rigid3dTypes, Rigid3dTypes >;
+#endif
+#ifndef SOFA_DOUBLE
+template class SOFA_MISC_MAPPING_API IdentityMultiMapping< Vec3fTypes, Vec3fTypes >;
+template class SOFA_MISC_MAPPING_API IdentityMultiMapping< Rigid3fTypes, Rigid3fTypes >;
+#endif
+
+} // namespace mapping
+
+} // namespace component
+
+} // namespace sofa
---
+++ modules/SofaMiscMapping/IdentityMultiMapping.h
@@ -0,0 +1,115 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef SOFA_COMPONENT_MAPPING_IDENTITYMULTIMAPPING_H
+#define SOFA_COMPONENT_MAPPING_IDENTITYMULTIMAPPING_H
+
+#include <sofa/core/MultiMapping.h>
+//#include <sofa/core/behavior/BaseMechanicalState.h>
+#include <sofa/component/component.h>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace mapping
+{
+
+/// concatanate several entire mechanical states together
+template <class TIn, class TOut>
+class IdentityMultiMapping : public core::MultiMapping<TIn, TOut>
+{
+public:
+    SOFA_CLASS(SOFA_TEMPLATE2(IdentityMultiMapping, TIn, TOut), SOFA_TEMPLATE2(core::MultiMapping, TIn, TOut));
+
+    typedef core::MultiMapping<TIn, TOut> Inherit;
+    typedef TIn In;
+    typedef TOut Out;
+
+    typedef typename Out::VecCoord OutVecCoord;
+    typedef typename Out::VecDeriv OutVecDeriv;
+    typedef typename Out::Coord OutCoord;
+    typedef typename Out::Deriv OutDeriv;
+    typedef typename Out::MatrixDeriv OutMatrixDeriv;
+    typedef typename In::VecCoord InVecCoord;
+    typedef typename In::VecDeriv InVecDeriv;
+    typedef typename In::Coord InCoord;
+    typedef typename In::Deriv InDeriv;
+    typedef typename In::MatrixDeriv InMatrixDeriv;
+    typedef typename InCoord::value_type Real;
+    typedef typename helper::vector <const InVecCoord*> vecConstInVecCoord;
+    typedef typename helper::vector<OutVecCoord*> vecOutVecCoord;
+
+    typedef Data<InVecCoord> InDataVecCoord;
+    typedef Data<InVecDeriv> InDataVecDeriv;
+    typedef Data<InMatrixDeriv> InDataMatrixDeriv;
+
+    typedef Data<OutVecCoord> OutDataVecCoord;
+    typedef Data<OutVecDeriv> OutDataVecDeriv;
+    typedef Data<OutMatrixDeriv> OutDataMatrixDeriv;
+
+
+    virtual void init();
+
+    virtual void apply(const core::MechanicalParams* mparams, const helper::vector<OutDataVecCoord*>& dataVecOutPos, const helper::vector<const InDataVecCoord*>& dataVecInPos);
+    virtual void applyJ(const core::MechanicalParams* mparams, const helper::vector<OutDataVecDeriv*>& dataVecOutVel, const helper::vector<const InDataVecDeriv*>& dataVecInVel);
+    virtual void applyJT(const core::MechanicalParams* mparams, const helper::vector<InDataVecDeriv*>& dataVecOutForce, const helper::vector<const OutDataVecDeriv*>& dataVecInForce);
+    virtual void applyDJT(const core::MechanicalParams* /*mparams*/, core::MultiVecDerivId /*inForce*/, core::ConstMultiVecDerivId /*outForce*/) {}
+    virtual void applyJT( const core::ConstraintParams* cparams, const helper::vector< InDataMatrixDeriv* >& dataMatOutConst, const helper::vector< const OutDataMatrixDeriv* >& dataMatInConst );
+
+    virtual const helper::vector<sofa::defaulttype::BaseMatrix*>* getJs();
+
+
+protected :
+
+    IdentityMultiMapping();
+    virtual ~IdentityMultiMapping();
+
+    vector<defaulttype::BaseMatrix*> baseMatrices;      ///< Jacobian of the mapping, in a vector
+
+    size_t m_outSize; ///< size of the mapped state
+
+};
+
+
+#if defined(SOFA_EXTERN_TEMPLATE) && !defined(SOFA_COMPONENT_MAPPING_IDENTITYMULTIMAPPING_CPP)
+#ifndef SOFA_FLOAT
+extern template class SOFA_MISC_MAPPING_API IdentityMultiMapping< defaulttype::Vec3dTypes, defaulttype::Vec3dTypes >;
+extern template class SOFA_MISC_MAPPING_API IdentityMultiMapping< defaulttype::Rigid3dTypes, defaulttype::Rigid3dTypes >;
+#endif
+#ifndef SOFA_DOUBLE
+extern template class SOFA_MISC_MAPPING_API IdentityMultiMapping< defaulttype::Vec3fTypes, defaulttype::Vec3fTypes >;
+extern template class SOFA_MISC_MAPPING_API IdentityMultiMapping< defaulttype::Rigid3fTypes, defaulttype::Rigid3fTypes >;
+#endif
+#endif
+
+} // namespace mapping
+
+} // namespace component
+
+} // namespace sofa
+
+#endif //SOFA_COMPONENT_MAPPING_IDENTITYMULTIMAPPING_H
