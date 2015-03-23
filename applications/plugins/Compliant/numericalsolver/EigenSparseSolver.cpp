fcefe68561798058a399b91bef201e4e31f272d9
nesme <matthieu.nesme@inria.fr>
2015-03-23 14:23:48 +0100

    LDLTSolver: pass only a triangular matrix to solve the Schur complement.
    Former-commit-id: 442aedc2795dfc45a86254a92504e9c0fe57e0b9

--- applications/plugins/Compliant/numericalsolver/EigenSparseSolver.cpp
+++ applications/plugins/Compliant/numericalsolver/EigenSparseSolver.cpp
@@ -11,12 +11,12 @@ namespace linearsolver {
 
 
 
-template class SOFA_Compliant_API EigenSparseSolver< Eigen::SimplicialLDLT< AssembledSystem::cmat >, true >;
+template class SOFA_Compliant_API EigenSparseSolver< LDLTSparseLinearSolver, true, LDLTSparseLinearSolver::UpLo >;
 SOFA_DECL_CLASS(LDLTSolver)
 static int LDLTSolverClass = core::RegisterObject("Direct LDLT solver").add< LDLTSolver >();
 
 
-template class SOFA_Compliant_API EigenSparseSolver< Eigen::SparseLU< AssembledSystem::cmat >, false >;
+template class SOFA_Compliant_API EigenSparseSolver< LUSparseLinearSolver >;
 SOFA_DECL_CLASS(LUSolver)
 static int LUSolverClass = core::RegisterObject("Direct LU solver").add< LUSolver >();
 
@@ -25,17 +25,17 @@ static int LUSolverClass = core::RegisterObject("Direct LU solver").add< LUSolve
 /////////////////////////////////////////////
 
 
-template class SOFA_Compliant_API EigenSparseIterativeSolver< Eigen::ConjugateGradient< AssembledSystem::cmat >, true >;
+template class SOFA_Compliant_API EigenSparseIterativeSolver< CGSparseLinearSolver, true >;
 SOFA_DECL_CLASS(EigenCGSolver)
 static int EigenCGSolverClass = core::RegisterObject("Conjugate Gradient solver").add< EigenCGSolver >();
 
 
-template class SOFA_Compliant_API EigenSparseIterativeSolver< Eigen::BiCGSTAB< AssembledSystem::cmat >, false >;
+template class SOFA_Compliant_API EigenSparseIterativeSolver< BiCGSTABSparseLinearSolver >;
 SOFA_DECL_CLASS(EigenBiCGSTABSolver)
 static int EigenBiCGSTABSolverClass = core::RegisterObject("Bi Conjugate Gradient stabilized solver").add< EigenBiCGSTABSolver >();
 
 
-template class SOFA_Compliant_API EigenSparseIterativeSolver< Eigen::MINRES< AssembledSystem::cmat >, true >;
+template class SOFA_Compliant_API EigenSparseIterativeSolver< MINRESSparseLinearSolver, true >;
 SOFA_DECL_CLASS(EigenMinresSolver)
 static int EigenMinresSolverClass = core::RegisterObject("MINRES solver").add< EigenMinresSolver >();
 
