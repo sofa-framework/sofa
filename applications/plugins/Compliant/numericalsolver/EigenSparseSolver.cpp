429262a01ab5b507a7e1657f9210f43c36dd1e7d
nesme <matthieu.nesme@inria.fr>
2015-03-23 10:43:49 +0100

    Compliant: specializing generic EigenSparseSolver with Eigen's iterative solvers (CG and BiCGSTAB) - just for fun.
    Former-commit-id: 361b466e3dbc5be61c31b2d9fc23f0931003d44f

--- applications/plugins/Compliant/numericalsolver/EigenSparseSolver.cpp
+++ applications/plugins/Compliant/numericalsolver/EigenSparseSolver.cpp
@@ -11,14 +11,28 @@ namespace linearsolver {
 
 
 
-template class EigenSparseSolver< Eigen::SimplicialLDLT< AssembledSystem::cmat >, true >;
+template class SOFA_Compliant_API EigenSparseSolver< Eigen::SimplicialLDLT< AssembledSystem::cmat >, true >;
 SOFA_DECL_CLASS(LDLTSolver)
 static int LDLTSolverClass = core::RegisterObject("Direct LDLT solver").add< LDLTSolver >();
 
 
-template class EigenSparseSolver< Eigen::SparseLU< AssembledSystem::cmat >, false >;
+template class SOFA_Compliant_API EigenSparseSolver< Eigen::SparseLU< AssembledSystem::cmat >, false >;
 SOFA_DECL_CLASS(LUSolver)
 static int LUSolverClass = core::RegisterObject("Direct LU solver").add< LUSolver >();
+
+
+
+/////////////////////////////////////////////
+
+
+template class SOFA_Compliant_API EigenSparseIterativeSolver< Eigen::ConjugateGradient< AssembledSystem::cmat >, true >;
+SOFA_DECL_CLASS(EigenCGSolver)
+static int EigenCGSolverClass = core::RegisterObject("Conjugate Gradient solver").add< EigenCGSolver >();
+
+
+template class SOFA_Compliant_API EigenSparseIterativeSolver< Eigen::BiCGSTAB< AssembledSystem::cmat >, false >;
+SOFA_DECL_CLASS(EigenBiCGSTABSolver)
+static int EigenBiCGSTABSolverClass = core::RegisterObject("Bi Conjugate Gradient stabilized solver").add< EigenBiCGSTABSolver >();
 
 
 }
