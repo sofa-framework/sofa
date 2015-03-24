b9e242af43c67966ea5aab3ab57b1439932e4aa1
nesme <matthieu.nesme@inria.fr>
2015-03-24 15:58:13 +0100

    Compliant: adding iterative Response (just for fun)
    Former-commit-id: 57d4b10aa6f1855b8507ba94df935c59ddcb38fc

--- applications/plugins/Compliant/numericalsolver/EigenSparseResponse.h
+++ applications/plugins/Compliant/numericalsolver/EigenSparseResponse.h
@@ -4,6 +4,8 @@
 #include "Response.h"
 #include <Eigen/SparseCholesky>
 #include <Eigen/SparseLU>
+#include <Eigen/IterativeLinearSolvers>
+#include <unsupported/Eigen/IterativeSolvers>
 
 namespace sofa {
 namespace component {
@@ -11,7 +13,7 @@ namespace linearsolver {
 
 /// Solving the dynamics equation
 /// @warning with "constant" set to true, mass and stiffness cannot be added dynamically (like a mouse-spring or penalities)
-template<class LinearSolver, int symmetric>
+template<class LinearSolver, bool symmetric=false>
 class SOFA_Compliant_API EigenSparseResponse : public Response {
 public:
     SOFA_ABSTRACT_CLASS(SOFA_TEMPLATE2(EigenSparseResponse,LinearSolver,symmetric), Response);
@@ -20,7 +22,7 @@ public:
 
     virtual void factor(const mat& sys, bool semidefinite=false);
 	virtual void solve(cmat& lval, const cmat& rval) const;
-	virtual void solve(vec& lval,  const vec& rval) const;
+    virtual void solve(vec& lval, const vec& rval) const;
     virtual void reinit();
 
     /// Add identity*regularize to matrix H to make it definite (Tikhonov regularization)
@@ -38,6 +40,8 @@ protected:
 	response_type response;
 
     bool m_factorized;
+
+    cmat tmp;
 	
 };
 
@@ -48,23 +52,51 @@ protected:
 /// (basically for any regular mechanical systems including rigids and deformables,
 /// it is why it is used by default)
 typedef Eigen::SimplicialLDLT< AssembledSystem::cmat > LDLTSparseLinearSolver;
-class SOFA_Compliant_API LDLTResponse : public EigenSparseResponse< LDLTSparseLinearSolver, LDLTSparseLinearSolver::UpLo >
+class SOFA_Compliant_API LDLTResponse : public EigenSparseResponse< LDLTSparseLinearSolver,true>
 {
 public:
-    SOFA_CLASS(LDLTResponse,SOFA_TEMPLATE2(EigenSparseResponse,LDLTSparseLinearSolver,LDLTSparseLinearSolver::UpLo));
+    SOFA_CLASS(LDLTResponse,SOFA_TEMPLATE2(EigenSparseResponse,LDLTSparseLinearSolver,true));
 };
 
 
 /// Solving the dynamics equation using a LU factorization
 /// Working for any square matrix (particularly useful for non-symmetric matrices, otherwise prefer the LDLTResponse)
 typedef Eigen::SparseLU< AssembledSystem::cmat > LUSparseLinearSolver;
-class SOFA_Compliant_API LUResponse : public EigenSparseResponse< LUSparseLinearSolver, 0 >
+class SOFA_Compliant_API LUResponse : public EigenSparseResponse< LUSparseLinearSolver >
 {
 public:
-    SOFA_CLASS(LUResponse,SOFA_TEMPLATE2(EigenSparseResponse,LUSparseLinearSolver,0));
+    SOFA_CLASS(LUResponse,SOFA_TEMPLATE(EigenSparseResponse,LUSparseLinearSolver));
 };
 
 
+/////////////////////
+
+
+
+/// Solve a dynamics system including bilateral constraints with an iterative linear solver
+template<class LinearSolver, bool symmetric=false>
+class EigenSparseIterativeResponse : public EigenSparseResponse< LinearSolver, symmetric >
+{
+public:
+
+    SOFA_ABSTRACT_CLASS(SOFA_TEMPLATE2(EigenSparseIterativeResponse,LinearSolver,symmetric),SOFA_TEMPLATE2(EigenSparseResponse,LinearSolver,symmetric));
+
+    Data<unsigned> d_iterations;
+    Data<SReal> d_tolerance;
+
+    EigenSparseIterativeResponse();
+
+    virtual void init();
+    virtual void reinit();
+
+};
+
+typedef Eigen::ConjugateGradient< AssembledSystem::cmat > CGSparseLinearSolver;
+class EigenCGResponse : public EigenSparseIterativeResponse< CGSparseLinearSolver, true >
+{
+public:
+    SOFA_CLASS(EigenCGResponse,SOFA_TEMPLATE2(EigenSparseIterativeResponse,CGSparseLinearSolver, true));
+};
 
 
 }
