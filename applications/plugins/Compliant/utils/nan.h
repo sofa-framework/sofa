de64930880b264ee61d130601ce8e61ac958f82a
nesme <matthieu.nesme@inria.fr>
2015-04-02 16:14:58 +0200

    Compliant: adding nan test for sparse matrices
    Former-commit-id: 6e637e4a80081816c9d7c45468ceca4b23640208

--- applications/plugins/Compliant/utils/nan.h
+++ applications/plugins/Compliant/utils/nan.h
@@ -2,6 +2,7 @@
 #define COMPLIANT_UTILS_NAN_H
 
 #include <Eigen/Core>
+#include <Eigen/Sparse>
 #include <cmath>
 
 template<class U, int M, int N>
@@ -9,11 +10,28 @@ bool has_nan(const Eigen::Matrix<U, M, N>& x) {
 	for(unsigned i = 0, m = x.rows(); i < m; ++i) {
 		for(unsigned j = 0, n = x.cols(); j < n; ++j) {
 			U xij = x(i, j);
-
-			if( xij != xij ) return true;
+            if( xij != xij )
+                return true;
 		}
 	}
 	return false;
+}
+
+template<typename _Scalar, int _Options, typename _Index>
+bool has_nan(const Eigen::SparseMatrix<_Scalar,_Options,_Index>& x)
+{
+    typedef Eigen::SparseMatrix<_Scalar,_Options,_Index> Mat;
+
+    for( unsigned i = 0 ; i < x.outerSize() ; ++i )
+    {
+        for( typename Mat::InnerIterator it(x, i) ; it ; ++it )
+        {
+            _Scalar xij = it.value();
+            if( xij != xij )
+                return true;
+        }
+    }
+    return false;
 }
 
 #endif
