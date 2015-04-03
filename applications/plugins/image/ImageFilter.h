3f0862a75e367725016300a2aba3c074728d7ae3
dickoah <ali-hamadi.dicko@inria.fr>
2015-04-03 13:12:02 +0200

    Image : add of sharpen filter to the imageFilter component
    Former-commit-id: d15c99da0764b7f9f5a7f6874f434d79f39fb090

--- applications/plugins/image/ImageFilter.h
+++ applications/plugins/image/ImageFilter.h
@@ -63,6 +63,7 @@
 #define CONNECTEDCOMPONENTS 25
 #define LARGESTCONNECTEDCOMPONENT 26
 #define MIRROR 27
+#define SHARPEN 28
 
 
 namespace sofa
@@ -136,7 +137,7 @@ public:
         inputTransform.setReadOnly(true);
         outputImage.setReadOnly(true);
         outputTransform.setReadOnly(true);
-        helper::OptionsGroup filterOptions(28	,"0 - None"
+        helper::OptionsGroup filterOptions(29	,"0 - None"
                                            ,"1 - Blur ( sigma )"
                                            ,"2 - Blur Median ( n )"
                                            ,"3 - Blur Bilateral ( sigma_s, sigma_r)"
@@ -164,6 +165,7 @@ public:
                                            ,"25 - Label connected components (tolerance=0)"
                                            ,"26 - Largest connected component (tolerance=0)"
                                            ,"27 - Mirror (axis=0)"
+										   ,"28 - Sharpen (sigma)"
                                            );
         filterOptions.setSelectedItem(NONE);
         filter.setValue(filterOptions);
@@ -234,7 +236,7 @@ protected:
                 float amplitude=0; if(p.size()) amplitude=(float)p[0];
                 cimglist_for(img,l) img(l)=inimg(l).get_blur_anisotropic (amplitude);
             }
-            break;
+			break;	
         case DERICHE:
             if(updateImage)
             {
@@ -637,7 +639,15 @@ protected:
                 else if(axis==0) cimglist_for(img,l) img(l)=inimg(l).get_mirror ('y');
                 else cimglist_for(img,l) img(l)=inimg(l).get_mirror ('z');
             }
-            break;
+			break;	 
+
+		case SHARPEN:
+			if(updateImage)
+			{
+				float sigma=0; if(p.size()) sigma=(float)p[0];
+				cimglist_for(img,l) img(l)=inimg(l).get_sharpen(sigma);
+			}
+			break;
 
         default:
             break;
