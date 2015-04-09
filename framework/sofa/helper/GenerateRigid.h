bfe12a9ba4cf99f81a4991f0132b85eb06225ae5
Thomas Lemaire <thomas.lemaire@inria.fr>
2015-04-09 19:07:40 +0200

    [helper] some more "user friendly" functions
    Former-commit-id: 532fc32a9d5fb068677d2e53e89eaba709450c77

--- framework/sofa/helper/GenerateRigid.h
+++ framework/sofa/helper/GenerateRigid.h
@@ -39,6 +39,13 @@ namespace helper
 /// base function to compute center of mass, mass and inertia tensor from a mesh
 void SOFA_HELPER_API generateRigid( defaulttype::Rigid3Mass& mass, defaulttype::Vector3& center, const helper::io::Mesh* mesh );
 
+/// user friendly function to compute center of mass, mass and inertia tensor from a mesh, a density, a scale and a rotation
+void generateRigid(defaulttype::Rigid3Mass& mass, defaulttype::Vector3& center, io::Mesh *mesh
+                                  , SReal density
+                                  , const defaulttype::Vector3& scale = defaulttype::Vector3(1,1,1)
+                                  , const defaulttype::Vector3& rotation /*Euler angles*/ = defaulttype::Vector3(0,0,0)
+                                  );
+
 /// user friendly function to compute center of mass, mass and inertia tensor from a mesh file, a density, a scale and a rotation
 bool SOFA_HELPER_API generateRigid( defaulttype::Rigid3Mass& mass, defaulttype::Vector3& center, const std::string& meshFilename
                                   , SReal density
@@ -56,6 +63,14 @@ struct GenerateRigidInfo
     defaulttype::Vector3 com;
     SReal mass;
 };
+
+/// user friendly function to compute rigid info from a mesh, a density, a scale
+void generateRigid( GenerateRigidInfo& res
+                                  , io::Mesh *mesh
+                                  , std::string const& meshName
+                                  , SReal density
+                                  , const defaulttype::Vector3& scale = defaulttype::Vector3(1,1,1)
+                                  );
 
 /// user friendly function to compute rigid info from a mesh file, a density, a scale
 bool SOFA_HELPER_API generateRigid( GenerateRigidInfo& res
