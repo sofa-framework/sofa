e508c3902fd3bf7d59e149e23dc14344fabb9d46
Marc Legendre <marc.legendre@inria.fr>
2015-04-21 16:30:51 +0200

    SofaTest: remove unnecessary includes
    Former-commit-id: e05b5c5f5c3cea98b53bc96d9fa74c0445d55e04

--- applications/plugins/SofaTest/PrimitiveCreation.h
+++ applications/plugins/SofaTest/PrimitiveCreation.h
@@ -16,8 +16,6 @@
 #include <sofa/simulation/common/Node.h>
 #include <sofa/simulation/common/xml/initXml.h>
 
-#include <sofa/gui/GUIManager.h>
-#include <sofa/gui/Main.h>
 #include <sofa/helper/system/FileRepository.h>
 
 #include <SofaMiscMapping/SubsetMultiMapping.h>
