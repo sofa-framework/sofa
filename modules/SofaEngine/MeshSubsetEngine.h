c2c86da3343a0a931a910dbc082d88538315f67e
Marc Legendre <marc.legendre@ymail.com>
2015-05-08 00:20:31 +0200

    Fix compilation
    Former-commit-id: db12be10b60a2c49736056c3080b71d718bf2d25

--- modules/SofaEngine/MeshSubsetEngine.h
+++ modules/SofaEngine/MeshSubsetEngine.h
@@ -25,7 +25,7 @@
 #ifndef SOFA_COMPONENT_ENGINE_MeshSubsetEngine_H
 #define SOFA_COMPONENT_ENGINE_MeshSubsetEngine_H
 
-#include <sofa/component/component.h>
+#include <sofa/SofaGeneral.h>
 #include <sofa/core/DataEngine.h>
 #include <sofa/core/objectmodel/BaseObject.h>
 #include <sofa/defaulttype/Vec.h>
