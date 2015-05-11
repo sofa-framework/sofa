939e5758e652d24a232fe0eb9a9ad74fb69d2adc
Benjamin GILLES <bgilles@scm.gforge.inria.fr>
2015-05-11 16:01:54 +0200

    [Compliant.sml] fix rigids with multiple meshes
    Former-commit-id: 1f1a658a74b6b43e718d4ebcb53a73e6b4364d5d

--- applications/plugins/Compliant/python/Compliant/sml.py
+++ applications/plugins/Compliant/python/Compliant/sml.py
@@ -62,11 +62,14 @@ def insertRigid(parentNode, rigidModel, density, param=None):
     if not param is None:
         rigid.dofs.showObject = param.showRigid
         rigid.dofs.showObjectScale = SofaPython.units.length_from_SI(param.showRigidScale)
-    # visual
 
+    # walk around to handle multiple meshes
+    # @todo: handle them in StructuralAPI ?
+    rigid.collisions=dict()
+    rigid.visuals=dict()
     for mesh in rigidModel.mesh :
-        cm = rigid.addCollisionMesh(mesh.source)
-        rigid.visual = cm.addVisualModel()
+        rigid.collisions[mesh.id] = rigid.addCollisionMesh(mesh.source)
+        rigid.visuals[mesh.id] = rigid.collisions[mesh.id].addVisualModel()
        
     return rigid
 
