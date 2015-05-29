10f7aab8828f8c6839203fae72bbba9258d2b8db
Benjamin GILLES <bgilles@scm.gforge.inria.fr>
2015-05-29 15:38:00 +0200

    [Flexible.API] refactoring to better handle overlapping cells
    Former-commit-id: 2e20b1d4cd2e64a1e995a688081cdcd9ca8e9a06

--- applications/plugins/Flexible/python/Flexible/API.py
+++ applications/plugins/Flexible/python/Flexible/API.py
@@ -10,21 +10,13 @@ import SofaPython.Tools
 from SofaPython.Tools import listToStr as concat
 from SofaPython import Quaternion
 
-def insertLinearMapping(node, dofRigidNode=None, dofAffineNode=None, position=None, labelImage=None, labels=None, assemble=True, geometricStiffness=2):
+def insertLinearMapping(node, dofRigidNode=None, dofAffineNode=None, position=None, cell='', assemble=True, geometricStiffness=2):
     """ insert the correct Linear(Multi)Mapping
     hopefully the template is deduced automatically by the component
     """
     if dofRigidNode is None and dofAffineNode is None:
         print "[Flexible.API.insertLinearMapping] ERROR: no dof given"
     else:
-        cell = ''
-        if not labelImage is None and not labels is None : # use labels to select specific voxels in branching image
-            node.createObject(
-                "BranchingCellOffsetsFromPositions", template="BranchingImageUC", name="cell",
-                position ="@"+SofaPython.Tools.getObjectPath(position)+".position",
-                src="@"+SofaPython.Tools.getObjectPath(labelImage.branchingImage), labels=concat(labels))
-            cell = "@cell.cell"
-
         if dofRigidNode is None:
             return node.createObject(
                 "LinearMapping", cell=cell, shapeFunction = 'shapeFunction',
@@ -85,7 +77,11 @@ class Deformable:
         self.mass = self.node.createObject('UniformMass', totalMass=totalMass)
 
     def addMapping(self, dofRigidNode=None, dofAffineNode=None, labelImage=None, labels=None, assemble=True):
-        self.mapping = insertLinearMapping(self.node, dofRigidNode, dofAffineNode, self.topology, labelImage, labels, assemble)
+        cell = ''
+        if not labelImage is None and not labels is None : # use labels to select specific voxels in branching image
+           offsets = self.node.createObject("BranchingCellOffsetsFromPositions", template="BranchingImageUC", name="cell", position ="@"+SofaPython.Tools.getObjectPath(self.topology)+".position", src="@"+SofaPython.Tools.getObjectPath(labelImage.branchingImage), labels=concat(labels))
+           cell = "@"+SofaPython.Tools.getObjectPath(offsets)+".cell"
+        self.mapping = insertLinearMapping(self.node, dofRigidNode, dofAffineNode, self.topology, cell, assemble)
 
     def addVisual(self, color=[1,1,1,1]):
         self.visual = Deformable.VisualModel(self.node, color)
@@ -272,6 +268,7 @@ class Behavior:
         self.sampler = None
         self.dofs = None
         self.mapping = None
+        self.cell = ''
 
     def addGaussPointSampler(self, shapeFunction, nbPoints, **kwargs):
         shapeFunctionPath = SofaPython.Tools.getObjectPath(shapeFunction.shapeFunction)
@@ -281,17 +278,19 @@ class Behavior:
             method="2", order=self.type[2:], targetNumber=nbPoints,
             mask="@"+SofaPython.Tools.getObjectPath(self.labelImage.branchingImage)+".branchingImage", maskLabels=concat(self.labels),
             **kwargs)
-
+        celloffsets = self.node.createObject("BranchingCellOffsetsFromPositions", template="BranchingImageUC", name="cell", position ="@"+SofaPython.Tools.getObjectPath(self.sampler)+".position", src="@"+SofaPython.Tools.getObjectPath(self.labelImage.branchingImage), labels=concat(self.labels))
+        self.cell = "@"+SofaPython.Tools.getObjectPath(celloffsets)+".cell"
 
     def addTopologyGaussPointSampler(self, mesh, order="2", **kwargs):
         meshPath = SofaPython.Tools.getObjectPath(mesh)
         self.sampler = self.node.createObject("TopologyGaussPointSampler", name="sampler", method="0", inPosition="@"+meshPath+".position", order=order, **kwargs)
+        self.cell = "@"+SofaPython.Tools.getObjectPath(self.sampler)+".cell"
 
     def addMechanicalObject(self, dofRigidNode=None, dofAffineNode=None, assemble=True):
         if self.sampler is None:
             print "[Flexible.API.Behavior] ERROR: no sampler"
         self.dofs = self.node.createObject("MechanicalObject", template="F"+self.type, name="dofs")
-        self.mapping = insertLinearMapping(self.node, dofRigidNode, dofAffineNode, self.sampler, self.labelImage, self.labels, assemble)
+        self.mapping = insertLinearMapping(self.node, dofRigidNode, dofAffineNode, self.sampler, self.cell , assemble)
     
 
     def getFilename(self, filenamePrefix=None, directory=""):
