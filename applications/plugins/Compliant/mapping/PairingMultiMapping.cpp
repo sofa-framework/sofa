f4c819b49b16baace65c4ea2da46d5f1e5189ff4
Maxime Tournier <tournier.maxime@gmail.com>
2015-06-01 13:42:15 +0200

    Compliant: added pairing multimapping for general constraints
    Former-commit-id: 883ca8a22a40c8b94c2e5cbee96ad6f32044b8b1

---
+++ applications/plugins/Compliant/mapping/PairingMultiMapping.cpp
@@ -0,0 +1,60 @@
+#include "PairingMultiMapping.h"
+
+#include <sofa/defaulttype/VecTypes.h>
+#include <sofa/core/MultiMapping.inl>
+
+#include <sofa/core/ObjectFactory.h>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace mapping
+{
+
+SOFA_DECL_CLASS(PairingMultiMapping)
+
+using namespace defaulttype;
+
+// Register in the Factory
+static int PairingMultiMappingClass = core::RegisterObject("dot product between vec dofs")
+
+#ifndef SOFA_FLOAT
+    .add< PairingMultiMapping< Vec6dTypes, Vec1dTypes > >()    
+    .add< PairingMultiMapping< Vec3dTypes, Vec1dTypes > >()
+    .add< PairingMultiMapping< Vec2dTypes, Vec1dTypes > >()    
+    .add< PairingMultiMapping< Vec1dTypes, Vec1dTypes > >()
+#endif
+#ifndef SOFA_DOUBLE
+    .add< PairingMultiMapping< Vec6fTypes, Vec1fTypes > >()    
+    .add< PairingMultiMapping< Vec3fTypes, Vec1fTypes > >()
+    .add< PairingMultiMapping< Vec2fTypes, Vec1fTypes > >()
+    .add< PairingMultiMapping< Vec1fTypes, Vec1fTypes > >()
+#endif
+;
+
+#ifndef SOFA_FLOAT
+template class SOFA_Compliant_API PairingMultiMapping<  Vec3dTypes, Vec1dTypes >;
+template class SOFA_Compliant_API PairingMultiMapping<  Vec6dTypes, Vec1dTypes >;
+template class SOFA_Compliant_API PairingMultiMapping<  Vec2dTypes, Vec1dTypes >;
+template class SOFA_Compliant_API PairingMultiMapping<  Vec1dTypes, Vec1dTypes >;
+#endif
+
+#ifndef SOFA_DOUBLE
+template class SOFA_Compliant_API PairingMultiMapping<  Vec3fTypes, Vec1fTypes >;
+template class SOFA_Compliant_API PairingMultiMapping<  Vec6fTypes, Vec1fTypes >;
+template class SOFA_Compliant_API PairingMultiMapping<  Vec2fTypes, Vec1fTypes >;
+template class SOFA_Compliant_API PairingMultiMapping<  Vec1fTypes, Vec1fTypes >;
+
+#endif
+
+
+
+} // namespace mapping
+
+} // namespace component
+
+} // namespace sofa
+
