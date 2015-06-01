0ec152fa2832bf2b2a35b6d4c04795fa285d644c
tony.galloy <tony.galloy@inria.fr>
2015-06-01 15:25:17 +0200

    Stable state, TODO optimization for marching cubes
    Former-commit-id: 3e475bf784e838e13b76244c1beb8ec1504c4b94

---
+++ examples/Demos/SkullOperations/Modeler_SofaSkull.bat
@@ -0,0 +1,1 @@
+C:\Users\SOFA_GPU\Downloads\sofa\sofa\build-64-extra\bin\Modeler.exe C:\Users\SOFA_GPU\Desktop\SkullOperations\test.scn 
---
+++ examples/Demos/SkullOperations/Runsofa_SofaSkull.bat
@@ -0,0 +1,1 @@
+C:\Users\SOFA_GPU\Downloads\sofa\sofa\build-64-extra\bin\runSofa.exe C:\Users\SOFA_GPU\Desktop\SkullOperations\Test.scn 
