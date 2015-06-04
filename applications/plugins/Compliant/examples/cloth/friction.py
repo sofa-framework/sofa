6f0c014b335cf2ae000b8a9a850a6936c3c5169f
Maxime Tournier <tournier.maxime@gmail.com>
2015-06-04 12:50:08 +0200

    Compliant: added friction example for modulus solver
    Former-commit-id: 58be72faef39145c8005e26498c65b0269c0ed76

---
+++ applications/plugins/Compliant/examples/cloth/friction.py
@@ -0,0 +1,52 @@
+
+
+import scene
+
+
+def createScene(node):
+    node.gravity = '0 -10 0'
+    node.dt = 1e-2
+    
+    scene.display_flags(node, show = 'Behavior Visual CollisionModels',
+                  hide = 'MechanicalMappings')
+    
+    scene.requires(node, 'Flexible', 'Compliant')
+
+    scene.contacts(node,
+                   response = 'FrictionCompliantContact',                 
+                   # response = 'CompliantContact',
+                   )
+
+    c = cloth(node)
+    b = ball(node)
+    
+    ode = node.createObject('CompliantImplicitSolver',
+                            stabilization = 0,
+                            neglecting_compliance_forces_in_geometric_stiffness = False)
+    
+    num = node.createObject('ModulusSolver',
+                            iterations = 10,
+                            precision = 1e-6,
+                            anderson = 4)
+    
+
+def ball(node, **kwargs):
+    root = scene.xml_load('ball.xml')
+
+    name = kwargs.get('name', 'ball')
+    res = scene.xml_insert(node, root)
+    res.name = name
+
+    return res
+
+
+def cloth(node, **kwargs):
+
+    root = scene.xml_load('cloth.xml')
+
+    name = kwargs.get('name', 'cloth')
+
+    res = scene.xml_insert(node, root)
+    res.name = name
+
+    return res
