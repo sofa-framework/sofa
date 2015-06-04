09c8d20ed77607421abd57583130d239fe00746f
Maxime Tournier <tournier.maxime@gmail.com>
2015-06-04 12:48:38 +0200

    Compliant: fixed (again) coulomb projector
    Former-commit-id: 03f06a2a4a81993030549fa6f58b62eb95ccdcb0

--- applications/plugins/Compliant/constraint/CoulombConstraint.cpp
+++ applications/plugins/Compliant/constraint/CoulombConstraint.cpp
@@ -20,8 +20,35 @@ CoulombConstraint::CoulombConstraint(SReal mu)
 
 void CoulombConstraint::project( SReal* out, unsigned n, unsigned /*index*/, bool correct ) const
 {
-    assert( n >= 3 );
+    assert( n % 3  == 0);    
 
+    // max: whatever happened to the old, correct version ? rewriting.
+    const unsigned m = n / 3;
+
+    typedef Eigen::Matrix<SReal, 3, 1> vec3;
+    static const vec3 normal = vec3::UnitX();
+
+    // projection operator
+    typedef vec3 (*proj_type)(const vec3&, const vec3& , SReal );
+    const proj_type proj = horizontalProjection ?
+        cone_horizontal<SReal> : cone<SReal>;
+    
+    for(unsigned i = 0; i < m; ++i) {
+
+        Eigen::Map< vec3 > view(&out[3*i]);
+
+        if( correct ) {
+            view = proj(view, normal, 0.0);
+        } else {
+            view = proj(view, normal, mu);
+        }
+
+    }
+    
+    return;
+    
+    // TODO FIXME why do we only consider the first contact ?!
+    
     // By construction the first component is aligned with the normal
 
     // no attractive force
@@ -53,6 +80,7 @@ void CoulombConstraint::project( SReal* out, unsigned n, unsigned /*index*/, boo
                 // coneProjection(out, mu);
                 view = cone<SReal>(view, normal, mu);
 
+             // TODO wtf is this ?
              for( unsigned int i=3 ; i<n ; ++i ) out[i] = 0;
          }
     }
