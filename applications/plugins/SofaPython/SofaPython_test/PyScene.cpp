97adbfb3310d5c2e7201af354ba731595d00e710
Bruno Carrez <bruno.carrez@inria.fr>
2015-06-18 16:09:55 +0200

    SofaPython: xml scene test (xml scene with a python controler)
    Former-commit-id: 497fd2178b8afe88046ec96e8a28c567565a839e

--- applications/plugins/SofaPython/SofaPython_test/PyScene.cpp
+++ applications/plugins/SofaPython/SofaPython_test/PyScene.cpp
@@ -1,13 +1,6 @@
 #include <SofaTest/Sofa_test.h>
-//#include <SofaBoundaryCondition/PartialFixedConstraint.h>
-//#include <sofa/defaulttype/VecTypes.h>
-//#include <sofa/simulation/common/Simulation.h>
 #include <sofa/simulation/graph/DAGSimulation.h>
 #include <sofa/simulation/common/Node.h>
-//#include <SofaBaseMechanics/MechanicalObject.h>
-//#include <SofaBaseMechanics/UniformMass.h>
-//#include <SceneCreator/SceneCreator.h>
-//#include <SofaBoundaryCondition/ConstantForceField.h>
 #include <PythonScriptController.h>
 
 namespace sofa {
@@ -45,7 +38,39 @@ struct PyScene_test : public Sofa_test<>
         return true;
     }
 
+    bool loadXmlSceneWithPyCtrlr()
+    {
+        static const std::string scenePath = std::string(SOFAPYTHON_TEST_PYTHON_DIR)+std::string("/test_XmlSceneWithPyCtlr.scn");
+        //std::cout<<scenePath<<std::endl;
+
+        //Init
+
+        simulation::Simulation* simulation;
+        sofa::simulation::setSimulation(simulation = new sofa::simulation::graph::DAGSimulation());
+
+        /// Scene creation
+        simulation::Node::SPtr root =  simulation::getSimulation()->load ( scenePath.c_str() );
+        simulation::getSimulation()->init(root.get());
+
+        // get the python script controller
+        component::controller::PythonScriptController *ctr = root->get<component::controller::PythonScriptController>();
+        if (!ctr)
+            return false; // no script controller ??
+
+        if (!ctr->scriptControllerInstance())
+            return false; // script not loaded correctly
+
+        //std::cout<<ctr->getName()<<std::endl;
+
+        return true;
+    }
+
 };
+
+TEST_F( PyScene_test, XmlScene)
+{
+    EXPECT_TRUE( this->loadXmlSceneWithPyCtrlr() );
+}
 
 TEST_F( PyScene_test, PythonScene)
 {
