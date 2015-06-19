4a30c40507b6fea65a6686729042ca932cc9ae92
nesme <matthieu.nesme@inria.fr>
2015-06-19 10:51:43 +0200

    SofaPython: fixing test
    Former-commit-id: 43d81ffc2a120d3217f03fd30a806f1b2f23cc7e

--- applications/plugins/SofaPython/SofaPython_test/python/test_PySceneWithPyCtlr.py
+++ applications/plugins/SofaPython/SofaPython_test/python/test_PySceneWithPyCtlr.py
@@ -4,9 +4,12 @@ import Sofa
 
 import os
 
+currentdir = os.path.dirname(__file__)
+
+
 def createScene(rootNode):
 
     # This create a PythonScriptController that permits to programatically implement new behavior 
     # or interactions using the Python programming langage. The controller is referring to a 
     # file named "controller.py". 
-    rootNode.createObject('PythonScriptController', filename="testController.py", classname="controller")
+    rootNode.createObject('PythonScriptController', filename=os.path.join(currentdir,"testController.py"), classname="controller")
