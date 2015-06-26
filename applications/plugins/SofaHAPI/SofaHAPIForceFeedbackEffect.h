dc5aaee23cc62206f7b93bc55bcc3bc017e9fcf0
tony.galloy <tony.galloy@inria.fr>
2015-06-26 16:58:20 +0200

    FIXED : Missing definition in SofaHAPI DLL on Windows. Credits to Bruno.
    Former-commit-id: 9ccce7ce0f8605a6fe12a44771019b7012d5808f

--- applications/plugins/SofaHAPI/SofaHAPIForceFeedbackEffect.h
+++ applications/plugins/SofaHAPI/SofaHAPIForceFeedbackEffect.h
@@ -25,7 +25,7 @@
 #ifndef SOFAHAPI_SOFAHAPIFORCEFEEDBACKEFFECT_H
 #define SOFAHAPI_SOFAHAPIFORCEFEEDBACKEFFECT_H
 
-#include "SofaHAPI.h"
+//#include "SofaHAPI.h"
 
 //HAPI include
 #include <HAPI/HAPIForceEffect.h>
