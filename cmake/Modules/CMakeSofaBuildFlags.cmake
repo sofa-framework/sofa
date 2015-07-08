3ea968acb75179f9d12b48493b82391045db7c9c
Marc Legendre <marc.legendre@inria.fr>
2015-07-08 18:09:40 +0200

    cmake: cleanup
    Former-commit-id: a2cfb608195927ac431702b57ba2ccc86ddad75a

--- cmake/Modules/CMakeSofaBuildFlags.cmake
+++ cmake/Modules/CMakeSofaBuildFlags.cmake
@@ -1,7 +1,6 @@
 ## Set some default build flags when compiling a SOFA library.
 ## unless cmake is called with CMAKE_BUILD_TYPE=Debug, release build is forced
 if(NOT CMAKE_BUILD_TYPE)
-    #message(STATUS "No build type selected, default to Release")
     set(CMAKE_BUILD_TYPE "Release")
 endif()
 
@@ -27,18 +26,17 @@ elseif(CMAKE_BUILD_TYPE MATCHES "Release")
         if("${GCXX_VERSION}" VERSION_EQUAL 4.6)
             set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -fno-partial-inlining")
         endif()
-        set ( CMAKE_SHARED_LINKER_FLAGS "-Wl,--no-undefined -lc ${CMAKE_SHARED_LINKER_FLAGS}")
-        set ( CMAKE_MODULE_LINKER_FLAGS "-Wl,--no-undefined -lc ${CMAKE_MODULE_LINKER_FLAGS}")
+        set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--no-undefined -lc ${CMAKE_SHARED_LINKER_FLAGS}")
+        set(CMAKE_MODULE_LINKER_FLAGS "-Wl,--no-undefined -lc ${CMAKE_MODULE_LINKER_FLAGS}")
     endif()
 elseif(CMAKE_BUILD_TYPE MATCHES "RelWithDebInfo")
 elseif(CMAKE_BUILD_TYPE MATCHES "MinSizeRel")
 endif()
 
-set(compilerDefines ${GLOBAL_COMPILER_DEFINES})
+set(compilerDefines)
 
-# windows specific
 if(WIN32)
-    add_definitions( "-DUNICODE")
+    add_definitions("-DUNICODE")
     add_definitions("-wd4250 -wd4251 -wd4275 -wd4675 -wd4996 /bigobj")
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
 endif()
@@ -48,7 +46,7 @@ if(XBOX)
 endif()
 
 # NDEBUG preprocessor macro
-if (WIN32 OR APPLE)
+if(WIN32 OR APPLE)
     # NDEBUG and _DEBUG are automatically set in the default c/cxx flags of the right configurations
 elseif(NOT CMAKE_BUILD_TYPE MATCHES "Debug")
     list(APPEND compilerDefines "NDEBUG")
@@ -60,11 +58,11 @@ if(PS3)
 endif()
 
 # SOFA_DEBUG preprocessor macro
-if (WIN32 OR APPLE)
+if(WIN32 OR APPLE)
     # Reminder: multi-configuration generators like Visual Studio and XCode do not use CMAKE_BUILD_TYPE,
     # as they generate all configurations in the project, not just one at a time!
-    set (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -DSOFA_DEBUG")
-    set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DSOFA_DEBUG")
+    set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -DSOFA_DEBUG")
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DSOFA_DEBUG")
 elseif(CMAKE_BUILD_TYPE MATCHES "Debug")
     list(APPEND compilerDefines "SOFA_DEBUG")
 endif()
@@ -73,21 +71,21 @@ endif()
 if(SOFA-MISC_OPENMP)
     list(APPEND compilerDefines "USING_OMP_PRAGMAS")
     find_package(OpenMP QUIET)
-    if (OPENMP_FOUND)
-        set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
-        set (CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} ${OpenMP_C_FLAGS}")
-        set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-        set (CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} ${OpenMP_CXX_FLAGS}")
+    if(OPENMP_FOUND)
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+        set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} ${OpenMP_C_FLAGS}")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+        set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} ${OpenMP_CXX_FLAGS}")
     endif()
 endif()
 
 # SSE Flags on Win32
 if(SOFA-MISC_VECTORIZE)
     if(MSVC)
-	set(ARCH_FLAGS "/arch:SSE2 /fp:fast")
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ARCH_FLAGS}")
-	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ARCH_FLAGS}")
-    endif(MSVC)
+	    set(ARCH_FLAGS "/arch:SSE2 /fp:fast")
+	    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ARCH_FLAGS}")
+	    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ARCH_FLAGS}")
+    endif()
 endif()
 
 
