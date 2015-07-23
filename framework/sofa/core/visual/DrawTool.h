fd7640a2060f6d219c0d97eb5455a4865eb048fe
remibessard <remi.bessard_duparc@inria.fr>
2015-07-23 11:40:58 +0200

    UPDATE: add a drawQuads() method + small fix in UniformMass (related to localRange data)
    Former-commit-id: 9eb606cae3e301cb263f2e9e003eae0ce77c0f52

--- framework/sofa/core/visual/DrawTool.h
+++ framework/sofa/core/visual/DrawTool.h
@@ -130,6 +130,7 @@ public:
     virtual void drawQuad(const Vector3 &p1,const Vector3 &p2,const Vector3 &p3,const Vector3 &p4,
             const Vector3 &normal1, const Vector3 &normal2, const Vector3 &normal3, const Vector3 &normal4,
             const Vec4f &c1, const Vec4f &c2, const Vec4f &c3, const Vec4f &c4) = 0 ;
+    virtual void drawQuads(const std::vector<Vector3> &points, const Vec4f colour) = 0 ;
 
     virtual void drawSphere( const Vector3 &p, float radius) = 0 ;
 
