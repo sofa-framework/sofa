3a36c145ae068d36bd52c71cff0f3f6de502182c
Marc Legendre <marc.legendre@inria.fr>
2015-08-31 18:13:21 +0200

    plugins: Get rid of python.cmake files
    Former-commit-id: 788dd67e5ce3b399b1a677dd0932eb752291586f

--- applications/plugins/Compliant/Compliant_test/python/python.cmake
+++
@@ -1,8 +0,0 @@
-set(PYTHON_FILES
-    python/AffineMultiMapping.py
-    python/Example.py
-    python/LambdaPropagation.py
-    python/UniformCompliance.py
-    python/GenerateRigid.py
-    python/friction.py
-)
