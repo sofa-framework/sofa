4f741d5270444d5e2598f2b8eaf1caa257efa9db
Marc Legendre <marc.legendre@inria.fr>
2015-09-13 21:33:36 +0200

    SofaEngine: Fix minor GCC warnings
    Namely: -Wunused-value, -Wunused-variable, -Wreorder.
    
    Former-commit-id: 9bfb8a9505d9fe3b045db5e16fd6cd53a280a517

--- modules/SofaEngine/GenerateSphere.h
+++ modules/SofaEngine/GenerateSphere.h
@@ -58,18 +58,18 @@ public:
     typedef typename DataTypes::VecCoord VecCoord;
     typedef typename DataTypes::Coord Coord;
     typedef typename DataTypes::Real Real;
-	typedef typename sofa::core::topology::Topology::PointID PointID;
+    typedef typename sofa::core::topology::Topology::PointID PointID;
     typedef sofa::core::topology::BaseMeshTopology::SeqTetrahedra SeqTetrahedra;
     typedef typename sofa::core::topology::Topology::Edge Edge;
     typedef sofa::core::topology::BaseMeshTopology::SeqTriangles SeqTriangles;
     typedef typename SeqTetrahedra::value_type Tetrahedron;
     typedef typename SeqTriangles::value_type Triangle;
 
-	typedef enum {
-		TETRAHEDRON=1,
-		OCTAHEDRON=2,
-		ICOSAHEDRON=3
-	} PlatonicTriangulation;
+    typedef enum {
+        TETRAHEDRON=1,
+        OCTAHEDRON=2,
+        ICOSAHEDRON=3
+    } PlatonicTriangulation;
 
 public:
 
@@ -94,24 +94,24 @@ public:
     }
 
 public:
-    Data<size_t > f_tessellationDegree; /// degree of tessellation of each platonic triangle 
-	Data<Real > f_radius; /// radius of the sphere
-    Data<Coord> f_origin; /// origin
-	PlatonicTriangulation platonicSolid; /// the type of platonic solid used for the tessellation
-	Data<std::string>	 f_platonicSolidName; // the name of the platonics solid 
+    Data<VecCoord> f_outputTetrahedraPositions; ///< Output tetrahedra positions
+    Data<SeqTetrahedra> f_tetrahedra; ///< Output tetrahedra
+    Data<VecCoord> f_outputTrianglesPositions; ///< Output triangle positions
+    Data<SeqTriangles> f_triangles; ///< Output triangles
 
-    Data<VecCoord> f_outputTetrahedraPositions; ///< ouput tetrahedra position
-	Data<VecCoord> f_outputTrianglesPositions; ///< ouput triangle positions
-    Data<SeqTetrahedra> f_tetrahedra; ///< output tetrahedra
-	Data<SeqTriangles> f_triangles; ///< output triangles
-	Data<sofa::helper::vector<Real> > f_bezierTriangleWeight; ///  output weight for rational Bezier triangles
-	Data<sofa::helper::vector<bool> > f_isBezierTriangleRational; ///  for each Bezier triangle indicates if it is rational or integral
-	Data<sofa::helper::vector<Real> > f_bezierTetrahedronWeight; ///  output weight for rational Bezier triangles
-	Data<sofa::helper::vector<bool> > f_isBezierTetrahedronRational; ///  for each Bezier tetrahedron indicates if it is rational
-	Data<size_t> f_bezierTriangleDegree; /// degree of Bezier triangles
-	Data<size_t> f_bezierTetrahedronDegree; /// degree of Bezier tetrahedron
+    Data<size_t> f_bezierTetrahedronDegree; ///< Degree of Bezier tetrahedra
+    Data<sofa::helper::vector<Real> > f_bezierTetrahedronWeight; ///<  Output weight for rational Bezier triangles
+    Data<sofa::helper::vector<bool> > f_isBezierTetrahedronRational; ///<  For each Bezier tetrahedron, indicates if it is rational
+    Data<size_t> f_bezierTriangleDegree; ///< Degree of Bezier triangles
+    Data<sofa::helper::vector<Real> > f_bezierTriangleWeight; ///< Output weight for rational Bezier triangles
+    Data<sofa::helper::vector<bool> > f_isBezierTriangleRational; ///< For each Bezier triangle indicates, if it is rational or integral
 
+    Data<Real > f_radius; ///< Radius of the sphere
+    Data<Coord> f_origin; ///< Origin
+    Data<size_t > f_tessellationDegree; ///< Degree of tessellation of each platonic triangle 
+    Data<std::string>     f_platonicSolidName; ///< Name of the platonics solid 
 
+    PlatonicTriangulation platonicSolid; ///< the type of platonic solid used for the tessellation
 };
 
 
