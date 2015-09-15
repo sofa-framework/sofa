c89da681595fd293457bf2442b07e9af66493b91
Marc Legendre <marc.legendre@inria.fr>
2015-09-15 09:28:49 +0200

    Monstruous cleanup of cmake files
    Former-commit-id: b4a303476352a07ae64be4ab97e3b7bd34392d15

--- applications/plugins/ManifoldTopologies/ManifoldEdgeSetTopologyModifier.h
+++ applications/plugins/ManifoldTopologies/ManifoldEdgeSetTopologyModifier.h
@@ -26,7 +26,7 @@
 #define SOFA_COMPONENT_TOPOLOGY_MANIFOLDEDGESETTOPOLOGYMODIFIER_H
 #include <ManifoldTopologies/config.h>
 
-#include <ManifoldTopologies/ManifoldTopologies.h>
+#include <ManifoldTopologies/config.h>
 
 #include <SofaBaseTopology/EdgeSetTopologyModifier.h>
 
