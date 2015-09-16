58a88cc332a184d8c22c718ee0c582428f3a2dae
Marc Legendre <marc.legendre@inria.fr>
2015-09-17 01:42:59 +0200

    Reorganize build
    - Reorganize build directory:
      - generated cmake files go to <build>/cmake
      - generated include files go to <build>/include
    
    - Get rid of "compatibility" variables in package configuration files,
      and use the same file in the build tree and the install tree.
    
    - The sofa_write_package_configuration_files(a b c d) macro becomes
    
      - sofa_write_package_configuration_files(package version install-include-subdir)
        which create FooConfig.cmake, FooConfigVersion.cmake and their
        install rules
    
      - sofa_install_targets(package targets install-include-subdir)
        which create FooTargets.cmake and the install rules for the given
        targets (and their public headers, if any)
    
      - sofa_create_package(package version targets)
        which is a shorthand for the other two combined
    
    Typically, a plugin that has a single CMakeLists.txt can create package
    config files and install rules in one line with sofa_create_package().
    
    Other packages in the repository are split in multiple directories, and
    their main CMakeLists.txt creates the *Config.cmake file with
    sofa_write_package_configuration_files(), and the CMakeLists.txt below
    it install their targets with sofa_install_targets().
    
    I think that's it.
    
    
    Former-commit-id: f8d0f48262225e1dfc09fba15a326b50c6441210

--- SofaFramework/SofaPackageConfigHelpers.cmake
+++ SofaFramework/SofaPackageConfigHelpers.cmake
@@ -1,25 +1,21 @@
 include(CMakePackageConfigHelpers)
 
-# sofa_write_package_config_files(Foo <version> <build-include-dirs> <install-include-dirs>)
+# sofa_write_package_config_files(Foo <version> <build-include-dirs>)
 #
 # Create CMake package configuration files
 # - In the build tree:
-#   - ${CMAKE_CURRENT_BINARY_DIR}/FooConfig.cmake
+#   - ${CMAKE_BINARY_DIR}/cmake/FooConfig.cmake
+#   - ${CMAKE_BINARY_DIR}/cmake/FooConfigVersion.cmake
 # - In the install tree:
 #   - lib/cmake/Foo/FooConfigVersion.cmake
 #   - lib/cmake/Foo/FooConfig.cmake
 #   - lib/cmake/Foo/FooTargets.cmake
 #
 # This macro factorizes boilerplate CMake code for the different
-# packages in Sofa.  Is assumes the following conventions hold:
-# - There is a FooConfig.cmake.in file template in the same directory.
-#   This file can refer to @PACKAGE_FOO_INCLUDE_DIR@, which will
-#   expand to the include directories for this package, either in the
-#   build tree or in the install tree. See example below.
-
-
-# Assuming Foo depends on Bar and Baz, and creates the targets Foo and
-# Qux, here is a typical FooConfig.cmake.in:
+# packages in Sofa.  It assumes that there is a FooConfig.cmake.in
+# file template in the same directory.  For example, if a package Foo
+# depends on Bar and Baz, and creates the targets Foo and Qux, here is
+# a typical FooConfig.cmake.in:
 #
 # @PACKAGE_INIT@
 #
@@ -27,47 +23,44 @@ include(CMakePackageConfigHelpers)
 # find_package(Baz REQUIRED)
 #
 # if(NOT TARGET Qux)
-# 	include("${CMAKE_CURRENT_LIST_DIR}/SofaBaseTargets.cmake")
+# 	include("${CMAKE_CURRENT_LIST_DIR}/FooTargets.cmake")
 # endif()
 #
 # check_required_components(Foo Qux)
-#
-# set(Foo_LIBRARIES Foo Qux)
-# set(Foo_INCLUDE_DIRS @PACKAGE_FOO_INCLUDE_DIR@ ${Bar_INCLUDE_DIRS} ${Baz_INCLUDE_DIRS})
 
+macro(sofa_install_targets package_name the_targets install_include_subdir)
 
-macro(sofa_write_package_config_files package_name version build_include_dirs install_include_dirs)
+    install(TARGETS ${the_targets}
+            EXPORT ${package_name}Targets
+            RUNTIME DESTINATION bin
+            LIBRARY DESTINATION lib
+            ARCHIVE DESTINATION lib
+            PUBLIC_HEADER DESTINATION include/${install_include_subdir})
 
-    string(TOUPPER ${package_name} uppercase_package_name)
+endmacro()
 
-    ## <package_name>ConfigVersion.cmake
-    set(filename ${package_name}ConfigVersion.cmake)
-    write_basic_package_version_file(${filename} VERSION ${version} COMPATIBILITY ExactVersion)
-    install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${filename}" DESTINATION lib/cmake/${package_name})
-
-
-    ### <package_name>Config.cmake
-
-    set(input_file ${package_name}Config.cmake.in)
-    set(output_file ${package_name}Config.cmake)
-    ## Build tree
-    set(${uppercase_package_name}_INCLUDE_DIR ${build_include_dirs} "${CMAKE_CURRENT_BINARY_DIR}")
-    configure_package_config_file(${input_file}
-                                  ${output_file}
-                                  INSTALL_DESTINATION none
-                                  PATH_VARS ${uppercase_package_name}_INCLUDE_DIR)
-    ## Install tree
-    set(${uppercase_package_name}_INCLUDE_DIR ${install_include_dirs})
-    configure_package_config_file(${input_file}
-                                  Installed${output_file}
-                                  INSTALL_DESTINATION lib/cmake/${package_name}
-                                  PATH_VARS ${uppercase_package_name}_INCLUDE_DIR)
-    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/Installed${output_file}
-            DESTINATION lib/cmake/${package_name}
-            RENAME ${output_file})
-
+macro(sofa_write_package_config_files package_name version)
 
     ## <package_name>Targets.cmake
     install(EXPORT ${package_name}Targets DESTINATION lib/cmake/${package_name})
 
+    ## <package_name>ConfigVersion.cmake
+    set(filename ${package_name}ConfigVersion.cmake)
+    write_basic_package_version_file(${filename} VERSION ${version} COMPATIBILITY ExactVersion)
+    configure_file("${CMAKE_CURRENT_BINARY_DIR}/${filename}"
+                   "${CMAKE_BINARY_DIR}/cmake/${filename}" COPYONLY)
+    install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${filename}" DESTINATION lib/cmake/${package_name})
+
+    ### <package_name>Config.cmake
+    configure_package_config_file(${package_name}Config.cmake.in
+                                  "${CMAKE_BINARY_DIR}/cmake/${package_name}Config.cmake"
+                                  INSTALL_DESTINATION lib/cmake/${package_name})
+    install(FILES "${CMAKE_BINARY_DIR}/cmake/${package_name}Config.cmake"
+            DESTINATION lib/cmake/${package_name})
+
+endmacro()
+
+macro(sofa_create_package package_name version the_targets include_subdir)
+    sofa_install_targets("${package_name}" "${the_targets}" "${include_subdir}")
+    sofa_write_package_config_files("${package_name}" "${version}")
 endmacro()
