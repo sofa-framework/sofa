73bc7b523772c9a7362a7fa9eb325ba779bfa065
nesme <matthieu.nesme@inria.fr>
2015-09-21 17:10:45 +0200

    SofaPython: managing vectors of unknown types as a string rather than as a vector of strings.
    Former-commit-id: c412240a2499ef60c63fa697b6bccf12fac79bc0

--- applications/plugins/SofaPython/Binding_Data.cpp
+++ applications/plugins/SofaPython/Binding_Data.cpp
@@ -123,6 +123,12 @@ PyObject *GetDataValuePython(BaseData* data)
     {
         // this is a vector; return a python list of the corresponding type (ints, scalars or strings)
 
+        if( !typeinfo->Text() && !typeinfo->Scalar() && !typeinfo->Integer() )
+        {
+            SP_MESSAGE_WARNING( "BaseData_getAttr_value unsupported native type="<<data->getValueTypeString()<<" for data "<<data->getName()<<" ; returning string value" )
+            return PyString_FromString(data->getValueString().c_str());
+        }
+
         PyObject *rows = PyList_New(nbRows);
         for (int i=0; i<nbRows; i++)
         {
@@ -147,9 +153,8 @@ PyObject *GetDataValuePython(BaseData* data)
                 }
                 else
                 {
-                    // this type is not yet supported
-                    SP_MESSAGE_WARNING( "BaseData_getAttr_value unsupported native type="<<data->getValueTypeString()<<" for data "<<data->getName()<<" ; returning string value" )
-                    PyList_SetItem(row,j,PyString_FromString(typeinfo->getTextValue(valueVoidPtr,i*rowWidth+j).c_str()));
+                    // this type is not yet supported (should not happen)
+                    SP_MESSAGE_ERROR( "BaseData_getAttr_value unsupported native type="<<data->getValueTypeString()<<" for data "<<data->getName()<<" ; returning string value (should not come here!)" )
                 }
             }
             PyList_SetItem(rows,i,row);
