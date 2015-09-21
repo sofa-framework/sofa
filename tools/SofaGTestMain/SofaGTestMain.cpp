5bd465a009af56c357fd58b94043bb495f324949
Marc Legendre <marc.legendre@inria.fr>
2015-09-21 20:40:44 +0200

    Fix use of SOFA_HAVE_DAG in tests
    Former-commit-id: 55e055a84180efd4e07d2c5297d7b35435edc82a

--- tools/SofaGTestMain/SofaGTestMain.cpp
+++ tools/SofaGTestMain/SofaGTestMain.cpp
@@ -1,7 +1,10 @@
 #include <sofa/helper/Utils.h>
 #include <sofa/helper/system/FileRepository.h>
 #include <sofa/helper/system/FileSystem.h>
-#include <sofa/simulation/graph/init.h>
+#include <sofa/simulation/config.h> // #defines SOFA_HAVE_DAG (or not)
+#ifdef SOFA_HAVE_DAG
+#  include <sofa/simulation/graph/init.h>
+#endif
 #include <sofa/simulation/tree/init.h>
 
 #include <gtest/gtest.h>
@@ -16,7 +19,9 @@ int main(int argc, char **argv)
     testing::InitGoogleTest(&argc, argv);
 
     sofa::simulation::tree::init();
+#ifdef SOFA_HAVE_DAG
     sofa::simulation::graph::init();
+#endif
 
 #ifdef WIN32
     const std::string pluginDirectory = Utils::getExecutableDirectory();
@@ -28,8 +33,10 @@ int main(int argc, char **argv)
 
     int ret =  RUN_ALL_TESTS();
 
-    sofa::simulation::graph::cleanup();
+#ifdef SOFA_HAVE_DAG
     sofa::simulation::tree::cleanup();
+#endif
+    sofa::simulation::graph::cleanup();
 
     return ret;
 }
