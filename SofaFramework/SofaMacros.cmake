0c6cae53ea676e14d260461506cab11b9d082ec4
Marc Legendre <marc.legendre@inria.fr>
2015-09-23 18:00:38 +0200

    Move all sofa macros to SofaMacros.cmake
    They are available as soon as you do find_package(SofaFramework), and
    the file SofaPackageConfigHelpers.cmake was removed.
    
    
    Former-commit-id: e3cf4fbb74b3e612865dc34bbfd3b3f6b4c18626

--- SofaFramework/SofaMacros.cmake
+++ SofaFramework/SofaMacros.cmake
@@ -1,3 +1,164 @@
+include(CMakePackageConfigHelpers)
+
+# sofa_write_package_config_files(Foo <version> <build-include-dirs>)
+#
+# Create CMake package configuration files
+# - In the build tree:
+#   - ${CMAKE_BINARY_DIR}/cmake/FooConfig.cmake
+#   - ${CMAKE_BINARY_DIR}/cmake/FooConfigVersion.cmake
+# - In the install tree:
+#   - lib/cmake/Foo/FooConfigVersion.cmake
+#   - lib/cmake/Foo/FooConfig.cmake
+#   - lib/cmake/Foo/FooTargets.cmake
+#
+# This macro factorizes boilerplate CMake code for the different
+# packages in Sofa.  It assumes that there is a FooConfig.cmake.in
+# file template in the same directory.  For example, if a package Foo
+# depends on Bar and Baz, and creates the targets Foo and Qux, here is
+# a typical FooConfig.cmake.in:
+#
+# @PACKAGE_INIT@
+#
+# find_package(Bar REQUIRED)
+# find_package(Baz REQUIRED)
+#
+# if(NOT TARGET Qux)
+# 	include("${CMAKE_CURRENT_LIST_DIR}/FooTargets.cmake")
+# endif()
+#
+# check_required_components(Foo Qux)
+
+macro(sofa_install_targets package_name the_targets install_include_subdir)
+
+    install(TARGETS ${the_targets}
+            EXPORT ${package_name}Targets
+            RUNTIME DESTINATION bin
+            LIBRARY DESTINATION lib
+            ARCHIVE DESTINATION lib
+            PUBLIC_HEADER DESTINATION include/${install_include_subdir})
+
+endmacro()
+
+macro(sofa_write_package_config_files package_name version)
+
+    ## <package_name>Targets.cmake
+    install(EXPORT ${package_name}Targets DESTINATION lib/cmake/${package_name})
+
+    ## <package_name>ConfigVersion.cmake
+    set(filename ${package_name}ConfigVersion.cmake)
+    write_basic_package_version_file(${filename} VERSION ${version} COMPATIBILITY ExactVersion)
+    configure_file("${CMAKE_CURRENT_BINARY_DIR}/${filename}"
+                   "${CMAKE_BINARY_DIR}/cmake/${filename}" COPYONLY)
+    install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${filename}" DESTINATION lib/cmake/${package_name})
+
+    ### <package_name>Config.cmake
+    configure_package_config_file(${package_name}Config.cmake.in
+                                  "${CMAKE_BINARY_DIR}/cmake/${package_name}Config.cmake"
+                                  INSTALL_DESTINATION lib/cmake/${package_name})
+    install(FILES "${CMAKE_BINARY_DIR}/cmake/${package_name}Config.cmake"
+            DESTINATION lib/cmake/${package_name})
+
+endmacro()
+
+macro(sofa_create_package package_name version the_targets include_subdir)
+    sofa_install_targets("${package_name}" "${the_targets}" "${include_subdir}")
+    sofa_write_package_config_files("${package_name}" "${version}")
+endmacro()
+
+
+
+# - Create an imported target from a library path and an include dir path.
+#   Handle the special case where LIBRARY_PATH is in fact an existing target.
+#   Handle the case where LIBRARY_PATH contains the following syntax supported by cmake:
+#                                      "optimized /usr/lib/foo.lib debug /usr/lib/foo_d.lib"
+#
+# sofa_create_imported_target(TARGETNAME LIBRARY_PATH INCLUDE_DIRS)
+#  TARGETNAME_Target  - (output) variable which contains the name of the created target.
+#                       It is usually contains TARGETNAME with one notable exception.
+#                       If LIBRARY_PATH is an existing target, TARGETNAME_Target
+#                       contains LIBRARY_PATH instead.
+#  TARGETNAME         - (input) the name of the target to create.
+#  NAMESPACE          - (input) the namespace where the target is put.
+#  LIBRARY_PATH       - (input) the path to the library ( .so or .lib depending on the platform)
+#  INCLUDE_DIRS       - (input) include directories associated with the library,
+#                       which are added as INTERFACE_INCLUDE_DIRECTORIES for the target.
+#
+# The typical usage scenario is to convert the absolute paths to a system library that cmake return
+# after a find_package call into an imported target. By using the cmake target mechanism, it is
+# easier to redistribute a software that depends on system libraries, whose locations are not
+# known before hand on the consumer system.
+#
+# For further reference about this subject :
+# http://public.kitware.com/pipermail/cmake-developers/2014-March/009983.html
+# Quoted from https://github.com/Kitware/CMake/blob/master/Help/manual/cmake-packages.7.rst
+# "Note that it is not advisable to populate any properties which may contain paths,
+#  such as :prop_tgt:`INTERFACE_INCLUDE_DIRECTORIES` and :prop_tgt:`INTERFACE_LINK_LIBRARIES`,
+#  with paths relevnt to dependencies. That would hard-code into installed packages the
+#  include directory or library paths for dependencies as found on the machine the package
+#  was made on."
+#
+# Example:
+#
+# add_library( SHARED myLib )
+# find_package(PNG REQUIRED)
+# sofa_create_target( PNG MyNamespace "${PNG_LIBRARY}" "${PNG_INCLUDE_DIRS}" )
+# target_link_libraries( myLib PUBLIC ${PNG_Target} )
+#
+
+include(CMakeParseLibraryList)
+
+macro(sofa_create_target TARGETNAME NAMESPACE LIBRARY_PATH INCLUDE_DIRS)
+    # message("TARGETNAME ${TARGETNAME}")
+    set(NAMESPACE_TARGETNAME "${NAMESPACE}::${TARGETNAME}")
+    # message("LIBRARY_PATH ${LIBRARY_PATH}")
+    parse_library_list( "${LIBRARY_PATH}" FOUND LIB_FOUND DEBUG LIB_DEBUG OPT LIB_OPT GENERAL LIB_GEN )
+
+    # message("FOUND ${LIB_FOUND} DEBUG: ${LIB_DEBUG} OPT: ${LIB_OPT} GEN: ${LIB_GEN}")
+    if(LIB_FOUND)
+        if(NOT TARGET ${TARGETNAME} )
+            set(${TARGETNAME}_Target ${NAMESPACE_TARGETNAME} )
+            if(NOT TARGET ${NAMESPACE_TARGETNAME} )
+                add_library( ${NAMESPACE_TARGETNAME} UNKNOWN IMPORTED )
+                set_target_properties( ${NAMESPACE_TARGETNAME} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${INCLUDE_DIRS}" )
+                if( NOT ${LIB_DEBUG} STREQUAL "")
+                    set_target_properties( ${NAMESPACE_TARGETNAME} PROPERTIES IMPORTED_LOCATION_DEBUG "${LIB_DEBUG}" )
+                endif()
+                if( NOT ${LIB_OPT} STREQUAL "")
+                    set_target_properties( ${NAMESPACE_TARGETNAME} PROPERTIES IMPORTED_LOCATION "${LIB_OPT}" )
+                elseif( NOT ${LIB_GEN} STREQUAL "" )
+                    set_target_properties( ${NAMESPACE_TARGETNAME} PROPERTIES IMPORTED_LOCATION "${LIB_GEN}" )
+                endif()
+            endif()
+        else()
+            message( SEND_ERROR "sofa_create_target error. ${TARGETNAME} is an already an existing TARGET.\
+                                 Choose a different name.")
+        endif()
+    else()
+        if(NOT TARGET "${LIBRARY_PATH}" )
+            # message("${LIBRARY_PATH} is not a TARGET")
+            if(NOT TARGET ${TARGETNAME} )
+                # message("${TARGETNAME} is not a TARGET")
+                set(${TARGETNAME}_Target ${NAMESPACE_TARGETNAME} )
+                if(NOT TARGET ${NAMESPACE_TARGETNAME} )
+                    # message("${NAMESPACE_TARGETNAME} is not a TARGET")
+                    add_library( ${NAMESPACE_TARGETNAME} UNKNOWN IMPORTED )
+                    set_target_properties( ${NAMESPACE_TARGETNAME} PROPERTIES IMPORTED_LOCATION "${LIBRARY_PATH}" )
+                    set_target_properties( ${NAMESPACE_TARGETNAME} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${INCLUDE_DIRS}" )
+                endif()
+            else()
+                message( SEND_ERROR "sofa_create_target error. ${TARGETNAME} is an already an existing TARGET.\
+                                     Choose a different name.")
+            endif()
+        else()
+            # message("${LIBRARY_PATH} is a TARGET")
+            set(${TARGETNAME}_Target ${LIBRARY_PATH} )
+        endif()
+
+    endif()
+endmacro()
+
+
+
 
 macro(sofa_add_plugin directory plugin_name)
     string(TOUPPER PLUGIN_${plugin_name} plugin_option)
