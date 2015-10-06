6a6b751e703d71a7ed0b19a8df7c77dd5fde1317
Frederick Roy <frederick.roy@inria.fr>
2015-10-06 16:20:13 +0200

    Add API for Stereo rendering (for QtViewer for now) [was possible already before but now parameters are handled in a BaseObject]
    - Stereo modes are managed with SofaStereo plugin (StereoCamera)
    - StereoOglModel allows to apply different textures for left and right view
    on a same object (used to display stereo video on a plane for augmented reality)
    
    TODO: implementation for QGLViewer
    
    Former-commit-id: f1ab2b574a24320f5c40574ec8fef1bb14151e8e

---
+++ applications/plugins/SofaStereo/initPlugin.cpp
@@ -0,0 +1,94 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 MGH, INRIA, USTL, UJF, CNRS                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Plugins                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#include "initPlugin.h"
+
+#include <sofa/helper/system/FileRepository.h>
+
+#ifndef STEREO_PLUGIN_DATA_DIR
+#define STEREO_PLUGIN_DATA_DIR ""
+#endif
+
+namespace sofa
+{
+
+namespace component
+{
+
+	//Here are just several convenient functions to help user to know what contains the plugin
+
+	extern "C" {
+                SOFA_STEREO_API void initExternalModule();
+                SOFA_STEREO_API const char* getModuleName();
+                SOFA_STEREO_API const char* getModuleVersion();
+                SOFA_STEREO_API const char* getModuleLicense();
+                SOFA_STEREO_API const char* getModuleDescription();
+                SOFA_STEREO_API const char* getModuleComponentList();
+	}
+	
+	void initExternalModule()
+	{
+		static bool first = true;
+        if (first)
+        {
+            first = false;
+
+            sofa::helper::system::DataRepository.addLastPath(std::string(STEREO_PLUGIN_DATA_DIR));
+            sofa::helper::system::DataRepository.addLastPath(std::string(STEREO_PLUGIN_DATA_DIR) + "/data");
+        }
+	}
+
+	const char* getModuleName()
+	{
+                 return "Stereo plugin by Santi";
+	}
+
+	const char* getModuleVersion()
+	{
+                return "0.0";
+	}
+
+	const char* getModuleLicense()
+	{
+		return "LGPL";
+	}
+
+
+	const char* getModuleDescription()
+	{
+        return "Registration code";
+	}
+
+	const char* getModuleComponentList()
+	{
+                return "...";
+	}
+
+
+
+} 
+
+} 
+
+
