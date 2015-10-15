4b6c4d7b7d0e0471f1985d72ded26a1817829a50
nesme <matthieu.nesme@inria.fr>
2015-10-15 11:14:05 +0200

    Compliant: adding maks in AssembledMultiMappings
    Former-commit-id: 50e48a6bc98fdf9f61a9d36e1ec604e3a6bc985c

--- applications/plugins/Compliant/mapping/DifferenceMapping.h
+++ applications/plugins/Compliant/mapping/DifferenceMapping.h
@@ -276,6 +276,22 @@ class SOFA_Compliant_API DifferenceMapping : public AssembledMapping<TIn, TOut>
             }
         }
 
+
+        virtual void updateForceMask()
+        {
+            const pairs_type& p = pairs.getValue();
+
+            for( size_t i = 0, iend = p.size(); i < iend; ++i )
+            {
+                if( this->maskTo[0]->getEntry(i) )
+                {
+                    const index_pair& indices = p[i];
+                    this->maskFrom[0]->insertEntry(indices[0]);
+                    this->maskFrom[1]->insertEntry(indices[1]);
+                }
+            }
+        }
+
     };
 
 
