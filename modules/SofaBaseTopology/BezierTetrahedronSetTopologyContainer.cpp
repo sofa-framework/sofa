058e00914e4b5f5166e8b61a2b90c8a08b8e5a5a
Hervé Delingette <Herve.Delingette@inria.fr>
2015-10-27 13:09:42 +0100

    UPDATE : use ControlPointLocation for indexing DOF UPDATE : add tetrahedronDOFArray
    Former-commit-id: 9d3d7372e6d23be9c847a8e41efcb76da337abee

--- modules/SofaBaseTopology/BezierTetrahedronSetTopologyContainer.cpp
+++ modules/SofaBaseTopology/BezierTetrahedronSetTopologyContainer.cpp
@@ -112,6 +112,7 @@ void BezierTetrahedronSetTopologyContainer::reinit()
 		// triangle index
 		if (degree>2) {
 			size_t ind;
+			offsetToTriangleBezierIndexArray.clear();
 			for (i=0;i<4;++i) {
 				for (ind=0,j=1;j<(degree-1);++j) {
 					for (k=1;k<(degree-j);++k,++ind) {
@@ -119,9 +120,11 @@ void BezierTetrahedronSetTopologyContainer::reinit()
 						bti[trianglesInTetrahedronArray[i][0]]=j;
 						bti[trianglesInTetrahedronArray[i][1]]=k;
 						bti[trianglesInTetrahedronArray[i][2]]=degree-j-k;
+						if (i==0)
+							offsetToTriangleBezierIndexArray.push_back(TriangleBezierIndex(j,k,degree-k-j));			
 						elementMap.insert(ElementMapType(bti,ElementTetrahedronIndex(-1,-1,i,-1)));
 						triangleOffsetMap.insert(OffsetMapType(bti,ind));
-//						std::cerr << "offsetMap["<<(size_t)bti[0]<<' '<<(size_t)bti[1]<<' '<<(size_t)bti[2]<<' '<<(size_t)bti[3]<<" ]= "<<ind<<std::endl;
+						//						std::cerr << "offsetMap["<<(size_t)bti[0]<<' '<<(size_t)bti[1]<<' '<<(size_t)bti[2]<<' '<<(size_t)bti[3]<<" ]= "<<ind<<std::endl;
 						localIndexMap.insert(OffsetMapType(bti,localIndex));
 						bezierIndexArray.push_back(bti);
 						localIndex++;
@@ -132,12 +135,14 @@ void BezierTetrahedronSetTopologyContainer::reinit()
 		// tetrahedron index
 		if (degree>3) {
 			size_t ind=0;
+			offsetToTetrahedronBezierIndexArray.clear();
 			for (i=1;i<(degree-2);++i) {
 				for (j=1;j<(degree-i-1);++j) {
 					for (k=1;k<(degree-j-i);++k,++ind) {
 						TetrahedronBezierIndex bti(0,0,0,0);
 						bti[0]=i;bti[1]=j;bti[2]=k;
 						bti[3]=degree-i-j-k;
+						offsetToTetrahedronBezierIndexArray.push_back(bti);
 						elementMap.insert(ElementMapType(bti,ElementTetrahedronIndex(-1,-1,-1,0)));
 						tetrahedronOffsetMap.insert(OffsetMapType(bti,ind));
 						localIndexMap.insert(OffsetMapType(bti,localIndex));
@@ -182,6 +187,18 @@ void BezierTetrahedronSetTopologyContainer::reinit()
 		d_numberOfTetrahedralPoints.setValue(vertexSet.size());
 
 	}
+		// initialize 	weightedDOFArray
+	tetrahedronDOFArray.clear(); 
+	VecPointID indexArray;
+	size_t i;
+	for (i=0;i<getNbTetrahedra();++i) {
+		indexArray.clear();
+		getGlobalIndexArrayOfBezierPointsInTetrahedron(i,indexArray);
+		tetrahedronDOFArray.push_back(indexArray);
+	}
+}
+const BezierTetrahedronSetTopologyContainer::VecPointID &BezierTetrahedronSetTopologyContainer::getGlobalIndexArrayOfBezierPoints(const TetraID tetrahedronIndex) const {
+	return tetrahedronDOFArray[tetrahedronIndex];
 }
 BezierDegreeType BezierTetrahedronSetTopologyContainer::getDegree() const{
 	return d_degree.getValue();
@@ -199,74 +216,95 @@ BezierDegreeType BezierTetrahedronSetTopologyContainer::getDegree() const{
 	 return(d_weightArray.getValue());
  }
 size_t BezierTetrahedronSetTopologyContainer::getGlobalIndexOfBezierPoint(const TetraID tetrahedronIndex,
-	const TetrahedronBezierIndex id) {
+	const TetrahedronBezierIndex id) 
+{		
+	Tetrahedron tet=getTetrahedron(tetrahedronIndex);
+	BezierDegreeType degree=d_degree.getValue();
+	ElementMapIterator emi=elementMap.find(id);
+	if (emi!=elementMap.end()) {
+		ElementTetrahedronIndex ei=(*emi).second;
+		if (ei[0]!= -1) {
+			// point is a vertex of the tetrahedral mesh
+			return (tet[ei[0]]);
+		} else if (ei[1]!= -1) {
+			// point is on an edge of the tetrahedral mesh
 
-		Tetrahedron tet=getTetrahedron(tetrahedronIndex);
-		BezierDegreeType degree=d_degree.getValue();
-		ElementMapIterator emi=elementMap.find(id);
-		if (emi!=elementMap.end()) {
-			ElementTetrahedronIndex ei=(*emi).second;
-			if (ei[0]!= -1) {
-				// point is a vertex of the tetrahedral mesh
-				return (tet[ei[0]]);
-			} else if (ei[1]!= -1) {
-				// point is on an edge of the tetrahedral mesh
-				// the points on edges are stored after the tetrahedron vertices
-				// there are (degree-1) points store on each edge
-				// eit[ei[1]] = id of edge where the point is located
-				// ei[1] = the local index (<6) of the edge where the point is located 
-				EdgesInTetrahedron eit=getEdgesInTetrahedron(tetrahedronIndex);
-				Edge e=getEdge(eit[ei[1]]);
-				// test if the edge is along the right direction
-				OffsetMapIterator omi=edgeOffsetMap.find(id);
+			// the points on edges are stored after the tetrahedron vertices
+			// there are (degree-1) points store on each edge
+			// eit[ei[1]] = id of edge where the point is located
+			// ei[1] = the local index (<6) of the edge where the point is located 
+			EdgesInTetrahedron eit=getEdgesInTetrahedron(tetrahedronIndex);
+			Edge e=getEdge(eit[ei[1]]);
+			// test if the edge is along the right direction
+			OffsetMapIterator omi=edgeOffsetMap.find(id);
+			if (locationToGlobalIndexMap.empty()) {
 				if (e[0]==tet[edgesInTetrahedronArray[ei[1]][0]]) {
 					return(getNumberOfTetrahedralPoints()+eit[ei[1]]*(degree-1)+(*omi).second);
 				} else {
 					// use the other direction
 					return(getNumberOfTetrahedralPoints()+eit[ei[1]]*(degree-1)+degree-2-(*omi).second);
 				}
-			} else if (ei[2]!= -1) {
-				// point is on an edge of the tetrahedral mesh
-				TrianglesInTetrahedron tit=getTrianglesInTetrahedron(tetrahedronIndex);
-				// the points on triangles are stored after the Bezier points on edges
-				// there are (degree-1)*(degree-2)/2 points store on each triangle
-				// eit[ei[2]] = id of triangle where the point is located
-				// ei[2] = the local index (<4) of the triangle where the point is located 
-				Triangle tr=getTriangle(tit[ei[2]]);
-				Triangle indexTriangle;
-				size_t k,i;
-				i=ei[2];
-				for (k=0;(tr[0]!=tet[trianglesInTetrahedronArray[i][k]]);++k);
-//				indexTriangle[0]=k;
-				indexTriangle[k]=0;
-				if (tr[1]==tet[trianglesInTetrahedronArray[i][(k+1)%3]]) {
-					indexTriangle[(k+1)%3]=1;
-					indexTriangle[(k+2)%3]=2;
-				} else {
-					indexTriangle[(k+2)%3]=1;
-					indexTriangle[(k+1)%3]=2;
+			} else {
+				ControlPointLocation cpl(eit[ei[1]],EDGE,(*omi).second);
+				if (e[0]==tet[edgesInTetrahedronArray[ei[1]][0]]) {
+					cpl=ControlPointLocation(eit[ei[1]],EDGE,degree-2-(*omi).second);
 				}
-				TetrahedronBezierIndex bti(0,0,0,0);
-				bti[trianglesInTetrahedronArray[i][indexTriangle[0]]]=id[trianglesInTetrahedronArray[i][0]];
-				bti[trianglesInTetrahedronArray[i][indexTriangle[1]]]=id[trianglesInTetrahedronArray[i][1]];
-				bti[trianglesInTetrahedronArray[i][indexTriangle[2]]]=id[trianglesInTetrahedronArray[i][2]];
-				OffsetMapIterator omi=triangleOffsetMap.find(bti);
-				return(getNumberOfTetrahedralPoints()+getNumberOfEdges()*(degree-1)+tit[i]*(degree-1)*(degree-2)/2+(*omi).second);
-			} else if (ei[3]!= -1) {
-				// the points on edges are stored after the tetrahedron vertices
-				// there are (degree-1)*(degree-2)/2 points store on each edge
-				// eit[ei[1]] = id of edge where the point is located
-				// ei[1] = the local index (<6) of the edge where the point is located 
-				OffsetMapIterator omi=tetrahedronOffsetMap.find(id);
-				return(getNumberOfTetrahedralPoints()+getNumberOfEdges()*(degree-1)+getNumberOfTriangles()*(degree-1)*(degree-2)/2+tetrahedronIndex*(degree-1)*(degree-2)*(degree-3)/6+(*omi).second);
+				assert(locationToGlobalIndexMap.find(cpl)!=locationToGlobalIndexMap.end());
+				return(locationToGlobalIndexMap[cpl]);
 			}
-		} else {
+		} else if (ei[2]!= -1) {
+			// point is on an edge of the tetrahedral mesh
+			TrianglesInTetrahedron tit=getTrianglesInTetrahedron(tetrahedronIndex);
+			// the points on triangles are stored after the Bezier points on edges
+			// there are (degree-1)*(degree-2)/2 points store on each triangle
+			// eit[ei[2]] = id of triangle where the point is located
+			// ei[2] = the local index (<4) of the triangle where the point is located 
+			Triangle tr=getTriangle(tit[ei[2]]);
+			Triangle indexTriangle;
+			size_t k,i;
+			i=ei[2];
+			for (k=0;(tr[0]!=tet[trianglesInTetrahedronArray[i][k]]);++k);
+			//				indexTriangle[0]=k;
+			indexTriangle[k]=0;
+			if (tr[1]==tet[trianglesInTetrahedronArray[i][(k+1)%3]]) {
+				indexTriangle[(k+1)%3]=1;
+				indexTriangle[(k+2)%3]=2;
+			} else {
+				indexTriangle[(k+2)%3]=1;
+				indexTriangle[(k+1)%3]=2;
+			}
+			TetrahedronBezierIndex bti(0,0,0,0);
+			bti[trianglesInTetrahedronArray[i][indexTriangle[0]]]=id[trianglesInTetrahedronArray[i][0]];
+			bti[trianglesInTetrahedronArray[i][indexTriangle[1]]]=id[trianglesInTetrahedronArray[i][1]];
+			bti[trianglesInTetrahedronArray[i][indexTriangle[2]]]=id[trianglesInTetrahedronArray[i][2]];
+			OffsetMapIterator omi=triangleOffsetMap.find(bti);
+			if (locationToGlobalIndexMap.empty()) {
+				return(getNumberOfTetrahedralPoints()+getNumberOfEdges()*(degree-1)+tit[i]*(degree-1)*(degree-2)/2+(*omi).second);
+			} else {
+				ControlPointLocation cpl(tit[i],TRIANGLE,(*omi).second);
+				assert(locationToGlobalIndexMap.find(cpl)!=locationToGlobalIndexMap.end());
+				return(locationToGlobalIndexMap[cpl]);
+			}
+		} else if (ei[3]!= -1) {
+			// the points on edges are stored after the tetrahedron vertices
+			// there are (degree-1)*(degree-2)/2 points store on each edge
+			// eit[ei[1]] = id of edge where the point is located
+			// ei[1] = the local index (<6) of the edge where the point is located 
+			OffsetMapIterator omi=tetrahedronOffsetMap.find(id);
+			if (locationToGlobalIndexMap.empty()) {
+				return(getNumberOfTetrahedralPoints()+getNumberOfEdges()*(degree-1)+getNumberOfTriangles()*(degree-1)*(degree-2)/2+tetrahedronIndex*(degree-1)*(degree-2)*(degree-3)/6+(*omi).second);
+			} else {
+				ControlPointLocation cpl(ei[3],TETRAHEDRON,(*omi).second);
+				assert(locationToGlobalIndexMap.find(cpl)!=locationToGlobalIndexMap.end());
+				return(locationToGlobalIndexMap[cpl]);
+			}
+		}
+	} else {
 #ifndef NDEBUG
-			sout << "Error. [BezierTetrahedronSetTopologyContainer::getGlobalIndexOfBezierPoint] Bezier Index "<< id <<" has not been recognized to be valid" << sendl;
+		sout << "Error. [BezierTetrahedronSetTopologyContainer::getGlobalIndexOfBezierPoint] Bezier Index "<< id <<" has not been recognized to be valid" << sendl;
 #endif
 		return (0);
-		} 
-        return 0;
+	} 
 }
 
 TetrahedronBezierIndex BezierTetrahedronSetTopologyContainer::getTetrahedronBezierIndex(const size_t localIndex) const
@@ -336,7 +374,7 @@ sofa::helper::vector<TetrahedronBezierIndex> BezierTetrahedronSetTopologyContain
 	// tetrahedron index
 	if (deg>3) {
 		for (i=1;i<(size_t)(deg-2);++i) {
-			for (j=1;j<(size_t)(deg-1);++j) {
+			for (j=1;j<(size_t)(deg-i-1);++j) {
 				for (k=1;k<(deg-j-i);++k) {
 					TetrahedronBezierIndex bti(0,0,0,0);
 					bti[0]=i;bti[1]=j;bti[2]=k;
@@ -383,17 +421,38 @@ void BezierTetrahedronSetTopologyContainer::getGlobalIndexArrayOfBezierPointsInT
 		EdgesInTetrahedron eit=getEdgesInTetrahedron(tetrahedronIndex);
 		for (i=0;i<6;++i) {
 			Edge e=getEdge(eit[i]);
-			offset=getNumberOfTetrahedralPoints()+eit[i]*(degree-1);
-			// check the order of the edge to be consistent with the tetrahedron
-			if (e[0]==tet[edgesInTetrahedronArray[i][0]]) {
-				for (j=0;j<(size_t)(degree-1);++j) {
-					indexArray.push_back(offset+j);
+			if (locationToGlobalIndexMap.empty()) {
+				offset=getNumberOfTetrahedralPoints()+eit[i]*(degree-1);
+				// check the order of the edge to be consistent with the tetrahedron
+				if (e[0]==tet[edgesInTetrahedronArray[i][0]]) {
+					for (j=0;j<(size_t)(degree-1);++j) {
+						indexArray.push_back(offset+j);
+					}
+				} else {
+					int jj;
+					for (jj=degree-2;jj>=0;--jj) {
+						indexArray.push_back(offset+jj);
+					}
 				}
 			} else {
-				int jj;
-				for (jj=degree-2;jj>=0;--jj) {
-					indexArray.push_back(offset+jj);
+				ControlPointLocation cpl;
+				
+				if (e[0]!=tet[edgesInTetrahedronArray[i][0]]) {
+					for (j=0;j<(size_t)(degree-1);++j) {
+						cpl=ControlPointLocation(eit[i],EDGE,j);
+						assert(locationToGlobalIndexMap.find(cpl)!=locationToGlobalIndexMap.end());
+						indexArray.push_back(locationToGlobalIndexMap[cpl]);
+					}
 				}
+				else {
+					int jj;
+					for (jj=degree-2;jj>=0;--jj) {
+						cpl=ControlPointLocation(eit[i],EDGE,(size_t)jj);
+						assert(locationToGlobalIndexMap.find(cpl)!=locationToGlobalIndexMap.end());
+						indexArray.push_back(locationToGlobalIndexMap[cpl]);
+					}
+				}
+
 			}
 		}
 	}
@@ -421,7 +480,13 @@ void BezierTetrahedronSetTopologyContainer::getGlobalIndexArrayOfBezierPointsInT
 					bti[trianglesInTetrahedronArray[i][indexTriangle[1]]]=k;
 					bti[trianglesInTetrahedronArray[i][indexTriangle[2]]]=degree-j-k;
 					OffsetMapIterator omi=triangleOffsetMap.find(bti);
-					indexArray.push_back(offset+(*omi).second);
+					if (locationToGlobalIndexMap.empty()) {
+						indexArray.push_back(offset+(*omi).second);
+					} else {
+						ControlPointLocation cpl(tit[i],TRIANGLE,(*omi).second);
+						assert(locationToGlobalIndexMap.find(cpl)!=locationToGlobalIndexMap.end());
+						indexArray.push_back(locationToGlobalIndexMap[cpl]);
+					}
 				}
 			}
 		}
@@ -436,53 +501,83 @@ void BezierTetrahedronSetTopologyContainer::getGlobalIndexArrayOfBezierPointsInT
 		for (i=0;i<(size_t)(degree-3);++i) {
 			for (j=0;j<(degree-i-3);++j) {
 				for (k=0;k<(degree-j-i-3);++k) {
-					indexArray.push_back(offset+rank);
+					if (locationToGlobalIndexMap.empty()) {
+						indexArray.push_back(offset+rank);
+					} else {
+						ControlPointLocation cpl(tetrahedronIndex,TETRAHEDRON,rank);
+						assert(locationToGlobalIndexMap.find(cpl)!=locationToGlobalIndexMap.end());
+						indexArray.push_back(locationToGlobalIndexMap[cpl]);
+					}
 					rank++;
 				}
 			}
 		}
 	}
 
-}
+	}
 void BezierTetrahedronSetTopologyContainer::getLocationFromGlobalIndex(const size_t globalIndex, BezierTetrahedronPointLocation &location, 
 	size_t &elementIndex, size_t &elementOffset)
 {
 	size_t gi=globalIndex;
-	if (gi<getNumberOfTetrahedralPoints()) {
-		location=POINT;
-		elementIndex=gi;
-		elementOffset=0;
-	} else {
-		gi-=getNumberOfTetrahedralPoints();
-		BezierDegreeType degree=d_degree.getValue();
-		if (gi<(getNumberOfEdges()*(degree-1))) {
-			location=EDGE;
-			elementIndex=gi/(degree-1);
-			elementOffset=gi%(degree-1);
+	if (globalIndexToLocationMap.empty()) {
+		if (gi<getNumberOfTetrahedralPoints()) {
+			location=POINT;
+			elementIndex=gi;
+			elementOffset=0;
 		} else {
-			gi-=getNumberOfEdges()*(degree-1);
-			size_t pointsPerTriangle=(degree-1)*(degree-2)/2;
-			if (gi<(getNumberOfTriangles()*pointsPerTriangle)) {
-				location=TRIANGLE;
-				elementIndex=gi/pointsPerTriangle;
-				elementOffset=gi%pointsPerTriangle;
+			gi-=getNumberOfTetrahedralPoints();
+			BezierDegreeType degree=d_degree.getValue();
+			if (gi<(getNumberOfEdges()*(degree-1))) {
+				location=EDGE;
+				elementIndex=gi/(degree-1);
+				elementOffset=gi%(degree-1);
 			} else {
-				gi-=getNumberOfTriangles()*pointsPerTriangle;
-				size_t pointsPerTetrahedron=(degree-1)*(degree-2)*(degree-3)/6;
-				if (gi<(getNumberOfTetrahedra()*pointsPerTetrahedron)) {
-					location=TETRAHEDRON;
-					elementIndex=gi/pointsPerTetrahedron;
-					elementOffset=gi%pointsPerTetrahedron;
-				}  else {
+				gi-=getNumberOfEdges()*(degree-1);
+				size_t pointsPerTriangle=(degree-1)*(degree-2)/2;
+				if (gi<(getNumberOfTriangles()*pointsPerTriangle)) {
+					location=TRIANGLE;
+					elementIndex=gi/pointsPerTriangle;
+					elementOffset=gi%pointsPerTriangle;
+				} else {
+					gi-=getNumberOfTriangles()*pointsPerTriangle;
+					size_t pointsPerTetrahedron=(degree-1)*(degree-2)*(degree-3)/6;
+					if (gi<(getNumberOfTetrahedra()*pointsPerTetrahedron)) {
+						location=TETRAHEDRON;
+						elementIndex=gi/pointsPerTetrahedron;
+						elementOffset=gi%pointsPerTetrahedron;
+					}  else {
 #ifndef NDEBUG
-					sout << "Error. [BezierTetrahedronSetTopologyContainer::getLocationFromGlobalIndex] Global Index "<< globalIndex <<" exceed the number of Bezier Points" << sendl;
+						sout << "Error. [BezierTetrahedronSetTopologyContainer::getLocationFromGlobalIndex] Global Index "<< globalIndex <<" exceed the number of Bezier Points" << sendl;
 #endif
+					}
 				}
 			}
 		}
+	} else {
+		std::map<size_t,ControlPointLocation>::iterator itcpl=globalIndexToLocationMap.find(globalIndex);
+#ifndef NDEBUG
+		if (itcpl==globalIndexToLocationMap.end())
+			sout << "Error. [BezierTetrahedronSetTopologyContainer::getLocationFromGlobalIndex] Global Index "<< globalIndex <<" is not in the map globalIndexToLocationMap " << sendl;
+		assert(itcpl!=globalIndexToLocationMap.end());
+#endif
+		location=std::get<1>((*itcpl).second);
+		elementIndex=std::get<0>((*itcpl).second);
+		elementOffset=std::get<2>((*itcpl).second);
 	}
 }
-
+void BezierTetrahedronSetTopologyContainer::getEdgeBezierIndexFromEdgeOffset(size_t offset, EdgeBezierIndex &ebi){
+	assert(offset<d_degree.getValue());
+	ebi[0]=offset+1;
+	ebi[1]=d_degree.getValue()-offset-1;
+}
+void BezierTetrahedronSetTopologyContainer::getTriangleBezierIndexFromTriangleOffset(size_t offset, TriangleBezierIndex &tbi){
+	assert(offset<(d_degree.getValue()-1)*(d_degree.getValue()-2)/2);
+	tbi=offsetToTriangleBezierIndexArray[offset];
+}
+void BezierTetrahedronSetTopologyContainer::getTetrahedronBezierIndexFromTetrahedronOffset(size_t offset, TetrahedronBezierIndex &tbi){
+	assert(offset<(d_degree.getValue()-1)*(d_degree.getValue()-2)*(d_degree.getValue()-3)/6);
+	tbi=offsetToTetrahedronBezierIndexArray[offset];
+}
 bool BezierTetrahedronSetTopologyContainer::checkBezierPointTopology()
 {
 	size_t nTetras,elem;
@@ -532,6 +627,17 @@ bool BezierTetrahedronSetTopologyContainer::checkBezierPointTopology()
 			}
 		}
 
+	}
+	if (locationToGlobalIndexMap.size()>0) {
+		// check consistency between both maps
+		assert(locationToGlobalIndexMap.size()==globalIndexToLocationMap.size());
+		std::map<ControlPointLocation,size_t>::iterator itcpl;
+		std::map<size_t,ControlPointLocation>::iterator itgi;
+		for (itcpl=locationToGlobalIndexMap.begin();itcpl!=locationToGlobalIndexMap.end();++itcpl) {
+			itgi=globalIndexToLocationMap.find(itcpl->second);
+			assert(itgi!=globalIndexToLocationMap.end());
+			assert((*itgi).second==(*itcpl).first);
+		}
 	}
 	return( true);
 }
