f034a1a6444a0e17d863a4293dd5993a19cd5edb
nesme <matthieu.nesme@inria.fr>
2015-10-27 16:31:06 +0100

    QTGUI: fixing QT4 specific includes
    Former-commit-id: 1e3b5e92d35bd8be4aae9724747322a8b8af926d

--- applications/sofa/gui/qt/DataFilenameWidget.h
+++ applications/sofa/gui/qt/DataFilenameWidget.h
@@ -25,15 +25,9 @@
 #ifndef SOFA_GUI_QT_DATAFILENAMEWIDGET_H
 #define SOFA_GUI_QT_DATAFILENAMEWIDGET_H
 
-#ifdef SOFA_QT_4
 #include <QLineEdit>
 #include <QPushButton>
 #include <QHBoxLayout>
-#else
-#include <qlineedit.h>
-#include <qpushbutton.h>
-#include <qlayout.h>
-#endif
 
 #include "DataWidget.h"
 
@@ -54,7 +48,7 @@ public:
         QWidget* parent,
         const char* name,
         core::objectmodel::Data<std::string>* data):
-        TDataWidget<std::string>(parent,name,data) {};
+        TDataWidget<std::string>(parent,name,data) {}
 
     virtual bool createWidgets();
     virtual void setDataReadOnly(bool readOnly);
