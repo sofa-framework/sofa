f7c2d31cd1206f62b7a06899cdcd61065b5dfe97
Francois Faure <francois.faure@inria.fr>
2015-11-09 12:42:13 +0900

    Fix compilation and optimize DisplacementMatrixEngine
    Former-commit-id: 86a7a5f1d4e24d414cd9aa7c86b667f2a00d2735

--- modules/SofaMiscEngine/DisplacementMatrixEngine.h
+++ modules/SofaMiscEngine/DisplacementMatrixEngine.h
@@ -1,5 +1,30 @@
-#ifndef FLEXIBLE_DisplacementMatrixENGINE_H
-#define FLEXIBLE_DisplacementMatrixENGINE_H
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-2015 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef SOFA_COMPONENT_ENGINE_DisplacementMatrixEngine_H
+#define SOFA_COMPONENT_ENGINE_DisplacementMatrixEngine_H
+#include "config.h"
 
 #include <sofa/core/DataEngine.h>
 #include <sofa/defaulttype/Vec.h>
@@ -17,8 +42,8 @@ namespace engine
 
 /*
  * Engine which computes a displacement matrix with respect to an origin matrix, as D(t) = M(t).M(0)^-1
- * Adapted from ComputeDualQuatEngine
- * François Faure, 2015
+ * @warning Assumes that the initial matrices never change after initialization. This allows computing their inverses only once. To force recomputation, use init().
+ * @author François Faure, 2015;
  *
  */
 template < class DataTypes >
@@ -27,42 +52,30 @@ class DisplacementMatrixEngine : public sofa::core::DataEngine
 public:
     SOFA_CLASS( SOFA_TEMPLATE( DisplacementMatrixEngine, DataTypes ), sofa::core::DataEngine );
 
-
-    // Typedefs
     typedef typename DataTypes::Real Real;
     typedef typename DataTypes::Coord Coord; // rigid
     typedef typename DataTypes::VecCoord VecCoord;
-
-
-    // Constructor
-    DisplacementMatrixEngine();
-
-
-    // To simplify the template name in the xml file
-    virtual std::string getTemplateName() const
-    {
-        return templateName(this);
-    }
-
-
-    // To simplify the template name in the xml file
-    static std::string templateName(const DisplacementMatrixEngine<DataTypes>* = NULL)
-    {
-        return DataTypes::Name();
-    }
-
-    // Update the engine
-    void update();
-
-    // First position
-    Data< VecCoord > d_x0;  // initial matrices
-    // Current position
-    Data< VecCoord > d_x;   // current matrices
-
-    // The displacement matrices, as vector of Mat4f
     typedef defaulttype::Mat4x4f Mat4;
-    Data< helper::vector< Mat4 > > d_displaceMats;
 
+    // inputs
+    Data< VecCoord > d_x0;  ///< initial bone positions
+    Data< VecCoord > d_x;   ///< current bone positions
+
+    // outputs
+    Data< helper::vector< Mat4 > > d_displaceMats; ///< displacement matrices
+
+
+    DisplacementMatrixEngine();
+    void init();   // compute the inverse matrices
+    void update(); // compute the displacements wrt original positions
+
+
+    // To simplify the template name in the xml file
+    virtual std::string getTemplateName() const { return templateName(this); }
+    static std::string templateName(const DisplacementMatrixEngine<DataTypes>* = NULL) { return DataTypes::Name(); }
+
+protected:
+    helper::vector<Mat4> inverses;  ///< inverse initial positions
 
 };
 
