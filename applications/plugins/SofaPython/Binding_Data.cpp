90081d7b8855cac6086d0a19b3657203db34ad15
nesme <matthieu.nesme@inria.fr>
2015-11-24 16:18:29 +0100

    SofaPython: a DataLink can now be set direclty with the = operator (no need to call 'setParent')
    Former-commit-id: e82a0f29b5c400fa522dee8cd6dea7a8f521d224

--- applications/plugins/SofaPython/Binding_Data.cpp
+++ applications/plugins/SofaPython/Binding_Data.cpp
@@ -381,7 +381,16 @@ bool SetDataValuePython(BaseData* data, PyObject* args)
     {
         // it's a string
         char *str = PyString_AsString(args); // for setters, only one object and not a tuple....
-        data->read(str);
+
+        if( strlen(str)>0 && str[0]=='@' ) // DataLink
+        {
+            data->setParent(str);
+            data->setDirtyOutputs(); // forcing children updates (should it be done in BaseData?)
+        }
+        else
+        {
+            data->read(str);
+        }
         return true;
     }
     else if (isList)
