5d65394b5eda2bd39c93ba8a9fdd184ce3425f94
Olivier Carré <ocarre@dhcp51.inrialpes.fr>
2015-12-03 13:50:41 +0100

    FIX (workaround): ColorMap when using OglTexture
    Former-commit-id: b701c0acc8324775d308c716ccda690bdcd7f043

--- modules/SofaOpenglVisual/ColorMap.cpp
+++ modules/SofaOpenglVisual/ColorMap.cpp
@@ -364,8 +364,6 @@ void ColorMap::drawVisual(const core::visual::VisualParams* vparams)
 
     if (!f_showLegend.getValue()) return;
 
-    glUseProgramObjectARB(0);
-
     // Prepare texture for legend
     // crashes on mac in batch mode (no GL context)
     if (vparams->isSupported(core::visual::API_OpenGL)
@@ -415,7 +413,6 @@ void ColorMap::drawVisual(const core::visual::VisualParams* vparams)
 
     glPushAttrib(GL_ENABLE_BIT);
     glDisable(GL_DEPTH_TEST);
-    glEnable(GL_TEXTURE_1D);
     glDisable(GL_LIGHTING);
     glDisable(GL_BLEND);
 
@@ -433,10 +430,20 @@ void ColorMap::drawVisual(const core::visual::VisualParams* vparams)
     glPushMatrix();
     glLoadIdentity();
 
+    for(int i = 0; i < 8; ++i)
+    {
+        glActiveTexture(GL_TEXTURE0 + i);
+        glDisable(GL_TEXTURE_2D);
+    }
+
+    glActiveTexture(GL_TEXTURE0);
+    glEnable(GL_TEXTURE_1D);
     glBindTexture(GL_TEXTURE_1D, texture);
 
     //glBlendFunc(GL_ONE, GL_ONE);
     glColor3f(1.0f, 1.0f, 1.0f);
+
+    glUseProgramObjectARB(0);
 
     glBegin(GL_QUADS);
 
@@ -453,6 +460,8 @@ void ColorMap::drawVisual(const core::visual::VisualParams* vparams)
     glVertex3f(10.0f+f_legendOffset.getValue().x(), yoffset+120.0f+f_legendOffset.getValue().y(), 0.0f);
 
     glEnd();
+
+    glDisable(GL_TEXTURE_1D);
 
     // Restore projection matrix
     glMatrixMode(GL_PROJECTION);
