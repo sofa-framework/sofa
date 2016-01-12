5fd66eaf27ef49777560b3d0378faff5690c28e1
Frederick Roy <frederick.roy@inria.fr>
2016-01-12 12:04:17 +0100

    Removed the OpenGL dependencies of UniformMass ADD drawCross() in drawTool API
    Former-commit-id: 6a046a56092c9198de38a41244a44e31abb5554d

--- framework/sofa/core/visual/DrawToolGL.cpp
+++ framework/sofa/core/visual/DrawToolGL.cpp
@@ -505,6 +505,24 @@ void DrawToolGL::drawArrow(const Vector3& p1, const Vector3 &p2, float radius, f
 
 }
 
+void DrawToolGL::drawCross(const Vector3&p, float length, const Vec4f& colour)
+{
+    std::vector<sofa::defaulttype::Vector3> bounds;
+
+    for ( unsigned int i=0 ; i<3 ; i++ )
+    {
+        sofa::defaulttype::Vector3 p0 = p;
+        sofa::defaulttype::Vector3 p1 = p;
+
+        p0[i] -= length;
+        p1[i] += length;
+
+        bounds.push_back(p0);
+        bounds.push_back(p1);
+    }
+    drawLines(bounds, 1, colour);
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void DrawToolGL::drawPlus ( const float& radius, const Vec<4,float>& colour, const int& subd)
