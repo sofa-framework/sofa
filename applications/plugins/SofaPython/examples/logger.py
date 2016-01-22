01114207e0968e9ec4bcd82308d648e4321f677b
nesme <matthieu.nesme@inria.fr>
2016-01-22 16:51:07 +0100

    SofaPython: adding comments
    Former-commit-id: 77b7ef52b1288c9a1347f79185721756659fba5d

--- applications/plugins/SofaPython/examples/logger.py
+++ applications/plugins/SofaPython/examples/logger.py
@@ -2,11 +2,13 @@ import Sofa
 
 def createScene(node):
 
+# w/ emitter
     Sofa.msg_info( "emitter1", "my message info" )
     Sofa.msg_warning( "emitter2", "my message warning" )
     Sofa.msg_error( "emitter3", "my message error" )
     Sofa.msg_fatal( "emitter4", "my message fatal" )
 
+# w/o emitter
     Sofa.msg_info( "my message info" )
     Sofa.msg_warning( "my message warning" )
     Sofa.msg_error( "my message error" )
