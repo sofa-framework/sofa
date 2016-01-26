65211c3d7a82d60ba1df011761b24570230049ac
Damien Marchal <damien.marchal@univ-lille1.fr>
2016-01-26 13:07:14 +0100

    A a sofa component to control how the messages are handled. There is also unity test with this component
    Former-commit-id: 52c44191015515e072d5b0838898e3e0c1daf953

---
+++ modules/SofaComponentBase/messageHandlerComponent.h
@@ -0,0 +1,106 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-2015 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* This component is open-source                                               *
+*                                                                             *
+* Authors: Damien Marchal                                                     *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+/*****************************************************************************
+* User of this library should read the documentation
+* in the messaging.h file.
+******************************************************************************/
+#ifndef SOFA_MESSAGEHANDLERCOMPONENT_H
+#define SOFA_MESSAGEHANDLERCOMPONENT_H
+
+#include <sofa/core/objectmodel/BaseObjectDescription.h>
+#include <sofa/core/objectmodel/BaseObject.h>
+#include <sofa/core/objectmodel/Data.h>
+
+#include <string>
+
+namespace sofa
+{
+namespace helper
+{
+namespace logging
+{
+    class MessageHandler ;
+}
+}
+}
+
+namespace sofa
+{
+namespace component
+{
+namespace logging
+{
+using sofa::core::objectmodel::BaseObjectDescription ;
+using sofa::core::objectmodel::BaseObject ;
+using sofa::core::objectmodel::Data ;
+using std::string ;
+using sofa::helper::logging::MessageHandler;
+
+class MessageHandlerComponent : public virtual BaseObject
+{
+public:
+    SOFA_CLASS(MessageHandlerComponent, BaseObject);
+
+    MessageHandlerComponent() ;
+    virtual ~MessageHandlerComponent(){} ;
+
+    /// Inherited from BaseObject.
+    /// Parse the given description to assign values to this object's fields and
+    /// potentially other parameters.
+    virtual void parse ( BaseObjectDescription* arg ) ;
+
+    Data<string>        d_type       ;
+    bool                m_isValid    ;
+
+    bool isValid(){ return m_isValid; }
+};
+
+class FileMessageHandlerComponent : public virtual BaseObject
+{
+public:
+    SOFA_CLASS(FileMessageHandlerComponent, BaseObject) ;
+
+    FileMessageHandlerComponent() ;
+    virtual ~FileMessageHandlerComponent() ;
+
+    /// Inherited from BaseObject.
+    /// Parse the given description to assign values to this object's fields and
+    /// potentially other parameters.
+    virtual void parse ( BaseObjectDescription* arg ) ;
+
+    Data<string>        d_filename        ;
+    MessageHandler*     m_handler         ;
+
+
+    bool                m_isValid    ;
+    bool isValid(){ return m_isValid; }
+};
+
+}
+}
+}
+#endif // SOFA_MESSAGEHANDLERCOMPONENT_H
