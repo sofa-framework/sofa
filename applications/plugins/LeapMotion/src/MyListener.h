f5c980d57c91c26cdcee59795b870ec09ccd6d12
htalbot <hugo.talbot@inria.fr>
2016-02-12 11:36:21 +0100

    FIX warnings in plugin LeapMotion/src/MyListener.h
    After previous commits, undetected warnings have been fixed.
    
    
    Former-commit-id: 09801be20a1f5803cb319f56f018232762a49f1a

--- applications/plugins/LeapMotion/src/MyListener.h
+++ applications/plugins/LeapMotion/src/MyListener.h
@@ -70,41 +70,38 @@ class MyListener : public Listener {
     virtual void onFrame(const Controller&);
     virtual void onFocusGained(const Controller&);
     virtual void onFocusLost(const Controller&);
-	void update( Leap::Frame frame );
+    void update( Leap::Frame frame );
 
     int64_t     getFrameID() { return frame.id(); }
     Vector      getPalmPosition() { return hand.palmPosition(); }
-	Vector      getStabilizedPalmPosition() { return hand.palmPosition(); }// hand.stabilizedPalmPosition(); }
-	Vector		getPalmNormal() { return hand.palmNormal(); }
+    Vector      getStabilizedPalmPosition() { return hand.palmPosition(); }// hand.stabilizedPalmPosition(); }
+    Vector      getPalmNormal() { return hand.palmNormal(); }
     Quat        getPalmRotation() { return palmOrientation; }
     FingerList  getFingers() { return hand.fingers(); }
-	FingerList  getSecondHandFingers() { return secondHand.fingers(); }
+    FingerList  getSecondHandFingers() { return secondHand.fingers(); }
     Hand        getHand() { return hand; }
-	Hand        getSecondHand() { return secondHand; }
+    Hand        getSecondHand() { return secondHand; }
     Vector      getHandSphereCenter() { return hand.sphereCenter(); }
     float       getHandSphereRadius() { return hand.sphereRadius(); }
     GestureList getGestures() { return gestures; }
     Vector      getTranslation() { return translation; }
-	InteractionBox getInteractionBox() { return interactionBox; }
-    float		getFPS() { return fUpdateFPS;}
-	float		getCurrentFramesPerSecond() {	return currentFramesPerSecond; }
+    InteractionBox getInteractionBox() { return interactionBox; }
+    float	getFPS() { return fUpdateFPS;}
+    float	getCurrentFramesPerSecond() {	return currentFramesPerSecond; }
 
 protected:
     Frame frame;
-	float currentFramesPerSecond;
-	InteractionBox interactionBox;
-	Hand hand, secondHand; 
-	Quat palmOrientation;
-	GestureList gestures;
-	Vector translation;
+    float currentFramesPerSecond;
+    InteractionBox interactionBox;
+    Hand hand, secondHand;
+    Quat palmOrientation;
+    GestureList gestures;
+    Vector translation;
 
 private:
     sofa::helper::system::thread::CTime timer;
-	float  fUpdateFPS;
-	RollingAverage m_avgUpdateDeltaTime;
-	double	m_fLastUpdateTimeSeconds;
+    float  fUpdateFPS;
+    RollingAverage m_avgUpdateDeltaTime;
+    double	m_fLastUpdateTimeSeconds;
     double m_fLastRenderTimeSeconds;
-	int timer_iterations;
-	double	m_lastUpdateTime;
-
 };
