5f3a64d0883f4f8976d9db0a8cfbf5c1d57e3a99
Etienne Schmitt <etienne.schmitt@inria.fr>
2016-02-13 14:23:41 +0100

    updating licenses.
    Signed-off-by: Etienne Schmitt <etienne.schmitt@inria.fr>
    
    Former-commit-id: 4b6a6d51ae2391fa2a861c26575be03baf3522da

--- modules/SofaOpenglVisual/OglVariable.h
+++ modules/SofaOpenglVisual/OglVariable.h
@@ -1,6 +1,6 @@
 /******************************************************************************
-*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
-*                (c) 2006-2011 MGH, INRIA, USTL, UJF, CNRS                    *
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-2016 INRIA, USTL, UJF, CNRS, MGH                    *
 *                                                                             *
 * This library is free software; you can redistribute it and/or modify it     *
 * under the terms of the GNU Lesser General Public License as published by    *
