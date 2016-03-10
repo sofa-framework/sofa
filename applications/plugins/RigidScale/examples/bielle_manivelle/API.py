aad0b9eea7aa57f4a1cce6df7f212464491bd42d
Dicko Ali Hamadi <ali-hamadi.dicko@inria.fr>
2016-03-10 09:25:11 +0100

    [RigidScale]: update on python script
    Former-commit-id: 5e276d587f8da1edc0164bdc238f9bf7aa2059e1

--- applications/plugins/RigidScale/examples/bielle_manivelle/API.py
+++ applications/plugins/RigidScale/examples/bielle_manivelle/API.py
@@ -5,13 +5,15 @@ import RigidScale.API
 
 # parts of the mechanism
 parts = [ 
-    ["Corps",[0, 0, 0, 0, 0, 0, 1],"7.8e+3","0.obj"],
-    ["Roue",[0, 0, -0.148, 0, 0, 0, 1],"7.8e+3","3.obj"],
-    ["Came",[1.085, -0.072, 0.33, 0, 0, 0, 1],"7.8e+3","2.obj"],
-    ["Piston",[2.05, 0, 0.33, 0, 0, 0, 1],"7.8e+3","1.obj"]
+    ["Corps", [0, 0, 0, 0, 0, 0, 1], 7.8e+3, "0.obj"],
+    ["Roue", [0, 0, -0.148, 0, 0, 0, 1], 7.8e+3, "3.obj"],
+    ["Came", [1.085, -0.072, 0.33, 0, 0, 0, 1], 7.8e+3, "2.obj"],
+    ["Piston", [2.05, 0, 0.33, 0, 0, 0, 1], 7.8e+3, "1.obj"]
 ]
 
+"""
 
+"""
 # joint offsets: name, rigid body, offset
 joint_offsets = [
     ["offset0", 0, [0, 0, 0, 0, 0, 0, 1]],
@@ -24,12 +26,11 @@ joint_offsets = [
     ["offset2", 3, [-0.113, 0, 0, 0, 0, 0, 1]],
 
     ["offset3", 3, [0.15, 0, 0, 0, 0, 0, 1]],
-    ["offset3", 0, [2.2, 0, 0.33, 0, 0, 0, 1]]    
+    ["offset3", 0, [2.2, 0, 0.33, 0, 0, 0, 1]]
 ]
 
 # joints: name, offset1, offset2, joint type, joint axis
 links = [ 
-
     #revolute joint around z
     ["hinge_corps-roue",    0, 1, Compliant.StructuralAPI.HingeRigidJoint,  2],
     ["hinge_roue-came",     2, 3, Compliant.StructuralAPI.HingeRigidJoint,  2],
@@ -39,9 +40,6 @@ links = [
     ["slider_corps-piston", 6, 7, Compliant.StructuralAPI.SliderRigidJoint, 0]
 ]
     
-
-
-
 def createScene(node):
     
     node.createObject('VisualStyle', displayFlags='showBehaviorModels hideCollisionModels hideMappings hideForceFields')
@@ -58,30 +56,31 @@ def createScene(node):
     # solvers
     compliance = 0    
     node.createObject('CompliantImplicitSolver', name='odesolver',stabilization=1)
-    #node.createObject('MinresSolver', name='numsolver', iterations='500', precision='1e-14');
-    node.createObject('LDLTSolver', name='numsolver', schur=0)
+    node.createObject('SequentialSolver', iterations=25, precision=1E-15, iterateOnBilaterals=1)
+    node.createObject('LDLTResponse', schur=0)
 
     # scene        
     sceneNode = node.createChild('scene') 
-    bodies  = []
+    bodies = []
     offsets = []
-    joints  = []
+    joints = []
         
     # create ShearlessAffineBody
     for p in parts:
-        body = RigidScale.API.ShearlessAffineBody( sceneNode, p[0] )
+        body = RigidScale.API.ShearlessAffineBody(sceneNode, p[0])
         
         mesh = SofaPython.Tools.localPath(__file__, p[3])
         density = float(p[2])
         offset = p[1]
-        body.setFromMesh( mesh, density, offset, voxelSize=0.1 )
+        body.setFromMesh(mesh, density, offset=offset, voxelSize=0.05)
+        body.addElasticBehavior("behavior", 1E6, 0)
         
-        cm = body.addCollisionMesh( mesh )
+        cm = body.addCollisionMesh(mesh, scale3d=[1,1,1], offset=offset)
         cm.addVisualModel() # visual model similar to collision model
         
-        body.affineDofs.showObject=True
+        body.affineDofs.showObject=False
         body.affineDofs.showObjectScale=0.5
-        
+
         bodies.append(body)
       
     # fix first body
@@ -89,24 +88,21 @@ def createScene(node):
       
     # create offsets
     for o in joint_offsets:
-      
-        o = bodies[o[1]].addRigidScaleOffset( o[0], o[2] )
-        
+        o = bodies[o[1]].addOffset(o[0], o[2])
+
         o.dofs.showObject=True
         o.dofs.showObjectScale=0.25
-        
-        offsets.append( o )
 
-    ## create joints
+        offsets.append(o)
+
+    # create joints
     for l in links:
-      
-      j = l[3] (l[4], l[0], offsets[l[1]].node, offsets[l[2]].node )
-      j.constraint.compliance.compliance = compliance
-      
-      joints.append( j )
+        j = l[3] (l[4], l[0], offsets[l[1]].node, offsets[l[2]].node)
+        j.constraint.compliance.compliance = compliance
+
+        joints.append(j)
           
     # just for fun!
-    #rigids[1].addMotor([0,0,0,0,0,1000])
+    bodies[1].addMotor([0,0,0,0,0,10000])
             
     return node
-
