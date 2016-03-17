7a23d2daee961a53b3f727c5e5565dc470b74b4c
Thomas Lemaire <thomas.lemaire@inria.fr>
2016-03-17 16:48:18 +0100

    [python-swig] example with data initialization
    Former-commit-id: fb398b8c2e185d5b4d3a3202ccbb27bca824493e

--- examples/bindings-python/simulation.py
+++ examples/bindings-python/simulation.py
@@ -2,6 +2,6 @@ import sofa.core
 import sofa.simulation
 
 rootNode = sofa.simulation.Simulation.GetRoot()
-mo = rootNode.createObject("MechanicalObject")
+mo = rootNode.createObject("MechanicalObject", position="1 2 3")
 print "dof:", mo.getPathName()
 print mo.findData("position")
