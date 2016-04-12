8bc954299c9a212cdc7916a005f9a65c898d1522
Thomas Lemaire <thomas.lemaire@inria.fr>
2016-04-12 15:46:14 +0200

    [RigidScale.API] support more than 1 sample per solid + cleanup + simple example
    Former-commit-id: 21469d6ad0e3e11b5c473b18df2db9c7fdab7134

---
+++ applications/plugins/RigidScale/examples/bielle_manivelle/API_roue.py
@@ -0,0 +1,39 @@
+import Sofa
+import SofaPython.Tools
+import Compliant.StructuralAPI
+import RigidScale.API
+
+def createScene(node):
+    
+    node.createObject('VisualStyle', displayFlags='showBehaviorModels hideCollisionModels hideMappings hideForceFields')
+    
+    node.dt=0.01
+    node.gravity='0 0 0'
+    
+    node.createObject('RequiredPlugin', name = 'Compliant' )
+    node.createObject('RequiredPlugin', name = 'Flexible' )
+    node.createObject('RequiredPlugin', name = 'image' )
+    node.createObject('RequiredPlugin', name = 'RigidScale' )
+    node.createObject('CompliantAttachButtonSetting' )
+    
+    # solvers
+    compliance = 0    
+    node.createObject('CompliantImplicitSolver', name='odesolver',stabilization=1)
+    node.createObject('SequentialSolver', iterations=25, precision=1E-15, iterateOnBilaterals=1)
+    node.createObject('LDLTResponse', schur=0)
+
+    # scene        
+    sceneNode = node.createChild('scene') 
+    
+    body = RigidScale.API.ShearlessAffineBody(sceneNode, "Roue")
+    mesh = SofaPython.Tools.localPath(__file__, "3.obj")
+    body.setFromMesh(mesh, voxelSize=0.05, numberOfPoints=5)
+    body.addBehavior(youngModulus=1e4, numberOfGaussPoint=50)
+    
+    cm = body.addCollisionMesh(mesh)
+    cm.addVisualModel() # visual model similar to collision model
+    
+    body.affineDofs.showObject=True
+    body.affineDofs.showObjectScale=0.5
+    
+    body.setFixed(True)
