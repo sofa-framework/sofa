b3edf977baddb93a526f96dc19b8aeb76522a193
Maxime Tournier <maxime.tournier@inria.fr>
2016-04-14 12:35:38 +0200

    [Compliant] added easy pure python mappings, see examples/numpy_mapping.py
    Former-commit-id: e8a65cad132d339dc92eafcfbf9b4e78c2f7beb0

--- applications/plugins/Compliant/mapping/PythonMultiMapping.h
+++ applications/plugins/Compliant/mapping/PythonMultiMapping.h
@@ -9,6 +9,16 @@ namespace sofa {
 namespace component {
 namespace mapping {
 
+// TODO move this elsewhere if reusable
+struct with_py_callback {
+    typedef void* (*py_callback_type)();
+    py_callback_type py_callback;
+
+    with_py_callback();
+    virtual ~with_py_callback();
+};
+
+    
 /** 
 	a very general mapping defined on the python side: f(x) = value, df(x) = jacobian
 
@@ -22,12 +32,12 @@ namespace mapping {
 */
 
 // TODO also fill a mask Data from python to be able to setup frommasks
-
+    
 template<class TIn, class TOut>
-class SOFA_Compliant_API PythonMultiMapping : public AssembledMultiMapping<TIn, TOut> {
+class SOFA_Compliant_API PythonMultiMapping : public AssembledMultiMapping<TIn, TOut>,
+                                              public with_py_callback {
 	typedef PythonMultiMapping self;
-	
-
+    
   public:
 	SOFA_CLASS(SOFA_TEMPLATE2(PythonMultiMapping,TIn,TOut), 
 			   SOFA_TEMPLATE2(AssembledMultiMapping,TIn,TOut));
@@ -37,38 +47,53 @@ class SOFA_Compliant_API PythonMultiMapping : public AssembledMultiMapping<TIn, 
 
 	Data<matrix_type> matrix;
 	Data<value_type> value;		
-	
+
+    
 	PythonMultiMapping() :
 		matrix(initData(&matrix, "jacobian", "jacobian for the mapping (row-major)")),
 		value(initData(&value, "value", "mapping value")) {
+        
 	}
 
+    enum {
+        out_deriv_size = TOut::Deriv::total_size,
+        in_deriv_size = TIn::Deriv::total_size,
+
+        out_coord_size = TOut::Coord::total_size,
+        in_coord_size = TIn::Coord::total_size
+    };
+    
 
     virtual void assemble( const helper::vector<typename self::in_pos_type>& in )  {
 		// initialize jacobians
 
-		typedef typename self::jacobian_type::CompressedMatrix jack_type;
+        const value_type& value = this->value.getValue();
+        const matrix_type& matrix = this->matrix.getValue();
+        
+		typedef typename self::jacobian_type::CompressedMatrix block_type;
 
-		// each input mstate
+		// resize jacobian blocks
         unsigned size = 0;
-        const unsigned rows = value.getValue().size() * TOut::Deriv::total_size;
+        
+        const unsigned rows = value.size() * out_deriv_size;
 
 		for(unsigned j = 0, m = in.size(); j < m; ++j) {
-			jack_type& jack = this->jacobian(j).compressedMatrix;
+			block_type& block = this->jacobian(j).compressedMatrix;
 
             const unsigned cols = this->from(j)->getMatrixSize();
-			jack.resize(rows, cols );
-			jack.setZero();
+			block.resize(rows, cols);
+			block.setZero();
             size += rows * cols;
 		}
 
-        if(matrix.getValue().size() != size) {
+        if(matrix.size() != size) {
 
-            if( matrix.getValue().size() ) serr << "matrix size incorrect" << sendl;
-            else {
-                // std::cout << "derp" << std::endl;
-                return;
+            // note: empty 'jacobian' will be silently treated as zero
+            if( matrix.size() ) {
+                serr << "assemble: incorrect jacobian size, treating as zero" << sendl;
             }
+            
+            return;
         }
 
 
@@ -76,27 +101,27 @@ class SOFA_Compliant_API PythonMultiMapping : public AssembledMultiMapping<TIn, 
 		unsigned off = 0;
 			
 		// each output mstate
-		for(unsigned i = 0, n = value.getValue().size(); i < n; ++i) {
+		for(unsigned i = 0, n = value.size(); i < n; ++i) {
 
-            for(unsigned v = 0; v < self::Nout; ++v) {
+            for(unsigned v = 0; v < out_deriv_size; ++v) {
 
                 // each input mstate
                 for(unsigned j = 0, m = in.size(); j < m; ++j) {
-                    jack_type& jack = this->jacobian(j).compressedMatrix;
+                    block_type& block = this->jacobian(j).compressedMatrix;
 				
                     const unsigned dim = this->from(j)->getMatrixSize();
 				
-                    const unsigned r = self::Nout * i + v;
-                    jack.startVec(r);
+                    const unsigned r = out_deriv_size * i + v;
+                    block.startVec(r);
 
                     // each input mstate dof
                     for(unsigned k = 0, p = in[j].size(); k < p; ++k) {
 					
                         // each dof dimension
-                        for(unsigned u = 0; u < self::Nin; ++u) {
-                            const unsigned c = k * self::Nin + u;
-                            const SReal value = matrix.getValue()[off + c];
-                            if( value ) jack.insertBack(r, c) = value;
+                        for(unsigned u = 0; u < in_deriv_size; ++u) {
+                            const unsigned c = k * in_deriv_size + u;
+                            const SReal value = matrix[off + c];
+                            if( value ) block.insertBack(r, c) = value;
                         }					
                     }
                     off += dim;
@@ -105,14 +130,13 @@ class SOFA_Compliant_API PythonMultiMapping : public AssembledMultiMapping<TIn, 
             }
 			
 		}
-		assert( off == matrix.getValue().size() );
+		assert( off == matrix.size() );
 
 		// each input mstate
 		for(unsigned j = 0, m = in.size(); j < m; ++j) {
-			jack_type& jack = this->jacobian(j).compressedMatrix;
+			block_type& block = this->jacobian(j).compressedMatrix;
 			
-			jack.finalize();
-            // std::cout << jack << std::endl;
+			block.finalize();
 		}
 		
 		
@@ -121,15 +145,17 @@ class SOFA_Compliant_API PythonMultiMapping : public AssembledMultiMapping<TIn, 
 
     virtual void apply(typename self::out_pos_type& out, 
                        const helper::vector<typename self::in_pos_type>& /*in*/ ) {
+        if(this->py_callback) this->py_callback();
+        
+        const value_type& value = this->value.getValue();
+        
+        if( out.size() != value.size() ) {
+            serr << "apply: size for data 'value' does not match output, auto-resizing" << sendl;
+            const_cast<value_type&>(value).resize( out.size() );
+        }
 		
-		// let's be paranoid
-		assert( out.size() == value.getValue().size() );
-		assert( matrix.getValue().size() % value.getValue().size() == 0 );
-		
-		// each out dof
-//		unsigned off = 0;
 		for(unsigned i = 0, n = out.size(); i < n; ++i) {
-			out[i] = value.getValue()[i];
+			out[i] = value[i];
 		}
 		
 	}
