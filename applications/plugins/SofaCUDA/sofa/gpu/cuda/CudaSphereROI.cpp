0b0d3dde3f1f8de53798bfc53cffbf73ea5afe0c
Frederick Roy <frederick.roy@inria.fr>
2016-05-10 15:19:09 +0200

    Fix CUDA compilation
    Former-commit-id: 9ae977f822b98db82e5397586ec161f2e8f80be8

--- applications/plugins/SofaCUDA/sofa/gpu/cuda/CudaSphereROI.cpp
+++ applications/plugins/SofaCUDA/sofa/gpu/cuda/CudaSphereROI.cpp
@@ -24,7 +24,7 @@
 ******************************************************************************/
 #include "CudaTypes.h"
 #include <sofa/core/ObjectFactory.h>
-#include <SofaEngine/SphereROI.inl>
+#include <SofaGeneralEngine/SphereROI.inl>
 #include <sofa/defaulttype/VecTypes.h>
 #include <sofa/defaulttype/RigidTypes.h>
 
