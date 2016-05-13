942c21d8381a8a568071c3bbedbd4ddd2dcf49e7
htalbot <hugo.talbot@inria.fr>
2016-05-13 10:48:56 +0200

    FIX the compilation of the added TorsionForceField
    The component TorsionForceField was left in
    modules/sofa/components. We moved it in SofaBoundaryCondition.
    Some fix was needed.
    
    
    Former-commit-id: 0f724fa49a8ee93e7d20987ae80f1395404c087e

--- modules/SofaBoundaryCondition/TorsionForceField.h
+++ modules/SofaBoundaryCondition/TorsionForceField.h
@@ -1,5 +1,30 @@
-#ifndef TORSIONFORCEFIELD_H
-#define TORSIONFORCEFIELD_H
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-2016 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef SOFA_COMPONENT_FORCEFIELD_TORSIONFORCEFIELD_H
+#define SOFA_COMPONENT_FORCEFIELD_TORSIONFORCEFIELD_H
+#include "config.h"
 
 #include <sofa/core/behavior/ForceField.h>
 #include <sofa/core/objectmodel/Data.h>
@@ -46,45 +71,6 @@ struct TorsionForceFieldTraits
 	typedef Mat<deriv_total_size, deriv_total_size, Real> MatrixBlock;
 };
 
-//template<typename DataTypes>
-//struct TorsionForceFieldUtility
-//{
-//	typedef typename DataTypes::Real Real;
-//	typedef typename DataTypes::Coord Coord;
-//	typedef typename DataTypes::Deriv Deriv;
-//	enum { dim = Coord::spatial_dimensions };
-
-//	static Vec<dim, Real> getCenter(const Coord& x) { return x; }
-//	static Vec<dim, Real> getVCenter(const Deriv& v) { return v; }
-//};
-
-
-//template<class TCoord, class TDeriv, typename TReal>
-//struct TorsionForceFieldUtility<defaulttype::StdVectorTypes<TCoord, TDeriv, TReal> >
-//{
-//	typedef defaulttype::StdVectorTypes<TCoord, TDeriv, TReal> DataType;
-//	typedef typename DataType::Real Real;
-//	enum { dim = DataType::Coord::spatial_dimensions };
-
-//	static Vec<dim, Real>& getCenter(const typename DataType::Coord& x) { return x; }
-
-//	static Vec<dim, Real>& getVCenter(const typename DataType::Deriv& v) { return v; }
-
-//};
-
-//template<int dim, typename TReal>
-//struct TorsionForceFieldUtility<defaulttype::StdRigidTypes<dim, TReal> >
-//{
-//	typedef defaulttype::StdRigidTypes<dim, TReal> DataType;
-//	typedef typename DataType::Real Real;
-////	enum { dim = DataType::Coord::spatial_dimensions };
-
-//	static Vec<dim, Real>& getCenter(const typename DataType::Coord& x) { return x.getCenter(); }
-////	static const Vec<dim, Real>& getCenter(const DataType::Coord& x) { return x.getCenter(); }
-
-//	static Vec<dim, Real> getVCenter(const typename DataType::Deriv& v) { return v.getVCenter(); }
-////	static const Vec<dim, Real>& getVCenter(const DataType::Deriv& v) { return v.getVCenter(); }
-//};
 
 ///
 ///	\brief TorsionForceField
@@ -125,6 +111,12 @@ public:
 	virtual void addDForce(const MechanicalParams *mparams, DataVecDeriv &df, const DataVecDeriv &dx);
 	virtual void addKToMatrix(defaulttype::BaseMatrix *matrix, double kFact, unsigned int &offset);
 
+    virtual SReal getPotentialEnergy(const core::MechanicalParams* /*mparams*/, const DataVecCoord&  /* x */) const
+    {
+        serr << "Get potentialEnergy not implemented" << sendl;
+        return 0.0;
+    }
+
 public :
 	Data<VecId> m_indices;		///< indices of the selected nodes.
 	Data<Real> m_torque;		///< torque to be applied.
@@ -153,12 +145,12 @@ void TorsionForceField<Rigid3dTypes>::addDForce(const core::MechanicalParams *mp
 
 #if defined(SOFA_EXTERN_TEMPLATE) && !defined(SOFA_COMPONENT_FORCEFIELD_TORSIONFORCEFIELD_CPP)
 #ifndef SOFA_FLOAT
-extern template class TorsionForceField<Vec3dTypes>;
-extern template class TorsionForceField<Rigid3dTypes>;
+extern template class SOFA_BOUNDARY_CONDITION_API TorsionForceField<Vec3dTypes>;
+extern template class SOFA_BOUNDARY_CONDITION_API TorsionForceField<Rigid3dTypes>;
 #endif
 #ifndef SOFA_DOUBLE
-extern template class TorsionForceField<Vec3fTypes>;
-extern template class TorsionForceField<Rigid3fTypes>;
+extern template class SOFA_BOUNDARY_CONDITION_API TorsionForceField<Vec3fTypes>;
+extern template class SOFA_BOUNDARY_CONDITION_API TorsionForceField<Rigid3fTypes>;
 #endif
 #endif
 
@@ -169,4 +161,4 @@ extern template class TorsionForceField<Rigid3fTypes>;
 
 
 
-#endif // TORSIONFORCEFIELD_H
+#endif // SOFA_COMPONENT_FORCEFIELD_TORSIONFORCEFIELD_H
