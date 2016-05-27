1965901278eec2b55549253bed555801ff847fcf
nesme <matthieu.nesme@inria.fr>
2016-05-27 18:37:46 +0200

    [Compliant] adding DotProduct[Multi]Mapping (with tests)
    Former-commit-id: 03cd32cf6dc561d991469e6739e87d5d99421305

---
+++ applications/plugins/Compliant/mapping/DotProductMapping.cpp
@@ -0,0 +1,70 @@
+#include "DotProductMapping.h"
+
+#include <sofa/core/ObjectFactory.h>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace mapping
+{
+
+SOFA_DECL_CLASS(DotProductMapping)
+
+using namespace defaulttype;
+
+// Register in the Factory
+int DotProductMappingClass = core::RegisterObject("Compute Dot Products between dofs")
+#ifndef SOFA_FLOAT
+.add< DotProductMapping< Vec3dTypes, Vec1dTypes > >()
+#endif
+#ifndef SOFA_DOUBLE
+.add< DotProductMapping< Vec3fTypes, Vec1fTypes > >()
+#endif
+;
+
+
+#ifndef SOFA_FLOAT
+template class SOFA_Compliant_API DotProductMapping< Vec3dTypes, Vec1dTypes >;
+#endif
+
+#ifndef SOFA_DOUBLE
+template class SOFA_Compliant_API DotProductMapping< Vec3fTypes, Vec1fTypes >;
+#endif
+
+
+///////////////////////
+
+
+SOFA_DECL_CLASS(DotProductMultiMapping)
+
+using namespace defaulttype;
+
+// Register in the Factory
+int DotProductMultiMappingClass = core::RegisterObject("Compute Dot Products between dofs in different mstates")
+#ifndef SOFA_FLOAT
+.add< DotProductMultiMapping< Vec3dTypes, Vec1dTypes > >()
+#endif
+#ifndef SOFA_DOUBLE
+.add< DotProductMultiMapping< Vec3fTypes, Vec1fTypes > >()
+#endif
+;
+
+
+#ifndef SOFA_FLOAT
+template class SOFA_Compliant_API DotProductMultiMapping< Vec3dTypes, Vec1dTypes >;
+#endif
+
+#ifndef SOFA_DOUBLE
+template class SOFA_Compliant_API DotProductMultiMapping< Vec3fTypes, Vec1fTypes >;
+#endif
+
+
+
+}
+}
+}
+
+
