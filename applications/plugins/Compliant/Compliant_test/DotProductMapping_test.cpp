ef5fef21f49214ac4ebdb53fceaae6def4080045
nesme <matthieu.nesme@inria.fr>
2016-06-08 10:43:51 +0200

    [Compliant] increasing DotProductMappingTest threshold

--- applications/plugins/Compliant/Compliant_test/DotProductMapping_test.cpp
+++ applications/plugins/Compliant/Compliant_test/DotProductMapping_test.cpp
@@ -51,7 +51,7 @@ struct DotProductMappingTest : public Mapping_test<Mapping>
     bool test()
     {
         // we need to increase the error for avoiding numerical problem
-        this->errorMax *= 500;
+        this->errorMax *= 600;
         this->deltaRange.first = this->errorMax*100;
         this->deltaRange.second = this->errorMax*1000;
 
@@ -120,7 +120,7 @@ struct DotProductMultiMappingTest : public MultiMapping_test<Mapping>
     bool test()
     {
         // we need to increase the error for avoiding numerical problem
-        this->errorMax *= 500;
+        this->errorMax *= 600;
         this->deltaRange.first = this->errorMax*100;
         this->deltaRange.second = this->errorMax*1000;
 
