InSimo/ISSofa

WARNING: ADD BaseContext::getActiveMeshTopology() to get relevant topology, taking BaseMapping::sameTopology() into account. CLEAN: MechanicalObject size and link to topology is now exposed as Data and Link, using the new getActiveMeshTopology() method. Set the link manually and/or set useTopology="false" to override.

modules/sofa/simulation/tree/ExportDotVisitor.cpp
@@ -76,6 +76,8 @@ ExportDotVisitor::ExportDotVisitor(const sofa::core::ExecParams* params /* PARAM
       tagNoExportGraph("NoExportGraph")
 {
     *out << "digraph G {" << std::endl;
+    //*out << "graph [concentrate=true]" << std::endl;
+    //*out << "graph [splines=curved]" << std::endl;
 }
 
 ExportDotVisitor::~ExportDotVisitor()
@@ -226,6 +228,7 @@ bool ExportDotVisitor::display(core::objectmodel::BaseObject* obj, const char **
 /// Returns an empty string if not found.
 std::string ExportDotVisitor::getParentName(core::objectmodel::BaseObject* obj)
 {
+    if (!obj) return "";
     core::objectmodel::BaseObject* master = obj->getMaster();
     if (showSlaves && master && display(master))
         return getName(master);
@@ -247,13 +250,13 @@ std::string ExportDotVisitor::getParentName(core::objectmodel::BaseObject* obj)
         dynamic_cast<core::topology::BaseTopologyObject*>(obj))
         return getName(node->topology);
     /// \todo consider all solvers instead of the first one (FF)
-    if (node->mechanicalState!=obj && node->linearSolver[0]!=obj && node->solver[0]!=obj  && node->animationManager!=obj && display(node->mechanicalState))
+    if (!node->mechanicalState.empty() && node->mechanicalState!=obj && node->linearSolver[0]!=obj && node->solver[0]!=obj  && node->animationManager!=obj && display(node->mechanicalState))
         return getName(node->mechanicalState);
-    if (node->linearSolver[0]!=obj && node->solver[0]!=obj && node->animationManager!=obj && display(node->linearSolver[0]))
+    if (!node->linearSolver.empty() && node->linearSolver[0]!=obj && node->solver[0]!=obj && node->animationManager!=obj && display(node->linearSolver[0]))
         return getName(node->linearSolver[0]);
-    if (node->solver[0]!=obj && node->animationManager!=obj && display(node->solver[0]))
+    if (!node->solver.empty() && node->solver[0]!=obj && node->animationManager!=obj && display(node->solver[0]))
         return getName(node->solver[0]);
-    if (node->animationManager!=obj && display(node->solver[0]))
+    if (!node->animationManager.empty() && node->animationManager!=obj && display(node->solver[0]))
         return getName(node->animationManager);
     if ((node->mechanicalState==obj || node->solver[0]==obj) && !node->mechanicalMapping && node->parent() && display(node->parent()->solver[0]))
         return getName(node->parent()->solver[0]);
@@ -265,6 +268,7 @@ std::string ExportDotVisitor::getParentName(core::objectmodel::BaseObject* obj)
 /// Compute the name of a given node or object
 std::string ExportDotVisitor::getName(core::objectmodel::Base* o, std::string prefix)
 {
+    if (!o) return "";
     if (names.count(o)>0)
         return names[o];
     std::string oname = o->getName();
@@ -338,11 +342,27 @@ void ExportDotVisitor::processObject(GNode* node, core::objectmodel::BaseObject*
         {
             *out << pname << " -> " << name;
             if (dynamic_cast<core::BaseMapping*>(obj) || dynamic_cast<core::topology::TopologicalMapping*>(obj))
-                *out << "[constraint=false,weight=5]";
+                *out << "[constraint=false,weight=10]";
             else
                 *out << "[weight=10]";
             *out << ";" << std::endl;
         }
+        /*
+        core::behavior::BaseMechanicalState* bms = dynamic_cast<core::behavior::BaseMechanicalState*>(obj);
+        if (bms!=NULL)
+        {
+            core::objectmodel::BaseLink* l_topology = bms->findLink("topology");
+            if (l_topology)
+            {
+                core::topology::BaseMeshTopology* topo = dynamic_cast<core::topology::BaseMeshTopology*>(l_topology->getLinkedBase());
+                if (topo)
+                {
+                    if (display(topo))
+                        *out << name << " -> " << getName(topo) << " [style=\"dashed\",constraint=false,color=\"#808080\",arrowhead=\"none\"];" << std::endl;
+                }
+            }
+        }
+        */
         core::behavior::BaseInteractionForceField* iff = dynamic_cast<core::behavior::BaseInteractionForceField*>(obj);
         if (iff!=NULL)
         {
@@ -508,7 +528,7 @@ simulation::Visitor::Result ExportDotVisitor::processNodeTopDown(GNode* node)
         *out << "\"];" << std::endl;
         if (node->parent())
         {
-            *out << getName(node->parent()) << " -> " << getName(node)<< " [minlen=2,style=\"bold\",weight=5];" << std::endl;
+            *out << getName(node->parent()) << " -> " << getName(node)<< " [minlen=2,style=\"bold\",weight=10];" << std::endl;
         }
     }
 

sofa-framework/sofa

SofaKernel/modules/SofaSimulationTree/ExportDotVisitor.cpp
@@ -65,6 +65,8 @@ ExportDotVisitor::ExportDotVisitor(const sofa::core::ExecParams* params, std::os
       labelObjectClass(true)
 {
     *out << "digraph G {" << std::endl;
+    //*out << "graph [concentrate=true]" << std::endl;
+    //*out << "graph [splines=curved]" << std::endl;
 }
 
 ExportDotVisitor::~ExportDotVisitor()
@@ -182,6 +184,7 @@ bool ExportDotVisitor::display(core::objectmodel::BaseObject* obj, const char **
 /// Returns an empty string if not found.
 std::string ExportDotVisitor::getParentName(core::objectmodel::BaseObject* obj)
 {
+    if (!obj) return "";
     GNode* node = dynamic_cast<GNode*>(obj->getContext());
     if (!node) return "";
     if (display(node))
@@ -195,11 +198,13 @@ std::string ExportDotVisitor::getParentName(core::objectmodel::BaseObject* obj)
                 obj->toCollisionGroupManager()))
         return getName(node->collisionPipeline);
     /// \todo consider all solvers instead of the first one (FF)
-    if (node->mechanicalState!=obj && node->solver[0]!=obj  && node->animationManager!=obj && display(node->mechanicalState))
+    if (!node->mechanicalState.empty() && node->mechanicalState!=obj && node->linearSolver[0]!=obj && node->solver[0]!=obj  && node->animationManager!=obj && display(node->mechanicalState))
         return getName(node->mechanicalState);
-    if (node->solver[0]!=obj && node->animationManager!=obj && display(node->solver[0]))
+    if (!node->linearSolver.empty() && node->linearSolver[0]!=obj && node->solver[0]!=obj && node->animationManager!=obj && display(node->linearSolver[0]))
+        return getName(node->linearSolver[0]);
+    if (!node->solver.empty() && node->solver[0]!=obj && node->animationManager!=obj && display(node->solver[0]))
         return getName(node->solver[0]);
-    if (node->animationManager!=obj && display(node->solver[0]))
+    if (!node->animationManager.empty() && node->animationManager!=obj && display(node->solver[0]))
         return getName(node->animationManager);
     if ((node->mechanicalState==obj || node->solver[0]==obj) && !node->mechanicalMapping && node->getFirstParent() && display(static_cast<GNode*>(node->getFirstParent())->solver[0]))
         return getName(static_cast<GNode*>(node->getFirstParent())->solver[0]);
@@ -211,6 +216,7 @@ std::string ExportDotVisitor::getParentName(core::objectmodel::BaseObject* obj)
 /// Compute the name of a given node or object
 std::string ExportDotVisitor::getName(core::objectmodel::Base* o, std::string prefix)
 {
+    if (!o) return "";
     if (names.count(o)>0)
         return names[o];
     std::string oname = o->getName();
@@ -279,9 +285,27 @@ void ExportDotVisitor::processObject(GNode* /*node*/, core::objectmodel::BaseObj
         {
             *out << pname << " -> " << name;
             if (obj->toBaseMapping())
-                *out << "[constraint=false]";
+                *out << "[constraint=false,weight=10]";
+            else
+                *out << "[weight=10]";
             *out << ";" << std::endl;
         }
+        /*
+        core::behavior::BaseMechanicalState* bms = dynamic_cast<core::behavior::BaseMechanicalState*>(obj);
+        if (bms!=NULL)
+        {
+            core::objectmodel::BaseLink* l_topology = bms->findLink("topology");
+            if (l_topology)
+            {
+                core::topology::BaseMeshTopology* topo = dynamic_cast<core::topology::BaseMeshTopology*>(l_topology->getLinkedBase());
+                if (topo)
+                {
+                    if (display(topo))
+                        *out << name << " -> " << getName(topo) << " [style=\"dashed\",constraint=false,color=\"#808080\",arrowhead=\"none\"];" << std::endl;
+                }
+            }
+        }
+        */
         core::behavior::BaseInteractionForceField* iff = obj->toBaseInteractionForceField();
         if (iff!=NULL)
         {
@@ -343,7 +367,7 @@ simulation::Visitor::Result ExportDotVisitor::processNodeTopDown(GNode* node)
         *out << "\"];" << std::endl;
         if (node->getFirstParent())
         {
-            *out << getName(node->getFirstParent()) << " -> " << getName(node)<< " [minlen=2,style=\"bold\"];" << std::endl;
+            *out << getName(node->getFirstParent()) << " -> " << getName(node)<< " [minlen=2,style=\"bold\",weight=10];" << std::endl;
         }
     }
 