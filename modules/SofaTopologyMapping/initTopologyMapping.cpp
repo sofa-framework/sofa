aa2f6acad50480a0e8428f91caae6cd7ad4eb938
Hervé Delingette <Herve.Delingette@inria.fr>
2016-08-28 18:00:20 +0200

    REMOVE  : removed toplogy mapping defined for High Order tetrahedron and triangle elements (moved to HighOrderFEM and HighOrderTopology plugin)

--- modules/SofaTopologyMapping/initTopologyMapping.cpp
+++ modules/SofaTopologyMapping/initTopologyMapping.cpp
@@ -39,9 +39,7 @@ void initTopologyMapping()
     }
 }
 
-SOFA_LINK_CLASS(BezierTetra2BezierTriangleTopologicalMapping)
-SOFA_LINK_CLASS(Bezier2MeshTopologicalMapping)
-SOFA_LINK_CLASS(Bezier2MeshMechanicalMapping)
+
 SOFA_LINK_CLASS(Mesh2PointMechanicalMapping)
 SOFA_LINK_CLASS(SimpleTesselatedTetraMechanicalMapping)
 SOFA_LINK_CLASS(CenterPointTopologicalMapping)
