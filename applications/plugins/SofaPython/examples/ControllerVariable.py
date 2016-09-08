00fbc385e86feaaa4f061fc9095f69aa6fec91e0
nesme <nesme@anatoscope.com>
2016-09-08 11:19:30 +0200

    [SofaPython] script.Controller can now handle optional arguments before createGraph is called.

--- applications/plugins/SofaPython/examples/ControllerVariable.py
+++ applications/plugins/SofaPython/examples/ControllerVariable.py
@@ -9,26 +9,37 @@ from SofaPython import script
 
 
 def createScene( root ):
-    
-    myController = MyControllerClass(root,"hello world controller")
+
+    myController = MyControllerClass(root,"hello world controller", myArg = 'additionalArguement')
     myController.helloWorld()
     myController.myText = "hello world!"
-    
-  
-  
+
+
+
 class MyControllerClass(script.Controller):
-                               
+
    ### Overloaded PythonScriptController callbacks
-   
-        def onBeginAnimationStep(self,dt):
-            print self.myText
-            sys.stdout.flush()
-            
-   
+
+    def createGraph(self, node):
+       print "createGraph: myText ==",self.myText
+       sys.stdout.flush()
+
+    def initGraph(self,node):
+        print "initGraph: myText ==", self.myText
+        sys.stdout.flush()
+
+
    ### Local variables / functions
-   
-        myText = "nothing to say"
-                
-        def helloWorld(self):
-            print "helloWorld() function"
-            sys.stdout.flush()
+
+    myText = "nothing to say"
+
+    def helloWorld(self):
+        print "helloWorld() function"
+        sys.stdout.flush()
+
+
+    ### to handle optional constructor arguments before createGraph
+    def additionalArguments(self,kwarg):
+        print "additionalArguments",kwarg
+        self.myText = kwarg['myArg']
+        pass
