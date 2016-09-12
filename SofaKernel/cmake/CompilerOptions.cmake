cc802e295dd125bf529e5cf205b4d08bb4c81aaa
Guillaume Paran <guillaume.paran@inria.fr>
2016-09-12 15:33:13 +0200

    Using correctly VERSION_LESS comparator

--- SofaKernel/cmake/CompilerOptions.cmake
+++ SofaKernel/cmake/CompilerOptions.cmake
@@ -84,7 +84,7 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
 # An important C++11 feature may be not enabled due to
 # the compiler being built without the --enable-libstdcxx-time option.
 if(CMAKE_COMPILER_IS_GNUCXX)
-    if(CMAKE_CXX_COMPILER_VERSION VERSION_LOWER 4.8)
+    if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.8)
         set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_GLIBCXX_USE_NANOSLEEP -D_GLIBCXX_USE_SCHED_YIELD")
     endif()
 endif()
