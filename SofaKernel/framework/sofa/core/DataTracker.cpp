7bbe08adb8f41135b30fad355076ac04a25d3024
nesme <nesme@anatoscope.com>
2016-09-29 17:57:09 +0200

    [dataupdate] let's begin!

--- SofaKernel/framework/sofa/core/DataTracker.cpp
+++ SofaKernel/framework/sofa/core/DataTracker.cpp
@@ -1,4 +1,5 @@
 #include "DataTracker.h"
+#include "objectmodel/BaseData.h"
 
 namespace sofa
 {
@@ -14,6 +15,38 @@ void DataTracker::setData(objectmodel::BaseData *data, bool dirtyAtBeginning )
 
 const std::string DataTracker::emptyString = "";
 
+
+
+//////////////////////
+
+
+void TrackerDDGNode::updateAllInputsIfDirty()
+{
+    const DDGLinkContainer& inputs = DDGNode::getInputs();
+    for(size_t i=0, iend=inputs.size() ; i<iend ; ++i )
+    {
+        static_cast<core::objectmodel::BaseData*>(inputs[i])->updateIfDirty();
+    }
+}
+
+void TrackerDDGNode::cleanDirty(const core::ExecParams* params)
+{
+    core::objectmodel::DDGNode::cleanDirty(params);
+
+    // it is also time to clean the tracked Data
+    for( DataTrackers::iterator it=m_dataTrackers.begin(),itend=m_dataTrackers.end() ; it!=itend ; ++it )
+        it->second.cleanDirty();
+}
+
+void TrackerDDGNode::trackData( objectmodel::BaseData* data )
+{
+    m_dataTrackers[data].setData( data );
+}
+
+bool TrackerDDGNode::isTrackedDataDirty( const objectmodel::BaseData& data )
+{
+    return m_dataTrackers[&data].isDirty();
+}
 
 
 }
--- SofaKernel/framework/sofa/core/DataTracker.h
+++ SofaKernel/framework/sofa/core/DataTracker.h
@@ -24,6 +24,59 @@ namespace core
         virtual objectmodel::BaseData* getData() const { return NULL; }
     };
 
+
+
+
+    /// A DDGNode with trackable input Data
+    class TrackerDDGNode : public core::objectmodel::DDGNode
+    {
+    public:
+
+        TrackerDDGNode() : core::objectmodel::DDGNode() {}
+
+    protected:
+
+        /// utility fonction to ensure all inputs are up-to-date
+        /// can be useful for particulary complex DataEngine
+        /// with a lot input/output imbricated access
+        void updateAllInputsIfDirty();
+
+    private:
+        TrackerDDGNode(const TrackerDDGNode& n) ;
+        TrackerDDGNode& operator=(const TrackerDDGNode& n) ;
+
+    public:
+
+        /// Set dirty flag to false
+        /// for the Engine and for all the tracked Data
+        virtual void cleanDirty(const core::ExecParams* params = 0);
+
+    protected:
+
+
+        /// @name Tracking Data mechanism
+        /// each tracked Data is connected to a DataTracker
+        /// that is dirtied with the tracked Data
+        /// but cleaned only in the DataEngine::cleanDirty()
+        /// @{
+
+        /// select a Data to track to be able to check
+        /// if it was dirtied since the previous update.
+        /// @see isTrackedDataDirty
+        void trackData( objectmodel::BaseData* data );
+
+        /// Was the tracked Data dirtied since last update?
+        /// @warning data must be a tracked Data @see trackData
+        bool isTrackedDataDirty( const objectmodel::BaseData& data );
+
+        /// map a tracked Data to a DataTracker
+        typedef std::map<const objectmodel::BaseData*,DataTracker> DataTrackers;
+        DataTrackers m_dataTrackers;
+
+        /// @}
+
+    };
+
 } // namespace core
 
 } // namespace sofa
