4b9c7838c275734dc45d4fef57ea2b7536dda391
Damien Marchal <damien.marchal@univ-lille1.fr>
2016-11-03 11:30:27 +0100

    [SofaConstraint] FIX the test that were seriously broken.
    Now we are testing vec3, rigid3f and rigid3d.
    This file still need a serious refactoring.

--- modules/SofaConstraint/SofaConstraint_test/BilateralInteractionConstraint_test.cpp
+++ modules/SofaConstraint/SofaConstraint_test/BilateralInteractionConstraint_test.cpp
@@ -71,66 +71,27 @@ struct BilateralInteractionConstraint_test : public Sofa_test<typename _DataType
     typedef container::MechanicalObject<DataTypes> MechanicalObject;
 
     simulation::Node::SPtr root;                 ///< Root of the scene graph, created by the constructor an re-used in the tests
-    simulation::Simulation* simulation;          ///< created by the constructor an re-used in the tests
+    simulation::Simulation* simulation {nullptr}; ///< created by the constructor an re-used in the tests
 
     /// Create the context for the tests.
     void SetUp()
     {
-        sofa::simulation::setSimulation(simulation = new sofa::simulation::graph::DAGSimulation());
-
-        /// Load the scene
-        std::string sceneName = "BilateralInteractionConstraint.scn";
-        std::string fileName  = std::string(SOFATEST_SCENES_DIR) + "/" + sceneName;
-        root = sofa::simulation::getSimulation()->load(fileName.c_str()).get();
-
-        //TODO(dmarchal): I'm very surprised that scene.loadSucceed could contain
-        // a state about the load results that happens "before" maybe a side effect
-        // of the static variable.
-
-        // Test if load has succeeded
-        sofa::simulation::SceneLoaderXML scene;
-
-        if(!root || !scene.loadSucceed)
-        {
-            ADD_FAILURE() << "Error while loading the scene: " << sceneName << std::endl;
-        }
+        if(simulation==nullptr)
+            sofa::simulation::setSimulation(simulation = new sofa::simulation::graph::DAGSimulation());
     }
 
-    void init_Setup()
-    {
-        /// Init
-        sofa::simulation::getSimulation()->init(root.get());
+    void TearDown(){
+        if(root)
+            simulation->unload(root);
     }
 
-    bool test_constrainedPositions()
+    void init_Vec3dSetup()
     {
-        std::vector<MechanicalObject*> meca;
-        root->get<MechanicalObject>(&meca,std::string("mecaConstraint"),root->SearchDown);
+    }
 
-        std::vector<Coord> points;
-        points.resize(2);
-
-        if(meca.size()!=2)
-        {
-            ADD_FAILURE() << "Error while searching mechanical object" << std::endl;
-        }
-
-        for(int i=0; i<10; i++)
-            sofa::simulation::getSimulation()->animate(root.get(),(double)0.001);
-
-        if(meca.size()==2)
-        {
-            for(unsigned int i=0; i<meca.size(); i++)
-                points[i] = meca[i]->read(core::ConstVecCoordId::position())->getValue()[0];
-        }
-
-        if(points[0] == points[1]) return true;
-        else
-        {
-            ADD_FAILURE() << "Error while testing if two positions are correctly constrained" << std::endl;
-        }
-
-        return false;
+    bool test_Vec3dconstrainedPositions()
+    {
+        return true;
     }
 
     /// It is important to freeze what are the available Data field
@@ -138,6 +99,20 @@ struct BilateralInteractionConstraint_test : public Sofa_test<typename _DataType
     /// If you remove/renamed a data field please add a deprecation
     /// message as well as update this test.
     void attributesTests(){
+        /// I'm using '\n' so that the XML parser correctly report the line number
+        /// in case of problems.
+        std::stringstream scene;
+        scene << "<?xml version='1.0'?>                                       \n"
+                 "<Node 	name='Root' gravity='0 0 0' time='0' animate='0'   > \n"
+                 "   <MechanicalObject name='o1' template='"<< DataTypes::Name() << "' position='1 2 3'/>   \n"
+                 "   <MechanicalObject name='o2' template='"<< DataTypes::Name() << "' position='1 2 3'/>   \n"
+                 "   <BilateralInteractionConstraint template='"<< DataTypes::Name() << "' object1='@./o1' object2='@./o2'/>     \n"
+                 "</Node>                                                     \n" ;
+
+        Node::SPtr root = SceneLoaderXML::loadFromMemory (__FILE__,
+                                                          scene.str().c_str(),
+                                                          scene.str().size()) ;
+        root->init(ExecParams::defaultInstance()) ;
 
         BilateralInteractionConstraint* constraint = root->getTreeObject<BilateralInteractionConstraint>() ;
         EXPECT_TRUE( constraint != nullptr ) ;
@@ -163,7 +138,7 @@ struct BilateralInteractionConstraint_test : public Sofa_test<typename _DataType
         std::stringstream scene;
         scene << "<?xml version='1.0'?>                                       \n"
                  "<Node 	name='Root' gravity='0 0 0' time='0' animate='0'   > \n"
-                 "   <BilateralConstraintCorrection template='"<< DataTypes::Name() << "'/>     \n"
+                 "   <BilateralInteractionConstraint template='"<< DataTypes::Name() << "' />     \n"
                  "</Node>                                                     \n" ;
 
         Node::SPtr root = SceneLoaderXML::loadFromMemory (__FILE__,
@@ -186,7 +161,9 @@ void BilateralInteractionConstraint_test<Rigid3fTypes>::checkRigid3fFixForBackwa
     std::stringstream scene;
     scene << "<?xml version='1.0'?>                                       \n"
              "<Node 	name='Root' gravity='0 0 0' time='0' animate='0'   > \n"
-             "   <BilateralConstraintCorrection template=' " << Rigid3fTypes::Name() << " '/>  \n"
+             "   <MechanicalObject name='o1' template='"<< DataTypes::Name() << "' position='1 2 3'/>   \n"
+             "   <MechanicalObject name='o2' template='"<< DataTypes::Name() << "' position='1 2 3'/>   \n"
+             "   <BilateralInteractionConstraint template='"<< DataTypes::Name() << "' object1='@./o1' object2='@./o2'/>     \n"
              "</Node>                                                     \n" ;
 
     Node::SPtr root = SceneLoaderXML::loadFromMemory (__FILE__,
@@ -198,16 +175,84 @@ void BilateralInteractionConstraint_test<Rigid3fTypes>::checkRigid3fFixForBackwa
 }
 
 
+template<>
+void BilateralInteractionConstraint_test<Vec3dTypes>::init_Vec3dSetup()
+{
+    /// Load the scene
+    //TODO(dmarchal): This general load should be updated... there is no reason to load
+    // a scene independently of the data template to use.
+    std::string sceneName = "BilateralInteractionConstraint.scn";
+    std::string fileName  = std::string(SOFATEST_SCENES_DIR) + "/" + sceneName;
+    root = sofa::simulation::getSimulation()->load(fileName.c_str()).get();
+
+    //TODO(dmarchal): I'm very surprised that scene.loadSucceed could contain
+    // a state about the load results that happens "before" maybe a side effect
+    // of the static variable.
+
+    // Test if load has succeeded
+    sofa::simulation::SceneLoaderXML scene;
+
+    if(!root || !scene.loadSucceed)
+    {
+        ADD_FAILURE() << "Error while loading the scene: " << sceneName << std::endl;
+    }
+
+
+    /// Init
+    sofa::simulation::getSimulation()->init(root.get());
+}
+
+template<>
+bool BilateralInteractionConstraint_test<Vec3dTypes>::test_Vec3dconstrainedPositions()
+{
+    std::vector<MechanicalObject*> meca;
+    root->get<MechanicalObject>(&meca,std::string("mecaConstraint"),root->SearchDown);
+
+    std::vector<Coord> points;
+    points.resize(2);
+
+    if(meca.size()!=2)
+    {
+        ADD_FAILURE() << "Error while searching mechanical object" << std::endl;
+    }
+
+    for(int i=0; i<10; i++)
+        sofa::simulation::getSimulation()->animate(root.get(),(double)0.001);
+
+    if(meca.size()==2)
+    {
+        for(unsigned int i=0; i<meca.size(); i++)
+            points[i] = meca[i]->read(core::ConstVecCoordId::position())->getValue()[0];
+    }
+
+    if(points[0] == points[1]) return true;
+    else
+    {
+        ADD_FAILURE() << "Error while testing if two positions are correctly constrained" << std::endl;
+    }
+
+    return false;
+}
+
 // Define the list of DataTypes to instanciate
 using testing::Types;
-typedef Types<Vec3Types> DataTypes; // the types to instanciate.
+typedef Types<Vec3Types
+#ifdef SOFA_WITH_DOUBLE
+              ,Rigid3dTypes
+#endif //
+#ifdef SOFA_WITH_FLOAT
+              ,Rigid3fTypes
+#endif //
+> DataTypes; // the types to instanciate.
 
 // Test suite for all the instanciations
 TYPED_TEST_CASE(BilateralInteractionConstraint_test, DataTypes);
-TYPED_TEST( BilateralInteractionConstraint_test , constrainedPositions )
+
+//TODO(dmarchal): Needs a serious refactor !!!
+TYPED_TEST( BilateralInteractionConstraint_test , checkVec3dconstrainedPositions )
 {
-    this->init_Setup();
-    ASSERT_TRUE(  this->test_constrainedPositions() );
+    this->init_Vec3dSetup();
+    ASSERT_TRUE(  this->test_Vec3dconstrainedPositions() );
 }
 
 
