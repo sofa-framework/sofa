aee0d912432d043a792f3b43160417e724b11704
nesme <nesme@anatoscope.com>
2016-11-16 09:40:12 +0100

    [SofaKernel] logger: back with the DefaultStyleMessageFormatter by default. RichConsoleStyleMessageFormatter is too verbose, and it uses more resources, it cannot be imposed by default. Anyway it is easy to switch to it in your own app.

--- SofaKernel/framework/sofa/helper/logging/MessageDispatcher.cpp
+++ SofaKernel/framework/sofa/helper/logging/MessageDispatcher.cpp
@@ -51,8 +51,8 @@ using std::remove ;
 
 #include <sofa/core/objectmodel/Base.h>
 
-#include <sofa/helper/logging/RichConsoleStyleMessageFormatter.h>
-using sofa::helper::logging::RichConsoleStyleMessageFormatter;
+#include <sofa/helper/logging/DefaultStyleMessageFormatter.h>
+using sofa::helper::logging::DefaultStyleMessageFormatter;
 
 #include <mutex>
 using std::lock_guard ;
@@ -68,7 +68,7 @@ namespace logging
 {
 
 MessageHandler* getDefaultMessageHandler(){
-    static ConsoleMessageHandler s_consoleMessageHandler(new RichConsoleStyleMessageFormatter());
+    static ConsoleMessageHandler s_consoleMessageHandler(&DefaultStyleMessageFormatter::getInstance());
     return &s_consoleMessageHandler ;
 }
 
