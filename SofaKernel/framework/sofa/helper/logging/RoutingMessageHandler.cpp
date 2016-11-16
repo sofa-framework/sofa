f77a446ccfd566229dff896f3bd78e8bb57f9c19
Damien Marchal <damien.marchal@univ-lille1.fr>
2016-11-16 14:20:24 +0100

    [SofaKernel] msg_* API improvement
    Implement the improvement discussed in
    https://github.com/sofa-framework/sofa/issues/56
    
    A new message class have been added for logging (in addition to Dev & Runtime).
    A new RoutingMessageHandler have been implemented.
    
    Eg of use to install a filter that will only send the
    Runtime message of type Warning to a File you can do something like:
    
    RoutingMessageHandler* m = new RoutingMessageHandler();
    FileMessageHandler* toAFile = new FileMessageHandler("the file")
    
    /// Yes this is using c++ lambda syntax
    m->setAFilter( [](Message& m)
            {
                if(m.context() == Message::Runtime && m.type() == Message::Warning)
                    return true ;
                return false ;
            }, toAFile );
    
    
    
    Signed-off-by: Damien Marchal <damien.marchal@univ-lille1.fr>

---
+++ SofaKernel/framework/sofa/helper/logging/RoutingMessageHandler.cpp
@@ -0,0 +1,90 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-20ll6 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* This component is open-source                                               *
+*                                                                             *
+* Contributors:                                                               *
+*       - damien.marchal@univ-lille1.fr                                       *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+/*****************************************************************************
+* User of this library should read the documentation
+* in the messaging.h file.
+******************************************************************************/
+#include <sofa/helper/logging/RoutingMessageHandler.h>
+
+namespace sofa
+{
+namespace helper
+{
+namespace logging
+{
+namespace routingmessagehandler
+{
+
+void RoutingMessageHandler::process(Message& m)
+{
+    for(auto& f : m_filters)
+    {
+        if(f.first(m))
+        {
+            f.second->process(m) ;
+        }
+    }
+}
+
+RoutingMessageHandler::RoutingMessageHandler()
+{
+}
+
+void RoutingMessageHandler::setAFilter(FilterFunction f, MessageHandler* handler)
+{
+    m_filters.push_back(std::make_pair(f, handler));
+}
+
+void RoutingMessageHandler::removeAllFilters()
+{
+    m_filters.clear();
+}
+
+RoutingMessageHandler& MainRoutingMessageHandler::getInstance()
+{
+    static RoutingMessageHandler s_instance;
+    return s_instance;
+}
+
+void MainRoutingMessageHandler::setAFilter(FilterFunction filter,
+                                           MessageHandler* handler)
+{
+    getInstance().setAFilter(filter, handler);
+}
+
+void MainRoutingMessageHandler::removeAllFilters()
+{
+    getInstance().removeAllFilters() ;
+}
+
+
+} // loggingmessagehandler
+} // logging
+} // helper
+} // sofa
+
