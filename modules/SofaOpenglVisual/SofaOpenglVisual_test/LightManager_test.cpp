29b131ea53387459a6f5bf465d6aa7cf18a4a725
olivier-goury <goury.olivier@gmail.com>
2016-11-25 17:09:18 +0100

    [SofaOpenGlVisual] Two tests with one open issue on LightManager

--- modules/SofaOpenglVisual/SofaOpenglVisual_test/LightManager_test.cpp
+++ modules/SofaOpenglVisual/SofaOpenglVisual_test/LightManager_test.cpp
@@ -27,6 +27,7 @@ using sofa::helper::logging::ClangMessageHandler ;
 
 #include <SofaTest/TestMessageHandler.h>
 using sofa::helper::logging::ExpectMessage ;
+using sofa::helper::logging::MessageAsTestFailure;
 using sofa::helper::logging::Message ;
 
 int initMessage(){
@@ -38,10 +39,13 @@ int messageInited = initMessage();
 
 namespace sofa {
 
-struct TestLightManager : public Sofa_test<double> {
+struct TestLightManager : public ::testing::Test {
+    void checkAttributes();
+    void checkColor_Ambient_OK();
+    void checkColor_Ambient_OpenIssue();
 };
 
-void checkAttributes()
+void  TestLightManager::checkAttributes()
 {
     std::stringstream scene ;
     scene << "<?xml version='1.0'?>"
@@ -71,10 +75,70 @@ void checkAttributes()
         EXPECT_NE( lm->findData(attrname), nullptr ) << "Missing attribute with name '" << attrname << "'." ;
 }
 
+void TestLightManager::checkColor_Ambient_OK()
+{
+    MessageAsTestFailure error(Message::Error) ;
+    MessageAsTestFailure warning(Message::Warning) ;
+
+    sofa::simulation::setSimulation(new sofa::simulation::graph::DAGSimulation());
+
+    std::stringstream scene ;
+    scene << "<?xml version='1.0'?>"
+             "<Node 	name='Root' gravity='0 -9.81 0' time='0' animate='0' >               \n"
+             "  <Node name='Level 1'>                                                        \n"
+             "   <MechanicalObject template='Vec3d'/>                                        \n"
+             "   <LightManager name='lightmanager' ambient='1 0 2 3'/>                         \n"
+             "  </Node>                                                                      \n"
+             "</Node>                                                                        \n" ;
+
+    Node::SPtr root = SceneLoaderXML::loadFromMemory ("testscene",
+                                                      scene.str().c_str(),
+                                                      scene.str().size()) ;
+    EXPECT_NE(root.get(), nullptr) ;
+    root->init(ExecParams::defaultInstance()) ;
+
+    BaseObject* lm = root->getTreeNode("Level 1")->getObject("lightmanager") ;
+    EXPECT_NE(lm, nullptr) ;
+}
+
+void TestLightManager::checkColor_Ambient_OpenIssue()
+{
+    MessageAsTestFailure error(Message::Error) ;
+    MessageAsTestFailure warning(Message::Warning) ;
+
+    sofa::simulation::setSimulation(new sofa::simulation::graph::DAGSimulation());
+
+    std::stringstream scene ;
+    scene << "<?xml version='1.0'?>"
+             "<Node 	name='Root' gravity='0 -9.81 0' time='0' animate='0' >               \n"
+             "  <Node name='Level 1'>                                                        \n"
+             "   <MechanicalObject template='Vec3d'/>                                        \n"
+             "   <LightManager name='lightmanager' ambient='red'/>                           \n"
+             "  </Node>                                                                      \n"
+             "</Node>                                                                        \n" ;
+
+    Node::SPtr root = SceneLoaderXML::loadFromMemory ("testscene",
+                                                      scene.str().c_str(),
+                                                      scene.str().size()) ;
+    EXPECT_NE(root.get(), nullptr) ;
+    root->init(ExecParams::defaultInstance()) ;
+
+    BaseObject* lm = root->getTreeNode("Level 1")->getObject("lightmanager") ;
+    EXPECT_NE(lm, nullptr) ;
+}
 
 TEST_F(TestLightManager, checkAttributes)
 {
     checkAttributes();
+}
+
+TEST_F(TestLightManager, checkColor_Ambient_OK)
+{
+    checkColor_Ambient_OK();
+}
+TEST_F(TestLightManager, checkColor_Ambient_OpenIssue)
+{
+    checkColor_Ambient_OpenIssue();
 }
 
 }
