55f66ef55df8212fbb63edd28c69e17739a1976d
nesme <nesme@anatoscope.com>
2017-01-04 09:29:56 +0100

    minor comment

--- SofaKernel/framework/sofa/helper/set.h
+++ SofaKernel/framework/sofa/helper/set.h
@@ -2,7 +2,7 @@
 *       SOFA, Simulation Open-Framework Architecture, development version     *
 *                (c) 2006-2016 INRIA, USTL, UJF, CNRS, MGH                    *
 *                                                                             *
-* _set library is free software; you can redistribute it and/or modify it     *
+* This library is free software; you can redistribute it and/or modify it     *
 * under the terms of the GNU Lesser General Public License as published by    *
 * the Free Software Foundation; either version 2.1 of the License, or (at     *
 * your option) any later version.                                             *
@@ -13,7 +13,7 @@
 * for more details.                                                           *
 *                                                                             *
 * You should have received a copy of the GNU Lesser General Public License    *
-* along with _set library; if not, write to the Free Software Foundation,     *
+* along with this library; if not, write to the Free Software Foundation,     *
 * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
 *******************************************************************************
 *                              SOFA :: Framework                              *
