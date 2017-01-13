a898047eab9edb8c6b80ff3f5525c7be9396f5aa
Benjamin GILLES <benjamin.gilles@anatoscope.com>
2017-01-13 12:20:28 +0100

    [image] fix example
    (cherry picked from commit 680c7e76dc7beb2eb99b1735ab734069f4e0e0b3)

--- applications/plugins/image/examples/loadHDR.py
+++ applications/plugins/image/examples/loadHDR.py
@@ -1,5 +1,5 @@
 def createScene(node):
-    node.createObject('RequiredPlugin', name="image")
+    node.createObject('RequiredPlugin', pluginName="image", name="imagePlugin")
     node.createObject('BackgroundSetting',color='1 1 1')
     node.createObject('ImageContainer', template="ImageUS", name="image", filename="data/knee.hdr")
     node.createObject('ImageViewer', template="ImageUS", name="viewer", src="@image", plane="70 70 70")
