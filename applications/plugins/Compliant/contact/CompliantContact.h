bdcedbdb6aef867bf712f2955d797053b2a5d48c
Maxime Tournier <maxime.tournier@anatoscope.com>
2017-01-25 13:26:04 +0100

    [Compliant] fixed reinit in CompliantContact

--- applications/plugins/Compliant/contact/CompliantContact.h
+++ applications/plugins/Compliant/contact/CompliantContact.h
@@ -252,8 +252,11 @@ protected:
         {
             compliance->compliance.setValue( this->compliance_value.getValue() );
             compliance->damping.setValue( this->damping_ratio.getValue() );
-            compliance->reinit();
         }
+
+        // don't forget reinit as dofs size may have changed !
+        compliance->reinit();
+
 
 //        // every contact points must propagate constraint forces
 //        for(unsigned i = 0; i < size; ++i)
