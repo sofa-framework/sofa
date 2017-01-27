a82e37c46cded1dbe7d8b7413c706daadd1536e8
Maxime Tournier <maxime.tournier@anatoscope.com>
2017-01-27 10:27:54 +0100

    [Compliant] bumped python math types

--- applications/plugins/Compliant/python/Compliant/types.py
+++ applications/plugins/Compliant/python/Compliant/types.py
@@ -2,14 +2,61 @@ import numpy as np
 import math
 import sys
 
+
+from numpy.linalg import norm
+
 def vec(*coords):
     return np.array(coords)
 
+deg = 180.0 / math.pi
+
+ex = vec(1, 0, 0)
+ey = vec(0, 1, 0)
+ez = vec(0, 0, 1)
+
+
 class Rigid3(np.ndarray):
 
+    dim = 6
+    __slots__ = ()
+
+    
+    class Deriv(np.ndarray):
+        '''lie algebra element as (translation, rotation)'''
+        __slots__ = ()
+
+        def __new__(cls, *args, **kwargs):
+            return np.ndarray.__new__(cls, 6)
+
+        @property
+        def linear(self):
+            return self[:3].view( np.ndarray )
+
+        @linear.setter
+        def linear(self, value):
+            self[:3] = value
+
+        @property
+        def angular(self):
+            return self[3:].view( np.ndarray )
+
+        @angular.setter
+        def angular(self, value):
+            self[3:] = value
+
+        def exp(self):
+            '''SE(3) exponential'''
+            res = Rigid3()
+
+            res.orient = Quaternion.exp( x.angular )
+            res.center = res.orient( Quaternion.dexp( x.angular ).dot( x.linear ) )
+
+            return res
+
+            
     @property
     def center(self):
-        return self[:3]
+        return self[:3].view( np.ndarray )
 
     @center.setter
     def center(self, value):
@@ -26,9 +73,11 @@ class Rigid3(np.ndarray):
     def __new__(cls, *args):
         return np.ndarray.__new__(cls, 7)
         
-    def __init__(self):
+    def __init__(self, **kwargs):
         self[-1] = 1
         self[:6] = 0
+
+        for k, v in kwargs.iteritems(): setattr(self, k, v)
 
     def inv(self):
         res = Rigid3()
@@ -74,8 +123,25 @@ class Rigid3(np.ndarray):
         res[3, 3] = 1
 
         return res
+
+    
+    def log(self):
+
+        res = Rigid3.Deriv()
+
+        res.angular = self.orient.log()
+        res.linear = self.orient.dlog().dot( self.orient.conj()( self.center ) )
+
+        return res
+
+
         
 class Quaternion(np.ndarray):
+
+    __slots__ = ()
+    
+    dim = 3
+    epsilon = sys.float_info.epsilon
     
     def __new__(cls, *args):
         return np.ndarray.__new__(cls, 4)
@@ -104,14 +170,14 @@ class Quaternion(np.ndarray):
     def real(self, value): self[-1] = value
 
     @property
-    def imag(self): return self[:3]
+    def imag(self): return self[:3].view( np.ndarray )
 
     @imag.setter
     def imag(self, value): self[:3] = value
 
     def normalize(self):
         '''normalize quaternion'''
-        self /= math.sqrt( self.dot(self) )
+        self /= norm(self)
 
     def flip(self):
         '''flip quaternion in the real positive halfplane, if needed'''
@@ -120,7 +186,8 @@ class Quaternion(np.ndarray):
     def __mul__(self, other):
         '''quaternion product'''
         res = Quaternion()
-        
+
+        # TODO there might be a more efficient way                
         res.real = self.real * other.real - self.imag.dot(other.imag)
         res.imag = self.real * other.imag + other.real * self.imag + np.cross(self.imag, other.imag)
         
@@ -134,24 +201,25 @@ class Quaternion(np.ndarray):
         tmp.real = 0
         tmp.imag = x
 
+        # TODO there might be a more efficient way        
         return (self * tmp * self.conj()).imag
 
 
-    # TODO this is horribly inefficient, optimize
     def matrix(self):
         '''rotation matrix'''
 
         R = np.identity(3)
-
+        
+        # TODO there *is certainly* a more efficient way
         for i in range(3):
             R[:, i] = self( np.eye(1, 3, i) )
 
         return R
         
-    
+
     @staticmethod
     def exp(x):
-        '''quaternion exponential (halved)'''
+        '''quaternion exponential, halved to match SO(3)'''
 
         x = np.array( x )
         theta = np.linalg.norm(x)
@@ -159,6 +227,7 @@ class Quaternion(np.ndarray):
         res = Quaternion()
         
         if math.fabs(theta) < sys.float_info.epsilon:
+            # fallback to gnomonic projection: (1 + x) / || 1 + x ||
             res.imag = x / 2.0
             res.normalize()
             return res
@@ -173,41 +242,101 @@ class Quaternion(np.ndarray):
 
         return res
 
+    @staticmethod
+    def dexp(x):
+        '''exponential derivative SO(3) in body-fixed coordinates'''
+
+        theta = norm(x)
+
+        if theta < Quaternion.epsilon:
+            return np.identity(3)
+        
+        n = x / theta
+        
+        P = np.outer(n, n)
+        H = Quaternion.hat(n)
+
+        # we want SO(3) exponential
+        theta = theta / 2.0
+        
+        s = math.sin(theta)
+        c = math.cos(theta)
+
+        I = np.identity(3)
+
+        return P + (s / theta) * (c * I - s * H).dot(I - P)
+
+
+    def dlog(self):
+        '''logarithm derivative SO(3) in body-fixed coordinates'''
+        
+        n, theta = self.axis_angle()
+        
+        if n is None: return np.identity(3)
+
+        theta /= 2
+        res = np.zeros( (3, 3) )
+
+        P = np.outer(n, n)
+
+        log = n * theta
+        
+        return (P + (theta / math.tan(theta)) * ( np.identity(3) - P ) + Quaternion.hat(log) )
+
+    
+
     def log(self):
-        '''quaternion logarithm (doubled)'''
+        '''quaternion logarithm, doubled to match SO(3)'''
+
+        axis, angle = self.axis_angle()
+
+        if axis is None: return np.zeros(3)
+        return angle * axis
+    
+
+    def axis_angle(self):
+        '''rotation axis/angle'''
+
+        q = self if self.real >= 0 else -self
         
-        half_theta = math.acos(self.real)
-        
-        if math.fabs(half_theta) < sys.float_info.epsilon:
-            return 2.0 * self.imag / self.real
-        
-        half_res = (half_theta / math.sin(half_theta)) * np.array(self.imag)
-        return 2.0 * half_res
+        half_angle = math.acos( min(q.real, 1.0) )
+        axis = q.imag / math.sin( half_angle ) if half_angle > Quaternion.epsilon else None
+
+        return axis, 2 * half_angle
     
 
     def angle(self):
         '''rotation angle'''
-        return 2 * math.acos(self.real)
+        return self.axis_angle()[1]
 
     def axis(self):
         '''rotation axis'''
-        return self.imag / math.sin(  math.acos(self.real) )
+        return self.axis_angle()[0]        
 
-    
     @staticmethod
     def from_vectors(x, y):
         '''rotation sending x to y'''
         
         res = Quaternion()
 
-        res.real = y.dot(x)
+        dot = x.dot(y)
+        res.real = dot
         res.imag = np.cross(x, y)
 
-        theta = np.linalg.norm(res)
+        theta = norm(res)
         res.real += theta
 
-        res.normalize()
+        theta = norm(res)
+        if theta < Quaternion.epsilon:
 
+            # x == y
+            if dot >= 0: return Quaternion()
+            
+            # x == -y
+            # TODO make up vector configurable
+            return Quaternion.exp( math.pi * ey )
+            
+        res /= theta
         return res
 
     
