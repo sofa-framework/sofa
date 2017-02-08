2caa30436e80bb8312a18640049a9e7bc879e563
Frederick Roy <frederick.roy@inria.fr>
2017-02-08 12:07:01 +0100

    [Plugins] Add CImgPlugin: using CImg, add new loader for images
    If loaded in a scene ("RequiredPlugin"), the user will be able
    to load png,jpeg,bmp and tiff image files.
    This plugin is supposed to replace the ImageQt class, as
    it is imposing us to have Qt to load jpeg image (so jpeg was only
    loadable if a Qt GUI was activated). ImagePNG and ImageBMP could
    be replaced too (to be discussed)
    CImg needs libpng (PNG), libjpeg (JPEG), libtiff (TIFF).
    Those formats will be disabled if those libs are not found at
    the CMake step.
    TODO: save image according to extension, add imagemagick lib for more
    exotic formats.

---
+++ applications/plugins/CImgPlugin/ImageCImg.h
@@ -0,0 +1,46 @@
+#ifndef SOFA_HELPER_IO_IMAGECIMG_H
+#define SOFA_HELPER_IO_IMAGECIMG_H
+
+#include <CImgPlugin.h>
+
+#include <sofa/helper/io/Image.h>
+#include <string>
+#include <cassert>
+#include <vector>
+
+#include <sofa/helper/system/config.h>
+
+namespace sofa
+{
+
+namespace helper
+{
+
+namespace io
+{
+
+class SOFA_CIMGPLUGIN_API ImageCImg : public Image
+{
+public:
+    ImageCImg ()
+    {
+    }
+
+    ImageCImg (const std::string &filename)
+    {
+        load(filename);
+    }
+
+    static void setCimgCreators();
+
+    bool load(std::string filename);
+    bool save(std::string filename, int compression_level = -1);
+};
+
+} // namespace io
+
+} // namespace helper
+
+} // namespace sofa
+
+#endif // SOFA_HELPER_IO_IMAGECIMG_H
