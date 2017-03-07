2ee1179e1cf7e6c06cba92cf45ddb70070f47a57
nesme <nesme@anatoscope.com>
2017-03-07 10:29:45 +0100

    [eigen3.3] fixing compil

--- modules/SofaBoundaryCondition/ProjectDirectionConstraint.h
+++ modules/SofaBoundaryCondition/ProjectDirectionConstraint.h
@@ -67,6 +67,7 @@ public:
     typedef typename DataTypes::Coord Coord;
     typedef typename DataTypes::Deriv Deriv;
     typedef typename DataTypes::CPos CPos;
+    typedef typename DataTypes::Real Real;
     typedef typename MatrixDeriv::RowIterator MatrixDerivRowIterator;
     typedef typename MatrixDeriv::RowType MatrixDerivRowType;
     typedef Data<VecCoord> DataVecCoord;
@@ -75,7 +76,7 @@ public:
     typedef helper::vector<unsigned int> Indices;
     typedef sofa::defaulttype::Vector3 Vector3;
     typedef sofa::component::topology::PointSubsetData< Indices > IndexSubsetData;
-    typedef linearsolver::EigenBaseSparseMatrix<SReal> BaseSparseMatrix;
+    typedef linearsolver::EigenBaseSparseMatrix<Real> BaseSparseMatrix;
     typedef linearsolver::EigenSparseMatrix<DataTypes,DataTypes> SparseMatrix;
     typedef typename SparseMatrix::Block Block;                                       ///< projection matrix of a particle displacement to the plane
     enum {bsize=SparseMatrix::Nin};                                                   ///< size of a block
