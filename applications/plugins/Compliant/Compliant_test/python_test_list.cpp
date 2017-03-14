34e977bb68640ed3981691170a764c337ec12362
Maxime Tournier <maxime.tournier@anatoscope.com>
2017-03-14 16:57:23 +0100

    [Compliant] use SofaTest test list from dir

--- applications/plugins/Compliant/Compliant_test/python_test_list.cpp
+++ applications/plugins/Compliant/Compliant_test/python_test_list.cpp
@@ -4,49 +4,12 @@
 namespace sofa {
 
 
-static bool ends_with(const std::string& suffix, const std::string& full){
-    const std::size_t lf = full.length();
-    const std::size_t ls = suffix.length();
-    
-    if(lf < ls) return false;
-    
-    return (0 == full.compare(lf - ls, ls, suffix));
-}
-
-static bool starts_with(const std::string& prefix, const std::string& full){
-    const std::size_t lf = full.length();
-    const std::size_t lp = prefix.length();
-    
-    if(lf < lp) return false;
-    
-    return (0 == full.compare(0, lp, prefix));
-}
-
-
-static bool is_scene_file(const std::string& filename) {
-    return starts_with("scene_", filename) && ends_with(".py", filename);
-}
-
-static bool is_test_file(const std::string& filename) {
-    return starts_with("test_", filename) && ends_with(".py", filename);
-}
 
 
 // these are sofa scenes
-static struct Tests : public Python_test_list
-{
-    Tests()
-    {
-        static const std::string scenePath = std::string(COMPLIANT_TEST_PYTHON_DIR);
-
-        std::vector<std::string> files;
-        helper::system::FileSystem::listDirectory(scenePath, files);
-
-        for(const std::string& file : files) {
-            if(is_scene_file(file) ) {
-                addTest(file, scenePath);
-            }
-        }
+static struct Tests : public Python_test_list {
+    Tests() {
+        addTestDir(COMPLIANT_TEST_PYTHON_DIR, "scene_");
         
     }
 } tests;
@@ -69,21 +32,9 @@ TEST_P(Python_scene_test, sofa_python_scene_tests)
 
 
 // these are just python files loaded in the sofa python environment (paths...)
-static struct Tests2 : public Python_test_list
-{
-    Tests2()
-    {
-        static const std::string testPath = std::string(COMPLIANT_TEST_PYTHON_DIR);
-
-        std::vector<std::string> files;
-        helper::system::FileSystem::listDirectory(testPath, files);
-
-        for(const std::string& file : files) {
-            if(is_test_file(file) ) {
-                addTest(file, testPath);
-            }
-        }
-        
+static struct Tests2 : public Python_test_list {
+    Tests2() {
+        addTestDir(COMPLIANT_TEST_PYTHON_DIR, "test_");
     }
 } tests2;
 
