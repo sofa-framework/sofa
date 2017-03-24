7d51434456938ac6c3e0fdfeaebc123fbdec5798
nesme <nesme@anatoscope.com>
2017-03-24 13:44:09 +0100

    [SofaPython] removing an unnecessary dynamic_cast
    (cherry picked from commit 3bf248ccad6c1b9ae1d8d3ab3ba24f72f56add2a)

--- applications/plugins/SofaPython/ScriptController.cpp
+++ applications/plugins/SofaPython/ScriptController.cpp
@@ -169,10 +169,11 @@ void ScriptController::handleEvent(core::objectmodel::Event *event)
     {
         script_onScriptEvent(static_cast<core::objectmodel::ScriptEvent *> (event));
     }
-    else if (dynamic_cast<IdleEvent *>(event))
+    else if (sofa::core::objectmodel::IdleEvent::checkEventType(event))
     {
         script_onIdleEvent(static_cast<IdleEvent *> (event));
-    }else
+    }
+    else
         Controller::handleEvent(event);
 }
 
