2d41867f67ef382dbf11b2e65eda8e39d07bcb9f
Frederick Roy <frederick.roy@inria.fr>
2017-04-07 15:02:44 +0200

    [SofaKernel] Add tests on PluginManager (with an plugin test implementation)

---
+++ SofaKernel/framework/framework_test/plugins/TestPlugin/ComponentA.h
@@ -0,0 +1,50 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-2017 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef TESTPLUGIN_COMPONENT_A_H
+#define TESTPLUGIN_COMPONENT_A_H
+
+#include <TestPlugin/TestPlugin.h>
+#include <sofa/core/objectmodel/BaseObject.h>
+
+namespace sofa
+{
+
+namespace test
+{
+
+class SOFA_TESTPLUGIN_API ComponentA : public sofa::core::objectmodel::BaseObject
+{
+
+public:
+    SOFA_CLASS(ComponentA, sofa::core::objectmodel::BaseObject);
+
+protected:
+    ComponentA();
+    ~ComponentA();
+};
+
+} // namespace test
+
+} // namespace sofa
+
+
+#endif // TESTPLUGIN_COMPONENT_A_H
