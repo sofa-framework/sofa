2c3d870d6a2c6b5ec18e77fcff957f6601d0675b
nesme <nesme@anatoscope.com>
2017-04-10 10:19:17 +0200

    [SofaPython] adding __str__ implementation to Data

--- applications/plugins/SofaPython/Binding_Data.cpp
+++ applications/plugins/SofaPython/Binding_Data.cpp
@@ -736,6 +736,17 @@ extern "C" PyObject * Data_isDirty(PyObject * self, PyObject * /*args*/)
     return PyBool_FromLong( data->isDirty() );
 }
 
+
+// implementation of __str__ to cast a Data to a string
+PyObject * Data_str(PyObject *self)
+{
+    BaseData* data=((PyPtr<BaseData>*)self)->object;
+    return PyString_FromString(data->getValueString().c_str());
+}
+
+
+
+
 SP_CLASS_METHODS_BEGIN(Data)
 SP_CLASS_METHOD(Data,getValueTypeString)
 SP_CLASS_METHOD(Data,getValueString)
@@ -760,4 +771,54 @@ SP_CLASS_ATTR(Data,name)
 SP_CLASS_ATTR(Data,value)
 SP_CLASS_ATTRS_END
 
-SP_CLASS_TYPE_BASE_PTR_ATTR(Data,BaseData)
+
+// similar to SP_CLASS_TYPE_BASE_PTR_ATTR(Data,BaseData) but also declaring the str() cast function
+PyTypeObject SP_SOFAPYTYPEOBJECT(Data) = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "Sofa.Data",                        //tp_name
+    sizeof(PyPtr<BaseData>),                //tp_basicsize
+    0,                                  //tp_itemsize
+    0,                                  //tp_dealloc
+    0,                                  //tp_print
+    0,                                  //tp_getattr
+    0,                                  //tp_setattr
+    0,                                  //tp_compare
+    0,                                  //tp_repr
+    0,                                  //tp_as_number
+    0,                                  //tp_as_sequence
+    0,                                  //tp_as_mapping
+    0,                                  //tp_hash
+    0,                                  //tp_call
+    &Data_str,                          //tp_str
+    0,                                  //tp_getattro
+    0,                                  //tp_setattro
+    0,                                  //tp_as_buffer
+    Py_TPFLAGS_DEFAULT|Py_TPFLAGS_BASETYPE,   //tp_flags
+    0,                                  //tp_doc
+    0,                                  //tp_traverse
+    0,                                  //tp_clear
+    0,                                  //tp_richcompare
+    0,                                  //tp_weaklistoffset
+    0,                                  //tp_iter
+    0,                                  //tp_iternext
+    SP_SOFAPYMETHODS(Data),              //tp_methods
+    0,                                  //tp_members
+    SP_SOFAPYATTRIBUTES(Data),         //tp_getset
+    &PyBaseObject_Type,                //tp_base
+    0,                                  //tp_dict
+    0,                                  //tp_descr_get
+    0,                                  //tp_descr_set
+    0,                                  //tp_dictoffset
+    0,                                  //tp_init
+    0,                                  //tp_alloc
+    0,                     //tp_new
+    0,                     //tp_free
+    0,    // tp_is_gc /* For PyObject_IS_GC */
+    0,   //tp_bases
+    0,   //tp_mro /* method resolution order */
+    0,   //tp_cache
+    0,   //tp_subclasses
+    0,   //tp_weaklist
+    0, // tp_del
+    0 // tp_verstion_tag
+};
