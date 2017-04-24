6b239b30da3f363714facb70a4cb6ab301e32008
nesme <nesme@anatoscope.com>
2017-04-24 17:10:00 +0200

    [SofaPython] fixing warning
    (cherry picked from commit 050dcc86c3958d3b498c2bf201726d976d7c2f8a)

--- applications/plugins/SofaPython/Binding_SofaModule.cpp
+++ applications/plugins/SofaPython/Binding_SofaModule.cpp
@@ -625,7 +625,7 @@ extern "C" PyObject * Sofa_loadPlugin(PyObject * /*self*/, PyObject * args)
 }
 
 
-static PyObject * Sofa_path(PyObject * /*self*/, PyObject * args) {
+static PyObject * Sofa_path(PyObject * /*self*/, PyObject * /*args*/) {
     return PyString_FromString(sofa::helper::Utils::getSofaPathPrefix().c_str());
 }
 
