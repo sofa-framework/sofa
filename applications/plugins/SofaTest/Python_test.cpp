31078fa009e58f45ad5e6ed610e8aa0dbdac9797
nesme <nesme@anatoscope.com>
2017-05-12 14:33:45 +0200

    fixing warnings
    (cherry picked from commit f6f062e3e452e88b0d205483b89e6a62554d2391)
    
    # Conflicts:
    #	applications/plugins/SofaTest/Python_test.cpp

--- applications/plugins/SofaTest/Python_test.cpp
+++ applications/plugins/SofaTest/Python_test.cpp
@@ -10,6 +10,9 @@
 #include <sofa/helper/logging/Messaging.h>
 #include <sofa/helper/system/FileSystem.h>
 
+#include <SofaPython/PythonMacros.h>
+#include <SofaPython/PythonFactory.h>
+
 namespace sofa {
 
 
@@ -102,7 +105,92 @@ struct Listener : core::objectmodel::BaseObject {
 };
 
 
-static simulation::Node::SPtr root;
+
+
+
+struct fail {
+    const char* message;
+    fail(const char* message)
+        : message(message) { }
+};
+
+static PyObject* operator||(PyObject* obj, const fail& error) {
+    if(obj) return obj;
+    throw std::runtime_error(error.message);
+    return nullptr;
+}
+
+static void operator||(int code, const fail& error) {
+    if(code >= 0) return;
+    throw std::runtime_error(error.message);
+}
+
+
+enum flag : char {
+    stop = 1 << 1,
+    test_failure = 1 << 2,
+    python_error = 1 << 3
+};
+
+
+
+
+// TODO FIXME: there's probably a MEMLEAK hiding in there, figure it out
+static PyObject* except_hook(PyObject* self, PyObject* args) {
+    // raise the stop flag 
+    char* flags = reinterpret_cast<char*>(PyCapsule_GetPointer(self, NULL));
+    assert(flags && "cannot get flags pointer (wtf?)");
+    if(!flags) std::exit(1);
+    
+    *flags |= flag::stop;
+    
+    // switch on exception type
+    PyObject* type;
+    PyObject* value;
+    PyObject* traceback;
+
+    if( !PyArg_ParseTuple(args, "OOO", &type, &value, &traceback) ) {
+        assert(false && "cannot parse excepthook args (wtf?)");
+        std::exit(1);        
+    }
+
+    if( type == PyExc_AssertionError ) {
+        // test failure 
+        *flags |= flag::test_failure;
+    } else {
+        // other python error
+        *flags |= flag::python_error;
+    }
+
+    // TODO should we decref after use?
+    PyObject* default_excepthook = PySys_GetObject((char*)"__excepthook__") || fail("cannot get default excepthook");
+    
+    // call default excepthook to get traceback etc
+    return PyObject_CallObject(default_excepthook, args);
+}
+
+static PyMethodDef except_hook_def = {
+    "sofa_excepthook",
+    except_hook,
+    METH_VARARGS,
+    NULL
+};
+
+
+static void install_sys_excepthook(char* flags) {
+    PyObject* self = PyCapsule_New(flags, NULL, NULL) || fail("cant wrap flags pointer");
+    
+    PyObject* excepthook = PyCFunction_NewEx(&except_hook_def, self, NULL)
+        || fail("cannot create excepthook closure");
+
+    PySys_SetObject((char*)"excepthook", excepthook) || fail("cannot set sys.excepthook");
+}
+
+
+Python_scene_test::Python_scene_test()
+    : max_steps(1000) {
+
+}
 
 void Python_scene_test::run( const Python_test_data& data ) {
 
@@ -115,22 +203,52 @@ void Python_scene_test::run( const Python_test_data& data ) {
         ASSERT_TRUE(scriptFound);
     }
 
-    simulation::setSimulation( new sofa::simulation::graph::DAGSimulation() );
+    if( !simulation::getSimulation() ) {
+        simulation::setSimulation( new sofa::simulation::graph::DAGSimulation() );
+    }
 
-    loader.loadSceneWithArguments(data.filepath.c_str(),
-                                  data.arguments,
-                                  &root);
-    
-	root->addObject( new Listener );
+    char flags = 0;
+    try {
+        install_sys_excepthook(&flags);
+    } catch( std::runtime_error& e) {
+        ASSERT_TRUE(false) << "error setting up python excepthook, aborting test";
+    }
 
-	simulation::getSimulation()->init(root.get());
+    simulation::Node::SPtr root;
+        
+    try {
+        
+        loader.loadSceneWithArguments(data.filepath.c_str(),
+                                      data.arguments,
+                                      &root);
+        ASSERT_TRUE(bool(root)) << "scene creation failed!";
 
-	try {
-		while(root->isActive()) {
-			simulation::getSimulation()->animate(root.get(), root->getDt());
-		}
-	} catch( const result& test_result ) {
-		ASSERT_TRUE(test_result.value);
+        root->addObject( new Listener );
+        simulation::getSimulation()->init(root.get());
+        
+            
+        // TODO eventually tests should only stop by throwing SystemExit
+        unsigned i;
+        for(i = 0; (i < max_steps) && !(flags & flag::stop) && root->isActive(); ++i) {
+            simulation::getSimulation()->animate(root.get(), root->getDt());
+        }
+
+        ASSERT_TRUE(i != max_steps) << "maximum allowed steps reached: " << max_steps;
+
+        if( flags & flag::test_failure ) {
+            FAIL() << "test failure";
+        }
+
+        if( flags & flag::python_error) {
+            FAIL() << "python error";
+        }
+
+	} catch( simulation::PythonEnvironment::system_exit& e) {
+        SUCCEED() << "test terminated normally";
+    } catch( const result& test_result ) {
+        ASSERT_TRUE(test_result.value);
+
+        // TODO raii for unloading
         simulation::getSimulation()->unload( root.get() );
 	}
 }
@@ -139,28 +257,3 @@ void Python_scene_test::run( const Python_test_data& data ) {
 
 } // namespace sofa
 
-
-
-extern "C" {
-
-    void finish() {
-        if(sofa::root) {
-            sofa::root->setActive(false);
-        }
-    }
-
-    void expect_true(bool test, const char* msg) {
-	EXPECT_TRUE(test) << msg;
-    }
-    
-    void assert_true(bool test, const char* msg) {
-        auto trigger = [&] {
-            ASSERT_TRUE(test) << msg;
-        };
-
-        trigger();
-        if(!test) finish();
-    }
-    
-
-}
