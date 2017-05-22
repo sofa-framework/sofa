99aa6d269b474cbfba0d30f5d2b447d805ff54b4
Damien Marchal <damien.marchal@univ-lille1.fr>
2017-05-22 20:58:28 +0200

    [sofa/helper/vector] Restore the correct location of function to make the PR cleaner.

--- SofaKernel/framework/sofa/helper/vector.h
+++ SofaKernel/framework/sofa/helper/vector.h
@@ -196,68 +196,6 @@ public:
 
 };
 
-
-
-
-// ======================  operations on standard vectors
-
-// -----------------------------------------------------------
-//
-/*! @name vector class-related methods
-
-*/
-//
-// -----------------------------------------------------------
-//@{
-/** Remove the first occurence of a given value.
-
-The remaining values are shifted.
-*/
-template<class T1, class T2>
-void remove( T1& v, const T2& elem )
-{
-    typename T1::iterator e = std::find( v.begin(), v.end(), elem );
-    if( e != v.end() )
-    {
-        typename T1::iterator next = e;
-        next++;
-        for( ; next != v.end(); ++e, ++next )
-            *e = *next;
-    }
-    v.pop_back();
-}
-
-/** Remove the first occurence of a given value.
-
-The last value is moved to where the value was found, and the other values are not shifted.
-*/
-template<class T1, class T2>
-void removeValue( T1& v, const T2& elem )
-{
-    typename T1::iterator e = std::find( v.begin(), v.end(), elem );
-    if( e != v.end() )
-    {
-        if (e != v.end()-1)
-            *e = v.back();
-        v.pop_back();
-    }
-}
-
-/// Remove value at given index, replace it by the value at the last index, other values are not changed
-template<class T, class TT>
-void removeIndex( std::vector<T,TT>& v, size_t index )
-{
-#if defined(SOFA_VECTOR_ACCESS_FAILURE)
-    //assert( 0<= static_cast<int>(index) && index <v.size() );
-    if (index>=v.size())
-        vector_access_failure(&v, v.size(), index, typeid(T));
-#endif
-    if (index != v.size()-1)
-        v[index] = v.back();
-    v.pop_back();
-}
-
-
 /// Input stream
 /// Specialization for reading vectors of int and unsigned int using "A-B" notation for all integers between A and B, optionnally specifying a step using "A-B-step" notation.
 template<> inline
@@ -438,8 +376,64 @@ std::istream& vector<unsigned char>::read(std::istream& in)
 }
 
 
+// ======================  operations on standard vectors
 
-//@}
+// -----------------------------------------------------------
+//
+/*! @name vector class-related methods
+
+*/
+//
+// -----------------------------------------------------------
+//@{
+/** Remove the first occurence of a given value.
+
+The remaining values are shifted.
+*/
+template<class T1, class T2>
+void remove( T1& v, const T2& elem )
+{
+    typename T1::iterator e = std::find( v.begin(), v.end(), elem );
+    if( e != v.end() )
+    {
+        typename T1::iterator next = e;
+        next++;
+        for( ; next != v.end(); ++e, ++next )
+            *e = *next;
+    }
+    v.pop_back();
+}
+
+/** Remove the first occurence of a given value.
+
+The last value is moved to where the value was found, and the other values are not shifted.
+*/
+template<class T1, class T2>
+void removeValue( T1& v, const T2& elem )
+{
+    typename T1::iterator e = std::find( v.begin(), v.end(), elem );
+    if( e != v.end() )
+    {
+        if (e != v.end()-1)
+            *e = v.back();
+        v.pop_back();
+    }
+}
+
+/// Remove value at given index, replace it by the value at the last index, other values are not changed
+template<class T, class TT>
+void removeIndex( std::vector<T,TT>& v, size_t index )
+{
+#if defined(SOFA_VECTOR_ACCESS_FAILURE)
+    //assert( 0<= static_cast<int>(index) && index <v.size() );
+    if (index>=v.size())
+        vector_access_failure(&v, v.size(), index, typeid(T));
+#endif
+    if (index != v.size()-1)
+        v[index] = v.back();
+    v.pop_back();
+}
+
 
 } // namespace helper
 } // namespace sofa
