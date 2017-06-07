f81948a9fcea2fdf23f28b6495dea3b055ebb60d
Maxime Tournier <maxime.tournier@anatoscope.com>
2017-06-07 14:38:25 +0200

    [SofaPython] added transition class SofaPython.Controller

--- applications/plugins/SofaPython/python/SofaPython/__init__.py
+++ applications/plugins/SofaPython/python/SofaPython/__init__.py
@@ -1,7 +1,7 @@
 import __builtin__
 import sys
 
-## @author Matthieu Nesme
+## @author Matthieu Nesme, Maxime Tournier
 ## @date 2017
 
 
@@ -26,5 +26,29 @@ def unloadModules():
     toremove = [name for name in sys.modules if not name in __SofaPythonEnvironment_importedModules__ and not name in __SofaPythonEnvironment_modulesExcludedFromReload ]
     for name in toremove:
         del(sys.modules[name]) # unload it
+
+
+
+
+import Sofa
+class Controller(Sofa.PythonScriptController):
+
+    def __init__(self, node, *args, **kwargs):
+        Sofa.msg_warning('SofaPython', 'SofaPython.Controller is intended as compatibility class only')
+
+        # setting attributes from kwargs
+        for name, value in kwargs.iteritems():
+            setattr(self, name, value)
+
+        # call createGraph for compatibility purposes
+        self.createGraph(node)
+
+
+        # check whether derived class has 'onLoaded'
+        cls = type(self)
+        if not cls.onLoaded is Sofa.PythonScriptController.onLoaded:
+            Sofa.msg_warning('SofaPython', 
+                             '`onLoaded` is defined in subclass but will not be called in the future' )
+
 
         
