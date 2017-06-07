baff932f9ba0e061353e2bb13a6219399a452a56
Maxime Tournier <maxime.tournier@anatoscope.com>
2017-06-07 15:04:00 +0200

    [Compliant] added user-friendly Coulomb constraint

---
+++ applications/plugins/Compliant/constraint/UserCoulombConstraint.h
@@ -0,0 +1,38 @@
+#ifndef COMPLIANT_USERCOULOMBCONSTRAINT_H
+#define COMPLIANT_USERCOULOMBCONSTRAINT_H
+
+#include <Compliant/constraint/Constraint.h>
+
+#include <sofa/helper/template_name.h>
+
+namespace sofa {
+namespace component {
+namespace linearsolver {
+
+// a more general friction constraint: user-defined normal direction, mu in a
+// data, projection method, etc.
+struct UserCoulombConstraint : Constraint {
+    SOFA_CLASS(UserCoulombConstraint, Constraint);
+    
+    Data<SReal> mu;
+
+    using normal_type = defaulttype::Vec<3, SReal>;
+    Data<normal_type> normal;
+
+    Data<bool> horizontal;
+    
+    UserCoulombConstraint();
+
+    // WARNING index is not used (see Constraint.h)
+    virtual void project( SReal* out, unsigned n, unsigned /*index*/, bool correct) const;
+    
+    virtual std::size_t getConstraintTypeIndex() const;
+};
+
+
+}
+}
+}
+
+
+#endif // COMPLIANT_COULOMBCONSTRAINT_H
