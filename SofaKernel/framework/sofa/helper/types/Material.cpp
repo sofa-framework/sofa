845be91983a1cdf9a7b375e3387adf34e9eba93a
Damien Marchal <damien.marchal@univ-lille1.fr>
2017-06-15 22:03:57 +0200

    [SofaKernel] SOFA_HELPER_API at the wrong place.
    I'm so tired of windows.

--- SofaKernel/framework/sofa/helper/types/Material.cpp
+++ SofaKernel/framework/sofa/helper/types/Material.cpp
@@ -38,7 +38,7 @@ namespace types
         emissive = defaulttype::RGBAColor(r,g,b,a);
     }
 
-    std::ostream& SOFA_HELPER_API operator << (std::ostream& out, const Material& m )
+    SOFA_HELPER_API std::ostream&  operator << (std::ostream& out, const Material& m )
     {
         out   << m.name         << " ";
         out  << "Diffuse"       << " " <<  m.useDiffuse   << " " <<  m.diffuse      << " ";
@@ -49,7 +49,7 @@ namespace types
         return out;
     }
 
-   std::istream& SOFA_HELPER_API operator >> (std::istream& in, Material &m )
+    SOFA_HELPER_API std::istream&  operator >> (std::istream& in, Material &m )
     {
         std::string element;
         in  >>  m.name ;
