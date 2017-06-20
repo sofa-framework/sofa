0a5992dc198cc68513bc39a02224b915aa2961ef
Damien Marchal <damien.marchal@univ-lille1.fr>
2017-06-20 22:47:01 +0200

    [PythonFactory] Move code from ".h" to ".cpp"

--- applications/plugins/SofaPython/PythonFactory.cpp
+++ applications/plugins/SofaPython/PythonFactory.cpp
@@ -25,10 +25,158 @@
 namespace sofa
 {
 
-    PyObject * PythonFactory::s_sofaPythonModule = NULL;
+PyObject* PythonFactory::s_sofaPythonModule = NULL;
 
-    PythonFactory::PythonBoundTypes PythonFactory::s_boundComponents[PythonFactory::NB_LISTS];
-    PythonFactory::PythonBoundTypes PythonFactory::s_boundData;
+PythonFactory::PythonBoundTypes PythonFactory::s_boundComponents[PythonFactory::NB_LISTS];
+PythonFactory::PythonBoundTypes PythonFactory::s_boundData;
 
+/// check for the corresponding type in the given list
+PyObject* PythonFactory::toPython( const PythonBoundTypes& list, sofa::core::objectmodel::Base* obj, PyTypeObject* pyTypeObject  )
+{
+    for(PythonBoundTypes::const_reverse_iterator it=list.rbegin(),itend=list.rend();it!=itend;++it)
+        if( (*it)->canCast( obj ) ) return BuildPySPtr<sofa::core::objectmodel::Base>(obj,(*it)->pyTypeObject);
+
+    return BuildPySPtr<sofa::core::objectmodel::Base>(obj,pyTypeObject);
+}
+
+
+/// to convert a sofa::core::objectmodel::Base-inherited object to its corresponding pyObject
+/// could be improve for known types, but since it is only setup...
+PyObject* PythonFactory::toPython(sofa::core::objectmodel::Base* obj)
+{
+    if( obj->toBaseObject() )
+    {
+        if( obj->toBaseLoader() ) return toPython( obj->toBaseLoader() );
+
+        if( obj->toTopology() )
+        {
+            if( obj->toBaseMeshTopology() ) return toPython( obj->toBaseMeshTopology() );
+            return toPython( obj->toTopology() );
+        }
+
+        if( obj->toBaseTopologyObject() ) return toPython( obj->toBaseTopologyObject() );
+
+        if( obj->toVisualModel()) return toPython( obj->toVisualModel() );
+
+        if( obj->toBaseState() )
+        {
+            if (obj->toBaseMechanicalState()) return toPython( obj->toBaseMechanicalState() );
+
+            return toPython( obj->toBaseState() );
+        }
+
+        if (obj->toBaseMapping()) return toPython( obj->toBaseMapping() );
+
+        if (obj->toDataEngine()) return toPython( obj->toDataEngine() );
+
+        return toPython( obj->toBaseObject() );
+    }
+    else if( obj->toBaseContext() ) return toPython( obj->toBaseContext() );
+
+    return toPython( s_boundComponents[Base], obj, &SP_SOFAPYTYPEOBJECT(Base) );
+}
+
+PyObject* PythonFactory::toPython(sofa::core::objectmodel::BaseObject* obj)
+{
+    if( obj->toBaseLoader() ) return toPython( obj->toBaseLoader() );
+
+    if( obj->toTopology() )
+    {
+        if( obj->toBaseMeshTopology() ) return toPython( obj->toBaseMeshTopology() );
+        return toPython( obj->toTopology() );
+    }
+
+    if( obj->toBaseTopologyObject() ) return toPython( obj->toBaseTopologyObject() );
+
+    if( obj->toVisualModel()) return toPython( obj->toVisualModel() );
+
+    if( obj->toBaseState() )
+    {
+        if (obj->toBaseMechanicalState()) return toPython( obj->toBaseMechanicalState() );
+
+        return toPython( obj->toBaseState() );
+    }
+
+    if (obj->toBaseMapping()) return toPython( obj->toBaseMapping() );
+
+    if (obj->toDataEngine()) return toPython( obj->toDataEngine() );
+
+    return toPython( s_boundComponents[BaseObject], obj, &SP_SOFAPYTYPEOBJECT(BaseObject) );
+}
+
+/// to convert a BaseContext-inherited object to its corresponding pyObject
+ PyObject* PythonFactory::toPython(sofa::core::objectmodel::BaseContext* obj)
+{
+    return toPython( s_boundComponents[BaseContext], obj, &SP_SOFAPYTYPEOBJECT(BaseContext) );
+}
+
+/// to convert a BaseLoader-inherited object to its corresponding pyObject
+ PyObject* PythonFactory::toPython(sofa::core::loader::BaseLoader* obj)
+{
+    return toPython( s_boundComponents[BaseLoader], obj, &SP_SOFAPYTYPEOBJECT(BaseLoader) );
+}
+
+/// to convert a Topology-inherited object to its corresponding pyObject
+ PyObject* PythonFactory::toPython(sofa::core::topology::Topology* obj)
+{
+    if( obj->toBaseMeshTopology() ) return toPython( obj->toBaseMeshTopology() );
+    return toPython( s_boundComponents[Topology], obj, &SP_SOFAPYTYPEOBJECT(Topology) );
+}
+
+/// to convert a BaseMeshTopology-inherited object to its corresponding pyObject
+ PyObject* PythonFactory::toPython(sofa::core::topology::BaseMeshTopology* obj)
+{
+    return toPython( s_boundComponents[BaseMeshTopology], obj, &SP_SOFAPYTYPEOBJECT(BaseMeshTopology) );
+}
+
+/// to convert a BaseTopologyObject-inherited object to its corresponding pyObject
+ PyObject* PythonFactory::toPython(sofa::core::topology::BaseTopologyObject* obj)
+{
+    return toPython( s_boundComponents[BaseTopologyObject], obj, &SP_SOFAPYTYPEOBJECT(BaseTopologyObject) );
+}
+
+/// to convert a VisualModel-inherited object to its corresponding pyObject
+ PyObject* PythonFactory::toPython(sofa::core::visual::VisualModel* obj)
+{
+    return toPython( s_boundComponents[VisualModel], obj, &SP_SOFAPYTYPEOBJECT(VisualModel) );
+}
+
+/// to convert a BaseState-inherited object to its corresponding pyObject
+ PyObject* PythonFactory::toPython(sofa::core::BaseState* obj)
+{
+    if (obj->toBaseMechanicalState()) return toPython( obj->toBaseMechanicalState() );
+
+    return toPython( s_boundComponents[BaseState], obj, &SP_SOFAPYTYPEOBJECT(BaseState) );
+}
+
+/// to convert a BaseMechanicalState-inherited object to its corresponding pyObject
+ PyObject* PythonFactory::toPython(sofa::core::behavior::BaseMechanicalState* obj)
+{
+    return toPython( s_boundComponents[BaseMechanicalState], obj, &SP_SOFAPYTYPEOBJECT(BaseMechanicalState) );
+}
+
+/// to convert a BaseMapping-inherited object to its corresponding pyObject
+ PyObject* PythonFactory::toPython(sofa::core::BaseMapping* obj)
+{
+    return toPython( s_boundComponents[BaseMapping], obj, &SP_SOFAPYTYPEOBJECT(BaseMapping) );
+}
+
+/// to convert a DataEngine-inherited object to its corresponding pyObject
+ PyObject* PythonFactory::toPython(sofa::core::DataEngine* obj)
+{
+    return toPython( s_boundComponents[DataEngine], obj, &SP_SOFAPYTYPEOBJECT(DataEngine) );
+}
+
+/// to convert a sofa::core::objectmodel::BaseData to its corresponding pyObject
+/// returns NULL if the data does not correpond to any special type
+ PyObject* PythonFactory::toPython(sofa::core::objectmodel::BaseData* data)
+{
+    for(PythonBoundTypes::const_reverse_iterator it=s_boundData.rbegin(),itend=s_boundData.rend();it!=itend;++it)
+    {
+        if( (*it)->canCast( data ) )
+            return BuildPyPtr<sofa::core::objectmodel::BaseData>(data,(*it)->pyTypeObject,false);
+    }
+    return NULL;
+}
 
 } // namespace sofa
