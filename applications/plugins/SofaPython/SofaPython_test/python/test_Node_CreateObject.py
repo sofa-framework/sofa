3fd866e6b0a22de772d47476614cad3463b4f349
Thomas Lemaire <lemaire@anatoscope.com>
2017-07-06 17:24:56 +0200

    [SofaKernel] change vec (de)serialization - can read whitespace separated values - can read [,] formatted string - CHANGED write [,] formatted string [SofaPython] test 3 ways to specify an input vector data in createObject()

---
+++ applications/plugins/SofaPython/SofaPython_test/python/test_Node_CreateObject.py
@@ -0,0 +1,42 @@
+import Sofa
+
+import SofaTest
+
+def createScene(rootNode):
+    
+    # simple input string
+    node = rootNode.createChild("nodeA")
+    node.createObject('MechanicalObject', template="Vec3", name="dof", position="1 1 1 2 2 2 3 3 3")
+    node.createObject('PythonScriptController',
+        filename = __file__,
+        classname = 'TestDataSerialization',
+        name = 'script' )
+    
+    position = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+    # input string with delimiters
+    node = rootNode.createChild("nodeB")
+    node.createObject('MechanicalObject', template="Vec3", name="dof", position=repr(position))
+    node.createObject('PythonScriptController',
+        filename = __file__,
+        classname = 'TestDataSerialization',
+        name = 'script' )
+    
+    # input python list with delimiters
+    node = rootNode.createChild("nodeB")
+    node.createObject('MechanicalObject', template="Vec3", name="dof", position=position)
+    node.createObject('PythonScriptController',
+        filename = __file__,
+        classname = 'TestDataSerialization',
+        name = 'script' )
+
+    
+
+class TestDataSerialization(SofaTest.Controller):
+
+    def onLoaded(self, node):
+        SofaTest.Controller.onLoaded(self, node)
+        self.dof = self.node.getObject("dof")
+
+    def onEndAnimationStep(self, dt):
+        self.ASSERT("[[1, 1, 1], [2, 2, 2], [3, 3, 3]]" == self.dof.findData('position').getValueString(), "test1")
+        self.sendSuccess()
