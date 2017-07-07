5304ce4aa5d4f80de1b8ab714868d08ef25b9774
Thomas Lemaire <lemaire@anatoscope.com>
2017-07-07 16:05:06 +0200

    [SofaPython] One more Node.createObject(), test on helper::vector<SReal>

--- applications/plugins/SofaPython/SofaPython_test/python/test_Node_CreateObject.py
+++ applications/plugins/SofaPython/SofaPython_test/python/test_Node_CreateObject.py
@@ -7,15 +7,18 @@ def createScene(rootNode):
     # simple input string
     node = rootNode.createChild("nodeA")
     node.createObject('MechanicalObject', template="Vec3", name="dof", position="1 1 1 2 2 2 3 3 3")
+    node.createObject('DilateEngine', template="Vec3", name="dilate", thickness="1.2 2.3 3.4")
     node.createObject('PythonScriptController',
         filename = __file__,
         classname = 'TestDataSerialization',
         name = 'script' )
     
     position = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+    thickness = [1.2, 2.3, 3.4]
     # input string with delimiters
     node = rootNode.createChild("nodeB")
     node.createObject('MechanicalObject', template="Vec3", name="dof", position=repr(position))
+    node.createObject('DilateEngine', template="Vec3", name="dilate", thickness=repr(thickness))
     node.createObject('PythonScriptController',
         filename = __file__,
         classname = 'TestDataSerialization',
@@ -24,6 +27,7 @@ def createScene(rootNode):
     # input python list
     node = rootNode.createChild("nodeB")
     node.createObject('MechanicalObject', template="Vec3", name="dof", position=position)
+    node.createObject('DilateEngine', template="Vec3", name="dilate", thickness=thickness)
     node.createObject('PythonScriptController',
         filename = __file__,
         classname = 'TestDataSerialization',
@@ -36,7 +40,9 @@ class TestDataSerialization(SofaTest.Controller):
     def onLoaded(self, node):
         SofaTest.Controller.onLoaded(self, node)
         self.dof = self.node.getObject("dof")
+        self.dilate = self.node.getObject("dilate")
 
     def onEndAnimationStep(self, dt):
-        self.ASSERT("[[1, 1, 1], [2, 2, 2], [3, 3, 3]]" == self.dof.findData('position').getValueString(), "test1")
+        self.ASSERT("[[1, 1, 1], [2, 2, 2], [3, 3, 3]]" == self.dof.findData('position').getValueString(), "test_vec_Vec3")
+        self.ASSERT("[1.2, 2.3, 3.4]" == self.dilate.findData('thickness').getValueString(), "test_vec_Real")
         self.sendSuccess()
