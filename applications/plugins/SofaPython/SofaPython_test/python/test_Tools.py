b4f62cad22dd975dd47ea119865bc38b3e5f2d43
Thomas Lemaire <lemaire@anatoscope.com>
2017-07-11 17:06:54 +0200

    [SofaPython] fix test, Tools.listToStr() is back to previous behavior

--- applications/plugins/SofaPython/SofaPython_test/python/test_Tools.py
+++ applications/plugins/SofaPython/SofaPython_test/python/test_Tools.py
@@ -5,6 +5,6 @@ from SofaTest.Macro import *
 def run():
     ok=True
     l = [1,2,3,4]
-    ok&=EXPECT_EQ(repr(l), SofaPython.Tools.listToStr(l) )
+    ok&=EXPECT_EQ("1 2 3 4", SofaPython.Tools.listToStr(l) )
     return ok
 
