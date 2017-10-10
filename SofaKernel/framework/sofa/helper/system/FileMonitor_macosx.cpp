e41fbf600e13e5f6bee1a30f95a79cb66fcaa349
Damien Marchal <damien.marchal@univ-lille1.fr>
2017-10-10 22:24:31 +0200

    [SofaKernel] Fix failure in FileMonitor (WIP)

--- SofaKernel/framework/sofa/helper/system/FileMonitor_macosx.cpp
+++ SofaKernel/framework/sofa/helper/system/FileMonitor_macosx.cpp
@@ -117,8 +117,11 @@ private:
     {
         MonitoredFile *mf = (MonitoredFile*)clientCallBackInfo;
         mf->m_changed=true;
-//printf("#################################################  eventCallback file=%s\n",mf->m_filename.c_str());
-//fflush(stdout);
+
+        printf("#################################################  eventCallback file=%s\n",mf->m_filename.c_str());
+        printf("... numEvent %", numEvents );
+
+        fflush(stdout);
     }
 
     FSEventStreamRef    m_eventStream;
@@ -207,8 +210,8 @@ int FileMonitor::updates(int timeout)
     // check file changes
     do{
         // update FSEventStreams
-        //printf("update streams...\n");
-        //fflush(stdout);
+        printf("update streams...\n");
+        fflush(stdout);
         CFRunLoopRunInMode(kCFRunLoopDefaultMode,
                            0,
                            false);
@@ -216,7 +219,7 @@ int FileMonitor::updates(int timeout)
         {
             if (!(*it_monitor)->update())
             {
-//                printf("FileListener::fileHasChanged(%s) called...\n",(*it_monitor)->m_filename.c_str());
+                printf("FileListener::fileHasChanged(%s) called...\n",(*it_monitor)->m_filename.c_str());
                 (*it_monitor)->m_listener->fileHasChanged((*it_monitor)->m_filename);
                 keep_going = 0; // we're done
             }
