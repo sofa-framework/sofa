6aeb14bd1298f0047424a09c26a1e9453f8eb2c0
Damien Marchal <damien.marchal@univ-lille1.fr>
2017-11-14 13:48:07 +0100

    [SofaTest] Adds additional ASSERT_* function into SofaTest/_init_.py

--- applications/plugins/SofaTest/python/SofaTest/Macro.py
+++ applications/plugins/SofaTest/python/SofaTest/Macro.py
@@ -43,7 +43,41 @@ def ASSERT_TRUE(value):
        (including line number).
     """
     if not value:
-        message  = "Expecting value 'True' while it is '"+str(value)+"' \n"
+        message  = "Expecting 'True' while value is '"+str(value)+"' \n"
+        raise Exception(message)
+
+def ASSERT_FALSE(value):
+        """raise an exception if value is not FALSE. The exception can then be catched
+           at top level and converted into a Sofa.msg_error() which can be catched by the
+           EXPECT_MSG_NOEMIT(Error) clause of BaseTest. So these failure are correctly reported in gtest
+           (including line number).
+        """
+        if value:
+            message  = "Expecting 'False' while value is '"+str(value)+"' \n"
+            raise Exception(message)
+
+def ASSERT_GT(value1, value2):
+    """raise an exception if value1 <= value2. The exception can then be catched
+       at top level and converted into a Sofa.msg_error() which can be catched by the
+       EXPECT_MSG_NOEMIT(Error) clause of BaseTest. So these failure are correctly reported in gtest
+       (including line number).
+    """
+    if value1 <= value2:
+        message  = "Expecting value1 > value2 while got\n"
+        message += "\t- value1: "+str(value1)+"\n"
+        message += "\t- value2: "+str(value2)
+        raise Exception(message)
+
+def ASSERT_LT(value1, value2):
+    """raise an exception if value1 >= value2. The exception can then be catched
+       at top level and converted into a Sofa.msg_error() which can be catched by the
+       EXPECT_MSG_NOEMIT(Error) clause of BaseTest. So these failure are correctly reported in gtest
+       (including line number).
+    """
+    if value1 >= value2:
+        message  = "Expecting value1 < value2 while got \n"
+        message += "\t- value1: "+str(value1)+"\n"
+        message += "\t- value2: "+str(value2)
         raise Exception(message)
 
 def ASSERT_NEQ(value1, value2):
