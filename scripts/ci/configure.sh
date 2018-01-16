8d9094cc714a038cdc341744dc37ffdfd3241566
Guillaume Paran <guillaume.paran@sofa-framework.org>
2018-01-16 17:25:15 +0100

    [CI] Restrict SofaHeadlessRecorder to Linux VMs

--- scripts/ci/configure.sh
+++ scripts/ci/configure.sh
@@ -184,7 +184,10 @@ case $CI_OPTIONS in
         append "-DPLUGIN_SOFADISTANCEGRID=ON"
         # Requires HAPI libraries.
         append "-DPLUGIN_SOFAHAPI=OFF"
-        append "-DPLUGIN_SOFAHEADLESSRECORDER=ON"
+        # HeadlessRecorder is Linux only for now
+        if [[ $(uname) = Linux ]]; then 
+            append "-DPLUGIN_SOFAHEADLESSRECORDER=ON"
+        fi
         append "-DPLUGIN_SOFAIMPLICITFIELD=ON"
         # Not sure if worth maintaining
         append "-DPLUGIN_SOFASIMPLEGUI=ON"
