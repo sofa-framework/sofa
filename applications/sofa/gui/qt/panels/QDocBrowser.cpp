59255d7a1c401f858fa7a4769f7a6de6dd396120
Margaret Koehler <misk92@gmail.com>
2018-01-26 15:18:57 +0100

    Fix GuiRepository path for Windows and file opening for the doc browser

--- applications/sofa/gui/qt/panels/QDocBrowser.cpp
+++ applications/sofa/gui/qt/panels/QDocBrowser.cpp
@@ -206,7 +206,7 @@ void DocBrowser::loadHtml(const std::string& filename)
     if (DataRepository.findFile(htmlfile, "", NULL))
     {
         m_htmlPage->setSearchPaths(QStringList(QString(rootdir.c_str())));
-        m_htmlPage->setSource( QUrl(QString(htmlfile.c_str())) );
+        m_htmlPage->setSource(QUrl::fromLocalFile(QString(htmlfile.c_str())) );
         m_browserhistory->push(htmlfile, filename, rootdir) ;
     }
 
