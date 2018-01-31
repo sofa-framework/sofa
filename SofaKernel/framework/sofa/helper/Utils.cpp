6e0424065133651914561620a7952b34447c1ed9
Guillaume Paran <guillaume.paran@sofa-framework.org>
2018-01-31 11:32:10 +0100

    [SofaHelper] FIX Utils for MacOS bundle
    - msg_error in computeSofaPathPrefix was triggering a segfault
    - getSofaPathTo needs to check that the path exists

--- SofaKernel/framework/sofa/helper/Utils.cpp
+++ SofaKernel/framework/sofa/helper/Utils.cpp
@@ -249,7 +249,7 @@ static std::string computeSofaPathPrefix()
         const std::string exePath = Utils::getExecutablePath();
         std::size_t pos = exePath.rfind("/bin/");
         if (pos == std::string::npos) {
-            msg_error("Utils::getSofaPathPrefix()") << "failed to deduce the root path of Sofa from the application path: (" << exePath << ")";
+            // msg_error("Utils::getSofaPathPrefix()") << "failed to deduce the root path of Sofa from the application path: (" << exePath << ")";
             // Safest thing to return in this case, I guess.
             return Utils::getExecutableDirectory();
         }
@@ -267,7 +267,15 @@ const std::string& Utils::getSofaPathPrefix()
 
 const std::string Utils::getSofaPathTo(const std::string& pathFromBuildDir)
 {
-    return getSofaPathPrefix() + "/" + pathFromBuildDir;
+    std::string path = Utils::getSofaPathPrefix() + "/" + pathFromBuildDir;
+    if(FileSystem::exists(path))
+    {
+        return path;
+    }
+    else
+    {
+        return Utils::getSofaPathPrefix();
+    }
 }
 
 std::map<std::string, std::string> Utils::readBasicIniFile(const std::string& path)
