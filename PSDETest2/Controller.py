67eac7d102f60d5ca4af2cd973be16c77b0cf1de
Stefan Escaida <stefan.escaida-navarro@inria.fr>
2018-02-06 12:04:43 +0100

    cleaning up examples

--- PSDETest2/Controller.py
+++ PSDETest2/Controller.py
@@ -15,28 +15,11 @@ class Controller(Sofa.PythonScriptController):
             inputvalue = self.node.getChild('constantForce').getObject('forceField').findData('forces')
             inputvalue2 = self.node.getChild('constantForce2').getObject('forceField2').findData('forces')
             inputvalue3 = self.node.getChild('constantForce3').getObject('forceField3').findData('forces')
-            #inputvalue2 = []
-            
-            if (c == "+"):
-               #displacement = inputvalue.value[0][0] + 1.
-                inputvalue.value = "30000 0 0"
-               
-            elif (c == "-"):
-               #displacement = inputvalue.value[0][0] - 1.
-               #if(displacement < 0):
-               # displacement = 0
-                inputvalue.value = "0 0 0"
-               
+            #inputvalue2 = []            
+       
             elif (c == "1"):
                 inputvalue2.value = "-30000 0 0"
                 
             elif (c == "2"):
-                inputvalue2.value = "0 0 0"
-                
-            elif (c == "3"):
-                inputvalue3.value = "0 -30000 0"
-                
-            elif (c == "4"):
-                inputvalue3.value = "0 0 0"
-               
+                inputvalue2.value = "0 0 0"                               
   
