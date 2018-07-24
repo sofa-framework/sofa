3fd889b60d86ad0ff25c2752fc0a0c84f82901ff
ESCAIDA NAVARRO Stefan <stefan.escaida-navarro@inria.fr>
2018-07-24 12:00:42 +0200

    small adjustments to examples to include chained PSDEs

--- applications/plugins/SofaPython/examples/PSDEExamples/01-basic/DataEngine2.py
+++ applications/plugins/SofaPython/examples/PSDEExamples/01-basic/DataEngine2.py
@@ -2,11 +2,12 @@ class MyDataEngine(Sofa.PythonScriptDataEngine):
   
     def parse(self):        
         # this is the correct place to define inputs and outputs! (linking will not work in init() for instance)
-        self.addNewInput('Positions',datatype='vector<Vec3d>',value='@loader.position')       
-        self.addNewOutput('NumPoints',datatype='int')                
+        self.addNewInput('NumPoints', value='@PSDE.NumPoints')       
+        self.addNewOutput('NumPoints2',datatype='int')                
 
     def init(self):
 	pass
 
     def update(self):        
-        self.NumPoints = len(self.Positions)
+        self.NumPoints2 = self.NumPoints
+        
