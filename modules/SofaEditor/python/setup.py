72fa56ac8555fbde4ae6653f4ef7f9f67c21089a
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-07-28 02:39:30 +0200

    [SofaEditor] Update CMakeLists.txt & setup.py to handle compilations without cython

--- modules/SofaEditor/python/setup.py
+++ modules/SofaEditor/python/setup.py
@@ -1,19 +1,39 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
+import sys, os, glob
 
-## Stop on first error
-from Cython.Compiler import Options
-Options.fast_fail = True
+if '--use-cython' in sys.argv:
+    USE_CYTHON = True
+    sys.argv.remove('--use-cython')
+else:
+    USE_CYTHON = False
 
-exts = Extension("SofaEditor.*", ["SofaEditor/*.pyx"],
-                 libraries=["SofaEditor"],
-                 language="c++",
-                 extra_compile_args=["-std=c++11"]
-                 )
+ext = 'pyx' if USE_CYTHON else 'cpp'
+
+def toImport(s):
+    return s.replace("/",".")[:-4]
+
+sources = glob.iglob("SofaEditor/*."+ext)
+exts=[]
+for source in sources:
+    print("Generating the binding using:" + source)
+    exts.append(Extension(toImport(source), [source],
+                    libraries=["SofaEditor"],
+                    language="c++",
+                    extra_compile_args=["-std=c++11"]
+                    ))
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+
+    ## Stop on first error
+    from Cython.Compiler import Options
+    Options.fast_fail = True
+
+    exts = cythonize(exts)
 
 setup(
     packages = ['SofaEditor'],
-    ext_modules = cythonize(exts)
+    ext_modules = exts
 )
