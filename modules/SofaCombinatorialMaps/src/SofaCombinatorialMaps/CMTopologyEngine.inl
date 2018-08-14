3c03d90a51116938c634d4c1636661f1ddb41fe4
lionel untereiner <lionel.untereiner@inria.fr>
2018-08-14 15:38:53 +0200

    [All] MOVE combinatorial files to a module

---
+++ modules/SofaCombinatorialMaps/src/SofaCombinatorialMaps/CMTopologyEngine.inl
@@ -0,0 +1,214 @@
+#ifndef CMTOPOLOGYENGINE_INL
+#define CMTOPOLOGYENGINE_INL
+
+#include <SofaCombinatorialMaps/CMTopologyEngine.h>
+
+//#include <SofaBaseTopology/TetrahedronSetTopologyContainer.h>
+//#include <SofaBaseTopology/HexahedronSetTopologyContainer.h>
+
+
+namespace sofa
+{
+namespace component
+{
+namespace cm_topology
+{
+
+template <typename VecT>
+TopologyEngineImpl< VecT>::TopologyEngineImpl(t_topologicalData *_topologicalData,
+        core::topology::MapTopology* _topology,
+        sofa::core::cm_topology::TopologyHandler *_topoHandler) :
+    m_topologicalData(_topologicalData),
+    m_topology(NULL),
+    m_topoHandler(_topoHandler),
+    m_pointsLinked(false), m_edgesLinked(false), m_trianglesLinked(false),
+    m_quadsLinked(false), m_tetrahedraLinked(false), m_hexahedraLinked(false)
+{
+    m_topology = _topology;
+
+    if (m_topology == NULL)
+        serr <<"Error: Topology is not dynamic" << sendl;
+
+    if (m_topoHandler == NULL)
+        serr <<"Error: Topology Handler not available" << sendl;
+}
+
+template <typename VecT>
+void TopologyEngineImpl< VecT>::init()
+{
+    // A pointData is by default child of positionSet Data
+    //this->linkToPointDataArray();  // already done while creating engine
+
+    // Name creation
+    if (m_prefix.empty()) m_prefix = "TopologyEngine_";
+    m_data_name = this->m_topologicalData->getName();
+    this->addOutput(this->m_topologicalData);
+
+    sofa::core::cm_topology::TopologyEngine::init();
+
+    // Register Engine in containter list
+    //if (m_topology)
+    //   m_topology->addTopologyEngine(this);
+    //this->registerTopology(m_topology);
+}
+
+
+template <typename VecT>
+void TopologyEngineImpl< VecT>::reinit()
+{
+    this->update();
+}
+
+
+template <typename VecT>
+void TopologyEngineImpl< VecT>::update()
+{
+#ifndef NDEBUG // too much warnings
+    sout << "TopologyEngine::update" << sendl;
+    sout<< "Number of topological changes: " << m_changeList.getValue().size() << sendl;
+#endif
+    this->cleanDirty();
+    this->ApplyTopologyChanges();
+}
+
+
+template <typename VecT>
+void TopologyEngineImpl< VecT>::registerTopology(core::topology::MapTopology* _topology)
+{
+    m_topology =  _topology;
+
+    if (m_topology == NULL)
+    {
+#ifndef NDEBUG // too much warnings
+        serr <<"Error: Topology is not dynamic" << sendl;
+#endif
+        return;
+    }
+    else
+        m_topology->addTopologyEngine(this);
+}
+
+
+template <typename VecT>
+void TopologyEngineImpl< VecT>::registerTopology()
+{
+    if (m_topology == NULL)
+    {
+#ifndef NDEBUG // too much warnings
+        serr <<"Error: Topology is not dynamic" << sendl;
+#endif
+        return;
+    }
+    else
+        m_topology->addTopologyEngine(this);
+}
+
+
+template <typename VecT>
+void TopologyEngineImpl< VecT>::ApplyTopologyChanges()
+{
+    // Rentre ici la premiere fois aussi....
+    if(m_topoHandler)
+    {
+        m_topoHandler->ApplyTopologyChanges(m_changeList.getValue(), m_topology->getNbPoints());
+
+        m_changeList.endEdit();
+    }
+}
+
+
+/// Function to link DataEngine with Data array from topology
+template <typename VecT>
+void TopologyEngineImpl< VecT>::linkToPointDataArray()
+{
+	if (m_pointsLinked) // avoid second registration
+		return;
+
+	if (m_topology->getPointDataArray().getValue().size() > 0)
+	{
+		m_topology->getPointDataArray().addOutput(this);
+		m_pointsLinked = true;
+	}
+}
+
+
+template <typename VecT>
+void TopologyEngineImpl< VecT>::linkToEdgeDataArray()
+{
+    if (m_edgesLinked) // avoid second registration
+        return;
+
+	if (m_topology->getTetrahedronDataArray().getValue().size() > 0)
+	{
+		m_topology->getEdgeDataArray().addOutput(this);
+		m_edgesLinked = true;
+	}
+}
+
+
+template <typename VecT>
+void TopologyEngineImpl< VecT>::linkToTriangleDataArray()
+{
+	if (m_trianglesLinked) // avoid second registration
+		return;
+
+	if (m_topology->getTetrahedronDataArray().getValue().size() > 0)
+	{
+		m_topology->getTriangleDataArray().addOutput(this);
+		m_trianglesLinked = true;
+	}
+}
+
+
+template <typename VecT>
+void TopologyEngineImpl< VecT>::linkToQuadDataArray()
+{
+    if (m_quadsLinked) // avoid second registration
+        return;
+
+	if (m_topology->getTetrahedronDataArray().getValue().size() > 0)
+	{
+		m_topology->getQuadDataArray().addOutput(this);
+		m_quadsLinked = true;
+	}
+}
+
+
+template <typename VecT>
+void TopologyEngineImpl< VecT>::linkToTetrahedronDataArray()
+{
+    if (m_tetrahedraLinked) // avoid second registration
+        return;
+
+	if (m_topology->getTetrahedronDataArray().getValue().size() > 0)
+	{
+		m_topology->getTetrahedronDataArray().addOutput(this);
+		m_tetrahedraLinked = true;
+	}
+}
+
+
+template <typename VecT>
+void TopologyEngineImpl< VecT>::linkToHexahedronDataArray()
+{
+    if (m_hexahedraLinked) // avoid second registration
+        return;
+
+	if (m_topology->getTetrahedronDataArray().getValue().size() > 0)
+	{
+		m_topology->getHexahedronDataArray().addOutput(this);
+		m_hexahedraLinked = true;
+	}
+}
+
+
+
+
+
+}// namespace cm_topology
+
+} // namespace component
+
+} // namespace sofa
+
+#endif // CMTOPOLOGYENGINE_INL
---
+++ modules/SofaCombinatorialMaps/src/SofaCombinatorialMaps/VolumeGeometryAlgorithms.h
@@ -0,0 +1,138 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-2016 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This library is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This library is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this library; if not, write to the Free Software Foundation,     *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+*******************************************************************************
+*                               SOFA :: Modules                               *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef SOFA_COMPONENT_TOPOLOGY_VOLUMEGEOMETRYALGORITHMS_H
+#define SOFA_COMPONENT_TOPOLOGY_VOLUMEGEOMETRYALGORITHMS_H
+#include "config.h"
+
+#include <SofaCombinatorialMaps/CMBaseTopology.h>
+#include <SofaBaseTopology/NumericalIntegrationDescriptor.h>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace topology
+{
+
+
+/**
+* A class that provides geometry information on an TetrahedronSet.
+*/
+template < class DataTypes >
+class VolumeGeometryAlgorithms : public core::cm_topology::GeometryAlgorithms
+{
+public:
+    SOFA_CLASS(SOFA_TEMPLATE(VolumeGeometryAlgorithms,DataTypes),core::cm_topology::GeometryAlgorithms);
+
+    typedef typename DataTypes::VecCoord VecCoord;
+    typedef typename DataTypes::Real Real;
+    typedef typename DataTypes::Coord Coord;
+
+
+protected:
+    bool initializedCubatureTables;
+    void defineTetrahedronCubaturePoints();
+
+    VolumeGeometryAlgorithms()
+        : Inherit1()
+        ,initializedCubatureTables(false)
+        , d_showVolumesIndices (initData(&d_showVolumesIndices, (bool) false, "showVolumesIndices", "Debug : view volumes indices"))
+        , d_drawVolumes(initData(&d_drawVolumes, false, "drawVolumes","if true, draw the volumes in the topology"))
+        , d_drawScaleVolumes(initData(&d_drawScaleVolumes, (float) 1.0, "drawScaleVolumes", "Scale of the terahedra (between 0 and 1; if <1.0, it produces gaps between the volumes)"))
+        , d_drawColorVolumes(initData(&d_drawColorVolumes, sofa::defaulttype::Vec4f(1.0f,1.0f,0.0f,1.0f), "drawColorVolumes", "RGBA code color used to draw volumes."))
+    {
+    }
+
+    virtual ~VolumeGeometryAlgorithms() {}
+public:
+    virtual void draw(const core::visual::VisualParams* vparams)
+	{
+
+	}
+
+	bool isInTriangle(const Coord& P, const helper::fixed_array< Coord, 3 >& triangle)
+	{
+	  //    const Coord & AP = P - triangle[0];
+	  //    const Coord & AB = triangle[1] - triangle[0];
+	  //    const Coord & AC = triangle[2] - triangle[0];
+	  //    const Coord & BA = -AB;
+	  //    const SReal & alpha = AB * AP ;
+	  //    const SReal & beta =  AC * AP ;
+	  //    const SReal & gamma = BA * (P - triangle[1]);
+	  //    return (alpha >= 0) && (beta >= 0) && (gamma >= 0);
+
+	  const Coord& v2 = P - triangle[0];
+	  const Coord& v1 = triangle[1] - triangle[0];
+	  const Coord& v0 = triangle[2] - triangle[0];
+
+	  const SReal& dot00 = v0 * v0;
+	  const SReal& dot01 = v0 * v1;
+	  const SReal& dot02 = v0 * v2;
+	  const SReal& dot11 = v1 * v1;
+	  const SReal& dot12 = v1 * v2;
+
+	  const SReal& invDenom = static_cast< SReal >(1) / (dot00 * dot11 - dot01 * dot01);
+	  const SReal& u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+	  const SReal& v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+
+	  return (u >= 0) && (v >= 0) && (u + v <= 1);
+	}
+
+protected:
+    Data<bool> d_showVolumesIndices;
+    Data<bool> d_drawVolumes;
+    Data<float> d_drawScaleVolumes;
+    Data<sofa::defaulttype::Vec4f> d_drawColorVolumes;
+    /// include cubature points
+//    NumericalIntegrationDescriptor<Real,4> tetrahedronNumericalIntegration;
+};
+
+#if defined(SOFA_EXTERN_TEMPLATE) && !defined(SOFA_COMPONENT_TOPOLOGY_TETRAHEDRONSETGEOMETRYALGORITHMS_CPP)
+#ifndef SOFA_FLOAT
+extern template class SOFA_BASE_TOPOLOGY_API VolumeGeometryAlgorithms<defaulttype::Vec3dTypes>;
+extern template class SOFA_BASE_TOPOLOGY_API VolumeGeometryAlgorithms<defaulttype::Vec2dTypes>;
+extern template class SOFA_BASE_TOPOLOGY_API VolumeGeometryAlgorithms<defaulttype::Vec1dTypes>;
+//extern template class SOFA_BASE_TOPOLOGY_API TetrahedronSetGeometryAlgorithms<defaulttype::Rigid3dTypes>;
+//extern template class SOFA_BASE_TOPOLOGY_API TetrahedronSetGeometryAlgorithms<defaulttype::Rigid2dTypes>;
+#endif
+
+#ifndef SOFA_DOUBLE
+extern template class SOFA_BASE_TOPOLOGY_API VolumeGeometryAlgorithms<defaulttype::Vec3fTypes>;
+extern template class SOFA_BASE_TOPOLOGY_API VolumeGeometryAlgorithms<defaulttype::Vec2fTypes>;
+extern template class SOFA_BASE_TOPOLOGY_API VolumeGeometryAlgorithms<defaulttype::Vec1fTypes>;
+//extern template class SOFA_BASE_TOPOLOGY_API TetrahedronSetGeometryAlgorithms<defaulttype::Rigid3fTypes>;
+//extern template class SOFA_BASE_TOPOLOGY_API TetrahedronSetGeometryAlgorithms<defaulttype::Rigid2fTypes>;
+#endif
+#endif
+
+} // namespace topology
+
+} // namespace component
+
+} // namespace sofa
+
+#endif // SOFA_COMPONENT_TOPOLOGY_VOLUMEGEOMETRYALGORITHMS_H
