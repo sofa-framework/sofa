e646f32244ea802970e4525eca0246b6797be3c1
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-08-30 13:59:33 +0200

    [SofaEditor] Refactors the tests so they use the unittest framework (python standard)

--- modules/SofaEditor/python/tests/pyfiles/SofaEditor_test.py
+++ modules/SofaEditor/python/tests/pyfiles/SofaEditor_test.py
@@ -1,45 +1,57 @@
+# -*- coding: utf-8 -*
 import unittest
-from SofaTest import *
 from SofaEditor import SofaEditor, SofaEditorState
 
 class SofaEditorState_test(unittest.TestCase):
     def test_constructor(self):
         anEditor = SofaEditorState("editor1")
-        EXPECT_EQ("editor1", anEditor.editorname)
+        self.assertEqual("editor1", anEditor.editorname)
 
     def test_property(self):
         anEditor = SofaEditorState("editor1")
-        EXPECT_EQ("editor1", anEditor.editorname)
+        self.assertEqual("editor1", anEditor.editorname)
 
         anEditor.editorname = "newName"
-        EXPECT_EQ("newName", anEditor.editorname)
+        self.assertEqual("newName", anEditor.editorname)
 
 class SofaEditor_test(unittest.TestCase):
-    def test_createId(self):
-        e1 = SofaEditorState("editor1")
-        e2 = SofaEditorState("editor2")
+    @classmethod
+    def setUpClass(cls):
+        ## Create two states and register them.
+        cls.s1 = SofaEditorState("editor1")
+        cls.s2 = SofaEditorState("editor2")
+        cls.id1 = SofaEditor.createIdAndAttachState(cls.s1)
+        cls.id2 = SofaEditor.createIdAndAttachState(cls.s2)
 
-        s1 = SofaEditor.createId(e1)
-        s2 = SofaEditor.createId(e2)
+    def test_getIdFromEditorname(self):
+        id1 = SofaEditor.getIdFromEditorName("editor1")
+        id2 = SofaEditor.getIdFromEditorName("editor2")
 
-        as1 = SofaEditor.getIdFromEditorName("editor1")
-        as2 = SofaEditor.getIdFromEditorName("editor2")
-
-        EXPECT_EQ(s1, as1)
-        EXPECT_EQ(s2, as2)
+        self.assertEqual(SofaEditor_test.id1, id1)
+        self.assertEqual(SofaEditor_test.id2, id2)
 
     def test_getState(self):
-        s1 = SofaEditor.getState(as1)
-        s2 = SofaEditor.getState(as2)
-        EXPECT_EQ(e1, s1)
-        EXPECT_EQ(e2, s2)
+        id1 = SofaEditor.getIdFromEditorName("editor1")
+        id2 = SofaEditor.getIdFromEditorName("editor2")
+
+        s1 = SofaEditor.getState(id1)
+        s2 = SofaEditor.getState(id2)
+
+        self.assertEqual(SofaEditor_test.s1, s1)
+        self.assertEqual(SofaEditor_test.s2, s2)
 
     def test_getSelection(self):
-        s1 = SofaEditor.getSelection(as1)
-        s2 = SofaEditor.getSelection(as2)
-        EXPECT_EQ([], s1)
-        EXPECT_EQ([], s2)
+        id1 = SofaEditor.getIdFromEditorName("editor1")
+        id2 = SofaEditor.getIdFromEditorName("editor2")
+
+        s1 = SofaEditor.getSelection(id1)
+        s2 = SofaEditor.getSelection(id2)
+
+        self.assertEqual([], s1)
+        self.assertEqual([], s2)
 
 def run():
-    unittest.main()
+    for testcase in [SofaEditorState_test, SofaEditor_test]:
+        suite = unittest.TestLoader().loadTestsFromTestCase(testcase)
+        unittest.TextTestRunner(verbosity=2).run(suite)
     return True
