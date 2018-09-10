a70737860a7e004d94b823abfe234476fd245ea3
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-09-10 13:43:45 +0200

    [pybind] Base

---
+++ modules/SofaPython3/bindings/Sofa/src/pybind/Binding_Base.cpp
@@ -0,0 +1,43 @@
+#include <pybind11/operators.h>
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+#include <sofa/core/objectmodel/Base.h>
+using sofa::core::objectmodel::Base;
+
+/// More info about smart pointer in
+/// /pybind11.readthedocs.io/en/stable/advanced/smart_ptrs.html
+PYBIND11_DECLARE_HOLDER_TYPE(Base, boost::intrusive_ptr<Base>, true);
+
+void init_Base(py::module &m)
+{
+  py::class_<Base, Base::SPtr> p(m, "Base");
+}
+
+
+
+  /*
+  p.def_readwrite("distance", &Plane::distance);
+  p.def_readwrite("normal", &Plane::normal);
+  p.def(py::init<const vec3 &, double>(), "normal"_a = Constants::XAxis,
+        "distance"_a = 0);
+  p.def(py::init([](py::list l, double dist) {
+          return std::unique_ptr<Plane>(
+              new Plane(vec3(double(l[0].cast<py::float_>()),
+                             double(l[1].cast<py::float_>()),
+                             double(l[2].cast<py::float_>())),
+                        dist));
+        }),
+        "normal"_a = Constants::XAxis, "distance"_a = 0);
+  p.def(py::init<const vec3 &, const vec3 &>(), "normal"_a, "point"_a);
+  p.def("raycast",
+        (bool (Plane::*)(const Ray &, double &) const) & Plane::raycast,
+        "ray"_a, "p"_a);
+  p.def("raycast",
+        [](const Plane &plane, Ray r) {
+          double p = 0.0;
+          plane.raycast(r, p);
+          return p;
+        },
+        "ray"_a);
+   */
