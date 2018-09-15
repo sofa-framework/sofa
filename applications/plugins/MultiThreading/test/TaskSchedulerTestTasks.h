eabc88e2ec73e494bbf2739d145f74bd4ba9f175
fspadoni <spadoni.federico@gmail.com>
2018-09-15 12:32:16 +0200

    [MultiThreading] Fix Task scheduler tests

--- applications/plugins/MultiThreading/test/TaskSchedulerTestTasks.h
+++ applications/plugins/MultiThreading/test/TaskSchedulerTestTasks.h
@@ -17,7 +17,7 @@ namespace sofa
 
 		virtual ~FibonacciTask() { }
 
-		virtual bool run() override sealed;
+		virtual bool run() final;
 
 	private:
 
@@ -40,7 +40,7 @@ namespace sofa
 
 		virtual ~IntSumTask() {}
 
-		virtual bool run() override sealed;
+		virtual bool run() final;
 
 
 	private:
