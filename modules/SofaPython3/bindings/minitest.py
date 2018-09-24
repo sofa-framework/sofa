2bace0c32610d6120f93576f9538f5499c54b73b
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-09-24 17:16:30 +0200

    [SofaPython3] Minor work in the testing .py

--- modules/SofaPython3/bindings/minitest.py
+++ modules/SofaPython3/bindings/minitest.py
@@ -2,19 +2,24 @@
 
 import sys
 import os
-os.environ["SOFA_ROOT"] = "/home/bruno/dev/refactorPython3/build/lib"
 sys.path.append("./Sofa/package")
 sys.path.append("./SofaRuntime/package")
+sys.path.append("./SofaTypes/package")
 
 import Sofa
 import SofaRuntime
+import SofaTypes
 
+d = SofaTypes.Vec3d(1,2,3)
+print(d)
+print("COUCOU")
+print(dir(d))
+print(d.y)
+print(d.xyz)
+print("Done")
 ## Register all the common component in the factory. 
 #SofaRuntime.importPlugin("SofaAllCommonComponents")
 
 ## Init the simulation singleton. 
-SofaRuntime.reinit()
-SofaRuntime.load("Sofa/tests/pyfiles/ScriptController.py")
-SofaRuntime.init(rootNode)
 
         
--- modules/SofaPython3/runSimu.py
+++ modules/SofaPython3/runSimu.py
@@ -6,8 +6,9 @@ if "SOFA_ROOT" not in os.environ:
         print("WARNING: missing SOFA_ROOT in you environment variable. ") 
         sys.exit(-1)
 
-sys.path.append("./Sofa/package")
-sys.path.append("./SofaRuntime/package")
+sys.path.append(os.path.abspath("./bindings/Sofa/package"))
+sys.path.append(os.path.abspath("./bindings/SofaRuntime/package"))
+sys.path.append(os.path.abspath("./bindings/SofaTypes/package"))
 
 import Sofa
 import SofaRuntime
