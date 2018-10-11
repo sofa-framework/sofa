e554895ff7d1b19b16d80edbec04fe23d58c8503
fspadoni <spadoni.federico@gmail.com>
2018-10-11 17:11:42 +0200

    [MultiThreading] replace thread_specific_ptr with SOFA_THREAD_SPECIFIC_PTR macro

--- SofaKernel/framework/sofa/helper/AdvancedTimer.h
+++ SofaKernel/framework/sofa/helper/AdvancedTimer.h
@@ -188,10 +188,10 @@ public:
             /// return the instance of the factory. Creates it if doesn't exist yet.
             static AdvancedTimer::Id<Base>::IdFactory& getInstance()
             {
-                static sofa::helper::system::thread::thread_specific_ptr<AdvancedTimer::Id<Base>::IdFactory> instance;
+                SOFA_THREAD_SPECIFIC_PTR(IdFactory, instance);
                 if (instance == nullptr)
                 {
-                    instance = new AdvancedTimer::Id<Base>::IdFactory;
+                    instance = new IdFactory;
                 }
                 return *instance;
             }
