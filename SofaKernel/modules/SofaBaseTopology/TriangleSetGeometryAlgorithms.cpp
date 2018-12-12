286f13ecc7a1d6cba3e2a47d26d4c7617be17828
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-12-12 22:58:00 +0100

    [SofaKernel] Replace #include Vec3Types.h by VecTypes.h

--- SofaKernel/modules/SofaBaseTopology/TriangleSetGeometryAlgorithms.cpp
+++ SofaKernel/modules/SofaBaseTopology/TriangleSetGeometryAlgorithms.cpp
@@ -21,7 +21,7 @@
 ******************************************************************************/
 #define SOFA_COMPONENT_TOPOLOGY_TRIANGLESETGEOMETRYALGORITHMS_CPP
 #include <SofaBaseTopology/TriangleSetGeometryAlgorithms.inl>
-#include <sofa/defaulttype/Vec3Types.h>
+#include <sofa/defaulttype/VecTypes.h>
 #include <sofa/core/ObjectFactory.h>
 namespace sofa
 {
