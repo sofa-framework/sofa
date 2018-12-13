ab5b102e1d2b7604083f93a40892ac25a3099003
erik pernod <erik.pernod@gmail.com>
2018-12-13 17:18:39 +0100

    [SofaBaseTopology] add alias vertices to seqPoints in sparseGridTopology for backward compatibility and change Data name in mergePoints and subsetTopoloyg scene to fix them.

--- SofaKernel/modules/SofaBaseTopology/SparseGridTopology.cpp
+++ SofaKernel/modules/SofaBaseTopology/SparseGridTopology.cpp
@@ -103,6 +103,8 @@ SparseGridTopology::SparseGridTopology(bool _isVirtual)
     _coarserSparseGrid = NULL;
     _usingMC = false;
 
+    addAlias(&seqPoints, "vertices");
+
     _regularGrid = sofa::core::objectmodel::New<RegularGridTopology>();
 
 }
@@ -111,7 +113,8 @@ SparseGridTopology::SparseGridTopology(Vec3i numVertices, BoundingBox box, bool 
     : SparseGridTopology(_isVirtual)
 {
     //Add alias to use MeshLoader
-    
+    addAlias(&seqPoints, "vertices");
+
     setN(numVertices);
     setMin(box.minBBox());
     setMax(box.maxBBox());
