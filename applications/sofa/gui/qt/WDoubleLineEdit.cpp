b691ef7031bc9895f47b4f54d361b3441a734024
Guillaume Paran <guillaume.paran@sofa-framework.org>
2018-12-19 10:53:51 +0100

    [SofaGui] FIX WDoubleLineEdit license (2)

--- applications/sofa/gui/qt/WDoubleLineEdit.cpp
+++ applications/sofa/gui/qt/WDoubleLineEdit.cpp
@@ -12,8 +12,8 @@
 * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    *
 * more details.                                                               *
 *                                                                             *
-* You should have received a copy of the GNU Lesser General Public License    *
-* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+* You should have received a copy of the GNU General Public License along     *
+* with this program. If not, see <http://www.gnu.org/licenses/>.              *
 *******************************************************************************
 * Authors: The SOFA Team and external contributors (see Authors.txt)          *
 *                                                                             *
