590d6eb26309e93b4580fdd46fd580c49134849a
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-12-19 09:06:51 +0100

    [SceneCreator] Fix ambiguous Vec3 in SceneCreator.h

--- applications/plugins/SceneCreator/SceneCreator.h
+++ applications/plugins/SceneCreator/SceneCreator.h
@@ -39,8 +39,8 @@ using sofa::core::objectmodel::BaseObject ;
 
 typedef SReal Scalar;
 typedef sofa::defaulttype::Vec3Types::Deriv Deriv3;
-typedef sofa::defaulttype::Vec<3,SReal> Vec3;
-typedef sofa::defaulttype::Vec<1,SReal> Vec1;
+using sofa::defaulttype::Vec3;
+using sofa::defaulttype::Vec1;
 
 typedef enum
 {
