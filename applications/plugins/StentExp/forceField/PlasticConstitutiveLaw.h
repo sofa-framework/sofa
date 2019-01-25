bfae4751a605d7349233a26e851b7864e38af9cb
Camille <camille.krewcun@uca.fr>
2019-01-25 15:06:21 +0100

    [StentExp] ADD 1D plastic constitutive law model
    To prepare the implementation of (isotropic) hardening, we need to be
    able to compute the tangent modulus at eah time step. This requires the
    derivation of the 1D plastic (non-linear) constitutive law associated to
    the simulated material (in our case, for instance, the L605
    cobalt-chromium alloy).
    As the exact expression of the relation between stress and strain is
    higly specific, the idea is to use a classic model describing the 1D
    stress-strain curve, and fit the parameters of this model on
    experimental data. The first model we try is the Ramberg-Osgood model.
    
    From the implementation point of view, we define a virtual class
    PlasticConstitutiveLaw, from which should inherit every concrete model
    to be tested. As a first example, we implement the Ramberg-Osgood model
    in this way.
    
    An object of type PlasticConstitutiveLaw should then be used in the
    Force Field to implement a hardening behaviour. This should be the
    purpose of the next commit.

---
+++ applications/plugins/StentExp/forceField/PlasticConstitutiveLaw.h
@@ -0,0 +1,83 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-2018 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-2018 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef SOFA_COMPONENT_FEM_PLASTICCONSTITUTIVELAW_H
+#define SOFA_COMPONENT_FEM_PLASTICCONSTITUTIVELAW_H
+
+#include "../config.h"
+#include <Eigen/Core>
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace fem
+{
+
+template<class DataTypes>
+class PlasticConstitutiveLaw
+{
+public:
+
+    typedef typename DataTypes::Coord Coord;
+    typedef typename Coord::value_type Real;
+    typedef Eigen::Matrix<double, 6, 1> VoigtTensor;
+
+    virtual ~PlasticConstitutiveLaw() {}
+
+
+    /** returns the strain energy of the current configuration */
+    virtual Real getTangentModulus(const double yieldStress) {
+        return 0;
+    }
+
+};
+
+} // namespace fem
+
+} // namespace component
+
+} // namespace sofa
+
+#endif // ifndef SOFA_COMPONENT_FEM_PLASTICCONSTITUTIVELAW_H
---
+++ applications/plugins/StentExp/forceField/RambergOsgood.h
@@ -0,0 +1,84 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-2018 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef SOFA_COMPONENT_FEM_RAMBERGOSGOOD_H
+#define SOFA_COMPONENT_FEM_RAMBERGOSGOOD_H
+#include "../config.h"
+
+#include "PlasticConstitutiveLaw.h"
+#include <sofa/defaulttype/Vec.h>
+#include <sofa/defaulttype/Mat.h>
+
+
+namespace sofa
+{
+
+namespace component
+{
+
+namespace fem
+{
+
+template<class DataTypes>
+class RambergOsgood : public PlasticConstitutiveLaw<DataTypes> {
+
+public:
+
+    typedef typename DataTypes::Coord::value_type Real;
+    typedef defaulttype::Mat<3, 3, Real> Matrix3;
+    typedef defaulttype::Mat<6, 6, Real> Matrix6;
+
+    RambergOsgood(Real E, Real yieldStress, unsigned int n=10, Real A=(Real)0.002)
+    {
+        _A = A;
+        _n = n;
+        _E = E;
+        _yieldStress = yieldStress;
+
+        // Computing _K
+        _K = A * pow(E / yieldStress, n);
+    }
+
+    virtual Real getTangentModulus(const double yieldStress) {
+
+        double En1 = pow(_E, _n - 1);
+        Real tangentModulus = (_E*En1) / ( En1 + _K*_n*pow(yieldStress,_n) );
+        return tangentModulus;
+    }
+
+protected:
+
+    Real _A;
+    Real _K;
+    unsigned int _n;
+    Real _E;
+    Real _yieldStress;
+
+};
+
+
+} // namespace fem
+
+} // namespace component
+
+} // namespace sofa
+
+#endif // ifndef SOFA_COMPONENT_FEM_RAMBERGOSGOOD_H
