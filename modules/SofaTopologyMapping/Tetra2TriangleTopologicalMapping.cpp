1d242f7d31b81d9158854f1a5e16521b96511b07
epernod <erik.pernod@gmail.com>
2019-03-26 23:31:59 +0100

    Revert "[SofaTopologyMapping] FIX: too complicated and wrong algo to get triangle inbetween tetrahedron created."
    This reverts commit c1cf977d204e83cf2decf22bbc69bd5ac6ef738b.

--- modules/SofaTopologyMapping/Tetra2TriangleTopologicalMapping.cpp
+++ modules/SofaTopologyMapping/Tetra2TriangleTopologicalMapping.cpp
@@ -382,7 +382,18 @@ void Tetra2TriangleTopologicalMapping::updateTopologicalMappingTopDown()
                     if (is_present) // already done, continue.
                         continue;
 
-                    core::topology::BaseMeshTopology::Triangle tri = fromModel->getTriangle(triangleId);
+                    core::topology::BaseMeshTopology::Triangle tri;
+                    const core::topology::BaseMeshTopology::Tetrahedron& otherTetra = tetrahedronArray[idOtherTetra];
+                    const BaseMeshTopology::TrianglesInTetrahedron& triInOtherTetra = fromModel->getTrianglesInTetrahedron(idOtherTetra);
+
+                    int posInTetra = fromModel->getTriangleIndexInTetrahedron(triInOtherTetra, triangleId);
+
+                    for (int i=0; i<3; i++)
+                    {
+                        unsigned int vIdInTetra = trianglesInTetrahedronArray[posInTetra][i];
+                        tri[i] = otherTetra[vIdInTetra];
+                    }
+
                     if(flipN)
                     {
                         unsigned int temp=tri[2];
