7467419382fb2c1533a73a116088cb9ada2f2d52
Bruno Marques <bmarques68@gmail.com>
2019-04-08 11:58:17 +0200

    Removing virtual keyword from GetObjectsCallBack dtor

--- SofaKernel/framework/sofa/core/objectmodel/BaseContext.h
+++ SofaKernel/framework/sofa/core/objectmodel/BaseContext.h
@@ -168,7 +168,7 @@ public:
     class GetObjectsCallBack
     {
     public:
-        virtual ~GetObjectsCallBack();
+        ~GetObjectsCallBack();
         virtual void operator()(void* ptr) = 0;
     };
 
