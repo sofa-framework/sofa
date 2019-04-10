400dd27d8b31e440de1ac09a9bc51220964f7b8d
Damien Marchal <damien.marchal@univ-lille1.fr>
2019-04-10 22:58:22 +0200

    [SofaGraphComponent] Remove SceneCheckAPI (for demos)

--- modules/SofaGraphComponent/SceneCheckerListener.cpp
+++ modules/SofaGraphComponent/SceneCheckerListener.cpp
@@ -40,7 +40,7 @@ namespace _scenechecking_
 
 SceneCheckerListener::SceneCheckerListener()
 {
-    m_sceneChecker.addCheck(SceneCheckAPIChange::newSPtr());
+    //m_sceneChecker.addCheck(SceneCheckAPIChange::newSPtr());
     m_sceneChecker.addCheck(SceneCheckDuplicatedName::newSPtr());
     m_sceneChecker.addCheck(SceneCheckMissingRequiredPlugin::newSPtr());
     m_sceneChecker.addCheck(SceneCheckUsingAlias::newSPtr());
