1fba327c72738cde95071c6bf07564b6541bbb02
epernod <erik.pernod@gmail.com>
2019-04-15 12:18:32 +0200

    [SofaKernel] Add QuadsOrientationInHexahedron array as static member.

--- SofaKernel/framework/sofa/core/topology/BaseMeshTopology.h
+++ SofaKernel/framework/sofa/core/topology/BaseMeshTopology.h
@@ -335,7 +335,8 @@ public:
 };
 
 // map of triangles orientation inside a tetrahedron.
-static const unsigned int trianglesOrientationInTetrahedronArray[4][3]= {{1,2,3}, {0,3,2}, {1,3,0},{0,2,1}};
+static const unsigned int trianglesOrientationInTetrahedronArray[4][3]= {{1,2,3}, {0,3,2}, {1,3,0}, {0,2,1}};
+static const unsigned int quadsOrientationInHexahedronArray[6][4]= {{0,3,2,1}, {4,5,6,7}, {0,1,5,4}, {1,2,6,5}, {2,3,7,6}, {3,0,4,7}};
 
 } // namespace topology
 
