8a09affc2c3944cbdf5bbc1f78c764389bf0a261
Hugo <hugo.talbot@sofa-framework.org>
2019-04-24 10:36:40 +0200

    Update CudaStandardTetrahedralFEMForceField.cu
    Add a comment

--- applications/plugins/SofaCUDA/sofa/gpu/cuda/CudaStandardTetrahedralFEMForceField.cu
+++ applications/plugins/SofaCUDA/sofa/gpu/cuda/CudaStandardTetrahedralFEMForceField.cu
@@ -73,12 +73,15 @@ void StandardTetrahedralFEMForceFieldCuda3d_addDForce(int nbTetra, int nbEdges, 
 // GPU-side methods //
 //////////////////////
 
+/// Declaration of two functions, implemented below
 template <class real>
 __device__ void StandardTetrahedralFEMForceFieldCuda_BoyceAndArruda_deriveSPKTensor(int tetraIndex, real* tetraInfo, float paramArray0, float paramArray1);
 
 template <class real>
 __device__ void StandardTetrahedralFEMForceFieldCuda_BoyceAndArruda_ElasticityTensor(int tetraIndex, real* tetraInfo, real outputTensor[][6], real param0, real param1);
 
+	
+	
 template <class real>
 __device__ real dot(real* A, real* B, int n)
 {
