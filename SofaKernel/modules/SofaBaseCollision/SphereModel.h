6aeb7ce24e2617427d4e75828ef2d24cfe22c33d
hugtalbot <hugo.talbot@sofa-framework.org>
2019-05-10 18:29:38 +0200

    Rename TLineModel in LineCollisionModel

--- SofaKernel/modules/SofaBaseCollision/SphereModel.h
+++ SofaKernel/modules/SofaBaseCollision/SphereModel.h
@@ -38,20 +38,20 @@ namespace collision
 
 
 template<class DataTypes>
-class TSphereModel;
+class SphereCollisionModel;
 
 //template <class TDataTypes>
 //class TSphere;
 
 template<class TDataTypes>
-class TSphere : public core::TCollisionElementIterator< TSphereModel<TDataTypes> >
+class TSphere : public core::TCollisionElementIterator< SphereCollisionModel<TDataTypes> >
 {
 public:
     typedef TDataTypes DataTypes;
     typedef typename DataTypes::Real   Real;
     typedef typename TDataTypes::CPos Coord;
 
-    typedef TSphereModel<DataTypes> ParentModel;
+    typedef SphereCollisionModel<DataTypes> ParentModel;
 
     TSphere(ParentModel* model, int index);
 
@@ -88,10 +88,10 @@ sofa::defaulttype::Vector3 TSphere<defaulttype::Vec3Types >::getContactPointWith
 
 
 template< class TDataTypes>
-class TSphereModel : public core::CollisionModel
+class SphereCollisionModel : public core::CollisionModel
 {
 public:
-    SOFA_CLASS(SOFA_TEMPLATE(TSphereModel, TDataTypes), core::CollisionModel);
+    SOFA_CLASS(SOFA_TEMPLATE(SphereCollisionModel, TDataTypes), core::CollisionModel);
 
     typedef TDataTypes DataTypes;
     typedef DataTypes InDataTypes;
@@ -103,9 +103,9 @@ public:
     typedef TSphere<DataTypes> Element;
     friend class TSphere<DataTypes>;
 protected:
-    TSphereModel();
+    SphereCollisionModel();
 
-    TSphereModel(core::behavior::MechanicalState<TDataTypes>* _mstate );
+    SphereCollisionModel(core::behavior::MechanicalState<TDataTypes>* _mstate );
 public:
     void init() override;
 
@@ -169,7 +169,7 @@ public:
         return templateName(this);
     }
 
-    static std::string templateName(const TSphereModel<DataTypes>* = NULL)
+    static std::string templateName(const SphereCollisionModel<DataTypes>* = NULL)
     {
         return DataTypes::Name();
     }
@@ -212,7 +212,7 @@ template<class DataTypes>
 inline const typename TSphere<DataTypes>::Coord& TSphere<DataTypes>::pFree() const { return (*this->model->mstate->read(core::ConstVecCoordId::freePosition())).getValue()[this->index]; }
 
 template<class DataTypes>
-inline const typename TSphereModel<DataTypes>::Coord& TSphereModel<DataTypes>::velocity(int index) const { return DataTypes::getDPos(mstate->read(core::ConstVecDerivId::velocity())->getValue()[index]);}
+inline const typename SphereCollisionModel<DataTypes>::Coord& SphereCollisionModel<DataTypes>::velocity(int index) const { return DataTypes::getDPos(mstate->read(core::ConstVecDerivId::velocity())->getValue()[index]);}
 
 template<class DataTypes>
 inline const typename TSphere<DataTypes>::Coord& TSphere<DataTypes>::v() const { return DataTypes::getDPos(this->model->mstate->read(core::ConstVecDerivId::velocity())->getValue()[this->index]); }
@@ -224,16 +224,16 @@ template<class DataTypes>
 inline bool TSphere<DataTypes>::hasFreePosition() const { return this->model->mstate->read(core::ConstVecCoordId::freePosition())->isSet(); }
 
 
-typedef TSphereModel<sofa::defaulttype::Vec3Types> SphereModel;
+typedef SphereCollisionModel<sofa::defaulttype::Vec3Types> SphereModel;
 typedef TSphere<sofa::defaulttype::Vec3Types> Sphere;
 
-typedef TSphereModel<sofa::defaulttype::Rigid3Types> RigidSphereModel;
+typedef SphereCollisionModel<sofa::defaulttype::Rigid3Types> RigidSphereModel;
 typedef TSphere<sofa::defaulttype::Rigid3Types> RigidSphere;
 
 #if  !defined(SOFA_COMPONENT_COLLISION_SPHEREMODEL_CPP)
 extern template class SOFA_BASE_COLLISION_API TSphere<defaulttype::Vec3Types>;
-extern template class SOFA_BASE_COLLISION_API TSphereModel<defaulttype::Vec3Types>;
-extern template class SOFA_BASE_COLLISION_API TSphereModel<defaulttype::Rigid3Types>;
+extern template class SOFA_BASE_COLLISION_API SphereCollisionModel<defaulttype::Vec3Types>;
+extern template class SOFA_BASE_COLLISION_API SphereCollisionModel<defaulttype::Rigid3Types>;
 
 #endif
 
