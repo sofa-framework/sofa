d46cbb4399750b92f7d4daf7f73fce7b3b93509d
Frédérick ROY <frederick.roy@inria.fr>
2019-05-12 02:29:33 +0200

    Add macro for windows

--- applications/plugins/SofaPython/PythonScriptControllerHelper.h
+++ applications/plugins/SofaPython/PythonScriptControllerHelper.h
@@ -54,7 +54,7 @@ SOFA_SOFAPYTHON_API void PythonScriptController_pyObjectToValue(PyObject* pyObje
 SOFA_SOFAPYTHON_API void PythonScriptController_pyObjectToValue(PyObject* pyObject, std::string & val);
 
 
-void PythonScriptController_parametersToVector(std::vector<PyObject*> & /*vecParam*/);
+SOFA_SOFAPYTHON_API void PythonScriptController_parametersToVector(std::vector<PyObject*> & /*vecParam*/);
 
 template<typename T, typename... ParametersType>
 void PythonScriptController_parametersToVector(std::vector<PyObject*> & vecParam, T param, ParametersType... otherParameters)
