1a96cb708db8fd3f12b77d8f027799193364dafa
Damien Marchal <damien.marchal@univ-lille1.fr>
2019-06-01 08:30:31 +0200

    [SofaOpenglVisual] Move definitions from MergeVisualModel.h to the .cpp

--- modules/SofaOpenglVisual/src/SofaOpenglVisual/MergeVisualModels.cpp
+++ modules/SofaOpenglVisual/src/SofaOpenglVisual/MergeVisualModels.cpp
@@ -31,9 +31,235 @@ namespace component
 namespace visualmodel
 {
 
-int MergeVisualModelsClass = core::RegisterObject("Merge several visual models")
+static int MergeVisualModelsClass = core::RegisterObject("Merge several visual models")
         .add< MergeVisualModels >(true);
 
+MergeVisualModels::MergeVisualModels()
+    : d_nbInput( initData (&d_nbInput, (unsigned)1, "nb", "number of input visual models to merge") )
+    , vl_input(this,"input", "input visual model")
+{
+    vl_input.resize(d_nbInput.getValue());
+}
+
+MergeVisualModels::~MergeVisualModels() {}
+
+void MergeVisualModels::update()
+{
+    unsigned int nb = d_nbInput.getValue();
+
+    unsigned int nbpos = 0, nbvert = 0, nbedges = 0, nbtris = 0, nbquads = 0;
+    for (unsigned int i=0; i<nb; ++i)
+    {
+        nbpos += (*vl_input[i])->m_positions.getValue().size();
+        nbvert += (*vl_input[i])->m_vertices2.getValue().size();
+        nbedges += (*vl_input[i])->m_edges.getValue().size();
+        nbtris += (*vl_input[i])->m_triangles.getValue().size();
+        nbquads += (*vl_input[i])->m_quads.getValue().size();
+    }
+
+
+    sofa::defaulttype::ResizableExtVector<Coord>& pos = *this->m_positions.beginWriteOnly();
+    pos.resize( nbpos );
+
+    {
+        unsigned offset = 0;
+        for (unsigned int i=0; i<nb; ++i)
+        {
+            const sofa::defaulttype::ResizableExtVector<Coord>& in = (*vl_input[i])->m_positions.getValue();
+            std::copy( in.begin(), in.end(), pos.begin()+offset );
+            offset += in.size();
+        }
+        this->m_positions.endEdit();
+    }
+
+
+
+
+    {
+        sofa::defaulttype::ResizableExtVector<Coord>& vert = *this->m_vertices2.beginWriteOnly();
+        vert.resize( nbvert );
+
+        unsigned offset = 0;
+        for (unsigned int i=0; i<nb; ++i)
+        {
+            const sofa::defaulttype::ResizableExtVector<Coord>& in = (*vl_input[i])->m_vertices2.getValue();
+            std::copy( in.begin(), in.end(), vert.begin()+offset );
+            offset += in.size();
+        }
+        this->m_vertices2.endEdit();
+    }
+
+
+
+    {
+        sofa::defaulttype::ResizableExtVector<int>& vertIdx = *this->m_vertPosIdx.beginWriteOnly();
+        vertIdx.resize( nbvert );
+
+        unsigned offset = 0;
+        unsigned offsetIdx = 0;
+        for (unsigned int i=0; i<nb; ++i)
+        {
+            const sofa::defaulttype::ResizableExtVector<int>& in = (*vl_input[i])->m_vertPosIdx.getValue();
+
+            for( size_t j=0;j<in.size();++j)
+            {
+                int& e = vertIdx[offset+j];
+                e = in[j];
+                e += offsetIdx;
+            }
+
+            offset += in.size();
+            offsetIdx += (*vl_input[i])->m_positions.getValue().size();
+        }
+        this->m_vertPosIdx.endEdit();
+    }
+
+    {
+        sofa::defaulttype::ResizableExtVector<int>& vertIdx = *this->m_vertNormIdx.beginWriteOnly();
+        vertIdx.resize( nbvert );
+
+        unsigned offset = 0;
+        unsigned offsetIdx = 0;
+        for (unsigned int i=0; i<nb; ++i)
+        {
+            const sofa::defaulttype::ResizableExtVector<int>& in = (*vl_input[i])->m_vertNormIdx.getValue();
+
+            for( size_t j=0;j<in.size();++j)
+            {
+                int& e = vertIdx[offset+j];
+                e = in[j];
+                e += offsetIdx;
+            }
+
+            offset += in.size();
+            offsetIdx += (*vl_input[i])->m_positions.getValue().size();
+        }
+        this->m_vertNormIdx.endEdit();
+    }
+
+
+
+    {
+        VecTexCoord& vert = *this->m_vtexcoords.beginWriteOnly();
+        vert.resize( nbvert );
+
+        unsigned offset = 0;
+        for (unsigned int i=0; i<nb; ++i)
+        {
+            const VecTexCoord& in = (*vl_input[i])->m_vtexcoords.getValue();
+            std::copy( in.begin(), in.end(), vert.begin()+offset );
+            offset += in.size();
+        }
+        this->m_vtexcoords.endEdit();
+    }
+
+
+
+
+    unsigned offsetPoint = 0;
+
+
+
+    sofa::defaulttype::ResizableExtVector<Edge>& edges = *this->m_edges.beginWriteOnly();
+    edges.resize( nbedges );
+
+    unsigned offsetEdge = 0;
+    for (unsigned int i=0; i<nb; ++i)
+    {
+        const sofa::defaulttype::ResizableExtVector<Edge>& in = (*vl_input[i])->m_edges.getValue();
+
+        for( size_t j=0;j<in.size();++j)
+        {
+            Edge& e = edges[offsetEdge+j];
+            e = in[j];
+            e[0] += offsetPoint;
+            e[1] += offsetPoint;
+        }
+
+        offsetEdge += in.size();
+        offsetPoint += (*vl_input[i])->m_vertices2.getValue().size();
+    }
+    this->m_edges.endEdit();
+
+
+
+
+
+    sofa::defaulttype::ResizableExtVector<Triangle>& tris = *this->m_triangles.beginWriteOnly();
+    tris.resize( nbtris );
+
+    offsetPoint = 0;
+    unsigned offsetTri = 0;
+    for (unsigned int i=0; i<nb; ++i)
+    {
+        const sofa::defaulttype::ResizableExtVector<Triangle>& in = (*vl_input[i])->m_triangles.getValue();
+
+        for( size_t j=0;j<in.size();++j)
+        {
+            Triangle& t = tris[offsetTri+j];
+            t = in[j];
+            t[0] += offsetPoint;
+            t[1] += offsetPoint;
+            t[2] += offsetPoint;
+        }
+
+        offsetTri += in.size();
+        offsetPoint += (*vl_input[i])->m_vertices2.getValue().size();
+    }
+    this->m_triangles.endEdit();
+
+
+
+    sofa::defaulttype::ResizableExtVector<Quad>& quads = *this->m_quads.beginWriteOnly();
+    quads.resize( nbquads );
+
+    offsetPoint = 0;
+    unsigned offsetQuad = 0;
+    for (unsigned int i=0; i<nb; ++i)
+    {
+        const sofa::defaulttype::ResizableExtVector<Quad>& in = (*vl_input[i])->m_quads.getValue();
+
+        for( size_t j=0;j<in.size();++j)
+        {
+            Quad& q = quads[offsetQuad+j];
+            q = in[j];
+            q[0] += offsetPoint;
+            q[1] += offsetPoint;
+            q[2] += offsetPoint;
+            q[3] += offsetPoint;
+        }
+
+        offsetQuad += in.size();
+        offsetPoint += (*vl_input[i])->m_vertices2.getValue().size();
+    }
+    this->m_quads.endEdit();
+}
+
+void MergeVisualModels::parse ( sofa::core::objectmodel::BaseObjectDescription* arg )
+{
+    vl_input.parseSizeLinks(arg, d_nbInput);
+    Inherit1::parse(arg);
+}
+
+void MergeVisualModels::parseFields ( const std::map<std::string,std::string*>& str )
+{
+    vl_input.parseFieldsSizeLinks(str, d_nbInput);
+    Inherit1::parseFields(str);
+}
+
+void MergeVisualModels::init()
+{
+    vl_input.resize(d_nbInput.getValue());
+    Inherit1::init();
+    update();
+}
+
+void MergeVisualModels::reinit()
+{
+    vl_input.resize(d_nbInput.getValue());
+    Inherit1::reinit();
+    update();
+}
 
 
 } // namespace visualmodel
