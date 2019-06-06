243010d27bca8931a3bc229b301453daabe12eff
Antonin Bernardin <antonin.bernardin@inria.fr>
2019-06-06 13:47:40 +0200

    Fix missing input parameter in call of Simulation::load(...)

--- applications/projects/runSofa/Main.cpp
+++ applications/projects/runSofa/Main.cpp
@@ -424,7 +424,7 @@ int main(int argc, char** argv)
     }
 
     const std::vector<std::string> sceneArgs = sofa::helper::ArgumentParser::extra_args();
-    Node::SPtr groot = sofa::simulation::getSimulation()->load(fileName, sceneArgs);
+    Node::SPtr groot = sofa::simulation::getSimulation()->load(fileName, false, sceneArgs);
     if( !groot )
         groot = sofa::simulation::getSimulation()->createNewGraph("");
 
