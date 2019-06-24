sofa-framework/sofa

commit 48395d837474beb67ca4d9721ebb8b7361089fe3

[runSofa] Improve the rendering of the data tooltips.
The same text is now use for both the label and the inner data widget which
make it much easier to read.

The new tooltips are following the following pattern:
"""
This is an help text
Data type: float
Owner: BaseObject
"""

Enjoy

applications/sofa/gui/qt/QDisplayDataWidget.cpp
@@ -63,19 +63,20 @@ QDisplayDataWidget::QDisplayDataWidget(QWidget* parent,
     if(data_ == nullptr)
         return;
 
-    const char* help_text = data_->getHelp();
-    const std::string label_text = help_text == NULL ? "" : help_text;
+    const std::string valuetype = data_->getValueTypeString();
+    std::stringstream s;
 
-    if (label_text != "TODO")
-    {
-        datainfowidget_ = new QDisplayDataInfoWidget(this,label_text,data_,flags.LINKPATH_MODIFIABLE_FLAG, flags_);
-        datainfowidget_->setContentsMargins(0, 0, 0, 0);
-        datainfowidget_->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
-        gridLayout_->addWidget(datainfowidget_, 0,0);
-        numWidgets_ += 1;
-    }
+    s << data_->getHelp()<< "\nData type: " << valuetype
+                         << "\nOwner: "<<data_->getOwnerClass() ;
+    const std::string fullHelpText = s.str();
+    setToolTip(fullHelpText.c_str());
+    datainfowidget_ = new QDisplayDataInfoWidget(this,fullHelpText,data_,
+                                                 flags.LINKPATH_MODIFIABLE_FLAG, flags_);
+    datainfowidget_->setContentsMargins(0, 0, 0, 0);
+    datainfowidget_->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+    gridLayout_->addWidget(datainfowidget_, 0,0);
+    numWidgets_ += 1;
 
-    setToolTip(data_->getHelp());
 
     DataWidget::CreatorArgument dwarg;
     dwarg.name =  data_->getName();
@@ -114,9 +115,8 @@ QDisplayDataWidget::QDisplayDataWidget(QWidget* parent,
     datawidget_->setContentsMargins(0, 16, 0, 0);
     datawidget_->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
     this->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
-    const std::string valuetype = data_->getValueTypeString();
-    if (!valuetype.empty())
-        datawidget_->setToolTip(valuetype.c_str());
+
+    datawidget_->setToolTip(s.str().c_str());
 
     numWidgets_ += datawidget_->sizeWidget();
     connect(datawidget_,SIGNAL(WidgetDirty(bool)), this, SIGNAL ( WidgetDirty(bool) ) );

commit 0e4bf69c931a099623448c5626099416cfee21c0

[SofaGuiQt] FIX tooltips segfault
introduced by 48395d837474beb67ca4d9721ebb8b7361089fe3

applications/sofa/gui/qt/QDisplayDataWidget.cpp
@@ -63,11 +63,15 @@ QDisplayDataWidget::QDisplayDataWidget(QWidget* parent,
     if(data_ == nullptr)
         return;
 
+    const char* help = data_->getHelp();
     const std::string valuetype = data_->getValueTypeString();
+    const char* ownerClass = data_->getOwnerClass();
     std::stringstream s;
 
-    s << data_->getHelp()<< "\nData type: " << valuetype
-                         << "\nOwner: "<<data_->getOwnerClass() ;
+    s << (help ? help : "< No help found >")
+      << "\nData type: " << valuetype
+      << "\nOwner: " << (ownerClass ? ownerClass : "< No owner found >");
+
     const std::string fullHelpText = s.str();
     setToolTip(fullHelpText.c_str());
     datainfowidget_ = new QDisplayDataInfoWidget(this,fullHelpText,data_,