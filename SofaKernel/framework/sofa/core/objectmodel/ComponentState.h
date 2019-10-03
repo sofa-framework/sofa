c3a8dbfe05cecf4317f326216d429aad7289bfdb
Damien Marchal <damien.marchal@univ-lille1.fr>
2019-10-03 10:06:41 +0200

    [SofaKernel] Add the file ComponentState.*

---
+++ SofaKernel/framework/sofa/core/objectmodel/ComponentState.h
@@ -0,0 +1,44 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-2019 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#pragma once
+
+#include <iostream>
+
+namespace sofa::core::objectmodel
+{
+
+/// enum class is a C++ x11 feature (http://en.cppreference.com/w/cpp/language/enum),
+/// Indicate the state of a sofa object.
+enum class ComponentState {
+    Undefined,                        ///< component that does not make use of this field have this one
+    Loading,                          ///< the component is loading but never passed successfully its init() function
+    Valid,                            ///< the component has passed successfully its init function
+    Dirty,                            ///< the component is ready to be used but requires a call to reinit
+    Busy,                             ///< the component is doing "something", don't trust its values for doing your computation
+    Invalid                           ///< the component reached an error and is thus unable to behave normally.
+};
+
+/// Defining the in/ou operator for use of component status with Data<>
+std::ostream& operator<<(std::ostream& o, const ComponentState& s);
+std::istream& operator>>(std::istream& i, ComponentState& s);
+
+} /// sofa::core::objectmodel
