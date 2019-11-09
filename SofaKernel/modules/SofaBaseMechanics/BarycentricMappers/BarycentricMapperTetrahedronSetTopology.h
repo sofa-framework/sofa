737c485de5eac91ee9af6afa5681f3dd632fdf13
erik pernod <erik.pernod@gmail.com>
2019-11-09 01:51:25 +0100

    [SofaKernel] Move code from BarycentricMapperTetrahedronSetTopology header to inl file.

--- SofaKernel/modules/SofaBaseMechanics/BarycentricMappers/BarycentricMapperTetrahedronSetTopology.h
+++ SofaKernel/modules/SofaBaseMechanics/BarycentricMappers/BarycentricMapperTetrahedronSetTopology.h
@@ -70,103 +70,9 @@ protected:
     void addPointInElement(const int elementIndex, const SReal* baryCoords) override;
 
     //handle topology changes depending on the topology
-    void processTopologicalChanges(const typename Out::VecCoord& out, const typename In::VecCoord& in, core::topology::Topology* t) {
-        using sofa::core::behavior::MechanicalState;
-        
-        if (t != m_toTopology) return;
+    void processTopologicalChanges(const typename Out::VecCoord& out, const typename In::VecCoord& in, core::topology::Topology* t);
 
-        if ( m_toTopology->beginChange() == m_toTopology->endChange() )
-            return;
-
-        auto itBegin = m_toTopology->beginChange();
-        auto itEnd = m_toTopology->endChange();
-
-        helper::WriteAccessor < Data< helper::vector<MappingData > > > vectorData = d_map;
-        vectorData.resize (out.size());
-
-        for (auto changeIt = itBegin; changeIt != itEnd; ++changeIt ) {
-            const core::topology::TopologyChangeType changeType = ( *changeIt )->getChangeType();
-            switch ( changeType )
-            {
-            //TODO: implementation of BarycentricMapperHexahedronSetTopology<In,Out>::handleTopologyChange()
-            case core::topology::POINTSINDICESSWAP:  ///< For PointsIndicesSwap.
-            {
-                const core::topology::PointsIndicesSwap * ps = static_cast<const core::topology::PointsIndicesSwap*>(*changeIt);
-                MappingData copy = vectorData[ps->index[0]];
-                vectorData[ps->index[0]] = vectorData[ps->index[1]];
-                vectorData[ps->index[1]] = copy;
-                break;
-            }
-            case core::topology::POINTSADDED:        ///< For PointsAdded.
-            {
-                const core::topology::PointsAdded * pa = static_cast<const core::topology::PointsAdded*>(*changeIt);
-                auto& array = pa->getElementArray();
-
-                for (unsigned i=0;i<array.size();i++) {
-                    unsigned pid = array[i];
-                    processAddPoint(Out::getCPos(out[pid]),
-                                    in,
-                                    vectorData[pid]);
-                }
-
-                break;
-            }
-            case core::topology::POINTSREMOVED:      ///< For PointsRemoved.
-            {
-                // nothing to do
-                break;
-            }
-            case core::topology::POINTSRENUMBERING:  ///< For PointsRenumbering.
-            {
-                const core::topology::PointsRenumbering * pr = static_cast<const core::topology::PointsRenumbering*>(*changeIt);
-                auto& array = pr->getIndexArray();
-                auto& inv_array = pr->getinv_IndexArray();
-                for (unsigned i=0;i<array.size();i++) {
-                    MappingData copy = vectorData[array[i]];
-                    vectorData[inv_array[i]] = vectorData[array[i]];
-                    vectorData[array[i]] = copy;
-                }
-                break;
-            }
-            default:
-            {
-                break;
-            }
-            }
-        }
-    }
-
-    void processAddPoint(const sofa::defaulttype::Vec3d & pos, const typename In::VecCoord& in, MappingData & vectorData){
-        const sofa::helper::vector<core::topology::BaseMeshTopology::Tetrahedron>& tetrahedra = this->m_fromTopology->getTetrahedra();
-
-        sofa::defaulttype::Vector3 coefs;
-        int index = -1;
-        double distance = std::numeric_limits<double>::max();
-        for ( unsigned int t = 0; t < tetrahedra.size(); t++ )
-        {
-            sofa::defaulttype::Mat3x3d base;
-            sofa::defaulttype::Vector3 center;
-            computeBase(base,in,tetrahedra[t]);
-            computeCenter(center,in,tetrahedra[t]);
-
-            sofa::defaulttype::Vec3d v = base * ( pos - in[tetrahedra[t][0]] );
-            double d = std::max ( std::max ( -v[0],-v[1] ),std::max ( -v[2],v[0]+v[1]+v[2]-1 ) );
-
-            if ( d>0 ) d = ( pos-center ).norm2();
-
-            if ( d<distance )
-            {
-                coefs = v;
-                distance = d;
-                index = t;
-            }
-        }
-
-        vectorData.in_index = index;
-        vectorData.baryCoords[0] = ( Real ) coefs[0];
-        vectorData.baryCoords[1] = ( Real ) coefs[1];
-        vectorData.baryCoords[2] = ( Real ) coefs[2];
-    }
+    void processAddPoint(const sofa::defaulttype::Vec3d & pos, const typename In::VecCoord& in, MappingData & vectorData);
 
     topology::TetrahedronSetTopologyContainer*      m_fromContainer {nullptr};
     topology::TetrahedronSetGeometryAlgorithms<In>*	m_fromGeomAlgo  {nullptr};
@@ -179,10 +85,12 @@ protected:
 
 #if !defined(SOFA_COMPONENT_MAPPING_BARYCENTRICMAPPERTETRAHEDRONSETTOPOLOGY_CPP)
 extern template class SOFA_BASE_MECHANICS_API BarycentricMapperTetrahedronSetTopology< Vec3dTypes, Vec3dTypes >;
-
-
 #endif
 
-}}}
+} // namespace mapping
 
-#endif
+} // namespace component
+
+} // namespace sofa
+
+#endif // SOFA_COMPONENT_MAPPING_BARYCENTRICMAPPERTETRAHEDRONSETTOPOLOGY_H
