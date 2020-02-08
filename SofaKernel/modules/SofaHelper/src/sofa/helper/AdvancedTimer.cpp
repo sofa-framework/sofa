6ac9227f8b8a4fc934725542cc5d393aef3679d1
Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
2020-02-08 10:28:51 +0100

    [SofaHelper] Add the obj id to labels when available

--- SofaKernel/modules/SofaHelper/src/sofa/helper/AdvancedTimer.cpp
+++ SofaKernel/modules/SofaHelper/src/sofa/helper/AdvancedTimer.cpp
@@ -1411,11 +1411,17 @@ helper::vector<Record> AdvancedTimer::getRecords(IdTimer id)
             case Record::RBEGIN: // Timer begins
             case Record::REND: // Timer ends
                 r.label = IdTimer::IdFactory::getName(r.id);
+                if (r.obj != 0 || (!IdObj::IdFactory::getName(r.obj).empty() && IdObj::IdFactory::getName(r.obj) != "0")) {
+                    r.label += " (" + IdObj::IdFactory::getName(r.obj) + ")";
+                }
                 break;
             case Record::RSTEP_BEGIN: // Step begins
             case Record::RSTEP_END: // Step ends
             case Record::RSTEP: // Step
                 r.label = IdStep::IdFactory::getName(r.id);
+                if (r.obj != 0 || (!IdObj::IdFactory::getName(r.obj).empty() && IdObj::IdFactory::getName(r.obj) != "0")) {
+                    r.label += " (" + IdObj::IdFactory::getName(r.obj) + ")";
+                }
                 break;
             case Record::RVAL_SET: // Sets a value
             case Record::RVAL_ADD: // Adds a value
