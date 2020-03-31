7b807d19bac510c7af894fd58f8b0b3686c756ff
Frederick Roy <fred.roy.me@gmail.com>
2020-03-31 20:34:57 +0200

    Modularize SofaMiscMapping

---
+++ modules/SofaMiscMapping/src/SofaMiscMapping/DeformableOnRigidFrameMapping.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-2019 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#define SOFA_COMPONENT_MAPPING_DEFORMABLEONRIGIDFRAMEMAPPING_CPP
+#include <SofaMiscMapping/DeformableOnRigidFrameMapping.inl>
+#include <sofa/core/ObjectFactory.h>
+
+namespace sofa::component::mapping
+{
+
+using namespace defaulttype;
+
+// Register in the Factory
+int DeformableOnRigidFrameMappingClass = core::RegisterObject("Set the positions and velocities of points attached to a rigid parent")
+        .add< DeformableOnRigidFrameMapping< Vec3dTypes, Rigid3Types, Vec3dTypes > >()
+
+        ;
+
+template class SOFA_SOFAMISCMAPPING_API DeformableOnRigidFrameMapping< Vec3dTypes, Rigid3Types, Vec3dTypes >;
+
+
+} // namespace sofa::component::mapping
