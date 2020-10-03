d3c40ec5e0adf6601cc5fd512977b6f6053ffe71
erik pernod <erik.pernod@gmail.com>
2020-10-03 02:08:44 +0200

    Fix bad merge

--- SofaKernel/modules/SofaCore/src/sofa/core/behavior/OdeSolver.h
+++ SofaKernel/modules/SofaCore/src/sofa/core/behavior/OdeSolver.h
@@ -61,7 +61,7 @@ public:
 protected:
     OdeSolver();
 
-    ~OdeSolver();
+    ~OdeSolver() override;
 	
 private:
 	OdeSolver(const OdeSolver& n) ;
@@ -85,7 +85,7 @@ public:
     ///
     /// pos_t and vel_t are the position and velocities at the begining of the time step
     /// the result is accumulated in Vecid::force()
-    virtual void computeResidual(const core::ExecParams* /*params*/, SReal /*dt*/, sofa::core::MultiVecCoordId /*pos_t*/, sofa::core::MultiVecDerivId /*vel_t*/) { serr << "ComputeResidual is not implemented in " << this->getName() << sendl; }
+    virtual void computeResidual(const core::ExecParams* /*params*/, SReal /*dt*/, sofa::core::MultiVecCoordId /*pos_t*/, sofa::core::MultiVecDerivId /*vel_t*/) { msg_error() << "ComputeResidual is not implemented in " << this->getName(); }
 
 
     /// Given an input derivative order (0 for position, 1 for velocity, 2 for acceleration),
@@ -105,13 +105,13 @@ public:
     /// The last column is returned by the getSolutionIntegrationFactor method.
     ///
     /// FF: What is the meaning of the parameters ?
-    virtual double getIntegrationFactor(int /*inputDerivative*/, int /*outputDerivative*/) const { serr<<"getIntegrationFactor not implemented !"<<sendl; return 0; }
+    virtual double getIntegrationFactor(int /*inputDerivative*/, int /*outputDerivative*/) const { msg_error() << "getIntegrationFactor not implemented !"; return 0; }
 
     /// Given a solution of the linear system,
     /// how much will it affect the output derivative of the given order.
     ///
     /// FF: What is the meaning of the parameters ?
-    virtual double getSolutionIntegrationFactor(int /*outputDerivative*/) const { serr<<"getSolutionIntegrationFactor not implemented !"<<sendl; return 0; }
+    virtual double getSolutionIntegrationFactor(int /*outputDerivative*/) const { msg_error() << "getSolutionIntegrationFactor not implemented !"; return 0; }
 
 
     /// Given the solution dx of the linear system inversion, how much will it affect the velocity
@@ -131,8 +131,8 @@ public:
     }
 
 
-    bool insertInNode( objectmodel::BaseNode* node );
-    bool removeInNode( objectmodel::BaseNode* node );
+    bool insertInNode( objectmodel::BaseNode* node ) override;
+    bool removeInNode( objectmodel::BaseNode* node ) override;
 
 };
 
