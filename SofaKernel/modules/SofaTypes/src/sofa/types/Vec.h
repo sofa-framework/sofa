ff6e660ee404ead3d37d2999ec1e2c7d720982bb
Frederick Roy <fred.roy.me@gmail.com>
2020-10-20 15:06:29 +0200

    update config.h.in file

--- SofaKernel/modules/SofaTypes/src/sofa/types/Vec.h
+++ SofaKernel/modules/SofaTypes/src/sofa/types/Vec.h
@@ -21,6 +21,8 @@
 ******************************************************************************/
 #pragma once
 
+#include <sofa/types/config.h>
+
 #include <sofa/helper/fixed_array.h>
 #include <sofa/helper/rmath.h>
 #include <sofa/defaulttype/DataTypeInfo.h>
