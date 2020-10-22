0e490e1dc20c4f7810102905fb7b849883045f7c
erik pernod <erik.pernod@gmail.com>
2020-10-23 01:45:57 +0200

    [compliant] Fix linux compilation

--- applications/plugins/Compliant/Compliant_test/WinchMultiMapping_test.cpp
+++ applications/plugins/Compliant/Compliant_test/WinchMultiMapping_test.cpp
@@ -48,31 +48,30 @@ struct WinchMultiMappingTest : public MultiMapping_test<Mapping>
 
     typedef WinchMultiMappingTest<Mapping> self;
     typedef MultiMapping_test<Mapping> base;
-
+    typedef typename self::OutVecCoord OutVecCoord;
+    typedef typename self::InVecCoord InVecCoord;
+    typedef typename helper::vector < InVecCoord> VecOfInVecCoord;
 
     bool test()
     {
         this->setupScene(2);
         Mapping* mapping = static_cast<Mapping*>(this->base::mapping);
 
-        //parent positions
-        helper::vector< self::InVecCoord > incoords(2);
+        VecOfInVecCoord incoords(2);
         for( int i=0; i<2; i++ )
         {
             incoords[i].resize(1);
-            incoords[i][0] = self::OutCoord(i+1.0) ;
-        }
-
+            incoords[i][0] = typename self::OutCoord(i + 1.0);
+        }            
+        
         // error
         mapping->factor.setValue(0.25);
-        typename self::OutVecCoord outcoords(1);  
-        outcoords[0] =  self::OutCoord(-0.5);
+        typename self::OutVecCoord outcoords(1);
+        outcoords[0] = typename self::OutCoord(-0.5);
 
         return this->runTest(incoords, outcoords);
 
     }
-
-
 };
 
 
