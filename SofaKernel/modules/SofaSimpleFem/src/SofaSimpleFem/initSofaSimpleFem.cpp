1ebd926b0e1f20f30cf8f8a64256ff4377d69aa4
Frederick Roy <fred.roy.me@gmail.com>
2020-11-07 17:05:54 +0100

    Fix typo (case sensitive)

--- SofaKernel/modules/SofaSimpleFem/src/SofaSimpleFem/initSofaSimpleFem.cpp
+++ SofaKernel/modules/SofaSimpleFem/src/SofaSimpleFem/initSofaSimpleFem.cpp
@@ -19,7 +19,7 @@
 *                                                                             *
 * Contact information: contact@sofa-framework.org                             *
 ******************************************************************************/
-#include <SofaSimpleFem/initSofaSimpleFEM.h>
+#include <SofaSimpleFem/initSofaSimpleFem.h>
 
 
 #include <sofa/core/ObjectFactory.h>
