1fbeb2fa517836d88bfe19f1984ccffb350fcd35
Frederick Roy <fred.roy.me@gmail.com>
2020-11-07 10:37:52 +0100

    fix onetetra tuto

--- applications/tutorials/oneTetrahedron/oneTetrahedron.cpp
+++ applications/tutorials/oneTetrahedron/oneTetrahedron.cpp
@@ -35,7 +35,6 @@ using VecCoord3 = sofa::helper::vector<Coord3>;
 
 #include <SofaBase/initSofaBase.h>
 #include <SofaCommon/initSofaCommon.h>
-#include <SofaGeneral/initSofaGeneral.h>
 
 #include <SofaBaseLinearSolver/CGLinearSolver.h>
 using CGLinearSolver = sofa::component::linearsolver::CGLinearSolver<sofa::component::linearsolver::GraphScatteredMatrix, sofa::component::linearsolver::GraphScatteredVector>;
@@ -85,7 +84,6 @@ int main(int argc, char** argv)
 
     sofa::component::initSofaBase();
     sofa::component::initSofaCommon();
-    sofa::component::initSofaGeneral();
 
     // The graph root node : gravity already exists in a GNode by default
     sofa::simulation::setSimulation(new sofa::simulation::tree::TreeSimulation());
