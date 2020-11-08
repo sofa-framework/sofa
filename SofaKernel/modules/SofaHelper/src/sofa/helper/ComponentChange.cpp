1f1417a457d2f55a2fc7c28f1619804544c36222
Frederick Roy <fred.roy.me@gmail.com>
2020-11-08 14:44:00 +0100

    clean up, remove warnings, component change, use index/size types...

--- SofaKernel/modules/SofaHelper/src/sofa/helper/ComponentChange.cpp
+++ SofaKernel/modules/SofaHelper/src/sofa/helper/ComponentChange.cpp
@@ -458,7 +458,7 @@ std::map<std::string, ComponentChange> uncreatableComponents = {
 
     // SofaEngine was pluginized in #1603
     { "BoxROI", Pluginized("v20.12", "SofaEngine") },
-
+    
     /***********************/
     // REMOVED SINCE v20.12
     { "DynamicSparseGridTopologyAlgorithms", Removed("v20.12", "v20.12") },
