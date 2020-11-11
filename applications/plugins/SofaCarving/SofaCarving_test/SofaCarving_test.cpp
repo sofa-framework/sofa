85e41c47dc9c7c75c4d7dd9e42eb9242ce4b0702
Frederick Roy <fred.roy.me@gmail.com>
2020-11-11 17:31:43 +0100

    fix the fixes...

--- applications/plugins/SofaCarving/SofaCarving_test/SofaCarving_test.cpp
+++ applications/plugins/SofaCarving/SofaCarving_test/SofaCarving_test.cpp
@@ -75,6 +75,7 @@ bool SofaCarving_test::createScene(const std::string& carvingDistance)
     createObject(m_root, "RequiredPlugin", { { "name","SofaGeneralLoader" } });
     createObject(m_root, "RequiredPlugin", { { "name","SofaEngine" } });
     createObject(m_root, "RequiredPlugin", { { "name","SofaImplicitOdeSolver" } });
+
     // create collision pipeline
     createObject(m_root, "CollisionPipeline", { { "name","Collision Pipeline" } });
     createObject(m_root, "BruteForceDetection", { { "name","Detection" } });
