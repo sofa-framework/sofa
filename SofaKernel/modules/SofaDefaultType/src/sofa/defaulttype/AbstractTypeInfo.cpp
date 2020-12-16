sofa-framework/sofa

commit e89df7cade7b0dbb433c18e13ea0812b957032b5

[All] Refactor DataTypeInfo system. (#1605)
* [SofaDefaultType] move AbstractTypeInfo from DataTypeInfo.h to AbstractTypeInfo.h

* [SofaDefaultType] add setCompilationTarget (resp. get) in AbstractTypeInfo.h

This allows us to track where the typeinfo has been generated.

* [SofaDefaultType] add TypeInfoID, TypeInfoRegistry, and TypeInfoRegistryTools

TypeInfoRegistry to store each defined TypeInfo and retrive it in constant time
using a TypeInfoId;

* {SofaDefaultType] move DataTypeInfo and VirtualTypeInfo in their own .h

DataTypeInfo moves to DataTypeInfo.h
while VirtualTypeInfo is renamed to DataTypeInfoDynamicWrapper. The renaming was done to to make more clear
its role is to generate a dynamic version of a DataTypeInfo.

* Fix compilation for MSVC

* [SofaKernel] Split DataTypeInfo.h into independant TypeInfo_*

* [FixUp] Add missing include

* [SofaKernel] Add full test in TypeInfoRepository_test.cpp

* [SofaKernel] fix compilation issues because if missing includes.

* [SofaDefaultType] Add NoTypeInfo to handle cases when there is not type info at all

* [SofaPython] fix conversion from std::string to char*

* [SofaDefaultType] Replace char* by std::string in TypeInfo_*

* [SofaDefaultType] remove un-used file typeinfo/TypeInfo.h

* [SofaPython] add new naming convention for type to SofaNumpy.py

* [all] Replace the use of not correct use of DataTypeName with the one DataTypeInfo.

* [SofaDefaultType] Add new test TypeInfoRepository to detect cross translation unit behavior.

* [plugins/SofaPython] Add new array type to support every registered typeinfo.

* [SofaDefaultType] Use a single TypeInfoID to avoid mismatch between the registery and the Id.

* [SofaKernel] Implement BaseType() and ValueType() without the need of overloading.

* [SofaDefaultType] Removale of the DefaultTypeInfo as fallback for DataTypeInfo

* [SofaDefaultType] Add multiple tests.

SofaKernel/modules/SofaDefaultType/src/sofa/defaulttype/AbstractTypeInfo.cpp
@@ -1,0 +1,32 @@
+/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+
+#include "AbstractTypeInfo.h"
+#include "TypeInfoRegistry.h"
+
+namespace sofa::defaulttype
+{
+
+const AbstractTypeInfo* AbstractTypeInfo::BaseType() const { return TypeInfoRegistry::Get(getBaseTypeId()); }
+const AbstractTypeInfo* AbstractTypeInfo::ValueType() const { return TypeInfoRegistry::Get(getValueTypeId()); }
+
+} /// namespace sofa::defaulttype

SofaKernel/modules/SofaDefaultType/src/sofa/defaulttype/AbstractTypeInfo.h
@@ -1,0 +1,188 @@
+/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#pragma once
+#include <sofa/defaulttype/config.h>
+#include <string>
+#include <typeinfo>
+
+namespace sofa::defaulttype
+{
+    class TypeInfoId;
+
+/** Information about the type of a value stored in a Data.
+
+    %AbstractTypeInfo is part of the introspection/reflection capabilities of
+    the Sofa scene graph API. It provides information about the type of the
+    content of Data objects (Is it a simple type?  A container? How much memory
+    should be allocated to copy it?), and allows manipulating Data generically,
+    without knowing their exact type.
+
+    This class is primarily used to copy information accross BaseData objects,
+    for example when there exists a link between two instances of BaseData.
+    E.g. this mecanism allows you to copy the content of a Data<vector<int>>
+    into a Data<vector<double>>, because there is an acceptable conversion
+    between integer and double, and because both Data use a resizable container.
+
+    <h4>Using TypeInfo</h4>
+
+    Use BaseData::getValueTypeInfo() to get a pointer to an AbtractTypeInfo, and
+    BaseData::getValueVoidPtr() to get a pointer to the content of a Data. You
+    can then use the methods of AbtractTypeInfo to access the Data generically.
+
+    Very basic example:
+    \code{.cpp}
+    BaseData *data = getADataFromSomewhere();
+    const AbstractTypeInfo *typeinfo = data->getValueTypeInfo();
+    const void* ptr = data->getValueVoidPtr();
+    for (int i = 0 ; i < typeinfo->size(ptr) ; i++)
+    std::string value = typeinfo->getTextValue(ptr, 0);
+    \endcode
+
+    <h4>Note about size and indices</h4>
+
+    All the getValue() and setValue() methods take an index as a parameter,
+    which means that every type is abstracted to a one-dimensional container.
+    See the detailed description of DataTypeInfo for more explanations.
+
+    \see DataTypeInfo provides similar mechanisms to manipulate Data objects
+    generically in template code.
+*/
+class SOFA_DEFAULTTYPE_API AbstractTypeInfo
+{
+public:
+    /// If the type is a container, returns the TypeInfo for the type of the
+    /// values inside this container.
+    /// For example, if the type is `fixed_array<fixed_array<int, 2> 3>`, it
+    /// returns the TypeInfo for `fixed_array<int, 2>`.
+    const AbstractTypeInfo* BaseType() const;
+
+    /// Returns the TypeInfo for the type of the values accessible by the
+    /// get*Value() functions.
+    /// For example, if the type is `fixed_array<fixed_array<int, 2> 3>`, it
+    /// returns the TypeInfo for `int`.    
+    const AbstractTypeInfo* ValueType() const;
+
+    /// \brief Returns the name of this type.
+    virtual std::string name() const = 0;
+    virtual std::string getTypeName() const = 0;
+
+    /// True iff the TypeInfo for this type contains valid information.
+    /// A Type is considered "Valid" if there's at least one specialization of the ValueType
+    virtual bool ValidInfo() const = 0;
+
+    /// True iff this type has a fixed size.
+    ///  (It cannot be resized)
+    virtual bool FixedSize() const = 0;
+    /// True iff the default constructor of this type is equivalent to setting the memory to 0.
+    virtual bool ZeroConstructor() const = 0;
+    /// True iff copying the data can be done with a memcpy().
+    virtual bool SimpleCopy() const = 0;
+    /// True iff the layout in memory is simply N values of the same base type.
+    /// It means that you can use the abstract index system to iterate over the elements of the type.
+    /// (It doesn't mean that the BaseType is of a fixed size)
+    virtual bool SimpleLayout() const = 0;
+    /// True iff this type uses integer values.
+    virtual bool Integer() const = 0;
+    /// True iff this type uses scalar values.
+    virtual bool Scalar() const = 0;
+    /// True iff this type uses text values.
+    virtual bool Text() const = 0;
+    /// True iff this type uses copy-on-write.
+    virtual bool CopyOnWrite() const = 0;
+    /// True iff this type is a container of some sort.
+    ///
+    /// That is, if it can contain several values. In particular, strings are
+    /// not considered containers.
+    virtual bool Container() const = 0;
+
+    /// The size of this type, in number of elements.
+    /// For example, the size of a `fixed_array<fixed_array<int, 2>, 3>` is 6,
+    /// and those six elements are conceptually numbered from 0 to 5.  This is
+    /// relevant only if FixedSize() is true. I FixedSize() is false,
+    /// the return value will be equivalent to the one of byteSize()
+    virtual sofa::Size size() const = 0;
+    /// The size in bytes of the ValueType
+    /// For example, the size of a fixed_array<fixed_array<int, 2>, 3>` is 4 on most systems,
+    /// as it is the byte size of the smallest dimension in the array (int -> 32bit)
+    virtual sofa::Size byteSize() const = 0;
+
+    /// The size of \a data, in number of iterable elements
+    /// (For containers, that'll be the number of elements in the 1st dimension).
+    /// For example, with type == `
+    virtual sofa::Size size(const void* data) const = 0;
+    /// Resize \a data to \a size elements, if relevant.
+
+    /// But resizing is not always relevant, for example:
+    /// - nothing happens if FixedSize() is true;
+    /// - sets can't be resized; they are cleared instead;
+    /// - nothing happens for vectors containing resizable values (i.e. when
+    ///   BaseType()::FixedSize() is false), because of the "single index"
+    ///   abstraction;
+    ///
+    /// Returns true iff the data was resizable
+    virtual bool setSize(void* data, sofa::Size size) const = 0;
+
+    /// Get the value at \a index of \a data as an integer.
+    /// Relevant only if this type can be casted to `long long`.
+    virtual long long   getIntegerValue(const void* data, Index index) const = 0;
+    /// Get the value at \a index of \a data as a scalar.
+    /// Relevant only if this type can be casted to `double`.
+    virtual double      getScalarValue (const void* data, Index index) const = 0;
+    /// Get the value at \a index of \a data as a string.
+    virtual std::string getTextValue   (const void* data, Index index) const = 0;
+
+    /// Set the value at \a index of \a data from an integer value.
+    virtual void setIntegerValue(void* data, Index index, long long value) const = 0;
+    /// Set the value at \a index of \a data from a scalar value.
+    virtual void setScalarValue (void* data, Index index, double value) const = 0;
+    /// Set the value at \a index of \a data from a string value.
+    virtual void setTextValue(void* data, Index index, const std::string& value) const = 0;
+
+    /// Get a read pointer to the underlying memory
+    /// Relevant only if this type is SimpleLayout
+    virtual const void* getValuePtr(const void* type) const = 0;
+
+    /// Get a write pointer to the underlying memory
+    /// Relevant only if this type is SimpleLayout
+    virtual void* getValuePtr(void* type) const = 0;
+
+    /// Get the type_info for this type.
+    virtual const std::type_info* type_info() const = 0;
+
+    const std::string& getCompilationTarget() const { return m_compilationTarget; }
+    void setCompilationTarget(const std::string& target) { m_compilationTarget=target; }
+
+protected: // only derived types can instantiate this class
+    AbstractTypeInfo() {}
+    virtual ~AbstractTypeInfo() {}
+
+    virtual const TypeInfoId& getBaseTypeId() const = 0;
+    virtual const TypeInfoId& getValueTypeId() const = 0;
+
+private: // copy constructor or operator forbidden
+    AbstractTypeInfo(const AbstractTypeInfo&) {}
+    void operator=(const AbstractTypeInfo&) {}
+
+    std::string m_compilationTarget;
+};
+
+} /// namespace sofa::defaulttype

SofaKernel/modules/SofaDefaultType/src/sofa/defaulttype/typeinfo/DataTypeInfoDynamicWrapper.h
@@ -1,0 +1,165 @@
+/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#pragma once
+#include <sofa/defaulttype/config.h>
+#include <sofa/defaulttype/AbstractTypeInfo.h>
+#include <sofa/defaulttype/typeinfo/DataTypeInfo.h>
+#include <sofa/defaulttype/TypeInfoID.h>
+#include <string>
+
+namespace sofa::defaulttype
+{
+
+/**
+ * @brief add a compatibility layer to supper the new existing GetTypeName.
+ */
+template<class T>
+class HasGetTypeName
+{
+    typedef char YesType[1];
+    typedef char NoType[2];
+
+    template<typename C> static YesType& test( decltype (&C::GetTypeName) );
+    template<typename C> static NoType& test(...);
+
+public:
+    enum { value = sizeof(test<T>(0)) == sizeof(YesType) };
+};
+
+
+
+
+/**
+    @brief wrap a DataTypeInfo type_traits in an AbstractTypeInfo
+
+    Example of use:
+    \code{.cpp}
+
+    /// This is a custom type
+    class MyType {};
+
+    /// This is a static DataTypeInfo that you can expose to in traits oriented code.
+    template<>
+    class DataTypeInfo<MyType> { }
+
+    /// Wrap the DataTypeInfo type in a dynamic version of type info (thus inheriting from AbstractTypeInfo).
+    AbstractTypeInfo* nfo = DataTypeInfoDynamicWrapper<DataTypeInfo<MyType>>();
+
+
+    /// Wrap the DataTypeInfo type in a dynamic version of type info (thus inheriting from AbstractTypeInfo).
+    TypeInfoRegistry::Set(TypeInfoId::getNewId<MyType>(), info);
+    \endcode
+**/
+template<class Info>
+class DataTypeInfoDynamicWrapper : public AbstractTypeInfo, public Info
+{
+public:
+    typedef typename Info::DataType DataType;
+
+    static AbstractTypeInfo* get() { static DataTypeInfoDynamicWrapper<Info> t; return &t; }
+
+    virtual const TypeInfoId& getBaseTypeId() const { return TypeInfoId::GetTypeId<typename Info::BaseType>(); }
+    virtual const TypeInfoId& getValueTypeId() const { return TypeInfoId::GetTypeId<typename Info::ValueType>(); }
+
+    std::string name() const override { return Info::name(); }
+    std::string getTypeName() const override {return Info::name();}
+
+    bool ValidInfo() const override       { return Info::ValidInfo; }
+    bool FixedSize() const override       { return Info::FixedSize; }
+    bool ZeroConstructor() const override { return Info::ZeroConstructor; }
+    bool SimpleCopy() const override      { return Info::SimpleCopy; }
+    bool SimpleLayout() const override    { return Info::SimpleLayout; }
+    bool Integer() const override         { return Info::Integer; }
+    bool Scalar() const override          { return Info::Scalar; }
+    bool Text() const override            { return Info::Text; }
+    bool CopyOnWrite() const override     { return Info::CopyOnWrite; }
+    bool Container() const override       { return Info::Container; }
+
+    sofa::Size size() const override
+    {
+        return Info::size();
+    }
+    sofa::Size byteSize() const override
+    {
+        return Info::byteSize();
+    }
+
+    sofa::Size size(const void* data) const override
+    {
+        return sofa::Size(Info::size(*(const DataType*)data));
+    }
+    bool setSize(void* data, sofa::Size size) const override
+    {
+        return Info::setSize(*(DataType*)data, size);
+    }
+
+    long long getIntegerValue(const void* data, Index index) const override
+    {
+        long long v = 0;
+        Info::getValue(*(const DataType*)data, index, v);
+        return v;
+    }
+
+    double    getScalarValue (const void* data, Index index) const override
+    {
+        double v = 0;
+        Info::getValue(*(const DataType*)data, index, v);
+        return v;
+    }
+
+    virtual std::string getTextValue   (const void* data, Index index) const override
+    {
+        std::string v;
+        Info::getValueString(*(const DataType*)data, index, v);
+        return v;
+    }
+
+    void setIntegerValue(void* data, Index index, long long value) const override
+    {
+        Info::setValue(*(DataType*)data, index, value);
+    }
+
+    void setScalarValue (void* data, Index index, double value) const override
+    {
+        Info::setValue(*(DataType*)data, index, value);
+    }
+
+    virtual void setTextValue(void* data, Index index, const std::string& value) const override
+    {
+        Info::setValueString(*(DataType*)data, index, value);
+    }
+    const void* getValuePtr(const void* data) const override
+    {
+        return Info::getValuePtr(*(const DataType*)data);
+    }
+    void* getValuePtr(void* data) const override
+    {
+        return Info::getValuePtr(*(DataType*)data);
+    }
+
+    virtual const std::type_info* type_info() const override { return &typeid(DataType); }
+
+protected: // only derived types can instantiate this class
+    DataTypeInfoDynamicWrapper() {}
+};
+
+} /// namespace sofa::defaulttype

commit 34926140a8e1afd7ac7d3f1caaa3a4177d192b01

[SofaKernel] Fix a broken that was introduced in pr #1605

SofaKernel/modules/SofaDefaultType/src/sofa/defaulttype/AbstractTypeInfo.cpp
@@ -26,7 +26,5 @@
 namespace sofa::defaulttype
 {
 
-const AbstractTypeInfo* AbstractTypeInfo::BaseType() const { return TypeInfoRegistry::Get(getBaseTypeId()); }
-const AbstractTypeInfo* AbstractTypeInfo::ValueType() const { return TypeInfoRegistry::Get(getValueTypeId()); }
 
 } /// namespace sofa::defaulttype

SofaKernel/modules/SofaDefaultType/src/sofa/defaulttype/AbstractTypeInfo.h
@@ -73,13 +73,13 @@ public:
     /// values inside this container.
     /// For example, if the type is `fixed_array<fixed_array<int, 2> 3>`, it
     /// returns the TypeInfo for `fixed_array<int, 2>`.
-    const AbstractTypeInfo* BaseType() const;
+    virtual const AbstractTypeInfo* BaseType() const = 0;
 
     /// Returns the TypeInfo for the type of the values accessible by the
     /// get*Value() functions.
     /// For example, if the type is `fixed_array<fixed_array<int, 2> 3>`, it
     /// returns the TypeInfo for `int`.    
-    const AbstractTypeInfo* ValueType() const;
+    virtual const AbstractTypeInfo* ValueType() const = 0;
 
     /// \brief Returns the name of this type.
     virtual std::string name() const = 0;

SofaKernel/modules/SofaDefaultType/src/sofa/defaulttype/typeinfo/DataTypeInfoDynamicWrapper.h
@@ -75,6 +75,15 @@ class DataTypeInfoDynamicWrapper : public AbstractTypeInfo, public Info
 public:
     typedef typename Info::DataType DataType;
 
+    const AbstractTypeInfo* BaseType() const override
+    {
+        return DataTypeInfoDynamicWrapper<DataTypeInfo<typename Info::BaseType>>::get();
+    }
+    const AbstractTypeInfo* ValueType() const override
+    {
+        return DataTypeInfoDynamicWrapper<DataTypeInfo<typename Info::ValueType>>::get();
+    }
+
     static AbstractTypeInfo* get() { static DataTypeInfoDynamicWrapper<Info> t; return &t; }
 
     const TypeInfoId& getBaseTypeId() const override { return TypeInfoId::GetTypeId<typename Info::BaseType>(); }

SofaKernel/modules/SofaDefaultType/src/sofa/defaulttype/typeinfo/NameOnlyTypeInfo.h
@@ -22,6 +22,7 @@
 #pragma once
 #include <sofa/defaulttype/AbstractTypeInfo.h>
 #include <sofa/defaulttype/TypeInfoID.h>
+#include <sofa/defaulttype/typeinfo/NoTypeInfo.h>
 
 namespace sofa::defaulttype
 {
@@ -29,6 +30,9 @@ namespace sofa::defaulttype
 class SOFA_DEFAULTTYPE_API NameOnlyTypeInfo : public AbstractTypeInfo
 {
 public:
+    const AbstractTypeInfo* BaseType() const override { return NoTypeInfo::Get(); }
+    const AbstractTypeInfo* ValueType() const override { return NoTypeInfo::Get(); }
+
     NameOnlyTypeInfo(const std::string& name, const std::string& typeName)
     {
         m_name = name;

SofaKernel/modules/SofaDefaultType/src/sofa/defaulttype/typeinfo/NoTypeInfo.h
@@ -30,6 +30,10 @@ class SOFA_DEFAULTTYPE_API NoTypeInfo : public AbstractTypeInfo
 {
 public:
     NoTypeInfo(){ setCompilationTarget("SofaDefaultType"); }
+
+    const AbstractTypeInfo* BaseType() const override { return Get(); }
+    const AbstractTypeInfo* ValueType() const override { return Get(); }
+
     static AbstractTypeInfo* Get(){ static NoTypeInfo t; return &t; }
 
     /// \brief Returns the name of this type.