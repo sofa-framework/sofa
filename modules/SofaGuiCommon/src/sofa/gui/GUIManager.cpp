d3a44a3f776896a4b5b2a053bc686d9522488f0a
Damien Marchal <damien.marchal@univ-lille1.fr>
2020-12-29 13:08:19 +0100

    Minor cosmetic cleaning. (#1685)
    * [caduceus.scn] Add missing Requires plugin & put them in a dedicated node.
    
    Because of pluginization we are going to have more and more plugins which
    can reduce the scenegraph visibility. Putting them in a dedicated Node with a clear
    name (eg: RequiredPlugins, Settings, RuntimeSettings) helps in readbility.
    
    * [modules] Cosmetic removal of duplicated text in BaseGUI.cpp & GUIManager.cpp
    
    * [SofaKernel] Cosmetic removal of an un-needed end of line in PluginManager.cpp

--- modules/SofaGuiCommon/src/sofa/gui/GUIManager.cpp
+++ modules/SofaGuiCommon/src/sofa/gui/GUIManager.cpp
@@ -76,7 +76,7 @@ int GUIManager::RegisterGUI(const char* name, CreateGUIFn* creator, RegisterGUIP
             ++it;
         if (it != itend)
         {
-            msg_error("GUIManager") << "ERROR(GUIManager): GUI "<<name<<" duplicate registration.";
+            msg_error("GUIManager") << "GUI "<<name<<" duplicate registration.";
             return 1;
         }
     }
@@ -119,7 +119,7 @@ const char* GUIManager::GetValidGUIName()
     if (guiCreators.empty())
     {
 
-        msg_error("GUIManager") << "ERROR(SofaGUI): No GUI registered.";
+        msg_error("GUIManager") << "No GUI registered.";
         return nullptr;
     }
     else
@@ -144,11 +144,11 @@ const char* GUIManager::GetValidGUIName()
                     return it1->name;
                 }
             }
-            msg_warning("GUIManager") << "WARNING(SofaGUI): Previously used GUI not registered. Using default GUI.";
+            msg_warning("GUIManager") << "Previously used GUI not registered. Using default GUI.";
         }
         else
         {
-            msg_info("GUIManager") << "INFO(SofaGUI): lastUsedGUI.ini not found; using default GUI.";
+            msg_info("GUIManager") << "LastUsedGUI.ini not found; using default GUI.";
         }
 
         std::list<GUICreator>::iterator it =guiCreators.begin();
@@ -293,25 +293,6 @@ void GUIManager::SetDimension(int  width , int  height )
 {
     if (currentGUI)
     {
-        //        std::string viewerFileName;
-        //        std::string path = sofa::helper::system::DataRepository.getFirstPath();
-        //        viewerFileName = path.append("/share/config/sofaviewer.ini");
-
-        //        if(sofa::helper::system::DataRepository.findFile(viewerFileName))
-        //        {
-        //            std::string configPath = sofa::helper::system::DataRepository.getFile(viewerFileName);
-        //            std::string w, h;
-        //            std::ifstream viewerStream(configPath.c_str());
-        //            std::getline(viewerStream,w);
-        //            std::getline(viewerStream,h);
-        //            viewerStream.close();
-
-        //            std::stringstream convertW(w);
-        //            convertW >> width;
-
-        //            std::stringstream convertH(h);
-        //            convertH >> height;
-        //        }
         currentGUI->setViewerResolution(width,height);
     }
 }
