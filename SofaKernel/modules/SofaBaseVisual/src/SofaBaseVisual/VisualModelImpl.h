140f4403e90e22cccb5b0503495d837d77279ce1
Damien Marchal <damien.marchal@univ-lille1.fr>
2021-01-25 11:20:37 +0100

    [SofaBaseVisual] Remove inclusion of TopologyData.inl in VisualModelImpl.h

--- SofaKernel/modules/SofaBaseVisual/src/SofaBaseVisual/VisualModelImpl.h
+++ SofaKernel/modules/SofaBaseVisual/src/SofaBaseVisual/VisualModelImpl.h
@@ -30,8 +30,7 @@
 #include <sofa/defaulttype/VecTypes.h>
 #include <sofa/defaulttype/RigidTypes.h>
 #include <sofa/helper/io/Mesh.h>
-#include <SofaBaseTopology/TopologyData.inl>
-
+#include <SofaBaseTopology/TopologyData.h>
 #include <string>
 
 namespace sofa::component::visualmodel
