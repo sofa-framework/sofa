05ae2feaa30680a21a85735fd7ea9f5d903c9c64
Damien Marchal <damien.marchal@univ-lille1.fr>
2021-01-28 10:58:24 +0100

    [SofaCore] Move friend operator<<() from .h to .cpp	for non templated classes.
    There is no need to have the operator<<() defined in the class declaration.
    The good design is:
    class MyClass
    {
    	friend operator<<(); ///< This allow the function << to access private property of the object.
    }
    
    Declare the function somewhere else (I put that in fwd.h) in a .h close to the forward declaration of MyClass:
    operator<<(..., MyClass&);
    
    Define the function in a .cpp:
    operator<<(..., MyClass& a){ ....put the implementation here... }

--- SofaKernel/modules/SofaCore/src/sofa/core/fwd.h
+++ SofaKernel/modules/SofaCore/src/sofa/core/fwd.h
@@ -4,41 +4,54 @@
 
 namespace sofa::core
 {
-    class BaseMapping;
+class BaseMapping;
 }
 
 namespace sofa::core::objectmodel
 {
-    class Base;
-    class BaseObject;
-    class BaseNode;
-    class BaseContext;
-    class BaseData;
-    class BaseLink;
+class Base;
+class BaseObject;
+class BaseNode;
+class BaseContext;
+class BaseData;
+class BaseLink;
+
+class Tag;
+SOFA_CORE_API std::ostream& operator<<(std::ostream& o, const Tag& t);
+SOFA_CORE_API std::istream& operator>>(std::istream& i, Tag& t);
 }
 
 namespace sofa::core::behavior
 {
-    class BaseForceField;
-    class BaseMass;
-    class BaseMechanicalState;
+class BaseForceField;
+class BaseMass;
+class BaseMechanicalState;
 }
 
 namespace sofa::core::topology
 {
-    class TopologyChange;
-
-    /// Output  stream
-    SOFA_CORE_API std::ostream& operator<< ( std::ostream& out, const sofa::core::topology::TopologyChange* t );
-
-    /// Input (empty) stream
-    SOFA_CORE_API std::istream& operator>> ( std::istream& in, sofa::core::topology::TopologyChange*& t );
-
-    /// Input (empty) stream
-    SOFA_CORE_API std::istream& operator>> ( std::istream& in, const sofa::core::topology::TopologyChange*& );
+class TopologyChange;
+SOFA_CORE_API std::ostream& operator<< ( std::ostream& out, const sofa::core::topology::TopologyChange* t );
+SOFA_CORE_API std::istream& operator>> ( std::istream& in, sofa::core::topology::TopologyChange*& t );
+SOFA_CORE_API std::istream& operator>> ( std::istream& in, const sofa::core::topology::TopologyChange*& );
 }
 
 namespace sofa::core::visual
 {
-    class VisualParams;
+class VisualParams;
+
+class FlagTreeItem;
+SOFA_CORE_API std::ostream& operator<< ( std::ostream& os, const FlagTreeItem& root );
+SOFA_CORE_API std::istream& operator>> ( std::istream& in, FlagTreeItem& root );
+
+class DislpayFlags;
+SOFA_CORE_API std::ostream& operator<< ( std::ostream& os, const DisplayFlags& flags );
+SOFA_CORE_API std::istream& operator>> ( std::istream& in, DisplayFlags& flags );
+}
+
+namespace sofa::component::topology
+{
+class TetrahedronSetTopologyContainer;
+SOFA_CORE_API std::ostream& operator<< (std::ostream& out, const TetrahedronSetTopologyContainer& t);
+SOFA_CORE_API std::istream& operator>>(std::istream& in, TetrahedronSetTopologyContainer& t);
 }
