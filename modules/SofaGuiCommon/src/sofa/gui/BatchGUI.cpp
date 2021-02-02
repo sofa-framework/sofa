dd042e6dcf6dca6dc900722f3d0e7bc44f59f0cb
Frederick Roy <fred.roy.me@gmail.com>
2021-02-02 22:28:12 +0100

    deprecate end() with node and add end() with times

--- modules/SofaGuiCommon/src/sofa/gui/BatchGUI.cpp
+++ modules/SofaGuiCommon/src/sofa/gui/BatchGUI.cpp
@@ -61,7 +61,7 @@ int BatchGUI::mainLoop()
 
         sofa::helper::AdvancedTimer::begin("Animate");
         sofa::simulation::getSimulation()->animate(groot.get());
-        msg_info("BatchGUI") << "Processing." << sofa::helper::AdvancedTimer::end("Animate", groot.get()) << msgendl;
+        msg_info("BatchGUI") << "Processing." << sofa::helper::AdvancedTimer::end("Animate", groot->getTime(), groot->getDt()) << msgendl;
         sofa::simulation::Visitor::ctime_t rtfreq = sofa::helper::system::thread::CTime::getRefTicksPerSec();
         sofa::simulation::Visitor::ctime_t tfreq = sofa::helper::system::thread::CTime::getTicksPerSec();
         sofa::simulation::Visitor::ctime_t rt = sofa::helper::system::thread::CTime::getRefTime();
