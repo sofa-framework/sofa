4dcfd6f97714e18ad419d37155fb62e050b66676
Guillaume Paran <guillaume.paran@sofa-framework.org>
2021-02-17 13:07:15 +0100

    [CI] testing WIP ignore

--- modules/SofaGuiQt/src/sofa/gui/qt/qt.conf.h
+++ modules/SofaGuiQt/src/sofa/gui/qt/qt.conf.h
@@ -2,8 +2,6 @@
 ** Resource object code
 **
 ** Created by: The Resource Compiler for Qt version 5.15.2
-**
-** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
 #include <sofa/config.h>
