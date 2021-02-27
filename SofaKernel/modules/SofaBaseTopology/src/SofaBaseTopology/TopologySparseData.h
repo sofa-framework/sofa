4573fe1ddd70a2e6bfac95bc3cc6fe2a51335d50
epernod <erik.pernod@gmail.com>
2021-02-27 20:08:31 +0100

    [SofaBaseTopology] rename TopologySubsetDataImpl into TopologySubsetData and TopologySparseDataImpl into TopologySparseData. make inheritence to TopologyData instead of BaseTopologyData
    (cherry picked from commit 122a0e870c1dcc62db0fc7fe8245f8b66c6e2af9)

--- SofaKernel/modules/SofaBaseTopology/src/SofaBaseTopology/TopologySparseData.h
+++ SofaKernel/modules/SofaBaseTopology/src/SofaBaseTopology/TopologySparseData.h
@@ -22,11 +22,9 @@
 #pragma once
 #include <SofaBaseTopology/config.h>
 
-#include <sofa/core/topology/BaseTopologyData.h>
+#include <SofaBaseTopology/TopologyData.h>
 #include <SofaBaseTopology/TopologyEngine.h>
 #include <SofaBaseTopology/TopologySparseDataHandler.h>
-
-#include <sofa/core/topology/BaseMeshTopology.h>
 
 namespace sofa::component::topology
 {
@@ -36,7 +34,7 @@ namespace sofa::component::topology
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 template< class TopologyElementType, class VecT>
-class TopologySparseDataImpl : public sofa::core::topology::BaseTopologyData<VecT>
+class TopologySparseData : public sofa::component::topology::TopologyData<TopologyElementType, VecT>
 {
 
 public:
@@ -56,15 +54,13 @@ public:
 
 
     /// Constructor
-    TopologySparseDataImpl( const typename sofa::core::topology::BaseTopologyData< VecT >::InitData& data)
-        : sofa::core::topology::BaseTopologyData< VecT >(data),
-          m_topologicalEngine(nullptr),
-          m_topology(nullptr),
-          m_topologyHandler(nullptr),
-          m_isConcerned(false)
+    TopologySparseData( const typename sofa::core::topology::BaseTopologyData< VecT >::InitData& data)
+        : sofa::component::topology::TopologyData< TopologyElementType, VecT >(data)
+        , m_topologyHandler(nullptr)
+        , m_isConcerned(false)
     {}
 
-    virtual ~TopologySparseDataImpl();
+    virtual ~TopologySparseData();
 
     /** Public functions to handle topological engine creation */
     /// To create topological engine link to this Data. Pointer to current topology is needed.
@@ -74,30 +70,7 @@ public:
     /// To create topological engine link to this Data. Pointer to current topology is needed.
     virtual void createTopologicalEngine(sofa::core::topology::BaseMeshTopology* _topology);
 
-    /// Allow to add additionnal dependencies to others Data.
-    void addInputData(sofa::core::objectmodel::BaseData* _data);
 
-    /// Function to link the topological Data with the engine and the current topology. And init everything.
-    /// This function should be used at the end of the all declaration link to this Data while using it in a component.
-    void registerTopologicalData();
-
-
-    value_type& operator[](int i)
-    {
-        container_type& data = *(this->beginEdit());
-        value_type& result = data[i];
-        this->endEdit();
-        return result;
-    }
-
-
-    /// Link Data to topology arrays
-    void linkToPointDataArray();
-    void linkToEdgeDataArray();
-    void linkToTriangleDataArray();
-    void linkToQuadDataArray();
-    void linkToTetrahedronDataArray();
-    void linkToHexahedronDataArray();
 
     void setMap2Elements(const sofa::helper::vector<Index> _map2Elements)
     {
@@ -129,19 +102,9 @@ protected:
 
     // same size as SparseData but contain id of element link to each data[]
     sofa::helper::vector<Index> m_map2Elements;
-
-    typename sofa::component::topology::TopologyEngineImpl<VecT>::SPtr m_topologicalEngine;
-    sofa::core::topology::BaseMeshTopology* m_topology;
     sofa::component::topology::TopologySparseDataHandler<TopologyElementType,VecT>* m_topologyHandler;
 
     bool m_isConcerned;
-
-    void linkToElementDataArray(sofa::core::topology::BaseMeshTopology::Point*      ) { linkToPointDataArray();       }
-    void linkToElementDataArray(sofa::core::topology::BaseMeshTopology::Edge*       ) { linkToEdgeDataArray();        }
-    void linkToElementDataArray(sofa::core::topology::BaseMeshTopology::Triangle*   ) { linkToTriangleDataArray();    }
-    void linkToElementDataArray(sofa::core::topology::BaseMeshTopology::Quad*       ) { linkToQuadDataArray();        }
-    void linkToElementDataArray(sofa::core::topology::BaseMeshTopology::Tetrahedron*) { linkToTetrahedronDataArray(); }
-    void linkToElementDataArray(sofa::core::topology::BaseMeshTopology::Hexahedron* ) { linkToHexahedronDataArray();  }
 
 };
 
@@ -150,12 +113,12 @@ protected:
 //////////////////////////////   Element Topology Data Implementation   ////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-template< class VecT > using PointSparseData = TopologySparseDataImpl<core::topology::BaseMeshTopology::Point, VecT>;
-template< class VecT > using EdgeSparseData = TopologySparseDataImpl<core::topology::BaseMeshTopology::Edge, VecT>;
-template< class VecT > using TriangleSparseData = TopologySparseDataImpl<core::topology::BaseMeshTopology::Triangle, VecT>;
-template< class VecT > using QuadSparseData = TopologySparseDataImpl<core::topology::BaseMeshTopology::Quad, VecT>;
-template< class VecT > using TetrahedronSparseData = TopologySparseDataImpl<core::topology::BaseMeshTopology::Tetrahedron, VecT>;
-template< class VecT > using HexahedronSparseData = TopologySparseDataImpl<core::topology::BaseMeshTopology::Hexahedron, VecT>;
+template< class VecT > using PointSparseData = TopologySparseData<core::topology::BaseMeshTopology::Point, VecT>;
+template< class VecT > using EdgeSparseData = TopologySparseData<core::topology::BaseMeshTopology::Edge, VecT>;
+template< class VecT > using TriangleSparseData = TopologySparseData<core::topology::BaseMeshTopology::Triangle, VecT>;
+template< class VecT > using QuadSparseData = TopologySparseData<core::topology::BaseMeshTopology::Quad, VecT>;
+template< class VecT > using TetrahedronSparseData = TopologySparseData<core::topology::BaseMeshTopology::Tetrahedron, VecT>;
+template< class VecT > using HexahedronSparseData = TopologySparseData<core::topology::BaseMeshTopology::Hexahedron, VecT>;
 
 
 } //namespace sofa::component::topology
