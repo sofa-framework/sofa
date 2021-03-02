e9e303b33a8847564ce31f07638d1bab6c410093
Guillaume Paran <guillaume.paran@sofa-framework.org>
2021-03-02 18:55:12 +0100

    [CMake] FIX duplicated code in windeployqt.cmake

--- cmake/Modules/windeployqt.cmake
+++ cmake/Modules/windeployqt.cmake
@@ -45,16 +45,6 @@ endif()
 # Add commands that copy the Qt runtime to the target's output directory after
 # build and install the Qt runtime to the specified directory
 function(windeployqt target build_dir install_dir)
-    # Retrieve the absolute path to qmake and then use that path to find
-    # the windeployqt binary
-    get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
-    find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
-
-    # Running this with MSVC 2015 requires CMake 3.6+
-    if((MSVC_VERSION VERSION_EQUAL 1900 OR MSVC_VERSION VERSION_GREATER 1900)
-            AND CMAKE_VERSION VERSION_LESS "3.6")
-        message(WARNING "Deploying with MSVC 2015+ requires CMake 3.6+")
-    endif()
 
     # execute windeployqt in a tmp directory after build
     add_custom_command(TARGET ${target}
