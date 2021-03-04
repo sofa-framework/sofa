6720212a4c0ca78c7d0dc7cbae6a3cd3f03d473e
EulalieCoevoet <eulalie.coevoet@gmail.com>
2021-03-04 04:16:18 -0500

    [SofaTopologyMapping] adds ellipse feature to Edge2QuadTopologyMapping (#1861)
    * [SofaTopologyMapping] adds ellipse feature to Edge2QuadTopologyMapping
    
    * [SofaTopologyMapping] attempt to fix compile
    
    Co-authored-by: EulalieCoevoet <>
    Co-authored-by: Frederick Roy <fred.roy.me@gmail.com>

--- applications/plugins/SofaPython/examples/Components/topology/Edge2QuadTopologyMapping.py
+++ applications/plugins/SofaPython/examples/Components/topology/Edge2QuadTopologyMapping.py
@@ -5,20 +5,23 @@ def createScene(rootNode):
     rootNode.createObject('VisualStyle', displayFlags='showVisualModels showWireframe')
     rootNode.createObject('RequiredPlugin', pluginName='SofaGeneralEngine SofaTopologyMapping SofaPython SofaOpenglVisual SofaMiscMapping')
 
-    positions = [[0,0,0],[10,0,0],[20,0,0],[30,0,0]]
-    edgetopo = rootNode.createChild('Edge')
-    edgetopo.createObject('EdgeSetTopologyContainer', position=positions, edges=[[k,k+1] for k in range(3)])
-    edgetopo.createObject('EdgeSetTopologyModifier')
-    edgetopo.createObject('MechanicalObject', template='Rigid3', position=[positions[k]+[0,0,0,1] for k in range(4)], showObject=True, showObjectScale=2)
+    positions = [[0,0,k*10] for k in range(4)]
+    for i in range(4):
+        mesh = rootNode.createChild('Mesh'+str(i+1))
+        edgetopo = mesh.createChild('Edge')
+        edgetopo.createObject('EdgeSetTopologyContainer', position=positions, edges=[[k,k+1] for k in range(3)])
+        edgetopo.createObject('EdgeSetTopologyModifier')
+        edgetopo.createObject('MechanicalObject', template='Rigid3', position=[positions[k]+[0,-0.707,0,0.707] for k in range(4)], translation=[40*i,0,0], showObject=True, showObjectScale=2)
 
-    quadtopo = edgetopo.createChild('Quad')
-    quadtopo.createObject('QuadSetTopologyContainer')
-    quadtopo.createObject('QuadSetTopologyModifier')
-    quadtopo.createObject('MechanicalObject')
-    quadtopo.createObject('Edge2QuadTopologicalMapping',
-                          input=edgetopo.EdgeSetTopologyContainer.getLinkPath(),
-                          output=quadtopo.QuadSetTopologyContainer.getLinkPath(),
-                          flipNormals=True, nbPointsOnEachCircle=8, radius=10)
-    quadtopo.createObject('OglModel', color=[1,1,1,1])
+        quadtopo = edgetopo.createChild('Quad')
+        quadtopo.createObject('QuadSetTopologyContainer')
+        quadtopo.createObject('QuadSetTopologyModifier')
+        quadtopo.createObject('MechanicalObject')
+        quadtopo.createObject('Edge2QuadTopologicalMapping',
+                              input=edgetopo.EdgeSetTopologyContainer.getLinkPath(),
+                              output=quadtopo.QuadSetTopologyContainer.getLinkPath(),
+                              flipNormals=True, nbPointsOnEachCircle=16, radius=10,
+                              radiusFocal= [0,15,15,20][i], focalAxis=[[0,0,1],[0,0,1],[0,1,0],[0,1,1]][i])
+        quadtopo.createObject('OglModel', color=[1,1,1,1])
 
     return
