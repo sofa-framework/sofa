9c90c0e46c5e41be9faf7c630065c1ef7231bb8f
Damien Marchal <damien.marchal@univ-lille1.fr>
2021-03-09 14:29:05 +0100

    [Sofa.Core] Add forward declaration & core::opaque functions
    The forward declaration are for commonly used type in our code base.

--- SofaKernel/modules/SofaCore/src/sofa/core/fwd.cpp
+++ SofaKernel/modules/SofaCore/src/sofa/core/fwd.cpp
@@ -1,5 +1,40 @@
 #include <sofa/core/fwd.h>
 #include <sofa/core/topology/TopologyChange.h>
+#include <sofa/core/BaseState.h>
+#include <sofa/core/BaseMapping.h>
+#include <sofa/core/CollisionModel.h>
+#include <sofa/core/objectmodel/BaseObject.h>
+#include <sofa/core/objectmodel/ConfigurationSetting.h>
+#include <sofa/core/objectmodel/ContextObject.h>
+
+#include <sofa/core/behavior/BaseMass.h>
+#include <sofa/core/BehaviorModel.h>
+#include <sofa/core/behavior/BaseAnimationLoop.h>
+#include <sofa/core/behavior/BaseMass.h>
+#include <sofa/core/behavior/OdeSolver.h>
+#include <sofa/core/behavior/ConstraintSolver.h>
+#include <sofa/core/behavior/BaseMechanicalState.h>
+#include <sofa/core/behavior/BaseForceField.h>
+#include <sofa/core/behavior/LinearSolver.h>
+#include <sofa/core/behavior/BaseConstraintSet.h>
+#include <sofa/core/behavior/BaseInteractionForceField.h>
+#include <sofa/core/behavior/BaseInteractionConstraint.h>
+#include <sofa/core/behavior/BaseProjectiveConstraintSet.h>
+#include <sofa/core/behavior/BaseConstraint.h>
+
+#include <sofa/core/topology/Topology.h>
+#include <sofa/core/topology/BaseMeshTopology.h>
+#include <sofa/core/topology/BaseTopology.h>
+
+#include <sofa/core/collision/Pipeline.h>
+
+#include <sofa/core/visual/VisualLoop.h>
+#include <sofa/core/visual/VisualModel.h>
+#include <sofa/core/visual/VisualManager.h>
+#include <sofa/core/visual/Shader.h>
+
+#include <sofa/core/collision/Pipeline.h>
+#include <sofa/core/collision/Pipeline.h>
 
 namespace sofa::core::topology
 {
@@ -28,6 +63,50 @@ std::istream& operator>> ( std::istream& in, const TopologyChange*& )
 {
     return in;
 }
+
+}
+
+namespace sofa::core
+{
+
+#define DEFINE_OPAQUE_FUNCTION_FOR(TYPENAME) \
+    template<> \
+    TYPENAME* castBaseTo(sofa::core::objectmodel::Base* base) \
+    { return dynamic_cast<TYPENAME*>(base); } \
+    sofa::core::objectmodel::Base* baseFrom(TYPENAME* b) \
+    { return dynamic_cast<sofa::core::objectmodel::Base*>(b); } \
+    namespace base { template<> const sofa::core::objectmodel::BaseClass* GetClass<TYPENAME>() \
+    { return TYPENAME::GetClass(); } }
+
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::BaseState);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::BaseMapping);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::BehaviorModel);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::CollisionModel);
+
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::objectmodel::BaseObject);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::objectmodel::ContextObject);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::objectmodel::ConfigurationSetting);
+
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseAnimationLoop);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseMass);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::OdeSolver);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::ConstraintSolver);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseLinearSolver);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseMechanicalState);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseForceField);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseInteractionForceField);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseProjectiveConstraintSet);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseConstraintSet);
+
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::topology::Topology);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::topology::BaseMeshTopology);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::topology::BaseTopologyObject);
+
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::collision::Pipeline);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::visual::VisualLoop);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::visual::Shader);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::visual::VisualModel);
+DEFINE_OPAQUE_FUNCTION_FOR(sofa::core::visual::VisualManager);
 
 }
 
--- SofaKernel/modules/SofaCore/src/sofa/core/fwd.h
+++ SofaKernel/modules/SofaCore/src/sofa/core/fwd.h
@@ -4,13 +4,14 @@
 
 namespace sofa::core
 {
+class BaseState;
 class ExecParams;
 class ConstraintParams;
 class BaseMapping;
 class CollisionModel;
 class CollisionElementIterator;
 class ConstraintParams;
-class ExecParams;
+class BehaviorModel;
 }
 
 namespace sofa::core::objectmodel
@@ -21,23 +22,35 @@ class BaseNode;
 class BaseContext;
 class BaseData;
 class BaseLink;
-class BaseNode;
+class BaseClass;
+class AbstractDataLink;
 class Event;
+class BaseClass;
+class AbstractDataLink;
+class ContextObject;
+class ConfigurationSetting;
+
 
 class Tag;
 SOFA_CORE_API std::ostream& operator<<(std::ostream& o, const Tag& t);
 SOFA_CORE_API std::istream& operator>>(std::istream& i, Tag& t);
 }
 
+
 namespace sofa::core::behavior
 {
 class BaseForceField;
 class BaseMass;
 class BaseMechanicalState;
+class BaseAnimationLoop;
 class BaseConstraint;
 class BaseConstraintSet;
 class ConstraintSolver;
 class ConstraintResolution;
+class OdeSolver;
+class BaseLinearSolver;
+class BaseInteractionForceField;
+class BaseProjectiveConstraintSet;
 
 template<class T>
 class MechanicalState;
@@ -45,17 +58,23 @@ class MechanicalState;
 
 namespace sofa::core::topology
 {
+class BaseMeshTopology;
+class BaseTopologyObject;
+class Topology;
+class TopologyEngine;
 class TopologyChange;
 SOFA_CORE_API std::ostream& operator<< ( std::ostream& out, const sofa::core::topology::TopologyChange* t );
 SOFA_CORE_API std::istream& operator>> ( std::istream& in, sofa::core::topology::TopologyChange*& t );
 SOFA_CORE_API std::istream& operator>> ( std::istream& in, const sofa::core::topology::TopologyChange*& );
-
-class Topology;
 }
 
 namespace sofa::core::visual
 {
 class VisualParams;
+class VisualLoop;
+class VisualModel;
+class VisualManager;
+class Shader;
 
 class FlagTreeItem;
 SOFA_CORE_API std::ostream& operator<< ( std::ostream& os, const FlagTreeItem& root );
@@ -66,11 +85,86 @@ SOFA_CORE_API std::ostream& operator<< ( std::ostream& os, const DisplayFlags& f
 SOFA_CORE_API std::istream& operator>> ( std::istream& in, DisplayFlags& flags );
 }
 
-namespace sofa::component::topology
+namespace sofa::core::collision
 {
-class TetrahedronSetTopologyContainer;
-SOFA_CORE_API std::ostream& operator<< (std::ostream& out, const TetrahedronSetTopologyContainer& t);
-SOFA_CORE_API std::istream& operator>>(std::istream& in, TetrahedronSetTopologyContainer& t);
+class Pipeline;
+}
+
+namespace sofa::core
+{
+/////////////////////////////// CORE::OPAQUE FUNCTION /////////////////////////////////////////////////
+///
+/// CORE::OPAQUE function are a groupe of function that make "opaque" some of the common sofa behaviors.
+///
+/// Core::Opaque functions are:
+///     - Base* sofa::core::baseFrom(T*) replace dynamic_cast<Base*>(T*);
+///     - T* sofa::core::castBaseTo(Base*) replace dynamic_cast<T*>(Base*);
+///     - sofa:core::base::GetClass<T>() replace T::GetClass();
+///
+/// These functions are called "opaque" as they work with only forward declaration of the involved
+/// types in comparison to class methods the requires the full class declaration to be used.
+///
+/// It is highly recommanded to use as much as possible opaque function in header files as this
+/// allow to reduce the dependency tree.
+///
+/// Opaque function may be slower at runtime (by one function call) but this is true only if LTO isn't
+/// able to optimize them properly. If you have experience/feedback with LTO please join the discussion
+/// in https://github.com/sofa-framework/sofa/discussions/1822
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+template<class B>
+sofa::core::objectmodel::Base* baseFrom(B*b){ return dynamic_cast<sofa::core::objectmodel::Base*>(b);}
+
+template<class Dest>
+Dest castBaseTo(sofa::core::objectmodel::Base* base){ return dynamic_cast<Dest>(base); }
+
+/// getClass is in the namespace base as it is an opaque function for Base::GetClass
+namespace base
+{
+template<class B>
+const sofa::core::objectmodel::BaseClass* GetClass(){return B::GetClass(); }
+}
+
+/// Macro use to simplify the declaration of the core opaque function.
+#define DECLARE_OPAQUE_FUNCTION_FOR(TYPENAME) \
+    template<> SOFA_CORE_API TYPENAME* castBaseTo(sofa::core::objectmodel::Base* base); \
+    SOFA_CORE_API sofa::core::objectmodel::Base* baseFrom(TYPENAME* b); \
+    namespace base { template<> SOFA_CORE_API const sofa::core::objectmodel::BaseClass* GetClass<TYPENAME>(); }
+
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::BaseState);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::BaseMapping);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::BehaviorModel);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::CollisionModel);
+
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::objectmodel::BaseObject);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::objectmodel::ContextObject);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::objectmodel::ConfigurationSetting);
+
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseAnimationLoop);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseMass);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::OdeSolver);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::ConstraintSolver);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseLinearSolver);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseMechanicalState);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseForceField);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseInteractionForceField);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseProjectiveConstraintSet);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::behavior::BaseConstraintSet);
+
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::topology::Topology);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::topology::BaseMeshTopology);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::topology::BaseTopologyObject);
+
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::collision::Pipeline);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::visual::VisualLoop);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::visual::Shader);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::visual::VisualModel);
+DECLARE_OPAQUE_FUNCTION_FOR(sofa::core::visual::VisualManager);
+}
+
+namespace sofa::core::objectmodel::basecontext
+{
+SOFA_CORE_API SReal getDt(sofa::core::objectmodel::BaseContext* context);
+SOFA_CORE_API SReal getTime(sofa::core::objectmodel::BaseContext* context);
 }
 
 namespace sofa::core::objectmodel::basecontext
