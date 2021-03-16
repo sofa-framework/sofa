6b9e4e625411c0f7ae5863959dec4a14607c33b3
alxbilger <alxbilger@users.noreply.github.com>
2021-03-16 16:05:51 +0100

    [SofaSimulationCore] Add more unit tests for TaskScheduler

--- SofaKernel/modules/SofaSimulationCore/src/sofa/simulation/DefaultTaskScheduler.cpp
+++ SofaKernel/modules/SofaSimulationCore/src/sofa/simulation/DefaultTaskScheduler.cpp
@@ -55,9 +55,11 @@ namespace sofa
             m_isClosing = false;
             
             // init global static thread local var
-            workerThreadIndex = new WorkerThread(this, 0, "Main  ");
-            _threads[std::this_thread::get_id()] = workerThreadIndex;// new WorkerThread(this, 0, "Main  ");
-            
+            if (_threads.find(std::this_thread::get_id()) == _threads.end())
+            {
+                workerThreadIndex = new WorkerThread(this, 0, "Main  ");
+                _threads[std::this_thread::get_id()] = workerThreadIndex;// new WorkerThread(this, 0, "Main  ");
+            }
         }
         
         DefaultTaskScheduler::~DefaultTaskScheduler()
