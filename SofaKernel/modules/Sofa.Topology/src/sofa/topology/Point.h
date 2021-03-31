43aa729185e6c67243c3cc4759bf3753f4e4a576
Frederick Roy <fredroy@users.noreply.github.com>
2021-03-31 09:18:01 +0200

    [SofaKernel] Create Sofa.Geometry and Sofa.Topology modules (#1912)
    * create sofa.topology stub
    
    * move topology.h from core to sofa.topology
    
    * clean topology class witout baseobject dep
    
    * move ElementType to a separate file
    
    * keep/add core::topology for now
    
    * sofaframework compiles
    
    * Split geometries in separate file/namespace
    
    * create specific files for TopologyElementInfo
    
    * create typeinfo files for topology
    
    * remove topology::Topology
    
    * remove core dependency in Mesh
    
    * fix compil/link for linux
    
    * rename TopologyElement* classes
    
    * rename classes themselves
    
    * use elementInfo for datatypeinfo
    
    * create Sofa.Geometry
    
    * remove geometry inside Topology, add elements and create cpps
    
    * update on geometry
    
    * fix compilation
    
    * redesign geometry/topo
    
    * replace Element
    
    * element default constructor fills with invalidID
    
    * Update SofaKernel/modules/Sofa.Geometry/Sofa.GeometryConfig.cmake.in
    
    Co-authored-by: Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
    
    * Update SofaKernel/modules/Sofa.Topology/Sofa.TopologyConfig.cmake.in
    
    Co-authored-by: Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
    
    * Update SofaKernel/modules/SofaDefaultType/src/sofa/defaulttype/typeinfo/TypeInfo_Topology.h
    
    Co-authored-by: Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
    
    * remove aliases directly from S.Topology and deprecates the ones in core::Topology
    
    * removed duplicated alias
    
    * disable temporarily deprecation messages for aliases
    
    * fix forgotten link with helper/core...
    
    * move list<change*> where it should be IMO
    
    * remove useless stuff
    
    * use the type::stdtype::vector instead of the compat version of vector
    
    * fix compilation
    
    * remove now useless cmake config about vector and compat
    
    * apply jnbrunet suggestion, moving check on the number of args as a static assert
    
    Co-authored-by: Jean-Nicolas Brunet <jnbrunet2000@gmail.com>

---
+++ SofaKernel/modules/Sofa.Topology/src/sofa/topology/Point.h
@@ -0,0 +1,36 @@
+/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#pragma once
+
+#include <sofa/topology/config.h>
+#include <sofa/type/vector.h>
+
+namespace sofa::topology
+{
+
+    typedef sofa::type::vector<sofa::Index> SetIndex;
+    typedef sofa::type::vector<sofa::Index> SetIndices;
+
+    // inline variable (c++17): https://en.cppreference.com/w/cpp/language/inline
+    inline static const sofa::type::vector<sofa::Index> InvalidSet;
+
+}
