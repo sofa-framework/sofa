077ebdeba61e505348b07a54a2adfd7628dd8574
Damien Marchal <damien.marchal@univ-lille1.fr>
2021-04-08 16:07:44 +0200

    [SofaSImulationCore] Split MechanicalMatrixVisitor.h to have one class per .h file.

---
+++ SofaKernel/modules/SofaSimulationCore/src/sofa/simulation/mechanicalvisitor/MechanicalMultiVectorFromBaseVectorVisitor.cpp
@@ -0,0 +1,50 @@
+/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#include <sofa/simulation/mechanicalvisitor/MechanicalMultiVectorFromBaseVectorVisitor.h>
+#include <sofa/core/behavior/BaseMechanicalState.h>
+#include <sofa/core/behavior/MultiMatrixAccessor.h>
+namespace sofa::simulation::mechanicalvisitor
+{
+
+MechanicalMultiVectorFromBaseVectorVisitor::MechanicalMultiVectorFromBaseVectorVisitor(
+        const core::ExecParams* params, sofa::core::MultiVecId _dest,
+        const defaulttype::BaseVector * _src,
+        const sofa::core::behavior::MultiMatrixAccessor* _matrix)
+    : BaseMechanicalVisitor(params) , src(_src), dest(_dest), matrix(_matrix), offset(0)
+{
+}
+
+MechanicalMultiVectorFromBaseVectorVisitor::Result MechanicalMultiVectorFromBaseVectorVisitor::fwdMechanicalState(simulation::Node* /*node*/, core::behavior::BaseMechanicalState* mm)
+{
+    if (matrix) offset = matrix->getGlobalOffset(mm);
+    if (src!= nullptr && offset >= 0)
+    {
+        unsigned int o = (unsigned int)offset;
+        mm->copyFromBaseVector(dest.getId(mm), src, o);
+        offset = (int)o;
+    }
+
+    return RESULT_CONTINUE;
+}
+
+} // namespace sofa::simulation::mechanicalvisitor
+
---
+++ SofaKernel/modules/SofaSimulationCore/src/sofa/simulation/mechanicalvisitor/MechanicalMultiVectorToBaseVectorVisitor.cpp
@@ -0,0 +1,51 @@
+/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#include <sofa/simulation/mechanicalvisitor/MechanicalMultiVectorToBaseVectorVisitor.h>
+#include <sofa/core/behavior/MultiMatrixAccessor.h>
+#include <sofa/core/behavior/BaseMechanicalState.h>
+
+namespace sofa::simulation::mechanicalvisitor
+{
+
+MechanicalMultiVectorToBaseVectorVisitor::MechanicalMultiVectorToBaseVectorVisitor(
+        const core::ExecParams* params,
+        sofa::core::ConstMultiVecId _src, defaulttype::BaseVector * _vect,
+        const sofa::core::behavior::MultiMatrixAccessor* _matrix)
+    : BaseMechanicalVisitor(params) , src(_src), vect(_vect), matrix(_matrix), offset(0)
+{
+}
+
+MechanicalMultiVectorToBaseVectorVisitor::Result MechanicalMultiVectorToBaseVectorVisitor::fwdMechanicalState(simulation::Node* /*node*/,
+                                                                                                              core::behavior::BaseMechanicalState* mm)
+{
+    if (matrix) offset = matrix->getGlobalOffset(mm);
+    if (vect != nullptr && offset >= 0)
+    {
+        unsigned int o = (unsigned int)offset;
+        mm->copyToBaseVector(vect, src.getId(mm), o);
+        offset = (int)o;
+    }
+    return RESULT_CONTINUE;
+}
+
+} // namespace sofa::simulation::mechanicalvisitor
+
