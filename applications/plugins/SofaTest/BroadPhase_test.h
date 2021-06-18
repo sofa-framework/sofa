99c1da8a98c6aea6b1b23264abe7d66dd7114988
Frederick Roy <fred.roy.me@gmail.com>
2021-06-18 12:43:34 +0200

    fix include

--- applications/plugins/SofaTest/BroadPhase_test.h
+++ applications/plugins/SofaTest/BroadPhase_test.h
@@ -5,7 +5,7 @@
 #include <SofaBaseCollision/NewProximityIntersection.h>
 #include <sofa/simulation/Node.h>
 #include <SofaSimulationGraph/DAGNode.h>
-#include <SofaBaseCollision/OBBModel.h>
+#include <SofaMiscCollision/OBBModel.h>
 #include <sofa/core/collision/NarrowPhaseDetection.h>
 
 #include <gtest/gtest.h>
