bb92244d83af81d0777e1fce6a8aad22231b65a5
Frederick Roy <frederick.roy@inria.fr>
2021-07-05 13:52:59 +0200

    remove sofapython files

--- applications/plugins/SofaPython/SofaPython_test/python/myrandom.py
+++
@@ -1,7 +0,0 @@
-import random
-
-# Simply generating a new random number.
-# Note the number is only generated at the first module import
-# while the already imported module would not execute this code
-# and would not generate a new number.
-random_number = random.random()
