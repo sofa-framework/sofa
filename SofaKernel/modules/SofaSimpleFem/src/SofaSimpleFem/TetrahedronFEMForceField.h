cdca434001f0dc5a04e42dff23d0c7f3c99d0252
alxbilger <alxbilger@users.noreply.github.com>
2021-07-08 09:18:07 +0200

    [SofaSimpleFem] Move duplicated code into a function

--- SofaKernel/modules/SofaSimpleFem/src/SofaSimpleFem/TetrahedronFEMForceField.h
+++ SofaKernel/modules/SofaSimpleFem/src/SofaSimpleFem/TetrahedronFEMForceField.h
@@ -44,6 +44,11 @@
 //   url          = "http://www-evasion.imag.fr/Publications/2005/NPF05"
 // }
 
+namespace sofa::component::linearsolver
+{
+template<typename TBloc, typename TVecBloc, typename TVecIndex>
+class CompressedRowSparseMatrix;
+}
 
 namespace sofa::component::forcefield
 {
@@ -306,6 +311,12 @@ protected:
     type::vector<Transformation>  _initialTransformation;
     void initSVD(Index i, Index&a, Index&b, Index&c, Index&d);
     void accumulateForceSVD( Vector& f, const Vector & p, typename VecElement::const_iterator elementIt, Index elementIndex );
+
+    /// Specialized addKToMatrix implementation for CRS matrices
+    template<class BlocReal>
+    void addKToBlocMatrix(
+            sofa::component::linearsolver::CompressedRowSparseMatrix<type::Mat<3,3,BlocReal>,  type::vector<type::Mat<3,3,BlocReal> >, type::vector<sofa::Index> > *crsmat,
+            SReal k, unsigned int &offset);
 
     void applyStiffnessCorotational( Vector& f, const Vector& x, Index i=0, Index a=0,Index b=1,Index c=2,Index d=3, SReal fact=1.0  );
 
