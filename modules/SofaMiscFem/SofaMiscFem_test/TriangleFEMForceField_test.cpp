63eed16e6f828941928e70abdc247427af7862a4
epernod <erik.pernod@gmail.com>
2021-07-13 17:04:28 +0200

    [SofaMiscFEM_test] Add test for TriangleFEMForceField and TriangularFEMForceField on init values and values after several timesteps

--- modules/SofaMiscFem/SofaMiscFem_test/TriangleFEMForceField_test.cpp
+++ modules/SofaMiscFem/SofaMiscFem_test/TriangleFEMForceField_test.cpp
@@ -43,6 +43,8 @@ using std::string;
 #include <sofa/helper/system/thread/CTime.h>
 #include <limits>
 
+#define PERFORMANCE_TESTS 0
+
 namespace sofa
 {
 using namespace sofa::defaulttype;
@@ -60,15 +62,13 @@ public:
     typedef MechanicalObject<DataTypes> MState;
     using TriangleFEM = sofa::component::forcefield::TriangleFEMForceField<DataTypes>;
     using TriangularFEM = sofa::component::forcefield::TriangularFEMForceField<DataTypes>;
-
+    using Vec3 = type::Vec<3, Real>;
+    using Mat33 = type::Mat<3, 3, Real>;
+    using Mat63 = type::Mat<6, 3, Real>;
 
 protected:
     simulation::Simulation* m_simulation = nullptr;
     simulation::Node::SPtr m_root;
-
-    int m_nbrStep = 100;
-    //std::vector<int> m_indices = { 0, 20, 40, 1200, 1560 };
-    std::vector<int> m_indices = { 3};
 
     ctime_t timeTicks = sofa::helper::system::thread::CTime::getRefTicksPerSec();
 
@@ -148,7 +148,7 @@ public:
     {
         Node::SPtr FEMNode = sofa::simpleapi::createChild(m_root, nodeName);
         createObject(FEMNode, "EulerImplicitSolver");
-        createObject(FEMNode, "CGLinearSolver", {{ "threshold", "1e-9" }});
+        createObject(FEMNode, "CGLinearSolver", {{ "iterations", "20" }, { "threshold", "1e-6" }});
 
         createObject(FEMNode, "MechanicalObject", {
             {"name","dof"}, {"template","Vec3d"}, {"position", "@../grid.position"} });
@@ -300,36 +300,86 @@ public:
     void checkInit(int FEMType)
     {
         createSingleTriangleFEMScene(FEMType, 100, 0.3, "large");
-        type::Mat<3, 3, Real> MatStiffness = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        
+        type::Vec<2, Mat33> exp_rotInit;
+        type::Vec<2, Mat33> exp_rotMat;
+        type::Vec<2, Mat33> exp_stiffnessMat;
+        type::Vec<2, Mat63> exp_strainDispl;
+
+        // 1st value expected values (square 2D triangle)
+        exp_rotInit[0] = Mat33(Vec3(0, 0, 0), Vec3(1, 0, 0), Vec3(0, 1, 0));
+        exp_rotMat[0] = Mat33(Vec3(1, 0, 0), Vec3(0, 1, 0), Vec3(0, 0, 1));
+        exp_stiffnessMat[0] = Mat33(Vec3(54.945053, 16.483517, 0), Vec3(16.483517, 54.945053, 0), Vec3(0, 0, 19.23077));
+        exp_strainDispl[0][0] = Vec3(-1, 0, -1); exp_strainDispl[0][1] = Vec3(0, -1, -1); exp_strainDispl[0][2] = Vec3(1, 0, 0);
+        exp_strainDispl[0][3] = Vec3(0, 0, 1); exp_strainDispl[0][4] = Vec3(0, 0, 1); exp_strainDispl[0][5] = Vec3(0, 1, 0);
+
+        // 2nd value expected values (isosceles 3D triangle)
+        exp_rotInit[1] = Mat33(Vec3(0, 0, 0), Vec3(1.4142135, 0, 0), Vec3(0.707107, 1.2247449, 0));
+        exp_rotMat[1] = Mat33(Vec3(0, -0.81649661, -0.57735), Vec3(0.707107, 0.40824831, -0.57735), Vec3(0.707107, -0.40824831, 0.57735));
+        exp_stiffnessMat[1] = Mat33(Vec3(95.1676, 28.550287, 0), Vec3(28.550287, 95.1676, 0), Vec3(0, 0, 33.30867));
+        exp_strainDispl[1][0] = Vec3(-1, 0, -1); exp_strainDispl[1][1] = Vec3(0, -1, -1); exp_strainDispl[1][2] = Vec3(1, 0, 0);
+        exp_strainDispl[1][3] = Vec3(0, 0, 1); exp_strainDispl[1][4] = Vec3(0, 0, 1); exp_strainDispl[1][5] = Vec3(0, 1, 0);
 
         if (FEMType == 0)
         {
             TriangleFEM::SPtr triFEM = m_root->getTreeObject<TriangleFEM>();
-            
-            std::cout << triFEM->getRotatedInitialElement(0) << std::endl;
-            std::cout << triFEM->getRotationMatrix(0) << std::endl;
-            std::cout << triFEM->getMaterialStiffness(0) << std::endl;
-            std::cout << triFEM->getStrainDisplacements(0) << std::endl;
 
-            std::cout << triFEM->getRotatedInitialElement(1) << std::endl;
-            std::cout << triFEM->getRotationMatrix(1) << std::endl;
-            std::cout << triFEM->getMaterialStiffness(1) << std::endl;
-            std::cout << triFEM->getStrainDisplacements(1) << std::endl;
+            for (int id = 0; id < 2; id++)
+            {
+                const type::fixed_array <Coord, 3>& rotInit = triFEM->getRotatedInitialElement(id);
+                const Mat33& rotMat = triFEM->getRotationMatrix(id);
+                const Mat33& stiffnessMat = triFEM->getMaterialStiffness(id);
+                const Mat63& strainDispl = triFEM->getStrainDisplacements(id);
 
+                for (int i = 0; i < 3; ++i)
+                {
+                    for (int j = 0; j < 3; ++j)
+                    {
+                        EXPECT_NEAR(rotInit[i][j], exp_rotInit[id][i][j], 1e-4);
+                        EXPECT_NEAR(rotMat[i][j], exp_rotMat[id][i][j], 1e-4);
+                        EXPECT_NEAR(stiffnessMat[i][j], exp_stiffnessMat[id][i][j], 1e-4);
+
+                        EXPECT_NEAR(strainDispl[i][j], exp_strainDispl[id][i][j], 1e-4);
+                        EXPECT_NEAR(strainDispl[i + 3][j], exp_strainDispl[id][i + 3][j], 1e-4);
+                    }
+                }
+            }
         }
         else if (FEMType == 1)
         {
             TriangularFEM::SPtr triFEM = m_root->getTreeObject<TriangularFEM>();
+            for (int id = 0; id < 2; id++)
+            {
+                TriangularFEM::TriangleInformation triangleInfo = triFEM->triangleInfo.getValue()[id];
+                const type::fixed_array <Coord, 3>& rotInit = triangleInfo.rotatedInitialElements;
+                const Mat33& rotMat = triangleInfo.initialTransformation;
+                const Mat33& stiffnessMat = triangleInfo.materialMatrix;
+                const Mat63& strainDispl = triangleInfo.strainDisplacementMatrix;
+
+                for (int i = 0; i < 3; ++i)
+                {
+                    for (int j = 0; j < 3; ++j)
+                    {
+                        EXPECT_NEAR(rotInit[i][j], exp_rotInit[id][i][j], 1e-4);
+                        EXPECT_NEAR(rotMat[i][j], exp_rotMat[id][i][j], 1e-4);
+                        EXPECT_NEAR(stiffnessMat[i][j], exp_stiffnessMat[id][i][j], 1e-4);
+
+                        EXPECT_NEAR(strainDispl[i][j], exp_strainDispl[id][i][j], 1e-4);
+                        EXPECT_NEAR(strainDispl[i + 3][j], exp_strainDispl[id][i + 3][j], 1e-4);
+                    }
+                }
+            }
         }
     }
 
 
 
-    void checkTriangleFEMValues()
+    void checkFEMValues(int FEMType)
     {
         // load Triangular FEM
         int nbrGrid = 40;
-        createGridFEMScene(0, nbrGrid);
+        int nbrStep = 100;
+        createGridFEMScene(FEMType, nbrGrid);
 
         if (m_root.get() == nullptr)
             return;
@@ -342,76 +392,83 @@ public:
         const VecCoord& positions = dofs->x.getValue();
         ASSERT_EQ(positions.size(), nbrGrid * nbrGrid);
         
-        
-        for (auto id : m_indices)
-        {
-            std::cout << "init: " << id << " -> " << positions[id] << std::endl;
-        }
-        
-        std::cout << "animate... " << std::endl;
+        EXPECT_NEAR(positions[1515][0], 8.97436, 1e-4);
+        EXPECT_NEAR(positions[1515][1], 9.48718, 1e-4);
+        EXPECT_NEAR(positions[1515][2], 0, 1e-4);
 
-        for (int i = 0; i < m_nbrStep; i++)
-        {
-            m_simulation->animate(m_root.get(), 0.01);
-        }
-        
-        for (auto id : m_indices)
-        {
-            std::cout << "end: " << id << " -> " << positions[id] << std::endl;
-        }
-    }
-
-    void checkTriangularFEMValues()
-    {
-        // load Triangular FEM
-        int nbrGrid = 40;
-        createGridFEMScene(1, nbrGrid);
-
-        if (m_root.get() == nullptr)
-            return;
-
-        // Access mstate
-        MState::SPtr dofs = m_root->getTreeObject<MState>();
-        ASSERT_TRUE(dofs.get() != nullptr);
-
-        // Access dofs
-        const VecCoord& positions = dofs->x.getValue();
-        ASSERT_EQ(positions.size(), nbrGrid * nbrGrid);
-        
-
-        for (auto id : m_indices)
-        {
-            std::cout << "init: " << id << " -> " << positions[id] << std::endl;
-        }
-
-        std::cout << "animate... " << std::endl;
-
-        for (int i = 0; i < m_nbrStep; i++)
+        for (int i = 0; i < nbrStep; i++)
         {
             m_simulation->animate(m_root.get(), 0.01);
         }
 
-        for (auto id : m_indices)
+        EXPECT_NEAR(positions[1515][0], 8.9135, 1e-4);
+        EXPECT_NEAR(positions[1515][1], 14.2499, 1e-4);
+        EXPECT_NEAR(positions[1515][2], 0, 1e-4);
+
+        // 1st value expected values (square 2D triangle)
+        static const Mat33 exp_rotInit = Mat33(Vec3(0, 0, 0), Vec3(0.25641, 0, 0), Vec3(0.25641, 0.25641, 0));
+        static const Mat33 exp_rotMat = Mat33(Vec3(0.99992, -0.0126608, 0), Vec3(0.0126608, 0.99992, 0), Vec3(0, 0, 1));
+        static const Mat33 exp_stiffnessMat = Mat33(Vec3(3.61243, 1.08373, 0), Vec3(1.08373, 3.61243, 0), Vec3(0, 0, 1.26435));
+        Mat63 exp_strainDispl;
+        exp_strainDispl[0] = Vec3(-3.89456, 0, -0.00185328); exp_strainDispl[1] = Vec3(0, -0.00185328, -3.89456); exp_strainDispl[2] = Vec3(3.89456, 0, -3.89816);
+        exp_strainDispl[3] = Vec3(0, -3.89816, 3.89456); exp_strainDispl[4] = Vec3(0, 0, 3.90001); exp_strainDispl[5] = Vec3(0, 3.90001, 0);
+
+        if (FEMType == 0)
         {
-            std::cout << "end: " << id << " -> " << positions[id] << std::endl;
+            TriangleFEM::SPtr triFEM = m_root->getTreeObject<TriangleFEM>();
+
+            const type::fixed_array <Coord, 3>& rotInit = triFEM->getRotatedInitialElement(42);
+            const Mat33& rotMat = triFEM->getRotationMatrix(42);
+            const Mat33& stiffnessMat = triFEM->getMaterialStiffness(42);
+            const Mat63& strainDispl = triFEM->getStrainDisplacements(42);
+
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    EXPECT_NEAR(rotInit[i][j], exp_rotInit[i][j], 1e-4);
+                    EXPECT_NEAR(rotMat[i][j], exp_rotMat[i][j], 1e-4);
+                    EXPECT_NEAR(stiffnessMat[i][j], exp_stiffnessMat[i][j], 1e-4);
+
+                    EXPECT_NEAR(strainDispl[i][j], exp_strainDispl[i][j], 1e-4);
+                    EXPECT_NEAR(strainDispl[i + 3][j], exp_strainDispl[i + 3][j], 1e-4);
+                }
+            }
+        }
+        else if (FEMType == 1)
+        {
+            TriangularFEM::SPtr triFEM = m_root->getTreeObject<TriangularFEM>();
+            
+            TriangularFEM::TriangleInformation triangleInfo = triFEM->triangleInfo.getValue()[42];
+            const type::fixed_array <Coord, 3>& rotInit = triangleInfo.rotatedInitialElements;
+            const Mat33& rotMat = triangleInfo.initialTransformation; // rotMat: [1 0 0,0 1 0,0 0 1]
+            const Mat33& stiffnessMat = triangleInfo.materialMatrix;
+            const Mat63& strainDispl = triangleInfo.strainDisplacementMatrix;
+
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    EXPECT_NEAR(rotInit[i][j], exp_rotInit[i][j], 1e-4);
+                    EXPECT_NEAR(rotMat[i][j], exp_rotMat[i][j], 1e-4);
+                    EXPECT_NEAR(stiffnessMat[i][j], exp_stiffnessMat[i][j], 1e-4);
+
+                    EXPECT_NEAR(strainDispl[i][j], exp_strainDispl[i][j], 1e-4);
+                    EXPECT_NEAR(strainDispl[i + 3][j], exp_strainDispl[i + 3][j], 1e-4);
+                }
+            }
         }
     }
 
 
-    void compareTriangleFEMValues()
-    {
-
-    }
-
-
-    void testTriangleFEMPerformance()
+    void testFEMPerformance(int FEMType)
     {
         // init
-        m_nbrStep = 1000;
+        int nbrStep = 1000;
         int nbrGrid = 40;
 
         // load Triangular FEM
-        createGridFEMScene(0, nbrGrid);
+        createGridFEMScene(FEMType, nbrGrid);
         if (m_root.get() == nullptr)
             return;
 
@@ -422,7 +479,7 @@ public:
         for (int i = 0; i < nbrTest; ++i)
         {
             ctime_t startTime = sofa::helper::system::thread::CTime::getRefTime();
-            for (int i = 0; i < m_nbrStep; i++)
+            for (int i = 0; i < nbrStep; i++)
             {
                 m_simulation->animate(m_root.get(), 0.01);
             }
@@ -443,56 +500,17 @@ public:
         //std::cout << "timeMin: " << timeMin << std::endl;
         //std::cout << "timeMax: " << timeMax << std::endl;
 
-        //Record:
-        //timeMean: 4.1545   ||   4.12943
-        //timeMin : 4.07156  ||   4.05766
-        //timeMax : 4.25747  ||   4.33603
-    }
-
-    void testTriangularFEMPerformance()
-    {
-        // init
-        m_nbrStep = 1000;
-        int nbrGrid = 40;
-
-        // load Triangular FEM
-        createGridFEMScene(1, nbrGrid);
-        if (m_root.get() == nullptr)
-            return;
-
-        int nbrTest = 10;
-        double diffTimeMs = 0;
-        double timeMin = std::numeric_limits<double>::max();
-        double timeMax = std::numeric_limits<double>::min();
-        for (int i = 0; i < nbrTest; ++i)
-        {
-            ctime_t startTime = sofa::helper::system::thread::CTime::getRefTime();
-            for (int i = 0; i < m_nbrStep; i++)
-            {
-                m_simulation->animate(m_root.get(), 0.01);
-            }
-
-            ctime_t diffTime = sofa::helper::system::thread::CTime::getRefTime() - startTime;
-            double diffTimed = sofa::helper::system::thread::CTime::toSecond(diffTime);
-
-            if (timeMin > diffTimed)
-                timeMin = diffTimed;
-            if (timeMax < diffTimed)
-                timeMax = diffTimed;
-
-            diffTimeMs += diffTimed;
-            m_simulation->reset(m_root.get());
-        }
-
-        //std::cout << "timeMean: " << diffTimeMs / nbrTest << std::endl;
-        //std::cout << "timeMin: " << timeMin << std::endl;
-        //std::cout << "timeMax: " << timeMax << std::endl;
-
-        //Record:
-        //timeMean: 5.32281   ||  5.21513   ||  4.32919
-        //timeMin : 5.171     ||  5.1868    ||  4.27747
-        //timeMax : 5.50842   ||  5.29571   ||  4.3987
-
+        // ### Record values ###
+        // {
+        //// TriangleFEMModel
+        //  timeMean: 4.1545   ||   4.12943
+        //  timeMin : 4.07156  ||   4.05766
+        //  timeMax : 4.25747  ||   4.33603
+        // 
+        //// TriangularFEMModel                 after optimisation
+        //  timeMean: 5.32281 || 5.21513 || 4.32919
+        //  timeMin : 5.171     ||  5.1868    ||  4.27747
+        //  timeMax : 5.50842   ||  5.29571   ||  4.3987
         // Optimisation:
         // addDForce: 0.00363014
         // addDForce: 0.00317752 -> getTriangle()[]
@@ -501,8 +519,8 @@ public:
         // addForce: 0.0011347
         // addForce: 0.00110351
         // addForce: 0.00094206
+        //}
     }
-
 };
 
 
@@ -539,12 +557,10 @@ TEST_F(TriangleFEMForceField3_test, checkTriangleFEMForceField_init)
     this->checkInit(0);
 }
 
-
-TEST_F(TriangleFEMForceField3_test, checkTriangleFEMValues)
+TEST_F(TriangleFEMForceField3_test, checkTriangleFEMForceField_values)
 {
-    this->checkTriangleFEMValues();
+    this->checkFEMValues(0);
 }
-
 
 
 
@@ -579,9 +595,9 @@ TEST_F(TriangleFEMForceField3_test, checkTriangularFEMForceField_init)
     this->checkInit(1);
 }
 
-TEST_F(TriangleFEMForceField3_test, checkTriangularFEMValues)
+TEST_F(TriangleFEMForceField3_test, checkTriangularFEMForceField_values)
 {
-    this->checkTriangularFEMValues();
+    this->checkFEMValues(1);
 }
 
 
@@ -589,12 +605,12 @@ TEST_F(TriangleFEMForceField3_test, checkTriangularFEMValues)
 #if(PERFORMANCE_TESTS)
 TEST_F(TriangleFEMForceField3_test, testTriangleFEMPerformance)
 {
-    this->testTriangleFEMPerformance();
+    this->testFEMPerformance(0);
 }
 
 TEST_F(TriangleFEMForceField3_test, testTriangularFEMPerformance)
 {
-    this->testTriangularFEMPerformance();
+    this->testFEMPerformance(1);
 }
 #endif
 } // namespace sofa
