800ed3eeba4c8828fcbfb973d40e99b9d81022d1
Frederick Roy <frederick.roy@inria.fr>
2021-08-25 15:33:58 +0200

    move image-related header from CImgPlugin to image

--- applications/plugins/image/ImageTypes.h
+++ applications/plugins/image/ImageTypes.h
@@ -37,7 +37,7 @@
 #include "VectorVis.h"
 
 //(imports + Image data structure + others) are in here
-#include <CImgPlugin/CImgData.h>
+#include <image/CImgData.h>
 
 #include <SofaBaseVisual/VisualModelImpl.h>
 #include <SofaBaseVisual/VisualStyle.h>
