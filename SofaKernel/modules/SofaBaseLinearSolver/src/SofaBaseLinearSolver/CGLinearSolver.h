dbfd469e58a74855314d5a3eddcfd54c174bb6b5
Damien Marchal <damien.marchal@univ-lille1.fr>
2021-09-07 15:37:10 +0200

    W

--- SofaKernel/modules/SofaBaseLinearSolver/src/SofaBaseLinearSolver/CGLinearSolver.h
+++ SofaKernel/modules/SofaBaseLinearSolver/src/SofaBaseLinearSolver/CGLinearSolver.h
@@ -22,10 +22,11 @@
 #pragma once
 #include <SofaBaseLinearSolver/config.h>
 
+#include <SofaBaseLinearSolver/MatrixLinearSolver[_].h>
 #include <SofaBaseLinearSolver/MatrixLinearSolver[GraphScatteredMatrix].h>
-#include <SofaBaseLinearSolver/MatrixLinearSolver[CompressedRowSparseMatrix].h>
-#include <SofaBaseLinearSolver/MatrixLinearSolver[SparseMatrix].h>
-#include <SofaBaseLinearSolver/MatrixLinearSolver[FullMatrix].h>
+#include <SofaBaseLinearSolver/CompressedRowSparseMatrix.h>
+#include <SofaBaseLinearSolver/FullMatrix.h>
+#include <SofaBaseLinearSolver/SparseMatrix.h>
 #include <sofa/helper/map.h>
 
 namespace sofa::component::linearsolver
