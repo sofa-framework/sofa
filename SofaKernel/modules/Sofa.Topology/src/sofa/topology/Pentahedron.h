4a29e92d42e11dd622cd150d5a1989762371bf62
Frederick Roy <frederick.roy@inria.fr>
2021-09-23 10:29:13 +0200

    consistency with other static constexpr

--- SofaKernel/modules/Sofa.Topology/src/sofa/topology/Pentahedron.h
+++ SofaKernel/modules/Sofa.Topology/src/sofa/topology/Pentahedron.h
@@ -30,5 +30,5 @@ namespace sofa::topology
 {
     using Pentahedron = sofa::topology::Element<sofa::geometry::Pentahedron>;
 
-    constexpr static Pentahedron InvalidPentahedron;
+    static constexpr Pentahedron InvalidPentahedron;
 }
