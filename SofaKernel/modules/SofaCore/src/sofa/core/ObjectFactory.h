6ec7050633e3f7d78360a8dc1e4f7dd0b9074aff
Damien Marchal <damien.marchal@univ-lille1.fr>
2021-11-29 16:43:37 +0100

    [SofaCore] Fix link error on windows.

--- SofaKernel/modules/SofaCore/src/sofa/core/ObjectFactory.h
+++ SofaKernel/modules/SofaCore/src/sofa/core/ObjectFactory.h
@@ -100,7 +100,7 @@ public:
         CreatorMap creatorMap;
         std::map<std::string, std::vector<std::string>> m_dataAlias ;
 
-        bool isADeprecatedAlias(const std::string& name);
+        bool SOFA_CORE_API isADeprecatedAlias(const std::string& name);
     };
     typedef std::map<std::string, ClassEntry::SPtr> ClassEntryMap;
 
