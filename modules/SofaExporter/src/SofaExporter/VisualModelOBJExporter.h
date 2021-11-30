8492c73ee2f4d1e5edd8dfec352843c76ce5101f
Guillaume Paran <guillaume.paran@sofa-framework.org>
2021-11-30 17:59:32 +0100

    [SofaExporter] CLEAN VisualModelOBJExporter alias

--- modules/SofaExporter/src/SofaExporter/VisualModelOBJExporter.h
+++ modules/SofaExporter/src/SofaExporter/VisualModelOBJExporter.h
@@ -50,24 +50,26 @@ protected:
     ~VisualModelOBJExporter() override;
 };
 
-using OBJExporter [[deprecated("The OBJExporter is now deprecated, please use VisualModelOBJExporter instead. Compatibility stops at v22.06")]] = VisualModelOBJExporter;
-
 } // namespace _visualmodelobjexporter_
-
 
 // Import the object in the exporter namespace to avoid having all the object straight in component.
 namespace exporter
 {
+    using OBJExporter
+        SOFA_ATTRIBUTE_DEPRECATED("v21.12", "v22.06", "Use VisualModelOBJExporter instead.")
+        = _visualmodelobjexporter_::VisualModelOBJExporter;
+
     using VisualModelOBJExporter = _visualmodelobjexporter_::VisualModelOBJExporter;
 } // namespace exporter
 
-
 // Import the object in the "old" namespaces to allow smooth update of code base.
-using VisualModelOBJExporter SOFA_ATTRIBUTE_DEPRECATED__SOFAEXPORTER_NAMESPACE_2106() = _visualmodelobjexporter_::VisualModelOBJExporter;
-namespace misc
-{
-    using VisualModelOBJExporter SOFA_ATTRIBUTE_DEPRECATED__SOFAEXPORTER_NAMESPACE_1712() = _visualmodelobjexporter_::VisualModelOBJExporter;
+using OBJExporter
+    SOFA_ATTRIBUTE_DISABLED__SOFAEXPORTER_NAMESPACE_2106()
+    = DeprecatedAndRemoved;
+namespace misc {
+    using OBJExporter
+        SOFA_ATTRIBUTE_DISABLED__SOFAEXPORTER_NAMESPACE_1712()
+        = DeprecatedAndRemoved;
 } // namespace misc
-
 
 } // namespace sofa::component
