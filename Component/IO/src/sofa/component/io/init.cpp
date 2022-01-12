3d4ffd6fc0ee4d516c0560df42e9c24b5891dee5
Frederick Roy <frederick.roy@inria.fr>
2022-01-12 18:18:55 +0100

    fix indentation

--- Component/IO/src/sofa/component/io/init.cpp
+++ Component/IO/src/sofa/component/io/init.cpp
@@ -4,22 +4,22 @@ namespace sofa::component::io
 {
 
 extern "C" {
-	SOFA_EXPORT_DYNAMIC_LIBRARY void initExternalModule();
-	SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleName();
+    SOFA_EXPORT_DYNAMIC_LIBRARY void initExternalModule();
+    SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleName();
 }
 
 void initExternalModule()
 {
-	static bool first = true;
-	if (first)
-	{
-		first = false;
-	}
+    static bool first = true;
+    if (first)
+    {
+        first = false;
+    }
 }
 
 const char* getModuleName()
 {
-	return MODULE_NAME;
+    return MODULE_NAME;
 }
 
 } // namespace sofa::component::io
