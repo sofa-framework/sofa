3ad0499c18e4867c066e31058f5be9dfa3afd6c5
Frederick Roy <frederick.roy@inria.fr>
2022-01-17 17:13:54 +0100

    move back mechanical mapping in sofatopomapping

--- Component/Compat/Sofa.Component.Topology.Mapping.cmake
+++ Component/Compat/Sofa.Component.Topology.Mapping.cmake
@@ -6,13 +6,9 @@ list(APPEND HEADER_FILES
     ${SOFATOPOLOGYMAPPING_SRC}/Hexa2QuadTopologicalMapping.h
     ${SOFATOPOLOGYMAPPING_SRC}/Hexa2TetraTopologicalMapping.h
     ${SOFATOPOLOGYMAPPING_SRC}/IdentityTopologicalMapping.h
-    ${SOFATOPOLOGYMAPPING_SRC}/Mesh2PointMechanicalMapping.h
-    ${SOFATOPOLOGYMAPPING_SRC}/Mesh2PointMechanicalMapping.inl
     ${SOFATOPOLOGYMAPPING_SRC}/Mesh2PointTopologicalMapping.h
     ${SOFATOPOLOGYMAPPING_SRC}/Quad2TriangleTopologicalMapping.h
     ${SOFATOPOLOGYMAPPING_SRC}/SimpleTesselatedHexaTopologicalMapping.h
-    ${SOFATOPOLOGYMAPPING_SRC}/SimpleTesselatedTetraMechanicalMapping.h
-    ${SOFATOPOLOGYMAPPING_SRC}/SimpleTesselatedTetraMechanicalMapping.inl
     ${SOFATOPOLOGYMAPPING_SRC}/SimpleTesselatedTetraTopologicalMapping.h
     ${SOFATOPOLOGYMAPPING_SRC}/SubsetTopologicalMapping.h
     ${SOFATOPOLOGYMAPPING_SRC}/Tetra2TriangleTopologicalMapping.h
