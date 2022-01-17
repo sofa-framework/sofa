37b510583002295985b83b949806cbc0a9be2dac
Frederick Roy <frederick.roy@inria.fr>
2022-01-17 16:22:49 +0100

    create topology.utility from misctopo

---
+++ Component/Topology/Utility/src/sofa/component/topology/utility/init.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <sofa/component/topology/utility/config.h>
+
+namespace sofa::component::topology::utility
+{
+    SOFA_COMPONENT_TOPOLOGY_UTILITY_API void init();
+} // namespace sofa::component::topology::utility
