4943182e5bf0f18b9342f0cf21f94b144569e3b1
Frederick Roy <frederick.roy@inria.fr>
2022-02-02 15:13:31 +0100

    create sofa.playback instead of sofa.io.misc

--- Component/IO/Misc/wrapper/SofaExporter/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <sofa/component/io/misc/config.h>
-
-#ifdef SOFA_BUILD_SOFAEXPORTER
-#  define SOFA_SOFAEXPORTER_API SOFA_EXPORT_DYNAMIC_LIBRARY
-#else
-#  define SOFA_SOFAEXPORTER_API SOFA_IMPORT_DYNAMIC_LIBRARY
-#endif
