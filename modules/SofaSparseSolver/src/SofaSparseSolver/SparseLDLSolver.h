0c7c4e774cd1eb79518fea951c25ee9b83b7ccd5
Alex Bilger <alexbilger0@gmail.com>
2022-02-04 08:54:16 +0100

    Use generic class name

--- modules/SofaSparseSolver/src/SofaSparseSolver/SparseLDLSolver.h
+++ modules/SofaSparseSolver/src/SofaSparseSolver/SparseLDLSolver.h
@@ -80,7 +80,7 @@ public :
         {
             const std::string header = "SparseLDLSolver(" + std::string(arg->getAttribute("name", "")) + ")";
             msg_warning(header) << "Template is empty\n"
-                                << "By default SparseLDLSolver uses blocks with a single double (to handle all cases of simulations).\n"
+                                << "By default " << helper::NameDecoder::getClassName<T>() << " uses blocks with a single double (to handle all cases of simulations).\n"
                                 << "If you are using only 3D DOFs, you may consider using blocks of Matrix3 to speedup the calculations.\n"
                                 << "If it is the case, add " << "template=\"CompressedRowSparseMatrixMat3x3d\" " << "to this object in your scene\n"
                                 << "Otherwise, if you want to disable this message, add " << "template=\"CompressedRowSparseMatrixd\" " << ".";
