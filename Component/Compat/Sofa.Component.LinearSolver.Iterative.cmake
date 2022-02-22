6c789fc0a6b0ecac2ff7388244631aaacc7f61d1
Frederick Roy <frederick.roy@inria.fr>
2022-02-22 11:00:52 +0100

    move some linearsolver files to sofacore

--- Component/Compat/Sofa.Component.LinearSolver.Iterative.cmake
+++ Component/Compat/Sofa.Component.LinearSolver.Iterative.cmake
@@ -8,11 +8,7 @@ list(APPEND HEADER_FILES
     ${SOFAGENERALLINEARSOLVER_SRC}/MinResLinearSolver.h
     ${SOFAGENERALLINEARSOLVER_SRC}/MinResLinearSolver.inl
     ${SOFAPRECONDITIONER_SRC}/ShewchukPCGLinearSolver.h
-    
-    ${SOFABASELINEARSOLVER_SRC}/BlocMatrixWriter.h
-    ${SOFABASELINEARSOLVER_SRC}/DefaultMultiMatrixAccessor.h
-    ${SOFABASELINEARSOLVER_SRC}/GraphScatteredTypes.h
     ${SOFABASELINEARSOLVER_SRC}/MatrixLinearSolver.h
     ${SOFABASELINEARSOLVER_SRC}/MatrixLinearSolver.inl
-    ${SOFABASELINEARSOLVER_SRC}/SingleMatrixAccessor.h
+    ${SOFABASELINEARSOLVER_SRC}/GraphScatteredTypes.h
 )
