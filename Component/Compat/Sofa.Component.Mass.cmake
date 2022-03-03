ea35e69c5f2a40d0c9d1d071b54e11c2b13c559d
Frederick Roy <frederick.roy@inria.fr>
2022-03-03 23:53:45 +0100

    fix sofacuda

--- Component/Compat/Sofa.Component.Mass.cmake
+++ Component/Compat/Sofa.Component.Mass.cmake
@@ -5,6 +5,7 @@ list(APPEND HEADER_FILES
     ${SOFABASEMECHANICS_SRC}/AddMToMatrixFunctor.h
     ${SOFABASEMECHANICS_SRC}/DiagonalMass.h
     ${SOFABASEMECHANICS_SRC}/DiagonalMass.inl
+    ${SOFABASEMECHANICS_SRC}/MassType.h
     ${SOFABASEMECHANICS_SRC}/UniformMass.h
     ${SOFABASEMECHANICS_SRC}/UniformMass.inl
     ${SOFAMISCFORCEFIELD_SRC}/MeshMatrixMass.h
