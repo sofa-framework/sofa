afed9d5d0edcd8d9fd22e7a52cd2d51ec83f02c8
epernod <erik.pernod@gmail.com>
2022-04-07 11:10:37 +0200

    [SofaSimpleFem_test] Add init and value tests for fastTetrahedralCorotationalFEM

--- SofaKernel/modules/SofaSimpleFem/SofaSimpleFem_test/TetrahedronFEMForceField_test.cpp
+++ SofaKernel/modules/SofaSimpleFem/SofaSimpleFem_test/TetrahedronFEMForceField_test.cpp
@@ -431,6 +431,7 @@ public:
     {
         createSingleTetrahedronFEMScene(FEMType, 1000, 0.3, "large");
 
+        // Expected values
         Transformation exp_initRot = { Vec3(0, 0.816497, 0.57735), Vec3(-0.707107, -0.408248, 0.57735), Vec3(0.707107, -0.408248, 0.57735) };
         TetraCoord exp_initPos = { Coord(0, 0, 0), Coord(1.41421, 0, 0), Coord(0.707107, 1.22474, 0), Coord(0.707107, 0.408248, -0.57735) };
 
@@ -485,7 +486,70 @@ public:
             stiffnessMat = tetraInfo.materialMatrix;
             strainD = tetraInfo.strainDisplacementTransposedMatrix;
         }
+        else
+        {
+            typename FastTetraCorotationalFEM::SPtr tetraFEM = m_root->getTreeObject<FastTetraCorotationalFEM>();
+            ASSERT_TRUE(tetraFEM.get() != nullptr);
+
+            const FastTetraCorotationalFEM::TetrahedronRestInformation& tetraInfo = tetraFEM->tetrahedronInfo.getValue()[0];
+            initRot.transpose(tetraInfo.restRotation); // TODO check why transposed is stored in this version
+            curRot = initRot; // not needed at init.
+
+            // Expected specific values
+            TetraCoord exp_shapeVector = { Coord(0, 1, 0), Coord(0, 0, 1), Coord(1, 0, 0), Coord(-1, -1, -1) };
+            Transformation exp_linearDfDxDiag[4]; 
+            exp_linearDfDxDiag[0] = { Vec3(64.1026, 0, 0), Vec3(0, 224.359, 0), Vec3(0, 0, 64.1026) };
+            exp_linearDfDxDiag[1] = { Vec3(64.1026, 0, -0), Vec3(0, 64.1026, -0), Vec3(-0, -0, 224.359) };
+            exp_linearDfDxDiag[2] = { Vec3(224.359, 0, 0), Vec3(0, 64.1026, 0), Vec3(0, 0, 64.1026) };
+            exp_linearDfDxDiag[3] = { Vec3(352.5641, 160.25641, 160.25641), Vec3(160.25641, 352.5641, 160.25641), Vec3(160.25641, 160.25641, 352.5641) };
+
+            Transformation exp_linearDfDx[6];
+            exp_linearDfDx[0] = { Vec3(0, -0, 0), Vec3(0, 0, 64.1026), Vec3(0, 96.1538, 0) };
+            exp_linearDfDx[1] = { Vec3(0, 96.1538, 0), Vec3(64.1026, 0, 0), Vec3(0, 0, 0) };
+            exp_linearDfDx[2] = { Vec3(-64.1026, -96.1538, -0), Vec3(-64.1026, -224.359, -64.1026), Vec3(-0, -96.1538, -64.1026) };
+            exp_linearDfDx[3] = { Vec3(0, -0, 96.1538), Vec3(-0, 0, 0), Vec3(64.1026, 0, 0) };
+            exp_linearDfDx[4] = { Vec3(-64.1026, 0, -96.1538), Vec3(0, -64.1026, -96.1538), Vec3(-64.1026, -64.1026, -224.359) };
+            exp_linearDfDx[5] = { Vec3(-224.359, -64.1026, -64.1026), Vec3(-96.1538, -64.1026, -0), Vec3(-96.1538, -0, -64.1026) };
+   
+
+            // check rotations
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    EXPECT_NEAR(exp_initRot[i][j], initRot[i][j], 1e-4);
+                    EXPECT_NEAR(exp_curRot[i][j], curRot[i][j], 1e-4);
+                }
+            }
+
+            // check shapeVector
+            for (int i = 0; i < 4; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    EXPECT_NEAR(exp_shapeVector[i][j], tetraInfo.shapeVector[i][j], 1e-4);
+                }
+            }
+
+            // check DfDx
+            for (int id = 0; id < 4; ++id)
+            {
+                for (int i = 0; i < 3; ++i)
+                {
+                    for (int j = 0; j < 3; ++j)
+                    {
+                        EXPECT_NEAR(exp_linearDfDxDiag[id][i][j], tetraInfo.linearDfDxDiag[id][i][j], 1e-4);
+                        EXPECT_NEAR(exp_linearDfDx[id][i][j], tetraInfo.linearDfDx[id][i][j], 1e-4);
+                    }
+                }
+            }
+
+            // Fast method do not share the other data.
+            return;
+        }
             
+
+
         // check rotations
         for (int i = 0; i < 3; ++i)
         {
@@ -557,6 +621,28 @@ public:
         EXPECT_NEAR(positions[159][1], 45.0487, 1e-4);
         EXPECT_NEAR(positions[159][2], 30.0011, 1e-4);
 
+        // Expected values
+        Transformation exp_initRot = { Vec3(-1, 0, 0), Vec3(0, -0.8, -0.6), Vec3(0, -0.6, 0.8) };
+        TetraCoord exp_initPos = { Coord(0, 0, 0), Coord(3.33333, 0, 0), Coord(3.33333, 5.55556, 0), Coord(0, 3.55556, 2.66667) };
+
+        Transformation exp_curRot = { Vec3(-1, 8.01488e-06, 0.000541687), Vec3(-0.000320764, -0.814541, -0.580106), Vec3(0.000436576, -0.580106, 0.814541) };
+
+        MaterialStiffness exp_stiffnessMat = { Vec6(2.72596, 1.16827, 1.16827, 0, 0, 0), Vec6(1.16827, 2.72596, 1.16827, 0, 0, 0), Vec6(1.16827, 1.16827, 2.72596, 0, 0, 0),
+            Vec6(0, 0, 0, 0.778846, 0, 0), Vec6(0, 0, 0, 0, 0.778846, 0), Vec6(0, 0, 0, 0, 0, 0.778846) };
+
+        StrainDisplacement exp_strainD = { Vec6(-14.8148, 0, 0, 1.18424e-14, 0, -18.5185),
+            Vec6(0, 1.18424e-14, 0, -14.8148, -18.5185, 0),
+            Vec6(0, 0, -18.5185, 0, 1.18424e-14, -14.8148),
+            Vec6(14.8148, 0, 0, -8.88889, 0, 11.8519),
+            Vec6(0, -8.88889, 0, 14.8148, 11.8519, 0),
+            Vec6(0, 0, 11.8519, 0, -8.88889, 14.8148),
+            Vec6(-0, 0, 0, 8.88889, 0, -11.8519),
+            Vec6(0, 8.88889, 0, -0, -11.8519, 0),
+            Vec6(0, 0, -11.8519, 0, 8.88889, -0),
+            Vec6(0, 0, 0, -1.18424e-14, 0, 18.5185),
+            Vec6(0, -1.18424e-14, 0, 0, 18.5185, 0),
+            Vec6(0, 0, 18.5185, 0, -1.18424e-14, 0) };
+
 
         Transformation initRot(type::NOINIT);
         Transformation curRot(type::NOINIT);
@@ -591,28 +677,72 @@ public:
             stiffnessMat = tetraInfo.materialMatrix;
             strainD = tetraInfo.strainDisplacementTransposedMatrix;
         }
+        else
+        {
+            typename FastTetraCorotationalFEM::SPtr tetraFEM = m_root->getTreeObject<FastTetraCorotationalFEM>();
+            ASSERT_TRUE(tetraFEM.get() != nullptr);
+
+            const FastTetraCorotationalFEM::TetrahedronRestInformation& tetraInfo = tetraFEM->tetrahedronInfo.getValue()[100];
+            initRot.transpose(tetraInfo.restRotation); // TODO check why transposed is stored in this version
+            curRot = tetraInfo.rotation; 
+            
+            // Expected specific values
+            exp_curRot = { Vec3(0.99999985, 0.00032076406, -0.00043657642), Vec3(-0.00033142383, 0.99969634, -0.024639719), Vec3(0.00042854031, 0.024639861, 0.9996963) };
+            TetraCoord exp_shapeVector = { Coord(0.3, 0.224999, -0.3), Coord(-0.3, 0, 0.3), Coord(0, 0, -0.3), Coord(0, -0.224999, 0.3) };
+
+            Transformation exp_linearDfDxDiag[4];
+            exp_linearDfDxDiag[0] = { Vec3(865.38462, 320.51282, -427.35043), Vec3(320.51282, 678.4188, -320.51282), Vec3(-427.35043, -320.51282, 865.38462) };
+            exp_linearDfDxDiag[1] = { Vec3(769.23077, -0, -427.35043), Vec3(-0, 341.88034, 0), Vec3(-427.35043, 0, 769.23077) };
+            exp_linearDfDxDiag[2] = { Vec3(170.94017, -0, 0), Vec3(-0, 170.94017, -0), Vec3(0, -0, 598.2906) };
+            exp_linearDfDxDiag[3] = { Vec3(267.09402, -0, 0), Vec3(-0, 507.47863, -320.51282), Vec3(0, -320.51282, 694.44444) };
+
+            Transformation exp_linearDfDx[6];
+            exp_linearDfDx[0] = { Vec3(-769.23077, -192.30769, 427.35043), Vec3(-128.20513, -341.88034, 128.20513), Vec3(427.35043, 192.30769, -769.23077) };
+            exp_linearDfDx[1] = { Vec3(170.94017, 0, -170.94017), Vec3(0, 170.94017, -128.20513), Vec3(-256.41026, -192.30769, 598.2906) };
+            exp_linearDfDx[2] = { Vec3(-267.09402, -128.20513, 170.94017), Vec3(-192.30769, -507.47863, 320.51282), Vec3(256.41026, 320.51282, -694.44444) };
+            exp_linearDfDx[3] = { Vec3(-170.94017, -0, 170.94017), Vec3(-0, -170.94017, 0), Vec3(256.41026, 0, -598.2906) };
+            exp_linearDfDx[4] = { Vec3(170.94017, 128.20513, -170.94017), Vec3(192.30769, 170.94017, -192.30769), Vec3(-256.41026, -128.20513, 598.2906) };
+            exp_linearDfDx[5] = { Vec3(-170.94017, 0, -0), Vec3(0, -170.94017, 192.30769), Vec3(-0, 128.20513, -598.2906) };
+
+
+            // check rotations
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    EXPECT_NEAR(exp_initRot[i][j], initRot[i][j], 1e-4);
+                    EXPECT_NEAR(exp_curRot[i][j], curRot[i][j], 1e-4);
+                }
+            }
+
+            // check shapeVector
+            for (int i = 0; i < 4; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    EXPECT_NEAR(exp_shapeVector[i][j], tetraInfo.shapeVector[i][j], 1e-4);
+                }
+            }
+
+            // check DfDx
+            for (int id = 0; id < 4; ++id)
+            {
+                for (int i = 0; i < 3; ++i)
+                {
+                    for (int j = 0; j < 3; ++j)
+                    {
+                        EXPECT_NEAR(exp_linearDfDxDiag[id][i][j], tetraInfo.linearDfDxDiag[id][i][j], 1e-4);
+                        EXPECT_NEAR(exp_linearDfDx[id][i][j], tetraInfo.linearDfDx[id][i][j], 1e-4);
+                    }
+                }
+            }
+
+            // Fast method do not share the other data.
+            return;
+        }
         
 
-        Transformation exp_initRot = { Vec3(-1, 0, 0), Vec3(0, -0.8, -0.6), Vec3(0, -0.6, 0.8) };
-        TetraCoord exp_initPos = { Coord(0, 0, 0), Coord(3.33333, 0, 0), Coord(3.33333, 5.55556, 0), Coord(0, 3.55556, 2.66667) };
-
-        Transformation exp_curRot = { Vec3(-1, 8.01488e-06, 0.000541687), Vec3(-0.000320764, -0.814541, -0.580106), Vec3(0.000436576, -0.580106, 0.814541) };
-
-        MaterialStiffness exp_stiffnessMat = { Vec6(2.72596, 1.16827, 1.16827, 0, 0, 0), Vec6(1.16827, 2.72596, 1.16827, 0, 0, 0), Vec6(1.16827, 1.16827, 2.72596, 0, 0, 0),
-            Vec6(0, 0, 0, 0.778846, 0, 0), Vec6(0, 0, 0, 0, 0.778846, 0), Vec6(0, 0, 0, 0, 0, 0.778846) };
-
-        StrainDisplacement exp_strainD = { Vec6(-14.8148, 0, 0, 1.18424e-14, 0, -18.5185),
-            Vec6(0, 1.18424e-14, 0, -14.8148, -18.5185, 0),
-            Vec6(0, 0, -18.5185, 0, 1.18424e-14, -14.8148),
-            Vec6(14.8148, 0, 0, -8.88889, 0, 11.8519),
-            Vec6(0, -8.88889, 0, 14.8148, 11.8519, 0),
-            Vec6(0, 0, 11.8519, 0, -8.88889, 14.8148),
-            Vec6(-0, 0, 0, 8.88889, 0, -11.8519),
-            Vec6(0, 8.88889, 0, -0, -11.8519, 0),
-            Vec6(0, 0, -11.8519, 0, 8.88889, -0),
-            Vec6(0, 0, 0, -1.18424e-14, 0, 18.5185),
-            Vec6(0, -1.18424e-14, 0, 0, 18.5185, 0),
-            Vec6(0, 0, 18.5185, 0, -1.18424e-14, 0) };
+       
 
         // check rotations
         for (int i = 0; i < 3; ++i)
@@ -857,15 +987,15 @@ TEST_F(FastTetrahedralCorotationalForceField3_test, checkDefaultAttributes)
 //    this->checkWrongAttributes(2);
 //}
 
-//TEST_F(FastTetrahedralCorotationalForceField3_test, checkInit)
-//{
-//    this->checkInit(2);
-//}
-//
-//TEST_F(FastTetrahedralCorotationalForceField3_test, checkFEMValues)
-//{
-//    this->checkFEMValues(2);
-//}
+TEST_F(FastTetrahedralCorotationalForceField3_test, checkInit)
+{
+    this->checkInit(2);
+}
+
+TEST_F(FastTetrahedralCorotationalForceField3_test, checkFEMValues)
+{
+    this->checkFEMValues(2);
+}
 
 
 // performances tests. Disabled by default
