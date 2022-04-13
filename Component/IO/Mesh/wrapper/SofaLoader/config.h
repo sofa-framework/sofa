8c826fc7b5e8d8b223ef4b3214a2544c87e87a3d
Frederick Roy <frederick.roy@inria.fr>
2022-04-13 18:07:26 +0200

    move tests to the new modules

--- Component/IO/Mesh/wrapper/SofaLoader/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <sofa/component/io/mesh/config.h>
-
-#ifdef SOFA_BUILD_SOFALOADER
-#  define SOFA_SOFALOADER_API SOFA_EXPORT_DYNAMIC_LIBRARY
-#else
-#  define SOFA_SOFALOADER_API SOFA_IMPORT_DYNAMIC_LIBRARY
-#endif
