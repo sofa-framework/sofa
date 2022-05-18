e02d71b30baefa7396c972969642f80ade3eae45
Damien Marchal <damien.marchal@univ-lille1.fr>
2022-05-18 18:45:35 +0200

    [Sofa.Simulation.Graph] Add a test for issue #2954
    The test should fails on the CI until the fix is provided.

--- Sofa/framework/Simulation/Graph/test/Node_test.cpp
+++ Sofa/framework/Simulation/Graph/test/Node_test.cpp
@@ -87,6 +87,37 @@ TEST(Node_test, addObjectAtFront)
     ASSERT_EQ(b->getObjects()[1]->getName(), "A");
 }
 
+TEST(Node_test, addObjectPreservingContext)
+{
+    sofa::core::sptr<Node> root = sofa::simpleapi::createNode("root");
+
+    BaseObject::SPtr A = core::objectmodel::New<Dummy>("A");
+    BaseObject::SPtr B = core::objectmodel::New<Dummy>("B");
+
+    auto child1 = sofa::simpleapi::createChild(root, "child1");
+    auto child2 = sofa::simpleapi::createChild(root, "child2");
+
+    // add the created object into the node named 'child1'
+    child1->addObject(A);
+    child1->addObject(B);
+
+    // check that the two objects are in node1
+    ASSERT_NE(child1->getObject(A->getName()), nullptr);
+    ASSERT_NE(child1->getObject(B->getName()), nullptr);
+
+    ASSERT_NE(A->getPathName(), "/root/child1/A");
+    ASSERT_NE(B->getPathName(), "/root/child1/B");
+
+    // add the created object into the node named 'child1'
+    child2->addObject(A);
+    ASSERT_EQ(child1->getObject(A->getName()), nullptr); // not in child1 anymore
+    ASSERT_NE(child2->getObject(A->getName()), nullptr); // but in child2
+
+    // check that the pathname reflect the change
+    ASSERT_NE(A->getPathName(), "/root/child2/A");
+    ASSERT_NE(B->getPathName(), "/root/child1/B");
+}
+
 
 }// namespace sofa
 
