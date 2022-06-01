078ee37bb3b48bca11847c009f669f4a23de160d
Frederick ROY <fred.roy.me@gmail.com>
2022-06-01 17:37:02 +0900

    remove sofatest usage in plugins (2)

--- applications/plugins/SofaImplicitField/SofaImplicitField_test/ImplicitShape_test.cpp
+++ applications/plugins/SofaImplicitField/SofaImplicitField_test/ImplicitShape_test.cpp
@@ -13,7 +13,7 @@
 * more details.                                                               *
 *                                                                             *
 * You should have received a copy of the GNU General Public License along     *
-* with this program. If not, see <http://www.gnu.org/licenses/>.              *
+* with this program. If not, see <http://www.gnuSceneCreator_test.org/licenses/>.              *
 *******************************************************************************
 * Authors: The SOFA Team and external contributors (see Authors.txt)          *
 *                                                                             *
