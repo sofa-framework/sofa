b1486b89349da89c36953ba669aaf7ed5417752d
Frederick Roy <froy@lnrobo.com>
2022-08-19 11:56:59 +0900

    if a fullvector is detected, use direct access instead of BM virtual functions

--- Sofa/Component/Constraint/Lagrangian/Correction/src/sofa/component/constraint/lagrangian/correction/LinearSolverConstraintCorrection.h
+++ Sofa/Component/Constraint/Lagrangian/Correction/src/sofa/component/constraint/lagrangian/correction/LinearSolverConstraintCorrection.h
@@ -149,7 +149,7 @@ private:
     linearalgebra::BaseMatrix* systemMatrix_buf;
     linearalgebra::BaseVector* systemRHVector_buf;
     linearalgebra::BaseVector* systemLHVector_buf;
-
+    linearalgebra::FullVector<Real>* systemLHVector_buf_fullvector;
 
     // par un vecteur de listes precaclues pour chaque contrainte
     std::vector< ListIndex > Vec_I_list_dof;   // vecteur donnant la liste des indices des dofs par block de contrainte
