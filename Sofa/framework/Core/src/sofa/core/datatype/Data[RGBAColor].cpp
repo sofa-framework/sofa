ffec46e541b40b62810441f55c6472b20330ed62
Damien Marchal <damien.marchal@univ-lille1.fr>
2022-09-12 22:24:04 +0200

    [Sofa.Core] Move Data<RGBAColor> into Data[RGBAColor].h to match new typeinfo API.

--- Sofa/framework/Core/src/sofa/core/datatype/Data[RGBAColor].cpp
+++ Sofa/framework/Core/src/sofa/core/datatype/Data[RGBAColor].cpp
@@ -19,38 +19,31 @@
 *                                                                             *
 * Contact information: contact@sofa-framework.org                             *
 ******************************************************************************/
-#pragma once
-
-#include <vector>
-#include <sofa/type/fixed_array.h>
-#include <sofa/type/vector.h>
-#include <sofa/helper/set.h>
-//#include <sofa/type/RGBAColor.h>
-#include <typeinfo>
-#include <sofa/defaulttype/AbstractTypeInfo.h>
-#include <sofa/defaulttype/typeinfo/DataTypeInfoDynamicWrapper.h>
-#include <sofa/defaulttype/typeinfo/DataTypeInfo.h>
-//#include <sofa/defaulttype/typeinfo/TypeInfo_Bool.h>
-#include <sofa/defaulttype/typeinfo/TypeInfo_Integer.h>
-#include <sofa/defaulttype/typeinfo/TypeInfo_Mat.h>
-#include <sofa/defaulttype/typeinfo/TypeInfo_Quat.h>
-#include <sofa/defaulttype/typeinfo/TypeInfo_Scalar.h>
-#include <sofa/defaulttype/typeinfo/TypeInfo_Set.h>
-#include <sofa/defaulttype/typeinfo/TypeInfo_Text.h>
-#include <sofa/defaulttype/typeinfo/TypeInfo_Vec.h>
-#include <sofa/defaulttype/typeinfo/TypeInfo_FixedArray.h>
-#include <sofa/defaulttype/typeinfo/TypeInfo_BoundingBox.h>
-//#include <sofa/defaulttype/typeinfo/TypeInfo_RGBAColor.h>
+#define SOFA_CORE_DATATYPE_DATARGBACOLOR_DEFINITION
+#include <sofa/core/datatype/Data[RGBAColor].h>
+#include <sofa/defaulttype/typeinfo/TypeInfo_RGBAColor.h>
 #include <sofa/defaulttype/typeinfo/TypeInfo_Vector.h>
-#include <sofa/defaulttype/typeinfo/TypeInfo_RigidTypes.h>
-#include <sofa/defaulttype/typeinfo/TypeInfo_VecTypes.h>
-#include <sofa/defaulttype/typeinfo/TypeInfo_Topology.h>
 
-namespace sofa::defaulttype
+namespace sofa::core::objectmodel
 {
 
-/// We make an alias to wrap around the old name to the new one.
-template<class T>
-using VirtualTypeInfo = DataTypeInfoDynamicWrapper<DataTypeInfo<T>>;
+template<> bool Data<sofa::type::RGBAColor>::AbstractTypeInfoRegistration()
+{
+    sofa::defaulttype::TypeInfoRegistry::Set(sofa::defaulttype::TypeInfoId::GetTypeId<bool>(), 
+                                             sofa::defaulttype::VirtualTypeInfo<bool>::get(),
+                                             "Sofa.Core");
+    return true;
+}
 
-} /// namespace sofa::defaulttype
+template<> bool Data<sofa::type::vector<sofa::type::RGBAColor>>::AbstractTypeInfoRegistration()
+{
+    sofa::defaulttype::TypeInfoRegistry::Set(sofa::defaulttype::TypeInfoId::GetTypeId<sofa::type::vector<sofa::type::RGBAColor>>(), 
+                                             sofa::defaulttype::VirtualTypeInfo<sofa::type::vector<sofa::type::RGBAColor>>::get(),
+                                             "Sofa.Core");
+    return true;    
+}
+
+template class SOFA_CORE_API Data<sofa::type::RGBAColor>;
+template class SOFA_CORE_API Data<sofa::type::vector<sofa::type::RGBAColor>>;
+
+}
