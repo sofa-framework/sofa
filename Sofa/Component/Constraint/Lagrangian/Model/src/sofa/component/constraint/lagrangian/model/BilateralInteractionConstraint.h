0889fbde1e6f32955e66bc20972e970636b38873
epernod <erik.pernod@gmail.com>
2022-10-07 23:10:13 +0200

    Add deprecated lifecycle mechanism

--- Sofa/Component/Constraint/Lagrangian/Model/src/sofa/component/constraint/lagrangian/model/BilateralInteractionConstraint.h
+++ Sofa/Component/Constraint/Lagrangian/Model/src/sofa/component/constraint/lagrangian/model/BilateralInteractionConstraint.h
@@ -104,6 +104,9 @@ protected:
     Data<bool> keepOrientDiff; ///< keep the initial difference in orientation (only for rigids)
     std::vector<Vec3d> prevForces;
 
+    SOFA_ATTRIBUTE_DEPRECATED("v22.12", "v23.06", "Data activateAtIteration has been deprecated, please use the Data d_activate instead and an engine or a script to change the behavior at the right step (see PR #3327).")
+    Data<int> activateAtIteration; ///< activate constraint at specified interation (0 = always enabled, -1=disabled)
+
     // grouped square constraints
     bool squareXYZ[3];
     Deriv dfree_square_total;
@@ -119,6 +122,17 @@ public:
     void bwdInit() override {}
 
     void reinit() override;
+
+    /// Temporary function to warn the user when old attribute names are used
+    void parse(sofa::core::objectmodel::BaseObjectDescription* arg) override
+    {
+        Inherit::parse(arg);
+
+        if (arg->getAttribute("activateAtIteration"))
+        {
+            msg_warning() << "input data 'activateAtIteration' has been deprecated, please use 'activate' instead and an engine or a script to change the behavior at the right step (see PR #3327).";
+        }
+    }
 
     void buildConstraintMatrix(const ConstraintParams* cParams,
                                        DataMatrixDeriv &c1, DataMatrixDeriv &c2,
