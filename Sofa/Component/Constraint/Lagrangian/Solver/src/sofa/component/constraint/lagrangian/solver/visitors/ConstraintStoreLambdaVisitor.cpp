0cc063a03115673a046f4abdc661ee0bf868a9fa
Alex Bilger <alxbilger@users.noreply.github.com>
2022-10-20 05:44:41 +0200

    [Lagrangian.Solver] Move ConstraintStoreLambdaVisitor in visitors folder (#3392)
    * Move ConstraintStoreLambdaVisitor in visitors folder
    
    * Compatibility

--- Sofa/Component/Constraint/Lagrangian/Solver/src/sofa/component/constraint/lagrangian/solver/visitors/ConstraintStoreLambdaVisitor.cpp
+++ Sofa/Component/Constraint/Lagrangian/Solver/src/sofa/component/constraint/lagrangian/solver/visitors/ConstraintStoreLambdaVisitor.cpp
@@ -1,4 +1,4 @@
-﻿/******************************************************************************
+/******************************************************************************
 *                 SOFA, Simulation Open-Framework Architecture                *
 *                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
 *                                                                             *
@@ -19,7 +19,47 @@
 *                                                                             *
 * Contact information: contact@sofa-framework.org                             *
 ******************************************************************************/
-#pragma once
+#include <sofa/core/behavior/BaseConstraint.h>
 #include <sofa/component/constraint/lagrangian/solver/visitors/ConstraintStoreLambdaVisitor.h>
+#include <sofa/core/ConstraintParams.h>
 
-SOFA_DEPRECATED_HEADER("v22.12", "v23.06", "sofa/component/constraint/lagrangian/solver/visitors/ConstraintStoreLambdaVisitor.h")
+namespace sofa::component::constraint::lagrangian::solver
+{
+
+ConstraintStoreLambdaVisitor::ConstraintStoreLambdaVisitor(const sofa::core::ConstraintParams* cParams, const sofa::linearalgebra::BaseVector* lambda)
+:simulation::BaseMechanicalVisitor(cParams)
+,m_cParams(cParams)
+,m_lambda(lambda)
+{
+}
+
+simulation::Visitor::Result ConstraintStoreLambdaVisitor::fwdConstraintSet(simulation::Node* node, core::behavior::BaseConstraintSet* cSet)
+{
+    if (core::behavior::BaseConstraint *c = dynamic_cast<core::behavior::BaseConstraint*>(cSet) )
+    {
+        ctime_t t0 = begin(node, c);
+        c->storeLambda(m_cParams, m_cParams->lambda(), m_lambda);
+        end(node, c, t0);
+    }
+    return RESULT_CONTINUE;
+}
+
+void ConstraintStoreLambdaVisitor::bwdMechanicalMapping(simulation::Node* node, core::BaseMapping* map)
+{
+    SOFA_UNUSED(node);
+
+    sofa::core::MechanicalParams mparams(*m_cParams);
+    mparams.setDx(m_cParams->dx());
+    mparams.setF(m_cParams->lambda());
+    map->applyJT(&mparams, m_cParams->lambda(), m_cParams->lambda());
+}
+
+bool ConstraintStoreLambdaVisitor::stopAtMechanicalMapping(simulation::Node* node, core::BaseMapping* map)
+{
+    SOFA_UNUSED(node);
+    SOFA_UNUSED(map);
+
+    return false;
+}
+
+} // namespace sofa::component::constraint::lagrangian::solver
