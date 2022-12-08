db7b32cd4c274aeeb67522ad875bc978986cdbfa
Frederick Roy <froy@lnrobo.com>
2022-12-08 09:28:01 +0900

    apply insimo code for CRSMechanical (previous CRS) and clean ups

---
+++ Sofa/framework/LinearAlgebra/src/sofa/linearalgebra/CompressedRowSparseMatrixMechanical.cpp
@@ -0,0 +1,342 @@
+/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#define SOFA_COMPONENT_LINEARSOLVER_COMPRESSEDROWSPARSEMATRIXMECHANICAL_CPP
+#include <sofa/linearalgebra/CompressedRowSparseMatrixMechanical.h>
+
+namespace sofa::linearalgebra
+{
+
+template <> template <>
+inline void CompressedRowSparseMatrixMechanical<double>::filterValues(CompressedRowSparseMatrixMechanical<type::Mat<3, 3, double> >& M, filter_fn* filter, const Real ref, bool keepEmptyRows)
+{
+    M.compress();
+    nRow = M.rowSize();
+    nCol = M.colSize();
+    nBlockRow = 1;
+    nBlockCol = 1;
+    rowIndex.clear();
+    rowBegin.clear();
+    colsIndex.clear();
+    colsValue.clear();
+    btemp.clear();
+    skipCompressZero = true;
+    rowIndex.reserve(M.rowIndex.size() * 3);
+    rowBegin.reserve(M.rowBegin.size() * 3);
+    colsIndex.reserve(M.colsIndex.size() * 9);
+    colsValue.reserve(M.colsValue.size() * 9);
+
+    if constexpr(Policy::StoreTouchFlags) touchedBlock.clear();
+
+    Index vid = 0;
+    for (std::size_t rowId = 0; rowId < M.rowIndex.size(); ++rowId)
+    {
+        Index i = M.rowIndex[rowId] * 3;
+
+        Range rowRange(M.rowBegin[rowId], M.rowBegin[rowId + 1]);
+
+        for (Index lb = 0; lb < 3; lb++)
+        {
+            rowIndex.push_back(i + lb);
+            rowBegin.push_back(vid);
+
+            for (std::size_t xj = static_cast<std::size_t>(rowRange.begin()); xj < static_cast<std::size_t>(rowRange.end()); ++xj)
+            {
+                Index j = M.colsIndex[xj] * 3;
+                type::Mat<3, 3, double> b = M.colsValue[xj];
+                if ((*filter)(i + lb, j + 0, b[lb][0], ref))
+                {
+                    colsIndex.push_back(j + 0);
+                    colsValue.push_back(b[lb][0]);
+                    if constexpr(Policy::StoreTouchFlags) touchedBlock.push_back(true);
+                    ++vid;
+                }
+                if ((*filter)(i + lb, j + 1, b[lb][1], ref))
+                {
+                    colsIndex.push_back(j + 1);
+                    colsValue.push_back(b[lb][1]);
+                    if constexpr(Policy::StoreTouchFlags) touchedBlock.push_back(true);
+                    ++vid;
+                }
+                if ((*filter)(i + lb, j + 2, b[lb][2], ref))
+                {
+                    colsIndex.push_back(j + 2);
+                    colsValue.push_back(b[lb][2]);
+                    if constexpr(Policy::StoreTouchFlags) touchedBlock.push_back(true);
+                    ++vid;
+                }
+            }
+
+            if (!keepEmptyRows && rowBegin.back() == vid)   // row was empty
+            {
+                rowIndex.pop_back();
+                rowBegin.pop_back();
+            }
+        }
+    }
+    rowBegin.push_back(vid); // end of last row
+}
+
+template <> template <>
+inline void CompressedRowSparseMatrixMechanical<double>::filterValues(CompressedRowSparseMatrixMechanical<type::Mat<3, 3, float> >& M, filter_fn* filter, const Real ref, bool keepEmptyRows)
+{
+    M.compress();
+    nRow = M.rowSize();
+    nCol = M.colSize();
+    nBlockRow = 1;
+    nBlockCol = 1;
+    rowIndex.clear();
+    rowBegin.clear();
+    colsIndex.clear();
+    colsValue.clear();
+    skipCompressZero = true;
+    btemp.clear();
+    rowIndex.reserve(M.rowIndex.size() * 3);
+    rowBegin.reserve(M.rowBegin.size() * 3);
+    colsIndex.reserve(M.colsIndex.size() * 9);
+    colsValue.reserve(M.colsValue.size() * 9);
+
+    if constexpr(Policy::StoreTouchFlags) touchedBlock.clear();
+
+    Index vid = 0;
+    for (std::size_t rowId = 0; rowId < M.rowIndex.size(); ++rowId)
+    {
+        Index i = M.rowIndex[rowId] * 3;
+
+        Range rowRange(M.rowBegin[rowId], M.rowBegin[rowId + 1]);
+
+        for (Index lb = 0; lb < 3; lb++)
+        {
+            rowIndex.push_back(i + lb);
+            rowBegin.push_back(vid);
+
+            for (std::size_t xj = static_cast<std::size_t>(rowRange.begin()); xj < static_cast<std::size_t>(rowRange.end()); ++xj)
+            {
+                Index j = M.colsIndex[xj] * 3;
+                type::Mat<3, 3, double> b = M.colsValue[xj];
+                if ((*filter)(i + lb, j + 0, b[lb][0], ref))
+                {
+                    colsIndex.push_back(j + 0);
+                    colsValue.push_back(b[lb][0]);
+                    if constexpr(Policy::StoreTouchFlags) touchedBlock.push_back(true);
+                    ++vid;
+                }
+                if ((*filter)(i + lb, j + 1, b[lb][1], ref))
+                {
+                    colsIndex.push_back(j + 1);
+                    colsValue.push_back(b[lb][1]);
+                    if constexpr(Policy::StoreTouchFlags) touchedBlock.push_back(true);
+                    ++vid;
+                }
+                if ((*filter)(i + lb, j + 2, b[lb][2], ref))
+                {
+                    colsIndex.push_back(j + 2);
+                    colsValue.push_back(b[lb][2]);
+                    if constexpr(Policy::StoreTouchFlags) touchedBlock.push_back(true);
+                    ++vid;
+                }
+            }
+
+            if (!keepEmptyRows && rowBegin.back() == vid)   // row was empty
+            {
+                rowIndex.pop_back();
+                rowBegin.pop_back();
+            }
+        }
+    }
+    rowBegin.push_back(vid); // end of last row
+}
+
+template <> template <>
+inline void CompressedRowSparseMatrixMechanical<float>::filterValues(CompressedRowSparseMatrixMechanical<type::Mat<3, 3, float> >& M, filter_fn* filter, const Real ref, bool keepEmptyRows)
+{
+    M.compress();
+    nRow = M.rowSize();
+    nCol = M.colSize();
+    nBlockRow = 1;
+    nBlockCol = 1;
+    rowIndex.clear();
+    rowBegin.clear();
+    colsIndex.clear();
+    colsValue.clear();
+    skipCompressZero = true;
+    btemp.clear();
+    rowIndex.reserve(M.rowIndex.size() * 3);
+    rowBegin.reserve(M.rowBegin.size() * 3);
+    colsIndex.reserve(M.colsIndex.size() * 9);
+    colsValue.reserve(M.colsValue.size() * 9);
+
+    if constexpr(Policy::StoreTouchFlags) touchedBlock.clear();
+
+    Index vid = 0;
+    for (std::size_t rowId = 0; rowId < M.rowIndex.size(); ++rowId)
+    {
+        Index i = M.rowIndex[rowId] * 3;
+
+        Range rowRange(M.rowBegin[rowId], M.rowBegin[rowId + 1]);
+
+        for (Index lb = 0; lb < 3; lb++)
+        {
+            rowIndex.push_back(i + lb);
+            rowBegin.push_back(vid);
+
+            for (std::size_t xj = static_cast<std::size_t>(rowRange.begin()); xj < static_cast<std::size_t>(rowRange.end()); ++xj)
+            {
+                Index j = M.colsIndex[xj] * 3;
+                type::Mat<3, 3, float> b = M.colsValue[xj];
+                if ((*filter)(i + lb, j + 0, b[lb][0], ref))
+                {
+                    colsIndex.push_back(j + 0);
+                    colsValue.push_back(b[lb][0]);
+                    if constexpr(Policy::StoreTouchFlags) touchedBlock.push_back(true);
+                    ++vid;
+                }
+                if ((*filter)(i + lb, j + 1, b[lb][1], ref))
+                {
+                    colsIndex.push_back(j + 1);
+                    colsValue.push_back(b[lb][1]);
+                    if constexpr(Policy::StoreTouchFlags) touchedBlock.push_back(true);
+                    ++vid;
+                }
+                if ((*filter)(i + lb, j + 2, b[lb][2], ref))
+                {
+                    colsIndex.push_back(j + 2);
+                    colsValue.push_back(b[lb][2]);
+                    if constexpr(Policy::StoreTouchFlags) touchedBlock.push_back(true);
+                    ++vid;
+                }
+            }
+
+            if (!keepEmptyRows && rowBegin.back() == vid)   // row was empty
+            {
+                rowIndex.pop_back();
+                rowBegin.pop_back();
+            }
+        }
+    }
+    rowBegin.push_back(vid); // end of last row
+}
+
+template <> template <>
+inline void CompressedRowSparseMatrixMechanical<float>::filterValues(CompressedRowSparseMatrixMechanical<type::Mat<3, 3, double> >& M, filter_fn* filter, const Real ref, bool keepEmptyRows)
+{
+    M.compress();
+    nRow = M.rowSize();
+    nCol = M.colSize();
+    nBlockRow = 1;
+    nBlockCol = 1;
+    rowIndex.clear();
+    rowBegin.clear();
+    colsIndex.clear();
+    colsValue.clear();
+    skipCompressZero = true;
+    btemp.clear();
+    rowIndex.reserve(M.rowIndex.size() * 3);
+    rowBegin.reserve(M.rowBegin.size() * 3);
+    colsIndex.reserve(M.colsIndex.size() * 9);
+    colsValue.reserve(M.colsValue.size() * 9);
+
+    if constexpr(Policy::StoreTouchFlags) touchedBlock.clear();
+
+    Index vid = 0;
+    for (std::size_t rowId = 0; rowId < M.rowIndex.size(); ++rowId)
+    {
+        Index i = M.rowIndex[rowId] * 3;
+
+        Range rowRange(M.rowBegin[rowId], M.rowBegin[rowId + 1]);
+
+        for (Index lb = 0; lb < 3; lb++)
+        {
+            rowIndex.push_back(i + lb);
+            rowBegin.push_back(vid);
+
+            for (std::size_t xj = static_cast<std::size_t>(rowRange.begin()); xj < static_cast<std::size_t>(rowRange.end()); ++xj)
+            {
+                Index j = M.colsIndex[xj] * 3;
+                type::Mat<3, 3, float> b = M.colsValue[xj];
+                if ((*filter)(i + lb, j + 0, b[lb][0], ref))
+                {
+                    colsIndex.push_back(j + 0);
+                    colsValue.push_back(b[lb][0]);
+                    if constexpr(Policy::StoreTouchFlags) touchedBlock.push_back(true);
+                    ++vid;
+                }
+                if ((*filter)(i + lb, j + 1, b[lb][1], ref))
+                {
+                    colsIndex.push_back(j + 1);
+                    colsValue.push_back(b[lb][1]);
+                    if constexpr(Policy::StoreTouchFlags) touchedBlock.push_back(true);
+                    ++vid;
+                }
+                if ((*filter)(i + lb, j + 2, b[lb][2], ref))
+                {
+                    colsIndex.push_back(j + 2);
+                    colsValue.push_back(b[lb][2]);
+                    if constexpr(Policy::StoreTouchFlags) touchedBlock.push_back(true);
+                    ++vid;
+                }
+            }
+
+            if (!keepEmptyRows && rowBegin.back() == vid)   // row was empty
+            {
+                rowIndex.pop_back();
+                rowBegin.pop_back();
+            }
+        }
+    }
+    rowBegin.push_back(vid); // end of last row
+}
+
+using namespace sofa::type;
+
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<float>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat1x1f>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat2x2f>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat3x3f>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat4x4f>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat<6, 6, float> >;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat<8, 8, float> >;
+
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<double>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat1x1d>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat2x2d>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat3x3d>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat4x4d>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat<6, 6, double> >;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat<8, 8, double> >;
+
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<float, CRSMechanicalStoreTouchedPolicy>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat1x1f, CRSMechanicalStoreTouchedPolicy>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat2x2f, CRSMechanicalStoreTouchedPolicy>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat3x3f, CRSMechanicalStoreTouchedPolicy>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat4x4f, CRSMechanicalStoreTouchedPolicy>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat<6, 6, float>, CRSMechanicalStoreTouchedPolicy >;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat<8, 8, float>, CRSMechanicalStoreTouchedPolicy >;
+
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<double, CRSMechanicalStoreTouchedPolicy>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat1x1d, CRSMechanicalStoreTouchedPolicy>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat2x2d, CRSMechanicalStoreTouchedPolicy>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat3x3d, CRSMechanicalStoreTouchedPolicy>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat4x4d, CRSMechanicalStoreTouchedPolicy>;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat<6, 6, double>, CRSMechanicalStoreTouchedPolicy >;
+template class SOFA_LINEARALGEBRA_API CompressedRowSparseMatrixMechanical<Mat<8, 8, double>, CRSMechanicalStoreTouchedPolicy >;
+
+} // namespace sofa::linearalgebra
