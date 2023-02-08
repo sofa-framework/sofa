914a2b128cee24af995e980b5d761e3b4eb84402
Hugo Talbot <hugo.talbot@sofa-framework.org>
2023-02-08 21:25:17 +0100

    first changes on compat on SofaSimulationCommon and Graph

--- Sofa/framework/Compat/src/SofaSimulationCommon/xml/BaseMultiMappingElement.h
+++ Sofa/framework/Compat/src/SofaSimulationCommon/xml/BaseMultiMappingElement.h
@@ -22,4 +22,4 @@
 #pragma once
 #include <sofa/simulation/common/xml/BaseMultiMappingElement.h>
 
-SOFA_DEPRECATED_HEADER("v22.06", "v23.06", "sofa/simulation/common/xml/BaseMultiMappingElement.h")
+SOFA_DISABLED_HEADER("v22.06", "v23.06", "sofa/simulation/common/xml/BaseMultiMappingElement.h")
--- Sofa/framework/Compat/src/SofaSimulationGraph/DAGSimulation.h
+++ Sofa/framework/Compat/src/SofaSimulationGraph/DAGSimulation.h
@@ -22,4 +22,4 @@
 #pragma once
 #include <sofa/simulation/graph/DAGSimulation.h>
 
-SOFA_DEPRECATED_HEADER("v22.06", "v23.06", "sofa/simulation/graph/DAGSimulation.h")
+SOFA_DISABLED_HEADER("v22.06", "v23.06", "sofa/simulation/graph/DAGSimulation.h")
