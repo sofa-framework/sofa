98c8738765b48bc0e06531cd0cbbf8f4f687801f
Alex Bilger <alxbilger@users.noreply.github.com>
2023-03-29 12:00:45 +0200

    [GUI.Qt] Register meta type to fix asynchronous Qt call (#3749)

--- Sofa/GUI/Qt/src/sofa/gui/qt/QtMessageRedirection.h
+++ Sofa/GUI/Qt/src/sofa/gui/qt/QtMessageRedirection.h
@@ -19,60 +19,11 @@
 *                                                                             *
 * Contact information: contact@sofa-framework.org                             *
 ******************************************************************************/
-#include <sofa/gui/qt/init.h>
+#pragma once
 
-#include <sofa/gui/common/GUIManager.h>
-#include <sofa/gui/qt/RealGUI.h>
-#include <sofa/gui/qt/QtMessageRedirection.h>
+#include <QString>
 
 namespace sofa::gui::qt
 {
-
-    extern "C" {
-        SOFA_EXPORT_DYNAMIC_LIBRARY void initExternalModule();
-        SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleName();
-        SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleVersion();
-    }
-
-    void initExternalModule()
-    {
-        init();
-    }
-
-    const char* getModuleName()
-    {
-        return MODULE_NAME;
-    }
-
-    const char* getModuleVersion()
-    {
-        return MODULE_VERSION;
-    }
-
-    void init()
-    {
-        static bool first = true;
-        if (first)
-        {
-#if SOFA_GUI_QT_ENABLE_QGLVIEWER
-            sofa::gui::common::GUIManager::RegisterGUI("qglviewer", &sofa::gui::qt::RealGUI::CreateGUI, nullptr, 3);
-#endif
-
-#if SOFA_GUI_QT_ENABLE_QTVIEWER
-            sofa::gui::common::GUIManager::RegisterGUI("qt", &sofa::gui::qt::RealGUI::CreateGUI, nullptr, 2);
-#endif
-
-            // if ObjectStateListener is triggered (either by changing the message number, or by
-            // changing the component name) in a thread different than the main thread (=UI thread),
-            // a Qt event is launched through the queued connection system. For that, the event
-            // parameters must be known to Qt's meta-object system. This is what qRegisterMetaType
-            // does in the following instruction.
-            qRegisterMetaType<QVector<int> >("QVector<int>");
-
-            qInstallMessageHandler(redirectQtMessages);
-
-            first = false;
-        }
-    }
-
-} // namespace sofa::gui::qt
+void redirectQtMessages(QtMsgType type, const QMessageLogContext& context, const QString& msg);
+}
