ff9103a4f47439a99a33696b1ceedf13cfee0ee4
Alex Bilger <alexbilger0@gmail.com>
2023-04-06 09:49:29 +0200

    [examples] Introduce example for SquareMapping

--- Sofa/Component/Mapping/NonLinear/tests/SquareDistanceMapping_test.cpp
+++ Sofa/Component/Mapping/NonLinear/tests/SquareDistanceMapping_test.cpp
@@ -24,6 +24,7 @@
 
 #include <sofa/component/mapping/testing/MappingTestCreation.h>
 #include <sofa/component/topology/container/dynamic/EdgeSetTopologyContainer.h>
+#include <sofa/simulation/graph/SimpleApi.h>
 
 namespace sofa {
 namespace {
@@ -128,4 +129,144 @@ TYPED_TEST( SquareDistanceMappingTest , test )
 
 
 } // namespace
+
+/**
+ * This test checks two different methods to simulate a triple pendulum using quadratic springs:
+ * 1) A SquaredDistanceMapping is used to transform the DOFs from the 3d space to a 1d space
+ * (representing the squared distances between the DOFs). Then a spring acts in the 1d space
+ * 2) A combination of two mappings: DistanceMapping and SquareMapping. It also transforms the 3d
+ * space to a 1d space where a spring is added.
+ *
+ * Two methods are supposed to lead to the same result. This test checks that both mechanical
+ * objects are at the same position, velocity and force.
+ * However, this test can fail easily with different parameters (number of strings, number of time
+ * steps etc).
+ */
+struct SquareDistanceMappingCompare_test : NumericTest<SReal>
+{
+    simulation::Node::SPtr root;
+    simulation::Node::SPtr oneMapping;
+    simulation::Node::SPtr twoMappings;
+
+    void onSetUp() override
+    {
+        if (!simulation::getSimulation()) {
+            simulation::setSimulation(new simulation::graph::DAGSimulation()) ;
+        }
+
+        root = simulation::getSimulation()->createNewNode("root");
+
+        simpleapi::createObject(root, "RequiredPlugin", {{"pluginName", "Sofa.Component"}});
+        simpleapi::createObject(root, "DefaultAnimationLoop");
+        simpleapi::createObject(root, "StringMeshCreator", {{"name", "loader"}, {"resolution", "3"}});
+
+        oneMapping = simpleapi::createChild(root, "oneMapping");
+        twoMappings = simpleapi::createChild(root, "twoMappings");
+
+        for (const auto& node : {oneMapping, twoMappings})
+        {
+            simpleapi::createObject(node, "EulerImplicitSolver", {{"rayleighStiffness", "0.1"}, {"rayleighMass","0.1"}});
+            simpleapi::createObject(node, "EdgeSetTopologyContainer",
+                {{"position", "@../loader.position"}, {"edges", "@../loader.edges"}, {"name", "topology"}});
+            simpleapi::createObject(node, "MechanicalObject", {{"name", "defoDOF"}, {"template", "Vec3"}});
+            simpleapi::createObject(node, "EdgeSetGeometryAlgorithms");
+            simpleapi::createObject(node, "FixedConstraint", {{"indices", "0"}});
+            simpleapi::createObject(node, "DiagonalMass", {{"totalMass", "1e-2"}});
+        }
+
+        auto oneMappingExtension = simpleapi::createChild(oneMapping, "extensionsNode");
+        auto twoMappingsExtension = simpleapi::createChild(twoMappings, "extensionsNode");
+
+        simpleapi::createObject(oneMappingExtension, "MechanicalObject", {{"name", "extensionsDOF"}, {"template", "Vec1"}});
+        simpleapi::createObject(twoMappingsExtension, "MechanicalObject", {{"name", "extensionsDOF"}, {"template", "Vec1"}});
+
+        simpleapi::createObject(oneMappingExtension, "SquareDistanceMapping",
+                                {{"topology", "@../topology"}, {"input", "@../defoDOF"},
+                                 {"output", "@extensionsDOF"}, {"geometricStiffness", "1"},
+                                 {"applyRestPosition", "true"}});
+        simpleapi::createObject(oneMappingExtension, "RestShapeSpringsForceField", {{"template", "Vec1"}, {"stiffness", "10000"}});
+
+
+
+
+        simpleapi::createObject(twoMappingsExtension, "DistanceMapping",
+                                {{"topology", "@../topology"}, {"input", "@../defoDOF"},
+                                 {"output", "@extensionsDOF"}, {"geometricStiffness", "1"},
+                                 {"applyRestPosition", "true"}, {"computeDistance", "true"}});
+        auto distanceMappingNode = simpleapi::createChild(twoMappingsExtension, "square");
+        simpleapi::createObject(distanceMappingNode, "MechanicalObject", {{"name", "squaredDOF"}, {"template", "Vec1"}});
+        simpleapi::createObject(distanceMappingNode, "SquareMapping",
+                                        {{"input", "@../extensionsDOF"},
+                                         {"output", "@squaredDOF"}, {"geometricStiffness", "1"},
+                                         {"applyRestPosition", "true"}});
+        simpleapi::createObject(distanceMappingNode, "RestShapeSpringsForceField", {{"template", "Vec1"}, {"stiffness", "10000"}});
+
+    }
+
+    void compareMechanicalObjects(unsigned int timeStepCount)
+    {
+        const auto epsilon = 1e-7_sreal;
+
+        core::behavior::BaseMechanicalState* mstate0 = oneMapping->getMechanicalState();
+        ASSERT_NE(mstate0, nullptr);
+
+        core::behavior::BaseMechanicalState* mstate1 = twoMappings->getMechanicalState();
+        ASSERT_NE(mstate1, nullptr);
+
+        //position
+        {
+            sofa::type::vector<SReal> mstatex0(mstate0->getMatrixSize());
+            mstate0->copyToBuffer(mstatex0.data(), core::ConstVecCoordId::position(), mstate0->getMatrixSize());
+
+            sofa::type::vector<SReal> mstatex1(mstate1->getMatrixSize());
+            mstate1->copyToBuffer(mstatex1.data(), core::ConstVecCoordId::position(), mstate1->getMatrixSize());
+
+            EXPECT_LT(this->vectorMaxDiff(mstatex0, mstatex1), epsilon) << "Time step " << timeStepCount
+                << "\n" << mstatex0 << "\n" << mstatex1;
+        }
+
+        //velocity
+        {
+            sofa::type::vector<SReal> mstatev0(mstate0->getMatrixSize());
+            mstate0->copyToBuffer(mstatev0.data(), core::ConstVecDerivId::velocity(), mstate0->getMatrixSize());
+
+            sofa::type::vector<SReal> mstatev1(mstate1->getMatrixSize());
+            mstate1->copyToBuffer(mstatev1.data(), core::ConstVecDerivId::velocity(), mstate1->getMatrixSize());
+
+            EXPECT_LT(this->vectorMaxDiff(mstatev0, mstatev1), epsilon) << "Time step " << timeStepCount
+                << "\n" << mstatev0 << "\n" << mstatev1;
+        }
+
+        //force
+        {
+            sofa::type::vector<SReal> mstatef0(mstate0->getMatrixSize());
+            mstate0->copyToBuffer(mstatef0.data(), core::ConstVecDerivId::force(), mstate0->getMatrixSize());
+
+            sofa::type::vector<SReal> mstatef1(mstate1->getMatrixSize());
+            mstate1->copyToBuffer(mstatef1.data(), core::ConstVecDerivId::force(), mstate1->getMatrixSize());
+
+            EXPECT_LT(this->vectorMaxDiff(mstatef0, mstatef1), epsilon) << "Time step " << timeStepCount
+                << "\n" << mstatef0 << "\n" << mstatef1;
+        }
+    }
+};
+
+TEST_F(SquareDistanceMappingCompare_test, compareToDistanceMappingAndSquareMapping)
+{
+    for (const auto& node : {oneMapping, twoMappings})
+    {
+        simpleapi::createObject(node, "CGLinearSolver", {{"iterations", "1e4"}, {"tolerance", "1.0e-9"}, {"threshold", "1.0e-9"}});
+    }
+
+    simulation::getSimulation()->init(root.get());
+
+    for (unsigned int i = 0 ; i < 100; ++i)
+    {
+        simulation::getSimulation()->animate(root.get(), 0.01_sreal);
+
+        compareMechanicalObjects(i);
+    }
+}
+
+
 } // namespace sofa
