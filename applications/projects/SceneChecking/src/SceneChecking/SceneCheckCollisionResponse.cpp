03e45be00f56b6c87a521c88efb5cf5f85a8cadb
Hugo Talbot <hugo.talbot@sofa-framework.org>
2023-05-11 07:58:09 +0200

    Separate contribution in a separate function

--- applications/projects/SceneChecking/src/SceneChecking/SceneCheckCollisionResponse.cpp
+++ applications/projects/SceneChecking/src/SceneChecking/SceneCheckCollisionResponse.cpp
@@ -98,19 +98,24 @@ void SceneCheckCollisionResponse::doCheckOn(Node* node)
                 }
                 else
                 {
-                    type::vector<core::CollisionModel*> colModels;
-                    root->get<core::CollisionModel>(&colModels, core::objectmodel::BaseContext::SearchDown);
-                    for (auto model : colModels)
-                    {
-                        if(model->isContactStiffnessSet())
-                        {
-                            m_message <<"The data \"contactStiffness\" is set in the component " << model->getClassName() <<" named " << model->getName() << msgendl;
-                            m_message <<"This data is not used when using a FrictionContactConstraint collision response." << msgendl;
-                            m_message <<"Remove the data \"contactStiffness\" to remove this warning" << msgendl;
-                        }
-                    }
+                    checkIfContactStiffnessIsSet(root);
                 }
             }
+        }
+    }
+}
+
+void SceneCheckCollisionResponse::checkIfContactStiffnessIsSet(const sofa::core::objectmodel::BaseContext* root)
+{
+    type::vector<core::CollisionModel*> colModels;
+    root->get<core::CollisionModel>(&colModels, core::objectmodel::BaseContext::SearchDown);
+    for (auto model : colModels)
+    {
+        if(model->isContactStiffnessSet())
+        {
+            m_message <<"The data \"contactStiffness\" is set in the component " << model->getClassName() <<" named " << model->getName() << msgendl;
+            m_message <<"This data is not used when using a FrictionContactConstraint collision response." << msgendl;
+            m_message <<"Remove the data \"contactStiffness\" to remove this warning" << msgendl;
         }
     }
 }
