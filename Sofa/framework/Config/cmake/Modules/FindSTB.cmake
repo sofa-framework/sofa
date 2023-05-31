89db6faf82e4f5a5006a0f288c4fef1478b8fe44
Olivier Roussel <olivier.roussel@inria.fr>
2023-05-31 11:18:18 +0200

    [Config] Fix cross-compilation for embedded external libs (#3870)
    [Config] Fix cross-compilation by turning off cmake_find_root_path for embedded external libs

--- Sofa/framework/Config/cmake/Modules/FindSTB.cmake
+++ Sofa/framework/Config/cmake/Modules/FindSTB.cmake
@@ -1,5 +1,10 @@
 find_path(STB_INCLUDE_DIR stb_image.h
 		  HINTS ${STB_ROOT}
+		  # If cross-compiling and typically use CMAKE_FIND_ROOT_PATH variable,
+		  # each of its directory entry will be prepended to PATHS locations, and
+		  # STB_ROOT is set as an absolute path. So we have to disable this behavior
+		  # for such external libs
+		  NO_CMAKE_FIND_ROOT_PATH
 	)
 
 include(FindPackageHandleStandardArgs)
