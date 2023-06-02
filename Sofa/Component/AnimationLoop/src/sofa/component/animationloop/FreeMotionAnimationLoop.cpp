7488063335f14927c7ffb4b09ed60ba0faf85f83
Hugo Talbot <hugo.talbot@sofa-framework.org>
2023-06-02 14:18:05 +0200

    unrelated esthetic change in message in  FreeMotionAnimationLoop

--- Sofa/Component/AnimationLoop/src/sofa/component/animationloop/FreeMotionAnimationLoop.cpp
+++ Sofa/Component/AnimationLoop/src/sofa/component/animationloop/FreeMotionAnimationLoop.cpp
@@ -400,7 +400,7 @@ void FreeMotionAnimationLoop::computeFreeMotion(const sofa::core::ExecParams* pa
 
 int FreeMotionAnimationLoopClass = core::RegisterObject(R"(
 The animation loop to use with constraints.
-You must add this loop at the beginning of the scene if you are using constraints.")")
+You must add this loop at the beginning of the scene if you are using constraints.)")
                                    .add< FreeMotionAnimationLoop >()
                                    .addAlias("FreeMotionMasterSolver");
 
