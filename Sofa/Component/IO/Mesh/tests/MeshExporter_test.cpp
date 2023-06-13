b047f2182bff31f76a6989909ca6c3ccc801cd3f
Mikel De Iturrate <59921026+mikelitu@users.noreply.github.com>
2023-06-13 08:32:51 +0100

    Update Sofa/Component/IO/Mesh/tests/MeshExporter_test.cpp
    Co-authored-by: Alex Bilger <alxbilger@users.noreply.github.com>

--- Sofa/Component/IO/Mesh/tests/MeshExporter_test.cpp
+++ Sofa/Component/IO/Mesh/tests/MeshExporter_test.cpp
@@ -49,15 +49,6 @@ using sofa::helper::system::FileSystem ;
 
 using ::testing::Types;
 
-#if __has_include(<filesystem>)
-  #include <filesystem>
-  namespace fs = std::filesystem;
-#elif __has_include(<experimental/filesystem>)
-  #include <experimental/filesystem> 
-  namespace fs = std::experimental::filesystem;
-#else
-  error "Missing the <filesystem> header."
-#endif
 namespace {
 const std::string tempdir = sofa::helper::system::FileRepository::getTempPath() ;
 
