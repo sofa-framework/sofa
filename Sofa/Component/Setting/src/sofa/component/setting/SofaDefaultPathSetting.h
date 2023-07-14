02f9cb7f14f72b7c71a8ec686ee47213c481f429
Frederick Roy <froy@lnrobo.com>
2023-07-14 11:40:24 +0900

    remove unused recorder feature (not enabled for 10years)

--- Sofa/Component/Setting/src/sofa/component/setting/SofaDefaultPathSetting.h
+++ Sofa/Component/Setting/src/sofa/component/setting/SofaDefaultPathSetting.h
@@ -37,7 +37,6 @@ public:
 protected:
     SofaDefaultPathSetting();   ///<Default constructor.
 public:
-    sofa::core::objectmodel::Data<std::string> recordPath;  ///<Path where will be saved the data of the recorded simulation.
     sofa::core::objectmodel::Data<std::string> gnuplotPath; ///<Path where will be saved the gnuplot files.
 };
 
