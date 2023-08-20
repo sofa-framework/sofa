cac646f428e9385f8f7471e12737282baf54e3e6
epernod <erik.pernod@gmail.com>
2023-08-21 00:48:29 +0200

    [Topology] Add PointModifier and TriangleModifier classes

---
+++ Sofa/Component/Topology/Container/Dynamic/src/sofa/component/topology/container/dynamic/PointModifiers.h
@@ -0,0 +1,76 @@
+/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#pragma once
+#include <sofa/component/topology/container/dynamic/config.h>
+
+#include <sofa/type/vector.h>
+#include <sofa/core/topology/BaseTopology.h>
+
+namespace sofa::component::topology::container::dynamic
+{
+
+using PointID = core::topology::BaseMeshTopology::PointID;
+
+
+/**
+* This class store all the info to create a new point in the mesh taking into account estimated id
+* id of duplicated point if this point will be splitted due to a cut.
+* This structure also store all the ancestors and coefficient to efficently add this point into the current mesh.
+*/
+class PointToAdd
+{
+public:
+    PointToAdd(PointID uniqueID, PointID idPoint, const sofa::type::vector<PointID>& ancestors, const sofa::type::vector<SReal>& coefs)
+        : m_uniqueID(uniqueID)
+        , m_idPoint(idPoint)
+        , m_ancestors(ancestors)
+        , m_coefs(coefs)
+    {}
+    
+    /// Unique ID of this point structure. Will be a code combining ancestors ids
+    PointID m_uniqueID;
+    
+    /// Future pointID of this pointToAdd
+    PointID m_idPoint = sofa::InvalidID;
+    /// Future pointID of this pointToAdd if this point is duplicated due to a cut
+    PointID m_idClone = sofa::InvalidID;
+    
+    /// List of ancestors (existing point ID of the mesh)
+    sofa::type::vector<PointID> m_ancestors;
+    /// List of corresponding coefficients 
+    sofa::type::vector<SReal> m_coefs;
+};
+
+
+static PointID getUniqueId(PointID ancestor0, PointID ancestor1)
+{
+    PointID uniqID;
+    if (ancestor0 > ancestor1)
+        uniqID = 1000000 * ancestor0 + ancestor1;
+    else
+        uniqID = 1000000 * ancestor1 + ancestor0;
+
+    return uniqID;
+}
+
+
+} //namespace sofa::component::topology::container::dynamic
---
+++ Sofa/Component/Topology/Container/Dynamic/src/sofa/component/topology/container/dynamic/TriangleModifiers.h
@@ -0,0 +1,165 @@
+/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#pragma once
+#include <sofa/component/topology/container/dynamic/config.h>
+#include <sofa/component/topology/container/dynamic/PointModifiers.h>
+
+#include <sofa/type/vector.h>
+#include <sofa/core/topology/BaseTopology.h>
+
+namespace sofa::component::topology::container::dynamic
+{
+
+using TriangleID = core::topology::BaseMeshTopology::TriangleID;
+using EdgeID = core::topology::BaseMeshTopology::EdgeID;
+
+
+class TriangleToAdd
+{
+public:
+    TriangleToAdd(TriangleID uniqueID, sofa::core::topology::BaseMeshTopology::Triangle _triangle,
+        const type::vector<TriangleID>& ancestors, const type::vector<SReal>& coefs)
+        : m_uniqueID(uniqueID)
+        , m_triangle(_triangle)
+        , m_ancestors(ancestors)
+        , m_coefs(coefs)
+    {}
+
+    virtual ~TriangleToAdd() {}
+
+    TriangleID m_uniqueID;
+    sofa::core::topology::BaseMeshTopology::Triangle m_triangle;
+    type::vector<TriangleID> m_ancestors;
+    type::vector<SReal> m_coefs;
+};
+
+
+class TriangleToSplit
+{
+public:
+    TriangleToSplit(TriangleID triangleId, const sofa::core::topology::BaseMeshTopology::Triangle& triangle)
+        : m_triangleId(triangleId)
+        , m_triangle(triangle)
+    {}
+
+    virtual ~TriangleToSplit() {}
+
+    int localVertexId(PointID vertexIndex) 
+    {
+        for (unsigned int i = 0; i < 3; ++i)
+        {
+            if (m_triangle[i] == vertexIndex)
+                return i;
+        }
+
+        return InvalidID;
+    }
+
+    TriangleID m_triangleId;
+    sofa::core::topology::BaseMeshTopology::Triangle m_triangle;
+    type::vector<PointToAdd*> m_points;
+};
+
+
+class TriangleSubdivider
+{
+public:
+    TriangleSubdivider(TriangleToSplit* _triangleToSplit)
+        : m_triangleToSplit(_triangleToSplit)
+    {}
+
+    virtual bool subdivide(const sofa::type::Vec3& ptA, const sofa::type::Vec3& ptB, const sofa::type::Vec3& ptC) 
+    {
+        SOFA_UNUSED(ptA);
+        SOFA_UNUSED(ptB);
+        SOFA_UNUSED(ptC);
+        return false;
+    }
+
+    TriangleToSplit* m_triangleToSplit;
+    type::vector<TriangleToAdd*> m_trianglesToAdd;
+};
+
+
+class TriangleSubdivider_1Edge : public TriangleSubdivider
+{
+public:
+    TriangleSubdivider_1Edge(TriangleToSplit* _triangleToSplit) : TriangleSubdivider(_triangleToSplit)
+    {}
+
+    EdgeID m_edgeId;
+    SReal m_edgeCoef;
+
+    
+};
+
+class TriangleSubdivider_1Node : public TriangleSubdivider
+{
+public:
+    TriangleSubdivider_1Node(TriangleToSplit* _triangleToSplit) : TriangleSubdivider(_triangleToSplit) {}
+
+    bool subdivide(const sofa::type::Vec3& ptA, const sofa::type::Vec3& ptB, const sofa::type::Vec3& ptC) override
+    {
+        return true;
+    }
+};
+
+
+class TriangleSubdivider_2Edge : public TriangleSubdivider
+{
+public:
+    TriangleSubdivider_2Edge(TriangleToSplit* _triangleToSplit) : TriangleSubdivider(_triangleToSplit) {}
+
+    EdgeID m_edge1Id;
+    SReal m_edge1Coef;
+    EdgeID m_edge2Id;
+    SReal m_edge2Coef;
+};
+
+class TriangleSubdivider_2Node : public TriangleSubdivider
+{
+public:
+    TriangleSubdivider_2Node(TriangleToSplit* _triangleToSplit) : TriangleSubdivider(_triangleToSplit) {}
+
+    EdgeID m_edge1Id;
+    SReal m_edge1Coef;
+    sofa::type::Vec<3, SReal> m_baryCoords;
+};
+
+
+class TriangleSubdivider_3N : public TriangleSubdivider
+{
+public:
+    TriangleSubdivider_3N(TriangleToSplit* _triangleToSplit) : TriangleSubdivider(_triangleToSplit) {}
+
+    EdgeID m_edge1Id;
+    SReal m_edge1Coef;
+    EdgeID m_edge2Id;
+    SReal m_edge2Coef;
+    EdgeID m_edge3Id;
+    SReal m_edge3Coef;
+};
+
+
+
+
+} //namespace sofa::component::topology::container::dynamic
