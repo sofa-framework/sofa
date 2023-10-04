fca2b552f9b7e1c1bacdf4ec37e294a8485bbdeb
Alex Bilger <alxbilger@users.noreply.github.com>
2023-10-04 14:39:32 +0200

    [MultiThreading] Introduce parallel CG (#4138)
    * [MultiThreading] Introduce parallel CG
    
    * Setup task scheduler
    
    * implement multithreading
    
    * Add support for Mat3x3
    
    * better scene
    
    * Move ParallelCompressedRowSparseMatrixMechanical in its own file
    
    * introduce inl file
    
    * Cleanup
    
    * Fix compilation
    
    * Fix compilation
    
    * Register parallel implementation
    
    * Fix compilation
    
    * Fix performances when inserting by block
    
    * add missing required plugin
    
    * add regression on the introduced scene

---
+++ applications/plugins/MultiThreading/src/MultiThreading/component/linearsolver/iterative/ParallelCGLinearSolver.h
@@ -0,0 +1,54 @@
+﻿/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#pragma once
+
+#include <MultiThreading/TaskSchedulerUser.h>
+#include <sofa/component/linearsolver/iterative/CGLinearSolver.h>
+
+namespace multithreading::component::linearsolver::iterative
+{
+
+template<class TMatrix, class TVector>
+class ParallelCGLinearSolver :
+    public sofa::component::linearsolver::iterative::CGLinearSolver<TMatrix, TVector>,
+    public TaskSchedulerUser
+{
+public:
+    SOFA_CLASS2(SOFA_TEMPLATE2(ParallelCGLinearSolver,TMatrix,TVector),
+               SOFA_TEMPLATE2(sofa::component::linearsolver::iterative::CGLinearSolver,TMatrix,TVector),
+               TaskSchedulerUser);
+
+    using Matrix = TMatrix;
+    using Vector = TVector;
+
+    void init() override;
+
+    void solve(Matrix& A, Vector& x, Vector& b) override;
+};
+
+#if !defined(SOFA_MULTITHREADING_PARALLELCGLINEARSOLVER_CPP)
+extern template class SOFA_MULTITHREADING_PLUGIN_API
+ParallelCGLinearSolver< ParallelCompressedRowSparseMatrix<SReal>, sofa::linearalgebra::FullVector<SReal> >;
+ParallelCGLinearSolver< ParallelCompressedRowSparseMatrix<sofa::type::Mat<3, 3, Real> >, sofa::linearalgebra::FullVector<SReal> >;
+#endif
+
+}
