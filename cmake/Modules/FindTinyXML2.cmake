71275d4e6e141099a1172065e6ca377f09cdd228
Olivier Roussel <olivier.roussel@inria.fr>
2023-10-30 15:15:41 +0100

    Replace find or fetch mechanism by classical cmake find module.

--- cmake/Modules/FindTinyXML2.cmake
+++ cmake/Modules/FindTinyXML2.cmake
@@ -1,11 +1,46 @@
-cmake_minimum_required(VERSION 3.24)
+# Find the tinyxml2 headers and libraries
+# Behavior is to first look for config files, such as the one installed by some package
+# managers who provides their own cmake files. If no config files
+# were found, this tries to find the library by looking at headers / lib file.
+#
+# Defines:
+#   TinyXML2_FOUND : True if tinyxml2 is found
+#
+# Provides target tinyxml2::tinyxml2.
+find_package(tinyxml2 NO_MODULE QUIET)
 
-include(FetchContent)
-FetchContent_Declare(
-  tinyxml2
-  URL https://github.com/leethomason/tinyxml2/archive/refs/tags/9.0.0.tar.gz
-  URL_HASH MD5=afecd941107a8e74d3d1b4363cf52bd7
-  FIND_PACKAGE_ARGS NAMES TinyXML2
+if(TARGET tinyxml2::tinyxml2)
+  set(TinyXML2_FOUND TRUE) # only tinyxml2_FOUND has been set
+else()
+
+  if(NOT TinyXML2_INCLUDE_DIR)
+    find_path(TinyXML2_INCLUDE_DIR
+      NAMES tinyxml2.h
+      PATH_SUFFIXES include
+    )
+  endif()
+
+  if(NOT TinyXML2_LIBRARY)
+  find_library(TinyXML2_LIBRARY
+    NAMES tinyxml2
+    PATH_SUFFIXES lib
   )
-set(tinyxml2_SHARED_LIBS ON)
-FetchContent_MakeAvailable(tinyxml2)
+  endif()
+
+  if(TinyXML2_INCLUDE_DIR AND TinyXML2_LIBRARY)
+    set(TinyXML2_FOUND TRUE)
+  endif()
+
+  if(TinyXML2_FOUND)
+    set(TinyXML2_LIBRARIES ${TinyXML2_LIBRARY})
+    set(TinyXML2_INCLUDE_DIRS ${TinyXML2_INCLUDE_DIR})
+
+    if(NOT TARGET tinyxml2::tinyxml2)
+      add_library(tinyxml2::tinyxml2 INTERFACE IMPORTED)
+      set_property(TARGET tinyxml2::tinyxml2 PROPERTY INTERFACE_LINK_LIBRARIES "${TinyXML2_LIBRARIES}")
+      set_property(TARGET tinyxml2::tinyxml2 PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${TinyXML2_INCLUDE_DIR}")
+    endif()
+  else()
+  endif()
+  mark_as_advanced(TinyXML2_INCLUDE_DIR TinyXML2_LIBRARY)
+endif()
