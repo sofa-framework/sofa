49d4c2ead895bfb2d89aa0eda3a58fcfb073d91f
Paul Baksic <30337881+bakpaul@users.noreply.github.com>
2023-11-29 11:31:37 +0100

    [Core] Minor clean of DefaultAnimationLoop  (#4314)
    * Remove unlogical use of visitor
    
    * Delete unused methods

--- Sofa/framework/Simulation/Core/src/sofa/simulation/DefaultAnimationLoop.h
+++ Sofa/framework/Simulation/Core/src/sofa/simulation/DefaultAnimationLoop.h
@@ -69,10 +69,8 @@ protected :
 
     void behaviorUpdatePosition(const sofa::core::ExecParams* params, SReal dt) const;
     void updateInternalData(const sofa::core::ExecParams* params) const;
-    void resetConstraint(const sofa::core::ExecParams* params) const;
     void beginIntegration(const sofa::core::ExecParams* params, SReal dt) const;
     void propagateIntegrateBeginEvent(const sofa::core::ExecParams* params) const;
-    void buildConstraintMatrix(sofa::core::ConstraintParams cparams) const;
     void accumulateMatrixDeriv(sofa::core::ConstraintParams cparams) const;
     void solve(const sofa::core::ExecParams* params, SReal dt) const;
     void propagateIntegrateEndEvent(const sofa::core::ExecParams* params) const;
