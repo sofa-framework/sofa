29b1a137f21da92ee15f8d1873d7d5aee28deedd
Olivier Roussel <olivier.roussel@inria.fr>
2024-02-15 10:15:56 +0100

    [all] Replace tinyxml by external tinyxml2 (#4240)
    * Replace tinyxml by external tinyxml2.
    
    * Customize TinyXML2 cmake module to use a find _package or fetch content mechanism.
    
    * Replace find or fetch mechanism by classical cmake find module.
    
    * Fix missed cmake target name change
    
    * Handle error if tinyxml2 package not found but required
    
    * Fix compilation with Dump Visitor activated
    
    * Install libtinyxml
    
    * FIX error checking
    
    * Actually fix...
    
    * FIX error loading VTU xml files
    
    * FIX scene test
    
    ---------
    
    Co-authored-by: bakpaul <bakpaul@hotmail.fr>
    Co-authored-by: Paul Baksic <30337881+bakpaul@users.noreply.github.com>

--- Sofa/Component/Mass/tests/UniformMass_test.cpp
+++ Sofa/Component/Mass/tests/UniformMass_test.cpp
@@ -338,7 +338,7 @@ struct UniformMassTest :  public BaseTest
                 "<?xml version='1.0'?>"
                 "<Node 	name='Root' gravity='0 0 0' time='0' animate='0'   > "
                 "   <MechanicalObject position='0 0 0 4 5 6'/>               "
-                "   <UniformMass name='m_mass' totalMass='-8.0' vertexMass=-4.0/>   "
+                "   <UniformMass name='m_mass' totalMass='-8.0' vertexMass='-4.0'/>   "
                 "</Node>                                                     " ;
 
         const Node::SPtr root = SceneLoaderXML::loadFromMemory("loadWithNoParam", scene.c_str());
