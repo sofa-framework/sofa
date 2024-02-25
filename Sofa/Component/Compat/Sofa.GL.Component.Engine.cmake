adf7b83c9cbc90030061abe59d4ee5069c719498
Hugo Talbot <hugo.talbot@sofa-framework.org>
2024-02-25 21:27:40 +0100

    [all] Lifecycle v24.06 (2): remove Compat

--- Sofa/Component/Compat/Sofa.GL.Component.Engine.cmake
+++
@@ -1,6 +0,0 @@
-set(SOFAGENERALENGINE_SRC src/SofaGeneralEngine)
-
-list(APPEND HEADER_FILES
-    ${SOFAGENERALENGINE_SRC}/TextureInterpolation.h
-    ${SOFAGENERALENGINE_SRC}/TextureInterpolation.inl
-)
