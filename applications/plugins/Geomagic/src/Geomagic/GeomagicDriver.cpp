8150619a310a3640835d5dba666b97b91399ddac
Hugo <hugo.talbot@sofa-framework.org>
2024-03-14 15:11:57 +0100

    Apply suggestions from code review
    Co-authored-by: Alex Bilger <alxbilger@users.noreply.github.com>

--- applications/plugins/Geomagic/src/Geomagic/GeomagicDriver.cpp
+++ applications/plugins/Geomagic/src/Geomagic/GeomagicDriver.cpp
@@ -493,7 +493,7 @@ void GeomagicDriver::draw(const sofa::core::visual::VisualParams* vparams)
 void GeomagicDriver::computeBBox(const core::ExecParams*  params, bool onlyVisible)
 {
     SOFA_UNUSED(params);
-    SOFA_UNUSED(onlyVisible);
+    if (!onlyVisible) return;
 
     SReal minBBox[3] = {1e10,1e10,1e10};
     SReal maxBBox[3] = {-1e10,-1e10,-1e10};
