3c4505ea41614918df577cc315c587f592aaf2f3
Alex Bilger <alexbilger0@gmail.com>
2024-04-12 12:27:58 +0200

    [FEM.HyperElastic] Convert string Data to OptionsGroup

--- Sofa/Component/SolidMechanics/FEM/HyperElastic/src/sofa/component/solidmechanics/fem/hyperelastic/material/BoyceAndArruda.h
+++ Sofa/Component/SolidMechanics/FEM/HyperElastic/src/sofa/component/solidmechanics/fem/hyperelastic/material/BoyceAndArruda.h
@@ -45,6 +45,9 @@ the determinant of the deformation gradient J and the right Cauchy Green deforma
 template<class DataTypes>
 class BoyceAndArruda : public HyperelasticMaterial<DataTypes>{
 
+public:
+    static constexpr std::string_view Name = "ArrudaBoyce";
+
     typedef typename DataTypes::Coord::value_type Real;
     typedef type::Mat<3,3,Real> Matrix3;
     typedef type::Mat<6,6,Real> Matrix6;
