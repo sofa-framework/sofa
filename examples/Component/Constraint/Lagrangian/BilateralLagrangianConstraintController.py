0a103be125130d699716bd763605b4f088240cb0
Damien Marchal <damien.marchal@univ-lille1.fr>
2024-04-16 13:28:39 +0200

    F

--- examples/Component/Constraint/Lagrangian/BilateralLagrangianConstraintController.py
+++ examples/Component/Constraint/Lagrangian/BilateralLagrangianConstraintController.py
@@ -1,11 +1,11 @@
 import Sofa
 from Sofa.constants import Key
-from splib3.loaders.xmlloader import loadXML 
+from splib3.loaders.xmlloader import loadXML
 
 class BilateralConstraintController(Sofa.Core.Controller):
     def __init__(self, *args, **kwargs):
         Sofa.Core.Controller.__init__(self, *args, **kwargs)
-        self.constraints = kwargs.get("constraints", []) 
+        self.constraints = kwargs.get("constraints", [])
 
     def onKeyreleasedEvent(self, event):
         if event["key"] == Key.A:
