48133acf4b50227da3cf03ceffbccc0f2e924e51
Damien Marchal <damien.marchal@univ-lille1.fr>
2024-04-16 13:23:08 +0200

    Update the example of controller for bilaterallagrangianconstraint

--- examples/Component/Constraint/Lagrangian/BilateralLagrangianConstraintController.py
+++ examples/Component/Constraint/Lagrangian/BilateralLagrangianConstraintController.py
@@ -1,16 +1,18 @@
 import Sofa
 from Sofa.constants import Key
+from splib3.loaders.xmlloader import loadXML 
 
 class BilateralConstraintController(Sofa.Core.Controller):
     def __init__(self, *args, **kwargs):
         Sofa.Core.Controller.__init__(self, *args, **kwargs)
-        self.constraint = kwargs.get("constraint") 
+        self.constraints = kwargs.get("constraints", []) 
 
     def onKeyreleasedEvent(self, event):
         if event["key"] == Key.A:
-            self.constraint.activate.value = not self.constraint.activate.value
+            for constraint in self.constraints:
+                constraint.activate.value = not constraint.activate.value
              
 def createScene(root):
-    root.addObject("MechanicalObject")
-    root.addObject("BilateralLagrangianConstraint", name="c")
-    root.addObject(BilateralConstraintController(constraint=root.c))
+    loadXML("BilateralInteractionConstraint_NNCG.scn", root)
+    listConstraints = [ object for object in root.objects if object.getClassName() == "BilateralLagrangianConstraint"]
+    root.addObject(BilateralConstraintController(constraints=listConstraints))
