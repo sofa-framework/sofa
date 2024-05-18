57d4051800b0f153086345ef5fd018017045a790
epernod <erik.pernod@gmail.com>
2024-05-19 00:53:41 +0200

    [Topology] Add doc on class header

--- Sofa/Component/Topology/Container/Dynamic/src/sofa/component/topology/container/dynamic/TriangleModifiers.h
+++ Sofa/Component/Topology/Container/Dynamic/src/sofa/component/topology/container/dynamic/TriangleModifiers.h
@@ -30,10 +30,16 @@ namespace sofa::component::topology::container::dynamic
 {
 
 using Triangle = core::topology::BaseMeshTopology::Triangle;
+using Edge = sofa::core::topology::BaseMeshTopology::Edge;
 using TriangleID = core::topology::BaseMeshTopology::TriangleID;
 using EdgeID = core::topology::BaseMeshTopology::EdgeID;
 using PointID = core::topology::BaseMeshTopology::PointID;
 
+/**
+* This class store all the info to create a new triangle in the mesh taking into account estimated unique id
+* triangle structure with vertex indices
+* This structure also store all the ancestors and coefficient to efficently add this triangle with the area ratio into the current mesh.
+*/
 class TriangleToAdd
 {
 public:
@@ -47,22 +53,24 @@ public:
 
     virtual ~TriangleToAdd() {}
 
-    TriangleID m_uniqueID;
-    sofa::core::topology::BaseMeshTopology::Triangle m_triangle;
-    type::vector<TriangleID> m_ancestors;
-    type::vector<SReal> m_coefs;
+    TriangleID m_uniqueID; ///< unique new id of the future triangle
+    Triangle m_triangle; ///< Triangle topological structure
+    type::vector<TriangleID> m_ancestors; ///< Triangle indices ancestors of this new Triangle
+    type::vector<SReal> m_coefs; ///< Coefficient to apply with the ancestors info to compute new triangle area
 
     sofa::type::fixed_array<sofa::type::Vec3, 3> m_triCoords;
     bool isUp = false;
 };
 
 
+/**
+* This class store all info to be able to split a Triangle into a given configuration.
+* Depending on the configuration, 2 or 3 new triangle will be computed as well as new points.
+* All info to create the new elements are computed and will be generated using a TriangleSetModifier component.
+*/
 class TriangleSubdivider
 {
 public:
-    using Triangle = sofa::core::topology::BaseMeshTopology::Triangle;
-    using Edge = sofa::core::topology::BaseMeshTopology::Edge;
-
     TriangleSubdivider(TriangleID triangleId, const Triangle& triangle);
 
     virtual bool subdivide(const sofa::type::fixed_array<sofa::type::Vec3, 3>& triCoords)
@@ -80,11 +88,15 @@ protected:
     TriangleID m_triangleId; ///< Index of this triangle in the Topology Container
     sofa::core::topology::BaseMeshTopology::Triangle m_triangle; ///< Triangle fixed array of the 3 vertex indices
 
-    type::vector<PointToAdd*> m_points;
-    type::vector<TriangleToAdd*> m_trianglesToAdd;
+    type::vector<PointToAdd*> m_points; ///< Vector of new point to be added while subdividing this Triangle
+    type::vector<TriangleToAdd*> m_trianglesToAdd; ///< Vector of triangle to be added while subdividing this Triangle
 };
 
 
+/**
+* Specialisation of TriangleSubdivider to compute configuration where 1 Node is added in inside of the Triangle
+* In this case 3 new Triangles and 1 Point will be created.
+*/
 class TriangleSubdivider_1Node : public TriangleSubdivider
 {
 public:
@@ -96,6 +108,10 @@ public:
 };
 
 
+/**
+* Specialisation of TriangleSubdivider to compute configuration where 1 Node is added on one edge of the Triangle
+* In this case 2 new Triangles and 1 Point will be created.
+*/
 class TriangleSubdivider_1Edge : public TriangleSubdivider
 {
 public:
@@ -107,6 +123,10 @@ public:
 };
 
 
+/**
+* Specialisation of TriangleSubdivider to compute configuration where 2 Nodes are added on 2 different edges of the Triangle
+* In this case 3 new Triangles and 2 Points will be created.
+*/
 class TriangleSubdivider_2Edge : public TriangleSubdivider
 {
 public:
@@ -118,6 +138,10 @@ public:
 };
 
 
+/**
+* Specialisation of TriangleSubdivider to compute configuration where 3 Nodes are added on 3 different edges of the Triangle
+* In this case 4 new Triangles and 3 Points will be created.
+*/
 class TriangleSubdivider_3Edge : public TriangleSubdivider
 {
 public:
@@ -129,6 +153,10 @@ public:
 };
 
 
+/**
+* Specialisation of TriangleSubdivider to compute configuration where 2 Nodes are added, 1 on edge and 1 inside of the Triangle
+* In this case 4 new Triangles and 2 Points will be created.
+*/
 class TriangleSubdivider_2Node : public TriangleSubdivider
 {
 public:
