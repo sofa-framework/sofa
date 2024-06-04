be94bc2566c3ca8a47ee7a0a1b03feffc6cc51e8
Hugo <hugo.talbot@sofa-framework.org>
2024-06-04 21:23:04 +0200

    Apply suggestions from code review
    Co-authored-by: Alex Bilger <alxbilger@users.noreply.github.com>

--- Sofa/Component/Collision/Geometry/src/sofa/component/collision/geometry/SphereModel.h
+++ Sofa/Component/Collision/Geometry/src/sofa/component/collision/geometry/SphereModel.h
@@ -165,7 +165,7 @@ public:
     Data<SReal> defaultRadius;
 
     Data< VecReal > d_radius; ///< Radius of each sphere
-    Data< SReal > d_defaultRadius; ///< Default radius (default=1.0)
+    Data< SReal > d_defaultRadius; ///< Default radius
     Data< bool > d_showImpostors; ///< Draw spheres as impostors instead of "real" spheres
 
 
