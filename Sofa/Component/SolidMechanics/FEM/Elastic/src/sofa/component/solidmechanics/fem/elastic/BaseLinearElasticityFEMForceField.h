98f7654157a74f21dcb925ce23aec9f8f4b831aa
Alex Bilger <alxbilger@users.noreply.github.com>
2024-07-15 16:09:20 +0200

    [FEM.Elastic] Fix dynamic symbols (#4831)

--- Sofa/Component/SolidMechanics/FEM/Elastic/src/sofa/component/solidmechanics/fem/elastic/BaseLinearElasticityFEMForceField.h
+++ Sofa/Component/SolidMechanics/FEM/Elastic/src/sofa/component/solidmechanics/fem/elastic/BaseLinearElasticityFEMForceField.h
@@ -61,7 +61,7 @@ public:
 };
 
 #if !defined(SOFA_COMPONENT_SOLIDMECHANICS_FEM_ELASTIC_BASELINEARELASTICITYFEMFORCEFIELD_CPP)
-extern template class BaseLinearElasticityFEMForceField<defaulttype::Vec3Types>;
+extern template class SOFA_COMPONENT_SOLIDMECHANICS_FEM_ELASTIC_API BaseLinearElasticityFEMForceField<defaulttype::Vec3Types>;
 #endif
 
 }
