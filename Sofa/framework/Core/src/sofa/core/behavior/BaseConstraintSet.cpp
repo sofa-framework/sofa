356d8895d335bd388992814b10e9944fb478b699
Paul Baksic <paul.baksic@outlook.fr>
2024-08-23 14:20:06 +0200

    Add completly missed Data due to slight change in macro name

--- Sofa/framework/Core/src/sofa/core/behavior/BaseConstraintSet.cpp
+++ Sofa/framework/Core/src/sofa/core/behavior/BaseConstraintSet.cpp
@@ -29,7 +29,7 @@ BaseConstraintSet::BaseConstraintSet()
     : group(initData(&group, 0, "group", "ID of the group containing this constraint. This ID is used to specify which constraints are solved by which solver, by specifying in each solver which groups of constraints it should handle."))
     , d_constraintIndex(initData(&d_constraintIndex, 0u, "constraintIndex", "Constraint index (first index in the right hand term resolution vector)"))
 {
-    m_constraintIndex.setParent(&d_constraintIndex);
+    m_constraintIndex.setOriginalData(&d_constraintIndex);
 }
 
 
