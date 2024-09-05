60f1e7f73dbb0138ad64144aafc55c53f50681c0
Alex Bilger <alexbilger0@gmail.com>
2024-09-05 12:05:03 +0200

    Modify default value of Poisson ratio in tests

--- Sofa/Component/SolidMechanics/FEM/Elastic/tests/BeamFEMForceField_test.cpp
+++ Sofa/Component/SolidMechanics/FEM/Elastic/tests/BeamFEMForceField_test.cpp
@@ -173,7 +173,7 @@ public:
         ASSERT_TRUE(bFEM.get() != nullptr);
         ASSERT_FLOATINGPOINT_EQ(bFEM->d_radius.getValue(), static_cast<Real>(0.1));
         ASSERT_FLOATINGPOINT_EQ(bFEM->getYoungModulusInElement(0), static_cast<Real>(5000));
-        ASSERT_FLOATINGPOINT_EQ(bFEM->getPoissonRatioInElement(0), static_cast<Real>(0.49));
+        ASSERT_FLOATINGPOINT_EQ(bFEM->getPoissonRatioInElement(0), static_cast<Real>(0.45));
     }
 
 
