fbed37be05966ff6e32fd0f8870a5a6f6f8682f1
Alex Bilger <alexbilger0@gmail.com>
2024-09-13 11:10:20 +0200

    attempt to replace sofa::type::Mat with Eigen::Matrix

---
+++ Sofa/framework/Type/src/sofa/type/EigenMatrixAddons.h
@@ -0,0 +1,69 @@
+﻿struct NoInit;
+
+explicit Matrix(const NoInit& noInit)
+{}
+
+void identity()
+{
+    *this = Identity();
+}
+
+void clear()
+{
+    *this = Zero();
+}
+
+auto ptr()
+{
+    return data();
+}
+
+auto ptr() const
+{
+    return data();
+}
+
+bool invert(const Matrix& mat)
+{
+    *this = mat.inverse();
+    return true;
+}
+
+auto transposed() const
+{
+    return transpose();
+}
+
+auto& x()
+{
+    return this->operator()(0,0);
+}
+
+const auto& x() const
+{
+    return this->operator()(0,0);
+}
+
+auto& operator[](Index i)
+{
+    if constexpr (IsRowMajor && ColsAtCompileTime > 1)
+    {
+        return this->row(i);
+    }
+    else
+    {
+        return this->operator()(i);
+    }
+}
+
+const auto& operator[](Index i) const
+{
+    if constexpr (IsRowMajor && ColsAtCompileTime > 1)
+    {
+        return this->row(i);
+    }
+    else
+    {
+        return this->operator()(i);
+    }
+}
