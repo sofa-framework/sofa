85e74330fb547ee91b050b77c7ea418ac588ce22
Alex Bilger <alexbilger0@gmail.com>
2024-11-08 16:18:37 +0100

    introduce strong types

---
+++ Sofa/framework/Type/test/StrongType_test.cpp
@@ -0,0 +1,60 @@
+﻿/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#include <gtest/gtest.h>
+#include <sofa/type/StrongType.h>
+
+namespace sofa
+{
+
+TEST(StrongType, constructor)
+{
+    using myint = sofa::type::StrongType<int, struct _myinttag>;
+
+    // myint a; //this is not possible as the default constructor is explicit
+    constexpr myint a ( 2 );
+}
+
+TEST(StrongType, preIncrementable)
+{
+    using myint = sofa::type::StrongType<int, struct _preincrementable, sofa::type::functionality::PreIncrementable>;
+    myint a ( 2 );
+    ++a;
+    EXPECT_EQ(a.get(), 3);
+}
+
+TEST(StrongType, postIncrementable)
+{
+    using myint = sofa::type::StrongType<int, struct _postincrementable, sofa::type::functionality::PostIncrementable>;
+    myint a ( 2 );
+    a++;
+    EXPECT_EQ(a.get(), 3);
+}
+
+TEST(StrongType, floatAddable)
+{
+    using myfloat = sofa::type::StrongType<float, struct _floataddable, sofa::type::functionality::Arithmetic>;
+    static constexpr myfloat a ( 2 );
+    static constexpr myfloat b ( 3 );
+    EXPECT_EQ((a + b).get(), 5);
+}
+
+}
