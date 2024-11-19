db4ba7f97643a9f7f3ae30ea0fa6b57f91d4c45a
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-11-19 17:43:16 +0100

    nix: add nixGL

--- flake.nix
+++ flake.nix
@@ -6,15 +6,27 @@
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # ref. https://github.com/NixOS/nixpkgs/pull/348549
     nixpkgs.url = "github:nim65s/nixpkgs/qt6-libqglviewer";
+    nixgl.url = "github:nix-community/nixGL";
   };
 
   outputs =
-    inputs@{ flake-parts, nixpkgs, ... }:
+    inputs@{ flake-parts, nixgl, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem =
-        { pkgs, self', ... }:
+        { pkgs, self', system, ... }:
         {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ nixgl.overlay ];
+          };
+          apps.nixgl = {
+            type = "app";
+            program = pkgs.writeShellApplication {
+              name = "nixgl-sofa";
+              text = "${pkgs.lib.getExe pkgs.nixgl.auto.nixGLDefault} ${pkgs.lib.getExe self'.packages.sofa}";
+            };
+          };
           devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
           packages = {
             default = self'.packages.sofa;
