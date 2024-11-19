6e336a406faf9cda85c5ca1c1ce7e4d765972f17
Alex Bilger <alexbilger0@gmail.com>
2024-11-19 09:33:45 +0100

    forgot the condition on diagonal masses

--- Sofa/Component/ODESolver/Forward/src/sofa/component/odesolver/forward/EulerExplicitSolver.cpp
+++ Sofa/Component/ODESolver/Forward/src/sofa/component/odesolver/forward/EulerExplicitSolver.cpp
@@ -24,6 +24,7 @@
 #include <sofa/simulation/MechanicalOperations.h>
 #include <sofa/simulation/VectorOperations.h>
 #include <sofa/core/ObjectFactory.h>
+#include <sofa/core/behavior/BaseMass.h>
 #include <sofa/core/behavior/LinearSolver.h>
 #include <sofa/helper/AdvancedTimer.h>
 #include <sofa/helper/ScopedAdvancedTimer.h>
@@ -87,10 +88,16 @@ void EulerExplicitSolver::solve(const core::ExecParams* params,
     sofa::Size nbNonDiagonalMasses = 0;
     this->getContext()->accept(
         sofa::core::objectmodel::topDownVisitor
-        | [&nbNonDiagonalMasses](sofa::core::behavior::BaseMass* mass){nbNonDiagonalMasses++;});
+        | [&nbNonDiagonalMasses](sofa::core::behavior::BaseMass* mass)
+        {
+            if (mass && !mass->isDiagonal())
+            {
+                nbNonDiagonalMasses++;
+            }
+        });
 
     // Mass matrix is diagonal, solution can thus be found by computing acc = f/m
-    if(nbNonDiagonalMasses == 0.)
+    if(nbNonDiagonalMasses == 0)
     {
         // acc = M^-1 * f
         computeAcceleration(&mop, acc, f);
