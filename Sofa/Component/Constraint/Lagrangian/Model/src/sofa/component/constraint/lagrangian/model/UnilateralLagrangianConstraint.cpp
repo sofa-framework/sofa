a20709168f8280b14d4f06777dc1398d77753ed6
Paul Baksic <paul.baksic@outlook.fr>
2024-12-05 11:56:51 +0100

    remove multiple registration in factory

--- Sofa/Component/Constraint/Lagrangian/Model/src/sofa/component/constraint/lagrangian/model/UnilateralLagrangianConstraint.cpp
+++ Sofa/Component/Constraint/Lagrangian/Model/src/sofa/component/constraint/lagrangian/model/UnilateralLagrangianConstraint.cpp
@@ -40,8 +40,6 @@ void registerUnilateralLagrangianConstraint(sofa::core::ObjectFactory* factory)
         .add< UnilateralLagrangianConstraint<Vec3Types> >());
 }
 
-int UnilateralLagrangianConstraintClass = core::RegisterObject("UnilateralLagrangianConstraint")
-        .add< UnilateralLagrangianConstraint<Vec3Types> >();
 
 template class SOFA_COMPONENT_CONSTRAINT_LAGRANGIAN_MODEL_API BaseContactLagrangianConstraint<Vec3Types,UnilateralLagrangianContactParameters>;
 template class SOFA_COMPONENT_CONSTRAINT_LAGRANGIAN_MODEL_API UnilateralLagrangianConstraint<Vec3Types>;
