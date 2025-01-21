55c58f88ae601c0f91c623dd2b14f652adf02226
Paul Baksic <paul.baksic@outlook.fr>
2025-01-21 15:22:49 +0100

    Put Qt related object fro mSofaMatrix into extensions folder

--- applications/plugins/SofaMatrix/extensions/Qt/src/SofaMatrix/Qt/BaseMatrixImageViewerWidget.cpp
+++ applications/plugins/SofaMatrix/extensions/Qt/src/SofaMatrix/Qt/BaseMatrixImageViewerWidget.cpp
@@ -19,7 +19,7 @@
 *                                                                             *
 * Contact information: contact@sofa-framework.org                             *
 ******************************************************************************/
-#include <SofaMatrix/BaseMatrixImageViewerWidget.h>
+#include <SofaMatrix/Qt/BaseMatrixImageViewerWidget.h>
 
 namespace sofa::gui::qt
 {
