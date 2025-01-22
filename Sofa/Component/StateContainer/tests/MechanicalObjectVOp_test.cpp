7af16040adb16c6a297c1b410b6b765d0ddbe4b5
Alex Bilger <alexbilger0@gmail.com>
2025-01-22 14:10:39 +0100

    missing test

--- Sofa/Component/StateContainer/tests/MechanicalObjectVOp_test.cpp
+++ Sofa/Component/StateContainer/tests/MechanicalObjectVOp_test.cpp
@@ -784,6 +784,11 @@ TYPED_TEST(MechanicalObjectVOpTest, resetVelocity)
     this->resetVelocity();
 }
 
+TYPED_TEST(MechanicalObjectVOpTest, resetVelocity)
+{
+    this->resetVelocity();
+}
+
 TYPED_TEST(MechanicalObjectVOpTest, multiplyByScalarPosition)
 {
     this->multiplyByScalarPosition();
