73dce77ceed1c6d28698f6148a5faf8178e12c2f
Alex Bilger <alexbilger0@gmail.com>
2025-01-23 11:41:34 +0100

    remove duplicated test

--- Sofa/Component/StateContainer/tests/MechanicalObjectVOp_test.cpp
+++ Sofa/Component/StateContainer/tests/MechanicalObjectVOp_test.cpp
@@ -784,11 +784,6 @@ TYPED_TEST(MechanicalObjectVOpTest, resetVelocity)
     this->resetVelocity();
 }
 
-TYPED_TEST(MechanicalObjectVOpTest, resetVelocity)
-{
-    this->resetVelocity();
-}
-
 TYPED_TEST(MechanicalObjectVOpTest, multiplyByScalarPosition)
 {
     this->multiplyByScalarPosition();
