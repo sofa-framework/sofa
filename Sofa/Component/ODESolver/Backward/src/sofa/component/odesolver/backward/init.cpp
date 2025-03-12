483dc4e98cbe373fcc8ca775f69c5ebdd0405c96
Alex Bilger <alexbilger0@gmail.com>
2025-03-12 16:09:17 +0100

    remove static solver

--- Sofa/Component/ODESolver/Backward/src/sofa/component/odesolver/backward/init.cpp
+++ Sofa/Component/ODESolver/Backward/src/sofa/component/odesolver/backward/init.cpp
@@ -32,7 +32,6 @@ extern void registerStaticSolver(sofa::core::ObjectFactory* factory);
 extern void registerVariationalSymplecticSolver(sofa::core::ObjectFactory* factory);
 extern void registerNewtonRaphsonSolver(sofa::core::ObjectFactory* factory);
 extern void registerBDFOdeSolver(sofa::core::ObjectFactory* factory);
-extern void registerStaticOdeSolver(sofa::core::ObjectFactory* factory);
 
 extern "C" {
     SOFA_EXPORT_DYNAMIC_LIBRARY void initExternalModule();
@@ -64,7 +63,6 @@ void registerObjects(sofa::core::ObjectFactory* factory)
     registerVariationalSymplecticSolver(factory);
     registerNewtonRaphsonSolver(factory);
     registerBDFOdeSolver(factory);
-    registerStaticOdeSolver(factory);
 }
 
 void init()
