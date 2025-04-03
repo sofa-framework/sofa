d93d2e1a652909a70ca159e6a42db92dcbe97f1b
Alex Bilger <alexbilger0@gmail.com>
2025-04-03 11:51:44 +0200

    restore ODE functions

--- Sofa/Component/ODESolver/Backward/src/sofa/component/odesolver/backward/StaticOdeSolver.h
+++ Sofa/Component/ODESolver/Backward/src/sofa/component/odesolver/backward/StaticOdeSolver.h
@@ -52,6 +52,81 @@ public:
     auto squared_residual_norms() const -> const std::vector<SReal> & = delete;
     auto squared_increment_norms() const -> const std::vector<SReal> & = delete;
 
+
+    /// Given a displacement as computed by the linear system inversion, how much will it affect the velocity
+    ///
+    /// This method is used to compute the compliance for contact corrections
+    /// For Euler methods, it is typically dt.
+    SReal getVelocityIntegrationFactor() const override
+    {
+        return 1.0; // getContext()->getDt();
+    }
+
+    /// Given a displacement as computed by the linear system inversion, how much will it affect the position
+    ///
+    /// This method is used to compute the compliance for contact corrections
+    /// For Euler methods, it is typically dt².
+    SReal getPositionIntegrationFactor() const override
+    {
+        return getPositionIntegrationFactor(getContext()->getDt());
+    }
+
+    virtual SReal getPositionIntegrationFactor(SReal dt ) const
+    {
+        return dt;
+    }
+
+    /// Given an input derivative order (0 for position, 1 for velocity, 2 for acceleration),
+    /// how much will it affect the output derivative of the given order.
+    ///
+    /// This method is used to compute the compliance for contact corrections.
+    /// For example, a backward-Euler dynamic implicit integrator would use:
+    /// Input:      x_t  v_t  a_{t+dt}
+    /// x_{t+dt}     1    dt  dt^2
+    /// v_{t+dt}     0    1   dt
+    ///
+    /// If the linear system is expressed on s = a_{t+dt} dt, then the final factors are:
+    /// Input:      x_t   v_t    a_t  s
+    /// x_{t+dt}     1    dt     0    dt
+    /// v_{t+dt}     0    1      0    1
+    /// a_{t+dt}     0    0      0    1/dt
+    /// The last column is returned by the getSolutionIntegrationFactor method.
+    SReal getIntegrationFactor(int inputDerivative, int outputDerivative) const override
+    {
+        return getIntegrationFactor(inputDerivative, outputDerivative, getContext()->getDt());
+    }
+
+    SReal getIntegrationFactor(int inputDerivative, int outputDerivative, SReal dt) const
+    {
+        const SReal matrix[3][3] =
+            {
+                { 1, dt, 0},
+                { 0, 1, 0},
+                { 0, 0, 0}
+            };
+        if (inputDerivative >= 3 || outputDerivative >= 3)
+            return 0;
+        else
+            return matrix[outputDerivative][inputDerivative];
+    }
+
+    /// Given a solution of the linear system,
+    /// how much will it affect the output derivative of the given order.
+    SReal getSolutionIntegrationFactor(int outputDerivative) const override
+    {
+        return getSolutionIntegrationFactor(outputDerivative, getContext()->getDt());
+    }
+
+    SReal getSolutionIntegrationFactor(int outputDerivative, SReal dt) const
+    {
+        const SReal vect[3] = { dt, 1, 1/dt};
+        if (outputDerivative >= 3)
+            return 0;
+        else
+            return vect[outputDerivative];
+    }
+
+
 protected:
 
     struct NewtonRaphsonDeprecatedData : core::objectmodel::lifecycle::DeprecatedData
