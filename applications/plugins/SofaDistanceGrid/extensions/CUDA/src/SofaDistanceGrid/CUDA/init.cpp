6da0b5617519a0737701fb872fa9acebdedb5b41
hugtalbot <hugo.talbot@sofa-framework.org>
2025-04-08 13:04:42 +0200

    Correctly sort registration in folder-appropriate init.cpp

--- applications/plugins/SofaDistanceGrid/extensions/CUDA/src/SofaDistanceGrid/CUDA/init.cpp
+++ applications/plugins/SofaDistanceGrid/extensions/CUDA/src/SofaDistanceGrid/CUDA/init.cpp
@@ -23,6 +23,12 @@
 #include <SofaDistanceGrid/initSofaDistanceGrid.h>
 #include <SofaCUDA/init.h>
 
+namespace sofa::gpu::cuda
+{
+    extern void registerCudaCollisionDetection(sofa::core::ObjectFactory* factory);
+    extern void registerCudaRigidDistanceGridCollisionModel(sofa::core::ObjectFactory* factory);
+}
+
 namespace sofadistancegrid::cuda
 {
 
@@ -30,7 +36,7 @@ extern "C" {
     SOFA_EXPORT_DYNAMIC_LIBRARY void initExternalModule();
     SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleName();
     SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleVersion();
-    SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleComponentList();
+    SOFA_SOFADISTANCEGRID_API void registerObjects(sofa::core::ObjectFactory* factory);
 }
 
 void initExternalModule()
@@ -57,6 +63,12 @@ void init()
         sofa::gpu::cuda::init();
         first = false;
     }
+}
+
+void registerObjects(sofa::core::ObjectFactory* factory)
+{
+    sofa::gpu::cuda::registerCudaCollisionDetection(factory);
+    sofa::gpu::cuda::registerCudaRigidDistanceGridCollisionModel(factory);
 }
 
 } // namespace volumetricrendering::cuda
