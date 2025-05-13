af79e47404aab1df7af5f636bcca570ae8f6b957
Alex Bilger <alxbilger@users.noreply.github.com>
2025-05-13 10:05:33 +0200

    [Core] Rename BaseConstraint to BaseLagrangianConstraint (#5423)
    * [Core] Rename BaseConstraint to BaseLagrangianConstraint
    
    * restore toBaseConstraint and deprecate it
    
    * fix?
    
    ---------
    
    Co-authored-by: Hugo <hugo.talbot@sofa-framework.org>

--- Sofa/Component/Constraint/Lagrangian/Model/src/sofa/component/constraint/lagrangian/model/BaseContactLagrangianConstraint.h
+++ Sofa/Component/Constraint/Lagrangian/Model/src/sofa/component/constraint/lagrangian/model/BaseContactLagrangianConstraint.h
@@ -61,15 +61,15 @@ public:
     typedef typename Coord::value_type Real;
     typedef typename core::behavior::MechanicalState<DataTypes> MechanicalState;
 
-    typedef core::behavior::BaseConstraint::ConstraintBlockInfo ConstraintBlockInfo;
-    typedef core::behavior::BaseConstraint::PersistentID PersistentID;
-    typedef core::behavior::BaseConstraint::ConstCoord ConstCoord;
+    typedef core::behavior::BaseLagrangianConstraint::ConstraintBlockInfo ConstraintBlockInfo;
+    typedef core::behavior::BaseLagrangianConstraint::PersistentID PersistentID;
+    typedef core::behavior::BaseLagrangianConstraint::ConstCoord ConstCoord;
 
-    typedef core::behavior::BaseConstraint::VecConstraintBlockInfo VecConstraintBlockInfo;
-    typedef core::behavior::BaseConstraint::VecPersistentID VecPersistentID;
-    typedef core::behavior::BaseConstraint::VecConstCoord VecConstCoord;
-    typedef core::behavior::BaseConstraint::VecConstDeriv VecConstDeriv;
-    typedef core::behavior::BaseConstraint::VecConstArea VecConstArea;
+    typedef core::behavior::BaseLagrangianConstraint::VecConstraintBlockInfo VecConstraintBlockInfo;
+    typedef core::behavior::BaseLagrangianConstraint::VecPersistentID VecPersistentID;
+    typedef core::behavior::BaseLagrangianConstraint::VecConstCoord VecConstCoord;
+    typedef core::behavior::BaseLagrangianConstraint::VecConstDeriv VecConstDeriv;
+    typedef core::behavior::BaseLagrangianConstraint::VecConstArea VecConstArea;
 
     typedef core::objectmodel::Data<VecCoord>		DataVecCoord;
     typedef core::objectmodel::Data<VecDeriv>		DataVecDeriv;
