6e7dd3587849e3f937c3de786207bcbd4a43fa17
Alex Bilger <alexbilger0@gmail.com>
2025-05-14 10:44:26 +0200

    Apply renaming

--- Sofa/Component/Constraint/Lagrangian/Model/src/sofa/component/constraint/lagrangian/model/UniformLagrangianConstraint.h
+++ Sofa/Component/Constraint/Lagrangian/Model/src/sofa/component/constraint/lagrangian/model/UniformLagrangianConstraint.h
@@ -28,10 +28,10 @@ namespace sofa::component::constraint::lagrangian::model
 {
 
 template < class DataTypes >
-class UniformLagrangianConstraint : public sofa::core::behavior::Constraint< DataTypes >
+class UniformLagrangianConstraint : public sofa::core::behavior::LagrangianConstraint< DataTypes >
 {
 public:
-    SOFA_CLASS(SOFA_TEMPLATE(UniformLagrangianConstraint, DataTypes), SOFA_TEMPLATE(sofa::core::behavior::Constraint, DataTypes));
+    SOFA_CLASS(SOFA_TEMPLATE(UniformLagrangianConstraint, DataTypes), SOFA_TEMPLATE(sofa::core::behavior::LagrangianConstraint, DataTypes));
 
     typedef typename DataTypes::VecCoord VecCoord;
     typedef typename DataTypes::VecDeriv VecDeriv;
