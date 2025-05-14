95e202d14182e4f928f404ff7ef73af992407939
Paul Baksic <paul.baksic@outlook.fr>
2025-05-14 09:37:06 +0200

    Change accessor for already present template methods to uniformize

--- Sofa/framework/Core/src/sofa/core/visual/VisualModel.h
+++ Sofa/framework/Core/src/sofa/core/visual/VisualModel.h
@@ -82,7 +82,6 @@ protected:
     VisualModel();
     ~VisualModel() override { }
 
-private:
     virtual void doDrawVisual(const VisualParams* /*vparams*/) {}
     virtual void doInitVisual(const VisualParams* /*vparams*/) {}
     virtual void doUpdateVisual(const VisualParams* /*vparams*/) {}
