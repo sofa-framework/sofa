fd1a02537796038d55410e055d4980526ede9780
Olivier Roussel <olivier.roussel@inria.fr>
2025-05-23 17:22:20 +0200

    moving to cmake presets (WIP)

--- development/scripts/pixi/activation.bat
+++ development/scripts/pixi/activation.bat
@@ -6,6 +6,9 @@ set CMAKE_EXPORT_COMPILE_COMMANDS=1
 :: Activate color output with Ninja
 set CMAKE_COLOR_DIAGNOSTICS=1
 
+:: Each environment have its dedicated build dir
+set VISP_BUILD_DIR=%CONDA_PREFIX%\visp-build
+
 :: Set default build value only if not previously set
 if not defined SOFA_CMAKE_PREFIX_PATH (set SOFA_CMAKE_PREFIX_PATH=%CONDA_PREFIX%)
 if not defined SOFA_BUILD_TYPE (set SOFA_BUILD_TYPE=Release)
