3b29036b6efdb91499affb0156baf0c0b7a2bf11
Alex Bilger <alexbilger0@gmail.com>
2025-05-27 15:44:29 +0200

    create plugin for imgui

---
+++ applications/plugins/SofaMatrix/extensions/imgui/src/SofaMatrix/imgui/init.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <SofaMatrix/imgui/config.h>
+
+namespace sofamatriximgui
+{
+
+/** Initialize the SofaMatrix.imgui plugin */
+void SOFAMATRIX_IMGUI_API initializePlugin();
+
+}
