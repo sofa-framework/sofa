00727f40fce4b1cc8769b45b47bbbb7f9dfe0185
Alex Bilger <alexbilger0@gmail.com>
2025-05-28 15:21:18 +0200

    implement the widget

--- applications/plugins/SofaMatrix/extensions/imgui/src/SofaMatrix/imgui/BaseMatrixImageProxyWidget.cpp
+++ applications/plugins/SofaMatrix/extensions/imgui/src/SofaMatrix/imgui/BaseMatrixImageProxyWidget.cpp
@@ -20,7 +20,145 @@
 * Contact information: contact@sofa-framework.org                             *
 ******************************************************************************/
 #include <SofaImGui/ImGuiDataWidget.h>
+#include <imgui.h>
+#include <GL/glew.h>
 #include <SofaMatrix/BaseMatrixImageProxy.h>
+
+#include <optional>
+
+namespace sofamatrix::imgui
+{
+
+std::optional<std::vector<unsigned char>> imageData(sofa::linearalgebra::BaseMatrix* ptr)
+{
+    static std::map<sofa::linearalgebra::BaseMatrix*, std::vector<unsigned char>> map;
+    auto mapIt = map.find(ptr);
+    if (mapIt == map.end())
+    {
+        const auto insertResult = map.emplace(ptr, std::vector<unsigned char>());
+        if (insertResult.second)
+        {
+            mapIt = insertResult.first;
+        }
+        else
+        {
+            return {};
+        }
+    }
+    return mapIt->second;
+}
+
+std::optional<GLuint> textureID(sofa::linearalgebra::BaseMatrix* ptr)
+{
+    static std::map<sofa::linearalgebra::BaseMatrix*, GLuint> map;
+    auto mapIt = map.find(ptr);
+    if (mapIt == map.end())
+    {
+        const auto insertResult = map.emplace(ptr, GLuint());
+        if (insertResult.second)
+        {
+            mapIt = insertResult.first;
+
+            // generate a new texture ID
+            GLuint& textureID = mapIt->second;
+            glGenTextures(1, &textureID);
+
+            glBindTexture(GL_TEXTURE_2D, textureID);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+        }
+        else
+        {
+            return {};
+        }
+    }
+    return mapIt->second;
+}
+
+void updateTexture(sofa::linearalgebra::BaseMatrix* matrix, unsigned& textureIDRef,
+                   std::vector<unsigned char>& imageDataRef)
+{
+    const auto width = matrix->rows();
+    const auto height = matrix->cols();
+
+    imageDataRef.resize(width * height, 255);
+
+    // write the data
+    for (sofa::SignedIndex y = 0; y < height; ++y)
+    {
+        for (sofa::SignedIndex x = 0; x < width; ++x)
+        {
+            imageDataRef[y * width + x] = !static_cast<bool>(matrix->element(x, y)) * std::numeric_limits<unsigned char>::max();
+        }
+    }
+
+    // Update the texture
+    glBindTexture(GL_TEXTURE_2D, textureIDRef);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, width, height, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE,
+                 imageDataRef.data());
+}
+
+void drawTexture(sofa::linearalgebra::BaseMatrix* matrix, bool updateTexture)
+{
+    if (matrix == nullptr)
+        return;
+
+    auto textureID = sofamatrix::imgui::textureID(matrix);
+    if (textureID.has_value() == false)
+        return;
+
+    auto imageData = sofamatrix::imgui::imageData(matrix);
+    if (imageData.has_value() == false)
+        return;
+
+    auto& textureIDRef = textureID.value();
+    auto& imageDataRef = imageData.value();
+
+    if (updateTexture)
+    {
+        sofamatrix::imgui::updateTexture(matrix, textureIDRef, imageDataRef);
+    }
+
+    const auto width = matrix->rows();
+    const auto height = matrix->cols();
+
+    ImVec2 imageSize(width, height);
+
+    // Get the available width
+    imageSize.x = ImGui::GetContentRegionAvail().x;
+
+    // Calculate the aspect ratio
+    float aspectRatio = (float)width / (float)height;
+
+    // Calculate the height based on the available width and aspect ratio
+    imageSize.y = imageSize.x / aspectRatio;
+
+    // Draw the texture using imgui
+    ImGui::Image((ImTextureID)(intptr_t)textureIDRef, imageSize);
+}
+
+static constexpr int unknownCounter = -1;
+int* counter(sofa::core::objectmodel::BaseData& data)
+{
+    static std::map<sofa::core::objectmodel::BaseData*, std::unique_ptr<int>> map;
+    auto mapIt = map.find(&data);
+    if (mapIt == map.end())
+    {
+        const auto insertResult = map.emplace(&data, std::make_unique<int>(unknownCounter));
+        if (insertResult.second)
+        {
+            mapIt = insertResult.first;
+        }
+        else
+        {
+            return nullptr;
+        }
+    }
+    return mapIt->second.get();
+}
+
+}
 
 namespace sofaimgui
 {
@@ -28,7 +166,29 @@ namespace sofaimgui
 template<>
 void DataWidget<sofa::type::BaseMatrixImageProxy>::showWidget(MyData& data)
 {
+    const auto& matrixProxy = data.getValue();
+
+    if (auto* matrix = matrixProxy.getMatrix())
+    {
+        std::stringstream ss;
+        ss << std::to_string(matrix->rows()) << "x" << std::to_string(matrix->cols());
+        ImGui::Text(ss.str().c_str());
+
+        const int counter = data.getCounter();
+        int* lastCounter = sofamatrix::imgui::counter(data);
+
+        const bool updateTexture = lastCounter == nullptr || *lastCounter != counter;
+        sofamatrix::imgui::drawTexture(matrix, updateTexture);
+        if (lastCounter)
+        {
+            *lastCounter = counter;
+        }
+    }
+    else
+    {
+        ImGui::Text("Invalid matrix");
+    }
 }
 }
 
-const bool dw_bool = sofaimgui::DataWidgetFactory::Add<sofa::type::BaseMatrixImageProxy>();
+const bool dw_matrixproxy = sofaimgui::DataWidgetFactory::Add<sofa::type::BaseMatrixImageProxy>();
