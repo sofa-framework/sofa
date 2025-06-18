7641b34a64b6110e077523701ab5bafe74da712d
EulalieCoevoet <eulalie.coevoet@gmail.com>
2025-06-18 11:28:52 +0200

    [Engine] VolumeFrom: updates header

--- Sofa/Component/Engine/Generate/tests/VolumeFromTetrahedrons_test.cpp
+++ Sofa/Component/Engine/Generate/tests/VolumeFromTetrahedrons_test.cpp
@@ -1,32 +1,24 @@
 /******************************************************************************
-*               SOFA, Simulation Open-Framework Architecture                  *
-*                (c) 2006-2018 INRIA, USTL, UJF, CNRS, MGH                    *
-*                                                                             *
-* This library is free software; you can redistribute it and/or modify it     *
-* under the terms of the GNU Lesser General Public License as published by    *
-* the Free Software Foundation; either version 2.1 of the License, or (at     *
-* your option) any later version.                                             *
-*                                                                             *
-* This library is distributed in the hope that it will be useful, but WITHOUT *
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
-* for more details.                                                           *
-*                                                                             *
-* You should have received a copy of the GNU Lesser General Public License    *
-* along with this library; if not, write to the Free Software Foundation,     *
-* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
-*******************************************************************************
-*                           Plugin SoftRobots v1.0                            *
-*                                                                             *
-* This plugin is also distributed under the GNU LGPL (Lesser General          *
-* Public License) license with the same conditions than SOFA.                 *
-*                                                                             *
-* Contributors: Defrost team  (INRIA, University of Lille, CNRS,              *
-*               Ecole Centrale de Lille)                                      *
-*                                                                             *
-* Contact information: https://project.inria.fr/softrobot/contact/            *
-*                                                                             *
-******************************************************************************/
+ *                 SOFA, Simulation Open-Framework Architecture                *
+ *                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+ *                                                                             *
+ * This program is free software; you can redistribute it and/or modify it     *
+ * under the terms of the GNU Lesser General Public License as published by    *
+ * the Free Software Foundation; either version 2.1 of the License, or (at     *
+ * your option) any later version.                                             *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful, but WITHOUT *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+ * for more details.                                                           *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public License    *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+ *******************************************************************************
+ * Authors: The SOFA Team and external contributors (see Authors.txt)          *
+ *                                                                             *
+ * Contact information: contact@sofa-framework.org                             *
+ ******************************************************************************/
 
 #include <string>
 using std::string ;
