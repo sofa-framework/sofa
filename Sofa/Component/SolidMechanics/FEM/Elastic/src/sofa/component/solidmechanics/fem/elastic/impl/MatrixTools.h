1dcbc95ce2aa91037496db3deaf51843ed453e23
Alex Bilger <alexbilger0@gmail.com>
2026-03-02 17:44:29 +0100

    remove determinantSquareMatrix as it is now integrated into SOFA

--- Sofa/Component/SolidMechanics/FEM/Elastic/src/sofa/component/solidmechanics/fem/elastic/impl/MatrixTools.h
+++ Sofa/Component/SolidMechanics/FEM/Elastic/src/sofa/component/solidmechanics/fem/elastic/impl/MatrixTools.h
@@ -37,54 +37,16 @@ template <class T>
 using ScalarOrMatrix = std::conditional_t<
     (T::nbLines==1 && T::nbCols==1), typename T::Real, T>;
 
-template <sofa::Size N, typename real>
-real determinantSquareMatrix(const sofa::type::Mat<N, N, real>& mat)
-{
-    if constexpr (N == 1)
-    {
-        return mat(0, 0);
-    }
-    else if constexpr (N == 2)
-    {
-        return mat(0, 0) * mat(1, 1) - mat(0, 1) * mat(1, 0);
-    }
-    else
-    {
-        real det = 0;
-        for (size_t p = 0; p < N; ++p)
-        {
-            sofa::type::Mat<N - 1, N - 1, real> submat;
-            for (size_t i = 1; i < N; ++i)
-            {
-                size_t colIndex = 0;
-                for (size_t j = 0; j < N; ++j)
-                {
-                    if (j == p) continue;
-                    submat(i - 1, colIndex++) = mat(i, j);
-                }
-            }
-            det += ((p % 2 == 0) ? 1 : -1) * mat(0, p) * determinantSquareMatrix(submat);
-        }
-        return det;
-    }
-}
-
-template <sofa::Size N, class real>
-real determinant(const sofa::type::Mat<N, N, real>& mat)
-{
-    return determinantSquareMatrix(mat);
-}
-
 template <sofa::Size L, sofa::Size C, class real>
 real absGeneralizedDeterminant(const sofa::type::Mat<L, C, real>& mat)
 {
     if constexpr (L == C)
     {
-        return std::abs(determinantSquareMatrix(mat));
+        return std::abs(sofa::type::determinant(mat));
     }
     else
     {
-        return std::sqrt(determinantSquareMatrix(mat.multTranspose(mat)));
+        return std::sqrt(sofa::type::determinant(mat.multTranspose(mat)));
     }
 }
 
