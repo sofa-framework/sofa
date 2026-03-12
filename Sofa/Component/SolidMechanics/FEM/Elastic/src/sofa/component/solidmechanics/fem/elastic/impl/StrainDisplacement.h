fd82eb2418fad80ae5ce0e400bfe2111e8a996b7
Alex Bilger <alexbilger0@gmail.com>
2026-03-12 17:30:15 +0100

    fix construction of strain-displacement matrix using Voigt mapping

--- Sofa/Component/SolidMechanics/FEM/Elastic/src/sofa/component/solidmechanics/fem/elastic/impl/StrainDisplacement.h
+++ Sofa/Component/SolidMechanics/FEM/Elastic/src/sofa/component/solidmechanics/fem/elastic/impl/StrainDisplacement.h
@@ -24,116 +24,188 @@
 #include <sofa/type/Mat.h>
 
 #include <sofa/component/solidmechanics/fem/elastic/impl/SymmetricTensor.h>
+#include <sofa/component/solidmechanics/fem/elastic/impl/VoigtNotation.h>
 
 namespace sofa::component::solidmechanics::fem::elastic
 {
 
-template <class DataTypes, class ElementType>
-struct StrainDisplacement;
-
-
-template<sofa::Size nbLines, sofa::Size nbColumns, class real>
-sofa::type::Vec<nbLines, real> strainDisplacementVectorProduct(const sofa::type::Mat<nbLines, nbColumns, real>& B, const sofa::type::Vec<nbColumns, real>& v)
+/**
+ * Computes the product between a strain-displacement matrix B and a displacement vector v.
+ *
+ * This function calculates the strain vector ε = B * v where B is represented in Voigt notation.
+ * For diagonal terms (normal strains), it performs a simple dot product.
+ * For off-diagonal terms (shear strains), it sums the contributions from both partial derivatives.
+ *
+ * @tparam StrainDisplacement The type of the strain-displacement matrix.
+ * @param B The strain-displacement matrix.
+ * @param v The displacement vector.
+ * @return The strain vector in Voigt notation.
+ */
+template <class StrainDisplacement>
+sofa::type::Vec<StrainDisplacement::nbLines,
+                typename StrainDisplacement::Real>
+strainDisplacementVectorProduct(
+    const StrainDisplacement& B,
+    const sofa::type::Vec<StrainDisplacement::nbColumns,
+                          typename StrainDisplacement::Real>& v)
 {
-    return B * v;
-}
+    static constexpr auto spatial_dimensions = StrainDisplacement::spatial_dimensions;
 
-template <sofa::Size nbLines, sofa::Size nbColumns, class real>
-sofa::type::Vec<nbColumns, real> strainDisplacementTransposedVectorProduct(
-    const sofa::type::Mat<nbLines, nbColumns, real>& B, const sofa::type::Vec<nbLines, real>& v)
-{
-    return B.multTranspose(v);
+    sofa::type::Vec<StrainDisplacement::nbLines,
+                    typename StrainDisplacement::Real> product;
+
+    for (std::size_t voigtIndex = 0; voigtIndex < StrainDisplacement::nbLines; ++voigtIndex)
+    {
+        const auto [i, j] = toTensorIndices<spatial_dimensions>(voigtIndex);
+
+        if (i == j)
+        {
+            for (sofa::Size ne = 0; ne < StrainDisplacement::nbNodesInElement; ++ne)
+            {
+                product[voigtIndex] += B(voigtIndex, ne * spatial_dimensions + i) * v(ne * spatial_dimensions + i);
+            }
+        }
+        else
+        {
+            for (sofa::Size ne = 0; ne < StrainDisplacement::nbNodesInElement; ++ne)
+            {
+                product[voigtIndex] +=
+                    B(voigtIndex, ne * spatial_dimensions + i) * v(ne * spatial_dimensions + i) +
+                    B(voigtIndex, ne * spatial_dimensions + j) * v(ne * spatial_dimensions + j);
+            }
+        }
+    }
+
+    return product;
 }
 
 /**
- * Specialization for a linear tetrahedron where the operations containing known zeros in the strain
- * displacement tensor are omitted.
+ * Computes the product between the transpose of a strain-displacement matrix B and a strain vector v.
+ *
+ * This function calculates the nodal force vector f = B^T * v where v is a strain (or stress) vector in Voigt notation.
+ *
+ * @tparam StrainDisplacement The type of the strain-displacement matrix.
+ * @param B The strain-displacement matrix.
+ * @param v The vector to be multiplied by the transpose of B.
+ * @return The resulting vector (typically nodal forces).
  */
-template <class real>
-sofa::type::Vec<6, real> strainDisplacementVectorProduct(const sofa::type::Mat<6, 12, real>& B, const sofa::type::Vec<12, real>& v)
+template <class StrainDisplacement>
+sofa::type::Vec<StrainDisplacement::nbColumns,
+                typename StrainDisplacement::Real>
+strainDisplacementTransposedVectorProduct(
+    const StrainDisplacement& B,
+    const sofa::type::Vec<StrainDisplacement::nbLines,
+                          typename StrainDisplacement::Real>& v)
 {
-    sofa::type::Vec<6, real> Bv{sofa::type::NOINIT};
+    static constexpr auto spatial_dimensions = StrainDisplacement::spatial_dimensions;
 
-    Bv[0] = B(0, 0) * v[0] + B(0, 3) * v[3] + B(0, 6) * v[6] + B(0, 9) * v[9];
-    Bv[1] = B(1, 1) * v[1] + B(1, 4) * v[4] + B(1, 7) * v[7] + B(1, 10) * v[10];
-    Bv[2] = B(2, 2) * v[2] + B(2, 5) * v[5] + B(2, 8) * v[8] + B(2, 11) * v[11];
-    Bv[3] = B(3, 0) * v[0] + B(3, 1) * v[1] + B(3, 3) * v[3] + B(3, 4) * v[4] + B(3, 6) * v[6] + B(3, 7) * v[7] + B(3, 9) * v[9] + B(3, 10) * v[10];
-    Bv[4] = B(4, 0) * v[0] + B(4, 2) * v[2] + B(4, 3) * v[3] + B(4, 5) * v[5] + B(4, 6) * v[6] + B(4, 8) * v[8] + B(4, 9) * v[9] + B(4, 11) * v[11];
-    Bv[5] = B(5, 1) * v[1] + B(5, 2) * v[2] + B(5, 4) * v[4] + B(5, 5) * v[5] + B(5, 7) * v[7] + B(5, 8) * v[8] + B(5, 10) * v[10] + B(5, 11) * v[11];
+    sofa::type::Vec<StrainDisplacement::nbColumns,
+                typename StrainDisplacement::Real> product;
 
-    return Bv;
+    for (std::size_t ne = 0; ne < StrainDisplacement::nbNodesInElement; ++ne)
+    {
+        for (std::size_t voigtIndex = 0; voigtIndex < StrainDisplacement::nbLines; ++voigtIndex)
+        {
+            const auto [i, j] = toTensorIndices<spatial_dimensions>(voigtIndex);
+
+            product[ne * spatial_dimensions + i] +=
+                B(voigtIndex, ne * spatial_dimensions + i) * v[voigtIndex];
+
+            if (i != j)
+            {
+                product[ne * spatial_dimensions + j] +=
+                    B(voigtIndex, ne * spatial_dimensions + j) * v[voigtIndex];
+            }
+        }
+    }
+
+    return product;
 }
 
 /**
- * Specialization for a linear tetrahedron where the operations containing known zeros in the strain
- * displacement tensor are omitted.
+ * Represents the strain-displacement matrix (B-matrix) for a finite element.
+ *
+ * The B-matrix relates the strain vector (in Voigt notation) to the nodal displacement vector: ε = B * u.
+ *
+ * @tparam DataTypes The traits defining the simulation's data types (e.g., Vec3d).
+ * @param ElementType The type of finite element (e.g., Tetrahedron).
  */
-template<class real>
-sofa::type::Vec<12, real> strainDisplacementTransposedVectorProduct(const sofa::type::Mat<6, 12, real>& B, const sofa::type::Vec<6, real>& v)
-{
-    sofa::type::Vec<12, real> B_Tv { sofa::type::NOINIT };
-
-    B_Tv[0] = B(0, 0) * v[0] + B(3, 0) * v[3] + B(4, 0) * v[4];
-    B_Tv[1] = B(1, 1) * v[1] + B(3, 1) * v[3] + B(5, 1) * v[5];
-    B_Tv[2] = B(2, 2) * v[2] + B(4, 2) * v[4] + B(5, 2) * v[5];
-    B_Tv[3] = B(0, 3) * v[0] + B(3, 3) * v[3] + B(4, 3) * v[4];
-    B_Tv[4] = B(1, 4) * v[1] + B(3, 4) * v[3] + B(5, 4) * v[5];
-    B_Tv[5] = B(2, 5) * v[2] + B(4, 5) * v[4] + B(5, 5) * v[5];
-    B_Tv[6] = B(0, 6) * v[0] + B(3, 6) * v[3] + B(4, 6) * v[4];
-    B_Tv[7] = B(1, 7) * v[1] + B(3, 7) * v[3] + B(5, 7) * v[5];
-    B_Tv[8] = B(2, 8) * v[2] + B(4, 8) * v[4] + B(5, 8) * v[5];
-    B_Tv[9] = B(0, 9) * v[0] + B(3, 9) * v[3] + B(4, 9) * v[4];
-    B_Tv[10] = B(1, 10) * v[1] + B(3, 10) * v[3] + B(5, 10) * v[5];
-    B_Tv[11] = B(2, 11) * v[2] + B(4, 11) * v[4] + B(5, 11) * v[5];
-
-    return B_Tv;
-}
-
-
 template <class DataTypes, class ElementType>
 struct StrainDisplacement
 {
     using Real = sofa::Real_t<DataTypes>;
-    static constexpr auto nbLines = symmetric_tensor::NumberOfIndependentElements<DataTypes::spatial_dimensions>;
-    static constexpr auto nbColumns = ElementType::NumberOfNodes * DataTypes::spatial_dimensions;
+    static constexpr auto spatial_dimensions = DataTypes::spatial_dimensions;
+    static constexpr auto nbLines = symmetric_tensor::NumberOfIndependentElements<spatial_dimensions>;
+    static constexpr auto nbNodesInElement = ElementType::NumberOfNodes;
+    static constexpr auto nbColumns = nbNodesInElement * spatial_dimensions;
 
+    /** Accesses the element at (i, j) in the strain-displacement matrix. */
     constexpr Real& operator()(sofa::Size i, sofa::Size j) noexcept
     {
         return B(i, j);
     }
 
+    /** Accesses the element at (i, j) in the strain-displacement matrix (const). */
     constexpr const Real& operator()(sofa::Size i, sofa::Size j) const noexcept
     {
         return B(i, j);
     }
 
+    /**
+     * Multiplies the B-matrix by a displacement vector.
+     * @param v Displacement vector.
+     * @return Resulting strain vector in Voigt notation.
+     */
     constexpr sofa::type::Vec<nbLines, Real> operator*(const sofa::type::Vec<nbColumns, Real>& v) const
     {
-        return strainDisplacementVectorProduct(B, v);
+        return strainDisplacementVectorProduct(*this, v);
     }
 
+    /**
+     * Multiplies the transpose of the B-matrix by a matrix.
+     * @tparam C Number of columns in the matrix v.
+     * @param v Matrix to multiply with B^T.
+     * @return Resulting matrix B^T * v.
+     */
     template<sofa::Size C>
     constexpr sofa::type::Mat<nbColumns, C, Real> multTranspose(const sofa::type::Mat<nbLines, C, Real>& v) const noexcept
     {
         return B.multTranspose(v);
     }
 
+    /**
+     * Multiplies the transpose of the B-matrix by a strain vector.
+     * @param v Strain (or stress) vector in Voigt notation.
+     * @return Resulting nodal force vector.
+     */
     constexpr sofa::type::Vec<nbColumns, Real> multTranspose(const sofa::type::Vec<nbLines, Real>& v) const noexcept
     {
-        return strainDisplacementTransposedVectorProduct(B, v);
+        return strainDisplacementTransposedVectorProduct(*this, v);
     }
 
+    /** Internal representation of the strain-displacement matrix. */
     sofa::type::Mat<nbLines, nbColumns, sofa::Real_t<DataTypes>> B;
 
+    /** Outputs the B-matrix to a stream. */
     friend std::ostream& operator<<(std::ostream& out, const StrainDisplacement& B)
     {
         return operator<<(out, B.B);
     }
 };
 
+/**
+ * Multiplies a matrix by a strain-displacement matrix.
+ * @tparam L Number of lines in matrix A.
+ * @tparam C Number of columns in matrix A (must match nbLines of B).
+ * @param A Matrix to multiply with B.
+ * @param B Strain-displacement matrix.
+ * @return Resulting matrix product A * B.
+ */
 template <sofa::Size L, sofa::Size C, class real, class DataTypes, class ElementType>
 sofa::type::Mat<L, StrainDisplacement<DataTypes, ElementType>::nbColumns, real>
-operator*(const sofa::type::Mat<L, C, real>& A, const StrainDisplacement<DataTypes, ElementType>& B)
+operator*(
+    const sofa::type::Mat<L, C, real>& A,
+    const StrainDisplacement<DataTypes, ElementType>& B)
 {
     return A * B.B;
 }
@@ -156,23 +228,20 @@ StrainDisplacement<DataTypes, ElementType> makeStrainDisplacement(
 {
     static constexpr sofa::Size spatial_dimensions = DataTypes::spatial_dimensions;
     static constexpr sofa::Size NumberOfNodesInElement = ElementType::NumberOfNodes;
+    static constexpr auto nbVoigtIndices = symmetric_tensor::NumberOfIndependentElements<DataTypes::spatial_dimensions>;
 
     StrainDisplacement<DataTypes, ElementType> B;
-    for (sofa::Size ne = 0; ne < NumberOfNodesInElement; ++ne)
-    {
-        for (sofa::Size i = 0; i < spatial_dimensions; ++i)
-        {
-            B(i, ne * spatial_dimensions + i) = gradientShapeFunctions[ne][i];
-        }
 
-        auto row = spatial_dimensions;
-        for (sofa::Size i = 0; i < spatial_dimensions; ++i)
+    for (std::size_t voigtIndex = 0; voigtIndex < nbVoigtIndices; ++voigtIndex)
+    {
+        const auto [i, j] = toTensorIndices<DataTypes>(voigtIndex);
+        for (sofa::Size ne = 0; ne < NumberOfNodesInElement; ++ne)
         {
-            for (sofa::Size j = i + 1; j < spatial_dimensions; ++j)
+            B(voigtIndex, ne * spatial_dimensions + i) = gradientShapeFunctions[ne][j];
+            if (i != j)
             {
-                B(row, ne * spatial_dimensions + i) = gradientShapeFunctions[ne][j];
-                B(row, ne * spatial_dimensions + j) = gradientShapeFunctions[ne][i];
-                ++row;
+                //engineering shear strain (no factor 1/2)
+                B(voigtIndex, ne * spatial_dimensions + j) = gradientShapeFunctions[ne][i];
             }
         }
     }
