14ce84d2d3855f9a3bea317a93d3f926986f0b20
Paul Baksic <paul.baksic@outlook.fr>
2026-05-13 16:13:14 +0200

    Bring back forward solvers

---
+++ Sofa/Component/IntegrationSchemes/Forward/src/sofa/component/integrationschemes/forward/CentralDifferenceSolver.h
@@ -0,0 +1,62 @@
+/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#pragma once
+#include <sofa/component/integrationschemes/forward/config.h>
+
+#include <sofa/simulation/integrationschemes/ExplicitIntegrationScheme.h>
+
+namespace sofa::component::integrationschemes::forward
+{
+
+/** Explicit time integrator using central difference (also known as Verlet of Leap-frop).
+ *
+ * @see http://www.dynasupport.com/support/tutorial/users.guide/time.integration
+ * @see http://en.wikipedia.org/wiki/Leapfrog_method
+ *
+ */
+class SOFA_COMPONENT_INTEGRATIONSCHEMES_FORWARD_API CentralDifferenceSolver : public simulation::integrationschemes::ExplicitIntegrationScheme
+{
+public:
+    SOFA_CLASS(CentralDifferenceSolver, simulation::integrationschemes::ExplicitIntegrationScheme);
+protected:
+    CentralDifferenceSolver();
+public:
+
+    Data<SReal> d_rayleighMass; ///< Rayleigh damping coefficient related to mass
+    Data<bool> d_threadSafeVisitor; ///< If true, do not use realloc and free visitors in fwdInteractionForceField.
+
+    virtual void integrate(const core::ExecParams* params, SReal dt, sofa::core::MultiVecCoordId xResult, sofa::core::MultiVecDerivId vResult) override;
+
+    virtual SReal getVelocityIntegrationFactor() const override
+    {
+        return m_dt;
+    }
+
+    virtual SReal getPositionIntegrationFactor() const override
+    {
+        return m_dt * m_dt;
+    }
+
+
+};
+
+} // namespace sofa::component::odesolver::forward
