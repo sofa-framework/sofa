3dba4662b673c86df2323d65f6d311a9375988d9
Frederick Roy <fredroy@users.noreply.github.com>
2026-06-15 16:22:16 +0900

    [SofaCUDA] ElementFEMForceField: Generic CUDA implementation (#6071)
    * [FEM.Elastic] Remove complex unproductive stiffness matrix data structure
    
    * explicit namespace
    
    * wip
    
    * add example
    
    * new version
    
    * improvment of new version
    
    * add cuda version of ElementLinearSmallStrainFEMForceField
    
    * add cuda version of addforce for ElementLinearSmallStrainFEMForceField
    
    * add cuda version of addforce for ElementCorotationalFEMForceField
    
    * update addforce to compute everything on GPU
    
    * try to fix with direct solver
    
    * fix corot
    
    * update examples
    
    * organize examples and add cpu-gpu comparison
    
    * add double version (templates)
    
    * add benchmarks
    
    * template CUDA kernels on Dim and remove runtime dispatch
    
    Replace hardcoded 3D assumption and extern "C" + switch(nbNodesPerElem) runtime dispatch with fully compile-time C++ template parameters <T, NNodes, Dim>. All kernel dimensions, stiffness block sizes,
       and gather loops are now generic over Dim. The .inl callers use a single template call with constexpr nNodes and dim from the trait, eliminating both the if-constexpr type branching and the runtime
      NNodes switch. Explicit template instantiations in the .cu files provide the needed symbols. Applied to both ElementLinearSmallStrainFEMForceField and ElementCorotationalFEMForceField CUDA
      implementations.
    
    * dont compile double version if SOFA_GPU_CUDA_DOUBLE is not enabled
    
    * use w accessors
    
    * Revert "use w accessors"
    
    This reverts commit e41d0b69300580da430bb878199069d7b845bb71.
    
    * refactor cuda code
    
    * clarify code
    
    * fix to follow change from removestiffness branch rebase
    
    ---------
    
    Co-authored-by: Alex Bilger <alexbilger0@gmail.com>

---
+++ applications/plugins/SofaCUDA/Component/src/SofaCUDA/component/solidmechanics/fem/elastic/CudaElementLinearSmallStrainFEMForceField.h
@@ -0,0 +1,128 @@
+/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#pragma once
+
+#include <sofa/gpu/cuda/CudaTypes.h>
+#include <sofa/component/solidmechanics/fem/elastic/ElementLinearSmallStrainFEMForceField.h>
+
+namespace sofa::gpu::cuda
+{
+
+template<typename T, int NNodes, int Dim>
+void ElementLinearSmallStrainFEMForceFieldCuda_addForce(
+    unsigned int nbElem,
+    unsigned int nbVertex,
+    unsigned int maxElemPerVertex,
+    const void* elements,
+    const void* stiffness,
+    const void* x,
+    const void* x0,
+    void* f,
+    void* eforce,
+    const void* velems);
+
+template<typename T, int NNodes, int Dim>
+void ElementLinearSmallStrainFEMForceFieldCuda_addDForce(
+    unsigned int nbElem,
+    unsigned int nbVertex,
+    unsigned int maxElemPerVertex,
+    const void* elements,
+    const void* stiffness,
+    const void* dx,
+    void* df,
+    void* eforce,
+    const void* velems,
+    T kFactor);
+
+} // namespace sofa::gpu::cuda
+
+namespace sofa::component::solidmechanics::fem::elastic
+{
+
+/**
+ * CUDA-accelerated version of ElementLinearSmallStrainFEMForceField.
+ *
+ * Works with any element type (Edge, Triangle, Quad, Tetrahedron, Hexahedron).
+ * Both addForce and addDForce run entirely on GPU.
+ *
+ * Uses a two-kernel approach for addDForce:
+ *   Kernel 1: compute per-element forces (1 thread/element, fully unrolled)
+ *   Kernel 2: gather per-vertex (1 thread/vertex, no atomics)
+ *
+ * Compared to the corotational version, no rotation matrices are needed.
+ */
+template<class DataTypes, class ElementType>
+class CudaElementLinearSmallStrainFEMForceField
+    : public ElementLinearSmallStrainFEMForceField<DataTypes, ElementType>
+{
+public:
+    SOFA_CLASS(
+        SOFA_TEMPLATE2(CudaElementLinearSmallStrainFEMForceField, DataTypes, ElementType),
+        SOFA_TEMPLATE2(ElementLinearSmallStrainFEMForceField, DataTypes, ElementType));
+
+    using Real = sofa::Real_t<DataTypes>;
+    using Coord = sofa::Coord_t<DataTypes>;
+    using Deriv = sofa::Deriv_t<DataTypes>;
+    using VecCoord = sofa::VecCoord_t<DataTypes>;
+    using VecDeriv = sofa::VecDeriv_t<DataTypes>;
+
+    static const std::string GetCustomClassName()
+    {
+        return ElementLinearSmallStrainFEMForceField<DataTypes, ElementType>::GetCustomClassName();
+    }
+
+    static const std::string GetCustomTemplateName()
+    {
+        return DataTypes::Name();
+    }
+
+    void init() override;
+
+    void addForce(
+        const sofa::core::MechanicalParams* mparams,
+        sofa::DataVecDeriv_t<DataTypes>& f,
+        const sofa::DataVecCoord_t<DataTypes>& x,
+        const sofa::DataVecDeriv_t<DataTypes>& v) override;
+
+    void addDForce(
+        const sofa::core::MechanicalParams* mparams,
+        sofa::DataVecDeriv_t<DataTypes>& df,
+        const sofa::DataVecDeriv_t<DataTypes>& dx) override;
+
+protected:
+
+    CudaElementLinearSmallStrainFEMForceField() = default;
+
+    void uploadStiffnessAndConnectivity();
+
+    gpu::cuda::CudaVector<Real> m_gpuStiffness;      ///< Symmetric block-format stiffness per element
+    gpu::cuda::CudaVector<int>  m_gpuElements;        ///< SoA connectivity: elements[nodeIdx * nbElem + elemId]
+    gpu::cuda::CudaVector<Real> m_gpuElementForce;    ///< Intermediate per-element per-node force buffer
+    gpu::cuda::CudaVector<int>  m_gpuVelems;          ///< SoA vertex-to-element mapping, 0-terminated
+
+    unsigned int m_maxElemPerVertex = 0;
+    unsigned int m_nbVertices = 0;
+
+    bool m_gpuDataUploaded = false;
+};
+
+} // namespace sofa::component::solidmechanics::fem::elastic
---
+++ applications/plugins/SofaCUDA/examples/ElementFEMForcefield/benchmarks/Hexahedron_corotational.py
@@ -0,0 +1,93 @@
+import Sofa
+
+import os
+import numpy as np
+from utilities import generate_regular_grid
+
+g_grid_min_corner=(0, 6, -2)
+g_grid_max_corner=(16, 10, 2)
+
+g_fem_version = os.environ.get('FEM_VERSION', 'new') #either 'new' or 'legacy'
+g_fem_template = os.environ.get('FEM_TEMPLATE', 'Vec3d')
+
+# default is (76, 16, 16)
+g_grid_nx = int(os.environ.get('NX', '76'))
+g_grid_ny = int(os.environ.get('NY', '16'))
+g_grid_nz = int(os.environ.get('NZ', '16'))
+
+g_nb_steps = int(os.environ.get('NBSTEPS', '1000'))
+
+def createScene(root_node):
+    root_node.name = "root"
+    root_node.gravity = (0, -9, 0)
+    root_node.dt = 0.01
+
+    plugin_node = root_node.addChild('Plugins')
+    plugin_node.addObject('RequiredPlugin', pluginName="Sofa.Component.Engine.Select")
+    plugin_node.addObject('RequiredPlugin', pluginName="Sofa.Component.LinearSolver.Iterative")
+    plugin_node.addObject('RequiredPlugin', pluginName="Sofa.Component.ODESolver.Backward")
+    plugin_node.addObject('RequiredPlugin', pluginName="Sofa.Component.StateContainer")
+    plugin_node.addObject('RequiredPlugin', pluginName="Sofa.Component.Topology.Container.Dynamic")
+    plugin_node.addObject('RequiredPlugin', pluginName="Sofa.Component.Topology.Container.Grid")
+    plugin_node.addObject('RequiredPlugin', pluginName="Sofa.Component.Visual")
+    plugin_node.addObject('RequiredPlugin', pluginName='Sofa.Component.Constraint.Projective') # Needed to use components [FixedProjectiveConstraint]  
+    plugin_node.addObject('RequiredPlugin', pluginName='Sofa.Component.Mass') # Needed to use components [DiagonalMass]  
+    plugin_node.addObject('RequiredPlugin', pluginName='Sofa.Component.SolidMechanics.FEM.Elastic') # Needed to use components [HexahedronCorotationalFEMForceField]
+    plugin_node.addObject('RequiredPlugin', pluginName='SofaCUDA.Component')
+    plugin_node.addObject('VisualStyle', displayFlags="showBehaviorModels showForceFields")
+
+    root_node.addObject('DefaultAnimationLoop')
+    root_node.addObject('VisualStyle', displayFlags="showBehaviorModels showForceFields")
+
+    grid_nodes, grid_hexa = generate_regular_grid(nx=g_grid_nx, ny=g_grid_ny, nz=g_grid_nz, min_corner=g_grid_min_corner, max_corner=g_grid_max_corner)
+
+    hexahedron_node = root_node.addChild('Hexahedron')
+    hexahedron_node.addObject('EulerImplicitSolver', rayleighStiffness="0.1", rayleighMass="0.1")
+    hexahedron_node.addObject('CGLinearSolver', iterations="250", name="linear_solver", tolerance="1.0e-12", threshold="1.0e-12")
+    hexahedron_node.addObject('MechanicalObject', name="ms", template=g_fem_template, position=grid_nodes)
+    hexahedron_node.addObject('HexahedronSetTopologyContainer', hexahedra=grid_hexa)
+    hexahedron_node.addObject('DiagonalMass', totalMass="50.0")
+    hexahedron_node.addObject('BoxROI', name="boxroi1", box="-0.1 5 -3 0.1 11 3", drawBoxes="1")
+    hexahedron_node.addObject('FixedProjectiveConstraint', indices="@boxroi1.indices")
+    if g_fem_version == "legacy":
+        hexahedron_node.addObject('HexahedronFEMForceField', name="LegacyFEM", template=g_fem_template, youngModulus="4000", poissonRatio="0.3", method="large")
+    if g_fem_version == "new":
+        hexahedron_node.addObject('HexahedronCorotationalFEMForceField', name="NewFEM", template=g_fem_template, youngModulus="4000", poissonRatio="0.3")
+
+def main():
+
+    enable_gui = False
+
+    try:
+        import Sofa.Gui
+        import SofaImGui
+    except:
+        enable_gui = False
+    
+    root = Sofa.Core.Node("root")
+    createScene(root)
+    
+    Sofa.Simulation.initRoot(root)
+
+    if enable_gui:
+        Sofa.Gui.GUIManager.Init("myscene","imgui")
+        Sofa.Gui.GUIManager.createGUI(root, __file__)
+        Sofa.Gui.GUIManager.MainLoop(root)
+        Sofa.Gui.GUIManager.closeGUI()
+    else:
+        import time
+
+        print(f"Running on {g_nb_steps} steps...")
+        start_timer = time.time()
+
+        for iteration in range(g_nb_steps):
+            Sofa.Simulation.animate(root, root.dt.value)
+
+        stop_timer = time.time()
+        print(f"... Done.")
+        print(f"{g_nb_steps} steps done in {stop_timer - start_timer:.3}s ({g_nb_steps/(stop_timer - start_timer):.5} fps).")
+
+
+# Function used only if this script is called from a python environment
+if __name__ == '__main__':
+    main()
