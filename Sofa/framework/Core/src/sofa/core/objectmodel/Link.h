7c79cf5998d7b1d7a4ea25a4b5bb3087343f38ed
Lucas-TJ <lucasbureltojo@gmail.com>
2026-09-23 03:09:41 +0200

    [CORE] Refactoring add functions from BaseLink and Link (#6129)
    * cleaning add functions
    
    * add unit test
    
    * [Project] Start dev phase v26.12 (#6195)
    
    * [v26.06] Add changelog  (#6180)
    
    Add changelog for v26.06
    
    * Bump version to v26.12.99
    
    * forgotten build action
    
    ---------
    
    Co-authored-by: Paul Baksic <30337881+bakpaul@users.noreply.github.com>
    
    * [Scene] TetrahedronHyperelasticityFEMForceField.scn: add correct plugin for topology (#6212)
    
    fix required plugin for TetrahedronHyperelasticityFEMForceField
    
    * [CORE] Add remove function in BaseLink (#6130)
    
    * add doremove
    
    * clean
    
    * add unit test
    
    * remove useless include
    
    * [Tests] Fix LCPForceFeedbackTests (#6213)
    
    * Fix unit test for forcefeedback
    
    * Fix forces values
    
    * [Plugins] Set SofaImplicitField as supported plugin (#6189)
    
    Remove plugin from sources and add it to the preset
    
    * [Engine] Rename VolumeFromTetrahedrons to VolumeFromVolumetricElements (#6175)
    
    * rename to VolumeFromVolumetricElements
    
    * compat
    
    * last changes
    
    * rename test file
    
    * rename example
    
    * add to component change list
    
    * add alias
    
    * [Mapper] Missing `override` keyword (#6215)
    
    * cleaning add functions
    
    * add unit test
    
    * [CORE] Add remove function in BaseLink (#6130)
    
    * add doremove
    
    * clean
    
    * add unit test
    
    * remove useless include
    
    * [CORE] Add remove function in BaseLink (#6130)
    
    * add doremove
    
    * clean
    
    * add unit test
    
    * remove useless include
    
    * cleaning add functions
    
    * add unit test
    
    * cleaning
    
    * cleaning
    
    * restore and rename functions in Link.h
    
    * cleaning
    
    * cleaning
    
    * revert changes + Add addRawPtr method to BaseLink
    
    * cleaning
    
    ---------
    
    Co-authored-by: Hugo <hugo.talbot@sofa-framework.org>
    Co-authored-by: Paul Baksic <30337881+bakpaul@users.noreply.github.com>
    Co-authored-by: Frederick Roy <fredroy@users.noreply.github.com>
    Co-authored-by: Alex Bilger <alxbilger@users.noreply.github.com>

--- Sofa/framework/Core/src/sofa/core/objectmodel/Link.h
+++ Sofa/framework/Core/src/sofa/core/objectmodel/Link.h
@@ -506,6 +506,24 @@ protected:
         return TLink::add(destptr, path);
     }
 
+    bool addRawPtr(Base* baseptr) override
+    {
+        /// If the pointer is null and the path empty we do nothing
+        if(!baseptr)
+            return false;
+
+        /// Downcast the pointer to a compatible type and
+        /// If the types are not compatible with the Link we returns false
+        auto destptr = castTo<DestType*>(baseptr);
+        if(!destptr)
+        {
+            return false;
+        }
+
+        /// TLink:adding accepts nullptr (for a not yet resolved link).
+        return TLink::add(destptr);
+    }
+
     /// Returns false on type mismatch
     bool _doSet_(Base* baseptr, const size_t index) override
     {
